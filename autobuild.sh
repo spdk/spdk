7004492324352780e5b702497f1df643dbb04192
Daniel Verkamp <daniel.verkamp@intel.com>
2015-09-23 08:52:44 -0700

    build: check formatting with astyle
    Change-Id: Ic09b4dd7ccccb557e8236fc0b7e3105fce1644b3
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -10,6 +10,8 @@ DPDK_DIR=/usr/local/dpdk-2.1.0/x86_64-native-linuxapp-gcc
 
 cd $src
 
+./scripts/check_format.sh
+
 scanbuild=''
 if hash scan-build; then
 	scanbuild="scan-build -o $out/scan-build-tmp --status-bugs"
