555a3d85c65c0e04ab801b9b90043af605b3c737
Daniel Verkamp <daniel.verkamp@intel.com>
2015-11-04 11:02:40 -0700

    build: add Travis CI integration
    Use the container-based travis-ci.org infrastructure to run only the unit
    tests.
    
    Change-Id: I4f7d011edb07341a668fa6fea495f6eb2a9c029b
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

---
+++ unittest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xe
+
+make -C test/lib/nvme/unit
+
+test/lib/nvme/unit/nvme_c/nvme_ut
+test/lib/nvme/unit/nvme_ctrlr_c/nvme_ctrlr_ut
+test/lib/nvme/unit/nvme_ctrlr_cmd_c/nvme_ctrlr_cmd_ut
+test/lib/nvme/unit/nvme_ns_cmd_c/nvme_ns_cmd_ut
+test/lib/nvme/unit/nvme_qpair_c/nvme_qpair_ut
