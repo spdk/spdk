bdece622f51271c2efdafbceebe0f4d26eeea236
Daniel Verkamp <daniel.verkamp@intel.com>
2016-05-10 16:17:51 -0700

    trace: add tracepoint library and app
    Change-Id: I472fb7e7a82e1337c6c06b1d3bb4e8a2a13d884a
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

---
+++ include/spdk/trace.h
@@ -0,0 +1,184 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \file
+ * Tracepoint library
+ */
+
+#ifndef _SPDK_TRACE_H_
+#define _SPDK_TRACE_H_
+
+#include <inttypes.h>
+#include <limits.h>
+
+#include <rte_config.h>
+#include <rte_lcore.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SPDK_TRACE_SIZE	 (32 * 1024)
+
+struct spdk_trace_entry {
+	uint64_t	tsc;
+	uint16_t	tpoint_id;
+	uint16_t	poller_id;
+	uint32_t	size;
+	uint64_t	object_id;
+	uint64_t	arg1;
+};
+
+/* If type changes from a uint8_t, change this value. */
+#define SPDK_TRACE_MAX_OWNER (UCHAR_MAX + 1)
+
+struct spdk_trace_owner {
+	uint8_t	type;
+	char	id_prefix;
+};
+
+/* If type changes from a uint8_t, change this value. */
+#define SPDK_TRACE_MAX_OBJECT (UCHAR_MAX + 1)
+
+struct spdk_trace_object {
+	uint8_t	type;
+	char	id_prefix;
+};
+
+#define SPDK_TRACE_MAX_GROUP_ID  16
+#define SPDK_TRACE_MAX_TPOINT_ID (SPDK_TRACE_MAX_GROUP_ID * 64)
+#define SPDK_TPOINT_ID(group, tpoint)	((group * 64) + tpoint)
+
+struct spdk_trace_tpoint {
+	char		name[24];
+	char		short_name[4];
+	uint16_t	tpoint_id;
+	uint8_t		owner_type;
+	uint8_t		object_type;
+	uint8_t		new_object;
+	uint8_t		arg1_is_ptr;
+	uint8_t		arg1_is_alias;
+	char		arg1_name[8];
+};
+
+struct spdk_trace_history {
+	/** Logical core number associated with this structure instance. */
+	int				lcore;
+
+	/**
+	 * Circular buffer of spdk_trace_entry structures for tracing
+	 *  tpoints on this core.  Debug tool spdk_trace reads this
+	 *  buffer from shared memory to post-process the tpoint entries and
+	 *  display in a human-readable format.
+	 */
+	struct spdk_trace_entry		entries[SPDK_TRACE_SIZE];
+
+	/**
+	 * Running count of number of occurrences of each tracepoint on this
+	 *  lcore.  Debug tools can use this to easily count tracepoints such as
+	 *  number of SCSI tasks completed or PDUs read.
+	 */
+	uint64_t			tpoint_count[SPDK_TRACE_MAX_TPOINT_ID];
+
+	/** Index to next spdk_trace_entry to fill in the circular buffer. */
+	uint32_t			next_entry;
+
+};
+
+struct spdk_trace_histories {
+	uint64_t			tsc_rate;
+	uint64_t			tpoint_mask[SPDK_TRACE_MAX_GROUP_ID];
+	struct spdk_trace_history	per_lcore_history[RTE_MAX_LCORE];
+	struct spdk_trace_owner		owner[UCHAR_MAX + 1];
+	struct spdk_trace_object	object[UCHAR_MAX + 1];
+	struct spdk_trace_tpoint	tpoint[SPDK_TRACE_MAX_TPOINT_ID];
+};
+
+
+void spdk_trace_record(uint16_t tpoint_id, uint16_t poller_id, uint32_t size,
+		       uint64_t object_id, uint64_t arg1);
+
+/** Returns the current tpoint mask. */
+uint64_t spdk_trace_get_tpoint_mask(uint32_t group_id);
+
+/** Adds the specified tpoints to the current tpoint mask for the given tpoint group. */
+void spdk_trace_set_tpoints(uint32_t group_id, uint64_t tpoint_mask);
+
+/** Clears the specified tpoints from the current tpoint mask for the given tpoint group. */
+void spdk_trace_clear_tpoints(uint32_t group_id, uint64_t tpoint_mask);
+
+/** Returns a mask of all tracepoint groups which have at least one tracepoint enabled. */
+uint64_t spdk_trace_get_tpoint_group_mask(void);
+
+/** For each tpoint group specified in the group mask, enable all of its tpoints. */
+void spdk_trace_set_tpoint_group_mask(uint64_t tpoint_group_mask);
+
+void spdk_trace_init(const char *shm_name);
+void spdk_trace_cleanup(void);
+
+#define OWNER_NONE 0
+#define OBJECT_NONE 0
+
+void spdk_trace_register_owner(uint8_t type, char id_prefix);
+void spdk_trace_register_object(uint8_t type, char id_prefix);
+void spdk_trace_register_description(const char *name, const char *short_name,
+				     uint16_t tpoint_id, uint8_t owner_type,
+				     uint8_t object_type, uint8_t new_object,
+				     uint8_t arg1_is_ptr, uint8_t arg1_is_alias,
+				     const char *arg1_name);
+
+struct spdk_trace_register_fn {
+	void (*reg_fn)(void);
+	struct spdk_trace_register_fn *next;
+};
+
+void spdk_trace_add_register_fn(struct spdk_trace_register_fn *reg_fn);
+
+#define SPDK_TRACE_REGISTER_FN(fn) 				\
+	static void fn(void);					\
+	struct spdk_trace_register_fn reg_ ## fn = {		\
+		.reg_fn = fn,					\
+		.next = NULL,					\
+	};							\
+	__attribute__((constructor)) static void _ ## fn(void)	\
+	{							\
+		spdk_trace_add_register_fn(&reg_ ## fn);	\
+	}							\
+	static void fn(void)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lib/trace/trace.c
@@ -0,0 +1,253 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/trace.h"
+
+#include <stdint.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <errno.h>
+
+#include <rte_config.h>
+#include <rte_cycles.h>
+#include <rte_lcore.h>
+
+static char g_shm_name[64];
+
+static struct spdk_trace_histories *g_trace_histories;
+static struct spdk_trace_register_fn *g_reg_fn_head = NULL;
+
+void
+spdk_trace_record(uint16_t tpoint_id, uint16_t poller_id, uint32_t size,
+		  uint64_t object_id, uint64_t arg1)
+{
+	struct spdk_trace_history *lcore_history;
+	struct spdk_trace_entry *next_entry;
+	uint64_t tsc;
+
+	/*
+	 * Tracepoint group ID is encoded in the tpoint_id.  Lower 6 bits determine the tracepoint
+	 *  within the group, the remaining upper bits determine the tracepoint group.  Each
+	 *  tracepoint group has its own tracepoint mask.
+	 */
+	if (g_trace_histories == NULL ||
+	    !((1ULL << (tpoint_id & 0x3F)) & g_trace_histories->tpoint_mask[tpoint_id >> 6])) {
+		return;
+	}
+
+	lcore_history = &g_trace_histories->per_lcore_history[rte_lcore_id()];
+	tsc = rte_get_timer_cycles();
+
+	lcore_history->tpoint_count[tpoint_id]++;
+
+	next_entry = &lcore_history->entries[lcore_history->next_entry];
+	next_entry->tsc = tsc;
+	next_entry->tpoint_id = tpoint_id;
+	next_entry->poller_id = poller_id;
+	next_entry->size = size;
+	next_entry->object_id = object_id;
+	next_entry->arg1 = arg1;
+
+	lcore_history->next_entry++;
+	if (lcore_history->next_entry == SPDK_TRACE_SIZE)
+		lcore_history->next_entry = 0;
+}
+
+uint64_t
+spdk_trace_get_tpoint_mask(uint32_t group_id)
+{
+	return g_trace_histories->tpoint_mask[group_id];
+}
+
+void
+spdk_trace_set_tpoints(uint32_t group_id, uint64_t tpoint_mask)
+{
+	g_trace_histories->tpoint_mask[group_id] |= tpoint_mask;
+}
+
+void
+spdk_trace_clear_tpoints(uint32_t group_id, uint64_t tpoint_mask)
+{
+	g_trace_histories->tpoint_mask[group_id] &= ~tpoint_mask;
+}
+
+uint64_t
+spdk_trace_get_tpoint_group_mask(void)
+{
+	uint64_t mask = 0x0;
+	int i;
+
+	for (i = 0; i < 64; i++) {
+		if (spdk_trace_get_tpoint_mask(i) != 0) {
+			mask |= (1ULL << i);
+		}
+	}
+
+	return mask;
+}
+
+void
+spdk_trace_set_tpoint_group_mask(uint64_t tpoint_group_mask)
+{
+	int i;
+
+	for (i = 0; i < 64; i++) {
+		if (tpoint_group_mask & (1ULL << i)) {
+			spdk_trace_set_tpoints(i, -1ULL);
+		}
+	}
+}
+
+void
+spdk_trace_init(const char *shm_name)
+{
+	struct spdk_trace_register_fn *reg_fn;
+	int trace_fd;
+	int i = 0;
+
+	strncpy(g_shm_name, shm_name, sizeof(g_shm_name));
+
+	trace_fd = shm_open(shm_name, O_RDWR | O_CREAT, 0600);
+	if (trace_fd == -1) {
+		fprintf(stderr, "could not shm_open spdk_trace\n");
+		fprintf(stderr, "errno=%d %s\n", errno, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if (ftruncate(trace_fd, sizeof(*g_trace_histories)) != 0) {
+		fprintf(stderr, "could not truncate shm\n");
+		exit(EXIT_FAILURE);
+	}
+
+	g_trace_histories = mmap(NULL, sizeof(*g_trace_histories), PROT_READ | PROT_WRITE,
+				 MAP_SHARED, trace_fd, 0);
+	if (g_trace_histories == NULL) {
+		fprintf(stderr, "could not mmap shm\n");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(g_trace_histories, 0, sizeof(*g_trace_histories));
+
+	g_trace_histories->tsc_rate = rte_get_timer_hz();
+
+	for (i = 0; i < RTE_MAX_LCORE; i++) {
+		g_trace_histories->per_lcore_history[i].lcore = i;
+	}
+
+	reg_fn = g_reg_fn_head;
+	while (reg_fn) {
+		reg_fn->reg_fn();
+		reg_fn = reg_fn->next;
+	}
+}
+
+void
+spdk_trace_cleanup(void)
+{
+	munmap(g_trace_histories, sizeof(struct spdk_trace_histories));
+	shm_unlink(g_shm_name);
+}
+
+void
+spdk_trace_register_owner(uint8_t type, char id_prefix)
+{
+	struct spdk_trace_owner *owner;
+
+	RTE_VERIFY(type != OWNER_NONE);
+
+	/* 'owner' has 256 entries and since 'type' is a uint8_t, it
+	 * can't overrun the array.
+	*/
+	owner = &g_trace_histories->owner[type];
+	RTE_VERIFY(owner->type == 0);
+
+	owner->type = type;
+	owner->id_prefix = id_prefix;
+}
+
+void
+spdk_trace_register_object(uint8_t type, char id_prefix)
+{
+	struct spdk_trace_object *object;
+
+	RTE_VERIFY(type != OBJECT_NONE);
+
+	/* 'object' has 256 entries and since 'type' is a uint8_t, it
+	 * can't overrun the array.
+	*/
+	object = &g_trace_histories->object[type];
+	RTE_VERIFY(object->type == 0);
+
+	object->type = type;
+	object->id_prefix = id_prefix;
+}
+
+void
+spdk_trace_register_description(const char *name, const char *short_name,
+				uint16_t tpoint_id, uint8_t owner_type,
+				uint8_t object_type, uint8_t new_object,
+				uint8_t arg1_is_ptr, uint8_t arg1_is_alias,
+				const char *arg1_name)
+{
+	struct spdk_trace_tpoint *tpoint;
+
+	RTE_VERIFY(tpoint_id != 0);
+	RTE_VERIFY(tpoint_id < SPDK_TRACE_MAX_TPOINT_ID);
+
+	tpoint = &g_trace_histories->tpoint[tpoint_id];
+	RTE_VERIFY(tpoint->tpoint_id == 0);
+
+	strncpy(tpoint->name, name, sizeof(tpoint->name));
+	strncpy(tpoint->short_name, short_name, sizeof(tpoint->short_name));
+	tpoint->tpoint_id = tpoint_id;
+	tpoint->object_type = object_type;
+	tpoint->owner_type = owner_type;
+	tpoint->new_object = new_object;
+	tpoint->arg1_is_ptr = arg1_is_ptr;
+	tpoint->arg1_is_alias = arg1_is_alias;
+	strncpy(tpoint->arg1_name, arg1_name, sizeof(tpoint->arg1_name));
+}
+
+void
+spdk_trace_add_register_fn(struct spdk_trace_register_fn *reg_fn)
+{
+	reg_fn->next = g_reg_fn_head;
+	g_reg_fn_head = reg_fn;
+}
