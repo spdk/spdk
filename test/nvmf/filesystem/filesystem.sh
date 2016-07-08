18ab3da5ad9a6e8db21466d0b190aefec8542632
Daniel Verkamp <daniel.verkamp@intel.com>
2016-07-08 15:43:07 -0700

    test/nvmf: clean up filesystem test
    Inline function that is only called once.
    
    Fix == bashism.
    
    Create the filesystems on the partition created by parted rather than on
    the raw namespace block device.
    
    Add 'sync' invocations to make sure data gets flushed to the block
    device we are testing.
    
    Drop the copy-pasted FIO cleanup code (filesystem test does not run
    FIO).
    
    Change-Id: Id47d68db208a618841291c4184824031476268f1
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/nvmf/filesystem/filesystem.sh
+++ test/nvmf/filesystem/filesystem.sh
@@ -5,32 +5,6 @@ rootdir=$(readlink -f $testdir/../../..)
 source $rootdir/scripts/autotest_common.sh
 source $rootdir/test/nvmf/common.sh
 
-function filesystem_test()
-{
-	mkdir -p  /mnt/device
-
-	devs=`lsblk -l -o NAME | grep nvme`
-
-	for dev in $devs; do
-		parted -s /dev/$dev mklabel msdos
-		parted -s /dev/$dev mkpart primary '0%' '100%'
-		sleep 1
-
-		for fstype in "ext4" "btrfs" "xfs"; do
-			if [ "$fstype" == "ext4" ]; then
-				mkfs.${fstype} -F /dev/${dev}
-			else
-				mkfs.${fstype} -f /dev/${dev}
-			fi
-
-			mount /dev/${dev} /mnt/device
-			touch /mnt/device/aaa
-			rm -rf /mnt/device/aaa
-			umount /mnt/device
-		done
-	done
-}
-
 set -e
 
 if ! rdma_nic_available; then
@@ -46,7 +20,7 @@ nvmfpid=$!
 
 trap "process_core; killprocess $nvmfpid; exit 1" SIGINT SIGTERM EXIT
 
-sleep 10
+sleep 5
 
 modprobe -v nvme-rdma
 
@@ -56,10 +30,35 @@ fi
 
 echo 'traddr='$NVMF_FIRST_TARGET_IP',transport=rdma,nr_io_queues=1,trsvcid='$NVMF_PORT',nqn=nqn.2016-06.io.spdk:cnode1' > /dev/nvme-fabrics
 
-# file system test
-filesystem_test
+mkdir -p /mnt/device
 
-rm -f ./local-job0-0-verify.state
+devs=`lsblk -l -o NAME | grep nvme`
+
+for dev in $devs; do
+	timing_enter parted
+	parted -s /dev/$dev mklabel msdos  mkpart primary '0%' '100%'
+	timing_exit parted
+	sleep 1
+
+	for fstype in "ext4" "btrfs" "xfs"; do
+		timing_enter $fstype
+		if [ $fstype = ext4 ]; then
+			force=-F
+		else
+			force=-f
+		fi
+
+		mkfs.${fstype} $force /dev/${dev}p1
+
+		mount /dev/${dev}p1 /mnt/device
+		touch /mnt/device/aaa
+		sync
+		rm /mnt/device/aaa
+		sync
+		umount /mnt/device
+		timing_exit $fstype
+	done
+done
 
 trap - SIGINT SIGTERM EXIT
 
