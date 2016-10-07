12f56a432f358a396e0f22009e805e1924456796
Daniel Verkamp <daniel.verkamp@intel.com>
2016-10-07 15:24:33 -0700

    travis: install DPDK and other dependencies
    Change the unittest.sh script to assume that all dependencies are
    available and just build the entirety of SPDK from the root directory.
    
    Change-Id: I394fe0cdfe25373c030b2769dd19c1a9827510c3
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- unittest.sh
+++ unittest.sh
@@ -2,11 +2,7 @@
 
 set -xe
 
-make config.h CONFIG_WERROR=y
-make -C lib/cunit CONFIG_WERROR=y
-make -C lib/log CONFIG_WERROR=y
-
-make -C test/lib/nvme/unit CONFIG_WERROR=y
+make CONFIG_WERROR=y
 
 test/lib/nvme/unit/nvme_c/nvme_ut
 test/lib/nvme/unit/nvme_ctrlr_c/nvme_ctrlr_ut
@@ -14,42 +10,23 @@ test/lib/nvme/unit/nvme_ctrlr_cmd_c/nvme_ctrlr_cmd_ut
 test/lib/nvme/unit/nvme_ns_cmd_c/nvme_ns_cmd_ut
 test/lib/nvme/unit/nvme_qpair_c/nvme_qpair_ut
 
-make -C test/lib/ioat/unit CONFIG_WERROR=y
-
 test/lib/ioat/unit/ioat_ut
-
-make -C test/lib/json CONFIG_WERROR=y
 
 test/lib/json/parse/json_parse_ut
 test/lib/json/util/json_util_ut
 test/lib/json/write/json_write_ut
 
-make -C lib/json CONFIG_WERROR=y
-make -C test/lib/jsonrpc CONFIG_WERROR=y
-
 test/lib/jsonrpc/server/jsonrpc_server_ut
 
-make -C test/lib/log CONFIG_WERROR=y
-
 test/lib/log/log_ut
-
-make -C test/lib/nvmf CONFIG_WERROR=y
 
 test/lib/nvmf/request/request_ut
 test/lib/nvmf/session/session_ut
 test/lib/nvmf/subsystem/subsystem_ut
 
-# TODO: allow lib/util to build without DPDK
-#make -C test/lib/scsi CONFIG_WERROR=y
-make -C test/lib/scsi/dev CONFIG_WERROR=y
-make -C test/lib/scsi/lun CONFIG_WERROR=y
-
 test/lib/scsi/dev/dev_ut
 test/lib/scsi/lun/lun_ut
-#test/lib/scsi/scsi_bdev/scsi_bdev_ut
-
-make -C test/lib/util/bit_array CONFIG_WERROR=y
-make -C test/lib/util/io_channel CONFIG_WERROR=y
+test/lib/scsi/scsi_bdev/scsi_bdev_ut
 
 test/lib/util/bit_array/bit_array_ut
 test/lib/util/io_channel/io_channel_ut
