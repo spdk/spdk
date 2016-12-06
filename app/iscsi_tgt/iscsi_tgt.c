2f7b4ff71bb71ba652a155dc3aad565f7b9fa728
Pan Liu <pan.liu@istuary.com>
2016-12-06 15:11:16 +0800

    change the default daemon mode of iscsi tgt to false

--- app/iscsi_tgt/iscsi_tgt.c
+++ app/iscsi_tgt/iscsi_tgt.c
@@ -75,9 +75,9 @@ usage(char *executable_name)
 	printf(" -s size    memory size in MB for DPDK\n");
 	spdk_tracelog_usage(stdout, "-t");
 	printf(" -H         show this usage\n");
+	printf(" -b         run iscsi target background, the default is foreground\n");
 	printf(" -d         disable coredump file enabling\n");
 	printf(" -q         disable notice level logging to stderr\n");
-	printf(" -f         run iscsi target foreground, the default is background\n");
 }
 
 static void
@@ -94,7 +94,7 @@ main(int argc, char **argv)
 {
 	int ch;
 	int rc, app_rc;
-	int daemon_mode = 1;
+	int daemon_mode = 0;
 	struct spdk_app_opts opts = {};
 
 	/* default value in opts structure */
@@ -103,7 +103,7 @@ main(int argc, char **argv)
 	opts.config_file = SPDK_ISCSI_DEFAULT_CONFIG;
 	opts.name = "iscsi";
 
-	while ((ch = getopt(argc, argv, "c:de:i:l:m:n:p:qs:t:fH")) != -1) {
+	while ((ch = getopt(argc, argv, "bc:de:i:l:m:n:p:qs:t:H")) != -1) {
 		switch (ch) {
 		case 'd':
 			opts.enable_coredump = false;
@@ -149,8 +149,8 @@ main(int argc, char **argv)
 		case 's':
 			opts.dpdk_mem_size = atoi(optarg);
 			break;
-		case 'f':
-			daemon_mode = 0;
+		case 'b':
+			daemon_mode = 1;
 			break;
 		case 'H':
 		default:
