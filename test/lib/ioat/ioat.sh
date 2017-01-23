48519cba2f0bc34ef85b3171883493d1c43ff109
Daniel Verkamp <daniel.verkamp@intel.com>
2017-01-23 16:38:35 -0700

    test/ioat: run ioat_kperf in autotest
    Change-Id: I8afc3869c7b854339d9308375ef3ebd47ec99115
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/lib/ioat/ioat.sh
+++ test/lib/ioat/ioat.sh
@@ -16,4 +16,14 @@ timing_enter verify
 $rootdir/examples/ioat/verify/verify -t 1
 timing_exit verify
 
+if [ `uname` = Linux ]; then
+	timing_enter kperf
+	$rootdir/scripts/setup.sh reset
+	insmod $rootdir/examples/ioat/kperf/kmod/dmaperf.ko
+	$rootdir/examples/ioat/kperf/ioat_kperf -n 4 -q 4 -s 12 -t 32
+	rmmod dmaperf.ko
+	$rootdir/scripts/setup.sh
+	timing_exit kperf
+fi
+
 timing_exit ioat
