18027e12e68e6ac3d1a720480177b12c7f7f1db9
Daniel Verkamp <daniel.verkamp@intel.com>
2017-01-24 15:44:19 -0700

    unittest.sh: allow running with valgrind
    This doesn't actually enable valgrind on the Travis CI instance, but it
    allows the possibility of adding it in the future.
    
    Change-Id: Id3787899e7e0f5f7ebbce3919caea8242b6bf706
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- unittest.sh
+++ unittest.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
+#
+# Environment variables:
+#  $valgrind    Valgrind executable name, if desired
 
 set -xe
 
 make CONFIG_WERROR=y
 
-test/lib/nvme/unit/nvme_c/nvme_ut
-test/lib/nvme/unit/nvme_ctrlr_c/nvme_ctrlr_ut
-test/lib/nvme/unit/nvme_ctrlr_cmd_c/nvme_ctrlr_cmd_ut
-test/lib/nvme/unit/nvme_ns_cmd_c/nvme_ns_cmd_ut
-test/lib/nvme/unit/nvme_qpair_c/nvme_qpair_ut
-test/lib/nvme/unit/nvme_pcie_c/nvme_pcie_ut
-test/lib/nvme/unit/nvme_quirks_c/nvme_quirks_ut
+$valgrind test/lib/nvme/unit/nvme_c/nvme_ut
+$valgrind test/lib/nvme/unit/nvme_ctrlr_c/nvme_ctrlr_ut
+$valgrind test/lib/nvme/unit/nvme_ctrlr_cmd_c/nvme_ctrlr_cmd_ut
+$valgrind test/lib/nvme/unit/nvme_ns_cmd_c/nvme_ns_cmd_ut
+$valgrind test/lib/nvme/unit/nvme_qpair_c/nvme_qpair_ut
+$valgrind test/lib/nvme/unit/nvme_pcie_c/nvme_pcie_ut
+$valgrind test/lib/nvme/unit/nvme_quirks_c/nvme_quirks_ut
 
-test/lib/ioat/unit/ioat_ut
+$valgrind test/lib/ioat/unit/ioat_ut
 
-test/lib/json/parse/json_parse_ut
-test/lib/json/util/json_util_ut
-test/lib/json/write/json_write_ut
+$valgrind test/lib/json/parse/json_parse_ut
+$valgrind test/lib/json/util/json_util_ut
+$valgrind test/lib/json/write/json_write_ut
 
-test/lib/jsonrpc/server/jsonrpc_server_ut
+$valgrind test/lib/jsonrpc/server/jsonrpc_server_ut
 
-test/lib/log/log_ut
+$valgrind test/lib/log/log_ut
 
-test/lib/nvmf/request/request_ut
-test/lib/nvmf/session/session_ut
-test/lib/nvmf/subsystem/subsystem_ut
-test/lib/nvmf/direct/direct_ut
+$valgrind test/lib/nvmf/request/request_ut
+$valgrind test/lib/nvmf/session/session_ut
+$valgrind test/lib/nvmf/subsystem/subsystem_ut
+$valgrind test/lib/nvmf/direct/direct_ut
 
-test/lib/scsi/dev/dev_ut
-test/lib/scsi/lun/lun_ut
-test/lib/scsi/scsi_bdev/scsi_bdev_ut
-test/lib/scsi/scsi_nvme/scsi_nvme_ut
+$valgrind test/lib/scsi/dev/dev_ut
+$valgrind test/lib/scsi/lun/lun_ut
+$valgrind test/lib/scsi/scsi_bdev/scsi_bdev_ut
+$valgrind test/lib/scsi/scsi_nvme/scsi_nvme_ut
 
-test/lib/util/bit_array/bit_array_ut
-test/lib/util/io_channel/io_channel_ut
+$valgrind test/lib/util/bit_array/bit_array_ut
+$valgrind test/lib/util/io_channel/io_channel_ut
