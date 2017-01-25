91d7d20c4e173c25c43cab35cdb719c381275b37
Ben Walker <benjamin.walker@intel.com>
2017-01-25 09:49:12 -0700

    event: add a reactor benchmark tool
    Change-Id: I57c7ed3475b19ffb8d00354eabb23ba86294164b
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/lib/event/event.sh
+++ test/lib/event/event.sh
@@ -7,5 +7,6 @@ source $rootdir/scripts/autotest_common.sh
 timing_enter event
 $testdir/event_perf/event_perf -m 0xF -t 5
 $testdir/reactor/reactor -t 1
+$testdir/reactor_perf/reactor_perf -t 5
 $testdir/subsystem/subsystem_ut
 timing_exit event
