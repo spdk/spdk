b0091cfe5ca18d5bbdfa59dc3338cad170e91121
Liang Yan <liang.z.yan@intel.com>
2017-04-06 15:15:40 +0800

    bdev/nvme: Increase maximum controllers to 1024
    Change-Id: I5a7a5f40b0d3c8c21e26f5358bc84f538a62bff4
    Signed-off-by: Liang Yan <liang.z.yan@intel.com>

--- lib/bdev/nvme/blockdev_nvme.h
+++ lib/bdev/nvme/blockdev_nvme.h
@@ -38,7 +38,7 @@
 
 #include "spdk/nvme.h"
 
-#define NVME_MAX_CONTROLLERS 16
+#define NVME_MAX_CONTROLLERS 1024
 
 int spdk_bdev_nvme_create(struct spdk_nvme_transport_id *trid,
 			  const char *base_name,
