f3d129572b60dd0dddd3a334e64cc158292c31d7
John Meneghini <johnm@netapp.com>
2017-04-11 00:36:05 -0400

    bdev: Use env mempool wrappers
    Change-Id: I22f4f7894462c576c447346684a780bbc34f7ba3

--- include/spdk/env.h
+++ include/spdk/env.h
@@ -157,6 +157,11 @@ void spdk_mempool_put(struct spdk_mempool *mp, void *ele);
 void spdk_mempool_put_bulk(struct spdk_mempool *mp, void *const *ele_arr, size_t count);
 
 /**
+ * Return the number of entries in the mempool.
+ */
+size_t spdk_mempool_count(const struct spdk_mempool *pool);
+
+/**
  * \brief Return the number of dedicated CPU cores utilized by
  * 	  this env abstraction
  */
