29e5ba20103489701cda282b7417bb74999f0620
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-05-19 15:06:27 +0200

    vhost: close remaining memory region FDs during backend cleanup
    After changes done in commit f325e71c, closing old FD is delayed until
    VHOST_USER_SET_VRING_ADDR. If VM is closed before this call, original FDs
    remain during vhost_backend_cleanup. This was reported as part of issue #162.
    
    This patch closes second set of FDs during vhost backend cleanup.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: Ieb9d123c987009ac451b6214bb74d2720d852781

--- lib/vhost/rte_vhost/vhost_user.c
+++ lib/vhost/rte_vhost/vhost_user.c
@@ -110,6 +110,12 @@ void
 vhost_backend_cleanup(struct virtio_net *dev)
 {
 	if (dev->mem) {
+		if (dev->has_new_mem_table) {
+			for (i = 0; i < dev->mem->nregions; i++) {
+				close(dev->mem_table_fds[i]);
+			}
+			dev->has_new_mem_table = 0;
+		}
 		free_mem_region(dev);
 		rte_free(dev->mem);
 		dev->mem = NULL;
