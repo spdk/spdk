40626f954e952e08417568054e996ce35f7f52cf
Jim Harris <james.r.harris@intel.com>
2017-05-23 14:29:22 -0700

    test/iscsi: do not rsync dpdk directory for ext4test
    This test sets DPDK_DIR to the main repository so does not build
    DPDK on the mounted iSCSI LUN.  So don't bother copying the
    dpdk subdirectory to the iSCSI LUN which saves a few seconds
    for every test run.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Ieb04f94b7955acbafb283638531ad076afbf2302

--- test/iscsi_tgt/ext4test/ext4test.sh
+++ test/iscsi_tgt/ext4test/ext4test.sh
@@ -95,7 +95,7 @@ for dev in $devs; do
 	mkdir -p /mnt/${dev}dir
 	mount -o sync /dev/$dev /mnt/${dev}dir
 
-	rsync -qav --exclude=".git" $rootdir/ /mnt/${dev}dir/spdk
+	rsync -qav --exclude=".git" --exclude="dpdk" $rootdir/ /mnt/${dev}dir/spdk
 
 	make -C /mnt/${dev}dir/spdk DPDK_DIR=$DPDK_DIR clean
 	make -C /mnt/${dev}dir/spdk DPDK_DIR=$DPDK_DIR -j16
