ad20e73dafa4ffd89f41a2a777810df0362515e4
Jim Harris <james.r.harris@intel.com>
2017-05-24 13:30:12 -0700

    app/stub: add a stub application to accelerate startup
    Starting SPDK applications can take a long time due to
    two factors:  DPDK memory initialization and NVMe
    device initialization.
    
    This stub application facilitates eliminating those
    delays by leveraging DPDK's multi-process mode.  This
    stub application acts as the primary process,
    initializing DPDK as well as all NVMe devices bound
    to userspace modules (uio/vfio).  Then another
    SPDK process can be started using the same instance
    ID, which will make that SPDK process a secondary
    process of the stub and leverage the DPDK memory and
    NVMe device initialization already performed by the
    primary process.
    
    To start this will be used to accelerate the iSCSI
    system level tests (in test/iscsi_tgt) to significantly
    reduce test time in the CI pool.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I3019c7d883f40b3ee8fb3345db013df2afc25645

---
+++ app/stub/stub.c
@@ -0,0 +1,110 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "spdk/env.h"
+#include "spdk/nvme.h"
+
+static void
+usage(char *executable_name)
+{
+	printf("%s [options]\n", executable_name);
+	printf("options:\n");
+	printf(" -i shared memory ID\n");
+	printf(" -m mask    core mask for DPDK\n");
+	printf(" -n channel number of memory channels used for DPDK\n");
+	printf(" -p core    master (primary) core for DPDK\n");
+	printf(" -s size    memory size in MB for DPDK\n");
+	printf(" -H         show this usage\n");
+}
+
+static bool
+probe_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
+	 struct spdk_nvme_ctrlr_opts *opts)
+{
+	return true;
+}
+
+static void
+attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
+	  struct spdk_nvme_ctrlr *ctrlr, const struct spdk_nvme_ctrlr_opts *opts)
+{
+}
+
+int
+main(int argc, char **argv)
+{
+	int ch;
+	struct spdk_env_opts opts = {};
+
+	/* default value in opts structure */
+	spdk_env_opts_init(&opts);
+
+	opts.name = "stub";
+
+	while ((ch = getopt(argc, argv, "i:m:n:p:s:H")) != -1) {
+		switch (ch) {
+		case 'i':
+			opts.shm_id = atoi(optarg);
+			break;
+		case 'm':
+			opts.core_mask = optarg;
+			break;
+		case 'n':
+			opts.dpdk_mem_channel = atoi(optarg);
+			break;
+		case 'p':
+			opts.dpdk_master_core = atoi(optarg);
+			break;
+		case 's':
+			opts.dpdk_mem_size = atoi(optarg);
+			break;
+		case 'H':
+		default:
+			usage(argv[0]);
+			exit(EXIT_SUCCESS);
+		}
+	}
+
+	optind = 1; /* reset the optind */
+
+	spdk_env_init(&opts);
+	spdk_nvme_probe(NULL, NULL, probe_cb, attach_cb, NULL);
+
+	while (1) {
+		sleep(1);
+	}
+
+	return 0;
+}
