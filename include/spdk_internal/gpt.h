4da235e76fdab8c5c2127b5dfe2109a6159492e5
Ziye Yang <optimistyzy@gmail.com>
2017-05-24 14:55:46 +0800

    GPT: add GPT bdev support
    Change-Id: I6a138e1c1d775e8c8bfeede9600f8b0f799ecdad
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

---
+++ include/spdk_internal/gpt.h
@@ -0,0 +1,65 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \file
+ * GPT Interface
+ */
+
+#ifndef SPDK_INTERNAL_GPT_H
+#define SPDK_INTERNAL_GPT_H
+
+#include "spdk/gpt_spec.h"
+#include "spdk/bdev.h"
+
+#define SPDK_GPT_BUFFER_SIZE 32768  /* 32KB */
+
+#define SPDK_GPT_PARTITION_TYPE_UNUSED						\
+	((struct spdk_gpt_guid) {.raw = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
+				 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}})
+#define	SPDK_EQUAL_GPT_UUID(x,y) (memcmp(x, y, sizeof(struct spdk_gpt_guid)) == 0)
+
+struct spdk_gpt {
+	unsigned char *buf;
+	uint64_t lba_start;
+	uint64_t lba_end;
+	uint64_t total_sectors;
+	uint32_t sector_size;
+	struct spdk_mbr *mbr;
+	struct spdk_gpt_header *head;
+	struct spdk_gpt_partition_entry *partitions;
+};
+
+int
+spdk_gpt_parse(struct spdk_gpt *gpt);
+
+#endif  /* SPDK_INTERNAL_GPT_H */
