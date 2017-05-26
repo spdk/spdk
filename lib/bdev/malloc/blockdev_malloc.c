351e55a9f82ddb68c0cc9ddd1fd0e651c8ba0788
Ziye Yang <optimistyzy@gmail.com>
2017-05-26 12:58:04 +0800

    bdev: make (v)bdev init in async manner
    Change-Id: I93684a004e2ae276734edbb4767b5ba1bac3dd48
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/bdev/malloc/blockdev_malloc.c
+++ lib/bdev/malloc/blockdev_malloc.c
@@ -424,7 +424,7 @@ static void free_malloc_disk(struct malloc_disk *mdisk)
 static int blockdev_malloc_initialize(void)
 {
 	struct spdk_conf_section *sp = spdk_conf_find_section(NULL, "Malloc");
-	int NumberOfLuns, LunSizeInMB, BlockSize, i;
+	int NumberOfLuns, LunSizeInMB, BlockSize, i, rc = 0;
 	uint64_t size;
 	struct spdk_bdev *bdev;
 
@@ -434,7 +434,8 @@ static int blockdev_malloc_initialize(void)
 		BlockSize = spdk_conf_section_get_intval(sp, "BlockSize");
 		if ((NumberOfLuns < 1) || (LunSizeInMB < 1)) {
 			SPDK_ERRLOG("Malloc section present, but no devices specified\n");
-			return EINVAL;
+			rc = EINVAL;
+			goto end;
 		}
 		if (BlockSize < 1) {
 			/* Default is 512 bytes */
@@ -445,11 +446,15 @@ static int blockdev_malloc_initialize(void)
 			bdev = create_malloc_disk(size / BlockSize, BlockSize);
 			if (bdev == NULL) {
 				SPDK_ERRLOG("Could not create malloc disk\n");
-				return EINVAL;
+				rc = EINVAL;
+				goto end;
 			}
 		}
 	}
-	return 0;
+
+end:
+	spdk_bdev_module_init_next(rc);
+	return rc;
 }
 
 static void blockdev_malloc_finish(void)
