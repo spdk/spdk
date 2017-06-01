d78956ade53e843274d1c4ee4f2a70c8abfe4c37
Ziye Yang <optimistyzy@gmail.com>
2017-06-01 13:02:54 +0800

    GPT: Add GPT bdev support
    Change-Id: I6a138e1c1d775e8c8bfeede9600f8b0f799ecdad
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -17,6 +17,38 @@ timing_enter bounds
 $testdir/bdevio/bdevio $testdir/bdev.conf
 timing_exit bounds
 
+if [ $(uname -s) = Linux ] && [ -f /usr/sbin/sgdisk ]; then
+
+	echo "[Gpt]" >> $testdir/bdev.conf
+	echo "  Disable Yes" >> $testdir/bdev.conf
+
+	echo "[Rpc]" >> $testdir/bdev.conf
+	echo "  Enable Yes" >> $testdir/bdev.conf
+
+	modprobe nbd
+	$testdir/nbd/nbd -c $testdir/bdev.conf -b Nvme0n1 -n /dev/nbd0 &
+	nbd_pid=$!
+	echo "Process nbd pid: $nbd_pid"
+	waitforlisten $nbd_pid 5260
+
+	#sleep 5 second and make sure process is still exisiting
+	sleep 5
+	if [ `ps aux | awk -F " " '{print $2}' | grep $nbd_pid` ]; then
+		if [ -e /dev/nbd0 ]; then
+			parted -s /dev/nbd0 mklabel gpt mkpart primary '0%' '50%' mkpart primary '50%' '100%'
+			#change the GUID to SPDK GUID value
+			/usr/sbin/sgdisk -u 1:$SPDK_GPT_UUID /dev/nbd0
+			/usr/sbin/sgdisk -u 2:$SPDK_GPT_UUID /dev/nbd0
+		fi
+
+		killprocess $nbd_pid
+	fi
+
+	#restore bdev.conf
+	cp $testdir/bdev.conf.in $testdir/bdev.conf
+	$rootdir/scripts/gen_nvme.sh >> $testdir/bdev.conf
+fi
+
 timing_enter verify
 $testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 32 -s 4096 -w verify -t 1
 timing_exit verify
