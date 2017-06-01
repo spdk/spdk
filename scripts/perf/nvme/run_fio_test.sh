8f25876d056acf65117d34a4a9ccbc92090995f7
John Kariuki <John.K.Kariuki@intel.com>
2017-05-31 20:57:13 -0700

    nvme/fio_test: add nvme performance test scripts
    adding scripts that automate running performance testing using fio for different workloads.
    
    Change-Id: Id6fd0ff28221d8cf8b2c8a77e47288f9739341fc
    Signed-off-by: John Kariuki <John.K.Kariuki@intel.com>

--- scripts/perf/nvme/run_fio_test.sh
+++ scripts/perf/nvme/run_fio_test.sh
@@ -9,5 +9,11 @@ $rootdir/scripts/setup.sh
 # Run Performance Test with 1 SSD
 python run_fio_test.py $rootdir/test/lib/nvme/performance/fio_test.conf $rootdir/examples/nvme/fio_plugin/fio_plugin 1
 
-# Run Performance Test with 8 SSD
+# 2 SSDs test run
+python run_fio_test.py $rootdir/test/lib/nvme/performance/fio_test.conf $rootdir/examples/nvme/fio_plugin/fio_plugin 2
+
+# 4 SSDs test run
+python run_fio_test.py $rootdir/test/lib/nvme/performance/fio_test.conf $rootdir/examples/nvme/fio_plugin/fio_plugin 4
+
+# 8 SSDs test run
 python run_fio_test.py $rootdir/test/lib/nvme/performance/fio_test.conf $rootdir/examples/nvme/fio_plugin/fio_plugin 8
