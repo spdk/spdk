53f30c9f90ce648cd0ba1b79a7896440c626acf2
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-06-02 19:48:11 +0200

    vhost: introduce spdk_vhost_vring_desc_to_iov
    Extract vring_dev to iovec translation logic to common place.
    
    
    Change-Id: I0c6fc5582dfe341e867ce6749ff29d14a9d12d94
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- lib/vhost/vhost_scsi.c
+++ lib/vhost/vhost_scsi.c
@@ -358,8 +358,7 @@ task_data_setup(struct spdk_vhost_task *task,
 
 		/* All remaining descriptors are data. */
 		while (iovcnt < iovcnt_max) {
-			iovs[iovcnt].iov_base = spdk_vhost_gpa_to_vva(vdev, desc->addr);
-			iovs[iovcnt].iov_len = desc->len;
+			spdk_vhost_vring_desc_to_iov(vdev, &iovs[iovcnt], desc);
 			len += desc->len;
 			iovcnt++;
 
@@ -395,8 +394,7 @@ task_data_setup(struct spdk_vhost_task *task,
 
 		/* Process descriptors up to response. */
 		while (!spdk_vhost_vring_desc_is_wr(desc) && iovcnt < iovcnt_max) {
-			iovs[iovcnt].iov_base = spdk_vhost_gpa_to_vva(vdev, desc->addr);
-			iovs[iovcnt].iov_len = desc->len;
+			spdk_vhost_vring_desc_to_iov(vdev, &iovs[iovcnt], desc);
 			len += desc->len;
 			iovcnt++;
 
