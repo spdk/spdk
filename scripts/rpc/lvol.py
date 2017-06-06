51313cf9a09dfb290e9fc8900dbeee5b795ebf31
Ben Walker <benjamin.walker@intel.com>
2017-06-06 14:22:03 -0700

    rpc: Break rpc client into individual python files
    The top level client is unchanged. This is primarily just
    moving code around. The client.py file is the only location
    with new code, which converts the old jsonrpc_call function
    into a class.
    
    Change-Id: I5fb7cd48f77f6affa3d9439128009bf63148acda
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

---
+++ scripts/rpc/lvol.py
@@ -0,0 +1,47 @@
+from client import print_dict, print_array, int_arg
+
+
+def construct_lvol_store(args):
+    params = {'bdev_name': args.bdev_name, 'lvs_name': args.lvs_name}
+    if args.cluster_sz:
+        params['cluster_sz'] = args.cluster_sz
+    print_array(args.client.call('construct_lvol_store', params))
+
+
+def construct_lvol_bdev(args):
+    num_bytes = (args.size * 1024 * 1024)
+    params = {'lvol_name': args.lvol_name, 'size': num_bytes}
+    if (args.uuid and args.lvs_name) or (not args.uuid and not args.lvs_name):
+        print("You need to specify either uuid or name of lvolstore")
+    else:
+        if args.uuid:
+            params['uuid'] = args.uuid
+        if args.lvs_name:
+            params['lvs_name'] = args.lvs_name
+        print_array(args.client.call('construct_lvol_bdev', params))
+
+
+# Logical volume resize feature is disabled, as it is currently work in progress
+#
+# def resize_lvol_bdev(args):
+#     params = {
+#         'name': args.name,
+#         'size': args.size,
+#     }
+#     args.client.call('resize_lvol_bdev', params)
+
+
+def destroy_lvol_store(args):
+    params = {}
+    if (args.uuid and args.lvs_name) or (not args.uuid and not args.lvs_name):
+        print("You need to specify either uuid or name of lvolstore")
+    else:
+        if args.uuid:
+            params['uuid'] = args.uuid
+        if args.lvs_name:
+            params['lvs_name'] = args.lvs_name
+        args.client.call('destroy_lvol_store', params)
+
+
+def get_lvol_stores(args):
+    print_dict(args.client.call('get_lvol_stores'))
---
+++ scripts/rpc/vhost.py
@@ -0,0 +1,73 @@
+from client import print_dict, print_array, int_arg
+
+def set_vhost_controller_coalescing(args):
+    params = {
+        'ctrlr': args.ctrlr,
+        'delay_base_us': args.delay_base_us,
+        'iops_threshold': args.iops_threshold,
+    }
+    args.client.call('set_vhost_controller_coalescing', params)
+
+def construct_vhost_scsi_controller(args):
+    params = {'ctrlr': args.ctrlr}
+
+    if args.cpumask:
+        params['cpumask'] = args.cpumask
+
+    args.client.call('construct_vhost_scsi_controller',
+                     params, verbose=args.verbose)
+
+
+def add_vhost_scsi_lun(args):
+    params = {
+        'ctrlr': args.ctrlr,
+        'bdev_name': args.bdev_name,
+        'scsi_target_num': args.scsi_target_num
+    }
+    args.client.call('add_vhost_scsi_lun', params, verbose=args.verbose)
+
+
+def remove_vhost_scsi_target(args):
+    params = {
+        'ctrlr': args.ctrlr,
+        'scsi_target_num': args.scsi_target_num
+    }
+    args.client.call('remove_vhost_scsi_target', params)
+
+
+def construct_vhost_blk_controller(args):
+    params = {
+        'ctrlr': args.ctrlr,
+        'dev_name': args.dev_name,
+    }
+    if args.cpumask:
+        params['cpumask'] = args.cpumask
+    if args.readonly:
+        params['readonly'] = args.readonly
+    args.client.call('construct_vhost_blk_controller', params)
+
+
+def get_vhost_controllers(args):
+    print_dict(args.client.call('get_vhost_controllers'))
+
+
+def remove_vhost_controller(args):
+    params = {'ctrlr': args.ctrlr}
+    args.client.call('remove_vhost_controller', params)
+
+
+def construct_virtio_user_scsi_bdev(args):
+    params = {
+        'path': args.path,
+        'name': args.name,
+    }
+    if args.vq_count:
+        params['vq_count'] = args.vq_count
+    if args.vq_size:
+        params['vq_size'] = args.vq_size
+    print_dict(args.client.call('construct_virtio_user_scsi_bdev', params))
+
+
+def remove_virtio_scsi_bdev(args):
+    params = {'name': args.name}
+    args.client.call('remove_virtio_scsi_bdev', params)
