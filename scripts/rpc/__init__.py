4dd3f4a7fa2a06ece6bc263f9b208c3d6aeb6cca
Ben Walker <benjamin.walker@intel.com>
2017-06-06 14:22:03 -0700

    rpc: Break rpc client into individual python files
    The top level client is unchanged. This is primarily just
    moving code around. The client.py file is the only location
    with new code, which converts the old jsonrpc_call function
    into a class.
    
    Change-Id: I5fb7cd48f77f6affa3d9439128009bf63148acda
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-on: https://review.gerrithub.io/364316
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

---
+++ scripts/rpc/__init__.py
@@ -0,0 +1,15 @@
+import app
+import bdev
+import client
+import iscsi
+import log
+import lvol
+import nbd
+import net
+import nvmf
+import pmem
+import vhost
+
+
+def get_rpc_methods(args):
+    print_dict(args.client.call('get_rpc_methods'))
