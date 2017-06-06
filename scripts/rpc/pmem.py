a20abc12dfe88c677c7e621aa71a764c13b0c25f
Ben Walker <benjamin.walker@intel.com>
2017-06-06 14:22:03 -0700

    rpc: Break rpc client into individual python files
    The top level client is unchanged. This is primarily just
    moving code around. The client.py file is the only location
    with new code, which converts the old jsonrpc_call function
    into a class.
    
    Change-Id: I5fb7cd48f77f6affa3d9439128009bf63148acda
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

---
+++ scripts/rpc/pmem.py
@@ -0,0 +1,27 @@
+from client import print_dict, print_array, int_arg
+
+
+def create_pmem_pool(args):
+    num_blocks = (args.total_size * 1024 * 1024) / args.block_size
+    params = {'pmem_file': args.pmem_file,
+              'num_blocks': num_blocks,
+              'block_size': args.block_size}
+    args.client.call('create_pmem_pool', params, verbose=args.verbose)
+
+
+def pmem_pool_info(args):
+    params = {'pmem_file': args.pmem_file}
+    print_dict(args.client.call('pmem_pool_info', params, verbose=args.verbose))
+
+
+def delete_pmem_pool(args):
+    params = {'pmem_file': args.pmem_file}
+    args.client.call('delete_pmem_pool', params, verbose=args.verbose)
+
+
+def construct_pmem_bdev(args):
+    params = {
+        'pmem_file': args.pmem_file,
+        'name': args.name
+    }
+    print_array(args.client.call('construct_pmem_bdev', params, verbose=args.verbose))
