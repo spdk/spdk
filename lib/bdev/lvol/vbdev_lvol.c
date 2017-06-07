6ab1015a44a5cd002d89738947dc65927aed9a50
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-06-07 16:05:12 +0200

    lvol: Initial logical volumes code
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I8c65187f37da7f303cb5bef20a89b35b66988c20

---
+++ lib/bdev/lvol/vbdev_lvol.c
@@ -0,0 +1,222 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * This is a simple example of a virtual block device that takes a single
+ * bdev and slices it into multiple smaller bdevs.
+ */
+
+
+#include "spdk/stdinc.h"
+
+#include "spdk/rpc.h"
+#include "spdk/conf.h"
+#include "spdk/endian.h"
+#include "spdk/io_channel.h"
+#include "spdk/lvol.h"
+#include "spdk/blob.h"
+#include "spdk/blob_bdev.h"
+#include "vbdev_lvol.h"
+
+#include "spdk_internal/log.h"
+
+static TAILQ_HEAD(, spdk_lvol_store) g_spdk_lvol_stores = TAILQ_HEAD_INITIALIZER(
+			g_spdk_lvol_stores);
+
+/* TODO:
+static struct spdk_bdev_fn_table vbdev_lvol_fn_table = {
+	.destruct		= vbdev_lvol_destruct,
+	.io_type_supported	= vbdev_lvol_io_type_supported,
+	.submit_request		= vbdev_lvol_submit_request,
+	.get_io_channel		= vbdev_lvol_get_io_channel,
+	.dump_config_json	= vbdev_lvol_dump_config_json,
+};
+ */
+
+static void
+vbdev_lvol_store_free_cb(void *cb_arg, int bserrno)
+{
+	struct vbdev_lvol_store_req *req = cb_arg;
+	struct spdk_bdev *base_bdev = req->base_bdev;
+
+	TAILQ_REMOVE(&g_spdk_lvol_stores, req->lvol_store, lvol_stores);
+	spdk_bdev_unclaim(base_bdev);
+	SPDK_TRACELOG(SPDK_TRACE_VBDEV_LVOL, "lvol store bdev deleted\n");
+	req->cb_fn2(req->cb_arg, 0);
+}
+
+static void
+vbdev_lvol_store_op_complete_cb(void *cb_arg, struct spdk_lvol_store *lvol_store, int bserrno)
+{
+	struct vbdev_lvol_store_req *req = cb_arg;
+	struct spdk_bs_dev *bs_dev = req->bs_dev;
+
+	if (bserrno != 0) {
+		assert(lvol_store == NULL);
+		req->cb_fn(req->cb_arg, lvol_store, -1);
+		bs_dev->destroy(bs_dev);
+		return;
+	}
+
+	assert(lvol_store != NULL);
+
+	TAILQ_INSERT_TAIL(&g_spdk_lvol_stores, lvol_store, lvol_stores);
+	SPDK_TRACELOG(SPDK_TRACE_VBDEV_LVOL, "lvol store bdev inserted\n");
+
+	req->cb_fn(req->cb_arg, lvol_store, 0);
+	free(req);
+
+	return;
+}
+
+int
+vbdev_construct_lvol_store(struct spdk_bdev *base_bdev,
+			   vbdev_lvol_store_op_with_handle_complete cb_fn,
+			   void *cb_arg)
+{
+	struct spdk_bs_dev *bs_dev;
+	struct vbdev_lvol_store_req *vbdev_ls_req;
+
+	if (!spdk_bdev_claim(base_bdev, NULL, NULL)) {
+		SPDK_ERRLOG("Lvol store bdev %s is already claimed\n", spdk_bdev_get_name(base_bdev));
+		return -1;
+	}
+
+	vbdev_ls_req = calloc(1, sizeof(*vbdev_ls_req));
+	if (!vbdev_ls_req) {
+		SPDK_ERRLOG("Cannot alloc memory for vbdev lvol store request pointer\n");
+		return -1;
+	}
+	vbdev_ls_req->base_bdev = base_bdev;
+	vbdev_ls_req->cb_fn = cb_fn;
+	vbdev_ls_req->cb_arg = cb_arg;
+
+	bs_dev = spdk_bdev_create_bs_dev(base_bdev);
+	if (!bs_dev) {
+		spdk_bdev_unclaim(base_bdev);
+		free(vbdev_ls_req);
+		return -1;
+	}
+
+	if (lvol_store_initialize(bs_dev, vbdev_lvol_store_op_complete_cb, vbdev_ls_req)) {
+		spdk_bdev_unclaim(base_bdev);
+		free(vbdev_ls_req);
+		bs_dev->destroy(bs_dev);
+		return -1;
+	}
+
+	return 0;
+}
+
+static void
+vbdev_destruct_lvol_store(struct spdk_lvol_store *lvol_store, vbdev_lvol_store_op_complete cb_fn,
+			  void *cb_arg)
+{
+
+	struct vbdev_lvol_store_req *vbdev_ls_req;
+
+	vbdev_ls_req = calloc(1, sizeof(*vbdev_ls_req));
+	if (!vbdev_ls_req) {
+		SPDK_ERRLOG("Cannot alloc memory for vbdev lvol store request pointer\n");
+		return;
+	}
+	vbdev_ls_req->cb_fn2 = cb_fn;
+	vbdev_ls_req->cb_arg = cb_arg;
+
+	lvol_store_free(lvol_store, vbdev_lvol_store_free_cb, vbdev_ls_req);
+	return;
+}
+
+static void
+vbdev_lvol_store_init(void)
+{
+	int rc = 0;
+	/* Automatic tasting will be done here */
+	/*
+		struct spdk_conf_section *sp;
+		const char *base_bdev_name;
+		int i, rc = 0;
+		struct spdk_bdev *base_bdev;
+
+
+
+		sp = spdk_conf_find_section(NULL, "Lvol");
+		if (sp == NULL) {
+			rc = 0;
+			goto end;
+		}
+
+		for (i = 0; ; i++) {
+			if (!spdk_conf_section_get_nval(sp, "Split", i)) {
+				break;
+			}
+
+			base_bdev_name = spdk_conf_section_get_nmval(sp, "Split", i, 0);
+			if (!base_bdev_name) {
+				SPDK_ERRLOG("lvol configuration missing blockdev name\n");
+				rc = -1;
+				goto end;
+			}
+
+			base_bdev = spdk_bdev_get_by_name(base_bdev_name);
+			if (!base_bdev) {
+				SPDK_ERRLOG("Could not find lvol bdev %s\n", base_bdev_name);
+				rc = -1;
+				goto end;
+			}
+			vbdev_construct_lvol_store(base_bdev, NULL);
+		}
+
+	end:
+	 */
+	spdk_vbdev_module_init_next(rc);
+}
+
+void
+vbdev_empty_destroy(void *cb_arg, int bserrno)
+{
+	return;
+}
+
+static void
+vbdev_lvol_store_fini(void)
+{
+	struct spdk_lvol_store *spdk_lvol_store, *tmp;
+
+	TAILQ_FOREACH_SAFE(spdk_lvol_store, &g_spdk_lvol_stores, lvol_stores, tmp) {
+		vbdev_destruct_lvol_store(spdk_lvol_store, vbdev_empty_destroy, NULL);
+	}
+}
+
+SPDK_VBDEV_MODULE_REGISTER(vbdev_lvol_store_init, vbdev_lvol_store_fini, NULL, NULL, NULL)
+SPDK_LOG_REGISTER_TRACE_FLAG("vbdev_lvol", SPDK_TRACE_VBDEV_LVOL)
---
+++ test/lib/bdev/lvol_ut/lvol_ut.c
@@ -0,0 +1,159 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "spdk_cunit.h"
+
+#include "vbdev_lvol.c"
+
+int g_bserrno;
+
+static struct spdk_bdev g_bdev = {};
+static struct spdk_bs_dev g_bs_dev = {};
+static struct spdk_lvol_store *g_lvol_store = NULL;
+
+bool
+spdk_bdev_claim(struct spdk_bdev *bdev, spdk_bdev_remove_cb_t remove_cb, void *remove_ctx)
+{
+	if (bdev->status == SPDK_BDEV_STATUS_CLAIMED)
+		return true;
+
+	bdev->status = SPDK_BDEV_STATUS_CLAIMED;
+	return false;
+}
+
+void
+spdk_bdev_unclaim(struct spdk_bdev *bdev)
+{
+	bdev->status = SPDK_BDEV_STATUS_UNCLAIMED;
+	return;
+}
+
+const char *
+spdk_bdev_get_name(const struct spdk_bdev *bdev)
+{
+	return "test";
+}
+
+struct spdk_bs_dev *
+spdk_bdev_create_bs_dev(struct spdk_bdev *bdev)
+{
+	return &g_bs_dev;
+}
+
+int
+lvol_store_initialize(struct spdk_bs_dev *bs_dev, spdk_lvol_store_op_complete cb_fn, void *cb_arg)
+{
+	g_lvol_store = calloc(1, sizeof(*g_lvol_store));
+	cb_fn(cb_arg, g_lvol_store, 0);
+	return 0;
+}
+
+int
+lvol_store_free(struct spdk_lvol_store *lvol_store, spdk_lvol_op_complete cb_fn, void *cb_arg)
+{
+	free(g_lvol_store);
+	cb_fn(cb_arg, 0);
+	return 0;
+}
+
+void
+spdk_vbdev_module_init_next(int rc)
+{
+	return;
+}
+
+void
+spdk_vbdev_module_list_add(struct spdk_bdev_module_if *vbdev_module)
+{
+	return;
+}
+
+static void
+lvol_store_op_complete(void *cb_arg, struct spdk_lvol_store *lvol_store, int bserrno)
+{
+	g_bserrno = bserrno;
+	g_lvol_store = lvol_store;
+	return;
+}
+
+
+static void
+lvol_init(void)
+{
+	int rc = 0;
+
+	g_bdev.status = SPDK_BDEV_STATUS_CLAIMED;
+	rc = vbdev_construct_lvol_store(&g_bdev, lvol_store_op_complete, NULL);
+	CU_ASSERT(rc == 0);
+	CU_ASSERT(g_bserrno == 0);
+	CU_ASSERT(g_lvol_store != NULL);
+	free(g_lvol_store);
+}
+
+static void
+lvol_fini(void)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+	CU_pSuite	suite = NULL;
+	unsigned int	num_failures;
+
+	if (CU_initialize_registry() != CUE_SUCCESS) {
+		return CU_get_error();
+	}
+
+	suite = CU_add_suite("lvol", NULL, NULL);
+	if (suite == NULL) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (
+		CU_add_test(suite, "lvol_init", lvol_init) == NULL ||
+		CU_add_test(suite, "lvol_fini", lvol_fini) == NULL
+	) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	num_failures = CU_get_number_of_failures();
+	CU_cleanup_registry();
+	return num_failures;
+}
