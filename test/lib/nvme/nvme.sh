0e58dbe688f06ecebf437d424a6e60ff48df77be
Ziye Yang <optimistyzy@gmail.com>
2017-06-12 13:08:19 +0800

    Autotest: Enable ASAN as if there is asan lib
    It seems that asan does not work well with valgrind.
    If asan is detected, do not use valgrind.
    
    Change-Id: Id4bd5ae0f545aaba7d028e3da14fdddc18682429
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- test/lib/nvme/nvme.sh
+++ test/lib/nvme/nvme.sh
@@ -48,7 +48,11 @@ timing_exit overhead
 if [ -d /usr/src/fio ]; then
 	timing_enter fio_plugin
 	for bdf in $(linux_iter_pci 0108); do
-		/usr/src/fio/fio $rootdir/examples/nvme/fio_plugin/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.} ns=1"
+		if [ $SPDK_CONFIG_ASAN -eq 1 ]; then
+			 LD_PRELOAD=/lib64/libasan.so.3 /usr/src/fio/fio $rootdir/examples/nvme/fio_plugin/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.} ns=1" 
+		else
+			/usr/src/fio/fio $rootdir/examples/nvme/fio_plugin/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.} ns=1"
+		fi
 		break
 	done
 
