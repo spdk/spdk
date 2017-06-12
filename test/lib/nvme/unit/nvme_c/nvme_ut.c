be74ecf79a9634d9de8653861369030e1af0b4af
Paul Luse <paul.e.luse@intel.com>
2017-06-12 14:31:50 -0700

    test: add one unit test showing example of ut_mock wrap
    This patch shows how to use the spdk_mock library which now only
    consists of --wrap capability for a few functions.
    
    Next will be a patch that provides a generic mock capability to
    use either the --wrap feature or a combination of globals and
    stubs to make adding more UT easier wrt mocking. After that is
    in place nvme_ut.c will be updated to use the new library for
    all mocking.
    
    Change-Id: I1a6ffb722da043bb70bd4cfe1afa7c5e39a0fccb
    Signed-off-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-on: https://review.gerrithub.io/365074
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/lib/nvme/unit/nvme_c/nvme_ut.c
+++ test/lib/nvme/unit/nvme_c/nvme_ut.c
@@ -39,6 +39,8 @@
 
 #include "lib/test_env.c"
 
+#include "spdk_internal/spdk_mock.h"
+
 int
 spdk_pci_nvme_enumerate(spdk_pci_enum_cb enum_cb, void *enum_ctx)
 {
@@ -149,6 +151,37 @@ memset_trid(struct spdk_nvme_transport_id *trid1, struct spdk_nvme_transport_id 
 {
 	memset(trid1, 0, sizeof(struct spdk_nvme_transport_id));
 	memset(trid2, 0, sizeof(struct spdk_nvme_transport_id));
+}
+
+static void
+test_nvme_robust_mutex_init_shared(void)
+{
+	pthread_mutex_t mtx;
+	int rc = 0;
+
+	ut_fake_pthread_mutexattr_init = 0;
+	ut_fake_pthread_mutex_init = 0;
+	rc = nvme_robust_mutex_init_shared(&mtx);
+	CU_ASSERT(rc == 0);
+
+	ut_fake_pthread_mutexattr_init = -1;
+	ut_fake_pthread_mutex_init = 0;
+	rc = nvme_robust_mutex_init_shared(&mtx);
+	/* for FreeBSD the only possible return value is 0 */
+#ifndef __FreeBSD__
+	CU_ASSERT(rc != 0);
+#else
+	CU_ASSERT(rc == 0);
+#endif
+
+	ut_fake_pthread_mutexattr_init = 0;
+	ut_fake_pthread_mutex_init = -1;
+	rc = nvme_robust_mutex_init_shared(&mtx);
+#ifndef __FreeBSD__
+	CU_ASSERT(rc != 0);
+#else
+	CU_ASSERT(rc == 0);
+#endif
 }
 
 static void
@@ -364,12 +397,16 @@ int main(int argc, char **argv)
 	}
 
 	if (
-		CU_add_test(suite, "test_opc_data_transfer", test_opc_data_transfer) == NULL ||
+		CU_add_test(suite, "test_opc_data_transfer",
+			    test_opc_data_transfer) == NULL ||
 		CU_add_test(suite, "test_spdk_nvme_transport_id_parse_trtype",
 			    test_spdk_nvme_transport_id_parse_trtype) == NULL ||
 		CU_add_test(suite, "test_spdk_nvme_transport_id_parse_adrfam",
 			    test_spdk_nvme_transport_id_parse_adrfam) == NULL ||
-		CU_add_test(suite, "test_trid_parse_and_compare", test_trid_parse_and_compare) == NULL
+		CU_add_test(suite, "test_trid_parse_and_compare",
+			    test_trid_parse_and_compare) == NULL ||
+		CU_add_test(suite, "test_nvme_robust_mutex_init_shared",
+			    test_nvme_robust_mutex_init_shared) == NULL
 	) {
 		CU_cleanup_registry();
 		return CU_get_error();
