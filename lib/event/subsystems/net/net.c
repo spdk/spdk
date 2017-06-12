87c9176ce3fb5243572b3c0928b022f65f91bdef
Ben Walker <benjamin.walker@intel.com>
2017-06-12 14:53:18 -0700

    net: Add a separate subsystem library
    This breaks the strict dependency on the
    SPDK event framework.
    
    Change-Id: Id93d79bfbc482350dc9802fa84a554f7e11f3826
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/event/subsystems/net/net.c
+++ lib/event/subsystems/net/net.c
@@ -31,56 +31,13 @@
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** \file
- * Net framework abstraction layer
- */
-
-#ifndef SPDK_NET_H
-#define SPDK_NET_H
-
 #include "spdk/stdinc.h"
 
-#include "spdk/queue.h"
+#include "spdk/net.h"
 
-#define IDLE_INTERVAL_TIME_IN_US 5000
+#include "spdk_internal/event.h"
 
-void spdk_interface_init(void);
-int spdk_interface_destroy(void);
+SPDK_SUBSYSTEM_REGISTER(interface, spdk_interface_init, spdk_interface_destroy, NULL)
 
-const char *spdk_net_framework_get_name(void);
-void spdk_net_framework_start(void);
-void spdk_net_framework_clear_socket_association(int sock);
-int spdk_net_framework_fini(void);
-int spdk_net_framework_idle_time(void);
-
-#define SPDK_IFNAMSIZE		32
-#define SPDK_MAX_IP_PER_IFC	32
-
-struct spdk_interface {
-	char name[SPDK_IFNAMSIZE];
-	uint32_t index;
-	uint32_t num_ip_addresses; /* number of IP addresses defined */
-	uint32_t ip_address[SPDK_MAX_IP_PER_IFC];
-	TAILQ_ENTRY(spdk_interface)	tailq;
-};
-
-int spdk_interface_add_ip_address(int ifc_index, char *ip_addr);
-int spdk_interface_delete_ip_address(int ifc_index, char *ip_addr);
-void *spdk_interface_get_list(void);
-
-int spdk_sock_getaddr(int sock, char *saddr, int slen, char *caddr, int clen);
-int spdk_sock_connect(const char *ip, int port);
-int spdk_sock_listen(const char *ip, int port);
-int spdk_sock_accept(int sock);
-int spdk_sock_close(int sock);
-ssize_t spdk_sock_recv(int sock, void *buf, size_t len);
-ssize_t spdk_sock_writev(int sock, struct iovec *iov, int iovcnt);
-
-int spdk_sock_set_recvlowat(int sock, int nbytes);
-int spdk_sock_set_recvbuf(int sock, int sz);
-int spdk_sock_set_sendbuf(int sock, int sz);
-
-bool spdk_sock_is_ipv6(int sock);
-bool spdk_sock_is_ipv4(int sock);
-
-#endif /* SPDK_NET_FRAMEWORK_H */
+SPDK_SUBSYSTEM_REGISTER(net_framework, spdk_net_framework_start, spdk_net_framework_fini, NULL)
+SPDK_SUBSYSTEM_DEPEND(net_framework, interface)
