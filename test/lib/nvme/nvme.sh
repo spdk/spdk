16ec2b9b5ab9a292b796e7741bb5036b73e0b44e
Ziye Yang <optimistyzy@gmail.com>
2017-06-12 13:08:19 +0800

    Autotest: Enable ASAN as if there is asan lib
    It seems that asan does not work well with:
    (1) Valgrind. If asan is enableed, do not use valgrind.
    (2) spdk fio plugin. If asan is enabled, we cannot work
    with it.
    (3) hotplug. If asan is enabled, it will get the segment
    fault by asan.
    
    Change-Id: Id4bd5ae0f545aaba7d028e3da14fdddc18682429
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- test/lib/nvme/nvme.sh
+++ test/lib/nvme/nvme.sh
@@ -48,7 +48,10 @@ timing_exit overhead
 if [ -d /usr/src/fio ]; then
 	timing_enter fio_plugin
 	for bdf in $(linux_iter_pci 0108); do
-		/usr/src/fio/fio $rootdir/examples/nvme/fio_plugin/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.} ns=1"
+		# Only test when ASAN is not enabled. If ASAN is enabled, we cannot test.
+		if [ $SPDK_CONFIG_ASAN -eq 0 ]; then
+			/usr/src/fio/fio $rootdir/examples/nvme/fio_plugin/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.} ns=1"
+		fi
 		break
 	done
 
@@ -61,6 +64,8 @@ timing_exit arbitration
 
 if [ $(uname -s) = Linux ] && [ $SPDK_TEST_NVME_MULTIPROCESS -eq 1 ]; then
 	timing_enter multi_process
+	# disable ALSR
+	echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 	$rootdir/examples/nvme/arbitration/arbitration -i 0 -s 4096 -t 10 -c 0xf &
 	pid=$!
 	sleep 3
@@ -81,6 +86,8 @@ if [ $(uname -s) = Linux ] && [ $SPDK_TEST_NVME_MULTIPROCESS -eq 1 ]; then
 		count=$(($count + 1))
 	done
 	wait $pid
+	# re-enable ALSR
+	echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 	timing_exit multi_process
 fi
 
