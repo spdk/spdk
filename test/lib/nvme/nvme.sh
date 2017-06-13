ef3d6d9824ce90c6b589bf06c24e47a39ce7cc99
Jim Harris <james.r.harris@intel.com>
2017-06-13 10:36:57 -0700

    test/nvme: add histogram to overhead test app
    Also add the nvme.sh test script to enable this new histogram
    when running the overhead tool.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I825de58362ad631808173a1d3d1b4ccb7df3bcf2
    
    Reviewed-on: https://review.gerrithub.io/365265
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/lib/nvme/nvme.sh
+++ test/lib/nvme/nvme.sh
@@ -42,7 +42,7 @@ $rootdir/examples/nvme/hello_world/hello_world
 timing_exit
 
 timing_enter overhead
-$rootdir/test/lib/nvme/overhead/overhead -s 4096 -t 1
+$rootdir/test/lib/nvme/overhead/overhead -s 4096 -t 1 -H
 timing_exit overhead
 
 PLUGIN_DIR=$rootdir/examples/nvme/fio_plugin
