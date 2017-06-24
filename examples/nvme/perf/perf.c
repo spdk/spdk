47bf0149ba92abebaafbd1d125dac05a94e8234b
Jim Harris <james.r.harris@intel.com>
2017-06-23 17:40:17 -0700

    nvme/perf: add additional latency cutoffs
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I1aee7aba522cc816f69709cfc95d12c50a5d0f4b

--- examples/nvme/perf/perf.c
+++ examples/nvme/perf/perf.c
@@ -90,11 +90,14 @@ static const double g_latency_cutoffs[] = {
 	0.75,
 	0.90,
 	0.95,
+	0.98,
 	0.99,
+	0.995,
 	0.999,
 	0.9999,
 	0.99999,
 	0.999999,
+	0.9999999,
 	-1,
 };
 
@@ -715,7 +718,7 @@ check_cutoff(void *ctx, uint64_t start, uint64_t end, uint64_t count,
 
 	so_far_pct = (double)so_far / total;
 	while (so_far_pct >= **cutoff && **cutoff > 0) {
-		printf("%8.4f%% : %9.3fus\n", **cutoff * 100, (double)end * 1000 * 1000 / g_tsc_rate);
+		printf("%9.5f%% : %9.3fus\n", **cutoff * 100, (double)end * 1000 * 1000 / g_tsc_rate);
 		(*cutoff)++;
 	}
 }
