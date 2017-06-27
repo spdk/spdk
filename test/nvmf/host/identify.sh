d1c4f0a193b3d31f58c27ad2c9df0f18da3c526d
Ben Walker <benjamin.walker@intel.com>
2017-06-27 11:26:19 -0700

    nvmf: Remove direct mode
    There is now only virtual mode. Virtual mode has been
    improved enough to reach feature parity with direct
    mode and performance benchmarks show no degradation.
    Simplify the code by always using virtual mode.
    
    Change-Id: Id5cdb5d4d8c54e661b245ed7250c2f9d66ca2152
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/nvmf/host/identify.sh
+++ test/nvmf/host/identify.sh
@@ -30,7 +30,7 @@ timing_exit start_nvmf_tgt
 
 bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
 
-$rpc_py construct_nvmf_subsystem Virtual nqn.2016-06.io.spdk:cnode1 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" "" -s SPDK00000000000001 -n "$bdevs"
+$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" '' -s SPDK00000000000001 -n "$bdevs"
 
 $rootdir/examples/nvme/identify/identify -r "\
         trtype:RDMA \
