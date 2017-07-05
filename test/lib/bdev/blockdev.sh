4e85cf8f078226bdd18c2c3d609c76c082a838ae
Ziye Yang <optimistyzy@gmail.com>
2017-07-05 13:04:15 +0800

    GPT, bdev: Add rpc support and the related test
    Change-Id: I8391d57b72dc9e39f902148e0e347bc5eb77ebdf
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -17,32 +17,7 @@ timing_enter bounds
 $testdir/bdevio/bdevio $testdir/bdev.conf
 timing_exit bounds
 
-if [ $(uname -s) = Linux ] && [ -f /usr/sbin/sgdisk ]; then
-	echo "[Rpc]" >> $testdir/bdev.conf
-	echo "  Enable Yes" >> $testdir/bdev.conf
-
-	modprobe nbd
-	$testdir/nbd/nbd -c $testdir/bdev.conf -b Nvme0n1 -n /dev/nbd0 &
-	nbd_pid=$!
-	echo "Process nbd pid: $nbd_pid"
-	waitforlisten $nbd_pid 5260
-
-	# Make sure nbd process still run. The reaon is that:
-	# nbd program may exist by calling spdk_app_stop due to some issues in
-	# nbd_start function. And we can not catch the abnormal exit.
-	sleep 5
-
-	if [ `ps aux | awk -F " " '{print $2}' | grep $nbd_pid` ]; then
-		if [ -e /dev/nbd0 ]; then
-			parted -s /dev/nbd0 mklabel gpt mkpart primary '0%' '50%' mkpart primary '50%' '100%'
-			#change the GUID to SPDK GUID value
-			/usr/sbin/sgdisk -u 1:$SPDK_GPT_UUID /dev/nbd0
-			/usr/sbin/sgdisk -u 2:$SPDK_GPT_UUID /dev/nbd0
-		fi
-
-		killprocess $nbd_pid
-	fi
-fi
+partition_dev $testdir/bdev.conf Nvme0n1
 
 timing_enter verify
 $testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 32 -s 4096 -w verify -t 1
