497e0a9fa23013773416bf00f51812062ba48e48
Ziye Yang <optimistyzy@gmail.com>
2017-07-05 13:04:15 +0800

    GPT, bdev: Add rpc support and the related rpc test
    Change-Id: I7a73cc8dabbad8e2138090b899e5a03b02c80c86
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -17,28 +17,7 @@ timing_enter bounds
 $testdir/bdevio/bdevio $testdir/bdev.conf
 timing_exit bounds
 
-if [ $(uname -s) = Linux ] && [ -f /usr/sbin/sgdisk ]; then
-	echo "[Rpc]" >> $testdir/bdev.conf
-	echo "  Enable Yes" >> $testdir/bdev.conf
-
-	if [ ! -z "`grep "Nvme0" $testdir/bdev.conf`" ]; then
-		modprobe nbd
-		$testdir/nbd/nbd -c $testdir/bdev.conf -b Nvme0n1 -n /dev/nbd0 &
-		nbd_pid=$!
-		echo "Process nbd pid: $nbd_pid"
-		waitforlisten $nbd_pid 5260
-		#if return 1, it will trap, so do not need to consider this case
-		waitforbdev Nvme0n1 $rootdir/scripts/rpc.py
-
-		if [ -e /dev/nbd0 ]; then
-			parted -s /dev/nbd0 mklabel gpt mkpart primary '0%' '50%' mkpart primary '50%' '100%'
-			#change the GUID to SPDK GUID value
-			/usr/sbin/sgdisk -u 1:$SPDK_GPT_UUID /dev/nbd0
-			/usr/sbin/sgdisk -u 2:$SPDK_GPT_UUID /dev/nbd0
-		fi
-		killprocess $nbd_pid
-	fi
-fi
+partition_dev $testdir/bdev.conf Nvme0n1 $rootdir
 
 timing_enter verify
 $testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 32 -s 4096 -w verify -t 1
