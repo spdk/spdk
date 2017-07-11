ce18c1f12186a5310e72bb7677870486b085513c
Isaac Otsiabah <iotsiabah@us.fujitsu.com>
2017-07-11 16:32:32 -0700

    RPC: Add rpc support to update firmware.
    Modified the above files to add rpc support to update NVMe firmware. Currently,
    the path parameter must be local to the RPC Server.
    
    Change-Id: I2b14e37792a2f0a5759e8b13e21137e7f346e58e
    Signed-off-by: Isaac Otsiabah <iotsiabah@us.fujitsu.com>
    Reviewed-on: https://review.gerrithub.io/369083
    Reviewed-by: Paul von Stamwitz <pvonstamwitz@us.fujitsu.com>
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

--- lib/bdev/nvme/bdev_nvme.h
+++ lib/bdev/nvme/bdev_nvme.h
@@ -43,5 +43,6 @@
 int spdk_bdev_nvme_create(struct spdk_nvme_transport_id *trid,
 			  const char *base_name,
 			  const char **names, size_t *count);
+struct spdk_nvme_ctrlr *spdk_bdev_nvme_get_ctrlr(void *bdev);
 
 #endif // SPDK_BDEV_NVME_H
