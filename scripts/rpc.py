be642a2318fd243201a66947c9237d90f144eca4
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-07-11 07:41:32 +0200

    lvol: Logical volume implementation
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: If6395fe19a3747dd0823f6dad39b120694c5cbf1
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -243,6 +243,35 @@ p = subparsers.add_parser('construct_error_bdev', help='Add bdev with error inje
 p.add_argument('base_name', help='base bdev name')
 p.set_defaults(func=construct_error_bdev)
 
+
+def construct_lvol_store(args):
+    params = {'base_name': args.base_name}
+    jsonrpc_call('construct_lvol_store', params)
+p = subparsers.add_parser('construct_lvol_store', help='Add lvol store on base bdev')
+p.add_argument('base_name', help='base bdev name')
+p.set_defaults(func=construct_lvol_store)
+
+
+def construct_lvol_bdev(args):
+    params = {
+        'guid': args.guid,
+        'size': args.size,
+    }
+    jsonrpc_call('construct_lvol_bdev', params)
+p = subparsers.add_parser('construct_lvol_bdev', help='Add a bdev with an lvol backend')
+p.add_argument('guid', help='lvol store GUID')
+p.add_argument('size', help='size for this bdev', type=int)
+p.set_defaults(func=construct_lvol_bdev)
+
+
+def destroy_lvol_store(args):
+    params = {'guid': args.guid}
+    jsonrpc_call('destroy_lvol_store', params)
+p = subparsers.add_parser('destroy_lvol_store', help='Destroy an lvol store')
+p.add_argument('guid', help='lvol store GUID')
+p.set_defaults(func=destroy_lvol_store)
+
+
 def set_trace_flag(args):
     params = {'flag': args.flag}
     jsonrpc_call('set_trace_flag', params)
