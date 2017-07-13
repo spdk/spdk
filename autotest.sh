02a57addd93185647fe634da19ba93eede09a37b
Jim Harris <james.r.harris@intel.com>
2017-07-12 21:39:51 -0700

    test/vhost: disable vhost-blk tests
    The vhost blk tests are causing a lot of intermittent
    failures in the test pool.  Disable for now while this
    is debugged further.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Ia49db7b48b5850e92f60983348c13f724c8895c0

--- autotest.sh
+++ autotest.sh
@@ -122,7 +122,8 @@ fi
 
 if [ $SPDK_TEST_VHOST -eq 1 ]; then
 	timing_enter vhost
-	run_test ./test/vhost/spdk_vhost.sh --integrity-blk
+	# Disable integrity-blk for now while the intermittent failures are debugged.
+	#run_test ./test/vhost/spdk_vhost.sh --integrity-blk
 	run_test ./test/vhost/spdk_vhost.sh --integrity
 	timing_exit vhost
 fi
