0f08d07908e3dd5c38be50c11c57230e0bc1de13
Ben Walker <benjamin.walker@intel.com>
2017-07-14 16:31:43 -0700

    bdev: Add new tests using the fio plugin
    Change-Id: I90edce0d9aac7be12ce8ba346aa6e16f67628b0e
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -4,19 +4,16 @@ set -e
 
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../../..)
+plugindir=$rootdir/examples/bdev/fio_plugin
+
 source $rootdir/scripts/autotest_common.sh
 
-testdir=$(readlink -f $(dirname $0))
-
-timing_enter blockdev
+timing_enter bdev
 
 cp $testdir/bdev.conf.in $testdir/bdev.conf
 $rootdir/scripts/gen_nvme.sh >> $testdir/bdev.conf
 
-timing_enter bounds
-$testdir/bdevio/bdevio $testdir/bdev.conf
-timing_exit bounds
-
+timing_enter nbd
 if [ $(uname -s) = Linux ] && hash sgdisk; then
 	echo "[Rpc]" >> $testdir/bdev.conf
 	echo "  Enable Yes" >> $testdir/bdev.conf
@@ -51,27 +48,17 @@ if [ $(uname -s) = Linux ] && hash sgdisk; then
 		killprocess $nbd_pid
 	fi
 fi
+timing_exit nbd
 
-timing_enter verify
-$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 32 -s 4096 -w verify -t 1
-timing_exit verify
-
-if [ $RUN_NIGHTLY -eq 1 ]; then
-	# Use size 192KB which both exceeds typical 128KB max NVMe I/O
-	#  size and will cross 128KB Intel DC P3700 stripe boundaries.
-	timing_enter perf
-	$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 128 -w read -s 196608 -t 5
-	timing_exit perf
-
-	# Temporarily disabled - infinite loop
-	#timing_enter reset
-	#$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 16 -w reset -s 4096 -t 60
-	#timing_exit reset
-
-	timing_enter unmap
-	$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 1 -w unmap -s 4096 -t 60
-	timing_exit unmap
+if [ $SPDK_RUN_ASAN -eq 0 ]; then
+	timing_enter fio
+	if [ $RUN_NIGHTLY -eq 0 ]; then
+		LD_PRELOAD=$plugindir/fio_plugin /home/bwalker/src/fio/fio $testdir/bdev.fio --iodepth=8 --bs=4k --runtime=10
+	else
+		LD_PRELOAD=$plugindir/fio_plugin /home/bwalker/src/fio/fio $testdir/bdev.fio --iodepth=128 --bs=192k --runtime=60
+	fi
+	timing_exit fio
 fi
 
 rm -f $testdir/bdev.conf
-timing_exit blockdev
+timing_exit bdev
