0fcb91e81c85e7e1cc7e67f2849cba2b53ed58bc
Ben Walker <benjamin.walker@intel.com>
2017-07-14 16:31:43 -0700

    bdev: Add new tests using the fio plugin
    Change-Id: I90edce0d9aac7be12ce8ba346aa6e16f67628b0e
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -4,11 +4,11 @@ set -e
 
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../../..)
+plugindir=$rootdir/examples/bdev/fio_plugin
+
 source $rootdir/scripts/autotest_common.sh
 
-testdir=$(readlink -f $(dirname $0))
-
-timing_enter blockdev
+timing_enter bdev
 
 cp $testdir/bdev.conf.in $testdir/bdev.conf
 $rootdir/scripts/gen_nvme.sh >> $testdir/bdev.conf
@@ -17,21 +17,28 @@ timing_enter bounds
 $testdir/bdevio/bdevio $testdir/bdev.conf
 timing_exit bounds
 
+timing_enter nbd
 if grep -q Nvme0 $testdir/bdev.conf; then
 	part_dev_by_gpt $testdir/bdev.conf Nvme0n1 $rootdir
 fi
+timing_exit nbd
 
-timing_enter verify
-$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 32 -s 4096 -w verify -t 1
-timing_exit verify
+if [ -d /usr/src/fio ] && [ $SPDK_RUN_ASAN -eq 0 ]; then
+	timing_enter fio
+
+	FIO_CMD="LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio --ioengine=spdk --spdk_conf=./test/lib/bdev/bdev.conf"
+	if [ $RUN_NIGHTLY -eq 0 ]; then
+		$FIO_CMD --iodepth=8 --bs=4k --runtime=10 $testdir/bdev.fio
+	else
+		# Use size 192KB which both exceeds typical 128KB max NVMe I/O
+		#  size and will cross 128KB Intel DC P3700 stripe boundaries.
+		$FIO_CMD --iodepth=128 --bs=192k --runtime=100 $testdir/bdev.fio
+	fi
+
+	timing_exit fio
+fi
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
-	# Use size 192KB which both exceeds typical 128KB max NVMe I/O
-	#  size and will cross 128KB Intel DC P3700 stripe boundaries.
-	timing_enter perf
-	$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 128 -w read -s 196608 -t 5
-	timing_exit perf
-
 	# Temporarily disabled - infinite loop
 	#timing_enter reset
 	#$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 16 -w reset -s 4096 -t 60
@@ -43,4 +50,4 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 fi
 
 rm -f $testdir/bdev.conf
-timing_exit blockdev
+timing_exit bdev
