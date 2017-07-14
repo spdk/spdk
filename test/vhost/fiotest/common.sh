a06ff1d2ebcbd4e88db8231c1a88323b4b33dbb9
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-07-14 15:25:08 +0200

    [DEBUG] test: fio SENT_ETA investigation
    Please do not merge.
    
    Change-Id: I4b40d97eb691bb57cd29602c3d0e8e30426e457c
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- test/vhost/fiotest/common.sh
+++ test/vhost/fiotest/common.sh
@@ -42,14 +42,8 @@ mkdir -p $TEST_DIR
 
 RPC_PORT=5260
 
-# Trace flag is optional, if it wasn't set earlier - disable it after sourcing
-# autotest_common.sh
-if [[ $- =~ x ]]; then
-	source $SPDK_BUILD_DIR/scripts/autotest_common.sh
-else
-	source $SPDK_BUILD_DIR/scripts/autotest_common.sh
-	set +x
-fi
+source $SPDK_BUILD_DIR/scripts/autotest_common.sh
+[[ -z "$x" ]] && set +x
 
 function error()
 {
@@ -127,7 +121,6 @@ function spdk_vhost_run()
 	local vhost_app="$SPDK_BUILD_DIR/app/vhost/vhost"
 	local vhost_log_file="$SPDK_VHOST_SCSI_TEST_DIR/vhost.log"
 	local vhost_pid_file="$SPDK_VHOST_SCSI_TEST_DIR/vhost.pid"
-	local vhost_socket="$SPDK_VHOST_SCSI_TEST_DIR/usvhost"
 	local vhost_conf_template="$BASE_DIR/vhost.conf.in"
 	local vhost_conf_file="$BASE_DIR/vhost.conf"
 	echo "INFO: starting vhost app in background"
@@ -152,7 +145,6 @@ function spdk_vhost_run()
 
 	echo "INFO: Loging to:   $vhost_log_file"
 	echo "INFO: Config file: $vhost_conf_file"
-	echo "INFO: Socket:      $vhost_socket"
 	echo "INFO: Command:     $cmd"
 
 	cd $SPDK_VHOST_SCSI_TEST_DIR; $cmd &
@@ -773,9 +765,11 @@ function vm_start_fio_server()
 	for vm_num in $@; do
 		echo "INFO: Starting fio server on VM$vm_num"
 		if [[ $fio_bin != "" ]]; then
+			echo "INFO: using $fio_bin  (version $(${fio_bin} --version))"
 			cat $fio_bin | vm_ssh $vm_num 'cat > /root/fio; chmod +x /root/fio'
 			vm_ssh $vm_num /root/fio $readonly --eta=never --server --daemonize=/root/fio.pid
 		else
+			echo "INFO: using native guest fio (version $(vm_ssh $vm_num fio --version))"
 			vm_ssh $vm_num fio $readonly --eta=never --server --daemonize=/root/fio.pid
 		fi
 	done
