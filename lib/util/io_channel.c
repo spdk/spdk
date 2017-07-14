spdk/spdk

commit 5533c3d2087bb72ac1b8a8647a1dd87a11acbebf

util: defer put_io_channel
With vbdevs, there are many more cases where the last
(or only) channel for a given I/O device may be destroyed
in the context of the poller for that I/O device.  To
avoid returning to the poller and having it check
for data on a resource that was just released, instead
defer the actual put_io_channel work via
spdk_thread_send_msg().

Signed-off-by: Jim Harris <james.r.harris@intel.com>
Change-Id: Ic99776bddbe9a305f221f8c094ea97dce2d6df0b

Reviewed-on: https://review.gerrithub.io/368619
Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>

lib/util/io_channel.c
@@ -273,9 +273,11 @@ spdk_get_io_channel(void *io_device)
 	return ch;
 }
 
-void
-spdk_put_io_channel(struct spdk_io_channel *ch)
+static void
+_spdk_put_io_channel(void *arg)
 {
+	struct spdk_io_channel *ch = arg;
+
 	if (ch->ref == 0) {
 		SPDK_ERRLOG("ref already zero\n");
 		return;
@@ -288,6 +290,12 @@ spdk_put_io_channel(struct spdk_io_channel *ch)
 		ch->destroy_cb(ch->io_device, (uint8_t *)ch + sizeof(*ch));
 		free(ch);
 	}
+}
+
+void
+spdk_put_io_channel(struct spdk_io_channel *ch)
+{
+	spdk_thread_send_msg(ch->thread, _spdk_put_io_channel, ch);
 }
 
 void *

commit c2c56f7cd527d32fb991bd3c941256ecacac6575

util: destroy related io_channels on io_device unregister
Since patch 5533c3d2 [1], hotremoved nvme devices leave dangling
io_channels even after actual nvme_ctrlr destruction, causing segfaults.

As of my hotremove patch 367824 (3 weeks ago) [2] hotremove path used to
look as following:

Hotremove trigger:
0 spdk_scsi_lun_hot_remove at lun.c:238
1 spdk_bdev_unregister at bdev.c:1355
2 remove_cb at blockdev_nvme.c:781
3 _nvme_pcie_hotplug_monitor at nvme_pcie.c:273
4 nvme_pcie_ctrlr_scan at nvme_pcie.c:643
5 nvme_transport_ctrlr_scan at nvme_transport.c:97
6 spdk_nvme_probe at nvme.c:423
7 blockdev_nvme_hotplug at blockdev_nvme.c:788
8 _spdk_reactor_run at reactor.c:351

spdk_scsi_lun_hot_remove starts poller which will eventually call
spdk_scsi_lun_hotplug

Channel destruction:
0 bdev_nvme_destroy_cb at blockdev_nvme.c:440
1 spdk_put_io_channel at io_channel.c:288
2 spdk_bdev_channel_destroy at bdev.c:657
3 spdk_put_io_channel at io_channel.c:288
4 spdk_scsi_lun_free_io_channel at lun.c:396
5 spdk_scsi_lun_hotplug at lun.c:230
6 _spdk_reactor_run at reactor.c:333

Actual ctrlr destruction:
0 bdev_nvme_destruct at blockdev_nvme.c:209
1 spdk_bdev_unregister at bdev.c:1369
2 spdk_bdev_unclaim at bdev.c:1415
3 spdk_scsi_lun_destruct at lun.c:309
4 spdk_scsi_lun_delete at lun.c:364
5 spdk_scsi_lun_hotplug at lun.c:231
6 _spdk_reactor_run at reactor.c:333

After patch 5533c3d2 [1] bdev_nvme_destroy_cb won't be called before
bdev_nvme_destruct. This results in dangling io_channel whose poller
will tick soon. What's more, that io_channel has dangling pointer to
nvme qpair, that has been already freed via bdev_nvme_destruct (in
nvme_ctrlr_destruct). The dangling io_channel poller dereferences that
qpair and causes segfault.

[1] commit 5533c3d2087bb72ac1b8a8647a1dd87a11acbebf
Author: Jim Harris <james.r.harris@intel.com>
Date:   Fri Jul 7 13:45:12 2017 -0700

    util: defer put_io_channel

[2] https://review.gerrithub.io/c/367824/

Fixes: 5533c3d2087b ("util: defer put_io_channel")

lib/util/io_channel.c
@@ -199,9 +199,21 @@ spdk_io_device_register(void *io_device, spdk_io_channel_create_cb create_cb,
 void
 spdk_io_device_unregister(void *io_device)
 {
+	struct spdk_thread *thread;
+	struct spdk_io_channel *ch;
 	struct io_device *dev;
 
 	pthread_mutex_lock(&g_devlist_mutex);
+	TAILQ_FOREACH(thread, &g_threads, tailq) {
+		TAILQ_FOREACH(ch, &thread->io_channels, tailq) {
+			if (ch->io_device == io_device) {
+				TAILQ_REMOVE(&thread->io_channels, ch, tailq);
+				ch->destroy_cb(ch->io_device, (uint8_t *)ch + sizeof(*ch));
+				free(ch);
+			}
+		}
+	}
+
 	TAILQ_FOREACH(dev, &g_io_devices, tailq) {
 		if (dev->io_device_ctx == io_device) {
 			TAILQ_REMOVE(&g_io_devices, dev, tailq);
@@ -277,9 +289,18 @@ static void
 _spdk_put_io_channel(void *arg)
 {
 	struct spdk_io_channel *ch = arg;
+	struct io_device *dev;
 
-	if (ch->ref == 0) {
-		SPDK_ERRLOG("ref already zero\n");
+	pthread_mutex_lock(&g_devlist_mutex);
+	TAILQ_FOREACH(dev, &g_io_devices, tailq) {
+		if (dev == ch->io_device) {
+			break;
+		}
+	}
+	pthread_mutex_unlock(&g_devlist_mutex);
+
+	if (ch->io_device != dev) {
+		/* channel has already been deleted */
 		return;
 	}
 
@@ -290,6 +311,7 @@ _spdk_put_io_channel(void *arg)
 		ch->destroy_cb(ch->io_device, (uint8_t *)ch + sizeof(*ch));
 		free(ch);
 	}
+
 }
 
 void