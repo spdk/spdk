12799e32a063b58c6693d0a481c579d5124ffb70
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-07-17 09:55:59 +0200

    lvol: io request processing
    This patch adds processing requests on lvol bdevs.
    
    WORK IN PROGRESS
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I2b38e992c5c6b21495acabf96e315dc389536a19

--- lib/lvol/lvol.c
+++ lib/lvol/lvol.c
@@ -49,6 +49,7 @@ _lvs_init_cb(void *cb_arg, struct spdk_blob_store *bs, int lvserrno)
 	} else {
 		assert(bs != NULL);
 		lvs->blobstore = bs;
+		lvs->channel = spdk_bs_alloc_io_channel(bs);
 		TAILQ_INIT(&lvs->lvols);
 
 		SPDK_TRACELOG(SPDK_TRACE_LVOL, "Lvol store initialized\n");
@@ -121,6 +122,11 @@ spdk_lvs_unload(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn,
 	lvs_req->u.lvs_basic.cb_arg = cb_arg;
 
 	SPDK_TRACELOG(SPDK_TRACE_LVOL, "Unloading lvol store\n");
+
+	assert(lvs->channel != NULL);
+	spdk_bs_free_io_channel(lvs->channel);
+	lvs->channel = NULL;
+
 	spdk_bs_unload(lvs->blobstore, _lvs_unload_cb, lvs_req);
 	free(lvs);
 
@@ -323,6 +329,14 @@ spdk_lvol_close(struct spdk_lvol *lvol)
 	TAILQ_REMOVE(&lvol->lvol_store->lvols, lvol, link);
 	free(lvol->name);
 	free(lvol);
+}
+
+struct spdk_io_channel *
+spdk_lvol_get_io_channel(struct spdk_lvol *lvol)
+{
+	assert(lvol->lvol_store->channel != NULL);
+
+	return lvol->lvol_store->channel;
 }
 
 SPDK_LOG_REGISTER_TRACE_FLAG("lvol", SPDK_TRACE_LVOL)
