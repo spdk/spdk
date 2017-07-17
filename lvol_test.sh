a278becb9b2e6b5fb7375f639ba96d1515c04533
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-07-17 15:17:19 +0200

    example: bdev
    Change-Id: I350e52859726e0ffcfd58e24efd18f566df6a5e9
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

---
+++ lvol_test.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+ROOT_DIR="$(dirname $(readlink -f $0))"
+exit 1
+rpc="${ROOT_DIR}/scripts/rpc.py"
+
+set -x
+
+if [ "$1" = "basic" ]; then
+	GUID=`$rpc construct_lvol_store Malloc0`
+	BLOB=`$rpc construct_lvol_bdev $GUID 25`
+	BLOB_2=`$rpc construct_lvol_bdev $GUID 25`
+	$rpc resize_lvol_bdev $BLOB 30
+	$rpc resize_lvol_bdev $BLOB 30
+	
+	$rpc -v bdev_open $BLOB
+	$rpc -v bdev_write 10
+	$rpc -v bdev_read 10
+	$rpc -v bdev_read 512
+	$rpc -v bdev_read 128
+	$rpc -v bdev_write 100
+	$rpc -v bdev_close $BLOB
+	
+	$rpc delete_bdev $BLOB_2
+	$rpc delete_bdev $BLOB
+	$rpc destroy_lvol_store $GUID
+#$rpc get_bdevs
+
+elif [ "$1" = "close_lvs" ]; then
+
+GUID=`$rpc construct_lvol_store Nvme0n1`
+$rpc get_bdevs
+#$rpc destroy_lvol_store $GUID
+
+elif [ "$1" = "delete" ]; then
+
+GUID=`$rpc construct_lvol_store Malloc0`
+BLOB=`$rpc construct_lvol_bdev $GUID 15`
+BLOB2=`$rpc construct_lvol_bdev $GUID 15`
+BLOB3=`$rpc construct_lvol_bdev $GUID 15`
+$rpc get_bdevs
+$rpc delete_bdev $BLOB_2
+$rpc destroy_lvol_store $GUID
+$rpc get_bdevs
+
+elif [ "$1" = "nest" ]; then
+
+GUID=`$rpc construct_lvol_store Malloc0`
+BLOB=`$rpc construct_lvol_bdev $GUID 25`
+GUID_2=`$rpc construct_lvol_store $BLOB`
+BLOB_2=`$rpc construct_lvol_bdev $GUID_2 15`
+$rpc get_bdevs
+$rpc destroy_lvol_store $GUID
+
+elif [ "$1" = "vhost_normal" ]; then
+
+$rpc get_bdevs
+$rpc add_vhost_scsi_lun vhost.0 0 Malloc0
+$rpc get_vhost_scsi_controllers
+
+elif [ "$1" = "vhost" ]; then
+
+GUID=`$rpc construct_lvol_store Malloc0`
+BLOB=`$rpc construct_lvol_bdev $GUID 25`
+$rpc get_bdevs
+$rpc add_vhost_scsi_lun vhost.0 0 $BLOB
+$rpc get_vhost_scsi_controllers
+fi
