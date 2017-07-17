e5ef136bec838c097387e0c6d0e85a2feb1a63a5
Ziye Yang <optimistyzy@gmail.com>
2017-07-17 15:45:46 +0800

    bdev,test: move common gpt partition code to autotest_common.sh
    Change-Id: I261e6ed578335ab091973f7546035e63a00dbbf4
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- scripts/autotest_common.sh
+++ scripts/autotest_common.sh
@@ -310,5 +310,40 @@ function print_backtrace() {
 	return 0
 }
 
+function part_dev_by_gpt () {
+	if [ $(uname -s) = Linux ] && hash sgdisk; then
+		conf=$1
+		devname=$2
+		rootdir=$3
+
+		if [ ! -e $conf ]; then
+			return 1
+		fi
+
+		echo "[Gpt]" >> $conf
+		echo "  Disable Yes" >> $conf
+
+		modprobe nbd
+		$rootdir/test/lib/bdev/nbd/nbd -c $conf -b $devname -n /dev/nbd0 &
+		nbd_pid=$!
+		echo "Process nbd pid: $nbd_pid"
+		waitforlisten $nbd_pid 5260
+		waitforbdev $devname "python $rootdir/scripts/rpc.py"
+
+		if [ -e /dev/nbd0 ]; then
+				parted -s /dev/nbd0 mklabel gpt mkpart first '0%' '50%' mkpart second '50%' '100%'
+				#change the GUID to SPDK GUID value
+				sgdisk -t 1:$SPDK_GPT_GUID /dev/nbd0
+				sgdisk -t 2:$SPDK_GPT_GUID /dev/nbd0
+		fi
+
+		killprocess $nbd_pid
+
+		sed -i'' '/Disable/d' $conf
+	fi
+
+	return 0
+}
+
 set -o errtrace
 trap "trap - ERR; print_backtrace >&2" ERR
