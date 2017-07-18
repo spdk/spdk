spdk/spdk

commit be9a3b9f6955ca176e369ac914b14274e046a3f7

bdev: pass descriptors for I/O operations
This enables checking permissions - for example,
spdk_bdev_write will fail if the descriptor was not
created with write permissions.

Signed-off-by: Jim Harris <james.r.harris@intel.com>
Change-Id: I68b65a560f471f2e0f71a7f42cfa6689b911110f

Reviewed-on: https://review.gerrithub.io/369493
Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>
Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>

lib/bdev/split/vbdev_split.c
@@ -52,6 +52,7 @@ SPDK_DECLARE_BDEV_MODULE(split);
 /* Base block device split context */
 struct split_base {
 	struct spdk_bdev	*base_bdev;
+	struct spdk_bdev_desc	*desc;
 	uint32_t		ref;
 };
 
@@ -132,7 +133,8 @@ vbdev_split_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev
 	case SPDK_BDEV_IO_TYPE_RESET:
 		base_ch = spdk_get_io_channel(split_disk->base_bdev);
 		*(struct spdk_io_channel **)bdev_io->driver_ctx = base_ch;
-		spdk_bdev_reset(split_disk->base_bdev, base_ch, _vbdev_split_complete_reset, bdev_io);
+		spdk_bdev_reset(split_disk->split_base->desc, base_ch,
+				_vbdev_split_complete_reset, bdev_io);
 		return;
 	default:
 		SPDK_ERRLOG("split: unknown I/O type %d\n", bdev_io->type);
@@ -141,7 +143,7 @@ vbdev_split_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev
 	}
 
 	/* Submit the modified I/O to the underlying bdev. */
-	spdk_bdev_io_resubmit(bdev_io, split_disk->base_bdev);
+	spdk_bdev_io_resubmit(bdev_io, split_disk->split_base->desc);
 }
 
 static void
@@ -240,12 +242,6 @@ vbdev_split_create(struct spdk_bdev *base_bdev, uint64_t split_count, uint64_t s
 	int rc;
 	struct split_base *split_base;
 
-	rc = spdk_bdev_module_claim_bdev(base_bdev, NULL, SPDK_GET_BDEV_MODULE(split));
-	if (rc) {
-		SPDK_ERRLOG("could not claim bdev %s\n", spdk_bdev_get_name(base_bdev));
-		return -1;
-	}
-
 	if (split_size_mb) {
 		if (((split_size_mb * mb) % base_bdev->blocklen) != 0) {
 			SPDK_ERRLOG("Split size %" PRIu64 " MB is not possible with block size "
@@ -281,6 +277,21 @@ vbdev_split_create(struct spdk_bdev *base_bdev, uint64_t split_count, uint64_t s
 	}
 	split_base->base_bdev = base_bdev;
 	split_base->ref = 0;
+
+	rc = spdk_bdev_open(base_bdev, false, NULL, NULL, &split_base->desc);
+	if (rc) {
+		SPDK_ERRLOG("could not open bdev %s\n", spdk_bdev_get_name(base_bdev));
+		free(split_base);
+		return -1;
+	}
+
+	rc = spdk_bdev_module_claim_bdev(base_bdev, split_base->desc, SPDK_GET_BDEV_MODULE(split));
+	if (rc) {
+		SPDK_ERRLOG("could not claim bdev %s\n", spdk_bdev_get_name(base_bdev));
+		spdk_bdev_close(split_base->desc);
+		free(split_base);
+		return -1;
+	}
 
 	offset_bytes = 0;
 	offset_blocks = 0;
@@ -334,7 +345,9 @@ vbdev_split_create(struct spdk_bdev *base_bdev, uint64_t split_count, uint64_t s
 
 cleanup:
 	if (split_base->ref == 0) {
-		/* If no split_disk instances were created, free the base context */
+		/* If no split_disk instances were created, free the resources */
+		spdk_bdev_module_release_bdev(split_base->base_bdev);
+		spdk_bdev_close(split_base->desc);
 		free(split_base);
 	}
 

include/spdk/bdev.h
@@ -164,6 +164,14 @@ int spdk_bdev_open(struct spdk_bdev *bdev, bool write, spdk_bdev_remove_cb_t rem
  */
 void spdk_bdev_close(struct spdk_bdev_desc *desc);
 
+/**
+ * Get the bdev associated with a bdev descriptor.
+ *
+ * \param desc Open block device desciptor
+ * \return bdev associated with the descriptor
+ */
+struct spdk_bdev *spdk_bdev_desc_get_bdev(struct spdk_bdev_desc *desc);
+
 bool spdk_bdev_io_type_supported(struct spdk_bdev *bdev, enum spdk_bdev_io_type io_type);
 
 int spdk_bdev_dump_config_json(struct spdk_bdev *bdev, struct spdk_json_write_ctx *w);
@@ -256,7 +264,7 @@ struct spdk_io_channel *spdk_bdev_get_io_channel(struct spdk_bdev_desc *desc);
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_read(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_read(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		   void *buf, uint64_t offset, uint64_t nbytes,
 		   spdk_bdev_io_completion_cb cb, void *cb_arg);
 
@@ -280,7 +288,7 @@ int spdk_bdev_read(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_readv(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_readv(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		    struct iovec *iov, int iovcnt,
 		    uint64_t offset, uint64_t nbytes,
 		    spdk_bdev_io_completion_cb cb, void *cb_arg);
@@ -300,7 +308,7 @@ int spdk_bdev_readv(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_write(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_write(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		    void *buf, uint64_t offset, uint64_t nbytes,
 		    spdk_bdev_io_completion_cb cb, void *cb_arg);
 
@@ -324,7 +332,7 @@ int spdk_bdev_write(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_writev(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_writev(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		     struct iovec *iov, int iovcnt,
 		     uint64_t offset, uint64_t len,
 		     spdk_bdev_io_completion_cb cb, void *cb_arg);
@@ -345,7 +353,7 @@ int spdk_bdev_writev(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_unmap(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_unmap(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		    struct spdk_scsi_unmap_bdesc *unmap_d,
 		    uint16_t bdesc_count,
 		    spdk_bdev_io_completion_cb cb, void *cb_arg);
@@ -366,7 +374,7 @@ int spdk_bdev_unmap(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_flush(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_flush(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		    uint64_t offset, uint64_t length,
 		    spdk_bdev_io_completion_cb cb, void *cb_arg);
 
@@ -382,7 +390,7 @@ int spdk_bdev_flush(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_reset(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+int spdk_bdev_reset(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		    spdk_bdev_io_completion_cb cb, void *cb_arg);
 
 /**
@@ -405,7 +413,7 @@ int spdk_bdev_reset(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_nvme_admin_passthru(struct spdk_bdev *bdev,
+int spdk_bdev_nvme_admin_passthru(struct spdk_bdev_desc *desc,
 				  struct spdk_io_channel *ch,
 				  const struct spdk_nvme_cmd *cmd,
 				  void *buf, size_t nbytes,
@@ -432,7 +440,7 @@ int spdk_bdev_nvme_admin_passthru(struct spdk_bdev *bdev,
  * be called (even if the request ultimately failed). Return
  * negated errno on failure, in which case the callback will not be called.
  */
-int spdk_bdev_nvme_io_passthru(struct spdk_bdev *bdev,
+int spdk_bdev_nvme_io_passthru(struct spdk_bdev_desc *bdev_desc,
 			       struct spdk_io_channel *ch,
 			       const struct spdk_nvme_cmd *cmd,
 			       void *buf, size_t nbytes,

include/spdk/scsi.h
@@ -95,6 +95,7 @@ struct spdk_scsi_task {
 	uint8_t				function; /* task mgmt function */
 	uint8_t				response; /* task mgmt response */
 	struct spdk_scsi_lun		*lun;
+	struct spdk_bdev_desc		*desc;
 	struct spdk_io_channel		*ch;
 	struct spdk_scsi_port		*target_port;
 	struct spdk_scsi_port		*initiator_port;

include/spdk_internal/bdev.h
@@ -398,7 +398,7 @@ void spdk_bdev_poller_stop(struct spdk_bdev_poller **ppoller);
 
 void spdk_bdev_io_get_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb);
 struct spdk_bdev_io *spdk_bdev_get_io(void);
-void spdk_bdev_io_resubmit(struct spdk_bdev_io *bdev_io, struct spdk_bdev *new_bdev);
+void spdk_bdev_io_resubmit(struct spdk_bdev_io *bdev_io, struct spdk_bdev_desc *new_bdev_desc);
 void spdk_bdev_io_complete(struct spdk_bdev_io *bdev_io,
 			   enum spdk_bdev_io_status status);
 

lib/bdev/bdev.c
@@ -580,8 +580,10 @@ spdk_bdev_io_submit(struct spdk_bdev_io *bdev_io)
 }
 
 void
-spdk_bdev_io_resubmit(struct spdk_bdev_io *bdev_io, struct spdk_bdev *new_bdev)
+spdk_bdev_io_resubmit(struct spdk_bdev_io *bdev_io, struct spdk_bdev_desc *new_bdev_desc)
 {
+	struct spdk_bdev *new_bdev = new_bdev_desc->bdev;
+
 	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_PENDING);
 	bdev_io->bdev = new_bdev;
 
@@ -764,10 +766,11 @@ spdk_bdev_io_valid(struct spdk_bdev *bdev, uint64_t offset, uint64_t nbytes)
 }
 
 int
-spdk_bdev_read(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_read(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 	       void *buf, uint64_t offset, uint64_t nbytes,
 	       spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
@@ -802,11 +805,12 @@ spdk_bdev_read(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
 }
 
 int
-spdk_bdev_readv(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_readv(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		struct iovec *iov, int iovcnt,
 		uint64_t offset, uint64_t nbytes,
 		spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
@@ -839,13 +843,18 @@ spdk_bdev_readv(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
 }
 
 int
-spdk_bdev_write(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_write(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		void *buf, uint64_t offset, uint64_t nbytes,
 		spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
+
+	if (!desc->write) {
+		return -EBADF;
+	}
 
 	if (spdk_bdev_io_valid(bdev, offset, nbytes) != 0) {
 		return -EINVAL;
@@ -877,14 +886,19 @@ spdk_bdev_write(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
 }
 
 int
-spdk_bdev_writev(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_writev(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		 struct iovec *iov, int iovcnt,
 		 uint64_t offset, uint64_t len,
 		 spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
+
+	if (!desc->write) {
+		return -EBADF;
+	}
 
 	if (spdk_bdev_io_valid(bdev, offset, len) != 0) {
 		return -EINVAL;
@@ -914,14 +928,19 @@ spdk_bdev_writev(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
 }
 
 int
-spdk_bdev_unmap(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_unmap(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		struct spdk_scsi_unmap_bdesc *unmap_d,
 		uint16_t bdesc_count,
 		spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
+
+	if (!desc->write) {
+		return -EBADF;
+	}
 
 	if (bdesc_count == 0) {
 		SPDK_ERRLOG("Invalid bdesc_count 0\n");
@@ -956,13 +975,18 @@ spdk_bdev_unmap(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
 }
 
 int
-spdk_bdev_flush(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_flush(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		uint64_t offset, uint64_t length,
 		spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
+
+	if (!desc->write) {
+		return -EBADF;
+	}
 
 	bdev_io = spdk_bdev_get_io();
 	if (!bdev_io) {
@@ -1045,9 +1069,10 @@ _spdk_bdev_start_next_reset(struct spdk_bdev *bdev)
 }
 
 int
-spdk_bdev_reset(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_reset(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 
@@ -1087,13 +1112,18 @@ spdk_bdev_get_io_stat(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
 }
 
 int
-spdk_bdev_nvme_admin_passthru(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_nvme_admin_passthru(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 			      const struct spdk_nvme_cmd *cmd, void *buf, size_t nbytes,
 			      spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
+
+	if (!desc->write) {
+		return -EBADF;
+	}
 
 	bdev_io = spdk_bdev_get_io();
 	if (!bdev_io) {
@@ -1119,13 +1149,23 @@ spdk_bdev_nvme_admin_passthru(struct spdk_bdev *bdev, struct spdk_io_channel *ch
 }
 
 int
-spdk_bdev_nvme_io_passthru(struct spdk_bdev *bdev, struct spdk_io_channel *ch,
+spdk_bdev_nvme_io_passthru(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 			   const struct spdk_nvme_cmd *cmd, void *buf, size_t nbytes,
 			   spdk_bdev_io_completion_cb cb, void *cb_arg)
 {
+	struct spdk_bdev *bdev = desc->bdev;
 	struct spdk_bdev_io *bdev_io;
 	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
 	int rc;
+
+	if (!desc->write) {
+		/*
+		 * Do not try to parse the NVMe command - we could maybe use bits in the opcode
+		 *  to easily determine if the command is a read or write, but for now just
+		 *  do not allow io_passthru with a read-only descriptor.
+		 */
+		return -EBADF;
+	}
 
 	bdev_io = spdk_bdev_get_io();
 	if (!bdev_io) {
@@ -1558,6 +1598,12 @@ spdk_bdev_module_release_bdev(struct spdk_bdev *bdev)
 {
 	assert(bdev->claim_module != NULL);
 	bdev->claim_module = NULL;
+}
+
+struct spdk_bdev *
+spdk_bdev_desc_get_bdev(struct spdk_bdev_desc *desc)
+{
+	return desc->bdev;
 }
 
 void

lib/bdev/error/vbdev_error.c
@@ -60,6 +60,7 @@ struct vbdev_error_info {
 struct vbdev_error_disk {
 	struct spdk_bdev		disk;
 	struct spdk_bdev		*base_bdev;
+	struct spdk_bdev_desc		*base_bdev_desc;
 	struct vbdev_error_info		error_vector[SPDK_BDEV_IO_TYPE_RESET];
 
 	TAILQ_ENTRY(vbdev_error_disk)	tailq;
@@ -162,7 +163,7 @@ vbdev_error_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev
 
 	error_type = vbdev_error_get_error_type(error_disk, bdev_io->type);
 	if (error_type == 0) {
-		spdk_bdev_io_resubmit(bdev_io, error_disk->base_bdev);
+		spdk_bdev_io_resubmit(bdev_io, error_disk->base_bdev_desc);
 		return;
 	} else if (error_type == VBDEV_IO_FAILURE) {
 		error_disk->error_vector[bdev_io->type].error_num--;
@@ -259,7 +260,12 @@ spdk_vbdev_error_create(struct spdk_bdev *base_bdev)
 		goto cleanup;
 	}
 
-	rc = spdk_bdev_module_claim_bdev(base_bdev, NULL, SPDK_GET_BDEV_MODULE(error));
+	rc = spdk_bdev_open(base_bdev, false, NULL, NULL, &disk->base_bdev_desc);
+	if (rc) {
+		SPDK_ERRLOG("could not open bdev %s\n", spdk_bdev_get_name(base_bdev));
+	}
+
+	rc = spdk_bdev_module_claim_bdev(base_bdev, disk->base_bdev_desc, SPDK_GET_BDEV_MODULE(error));
 	if (rc) {
 		SPDK_ERRLOG("could not claim bdev %s\n", spdk_bdev_get_name(base_bdev));
 		goto cleanup;

lib/bdev/gpt/vbdev_gpt.c
@@ -208,7 +208,7 @@ vbdev_gpt_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_i
 	case SPDK_BDEV_IO_TYPE_RESET:
 		base_ch = spdk_get_io_channel(gpt_partition_disk->base_bdev);
 		*(struct spdk_io_channel **)bdev_io->driver_ctx = base_ch;
-		spdk_bdev_reset(gpt_partition_disk->base_bdev, base_ch,
+		spdk_bdev_reset(gpt_partition_disk->gpt_base->bdev_desc, base_ch,
 				_vbdev_gpt_complete_reset, bdev_io);
 		return;
 	default:
@@ -218,7 +218,7 @@ vbdev_gpt_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_i
 	}
 
 	/* Submit the modified I/O to the underlying bdev. */
-	spdk_bdev_io_resubmit(bdev_io, gpt_partition_disk->base_bdev);
+	spdk_bdev_io_resubmit(bdev_io, gpt_partition_disk->gpt_base->bdev_desc);
 }
 
 static void
@@ -429,8 +429,6 @@ spdk_gpt_bdev_complete(struct spdk_bdev_io *bdev_io, bool status, void *arg)
 	/* free the ch and also close the bdev_desc */
 	spdk_put_io_channel(gpt_bdev->ch);
 	gpt_bdev->ch = NULL;
-	spdk_bdev_close(gpt_bdev->bdev_desc);
-	gpt_bdev->bdev_desc = NULL;
 	spdk_bdev_free_io(bdev_io);
 
 	if (status != SPDK_BDEV_IO_STATUS_SUCCESS) {
@@ -445,7 +443,7 @@ spdk_gpt_bdev_complete(struct spdk_bdev_io *bdev_io, bool status, void *arg)
 		goto end;
 	}
 
-	rc = spdk_bdev_module_claim_bdev(bdev, NULL, SPDK_GET_BDEV_MODULE(gpt));
+	rc = spdk_bdev_module_claim_bdev(bdev, gpt_bdev->bdev_desc, SPDK_GET_BDEV_MODULE(gpt));
 	if (rc) {
 		SPDK_ERRLOG("could not claim bdev %s\n", spdk_bdev_get_name(bdev));
 		goto end;
@@ -467,6 +465,7 @@ end:
 
 	if (gpt_bdev->ref == 0) {
 		/* If no gpt_partition_disk instances were created, free the base context */
+		spdk_bdev_close(gpt_bdev->bdev_desc);
 		spdk_gpt_bdev_free(gpt_bdev);
 		if (claimed) {
 			spdk_bdev_module_release_bdev(bdev);
@@ -486,7 +485,7 @@ vbdev_gpt_read_gpt(struct spdk_bdev *bdev)
 		return -1;
 	}
 
-	rc = spdk_bdev_read(gpt_bdev->bdev, gpt_bdev->ch, gpt_bdev->gpt.buf, 0, SPDK_GPT_BUFFER_SIZE,
+	rc = spdk_bdev_read(gpt_bdev->bdev_desc, gpt_bdev->ch, gpt_bdev->gpt.buf, 0, SPDK_GPT_BUFFER_SIZE,
 			    spdk_gpt_bdev_complete, gpt_bdev);
 	if (rc < 0) {
 		spdk_gpt_bdev_free(gpt_bdev);

lib/blob/bdev/blob_bdev.c
@@ -81,7 +81,7 @@ bdev_blob_read(struct spdk_bs_dev *dev, struct spdk_io_channel *channel, void *p
 	int rc;
 	uint32_t block_size = spdk_bdev_get_block_size(bdev);
 
-	rc = spdk_bdev_read(bdev, channel, payload, lba * block_size,
+	rc = spdk_bdev_read(__get_desc(dev), channel, payload, lba * block_size,
 			    lba_count * block_size, bdev_blob_io_complete, cb_args);
 	if (rc) {
 		cb_args->cb_fn(cb_args->channel, cb_args->cb_arg, rc);
@@ -96,7 +96,7 @@ bdev_blob_write(struct spdk_bs_dev *dev, struct spdk_io_channel *channel, void *
 	int rc;
 	uint32_t block_size = spdk_bdev_get_block_size(bdev);
 
-	rc = spdk_bdev_write(bdev, channel, payload, lba * block_size,
+	rc = spdk_bdev_write(__get_desc(dev), channel, payload, lba * block_size,
 			     lba_count * block_size, bdev_blob_io_complete, cb_args);
 	if (rc) {
 		cb_args->cb_fn(cb_args->channel, cb_args->cb_arg, rc);
@@ -107,7 +107,6 @@ static void
 bdev_blob_unmap(struct spdk_bs_dev *dev, struct spdk_io_channel *channel, uint64_t lba,
 		uint32_t lba_count, struct spdk_bs_dev_cb_args *cb_args)
 {
-	struct spdk_bdev *bdev = __get_bdev(dev);
 	struct spdk_scsi_unmap_bdesc *desc;
 	int rc;
 
@@ -118,7 +117,7 @@ bdev_blob_unmap(struct spdk_bs_dev *dev, struct spdk_io_channel *channel, uint64
 	to_be32(&desc->block_count, lba_count);
 	desc->reserved = 0;
 
-	rc = spdk_bdev_unmap(bdev, channel, desc, 1, bdev_blob_io_complete, cb_args);
+	rc = spdk_bdev_unmap(__get_desc(dev), channel, desc, 1, bdev_blob_io_complete, cb_args);
 	if (rc) {
 		cb_args->cb_fn(cb_args->channel, cb_args->cb_arg, rc);
 	}

commit 4bd8c1e7169bc6453c80a041e845a62007648fb8

split: close descriptor and release bdev module when freeing split base
This was probably overlooked in patch be9a3b9f.

Fixes: be9a3b9f6955 ("bdev: pass descriptors for I/O operations")

Change-Id: If29ad65ac168f3dbf7e1602f26f939dfbf17599a
Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

lib/bdev/split/vbdev_split.c
@@ -154,10 +154,20 @@ vbdev_split_base_get_ref(struct split_base *split_base, struct split_disk *split
 }
 
 static void
+vbdev_split_base_free(struct split_base *split_base)
+{
+	assert(split_base->base_bdev);
+	assert(split_base->desc);
+	spdk_bdev_module_release_bdev(split_base->base_bdev);
+	spdk_bdev_close(split_base->desc);
+	free(split_base);
+}
+
+static void
 vbdev_split_base_put_ref(struct split_base *split_base)
 {
 	if (__sync_sub_and_fetch(&split_base->ref, 1) == 0) {
-		free(split_base);
+		vbdev_split_base_free(split_base);
 	}
 }
 
@@ -346,9 +356,7 @@ vbdev_split_create(struct spdk_bdev *base_bdev, uint64_t split_count, uint64_t s
 cleanup:
 	if (split_base->ref == 0) {
 		/* If no split_disk instances were created, free the resources */
-		spdk_bdev_module_release_bdev(split_base->base_bdev);
-		spdk_bdev_close(split_base->desc);
-		free(split_base);
+		vbdev_split_base_free(split_base);
 	}
 
 	return rc;