d94a5c48bcf74c0f7e1d1e49f966c1afd1b95456
Ben Walker <benjamin.walker@intel.com>
2017-07-19 14:55:03 -0700

    bdev/fio_plugin: Add trim support
    Change-Id: I4b046e63a669d52c875efffb5a91e99845fc623d
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -30,6 +30,7 @@ timing_exit bdev_svc
 if [ -d /usr/src/fio ] && [ $SPDK_RUN_ASAN -eq 0 ]; then
 	timing_enter fio
 
+	timing_enter fio_rw_verify
 	# Generate the fio config file given the list of all unclaimed bdevs
 	fio_config_gen $testdir/bdev.fio verify
 	for b in $(echo $bdevs | jq -r '.name'); do
@@ -46,18 +47,35 @@ if [ -d /usr/src/fio ] && [ $SPDK_RUN_ASAN -eq 0 ]; then
 
 	rm -f *.state
 	rm -f $testdir/bdev.fio
+	timing_exit fio_rw_verify
+
+	timing_enter fio_trim
+	# Generate the fio config file given the list of all unclaimed bdevs that support unmap
+	fio_config_gen $testdir/bdev.fio trim
+	for b in $(echo $bdevs | jq -r 'select(.supported_io_types.unmap == true) | .name'); do
+		fio_config_add_job $testdir/bdev.fio $b
+	done
+
+	if [ $RUN_NIGHTLY -eq 0 ]; then
+		LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio --ioengine=spdk_bdev --spdk_conf=./test/lib/bdev/bdev.conf --iodepth=8 --bs=4k --runtime=10 $testdir/bdev.fio
+	else
+		# Use size 192KB which both exceeds typical 128KB max NVMe I/O
+		#  size and will cross 128KB Intel DC P3700 stripe boundaries.
+		LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio --ioengine=spdk_bdev --spdk_conf=./test/lib/bdev/bdev.conf --iodepth=128 --bs=192k --runtime=100 $testdir/bdev.fio
+	fi
+
+	rm -f *.state
+	rm -f $testdir/bdev.fio
+	timing_exit fio_trim
+
 	timing_exit fio
 fi
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	# Temporarily disabled - infinite loop
-	#timing_enter reset
+	timing_enter reset
 	#$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 16 -w reset -s 4096 -t 60
-	#timing_exit reset
-
-	timing_enter unmap
-	$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 1 -w unmap -s 4096 -t 60
-	timing_exit unmap
+	timing_exit reset
 fi
 
 rm -f $testdir/bdev.conf
