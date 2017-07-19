0ce9b8aa3f653278e94d6f2e45da4f243e2c21b3
Seth Howell <seth.howell@intel.com>
2017-07-19 16:22:38 -0700

    rbd: add option for using installed version of ceph on rbd tests
    previously, the tests have been hardcoded to use a manually installed version of ceph.
    The attached scripts allow the user to create a ceph cluster from a package managed version
    of ceph which requires no extra configuration.
    
    Change-Id: I22e4ea9eb95f1e84a968ee2bce6133568fda33a8
    Signed-off-by: Seth Howell <seth.howell@intel.com>
    Reviewed-on: https://review.gerrithub.io/370399
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

--- test/iscsi_tgt/iscsi_tgt.sh
+++ test/iscsi_tgt/iscsi_tgt.sh
@@ -36,7 +36,9 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 	run_test ./test/iscsi_tgt/ip_migration/ip_migration.sh
 fi
 run_test ./test/iscsi_tgt/ext4test/ext4test.sh
-run_test ./test/iscsi_tgt/rbd/rbd.sh
+if [ $SPDK_TEST_RBD -eq 1 ]; then
+	run_test ./test/iscsi_tgt/rbd/rbd.sh
+fi
 
 trap - SIGINT SIGTERM EXIT
 kill_stub
