a3118c42508b87a9d00bafabacd04c907ca6d775
Ben Walker <benjamin.walker@intel.com>
2017-07-19 14:55:03 -0700

    bdev/fio_plugin: Add trim support
    Change-Id: I4b046e63a669d52c875efffb5a91e99845fc623d
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -24,15 +24,16 @@ fi
 timing_exit nbd
 
 timing_enter bdev_svc
-bdevs=$(discover_bdevs $rootdir $testdir/bdev.conf | jq -r '.[] | select(.bdev_opened_for_write == false) | .name')
+bdevs=$(discover_bdevs $rootdir $testdir/bdev.conf | jq -r '.[] | select(.bdev_opened_for_write == false)')
 timing_exit bdev_svc
 
 if [ -d /usr/src/fio ] && [ $SPDK_RUN_ASAN -eq 0 ]; then
 	timing_enter fio
 
+	timing_enter fio_rw_verify
 	# Generate the fio config file given the list of all unclaimed bdevs
 	fio_config_gen $testdir/bdev.fio verify
-	for b in $bdevs; do
+	for b in $(echo $bdevs | jq -r '.name'); do
 		fio_config_add_job $testdir/bdev.fio $b
 	done
 
@@ -46,18 +47,35 @@ if [ -d /usr/src/fio ] && [ $SPDK_RUN_ASAN -eq 0 ]; then
 
 	rm -f *.state
 	rm -f $testdir/bdev.fio
+	timing_exit fio_rw_verify
+
+	timing_enter fio_trim
+	# Generate the fio config file given the list of all unclaimed bdevs that support unmap
+	fio_config_gen $testdir/bdev.fio trim
+	for b in $(echo $bdevs | jq -r 'select(.io_types.unmap == true) | .name'); do
+		fio_config_add_job $testdir/bdev.fio $b
+	done
+
+	if [ $RUN_NIGHTLY -eq 0 ]; then
+		LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio --ioengine=spdk --spdk_conf=./test/lib/bdev/bdev.conf --iodepth=8 --bs=4k --runtime=10 $testdir/bdev.fio
+	else
+		# Use size 192KB which both exceeds typical 128KB max NVMe I/O
+		#  size and will cross 128KB Intel DC P3700 stripe boundaries.
+		LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio --ioengine=spdk --spdk_conf=./test/lib/bdev/bdev.conf --iodepth=128 --bs=192k --runtime=100 $testdir/bdev.fio
+	fi
+
+	rm -f *.state
+	rm -f $testdir/bdev.fio
+	timing_exit fio_trim
+
 	timing_exit fio
 fi
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	# Temporarily disabled - infinite loop
-	#timing_enter reset
+	timing_enter reset
 	#$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 16 -w reset -s 4096 -t 60
-	#timing_exit reset
-
-	timing_enter unmap
-	$testdir/bdevperf/bdevperf -c $testdir/bdev.conf -q 1 -w unmap -s 4096 -t 60
-	timing_exit unmap
+	timing_exit reset
 fi
 
 rm -f $testdir/bdev.conf
