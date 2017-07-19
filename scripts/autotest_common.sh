dab3fa812b12ebfccddcc1098ede6dfdad13c9fb
Ben Walker <benjamin.walker@intel.com>
2017-07-19 14:55:03 -0700

    bdev/fio_plugin: Add trim support
    Change-Id: I4b046e63a669d52c875efffb5a91e99845fc623d
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- scripts/autotest_common.sh
+++ scripts/autotest_common.sh
@@ -401,6 +401,8 @@ EOL
 	if [ "$workload" == "verify" ]; then
 		echo "verify=sha1" >> $config_file
 		echo "rw=randwrite" >> $config_file
+	elif [ "$workload" == "trim" ]; then
+		echo "rw=trimwrite" >> $config_file
 	else
 		echo "rw=$workload" >> $config_file
 	fi
