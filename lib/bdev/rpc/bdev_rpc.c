6999270847edeafdd792d064b1a5047132575969
Ziye Yang <optimistyzy@gmail.com>
2017-07-20 13:15:22 +0800

    bdev, rpc: add general examine bdev support
    Change-Id: I95ab82c496022cc002714ef2c46e627cc6f11f13
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/bdev/rpc/bdev_rpc.c
+++ lib/bdev/rpc/bdev_rpc.c
@@ -33,6 +33,7 @@
 
 #include "spdk/log.h"
 #include "spdk/rpc.h"
+#include "spdk/util.h"
 
 #include "spdk_internal/bdev.h"
 
@@ -146,3 +147,56 @@ invalid:
 	free_rpc_delete_bdev(&req);
 }
 SPDK_RPC_REGISTER("delete_bdev", spdk_rpc_delete_bdev)
+
+struct rpc_examine_bdev {
+	char *module_name;
+	char *bdev_name;
+};
+
+static void
+free_rpc_examine_bdev(struct rpc_examine_bdev *req)
+{
+	free(req->module_name);
+	free(req->bdev_name);
+}
+
+static const struct spdk_json_object_decoder rpc_examine_bdev_decoders[] = {
+	{"module_name", offsetof(struct rpc_examine_bdev, module_name), spdk_json_decode_string},
+	{"bdev_name", offsetof(struct rpc_examine_bdev, bdev_name), spdk_json_decode_string},
+};
+
+static void
+spdk_rpc_examine_bdev(struct spdk_jsonrpc_request *request,
+		      const struct spdk_json_val *params)
+{
+	struct rpc_examine_bdev req = {};
+	struct spdk_json_write_ctx *w;
+
+	if (spdk_json_decode_object(params, rpc_examine_bdev_decoders,
+				    SPDK_COUNTOF(rpc_examine_bdev_decoders),
+				    &req)) {
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		goto invalid;
+	}
+
+	if (spdk_bdev_module_examine_bdev(req.module_name, req.bdev_name)) {
+		goto invalid;
+	}
+
+	free_rpc_examine_bdev(&req);
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w == NULL) {
+		return;
+	}
+
+	spdk_json_write_bool(w, true);
+	spdk_jsonrpc_end_result(request, w);
+
+	return;
+
+invalid:
+	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, "Invalid parameters");
+	free_rpc_examine_bdev(&req);
+}
+SPDK_RPC_REGISTER("examine_bdev", spdk_rpc_examine_bdev)
