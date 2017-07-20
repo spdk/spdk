spdk/spdk

commit afe860aeb1617a178943f3a8e4accc56307801b1

channel: Correctly defer unregisters if channels exist
Unregistering a device should be deferred if I/O
channels still exist. Those I/O channels are likely
undergoing a deferred unregister themselves.

Change-Id: I67186232a58f212b867f6ef894c3d37aae2a4d53
Signed-off-by: Ben Walker <benjamin.walker@intel.com>
Reviewed-on: https://review.gerrithub.io/370351
Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>

lib/util/io_channel.c
@@ -39,18 +39,20 @@
 static pthread_mutex_t g_devlist_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct io_device {
-	void			*io_device_ctx;
+	void			*io_device;
 	spdk_io_channel_create_cb create_cb;
 	spdk_io_channel_destroy_cb destroy_cb;
 	uint32_t		ctx_size;
 	TAILQ_ENTRY(io_device)	tailq;
+
+	bool			unregistered;
 };
 
 static TAILQ_HEAD(, io_device) g_io_devices = TAILQ_HEAD_INITIALIZER(g_io_devices);
 
 struct spdk_io_channel {
 	struct spdk_thread		*thread;
-	void				*io_device;
+	struct io_device		*dev;
 	uint32_t			ref;
 	TAILQ_ENTRY(spdk_io_channel)	tailq;
 	spdk_io_channel_destroy_cb	destroy_cb;
@@ -178,14 +180,15 @@ spdk_io_device_register(void *io_device, spdk_io_channel_create_cb create_cb,
 		return;
 	}
 
-	dev->io_device_ctx = io_device;
+	dev->io_device = io_device;
 	dev->create_cb = create_cb;
 	dev->destroy_cb = destroy_cb;
 	dev->ctx_size = ctx_size;
+	dev->unregistered = false;
 
 	pthread_mutex_lock(&g_devlist_mutex);
 	TAILQ_FOREACH(tmp, &g_io_devices, tailq) {
-		if (tmp->io_device_ctx == io_device) {
+		if (tmp->io_device == io_device) {
 			SPDK_ERRLOG("io_device %p already registered\n", io_device);
 			free(dev);
 			pthread_mutex_unlock(&g_devlist_mutex);
@@ -196,6 +199,27 @@ spdk_io_device_register(void *io_device, spdk_io_channel_create_cb create_cb,
 	pthread_mutex_unlock(&g_devlist_mutex);
 }
 
+static void
+_spdk_io_device_attempt_free(struct io_device *dev)
+{
+	struct spdk_thread *thread;
+	struct spdk_io_channel *ch;
+
+	TAILQ_FOREACH(thread, &g_threads, tailq) {
+		TAILQ_FOREACH(ch, &thread->io_channels, tailq) {
+			if (ch->dev == dev) {
+				/* A channel that references this I/O
+				 * device still exists. Defer deletion
+				 * until it is removed.
+				 */
+				return;
+			}
+		}
+	}
+
+	free(dev);
+}
+
 void
 spdk_io_device_unregister(void *io_device)
 {
@@ -203,14 +227,21 @@ spdk_io_device_unregister(void *io_device)
 
 	pthread_mutex_lock(&g_devlist_mutex);
 	TAILQ_FOREACH(dev, &g_io_devices, tailq) {
-		if (dev->io_device_ctx == io_device) {
-			TAILQ_REMOVE(&g_io_devices, dev, tailq);
-			free(dev);
-			pthread_mutex_unlock(&g_devlist_mutex);
-			return;
+		if (dev->io_device == io_device) {
+			break;
 		}
 	}
-	SPDK_ERRLOG("io_device %p not found\n", io_device);
+
+	if (!dev) {
+		SPDK_ERRLOG("io_device %p not found\n", io_device);
+		pthread_mutex_unlock(&g_devlist_mutex);
+		return;
+	}
+
+	dev->unregistered = true;
+	TAILQ_REMOVE(&g_io_devices, dev, tailq);
+	_spdk_io_device_attempt_free(dev);
+
 	pthread_mutex_unlock(&g_devlist_mutex);
 }
 
@@ -224,7 +255,7 @@ spdk_get_io_channel(void *io_device)
 
 	pthread_mutex_lock(&g_devlist_mutex);
 	TAILQ_FOREACH(dev, &g_io_devices, tailq) {
-		if (dev->io_device_ctx == io_device) {
+		if (dev->io_device == io_device) {
 			break;
 		}
 	}
@@ -241,15 +272,14 @@ spdk_get_io_channel(void *io_device)
 		return NULL;
 	}
 
-	pthread_mutex_unlock(&g_devlist_mutex);
-
 	TAILQ_FOREACH(ch, &thread->io_channels, tailq) {
-		if (ch->io_device == io_device) {
+		if (ch->dev == dev) {
 			ch->ref++;
 			/*
 			 * An I/O channel already exists for this device on this
 			 *  thread, so return it.
 			 */
+			pthread_mutex_unlock(&g_devlist_mutex);
 			return ch;
 		}
 	}
@@ -257,19 +287,27 @@ spdk_get_io_channel(void *io_device)
 	ch = calloc(1, sizeof(*ch) + dev->ctx_size);
 	if (ch == NULL) {
 		SPDK_ERRLOG("could not calloc spdk_io_channel\n");
-		return NULL;
-	}
-	rc = dev->create_cb(io_device, (uint8_t *)ch + sizeof(*ch));
-	if (rc == -1) {
-		free(ch);
+		pthread_mutex_unlock(&g_devlist_mutex);
 		return NULL;
 	}
 
-	ch->io_device = io_device;
+	ch->dev = dev;
 	ch->destroy_cb = dev->destroy_cb;
 	ch->thread = thread;
 	ch->ref = 1;
 	TAILQ_INSERT_TAIL(&thread->io_channels, ch, tailq);
+
+	pthread_mutex_unlock(&g_devlist_mutex);
+
+	rc = dev->create_cb(io_device, (uint8_t *)ch + sizeof(*ch));
+	if (rc == -1) {
+		pthread_mutex_lock(&g_devlist_mutex);
+		TAILQ_REMOVE(&ch->thread->io_channels, ch, tailq);
+		free(ch);
+		pthread_mutex_unlock(&g_devlist_mutex);
+		return NULL;
+	}
+
 	return ch;
 }
 
@@ -285,11 +323,22 @@ _spdk_put_io_channel(void *arg)
 
 	ch->ref--;
 
-	if (ch->ref == 0) {
-		TAILQ_REMOVE(&ch->thread->io_channels, ch, tailq);
-		ch->destroy_cb(ch->io_device, (uint8_t *)ch + sizeof(*ch));
-		free(ch);
+	if (ch->ref > 0) {
+		return;
 	}
+
+	ch->destroy_cb(ch->dev->io_device, spdk_io_channel_get_ctx(ch));
+
+	pthread_mutex_lock(&g_devlist_mutex);
+
+	TAILQ_REMOVE(&ch->thread->io_channels, ch, tailq);
+
+	if (ch->dev->unregistered) {
+		_spdk_io_device_attempt_free(ch->dev);
+	}
+	free(ch);
+
+	pthread_mutex_unlock(&g_devlist_mutex);
 }
 
 void
@@ -347,7 +396,7 @@ _call_channel(void *ctx)
 	thread = TAILQ_NEXT(thread, tailq);
 	while (thread) {
 		TAILQ_FOREACH(ch, &thread->io_channels, tailq) {
-			if (ch->io_device == ch_ctx->io_device) {
+			if (ch->dev->io_device == ch_ctx->io_device) {
 				ch_ctx->cur_thread = thread;
 				ch_ctx->cur_ch = ch;
 				pthread_mutex_unlock(&g_devlist_mutex);
@@ -387,7 +436,7 @@ spdk_for_each_channel(void *io_device, spdk_channel_msg fn, void *ctx,
 
 	TAILQ_FOREACH(thread, &g_threads, tailq) {
 		TAILQ_FOREACH(ch, &thread->io_channels, tailq) {
-			if (ch->io_device == io_device) {
+			if (ch->dev->io_device == io_device) {
 				ch_ctx->cur_thread = thread;
 				ch_ctx->cur_ch = ch;
 				pthread_mutex_unlock(&g_devlist_mutex);

commit e13123349f31ea9a8822954965a7aeb1f6150006

util/nvme: added io_device unregister callback
Patch afe860ae deferred freeing the io_device. However, for nvme, the
io_device context (spdk_nvme_ctrlr) is still being destructed before
io_device is freed.

This patch defers io_device context destruction and fixes nvme
hotremove

Fixes: afe860aeb161 ("channel: Correctly defer unregisters if channels exist")
Fixes: 5533c3d2087b ("util: defer put_io_channel")

Change-Id: I7af699174cac0c6c6a6faa2cc65418c47347eb9a
Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

include/spdk/io_channel.h
@@ -52,6 +52,8 @@ typedef void (*spdk_thread_pass_msg)(spdk_thread_fn fn, void *ctx,
 typedef int (*spdk_io_channel_create_cb)(void *io_device, void *ctx_buf);
 typedef void (*spdk_io_channel_destroy_cb)(void *io_device, void *ctx_buf);
 
+typedef void (*spdk_io_device_unregister_cb)(void *io_device);
+
 typedef void (*spdk_channel_msg)(void *io_device, struct spdk_io_channel *ch,
 				 void *ctx);
 typedef void (*spdk_channel_for_each_cpl)(void *io_device, void *ctx);
@@ -110,10 +112,11 @@ void spdk_io_device_register(void *io_device, spdk_io_channel_create_cb create_c
 /**
  * \brief Unregister the opaque io_device context as an I/O device.
  *
- * Callers must ensure they release references to any I/O channel related to this
- *  device before calling this function.
+ * The actual unregistration might be deferred until all active I/O channels are destroyed.
+ *  unregister_cb is an optional callback function invoked to release any references to
+ *  this I/O device.
  */
-void spdk_io_device_unregister(void *io_device);
+void spdk_io_device_unregister(void *io_device, spdk_io_device_unregister_cb unregister_cb);
 
 /**
  * \brief Gets an I/O channel for the specified io_device to be used by the calling thread.

lib/bdev/bdev.c
@@ -522,7 +522,7 @@ spdk_bdev_finish(void)
 	spdk_mempool_free(g_bdev_mgr.buf_small_pool);
 	spdk_mempool_free(g_bdev_mgr.buf_large_pool);
 
-	spdk_io_device_unregister(&g_bdev_mgr);
+	spdk_io_device_unregister(&g_bdev_mgr, NULL);
 
 	return 0;
 }
@@ -1462,7 +1462,7 @@ spdk_bdev_unregister(struct spdk_bdev *bdev)
 
 	pthread_mutex_destroy(&bdev->mutex);
 
-	spdk_io_device_unregister(bdev);
+	spdk_io_device_unregister(bdev, NULL);
 
 	rc = bdev->fn_table->destruct(bdev->ctxt);
 	if (rc < 0) {

lib/bdev/nvme/bdev_nvme.c
@@ -224,6 +224,14 @@ bdev_nvme_poll_adminq(void *arg)
 	spdk_nvme_ctrlr_process_admin_completions(ctrlr);
 }
 
+static void
+bdev_nvme_unregister_cb(void *io_device)
+{
+	struct spdk_nvme_ctrlr *ctrlr = io_device;
+
+	spdk_nvme_detach(ctrlr);
+}
+
 static int
 bdev_nvme_destruct(void *ctx)
 {
@@ -240,9 +248,8 @@ bdev_nvme_destruct(void *ctx)
 	if (nvme_ctrlr->ref == 0) {
 		TAILQ_REMOVE(&g_nvme_ctrlrs, nvme_ctrlr, tailq);
 		pthread_mutex_unlock(&g_bdev_nvme_mutex);
-		spdk_io_device_unregister(nvme_ctrlr->ctrlr);
+		spdk_io_device_unregister(nvme_ctrlr->ctrlr, bdev_nvme_unregister_cb);
 		spdk_bdev_poller_stop(&nvme_ctrlr->adminq_timer_poller);
-		spdk_nvme_detach(nvme_ctrlr->ctrlr);
 		free(nvme_ctrlr->name);
 		free(nvme_ctrlr);
 		return 0;

lib/blob/blobstore.c
@@ -1141,8 +1141,8 @@ _spdk_bs_free(struct spdk_blob_store *bs)
 	struct spdk_blob	*blob, *blob_tmp;
 
 	spdk_bs_unregister_md_thread(bs);
-	spdk_io_device_unregister(&bs->io_target);
-	spdk_io_device_unregister(&bs->md_target);
+	spdk_io_device_unregister(&bs->io_target, NULL);
+	spdk_io_device_unregister(&bs->md_target, NULL);
 
 	TAILQ_FOREACH_SAFE(blob, &bs->blobs, link, blob_tmp) {
 		TAILQ_REMOVE(&bs->blobs, blob, link);

lib/blobfs/blobfs.c
@@ -440,10 +440,10 @@ spdk_fs_init(struct spdk_bs_dev *dev, fs_send_request_fn send_request_fn,
 	req = alloc_fs_request(fs->md_target.md_fs_channel);
 	if (req == NULL) {
 		spdk_put_io_channel(fs->md_target.md_io_channel);
-		spdk_io_device_unregister(&fs->md_target);
+		spdk_io_device_unregister(&fs->md_target, NULL);
 		spdk_put_io_channel(fs->sync_target.sync_io_channel);
-		spdk_io_device_unregister(&fs->sync_target);
-		spdk_io_device_unregister(&fs->io_target);
+		spdk_io_device_unregister(&fs->sync_target, NULL);
+		spdk_io_device_unregister(&fs->io_target, NULL);
 		free(fs);
 		cb_fn(cb_arg, NULL, -ENOMEM);
 		return;
@@ -566,10 +566,10 @@ spdk_fs_load(struct spdk_bs_dev *dev, fs_send_request_fn send_request_fn,
 	req = alloc_fs_request(fs->md_target.md_fs_channel);
 	if (req == NULL) {
 		spdk_put_io_channel(fs->md_target.md_io_channel);
-		spdk_io_device_unregister(&fs->md_target);
+		spdk_io_device_unregister(&fs->md_target, NULL);
 		spdk_put_io_channel(fs->sync_target.sync_io_channel);
-		spdk_io_device_unregister(&fs->sync_target);
-		spdk_io_device_unregister(&fs->io_target);
+		spdk_io_device_unregister(&fs->sync_target, NULL);
+		spdk_io_device_unregister(&fs->io_target, NULL);
 		free(fs);
 		cb_fn(cb_arg, NULL, -ENOMEM);
 		return;
@@ -600,9 +600,9 @@ unload_cb(void *ctx, int bserrno)
 	args->fn.fs_op(args->arg, bserrno);
 	free(req);
 
-	spdk_io_device_unregister(&fs->io_target);
-	spdk_io_device_unregister(&fs->sync_target);
-	spdk_io_device_unregister(&fs->md_target);
+	spdk_io_device_unregister(&fs->io_target, NULL);
+	spdk_io_device_unregister(&fs->sync_target, NULL);
+	spdk_io_device_unregister(&fs->md_target, NULL);
 
 	free(fs);
 }

lib/util/io_channel.c
@@ -42,6 +42,7 @@ struct io_device {
 	void			*io_device;
 	spdk_io_channel_create_cb create_cb;
 	spdk_io_channel_destroy_cb destroy_cb;
+	spdk_io_device_unregister_cb unregister_cb;
 	uint32_t		ctx_size;
 	TAILQ_ENTRY(io_device)	tailq;
 
@@ -183,6 +184,7 @@ spdk_io_device_register(void *io_device, spdk_io_channel_create_cb create_cb,
 	dev->io_device = io_device;
 	dev->create_cb = create_cb;
 	dev->destroy_cb = destroy_cb;
+	dev->unregister_cb = NULL;
 	dev->ctx_size = ctx_size;
 	dev->unregistered = false;
 
@@ -217,11 +219,15 @@ _spdk_io_device_attempt_free(struct io_device *dev)
 		}
 	}
 
+	if (dev->unregister_cb) {
+		dev->unregister_cb(dev->io_device);
+	}
+
 	free(dev);
 }
 
 void
-spdk_io_device_unregister(void *io_device)
+spdk_io_device_unregister(void *io_device, spdk_io_device_unregister_cb unregister_cb)
 {
 	struct io_device *dev;
 
@@ -238,6 +244,7 @@ spdk_io_device_unregister(void *io_device)
 		return;
 	}
 
+	dev->unregister_cb = unregister_cb;
 	dev->unregistered = true;
 	TAILQ_REMOVE(&g_io_devices, dev, tailq);
 	_spdk_io_device_attempt_free(dev);

test/unit/lib/bdev/bdev.c/bdev_ut.c
@@ -55,7 +55,7 @@ spdk_io_device_register(void *io_device, spdk_io_channel_create_cb create_cb,
 }
 
 void
-spdk_io_device_unregister(void *io_device)
+spdk_io_device_unregister(void *io_device, spdk_io_device_unregister_cb unregister_cb)
 {
 }
 

test/unit/lib/util/io_channel.c/io_channel_ut.c
@@ -146,9 +146,9 @@ channel(void)
 	ch1 = spdk_get_io_channel(&device3);
 	CU_ASSERT(ch1 == NULL);
 
-	spdk_io_device_unregister(&device1);
-	spdk_io_device_unregister(&device2);
-	spdk_io_device_unregister(&device3);
+	spdk_io_device_unregister(&device1, NULL);
+	spdk_io_device_unregister(&device2, NULL);
+	spdk_io_device_unregister(&device3, NULL);
 	CU_ASSERT(TAILQ_EMPTY(&g_io_devices));
 	spdk_free_thread();
 	CU_ASSERT(TAILQ_EMPTY(&g_threads));