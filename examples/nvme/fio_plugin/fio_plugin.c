3113ef33c00770e5beda62bae88fd9ae29e8d926
GangCao <gang.cao@intel.com>
2017-07-20 23:01:03 -0400

    nvme: add NVMe-oF well-known discovery ctrlr check in the probe_cb
    Change-Id: Idf67ee5966f6753918c12604342c892d2f3bbe3a
    Signed-off-by: GangCao <gang.cao@intel.com>

--- examples/nvme/fio_plugin/fio_plugin.c
+++ examples/nvme/fio_plugin/fio_plugin.c
@@ -92,6 +92,14 @@ static bool
 probe_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 	 struct spdk_nvme_ctrlr_opts *opts)
 {
+	if (spdk_nvme_transport_id_is_nvmf_discovery_ctrlr(trid) == true) {
+		return false;
+	}
+
+	struct thread_data *td = (struct thread_data *)cb_ctx;
+
+	opts->io_queue_size = td->o.iodepth + 1;
+
 	return true;
 }
 
