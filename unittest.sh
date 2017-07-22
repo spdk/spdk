48581a14086f2fe629258e14f84f5918abcb7efc
Hailiang Wang <hailiangx.e.wang@intel.com>
2017-07-23 00:32:54 +0800

    Gpt: add a unit test for spdk_gpt_parse function.
    Change-Id: I45c6f696052213b00c8db0c9c63e01d14b900672
    Signed-off-by: Hailiang Wang <hailiangx.e.wang@intel.com>

--- unittest.sh
+++ unittest.sh
@@ -42,6 +42,7 @@ if [ "$cov_avail" = "yes" ]; then
 fi
 
 $valgrind test/unit/lib/bdev/bdev.c/bdev_ut
+$valgrind test/unit/lib/bdev/gpt/gpt.c/gpt_ut
 $valgrind test/unit/lib/bdev/scsi_nvme.c/scsi_nvme_ut
 
 $valgrind test/unit/lib/blob/blob.c/blob_ut
