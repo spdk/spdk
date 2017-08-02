bf87b61dfc5d5372ca3c1c75f15fe5ae8ab9c4f7
Seth Howell <seth.howell@intel.com>
2017-08-02 16:37:38 -0700

    lib/nvme: split large write_zeroes operations
    the NVMe write_zeores command only supports io of up to 2^16 blocks at a
    time. Any i/o larger than this is split into multiple commands.
    
    Change-Id: I1c518d295d1817c7fa4c8357ac882bb02eecfe1b
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/unit/lib/nvme/nvme_ns_cmd.c/nvme_ns_cmd_ut.c
+++ test/unit/lib/nvme/nvme_ns_cmd.c/nvme_ns_cmd_ut.c
@@ -550,14 +550,17 @@ test_nvme_ns_cmd_write_zeroes(void)
 	struct spdk_nvme_ns	ns = { 0 };
 	struct spdk_nvme_ctrlr	ctrlr = { 0 };
 	struct spdk_nvme_qpair	qpair;
-	spdk_nvme_cmd_cb	cb_fn = NULL;
+	struct nvme_request *child;
+	spdk_nvme_cmd_cb cb_fn = NULL;
 	void			*cb_arg = NULL;
 	uint64_t		cmd_lba;
 	uint32_t		cmd_lba_count;
-	int			rc;
+	uint32_t		local_lba_count;
+	int rc, i;
 
 	prepare_for_test(&ns, &ctrlr, &qpair, 512, 0, 128 * 1024, 0, false);
 
+	/* valid i/o, no split */
 	rc = spdk_nvme_ns_cmd_write_zeroes(&ns, &qpair, 0, 2, cb_fn, cb_arg, 0);
 	SPDK_CU_ASSERT_FATAL(rc == 0);
 	SPDK_CU_ASSERT_FATAL(g_request != NULL);
@@ -568,6 +571,82 @@ test_nvme_ns_cmd_write_zeroes(void)
 	CU_ASSERT_EQUAL(cmd_lba_count, 2);
 
 	nvme_free_request(g_request);
+	cleanup_after_test(&qpair);
+
+	prepare_for_test(&ns, &ctrlr, &qpair, 512, 0, 128 * 1024, 0, false);
+
+	/* valid i/o, split */
+	rc = spdk_nvme_ns_cmd_write_zeroes(&ns, &qpair, 0, 131071, cb_fn, cb_arg, 0);
+	SPDK_CU_ASSERT_FATAL(rc == 0);
+	SPDK_CU_ASSERT_FATAL(g_request != NULL);
+	CU_ASSERT(g_request->cmd.opc == SPDK_NVME_OPC_WRITE_ZEROES);
+	CU_ASSERT(g_request->cmd.nsid == ns.id);
+	CU_ASSERT_EQUAL(g_request->num_children, 2);
+
+	child = TAILQ_FIRST(&g_request->children);
+	SPDK_CU_ASSERT_FATAL(child != NULL);
+	nvme_request_remove_child(g_request, child);
+	nvme_cmd_interpret_rw(&child->cmd, &cmd_lba, &cmd_lba_count);
+	CU_ASSERT(child->num_children == 0);
+	CU_ASSERT(cmd_lba == 0);
+	CU_ASSERT(cmd_lba_count == UINT16_MAX + 1);
+	CU_ASSERT(child->cmd.opc == SPDK_NVME_OPC_WRITE_ZEROES);
+	CU_ASSERT(child->cmd.nsid == ns.id);
+	nvme_free_request(child);
+
+	child = TAILQ_FIRST(&g_request->children);
+	SPDK_CU_ASSERT_FATAL(child != NULL);
+	nvme_request_remove_child(g_request, child);
+	nvme_cmd_interpret_rw(&child->cmd, &cmd_lba, &cmd_lba_count);
+	CU_ASSERT(child->num_children == 0);
+	CU_ASSERT(cmd_lba == UINT16_MAX + 1);
+	CU_ASSERT(cmd_lba_count == UINT16_MAX);
+	CU_ASSERT(child->cmd.opc == SPDK_NVME_OPC_WRITE_ZEROES);
+	CU_ASSERT(child->cmd.nsid == ns.id);
+	nvme_free_request(child);
+
+	CU_ASSERT(TAILQ_EMPTY(&g_request->children));
+	nvme_free_request(g_request);
+	cleanup_after_test(&qpair);
+
+	prepare_for_test(&ns, &ctrlr, &qpair, 512, 0, 128 * 1024, 0, false);
+
+	/* valid i/o, asynchronous split */
+	rc = spdk_nvme_ns_cmd_write_zeroes(&ns, &qpair, 0, 393216, cb_fn, cb_arg, 0);
+	SPDK_CU_ASSERT_FATAL(g_request != NULL);
+	CU_ASSERT(g_request->cmd.opc == SPDK_NVME_OPC_WRITE_ZEROES);
+	CU_ASSERT(g_request->cmd.nsid == ns.id);
+	CU_ASSERT_EQUAL(g_request->num_children, 5);
+	CU_ASSERT_EQUAL(g_request->current_lba, 327680);
+
+	local_lba_count = 0;
+	for (i = 0; i < 5; i++) {
+		child = TAILQ_FIRST(&g_request->children);
+		SPDK_CU_ASSERT_FATAL(child != NULL);
+		nvme_request_remove_child(g_request, child);
+		nvme_cmd_interpret_rw(&child->cmd, &cmd_lba, &cmd_lba_count);
+		CU_ASSERT(child->num_children == 0);
+		CU_ASSERT(cmd_lba == local_lba_count);
+		CU_ASSERT(cmd_lba_count == UINT16_MAX + 1);
+		CU_ASSERT(child->cmd.opc == SPDK_NVME_OPC_WRITE_ZEROES);
+		CU_ASSERT(child->cmd.nsid == ns.id);
+		_spdk_nvme_ns_cmd_write_zeroes_splitter(&ns, g_request, &qpair, g_request->current_lba,
+							393216 - g_request->current_lba, cb_fn, cb_arg, 0);
+		nvme_free_request(child);
+		CU_ASSERT_EQUAL(g_request->num_children, 5 - i);
+		local_lba_count += UINT16_MAX + 1;
+	}
+
+	SPDK_CU_ASSERT_FATAL(rc == 0);
+
+	cleanup_after_test(&qpair);
+
+	prepare_for_test(&ns, &ctrlr, &qpair, 512, 0, 128 * 1024, 0, false);
+
+	/* invalid i/o, no blocks written */
+	rc = spdk_nvme_ns_cmd_write_zeroes(&ns, &qpair, 0, 0, cb_fn, cb_arg, 0);
+	CU_ASSERT(rc == -EINVAL);
+
 	cleanup_after_test(&qpair);
 }
 
