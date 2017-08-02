7fedfb484fb17c57dafc13aa1224ea0a5cc8ba51
Seth Howell <seth.howell@intel.com>
2017-08-02 09:12:36 -0700

    scripts/ceph: confirm that loop device is removed.
    This patch confirms that the rados loop device is removed from the
    system, avioding latent errors on the next build.
    
    Change-Id: Ic8d833aeb4808ba9b0bb462a93b65377e4f7616d
    Signed-off-by: Seth Howell <seth.howell@intel.com>
    Reviewed-on: https://review.gerrithub.io/372327
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- scripts/ceph/stop.sh
+++ scripts/ceph/stop.sh
@@ -5,8 +5,11 @@ set -x
 base_dir=`pwd`
 home_folder=${base_dir}/ceph
 image=${base_dir}/../../../output/ceph_raw.img
+dev_backend=/dev/ceph
+
 pkill -9 ceph
 sleep 3
 umount /dev/loop200p2
+losetup -d $dev_backend
 rm -rf $home_folder
 rm $image
