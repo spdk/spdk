52d2bbf2b753263b66b7fecc83ddb427d00ec42a
Ziye Yang <optimistyzy@gmail.com>
2017-08-02 15:51:55 +0800

    blobfs, cache: Change definition of CACHE_BUFFER_SIZE
    The CACHE_BUFFER_SIZE should depend on CACHE_BUFFER_SHIFT
    
    Change-Id: I2490064a19adc5cbd03a2c231d5f14fbf5b95792
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/blobfs/tree.h
+++ lib/blobfs/tree.h
@@ -44,8 +44,9 @@ struct cache_buffer {
 	bool			in_progress;
 };
 
-#define CACHE_BUFFER_SIZE (256 * 1024)
 #define CACHE_BUFFER_SHIFT (18)
+#define CACHE_BUFFER_SIZE (2 << CACHE_BUFFER_SHIFT)
+
 #define NEXT_CACHE_BUFFER_OFFSET(offset)	\
 	(((offset + CACHE_BUFFER_SIZE) >> CACHE_BUFFER_SHIFT) << CACHE_BUFFER_SHIFT)
 
