7b7f2aa6854745caf6e2803133043132ca400285
GangCao <gang.cao@intel.com>
2017-08-02 21:55:44 -0400

    app: move the core usage display after DPDK init
    Change-Id: I29a46cbaef20e15173781680a62bc8087ca5672f
    Signed-off-by: GangCao <gang.cao@intel.com>
    Reviewed-on: https://review.gerrithub.io/372426
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/event/app.c
+++ lib/event/app.c
@@ -327,6 +327,8 @@ spdk_app_start(struct spdk_app_opts *opts, spdk_event_fn start_fn,
 
 	spdk_env_init(&env_opts);
 
+	printf("Total cores available: %d\n", spdk_env_get_core_count());
+
 	/*
 	 * If mask not specified on command line or in configuration file,
 	 *  reactor_mask will be 0x1 which will enable core 0 to run one
