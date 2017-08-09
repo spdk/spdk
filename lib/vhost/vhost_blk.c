dd3ec6e6b541f54aff5a8154ecd1ffc9fff155e1
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-08-09 18:59:53 +0200

    vhost: pull out common features to vhost_internal.h
    Change-Id: Ie247d7a0266d253769d7bd7d9fa60227749bd502
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/373577
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/vhost/vhost_blk.c
+++ lib/vhost/vhost_blk.c
@@ -606,15 +606,14 @@ spdk_vhost_blk_dump_config_json(struct spdk_vhost_dev *vdev, struct spdk_json_wr
 }
 
 static const struct spdk_vhost_dev_backend vhost_blk_device_backend = {
-	.virtio_features = (1ULL << VHOST_F_LOG_ALL) | (1ULL << VHOST_USER_F_PROTOCOL_FEATURES) |
-	(1ULL << VIRTIO_F_VERSION_1) | (1ULL << VIRTIO_F_NOTIFY_ON_EMPTY) |
+	.virtio_features = SPDK_VHOST_FEATURES |
 	(1ULL << VIRTIO_BLK_F_SIZE_MAX) | (1ULL << VIRTIO_BLK_F_SEG_MAX) |
 	(1ULL << VIRTIO_BLK_F_GEOMETRY) | (1ULL << VIRTIO_BLK_F_RO) |
 	(1ULL << VIRTIO_BLK_F_BLK_SIZE) | (1ULL << VIRTIO_BLK_F_TOPOLOGY) |
 	(1ULL << VIRTIO_BLK_F_BARRIER)  | (1ULL << VIRTIO_BLK_F_SCSI) |
 	(1ULL << VIRTIO_BLK_F_FLUSH)    | (1ULL << VIRTIO_BLK_F_CONFIG_WCE) |
 	(1ULL << VIRTIO_BLK_F_MQ),
-	.disabled_features = (1ULL << VHOST_F_LOG_ALL) | (1ULL << VIRTIO_BLK_F_GEOMETRY) |
+	.disabled_features = SPDK_VHOST_DISABLED_FEATURES | (1ULL << VIRTIO_BLK_F_GEOMETRY) |
 	(1ULL << VIRTIO_BLK_F_RO) | (1ULL << VIRTIO_BLK_F_FLUSH) | (1ULL << VIRTIO_BLK_F_CONFIG_WCE) |
 	(1ULL << VIRTIO_BLK_F_BARRIER) | (1ULL << VIRTIO_BLK_F_SCSI),
 	.dump_config_json = spdk_vhost_blk_dump_config_json,
