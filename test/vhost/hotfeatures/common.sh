8ddcb7499e7cff818fb33dd7cb11e42827c8b750
Lukasz Galka <lukaszx.galka@intel.com>
2017-08-10 21:40:52 +0200

    test/vhost: add vhost hot remove tests
    Adding tests for vhost scsi and blk hot-remove tests.
    
    Change-Id: I1a42bc5b0a8a4b41d66e09aceca2bf7d01710ffb
    Signed-off-by: Lukasz Galka <lukaszx.galka@intel.com>

--- test/vhost/hotfeatures/common.sh
+++ test/vhost/hotfeatures/common.sh
@@ -10,9 +10,13 @@ fio_jobs="$BASE_DIR/fio_jobs/"
 test_type=spdk_vhost_scsi
 reuse_vms=false
 force_build=false
+single_vm=0
 vms=()
 used_vms=""
+nvme_disk=""
+nvme_disk1=""
 disk_split=""
+test_case=""
 x=""
 
 
@@ -37,6 +41,10 @@ function usage() {
     echo "                          DISKS - VM os test disks/devices path (virtio - optional, kernel_vhost - mandatory)"
     echo "                          If test-type=spdk_vhost_blk then each disk can have additional size parameter, e.g."
     echo "                          --vm=X,os.qcow,DISK_size_35G; unit can be M or G; default - 20G"
+    echo "    --test-case"
+    echo "    --nvme-disk"
+    echo "    --nvme-disk1"
+    echo "    --single-vm"
     exit 0
 }
 
@@ -49,6 +57,10 @@ while getopts 'xh-:' optchar; do
             fio-bin=*) fio_bin="${OPTARG#*=}" ;;
             fio-jobs=*) fio_jobs="${OPTARG#*=}" ;;
             test-type=*) test_type="${OPTARG#*=}" ;;
+            test-case=*) test_case="${OPTARG#*=}" ;;
+            nvme-disk=*) nvme_disk="${OPTARG#*=}" ;;
+            nvme-disk1=*) nvme_disk1="${OPTARG#*=}" ;;
+            single-vm=*) single_vm="${OPTARG#*=}" ;;
             vm=*) vms+=("${OPTARG#*=}") ;;
             *) usage $0 "Invalid argument '$OPTARG'" ;;
         esac
@@ -67,15 +79,55 @@ tmp_job=$BASE_DIR/fio_jobs/fio.job.tmp
 
 rpc_py="python $SPDK_BUILD_DIR/scripts/rpc.py -s 127.0.0.1 "
 
-function print_test_fio_header() {
-    echo "==============="
-    echo ""
-    echo "INFO: Testing..."
+function back_configuration() {
+    rm $BASE_DIR/vhost.conf.in
+    echo "$1">/sys/bus/pci/drivers/uio_pci_generic/bind
+}
 
-    echo "INFO: Running fio jobs ..."
-    if [ $# -gt 0 ]; then
-        echo $1
+function config_base() {
+    cat $BASE_DIR/vhost.conf.base>$BASE_DIR/vhost.conf.in
+    $SPDK_BUILD_DIR/scripts/gen_nvme.sh>$BASE_DIR/vhost.conf.in
+    echo "HotplugEnable Yes">$BASE_DIR/vhost.conf.in
+    echo "[Split]">$BASE_DIR/vhost.conf.in
+    echo "  Split Nvme0n1 2">$BASE_DIR/vhost.conf.in
+    echo "  Split Nvme1n1 2">$BASE_DIR/vhost.conf.in
+}
+
+function check_fio_retcode() {
+    fio_retcode=$3
+    echo $1
+    retcode_expected=$2
+    if test $retcode_expected == 0; then
+        if [ "$fio_retcode" != 0 ]; then
+            echo "    Fio test ended with error."
+            vm_shutdown_all
+            spdk_vhost_kill
+            exit 1
+        else
+            echo "    Fio test ended with success."
+        fi
+    else
+        if [ "$fio_retcode" != 0 ]; then
+            echo "    Fio test ended with expected error."
+        else
+            echo "    Fio test ended with unexpected success."
+            vm_shutdown_all
+            spdk_vhost_kill
+            exit 1
+        fi
     fi
+}
+
+function get_disk() {
+    vm_check_scsi_location $1
+    disk_array=( $SCSI_DISK )
+    NVME_DISK=${disk_array[$1]}
+}
+
+function get_nvme_pci_addr() {
+    chmod 755 $1
+    [[ $(  grep $2 $1 ) =~ ([0-9a-fA-F]{4}(:[0-9a-fA-F]{2}){2}.[0-9a-fA-F]) ]]
+    echo ${BASH_REMATCH[1]}
 }
 
 function run_vhost() {
@@ -86,7 +138,6 @@ function run_vhost() {
     $BASE_DIR/../common/run_vhost.sh $x --work-dir=$TEST_DIR --conf-dir=$BASE_DIR
     echo
 }
-
 
 function vms_setup() {
     for vm_conf in ${vms[@]}; do
@@ -117,12 +168,12 @@ function vms_setup() {
 function vms_setup_and_run() {
     vms_setup
     # Run everything
-    $BASE_DIR/../common/vm_run.sh $x --work-dir=$TEST_DIR $used_vms
-    vm_wait_for_boot 600 $used_vms
+    $BASE_DIR/../common/vm_run.sh $x --work-dir=$TEST_DIR $1
+    vm_wait_for_boot 600 $1
 }
 
 function vms_prepare() {
-    for vm_num in $used_vms; do
+    for vm_num in $1; do
         vm_dir=$VM_BASE_DIR/$vm_num
 
         qemu_mask_param="VM_${vm_num}_qemu_mask"
@@ -134,36 +185,57 @@ function vms_prepare() {
     done
 }
 
+function vms_reboot() {
+    vms_reboot_all $1
+    vms_prepare $1
+}
+
 function vms_reboot_all() {
     echo "Rebooting all vms "
-    for vm_num in $used_vms; do
+    for vm_num in $1; do
         vm_ssh $vm_num "reboot" || true
     done
 
-    vm_wait_for_boot 600 $used_vms
+    vm_wait_for_boot 600 $1
 }
 
-function check_fio_retcode() {
-    fio_retcode=$3
-    echo $1
-    retcode_expected=$2
-    if test $retcode_expected == 0; then
-        if [ "$fio_retcode" != 0 ]; then
-            echo "    Fio test ended with error."
-            vm_shutdown_all
-            spdk_vhost_kill
-            exit 1
+function vms_start() {
+    vms_setup_and_run $1
+    vms_prepare $1
+}
+
+function unbind_bdev() {
+    (sleep 5; echo "$1">/sys/bus/pci/devices/$bdf/driver/unbind) &
+}
+
+function prepare_fio() {
+    run_fio="$fio_bin --eta=never "
+    for vm_num in $1; do
+        cp $fio_job $tmp_job
+        vm_dir=$VM_BASE_DIR/$vm_num
+        if [[ $test_type == "spdk_vhost_scsi" ]]; then
+            vm_check_scsi_location $vm_num
         else
-            echo "    Fio test ended with success."
+            vm_check_blk_location $vm_num
         fi
-    else
-        if [ "$fio_retcode" != 0 ]; then
-            echo "    Fio test ended with expected error."
-        else
-            echo "    Fio test ended with unexpected success."
-            vm_shutdown_all
-            spdk_vhost_kill
-            exit 1
-        fi
+        for disk in $SCSI_DISK; do
+            echo "[nvme-host$disk]"
+            echo "filename=/dev/$disk"
+            echo "time_based=20"
+        done >> $tmp_job
+        vm_scp "$vm_num" $tmp_job 127.0.0.1:/root/default_integrity_discs.job
+        run_fio+="--client=127.0.0.1,$(vm_fio_socket $vm_num) --remote-config /root/default_integrity_discs.job "
+        rm $tmp_job
+    done
+}
+
+function print_test_fio_header() {
+    echo "==============="
+    echo ""
+    echo "INFO: Testing..."
+
+    echo "INFO: Running fio jobs ..."
+    if [ $# -gt 0 ]; then
+        echo $1
     fi
 }
