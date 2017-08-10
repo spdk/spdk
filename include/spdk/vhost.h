4bf2f2367bd58922dce04e3450a06b3c20b071c4
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-08-10 19:51:40 +0200

    vhost: added public documentation
    Change-Id: Ib857d996ebc971ee28f5c9e88725ff6ee2a4febe
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- include/spdk/vhost.h
+++ include/spdk/vhost.h
@@ -43,36 +43,212 @@
 
 #include "spdk/event.h"
 
+/**
+ * Init vhost event subsystem. This is called once on event
+ * subsystem initialization and should not be called by the user.
+ */
 int spdk_vhost_init(void);
+
+/**
+ * Deinit vhost event subsystem. This is called once on event
+ * subsystem deinitialization and should not be called by the user.
+ */
 int spdk_vhost_fini(void);
 
 /**
- * \param event event object. event arg1 is optional path to vhost socket.
+ * Init vhost application. This is called once by SPDK app layer
+ * and should not be called by the user.
+ * \param arg1 is optional path to vhost socket.
+ * \param arg2 unused
  */
 void spdk_vhost_startup(void *arg1, void *arg2);
+
+/**
+ * Deinit vhost application. This is called once by SPDK app layer
+ * and should not be called by the user.
+ */
 void spdk_vhost_shutdown_cb(void);
 
-/* Forward declaration */
+/**
+ * SPDK vhost controller.
+ *
+ * All controller-changing functions operate directly on this object.
+ * Note that \c spdk_vhost_dev cannot be accquired. This object is
+ * only accessible as a callback parameter via \c
+ * spdk_vhost_call_external_event and it's derivatives. This ensures
+ * that all access to the controller is piped through a single,
+ * thread-safe API.
+ */
 struct spdk_vhost_dev;
 
+/** Synchronized vhost event used for user callbacks */
 typedef int (*spdk_vhost_event_fn)(struct spdk_vhost_dev *, void *);
 
+/**
+ * Get name of the vhost controller. This is equal to the filename
+ * of socket file. The name is constant throughout the lifetime of
+ * a controller.
+ *
+ * \param ctrlr vhost controller
+ * \return name of the controller
+ */
 const char *spdk_vhost_dev_get_name(struct spdk_vhost_dev *ctrl);
+
+/**
+ * Get cpumask of the vhost controller. The mask is constant
+ * throughout the lifetime of a controller. It must be a subset
+ * of SPDK cpumask vhost was started with.
+ *
+ * \param ctrlr vhost controller
+ * \return cpumask of the controller. This is a number greater or
+ * equal than 0 and lesser than number of logical CPU cores.
+ */
 uint64_t spdk_vhost_dev_get_cpumask(struct spdk_vhost_dev *ctrl);
+
+/**
+ * Try to convert cpumask string into valid mask number. Given mask
+ * must be a subset of SPDK cpumask vhost was started with.
+ *
+ * \param mask string containing cpumask in either hex or base 10
+ * to be parsed. The format will be detected automatically. In case
+ * of hex format the leading *0x* is allowed but not required.
+ * \param cpumask pointer where parsed mask will be put. In case
+ * this function returns -1, the value under this pointer is
+ * undefined.
+ * \return 0 on success, -1 on failure. This function will print
+ * error message describing the failure.
+ */
 int spdk_vhost_parse_core_mask(const char *mask, uint64_t *cpumask);
 
+/**
+ * Construct an empty vhost SCSI controller. This will create a
+ * Unix domain socket together with a vhost-user slave server waiting
+ * for a connection on this socket. Creating the controller does not
+ * start any I/O pollers and does not hog the CPU. I/O processing
+ * starts after receiving proper message on the created socket.
+ * All physical devices have to be separately attached to this
+ * controller via \c spdk_vhost_scsi_dev_add_dev().
+ *
+ * This function is thread-safe.
+ *
+ * \param name name of the controller. The name will also be used
+ * for socket name, which is exactly \c socket_base_dir/name
+ * \param mask string containing cpumask in either hex or base 10
+ * to be parsed. The format will be detected automatically. In case
+ * of hex the leading *0x* is allowed but not required.
+ * \return 0 on success, negative errno on error. This function might
+ * print error message describing the failure.
+ */
 int spdk_vhost_scsi_dev_construct(const char *name, uint64_t cpumask);
+
+/**
+ * Remove an empty vhost SCSI controller. The controller must not
+ * have any SCSI devices attached nor have any open connection on
+ * it's socket.
+ *
+ * \param vdev vhost SCSI controller
+ * \return 0 on success, negative errno on error. This function might
+ * print error message describing the failure.
+ */
 int spdk_vhost_scsi_dev_remove(struct spdk_vhost_dev *vdev);
-struct spdk_scsi_dev *spdk_vhost_scsi_dev_get_dev(struct spdk_vhost_dev *ctrl,
-		uint8_t num);
+
+/**
+ * Construct and attach new SCSI device to the vhost SCSI controller
+ * on given (unoccupied) slot. The device will be created with a single
+ * LUN0 associated with given SPDK bdev. Currently only one LUN per
+ * device is supported.
+ *
+ * If vhost SCSI controller has an active socket connection, it is
+ * required that it has negotiated \c VIRTIO_SCSI_F_HOTPLUG feature
+ * flag. Otherwise an -ENOTSUP error code is returned.
+ *
+ * \param vdev vhost SCSI controller
+ * \param scsi_dev_num slot to attach to
+ * \param lun_name name of the SPDK bdev to associate with SCSI LUN0
+ * \return 0 on success, negative errno on error. This function might
+ * print error message describing the failure.
+ */
 int spdk_vhost_scsi_dev_add_dev(struct spdk_vhost_dev *vdev, unsigned scsi_dev_num,
 				const char *lun_name);
+
+/**
+ * Get SCSI device from vhost SCSI controller on given slot. Max
+ * number of available slots is defined by
+ * \c SPDK_VHOST_SCSI_CTRLR_MAX_DEVS.
+ *
+ * \param ctrl vhost SCSI controller
+ * \param num slot id
+ * \return SCSI device on given slot or NULL
+ */
+struct spdk_scsi_dev *spdk_vhost_scsi_dev_get_dev(struct spdk_vhost_dev *ctrl,
+		uint8_t num);
+
+/**
+ * Detach and destruct SCSI device from vhost SCSI controller.
+ *
+ * If vhost SCSI controller has an active socket connection, it is
+ * required that it has negotiated \c VIRTIO_SCSI_F_HOTPLUG feature
+ * flag.Otherwise an -ENOTSUP error code is returned. If the flag has
+ * been negotiated, the device will be marked to be deleted. Actual
+ * deletion is deferred until after all pending I/O to this device
+ * has finished.
+ *
+ * Once the device has been deleted (whether or not vhost SCSI
+ * controller is in use) given callback will be called.
+ *
+ * \param vdev vhost SCSI controller
+ * \param scsi_dev_num slot id to delete device from
+ * \param cb_fn callback to be fired once device has been successfully
+ * deleted. The first parameter of callback function is the vhost SCSI
+ * controller, the second is user provided argument *cb_arg*.
+ * \param cb_arg parameter to be passed to *cb_fn*.
+ * \return 0 on success, negative errno on error. This function might
+ * print error message describing the failure.
+ */
 int spdk_vhost_scsi_dev_remove_dev(struct spdk_vhost_dev *vdev, unsigned scsi_dev_num,
 				   spdk_vhost_event_fn cb_fn, void *cb_arg);
 
+/**
+ * Construct a vhost blk controller. This will create a Unix domain
+ * socket together with a vhost-user slave server waiting for a
+ * connection on this socket. Creating the controller does not start
+ * any I/O pollers and does not hog the CPU. I/O processing starts
+ * after receiving proper message on the created socket. Vhost blk
+ * controller is tightly associated with given SPDK bdev. Given
+ * bdev can not be changed, unless it has been hotremoved. This
+ * would result in all I/O failing with virtio \c VIRTIO_BLK_S_IOERR
+ * error code.
+ *
+ * This function is thread-safe.
+ *
+ * \param name name of the controller. The name will also be used
+ * for socket name, which is exactly \c socket_base_dir/name
+ * \param mask string containing cpumask in either hex or base 10
+ * to be parsed. The format will be detected automatically. In case
+ * of hex the leading *0x* is allowed but not required.
+ * \return 0 on success, negative errno on error. This function might
+ * print error message describing the failure.
+ */
 int spdk_vhost_blk_construct(const char *name, uint64_t cpumask, const char *dev_name,
 			     bool readonly);
+
+/**
+ * Remove a vhost blk controller. The controller must not have any
+ * open connection on it's socket.
+ *
+ * \param vdev vhost blk controller
+ * \return 0 on success, negative errno on error. This function might
+ * print error message describing the failure.
+ */
 int spdk_vhost_blk_destroy(struct spdk_vhost_dev *dev);
+
+/**
+ * Get underlying SPDK bdev from vhost blk controller.
+ * The bdev might be NULL, as it could have been hotremoved.
+ *
+ * \param ctrl vhost blk controller
+ * \return SPDK bdev associated with given controller
+ */
 struct spdk_bdev *spdk_vhost_blk_get_dev(struct spdk_vhost_dev *ctrlr);
 
 /**
