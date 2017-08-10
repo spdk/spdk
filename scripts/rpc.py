51a7074dc5f1d85be9466855ae3c6ba38aac9b67
Girish Chandrashekar <Girish.Chandrashekar@netapp.com>
2017-08-10 04:22:04 -0400

    util: add histogram facility
      - add histogram facility power 2 scale bucket ranges and rest
      - add histogram facility avoid jsonrpc changes
      - add histogram viewer and fix in histogram range calc
    
    Change-Id: I6db92dda23ed8cabef7f222aba0c9523e8f75bf7
    Signed-off-by: John Meneghini <johnm@netapp.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -531,6 +531,58 @@ p.add_argument('ctrlr', help='controller name to remove device from')
 p.add_argument('scsi_dev_num', help='scsi_dev_num', type=int)
 p.set_defaults(func=remove_vhost_scsi_dev)
 
+def hist_enable(args):
+    params = {
+        'hist_id': args.hist_id
+    }
+    print(jsonrpc_call('hist_enable', params))
+
+p = subparsers.add_parser('hist_enable', help='histogram enable')
+p.add_argument('hist_id', help='Histogram ID', type=int)
+p.set_defaults(func=hist_enable)
+
+def hist_get_stats(args):
+    params = {
+        'hist_id': args.hist_id
+    }
+    print_dict(jsonrpc_call('hist_get_stats', params))
+
+p = subparsers.add_parser('hist_get_stats', help='histogram get_stats')
+p.add_argument('hist_id', help='Histogram ID', type=int)
+p.set_defaults(func=hist_get_stats)
+
+def hist_list_ids(args):
+    print_dict(jsonrpc_call('hist_list_ids'))
+
+p = subparsers.add_parser('hist_list_ids', help='show details of all existing histogram')
+p.set_defaults(func=hist_list_ids)
+
+def hist_disable(args):
+    params = {
+        'hist_id': args.hist_id
+    }
+    print(jsonrpc_call('hist_disable', params))
+
+p = subparsers.add_parser('hist_disable', help='histogram disable')
+p.add_argument('hist_id', help='Histogram ID', type=int)
+p.set_defaults(func=hist_disable)
+
+def hist_clear(args):
+    params = {
+        'hist_id': args.hist_id
+    }
+    print(jsonrpc_call('hist_clear', params))
+
+p = subparsers.add_parser('hist_clear', help='clear histogram')
+p.add_argument('hist_id', help='Histogram ID', type=int)
+p.set_defaults(func=hist_clear)
+
+def hist_clear_all(args):
+    print(jsonrpc_call('hist_clear_all'))
+
+p = subparsers.add_parser('hist_clear_all', help='clear all the existing histogram')
+p.set_defaults(func=hist_clear_all)
+
 def construct_vhost_blk_controller(args):
     params = {
         'ctrlr': args.ctrlr,
