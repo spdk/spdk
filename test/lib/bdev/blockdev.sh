aab625279419c4bdb5864c4df318b597e715b6c3
Daniel Verkamp <daniel.verkamp@intel.com>
2017-08-11 15:06:17 -0700

    test/bdev: remove partition table after GPT test
    Make sure later tests don't fail due to a GPT partition table created on
    an NVMe device.
    
    Change-Id: I623e4fd0a84588a85f9d1ced6daf539d48267f50
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/lib/bdev/blockdev.sh
+++ test/lib/bdev/blockdev.sh
@@ -81,6 +81,11 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 	timing_exit reset
 fi
 
+
+if grep -q Nvme0 $testdir/bdev.conf; then
+	part_dev_by_gpt $testdir/bdev.conf Nvme0n1 $rootdir reset
+fi
+
 rm -f /tmp/aiofile
 rm -f $testdir/bdev.conf
 timing_exit bdev
