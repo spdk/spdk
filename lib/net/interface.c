41d0166019d2b4560cb5215b958806e0bd7ec300
Seth Howell <seth.howell@intel.com>
2017-08-11 16:27:04 -0700

    lib: replace strerror with strerror_r
    replaces all references to strerror in the spdk lib directory with
    references to the thread safe strerror_r
    
    Change-Id: I80d946cce3299007ee10500b93f7e1c8e503ee41
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/net/interface.c
+++ lib/net/interface.c
@@ -32,6 +32,7 @@
  */
 
 #include "spdk/stdinc.h"
+#include "spdk/string.h"
 
 #include "spdk/log.h"
 #include "spdk/net.h"
@@ -50,6 +51,7 @@ static uint32_t spdk_get_ifc_ipv4(void)
 	int ret;
 	int rtattrlen;
 	int netlink_fd;
+	char errbuf[64];
 	uint32_t ipv4_addr;
 
 	struct {
@@ -89,14 +91,16 @@ static uint32_t spdk_get_ifc_ipv4(void)
 	/* Send and recv the message from kernel */
 	ret = send(netlink_fd, &req, req.n.nlmsg_len, 0);
 	if (ret < 0) {
-		SPDK_ERRLOG("netlink send failed: %s\n", strerror(errno));
+		spdk_strerror_r(errno, errbuf, sizeof(errbuf));
+		SPDK_ERRLOG("netlink send failed: %s\n", errbuf);
 		ret = 1;
 		goto exit;
 	}
 
 	ret = recv(netlink_fd, buf, sizeof(buf), 0);
 	if (ret <= 0) {
-		SPDK_ERRLOG("netlink recv failed: %s\n", strerror(errno));
+		spdk_strerror_r(errno, errbuf, sizeof(errbuf));
+		SPDK_ERRLOG("netlink recv failed: %s\n", errbuf);
 		ret = 1;
 		goto exit;
 	}
