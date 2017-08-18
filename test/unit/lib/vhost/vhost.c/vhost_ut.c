d900a8584356c719af7b6de8159180c9fc8b3986
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2017-08-18 14:32:54 +0200

    vhost.c: add unit tests
    Change-Id: I6a2376fc5f582d1234290343df04bd5e8ded8db2
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/unit/lib/vhost/vhost.c/vhost_ut.c
+++ test/unit/lib/vhost/vhost.c/vhost_ut.c
@@ -95,6 +95,7 @@ alloc_vdev(void)
 	vdev->mem->regions[1].guest_phys_addr = 0x400000;
 	vdev->mem->regions[1].size = 0x400000; /* 4 MB */
 	vdev->mem->regions[1].host_user_addr = 0x2000000;
+	vdev->vid = 0x10;
 
 	return vdev;
 }
@@ -198,6 +199,23 @@ desc_to_iov_test(void)
 	CU_ASSERT(true);
 }
 
+static void
+dev_find_by_vid_test(void)
+{
+	struct spdk_vhost_dev *vdev;
+
+	g_spdk_vhost_devices[0] = alloc_vdev();
+
+	vdev = spdk_vhost_dev_find_by_vid(0x10);
+	CU_ASSERT(vdev != NULL);
+
+	vdev = spdk_vhost_dev_find_by_vid(0xFF);
+	CU_ASSERT(vdev == NULL);
+
+	free_vdev(g_spdk_vhost_devices[0]);
+	g_spdk_vhost_devices[0] = NULL;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -215,7 +233,8 @@ main(int argc, char **argv)
 	}
 
 	if (
-		CU_add_test(suite, "desc_to_iov", desc_to_iov_test) == NULL
+		CU_add_test(suite, "desc_to_iov", desc_to_iov_test) == NULL ||
+		CU_add_test(suite, "dev_find_by_vid", dev_find_by_vid_test) == NULL
 	) {
 		CU_cleanup_registry();
 		return CU_get_error();
