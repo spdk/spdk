b4ff933233734009608bd7aebe171efacf746ed7
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-08-21 14:48:06 +0200

    bdev_virtio: enabled debug logs
    Change-Id: I7f03921fcc8412f3e36e135959d30f5564dc9b29
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio_logs.h
+++ lib/bdev/virtio/rte_virtio/virtio_logs.h
@@ -36,10 +36,17 @@
 
 #include <rte_log.h>
 
+#ifdef DEBUG
+#define RTE_LIBRTE_VIRTIO_DEBUG_INIT 1
+#define RTE_LIBRTE_VIRTIO_DEBUG_RX 1
+#define RTE_LIBRTE_VIRTIO_DEBUG_TX 1
+#define RTE_LIBRTE_VIRTIO_DEBUG_DRIVER 1
+#endif
+
 #ifdef RTE_LIBRTE_VIRTIO_DEBUG_INIT
 #define PMD_INIT_LOG(level, fmt, args...) \
-	RTE_LOG(level, PMD, "%s(): " fmt "\n", __func__, ## args)
-#define PMD_INIT_FUNC_TRACE() PMD_INIT_LOG(DEBUG, " >>")
+	RTE_LOG(ERR, PMD, "%s(): " fmt "\n", __func__, ## args)
+#define PMD_INIT_FUNC_TRACE() PMD_INIT_LOG(ERR, " >>")
 #else
 #define PMD_INIT_LOG(level, fmt, args...) do { } while(0)
 #define PMD_INIT_FUNC_TRACE() do { } while(0)
@@ -47,14 +54,14 @@
 
 #ifdef RTE_LIBRTE_VIRTIO_DEBUG_RX
 #define PMD_RX_LOG(level, fmt, args...) \
-	RTE_LOG(level, PMD, "%s() rx: " fmt "\n", __func__, ## args)
+	RTE_LOG(ERR, PMD, "%s() rx: " fmt "\n", __func__, ## args)
 #else
 #define PMD_RX_LOG(level, fmt, args...) do { } while(0)
 #endif
 
 #ifdef RTE_LIBRTE_VIRTIO_DEBUG_TX
 #define PMD_TX_LOG(level, fmt, args...) \
-	RTE_LOG(level, PMD, "%s() tx: " fmt "\n", __func__, ## args)
+	RTE_LOG(ERR, PMD, "%s() tx: " fmt "\n", __func__, ## args)
 #else
 #define PMD_TX_LOG(level, fmt, args...) do { } while(0)
 #endif
@@ -62,7 +69,7 @@
 
 #ifdef RTE_LIBRTE_VIRTIO_DEBUG_DRIVER
 #define PMD_DRV_LOG(level, fmt, args...) \
-	RTE_LOG(level, PMD, "%s(): " fmt "\n", __func__, ## args)
+	RTE_LOG(ERR, PMD, "%s(): " fmt "\n", __func__, ## args)
 #else
 #define PMD_DRV_LOG(level, fmt, args...) do { } while(0)
 #endif
