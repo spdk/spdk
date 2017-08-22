185bdb57e6820e2ca485959e2f5982359bb33f1c
Lukasz Galka <lukaszx.galka@intel.com>
2017-08-22 17:38:30 +0200

    test/lvol: RPC lvol tests
    Functional negative and positive lvol tests using RPC calls
    
    Change-Id: I9b8d8501b8ab3087e786886ef1e78abe287f8820
    Signed-off-by: Lukasz Galka <lukaszx.galka@intel.com>
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

---
+++ test/lvol/lvol_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+from test_cases import *
+
+def check_fail_count(fail_count, num_test):
+    if not fail_count:
+        print("Tests: {num_test} - PASSED".format(num_test=num_test))
+    else:
+        print("Test: {num_test} - FAILED".format(num_test = num_test))
+
+if __name__ == "__main__":
+    rpc_py = None
+    total_size = None
+    block_size = None
+    num_test = None
+    fail_count = 0
+    tc_list = []
+
+    if len(sys.argv) >= 5 and len(sys.argv) <= test_counter():
+        rpc_py = sys.argv[1]
+        total_size = int(sys.argv[2])
+        block_size = int(sys.argv[3])
+        base_dir_path = int(sys.argv[4])
+        tc_list = sys.argv[5].split(',')
+    else:
+        print("Invalid argument")
+    try:
+        tc = TestCases(rpc_py, total_size, block_size, base_dir_path)
+        if "all" in tc_list:
+            for num_test in range(1, test_counter() + 1):
+                exec("fail_count += tc.test_case{num_test}"
+                     "()".format(num_test=num_test))
+                check_fail_count(fail_count, num_test)
+        else:
+            for num_test in tc_list:
+                exec("fail_count += tc.test_case{num_test}"
+                     "()".format(num_test=num_test))
+                check_fail_count(fail_count, num_test)
+        if not fail_count:
+            print("RESULT: All test cases - PASSED")
+    except:
+        print("Test: {num_test} - FAILED".format(num_test=num_test))
