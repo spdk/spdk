d924cdff9df686558fdb9d171cc6ba58dfa8c918
Lukasz Galka <lukaszx.galka@intel.com>
2017-08-22 17:38:30 +0200

    test/lvol: RPC lvol tests
    Functional negative and positive lvol tests using RPC calls
    
    Change-Id: I9b8d8501b8ab3087e786886ef1e78abe287f8820
    Signed-off-by: Lukasz Galka <lukaszx.galka@intel.com>
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -126,6 +126,7 @@ if [ $SPDK_TEST_VHOST -eq 1 ]; then
 	run_test ./test/vhost/spdk_vhost.sh --integrity
 	run_test ./test/vhost/spdk_vhost.sh --integrity-lvol-scsi
 	run_test ./test/vhost/spdk_vhost.sh --integrity-lvol-blk
+	run_test ./test/lvol/lvol.sh --test-cases=1,2,3,5,6,7,9,10,11,12,13,16,17,21
 	timing_exit vhost
 fi
 
