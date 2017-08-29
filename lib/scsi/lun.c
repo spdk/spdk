9d61026b38d28c127f8f7532103e4bdbe03af6f1
Daniel Verkamp <daniel.verkamp@intel.com>
2017-08-29 16:42:37 -0700

    scsi: remove LUN I/O channel ref counting
    spdk_bdev_get_io_channel() already handles this.
    
    Change-Id: I6b28fe10b86b00762ff15324fcd0a32aae94e012
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/scsi/lun.c
+++ lib/scsi/lun.c
@@ -373,12 +373,8 @@ spdk_scsi_lun_delete(const char *lun_name)
 int spdk_scsi_lun_allocate_io_channel(struct spdk_scsi_lun *lun)
 {
 	if (lun->io_channel != NULL) {
-		if (pthread_self() == lun->thread_id) {
-			lun->ref++;
-			return 0;
-		}
-		SPDK_ERRLOG("io_channel already allocated for lun %s\n", lun->name);
-		return -1;
+		/* io_channel was already allocated from a different thread */
+		assert(spdk_io_channel_get_thread(lun->io_channel) == spdk_get_thread());
 	}
 
 	lun->lcore = spdk_env_get_current_core();
@@ -387,19 +383,14 @@ int spdk_scsi_lun_allocate_io_channel(struct spdk_scsi_lun *lun)
 	if (lun->io_channel == NULL) {
 		return -1;
 	}
-	lun->thread_id = pthread_self();
-	lun->ref = 1;
 	return 0;
 }
 
 void spdk_scsi_lun_free_io_channel(struct spdk_scsi_lun *lun)
 {
 	if (lun->io_channel != NULL) {
-		lun->ref--;
-		if (lun->ref == 0) {
-			spdk_put_io_channel(lun->io_channel);
-			lun->io_channel = NULL;
-		}
+		spdk_put_io_channel(lun->io_channel);
+		lun->io_channel = NULL;
 	}
 }
 
