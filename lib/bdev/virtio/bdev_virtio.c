db94fa19c65d540f447a149667aa0337e65feda7
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-08-31 17:04:08 +0200

    rte_virtio: removed eth_virtio_dev_init()
    This patch also fixes segfault on startup when using
    a PCI device as a DPDK secondary process.
    
    Change-Id: I93116ae207649fae2fb3461d32d04a98cecb4cf6
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/bdev_virtio.c
+++ lib/bdev/virtio/bdev_virtio.c
@@ -503,7 +503,7 @@ bdev_virtio_initialize(void)
 			       spdk_env_get_current_core(), 0);
 
 	/* TODO check rc, add virtio_dev_deinit() */
-	eth_virtio_dev_init(hw);
+	virtio_init_device(hw, VIRTIO_PMD_DEFAULT_GUEST_FEATURES);
 	virtio_dev_start(hw);
 
 	for (i = 0; i < 64; i++) {
