9b47ee13f209d5e8507cf80d02f1dda5d9c7fe77
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-08-31 17:17:57 +0200

    vhost: add virtual queue size mask to not calculate it all over again
    Queue size is set once during vdev startup so no need to recalculate.
    
    Change-Id: I7d97b611246dc2fbbb2c4f1353c49f41e5eaae38
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- lib/vhost/vhost_internal.h
+++ lib/vhost/vhost_internal.h
@@ -83,6 +83,7 @@ enum spdk_vhost_dev_type {
 
 struct spdk_vhost_virtqueue {
 	struct rte_vhost_vring vring;
+	uint16_t size_mask;
 } __attribute((aligned(SPDK_CACHE_LINE_SIZE)));
 
 struct spdk_vhost_dev_backend {
