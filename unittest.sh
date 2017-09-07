e23c8a7c2d2c50d5b9efdb20b6a85f882e4d2247
pelplinski <piotr.pelplinski@intel.com>
2017-09-07 09:47:36 +0200

    lvol: Logical volume implementation
    Change-Id: Ia96ae78ff9530d953181ac5f7255a38f3c8ec430
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>

--- unittest.sh
+++ unittest.sh
@@ -44,6 +44,7 @@ fi
 $valgrind test/unit/lib/bdev/bdev.c/bdev_ut
 $valgrind test/unit/lib/bdev/scsi_nvme.c/scsi_nvme_ut
 $valgrind test/unit/lib/bdev/gpt/gpt.c/gpt_ut
+$valgrind test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut
 
 $valgrind test/unit/lib/blob/blob.c/blob_ut
 $valgrind test/unit/lib/blobfs/tree.c/tree_ut
@@ -81,6 +82,9 @@ $valgrind test/unit/lib/scsi/dev.c/dev_ut
 $valgrind test/unit/lib/scsi/lun.c/lun_ut
 $valgrind test/unit/lib/scsi/scsi.c/scsi_ut
 $valgrind test/unit/lib/scsi/scsi_bdev.c/scsi_bdev_ut
+
+$valgrind test/unit/lib/lvol/lvol.c/lvol_ut
+$valgrind test/unit/lib/lvol/bdev/lvs_bdev.c/lvs_bdev_ut
 
 $valgrind test/unit/lib/iscsi/param.c/param_ut
 $valgrind test/unit/lib/iscsi/tgt_node.c/tgt_node_ut test/unit/lib/iscsi/tgt_node.c/tgt_node.conf
