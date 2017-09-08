34e3bf9c86eb3f7b335a399f6a163ccbc65ecb06
Daniel Verkamp <daniel.verkamp@intel.com>
2017-09-08 15:12:02 -0700

    test: move subsystem_ut to unit test directory
    Change-Id: I22c4273812922e1a2f7bb7ec0d3e8353e74780a3
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/377822
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/lib/event/event.sh
+++ test/lib/event/event.sh
@@ -8,5 +8,4 @@ timing_enter event
 $testdir/event_perf/event_perf -m 0xF -t 1
 $testdir/reactor/reactor -t 1
 $testdir/reactor_perf/reactor_perf -t 1
-$testdir/subsystem/subsystem_ut
 timing_exit event
