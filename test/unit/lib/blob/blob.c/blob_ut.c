92d07a747638aabf253cc516d6cd6e74551d075a
Cunyin Chang <cunyin.chang@intel.com>
2017-09-13 11:09:22 +0800

    blob: Add support for crc of super block.
    Change-Id: I424833a0af6e5e8265c03c168b9dfdbae1a49cd8
    Signed-off-by: Cunyin Chang <cunyin.chang@intel.com>

--- test/unit/lib/blob/blob.c/blob_ut.c
+++ test/unit/lib/blob/blob.c/blob_ut.c
@@ -1250,6 +1250,7 @@ blob_crc(void)
 	uint32_t page_num;
 	int index;
 	struct spdk_blob_md_page *page;
+	struct spdk_bs_super_block *super_blob;
 
 	dev = init_dev();
 
@@ -1289,6 +1290,14 @@ blob_crc(void)
 	spdk_bs_unload(g_bs, bs_op_complete, NULL);
 	CU_ASSERT(g_bserrno == 0);
 	g_bs = NULL;
+
+	super_blob = (struct spdk_bs_super_block *)g_dev_buffer;
+	super_blob->crc = 0;
+	dev = init_dev();
+	/* Load an existing blob store */
+	spdk_bs_load(dev, bs_op_with_handle_complete, NULL);
+
+	CU_ASSERT(g_bserrno == -EILSEQ);
 }
 
 int main(int argc, char **argv)
