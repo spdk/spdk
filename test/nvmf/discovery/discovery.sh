197d41f2fe19b26030fe11b4f40a397389ff54e3
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-09-14 15:42:57 +0200

    test/bdev/gpt: Verifying issue on test pool
    Please do not merge, just testing for negative results.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I07d93d4019a2e456023a47173b8519d2e5570c9c

--- test/nvmf/discovery/discovery.sh
+++ test/nvmf/discovery/discovery.sh
@@ -49,6 +49,16 @@ $rpc_py get_nvmf_subsystems
 
 $rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk:cnode1
 
+$rpc_py delete_bdev Null1
+$rpc_py delete_bdev Null0
+
+check_bdevs=$($rpc get_bdevs | jq -r '.[].name')
+if [ ! -z "$check_bdevs" ]; then
+        echo $check_bdevs
+	exit 1
+fi
+
+
 trap - SIGINT SIGTERM EXIT
 
 nvmfcleanup
