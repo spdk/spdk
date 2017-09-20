01772be0ea6008f92ec5720341e43d6c7739de54
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-09-20 18:06:28 +0200

    rte_virtio: removed vtpci->reset callback
    It's not used anywhere. There's already a
    vtpci->set_status(RESET) equivalent that
    does the same.
    
    Change-Id: I0f04ba094848806c647ede4be2ef77b423ead288
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio_user.c
+++ lib/bdev/virtio/rte_virtio/virtio_user.c
@@ -69,21 +69,13 @@ virtio_user_write_dev_config(struct virtio_dev *vdev, size_t offset,
 }
 
 static void
-virtio_user_reset(struct virtio_dev *vdev)
-{
-	struct virtio_user_dev *dev = virtio_dev_get_user_dev(vdev);
-
-	if (dev->status & VIRTIO_CONFIG_STATUS_DRIVER_OK)
-		virtio_user_stop_device(dev);
-}
-
-static void
 virtio_user_set_status(struct virtio_dev *vdev, uint8_t status)
 {
 	struct virtio_user_dev *dev = virtio_dev_get_user_dev(vdev);
 
-	if (status == VIRTIO_CONFIG_STATUS_RESET)
-		virtio_user_reset(vdev);
+	if (status == VIRTIO_CONFIG_STATUS_RESET
+			&& (dev->status & VIRTIO_CONFIG_STATUS_DRIVER_OK))
+		virtio_user_stop_device(dev);
 	dev->status = status;
 }
 
@@ -231,7 +223,6 @@ virtio_user_notify_queue(struct virtio_dev *vdev, struct virtqueue *vq)
 const struct virtio_pci_ops virtio_user_ops = {
 	.read_dev_cfg	= virtio_user_read_dev_config,
 	.write_dev_cfg	= virtio_user_write_dev_config,
-	.reset		= virtio_user_reset,
 	.get_status	= virtio_user_get_status,
 	.set_status	= virtio_user_set_status,
 	.get_features	= virtio_user_get_features,
