f9830f1e930e456c0504f03c1345cbd895db9a2e
Maciej Szwed <maciej.szwed@intel.com>
2017-09-21 12:11:53 +0200

    lvol: Lvol store tasting
    This patch introduces lvol store and lvols parameters saving
    on persistent memories and loading it from saved data on app start.
    
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Ia63f0cf3d6365d59f31c5f0a1724636bfe73b5b8

--- include/spdk_internal/lvolstore.h
+++ include/spdk_internal/lvolstore.h
@@ -72,6 +72,8 @@ struct spdk_lvol_store_req {
 struct spdk_lvol_store {
 	struct spdk_bs_dev		*bs_dev;
 	struct spdk_blob_store		*blobstore;
+	struct spdk_blob		*super_blob;
+	spdk_blob_id			super_blob_id;
 	uuid_t				uuid;
 	uint64_t			page_size;
 	struct spdk_lvol_store_req *destruct_req;
@@ -95,5 +97,6 @@ struct lvol_task {
 
 struct lvol_store_bdev *vbdev_lvol_store_first(void);
 struct lvol_store_bdev *vbdev_lvol_store_next(struct lvol_store_bdev *prev);
+void spdk_lvs_examine_cb(void *cb_arg, struct spdk_blob_store *bs, int bserrno);
 
 #endif /* SPDK_INTERNAL_LVOLSTORE_H */
