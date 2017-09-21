76f5eecbbabf0e95b0328646885a0c2c222f3605
Maciej Szwed <maciej.szwed@intel.com>
2017-09-21 12:11:53 +0200

    lvol: Lvol store tasting
    This patch introduces lvol store and lvols parameters saving
    on persistent memories and loading it from saved data on app start.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Ia63f0cf3d6365d59f31c5f0a1724636bfe73b5b8

--- test/unit/lib/lvol/lvol.c/lvol_ut.c
+++ test/unit/lib/lvol/lvol.c/lvol_ut.c
@@ -51,10 +51,36 @@ struct spdk_lvol *g_lvol;
 struct spdk_blob_store {};
 
 void
+spdk_bs_get_super(struct spdk_blob_store *bs,
+		  spdk_blob_op_with_id_complete cb_fn, void *cb_arg)
+{
+	return;
+}
+
+void
 spdk_bs_set_super(struct spdk_blob_store *bs, spdk_blob_id blobid,
 		  spdk_bs_op_complete cb_fn, void *cb_arg)
 {
 	cb_fn(cb_arg, 0);
+}
+
+const char *
+spdk_bs_get_type(struct spdk_blob_store *bs)
+{
+	return NULL;
+}
+
+void
+spdk_bs_opts_init(struct spdk_bs_opts *opts)
+{
+	return;
+}
+
+void
+spdk_bs_load(struct spdk_bs_dev *dev,
+	     spdk_bs_op_with_handle_complete cb_fn, void *cb_arg)
+{
+	return;
 }
 
 struct spdk_io_channel *spdk_bs_alloc_io_channel(struct spdk_blob_store *bs)
@@ -65,6 +91,13 @@ struct spdk_io_channel *spdk_bs_alloc_io_channel(struct spdk_blob_store *bs)
 int
 spdk_blob_md_set_xattr(struct spdk_blob *blob, const char *name, const void *value,
 		       uint16_t value_len)
+{
+	return 0;
+}
+
+int
+spdk_bs_md_get_xattr_value(struct spdk_blob *blob, const char *name,
+			   const void **value, size_t *value_len)
 {
 	return 0;
 }
