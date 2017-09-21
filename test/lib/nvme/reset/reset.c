46ee2b5a8591b4e3569005507a723fb6e3e9f4a6
Liang Yan <liang.z.yan@intel.com>
2017-09-21 11:24:03 +0800

    test:move env initialization steps after the parameters parsing
    Move the nvme/reset tool's environment initialization steps
    after the parameter parsing.
    
    Change-Id: Id7a4302fa85b88b279895453293c701b982914a7
    Signed-off-by: Liang Yan <liang.z.yan@intel.com>

--- test/lib/nvme/reset/reset.c
+++ test/lib/nvme/reset/reset.c
@@ -619,15 +619,16 @@ int main(int argc, char **argv)
 	int 			i;
 	struct spdk_env_opts	opts;
 
-	spdk_env_opts_init(&opts);
-	opts.name = "reset";
-	opts.core_mask = "0x1";
-	spdk_env_init(&opts);
 
 	rc = parse_args(argc, argv);
 	if (rc != 0) {
 		return rc;
 	}
+
+	spdk_env_opts_init(&opts);
+	opts.name = "reset";
+	opts.core_mask = "0x1";
+	spdk_env_init(&opts);
 
 	task_pool = rte_mempool_create("task_pool", 8192,
 				       sizeof(struct reset_task),
