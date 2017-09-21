e2dce026d9b6405620f3befa1be5e96a8d4eb052
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-09-21 18:14:05 +0200

    bdev_pmem: unit tests
    Change-Id: I4837d489d4730423825929f0e53d3ef6b487f0d0
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- unittest.sh
+++ unittest.sh
@@ -42,6 +42,7 @@ if [ "$cov_avail" = "yes" ]; then
 fi
 
 $valgrind test/unit/lib/bdev/bdev.c/bdev_ut
+$valgrind test/unit/lib/bdev/pmem.c/bdev_pmem_ut
 $valgrind test/unit/lib/bdev/scsi_nvme.c/scsi_nvme_ut
 $valgrind test/unit/lib/bdev/gpt/gpt.c/gpt_ut
 $valgrind test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut
