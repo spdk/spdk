fba4efd24f718b668a40670ca03f9604e3eef8a1
Maciej Szwed <maciej.szwed@intel.com>
2017-09-22 14:13:56 +0200

    lvol: create super blob on lvolstore initialization
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I68a56666d30a550de4a2dc5a66fb669dfbad12dd

--- include/spdk_internal/lvolstore.h
+++ include/spdk_internal/lvolstore.h
@@ -72,6 +72,8 @@ struct spdk_lvol_store_req {
 struct spdk_lvol_store {
 	struct spdk_bs_dev		*bs_dev;
 	struct spdk_blob_store		*blobstore;
+	struct spdk_blob		*super_blob;
+	spdk_blob_id			super_blob_id;
 	uuid_t				uuid;
 	uint64_t			page_size;
 	struct spdk_lvol_store_req *destruct_req;
