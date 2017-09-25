38222b406602ee0dadbddae9a4ab7d7fadae9207
heluwei <simple_hlw@163.com>
2017-09-25 16:14:03 +0800

    [WIP]Add Blobkv.c to support kv
    The blobkv.c in which serveral stub functions have been
    written is submitted. It is not rigorous and still being
    modified.
    
    For the blobkv[0], we should be able to add a hashtable into
    the memory to perform matching with key.
    
    When blobkv receives the request of GET, firstly compare the
    k of function kv_blob_read() with the key of the hashtable.
    If the match is successful, it returns the blob’s address.
    And then call the function in the blobstore to read data from
    the specific blob.
    
    When blobkv receives the request of PUT, firstly validate the
    size of v in the function kv_blob_write(). If the data is
    oversize, call the function kv_blob_write_part() to execute
    partial storage. Before storing data v, write the k into the
    hashtable, and then call the function in the blobstore to
    write data on the blob.
    
    When blobkv receives the request of DELETE, like PUT.
    
    To make blobstore not to do changes as much as possible
    to support object storage, we introduced the blobkv_context
    structure, as follows:
    struct blobkv_context {
        struct spdk_blob_store *bs;
        struct spdk_blob *blob;
        spdk_blob_id blobid;
        struct spdk_io_channel *channel;
        uint8_t *read_buff;
        uint8_t *write_buff;
        uint64_t page_size;
    };
    
    [0] https://trello.com/b/P5xBO7UR/things-to-do
    
    Change-Id: Iea361e5fddbe8af44ca8450b8333f8107e835a55
    Signed-off-by: heluwei <simple_hlw@163.com>

---
+++ lib/blobkv/blobkv.c
@@ -0,0 +1,153 @@
+#include "spdk/stdinc.h"
+
+#include "spdk/bdev.h"
+#include "spdk/env.h"
+#include "spdk/event.h"
+#include "spdk/blob_bdev.h"
+#include "spdk/blob.h"
+#include "spdk/log.h"
+#include "hashtable.h"
+
+#include "spdk/hashtable.h"
+
+struct blobkv_context_t
+{
+    struct spdk_blob_store *bs;
+    struct spdk_blob *blob;
+    spdk_blob_id blobid;
+    struct spdk_io_channel *channel;
+    uint8_t *read_buff;
+    uint8_t *write_buff;
+    uint64_t page_size;
+    int rc;
+};
+
+static void
+kv_blob_read(void *k, void *v)
+{
+    if (exists(k))
+    {
+        hashget(k);
+        read_blob(k->blob)
+    }
+}
+
+static void
+kv_blob_write(void *k, void *v)
+{
+    if(checksize(v))
+    {
+        kv_blob_write_part(k, v)
+    }
+    blob_delete(v)
+}
+
+static void
+kv_blob_write_part ( void *k,  struct blobkv_context_t *v, uint64_t size )
+{
+
+}
+
+static void
+kv_blob_delete ( void *k, void *v )
+{
+    if(checksize(v))
+    {
+        kv_blob_write_part(k, v)
+    }
+    blob_write(v)
+}
+
+static void
+kv_blob_delete_part ( void *k,  struct blobkv_context_t *v, uint64_t size )
+{
+
+}
+
+static void
+spdk_bs_destroy ( void *k, void *v )
+{
+
+}
+
+
+static void
+blobkv_cleanup(struct blobkv_context_t *blobkv_context)
+{
+    spdk_dma_free(blobkv_context->read_buff);
+    spdk_dma_free(blobkv_context->write_buff);
+    free(blobkv_context);
+}
+
+static void
+read_blob(struct blobkv_context_t *blobkv_context)
+{
+    SPDK_NOTICELOG("entry\n");
+
+    blobkv_context->read_buff = spdk_dma_malloc(blobkv_context->page_size,
+                                0x1000, NULL);
+
+    spdk_bs_io_read_blob(blobkv_context->blob, blobkv_context->channel,
+                         blobkv_context->read_buff, 0, 1, NULL,
+                         blobkv_context);
+}
+
+static void
+blob_write(struct blobkv_context_t *blobkv_context)
+{
+    SPDK_NOTICELOG("entry\n");
+
+    blobkv_context->write_buff = spdk_dma_malloc(blobkv_context->page_size,
+                                 0x1000, NULL);
+    memset(blobkv_context->write_buff, 0x5a, blobkv_context->page_size);
+
+    blobkv_context->channel = spdk_bs_alloc_io_channel(blobkv_context->bs);
+
+    spdk_bs_io_write_blob(blobkv_context->blob, blobkv_context->channel,
+                          blobkv_context->write_buff,
+                          0, 1, NULL, blobkv_context);
+}
+
+static void
+delete_blob(void *arg1, int bserrno)
+{
+    struct blobkv_context_t *blobkv_context = arg1;
+
+    SPDK_NOTICELOG("entry\n");
+
+    if (bserrno)
+    {
+        unload_bs(blobkv_context, "Error in close completion",
+                  bserrno);
+        return;
+    }
+
+    spdk_bs_md_delete_blob(blobkv_context->bs, blobkv_context->blobid,
+                           NULL, blobkv_context);
+}
+
+
+static void
+unload_bs(struct blobkv_context_t *blobkv_context, char *msg, int bserrno)
+{
+    if (bserrno)
+    {
+        SPDK_ERRLOG("%s (err %d)\n", msg, bserrno);
+        blobkv_context->rc = bserrno;
+    }
+    if (blobkv_context->bs)
+    {
+        if (blobkv_context->channel)
+        {
+            spdk_bs_free_io_channel(blobkv_context->channel);
+        }
+        spdk_bs_unload(blobkv_context->bs, NULL, blobkv_context);
+    }
+    else
+    {
+        spdk_app_stop(bserrno);
+    }
+}
+
+
+
