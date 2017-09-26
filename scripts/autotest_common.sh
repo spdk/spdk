3a276e8b0f440c3a8438949fad32981dfe2d0da1
Pawel Kaminski <pawelx.kaminski@intel.com>
2017-09-26 20:43:40 +0200

    test/vhost: Add vhost initiator tests for guest.
    Change-Id: I60c99957ef68a24af02b763783bb2061465be9b2
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- scripts/autotest_common.sh
+++ scripts/autotest_common.sh
@@ -371,7 +371,7 @@ function discover_bdevs()
 
 	# Start the bdev service to query for the list of available
 	# bdevs.
-	$rootdir/test/app/bdev_svc/bdev_svc -i 0 -c $config_file &>/dev/null &
+	$rootdir/test/app/bdev_svc/bdev_svc -i 0 -s 1024 -c $config_file &>/dev/null &
 	stubpid=$!
 	while ! [ -e /var/run/spdk_bdev0 ]; do
 		sleep 1
