003a40b7941ef356085fa002cf8eb373eda199e9
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2017-09-26 15:02:52 +0200

    test/pmem: RPC pmem functional tests.
    Functional pmem tests according to test plan:
    https://review.gerrithub.io/#/c/378618/18/test/pmem/test_plan.md
    
    Change-Id: Ia4594fe96a542c654656d2a6d2fed18eb1c2fcf7
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

---
+++ test/pmem/common.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(readlink -f $(dirname $0))
+[[ -z "$TEST_DIR" ]] && TEST_DIR="$(cd $BASE_DIR/../../ && pwd)"
+rpc_py="$TEST_DIR/scripts/rpc.py "
+RPC_PORT=5260
+
+source $TEST_DIR/scripts/autotest_common.sh
+
+# Prints error message and return error code, closes vhost app and remove
+# pmem pool file
+# input: error message, error code
+function error()
+{
+	local error_code=${2:-1}
+	echo "==========="
+	echo -e "ERROR: $1"
+	echo "error code: $error_code"
+	echo "==========="
+	vhost_kill
+	pmem_clean_pool_file
+	return $error_code
+}
+
+# check if there is pool file & remove it
+# input: path to pool file
+# default: $TEST_DIR/test/pmem/pool_file
+function pmem_clean_pool_file()
+{
+	local pool_file=${1:-$TEST_DIR/test/pmem/pool_file}
+
+	if [ -f $pool_file ]; then
+		echo "Deleting old pool_file"
+		rm $pool_file
+	fi
+}
+
+# create new pmem file
+# input: path to pool file, size in MB, block_size
+# default: $TEST_DIR/test/pmem/pool_file 32 512
+function pmem_create_pool_file()
+{
+	local pool_file=${1:-$TEST_DIR/test/pmem/pool_file}
+	local size=${2:-32}
+	local block_size=${3:-512}
+
+	pmem_clean_pool_file $pool_file
+	echo "Creating new pool file"
+	if ! $rpc_py create_pmem_pool $pool_file $size $block_size; then
+		error "Creating pool_file failed!"
+	fi
+
+	if [ ! -f $pool_file ]; then
+		error "Creating pool_file failed!"
+	fi
+}
+
+function pmem_unmount_ramspace
+{
+	if [ -d "$TEST_DIR/test/pmem/ramspace" ]; then
+		if mount | grep -q "$TEST_DIR/test/pmem/ramspace"; then
+			umount $TEST_DIR/test/pmem/ramspace
+		fi
+
+		rm -rf $TEST_DIR/test/pmem/ramspace
+	fi
+}
+
+function pmem_print_tc_name
+{
+	echo ""
+	echo "==============================================================="
+	echo "Now running: $1"
+	echo "==============================================================="
+}
+
+function vhost_start()
+{
+	local vhost_conf_template="$TEST_DIR/test/pmem/vhost.conf.in"
+	local vhost_pid
+
+	$TEST_DIR/app/vhost/vhost -c $vhost_conf_template &
+	if [ $? != 0 ]; then
+		echo -e "ERROR: Failed to launch vhost!"
+		return 1
+	fi
+
+	vhost_pid=$!
+	echo $vhost_pid > $TEST_DIR/test/pmem/vhost.pid
+	waitforlisten $vhost_pid $RPC_PORT
+}
+
+function vhost_kill()
+{
+	local vhost_pid_file="$TEST_DIR/test/pmem/vhost.pid"
+	local vhost_pid="$(cat $vhost_pid_file)"
+
+	if [[ ! -f $TEST_DIR/test/pmem/vhost.pid ]]; then
+		echo -e "ERROR: No vhost pid file found!"
+		return 1
+	fi
+
+	if ! kill -s INT $vhost_pid; then
+		echo -e "ERROR: Failed to exit vhost / invalid pid!"
+		rm $vhost_pid_file
+		return 1
+	fi
+
+	sleep 1
+	rm $vhost_pid_file
+}
