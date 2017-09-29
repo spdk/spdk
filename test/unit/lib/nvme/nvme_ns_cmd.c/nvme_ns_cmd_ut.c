508b993c8b82de086eda62fcffc96a0864a0bec0
GangCao <gang.cao@intel.com>
2017-09-29 12:14:00 -0400

    nvme: add a new opts_size parameter for default ctrlr opts
    Add a new parameter for the default ctrlr opts initialization.
    This is to make sure future compatibility when SPDK components
    are built as a shared libary. User's version and SPDK's version
    may be in different size.
    
    The change here is to make sure the backward compatibility when
    new fields are added in the struct spdk_nvme_ctrlr_opts.
    
    Change-Id: Icfc9640993cb06063b825d4df5835d920dd374e5
    Signed-off-by: GangCao <gang.cao@intel.com>

--- test/unit/lib/nvme/nvme_ns_cmd.c/nvme_ns_cmd_ut.c
+++ test/unit/lib/nvme/nvme_ns_cmd.c/nvme_ns_cmd_ut.c
@@ -135,7 +135,7 @@ spdk_pci_addr_compare(const struct spdk_pci_addr *a1, const struct spdk_pci_addr
 }
 
 void
-spdk_nvme_ctrlr_opts_set_defaults(struct spdk_nvme_ctrlr_opts *opts)
+spdk_nvme_ctrlr_opts_set_defaults(struct spdk_nvme_ctrlr_opts *opts, size_t opts_size)
 {
 	memset(opts, 0, sizeof(*opts));
 }
