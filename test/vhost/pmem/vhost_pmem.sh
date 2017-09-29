be0e82399a4f37d336587de9f9382e2accb3b961
Lukasz Galka <lukaszx.galka@intel.com>
2017-09-29 14:48:16 +0200

    test/vhost: Add vhost with NVML backend test
    Add  test script to use SPDK vhost with NVML backends and run FIO read/write
    traffic with verify flag enabled.
    
    Change-Id: I1aa2ebaba9cef667670ed0d453577a1b19423563
    Signed-off-by: Lukasz Galka <lukaszx.galka@intel.com>

---
+++ test/vhost/pmem/vhost_pmem.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+set -ex
+BASE_DIR=$(readlink -f $(dirname $0))
+COMMON_DIR=="$(cd $BASE_DIR/../common)"
+TEST_DIR=$(readlink -f $BASE_DIR/../../../..)
+ROOT_DIR=~
+
+PMEM_SIZE=128
+PMEM_BLOCK_SIZE=512
+LOOP_RANGE=10
+
+rpc_py="python $TEST_DIR/spdk/scripts/rpc.py"
+RPC_PORT=5260
+
+. $BASE_DIR/../common/common.sh
+
+os=$ROOT_DIR/vhost_vm_image.qcow2
+work_dir=$TEST_DIR
+fio_bin=$ROOT_DIR/fio_ubuntu
+ctrl_type=spdk_vhost_blk
+x=""
+
+function fio_jobs ()
+{
+    echo "[global]"
+    echo "ioengine=libaio"
+    echo "size=120M"
+    echo "io_size=10G"
+    echo "filename=/dev/${disk[0]}"
+    echo "numjobs=1"
+    echo "bs=4k"
+    echo "iodepth=128"
+    echo "direct=1"
+    echo "rw=randread"
+    echo "group_reporting"
+    echo "thread"
+    echo "[nvme-host]"
+}
+
+function clear_pmem_pool ()
+{
+    for i in `seq 0 $LOOP_RANGE`; do
+        $rpc_py remove_vhost_controller naa.pmem$i.$i
+        $rpc_py delete_bdev pmem$i
+        $rpc_py delete_pmem_pool /tmp/pool_file$i
+    done
+}
+
+function prepare_fio ()
+{
+    tmp_job=$BASE_DIR/fio.job.tmp
+    run_fio="$fio_bin --eta=never "
+    for vm_num in $1; do
+        vm_dir=$VM_BASE_DIR/$vm_num
+        vm_check_blk_location $vm_num
+        disk=( $SCSI_DISK )
+        fio_jobs>>$tmp_job
+        vm_scp "$vm_num" $tmp_job 127.0.0.1:/root/default_integrity_disc.job
+        run_fio+="--client=127.0.0.1,$(vm_fio_socket $vm_num) --remote-config /root/default_integrity_disc.job "
+        rm $tmp_job
+    done
+}
+
+function usage ()
+{
+    [[ ! -z $2 ]] && ( echo "$2"; echo ""; )
+    echo "Shortcut script for doing automated hotattach/hotdetach test"
+    echo "Usage: $(basename $1) [OPTIONS]"
+    echo
+    echo "-h, --help                print help and exit"
+    echo "    --ctrl-type=TYPE      Controller type to use for test:"
+    echo "                          vhost_scsi - use spdk vhost scsi"
+    echo "                          vhost_blk - use spdk vhost block"
+    echo "-x                        set -x for script debug"
+    echo "    --fio-bin=FIO         Use specific fio binary (will be uploaded to VM)"
+    echo "    --work-dir=WORK_DIR   Where to find build file. Must exist. [default: $ROOT_DIR]"
+    echo "    --os= OS.qcow2        OS - VM os disk path (optional)"
+    exit 0
+}
+
+while getopts 'xh-:' optchar; do
+    case "$optchar" in
+        -)
+        case "$OPTARG" in
+            help) usage $0 ;;
+            work-dir=*) ROOT_DIR="${OPTARG#*=}" ;;
+            ctrl-type=*) ctrl_type="${OPTARG#*=}" ;
+            fio-bin=*) fio_bin="${OPTARG#*=}" ;;
+            os=*) os="${OPTARG#*=}" ;;
+            *) usage $0 "Invalid argument '$OPTARG'" ;;
+        esac
+        ;;
+    h) usage $0 ;;
+    x) set -x
+        x="-x" ;;
+    *) usage $0 "Invalid argument '$OPTARG'"
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+$COMMON_DIR/run_vhost.sh $x --work-dir=$work_dir --conf-dir=$BASE_DIR &
+waitforlisten $! $RPC_PORT
+
+trap "vm_shutdown_all; spdk_vhost_kill; rm -f /tmp/pool_file*; exit 1" SIGINT SIGTERM EXIT
+
+for i in `seq 0 $LOOP_RANGE`; do
+    $rpc_py create_pmem_pool /tmp/pool_file$i $PMEM_SIZE $PMEM_BLOCK_SIZE
+    PMEM_BDEV="$($rpc_py construct_pmem_bdev /tmp/pool_file$i)"
+    if [ $test_type == "ctrl_vhost_blk"]; then 
+        $rpc_py construct_vhost_blk_controller naa.$PMEM_BDEV.$i $PMEM_BDEV
+    else
+        $rpc_py construct_vhost_scsi_controller naa.$PMEM_BDEV.$i
+        $rpc_py add_vhost_scsi_lun naa.$PMEM_BDEV.$i $i $PMEM_BDEV
+    fi
+    $COMMON_DIR/vm_setup.sh $x --work-dir=$work_dir --test-type=$ctrl_type --os=$os --disk=$PMEM_BDEV -f $i
+    if ! [ -d "${work_dir}/vms/${i}" ]; then
+        echo "VM $i is not properly prepared"
+        break
+    fi
+done
+
+$COMMON_DIR/vm_run.sh $x --work-dir=$work_dir `seq 0 $LOOP_RANGE`
+vm_wait_for_boot 600 `seq 0 $LOOP_RANGE`
+vms_prepare
+
+for i in `seq 0 $LOOP_RANGE`; do
+    prepare_fio $i
+    $run_fio
+done
+
+trap - SIGINT SIGTERM EXIT
+
+vm_shutdown_all
+
+rm -f ./local-job*
+clear_pmem_pool
+spdk_vhost_kill
--- test/vhost/spdk_vhost.sh
+++ test/vhost/spdk_vhost.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ ! -f "/home/sys_sgsw/vhost_vm_image.qcow2" ]; then
-	echo "VM does not exist, exiting vhost tests without running"
-	exit 0
+    echo "VM does not exist, exiting vhost tests without running"
+    exit 0
 fi
 
 WORKDIR=$(dirname $0)
@@ -18,68 +18,80 @@ echo Running SPDK vhost fio autotest...
 
 case $param in
     -p|--performance)
-	echo Running performance suite...
-	./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
-	--vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0 \
-	--test-type=spdk_vhost_scsi \
-	--fio-jobs=$WORKDIR/common/fio_jobs/default_performance.job \
-	--qemu-src=/home/sys_sgsw/vhost/qemu
+    echo Running performance suite...
+    ./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0 \
+    --test-type=spdk_vhost_scsi \
+    --fio-jobs=$WORKDIR/common/fio_jobs/default_performance.job \
+    --qemu-src=/home/sys_sgsw/vhost/qemu
     ;;
-	-pb|--performance-blk)
-	echo Running blk performance suite...
-	./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
-	--vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0 \
-	--test-type=spdk_vhost_blk \
-	--fio-jobs=$WORKDIR/common/fio_jobs/default_performance.job \
-	--qemu-src=/home/sys_sgsw/vhost/qemu
+    -pb|--performance-blk)
+    echo Running blk performance suite...
+    ./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0 \
+    --test-type=spdk_vhost_blk \
+    --fio-jobs=$WORKDIR/common/fio_jobs/default_performance.job \
+    --qemu-src=/home/sys_sgsw/vhost/qemu
     ;;
     -i|--integrity)
-	echo Running integrity suite...
-	./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
-	--vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0:Nvme0n1p1:Nvme0n1p2:Nvme0n1p3 \
-	--test-type=spdk_vhost_scsi \
-	--fio-jobs=$WORKDIR/common/fio_jobs/default_integrity.job \
-	--qemu-src=/home/sys_sgsw/vhost/qemu -x
+    echo Running integrity suite...
+    ./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0:Nvme0n1p1:Nvme0n1p2:Nvme0n1p3 \
+    --test-type=spdk_vhost_scsi \
+    --fio-jobs=$WORKDIR/common/fio_jobs/default_integrity.job \
+    --qemu-src=/home/sys_sgsw/vhost/qemu -x
     ;;
     -ib|--integrity-blk)
-	echo Running blk integrity suite...
-	./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
-	--vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0:Nvme0n1p1:Nvme0n1p2:Nvme0n1p3 \
-	--test-type=spdk_vhost_blk \
-	--fio-jobs=$WORKDIR/common/fio_jobs/default_integrity.job \
-	--qemu-src=/home/sys_sgsw/vhost/qemu -x
+    echo Running blk integrity suite...
+    ./fiotest/autotest.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --vm=0,/home/sys_sgsw/vhost_vm_image.qcow2,Nvme0n1p0:Nvme0n1p1:Nvme0n1p2:Nvme0n1p3 \
+    --test-type=spdk_vhost_blk \
+    --fio-jobs=$WORKDIR/common/fio_jobs/default_integrity.job \
+    --qemu-src=/home/sys_sgsw/vhost/qemu -x
     ;;
-	-fs|--fs-integrity-scsi)
-	echo Running filesystem integrity suite...
-	./integrity/integrity_start.sh -i /home/sys_sgsw/vhost_vm_image.qcow2 -m scsi -f ntfs
-	;;
-	-fb|--fs-integrity-blk)
-	echo Running filesystem integrity suite...
-	./integrity/integrity_start.sh -i /home/sys_sgsw/vhost_vm_image.qcow2 -m blk -f ntfs
-	;;
-	-ils|--integrity-lvol-scsi)
-	echo Running lvol integrity suite...
-	./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
-	--ctrl-type=vhost_scsi
+    -fs|--fs-integrity-scsi)
+    echo Running filesystem integrity suite...
+    ./integrity/integrity_start.sh -i /home/sys_sgsw/vhost_vm_image.qcow2 -m scsi -f ntfs
     ;;
-	-ilb|--integrity-lvol-blk)
-	echo Running lvol integrity suite...
-	./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
-	--ctrl-type=vhost_blk
+    -fb|--fs-integrity-blk)
+    echo Running filesystem integrity suite...
+    ./integrity/integrity_start.sh -i /home/sys_sgsw/vhost_vm_image.qcow2 -m blk -f ntfs
+    ;;
+    -ils|--integrity-lvol-scsi)
+    echo Running lvol integrity suite...
+    ./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --ctrl-type=vhost_scsi
+    ;;
+    -ilb|--integrity-lvol-blk)
+    echo Running lvol integrity suite...
+    ./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --ctrl-type=vhost_blk
+    ;;
+    -ips|--integrity-pmem-scsi)
+    echo Running pmem integrity suite...
+    ./pmem/vhost_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --ctrl-type=vhost_scsi
+    ;;
+    -ipb|--integrity-pmem-blk)
+    echo Running pmem integrity suite...
+    ./pmem/vhost_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+    --ctrl-type=vhost_blk
     ;;
     -h|--help)
-	echo "-i |--integrity             for running an integrity test with vhost scsi"
-	echo "-fs|--fs-integrity-scsi     for running an integrity test with filesystem"
-	echo "-fb|--fs-integrity-blk      for running an integrity test with filesystem"
-	echo "-p |--performance           for running a performance test with vhost scsi"
-	echo "-ib|--integrity-blk         for running an integrity test with vhost blk"
-	echo "-pb|--performance-blk       for running a performance test with vhost blk"
-	echo "-ils|--integrity-lvol-scsi  for running an integrity test with vhost scsi and lvol backends"
-	echo "-ilb|--integrity-lvol-blk   for running an integrity test with vhost blk and lvol backends"
-	echo "-h |--help                  prints this message"
+    echo "-i |--integrity             for running an integrity test with vhost scsi"
+    echo "-fs|--fs-integrity-scsi     for running an integrity test with filesystem"
+    echo "-fb|--fs-integrity-blk      for running an integrity test with filesystem"
+    echo "-p |--performance           for running a performance test with vhost scsi"
+    echo "-ib|--integrity-blk         for running an integrity test with vhost blk"
+    echo "-pb|--performance-blk       for running a performance test with vhost blk"
+    echo "-ils|--integrity-lvol-scsi  for running an integrity test with vhost scsi and lvol backends"
+    echo "-ilb|--integrity-lvol-blk   for running an integrity test with vhost blk and lvol backends"
+    echo "-ips|--integrity-pmem-scsi  for running an integrity test with vhost scsi and pmem backends"
+    echo "-ipb|--integrity-pmem-blk   for running an integrity test with vhost blk and pmem backends"
+    echo "-h |--help                  prints this message"
     ;;
     *)
-	echo "unknown test type"
+    echo "unknown test type"
     ;;
 esac
 
