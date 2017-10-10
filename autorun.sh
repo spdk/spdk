23c3e1deb14900cbff7dffe70b934a9f40b757e7
Seth Howell <seth.howell@intel.com>
2017-10-10 14:48:52 -0700

    TEST: just testing pool after restart.
    Change-Id: I778ec788c0d9013adcd77b52138af57ae6ba01d2
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,7 @@ rootdir=$(readlink -f $(dirname $0))
 
 conf=~/autorun-spdk.conf
 
+#testing that the pool came up right.
 # Runs agent scripts
 $rootdir/autobuild.sh "$conf"
 sudo $rootdir/autotest.sh "$conf"
