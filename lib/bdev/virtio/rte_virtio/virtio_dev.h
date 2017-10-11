0ba8afb28e77d104adaf3eff2c52620b74a347a2
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-10-11 13:45:53 +0200

    bdev_virtio: fix memleak on init failure
    This patch also introduces
    vq->poller ctx. It will be later
    reused for per-virtqueue task pools
    to allow multiple threads using
    the same virtqueue.
    
    Since at the time of scanning there
    is no I/O traffic, this field is
    now being used to keep scan base
    pointer. It has to be freed if an
    initialization error occurs.
    
    Change-Id: Ia54ee6c8402d38218dc811b4994761105d17269a
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio_dev.h
+++ lib/bdev/virtio/rte_virtio/virtio_dev.h
@@ -103,6 +103,15 @@ struct virtqueue *virtio_dev_acquire_queue(struct virtio_dev *vdev, uint16_t sta
 					   uint32_t lcore);
 
 /**
+ * Look for a virtqueue acquired by the current lcore.  If multiple queues
+ * are found, the one with the lowest index will be returned.
+ *
+ * \param vdev vhost device
+ * \return virtqueue or NULL
+ */
+struct virtqueue *virtio_dev_get_acquired_queue(struct virtio_dev *vdev);
+
+/**
  * Release previously acquired queue.
  *
  * This function must be called from the thread that acquired the queue.
