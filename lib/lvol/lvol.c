56be9c0e0cff4e74cc38181a45f21fdd4b162afa
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-10-12 09:56:29 +0200

    lvol: Add spdk_lvs_destroy() API [2/3]
    This goes along with previous patch in series.
    Adding functionality to remove logical volume store from
    device.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I6338a35ed02838498a3cd9bb2dddd25803e65f79

--- lib/lvol/lvol.c
+++ lib/lvol/lvol.c
@@ -269,6 +269,48 @@ spdk_lvs_unload(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn,
 }
 
 static void
+_lvs_destroy_cb(void *cb_arg, int lvserrno)
+{
+	struct spdk_lvs_req *lvs_req = cb_arg;
+
+	SPDK_INFOLOG(SPDK_TRACE_LVOL, "Lvol store destroyed\n");
+	assert(lvs_req->cb_fn != NULL);
+	lvs_req->cb_fn(lvs_req->cb_arg, lvserrno);
+	free(lvs_req);
+}
+
+int
+spdk_lvs_destroy(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn, void *cb_arg)
+{
+	struct spdk_lvs_req *lvs_req;
+
+	if (lvs == NULL) {
+		SPDK_ERRLOG("Lvol store is NULL\n");
+		return -ENODEV;
+	}
+
+	if (!TAILQ_EMPTY(&lvs->lvols)) {
+		SPDK_ERRLOG("Lvols still present on lvol store\n");
+		return -EBUSY;
+	}
+
+	lvs_req = calloc(1, sizeof(*lvs_req));
+	if (!lvs_req) {
+		SPDK_ERRLOG("Cannot alloc memory for lvol store request pointer\n");
+		return -ENOMEM;
+	}
+
+	lvs_req->cb_fn = cb_fn;
+	lvs_req->cb_arg = cb_arg;
+
+	SPDK_INFOLOG(SPDK_TRACE_LVOL, "Destroying lvol store\n");
+	spdk_bs_destroy(lvs->blobstore, _lvs_destroy_cb, lvs_req);
+	free(lvs);
+
+	return 0;
+}
+
+static void
 _spdk_lvol_return_to_caller(void *cb_arg, int lvolerrno)
 {
 	struct spdk_lvol_with_handle_req *req = cb_arg;
