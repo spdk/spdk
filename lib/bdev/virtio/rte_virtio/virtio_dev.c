dc72227595ede1ca932549d2d59bff85dd698058
Daniel Verkamp <daniel.verkamp@intel.com>
2017-10-16 13:44:51 -0700

    rte_virtio: unlock vdev mutex on error path
    Change-Id: Ice2f157d79ba88a80d01460b67b78376aa0a5890
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/382721
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio_dev.c
+++ lib/bdev/virtio/rte_virtio/virtio_dev.c
@@ -593,6 +593,7 @@ virtio_dev_release_queue(struct virtio_dev *vdev, uint16_t index)
 	vq = vdev->vqs[index];
 	if (vq == NULL) {
 		SPDK_ERRLOG("virtqueue at index %"PRIu16" is not initialized.\n", index);
+		pthread_mutex_unlock(&vdev->mutex);
 		return;
 	}
 
