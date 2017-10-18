b6090ce323d3259a49cc27d8918ebc09c520043c
Paul Luse <paul.e.luse@intel.com>
2017-10-17 19:21:27 -0700

    blobcli: minor fixes
    - fixed issue with token parsing where only a space was desired
    and some other junk ended up in there that would cause issues
    with parsing filenames because of the .
    - made s few fatal error exits consistent
    
    Change-Id: Ib7fa07416881df42db88db7d94542963e492ca9b
    Signed-off-by: Paul Luse <paul.e.luse@intel.com>

--- examples/blob/cli/blobcli.c
+++ examples/blob/cli/blobcli.c
@@ -857,7 +857,7 @@ load_bs_cb(void *arg1, struct spdk_blob_store *bs, int bserrno)
 
 	default:
 		/* should never get here */
-		spdk_app_stop(-1);
+		exit(-1);
 		break;
 	}
 }
@@ -1296,7 +1296,7 @@ cli_shell(void *arg1, void *arg2)
 		cli_context->argv[cli_context->argc] = strdup(tok);
 		tok_len = strlen(tok);
 		cli_context->argc++;
-		tok = strtok(NULL, " ,.-");
+		tok = strtok(NULL, " ");
 	}
 
 	/* replace newline on last arg with null */
@@ -1379,7 +1379,7 @@ cli_start(void *arg1, void *arg2)
 		break;
 	default:
 		/* should never get here */
-		spdk_app_stop(-1);
+		exit(-1);
 		break;
 	}
 }
@@ -1436,7 +1436,7 @@ main(int argc, char **argv)
 		printf("To create a config file named 'blobcli.conf' for your NVMe device:\n");
 		printf("   <path to spdk>/scripts/gen_nvme.sh > blobcli.conf\n");
 		printf("and then re-run the cli tool.\n");
-		exit(1);
+		exit(-1);
 	}
 
 	/*
