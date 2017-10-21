8b599009aefa2dd2c724e466faa2f0cba2246440
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-10-21 03:23:54 +0200

    lvol: Add vbdev_lvs_destruct() and change unload behaviour [3/3]
    This patch adds new API to remove logical volume store
    from device it is on. It is only used from RPC, when
    user explicitly requests. It allows to use the device to
    use as any other bdev.
    
    vbdev_lvs_unload() is now only called from hotremove and
    during application shutdown. Which makes it possible to
    load it again during application start up.
    
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: If6452ecc3fff99237d1704ff7cd8de4d7133221d

--- lib/bdev/lvol/vbdev_lvol.c
+++ lib/bdev/lvol/vbdev_lvol.c
@@ -52,7 +52,7 @@ vbdev_lvs_hotremove_cb(void *ctx)
 	TAILQ_FOREACH_SAFE(lvs_bdev, &g_spdk_lvol_pairs, lvol_stores, tmp) {
 		if (lvs_bdev) {
 			if (lvs_bdev->bdev == bdev) {
-				vbdev_lvs_destruct(lvs_bdev->lvs, NULL, NULL);
+				vbdev_lvs_unload(lvs_bdev->lvs, NULL, NULL);
 			}
 		}
 	}
@@ -147,6 +147,60 @@ vbdev_lvs_create(struct spdk_bdev *base_bdev, uint32_t cluster_sz,
 }
 
 static void
+_vbdev_lvs_unload_cb(void *cb_arg, int lvserrno)
+{
+	struct spdk_lvs_req *req = cb_arg;
+
+	SPDK_INFOLOG(SPDK_TRACE_VBDEV_LVOL, "Lvol store bdev unloaded\n");
+
+	if (req->cb_fn != NULL)
+		req->cb_fn(req->cb_arg, lvserrno);
+	free(req);
+}
+
+void
+vbdev_lvs_unload(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn, void *cb_arg)
+{
+	struct spdk_lvs_req *req;
+	struct lvol_store_bdev *lvs_bdev;
+	struct spdk_lvol *lvol, *tmp;
+
+	lvs_bdev = vbdev_get_lvs_bdev_by_lvs(lvs);
+	TAILQ_REMOVE(&g_spdk_lvol_pairs, lvs_bdev, lvol_stores);
+
+	req = calloc(1, sizeof(*req));
+	if (!req) {
+		SPDK_ERRLOG("Cannot alloc memory for vbdev lvol store request pointer\n");
+		if (cb_fn != NULL)
+			cb_fn(cb_arg, -ENOMEM);
+		return;
+	}
+
+	req->cb_fn = cb_fn;
+	req->cb_arg = cb_arg;
+
+	if (TAILQ_EMPTY(&lvs->lvols)) {
+		spdk_lvs_unload(lvs, _vbdev_lvs_unload_cb, req);
+	} else {
+		lvs->destruct_req = calloc(1, sizeof(*lvs->destruct_req));
+		if (!lvs->destruct_req) {
+			SPDK_ERRLOG("Cannot alloc memory for vbdev lvol store request pointer\n");
+			_vbdev_lvs_unload_cb(req, -ENOMEM);
+			return;
+		}
+		lvs->destruct_req->cb_fn = _vbdev_lvs_unload_cb;
+		lvs->destruct_req->cb_arg = req;
+		lvs->destruct = false;
+		TAILQ_FOREACH_SAFE(lvol, &lvs->lvols, link, tmp) {
+			lvol->close_only = true;
+			spdk_vbdev_unregister(lvol->bdev);
+		}
+	}
+
+	free(lvs_bdev);
+}
+
+static void
 _vbdev_lvs_destruct_cb(void *cb_arg, int lvserrno)
 {
 	struct spdk_lvs_req *req = cb_arg;
@@ -159,10 +213,8 @@ _vbdev_lvs_destruct_cb(void *cb_arg, int lvserrno)
 }
 
 void
-vbdev_lvs_destruct(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn,
-		   void *cb_arg)
+vbdev_lvs_destruct(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn, void *cb_arg)
 {
-
 	struct spdk_lvs_req *req;
 	struct lvol_store_bdev *lvs_bdev;
 	struct spdk_lvol *lvol, *tmp;
@@ -189,7 +241,7 @@ vbdev_lvs_destruct(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn,
 	}
 
 	if (all_lvols_closed == true) {
-		spdk_lvs_unload(lvs, _vbdev_lvs_destruct_cb, req);
+		spdk_lvs_destroy(lvs, false, _vbdev_lvs_destruct_cb, req);
 	} else {
 		lvs->destruct_req = calloc(1, sizeof(*lvs->destruct_req));
 		if (!lvs->destruct_req) {
@@ -199,8 +251,9 @@ vbdev_lvs_destruct(struct spdk_lvol_store *lvs, spdk_lvs_op_complete cb_fn,
 		}
 		lvs->destruct_req->cb_fn = _vbdev_lvs_destruct_cb;
 		lvs->destruct_req->cb_arg = req;
+		lvs->destruct = true;
 		TAILQ_FOREACH_SAFE(lvol, &lvs->lvols, link, tmp) {
-			lvol->close_only = true;
+			lvol->close_only = false;
 			spdk_vbdev_unregister(lvol->bdev);
 		}
 	}
@@ -636,7 +689,7 @@ vbdev_lvs_fini(void)
 	struct lvol_store_bdev *lvs_bdev, *tmp;
 
 	TAILQ_FOREACH_SAFE(lvs_bdev, &g_spdk_lvol_pairs, lvol_stores, tmp) {
-		vbdev_lvs_destruct(lvs_bdev->lvs, NULL, NULL);
+		vbdev_lvs_unload(lvs_bdev->lvs, NULL, NULL);
 	}
 }
 
