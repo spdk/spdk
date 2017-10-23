d810d0f589cb4a7d9bc5f2b4a02d73edf1764168
Cunyin Chang <cunyin.chang@intel.com>
2017-10-23 13:45:26 +0800

    blobfs: Add conf parse for blobfs.
    This patch make the cache buffer shift of blobfs configurable. We
    can configure the cache buffer shift according workload, it will
    help to improve the performance.
    
    Change-Id: Ied1c2e5d6fd9eaa7aa0759c03c654fcf2e77aa23
    Signed-off-by: Cunyin Chang <cunyin.chang@intel.com>

--- test/lib/blobfs/blobfs_sync_ut/blobfs_sync_ut.c
+++ test/lib/blobfs/blobfs_sync_ut/blobfs_sync_ut.c
@@ -49,6 +49,20 @@ struct spdk_filesystem *g_fs;
 struct spdk_file *g_file;
 int g_fserrno;
 
+/* Return NULL/-1 to test hardcoded defaults. */
+struct spdk_conf_section *
+spdk_conf_find_section(struct spdk_conf *cp, const char *name)
+{
+	return NULL;
+}
+
+/* Return NULL/-1 to test hardcoded defaults. */
+int
+spdk_conf_section_get_intval(struct spdk_conf_section *sp, const char *key)
+{
+	return -1;
+}
+
 static void
 _fs_send_msg(spdk_thread_fn fn, void *ctx, void *thread_ctx)
 {
