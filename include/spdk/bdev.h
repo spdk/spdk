dda0065536b4660c0a8dafb5e1c27e6e5840ae6e
Yanbo Zhou <yanbo.zhou@intel.com>
2017-10-25 15:40:56 +0800

    include/bdev.h:add comments for public API
    Change-Id: I0f0e75228561105c1d717e8b879373231bf3cdcd
    Signed-off-by: Yanbo Zhou <yanbo.zhou@intel.com>

--- include/spdk/bdev.h
+++ include/spdk/bdev.h
@@ -123,12 +123,36 @@ typedef void (*spdk_bdev_poller_start_cb)(struct spdk_bdev_poller **ppoller,
 		uint64_t period_microseconds);
 typedef void (*spdk_bdev_poller_stop_cb)(struct spdk_bdev_poller **ppoller);
 
+/**
+ * Initialize a block device.
+ *
+ * \param cb_fn Callback foction for initializing block device.
+ * \param cb_arg Argument passed to founction cb_fn.
+ * \param start_poller_fn Callback function when poller starts.
+ * \param stop_poller_fn Callback function when poller stop.
+ */
 void spdk_bdev_initialize(spdk_bdev_init_cb cb_fn, void *cb_arg,
 			  spdk_bdev_poller_start_cb start_poller_fn,
 			  spdk_bdev_poller_stop_cb stop_poller_fn);
+
+/**
+ * Perform cleanup work which will remove the modules registered by the block device and free buffers to memory pool.
+ */
 void spdk_bdev_finish(void);
+
+/**
+ * Get the configuration options for modules registered by the block device form the file.
+ *
+ * \param fp The pointer of a file containing the configuration options.
+ */
 void spdk_bdev_config_text(FILE *fp);
 
+/**
+ * Get block device by block device name.
+ *
+ * \param bdev_name The name of block device.
+ * \return Block device associated with the name.
+ */
 struct spdk_bdev *spdk_bdev_get_by_name(const char *bdev_name);
 
 /**
@@ -173,8 +197,18 @@ void spdk_bdev_close(struct spdk_bdev_desc *desc);
  */
 struct spdk_bdev *spdk_bdev_desc_get_bdev(struct spdk_bdev_desc *desc);
 
+/**
+ * Check whether the block device supports the I/O type.
+ *
+ * \param bdev Block device to be checked.
+ * \param io_type The specific I/O type like read, write, flush, unmap.
+ * \return true if support, false otherwise.
+ */
 bool spdk_bdev_io_type_supported(struct spdk_bdev *bdev, enum spdk_bdev_io_type io_type);
 
+/**
+ * Output driver-specific configuration to a JSON stream.
+ */
 int spdk_bdev_dump_config_json(struct spdk_bdev *bdev, struct spdk_json_write_ctx *w);
 
 /**
