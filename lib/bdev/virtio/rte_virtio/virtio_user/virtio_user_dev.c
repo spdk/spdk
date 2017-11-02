fbbacff942e9b5dcd85993c683a4d96747eda99d
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-11-02 19:05:48 +0100

    rte_virtio: properly free vdev on init failure
    virtio_dev_alloc allocates e.g. a mutex
    that has to be freed via virtio_dev_free.
    
    Change-Id: I07f3dd91e6f109232c9993ec89507a58f6079198
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio_user/virtio_user_dev.c
+++ lib/bdev/virtio/rte_virtio/virtio_user/virtio_user_dev.c
@@ -197,7 +197,8 @@ virtio_user_dev_init(const char *name, const char *path, uint16_t requested_queu
 	vdev = virtio_dev_alloc(&virtio_user_ops, dev);
 	if (vdev == NULL) {
 		SPDK_ERRLOG("Failed to init device: %s\n", path);
-		goto err;
+		free(dev);
+		return NULL;
 	}
 
 	vdev->is_hw = 0;
@@ -240,7 +241,7 @@ virtio_user_dev_init(const char *name, const char *path, uint16_t requested_queu
 
 err:
 	free(vdev->name);
-	free(dev);
+	virtio_dev_free(vdev);
 	return NULL;
 }
 
