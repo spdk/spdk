2290494eb06d49f7e85332cae28fb31f16e544b8
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-11-02 15:20:25 +0100

    rte_virtio: move vtpci code to virtio_dev.c/.h
    The vtpci layer is about to be removed.
    This is only 1:1 code move, all functions
    will be renamed/refactored in future patches.
    
    Change-Id: I63c1b30fdaf6eff2e5d8bf60e62c87c2cb9f2b15
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio_pci.h
+++ lib/bdev/virtio/rte_virtio/virtio_pci.h
@@ -36,52 +36,9 @@
 
 #include "spdk/stdinc.h"
 
-#include <linux/virtio_config.h>
-#include <linux/virtio_pci.h>
-
-#include "spdk/env.h"
-#include "spdk/json.h"
 #include "virtio_dev.h"
 
 struct virtqueue;
-
-/* Extra status define for readability */
-#define VIRTIO_CONFIG_S_RESET 0
-
-/* Number of non-request queues - eventq and controlq */
-#define SPDK_VIRTIO_SCSI_QUEUE_NUM_FIXED 2
-
-struct virtio_pci_ops {
-	void (*read_dev_cfg)(struct virtio_dev *hw, size_t offset,
-			     void *dst, int len);
-	void (*write_dev_cfg)(struct virtio_dev *hw, size_t offset,
-			      const void *src, int len);
-	uint8_t (*get_status)(struct virtio_dev *hw);
-	void (*set_status)(struct virtio_dev *hw, uint8_t status);
-
-	/**
-	 * Get device features. The features might be already
-	 * negotiated with driver (guest) features.
-	 */
-	uint64_t (*get_features)(struct virtio_dev *vdev);
-
-	/**
-	 * Negotiate and set device features.
-	 * The negotiation can fail with return code -1.
-	 * This function should also set vdev->negotiated_features field.
-	 */
-	int (*set_features)(struct virtio_dev *vdev, uint64_t features);
-
-	/** Deinit and free virtio device */
-	void (*free_vdev)(struct virtio_dev *vdev);
-
-	uint16_t (*get_queue_num)(struct virtio_dev *hw, uint16_t queue_id);
-	int (*setup_queue)(struct virtio_dev *hw, struct virtqueue *vq);
-	void (*del_queue)(struct virtio_dev *hw, struct virtqueue *vq);
-	void (*notify_queue)(struct virtio_dev *hw, struct virtqueue *vq);
-
-	void (*dump_json_config)(struct virtio_dev *hw, struct spdk_json_write_ctx *w);
-};
 
 struct virtio_hw {
 	uint8_t	    use_msix;
@@ -102,42 +59,9 @@ struct virtio_hw {
 	struct virtio_scsi_config *dev_cfg;
 };
 
-struct virtio_driver {
-	TAILQ_HEAD(, virtio_dev) init_ctrlrs;
-	TAILQ_HEAD(, virtio_dev) attached_ctrlrs;
-
-	/* Increment-only virtio_dev counter */
-	unsigned ctrlr_counter;
-};
-
-extern struct virtio_driver g_virtio_driver;
-
-static inline int
-vtpci_with_feature(struct virtio_dev *dev, uint64_t bit)
-{
-	return (dev->negotiated_features & (1ULL << bit)) != 0;
-}
-
 /**
  * Init all compatible Virtio PCI devices.
  */
 int vtpci_enumerate_pci(void);
-
-void vtpci_reset(struct virtio_dev *);
-
-uint8_t vtpci_get_status(struct virtio_dev *);
-void vtpci_set_status(struct virtio_dev *, uint8_t);
-
-uint64_t vtpci_negotiate_features(struct virtio_dev *, uint64_t);
-
-void vtpci_write_dev_config(struct virtio_dev *, size_t, const void *, int);
-
-void vtpci_read_dev_config(struct virtio_dev *, size_t, void *, int);
-
-const struct virtio_pci_ops *vtpci_ops(struct virtio_dev *dev);
-
-void vtpci_dump_json_config(struct virtio_dev *hw, struct spdk_json_write_ctx *w);
-
-extern const struct virtio_pci_ops virtio_user_ops;
 
 #endif /* _VIRTIO_PCI_H_ */
