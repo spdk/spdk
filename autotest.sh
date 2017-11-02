576470c3351e8c1407defab3776064b6b097c358
Karol Latecki <karol.latecki@intel.com>
2017-11-02 15:07:51 +0100

    test/lvol: add friendly name specific tests
    Adding positive and negative lvol friendly names specific tests.
    Test plan updated.
    Also fix some test naming discrepancies between plan and
    script while at it
    
    Change-Id: Ifb1a62d6a231f4f4ebfb5bb6178d941fc2b9770d
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -134,7 +134,7 @@ fi
 
 if [ $SPDK_TEST_LVOL -eq 1 ]; then
 	timing_enter lvol
-	run_test ./test/lvol/lvol.sh --test-cases=1,2,3,5,6,7,10,11,12,13,16,17,21,22,23,24
+	run_test ./test/lvol/lvol.sh --test-cases=1,2,3,4,5,7,8,9,10,13,14,15,16,17,18,21,22,25,26,27,28,29
 	timing_exit lvol
 fi
 
