967a5b86c041dcfe12f4dafebdf25948a22edad0
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-11-06 12:00:42 +0100

    fio_plugin: poll all remaining events on thread shutdown
    There were unprocessed events
    in the event ring at the time
    of destroying it's thread.
    
    Polling until *done* flag is set
    is not sufficient. Since a single
    fio_getevents call will poll
    only a single message, any
    message positioned after the
    callback that sets the *done*
    flag would not be processed.
    Until now
    
    Change-Id: I8fee384cb980373672bed4bc498f75774aa64a9e
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- examples/bdev/fio_plugin/fio_plugin.c
+++ examples/bdev/fio_plugin/fio_plugin.c
@@ -99,6 +99,7 @@ static unsigned g_spdk_fio_thread_count = 0;
 
 static int spdk_fio_init(struct thread_data *td);
 static void spdk_fio_cleanup(struct thread_data *td);
+static size_t spdk_fio_poll_msg(struct thread_data *td);
 static int spdk_fio_getevents(struct thread_data *td, unsigned int min,
 			      unsigned int max, const struct timespec *t);
 
@@ -282,6 +283,7 @@ spdk_fio_init_env(struct thread_data *td)
 	while (!done) {
 		spdk_fio_getevents(td, 0, 128, NULL);
 	}
+	while (spdk_fio_poll_msg(td));
 
 	/* Destroy the temporary SPDK thread */
 	spdk_fio_cleanup(td);
@@ -403,6 +405,9 @@ spdk_fio_cleanup(struct thread_data *td)
 		spdk_bdev_close(target->desc);
 		free(target);
 	}
+
+	/* Poll e.g. the asynchronous put_io_channel event */
+	while (spdk_fio_poll_msg(td));
 
 	spdk_free_thread();
 	spdk_ring_free(fio_thread->ring);
@@ -538,6 +543,22 @@ spdk_fio_event(struct thread_data *td, int event)
 	return fio_thread->iocq[event];
 }
 
+static size_t
+spdk_fio_poll_msg(struct thread_data *td)
+{
+	struct spdk_fio_thread *fio_thread = td->io_ops_data;
+	struct spdk_fio_msg *msg;
+	size_t count;
+
+	count = spdk_ring_dequeue(fio_thread->ring, (void **)&msg, 1);
+	if (count > 0) {
+		msg->cb_fn(msg->cb_arg);
+		free(msg);
+	}
+
+	return count;
+}
+
 static int
 spdk_fio_getevents(struct thread_data *td, unsigned int min,
 		   unsigned int max, const struct timespec *t)
@@ -545,9 +566,7 @@ spdk_fio_getevents(struct thread_data *td, unsigned int min,
 	struct spdk_fio_thread *fio_thread = td->io_ops_data;
 	struct timespec t0, t1;
 	uint64_t timeout = 0;
-	struct spdk_fio_msg *msg;
 	struct spdk_fio_poller *p, *tmp;
-	size_t count;
 
 	if (t) {
 		timeout = t->tv_sec * 1000000000L + t->tv_nsec;
@@ -558,11 +577,7 @@ spdk_fio_getevents(struct thread_data *td, unsigned int min,
 
 	for (;;) {
 		/* Process new events */
-		count = spdk_ring_dequeue(fio_thread->ring, (void **)&msg, 1);
-		if (count > 0) {
-			msg->cb_fn(msg->cb_arg);
-			free(msg);
-		}
+		spdk_fio_poll_msg(td);
 
 		/* Call all pollers */
 		TAILQ_FOREACH_SAFE(p, &fio_thread->pollers, link, tmp) {
@@ -682,13 +697,15 @@ spdk_fio_finish_env(void)
 	while (!done) {
 		spdk_fio_getevents(td, 0, 128, NULL);
 	}
-	done = false;
+	while (spdk_fio_poll_msg(td));
 
+	done = false;
 	spdk_copy_engine_finish(spdk_fio_module_finish_done, &done);
 
 	while (!done) {
 		spdk_fio_getevents(td, 0, 128, NULL);
 	}
+	while (spdk_fio_poll_msg(td));
 
 	/* Destroy the temporary SPDK thread */
 	spdk_fio_cleanup(td);
