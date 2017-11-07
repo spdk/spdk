30afe085c192164227f14e6d662062e0bd0002a9
Ben Walker <benjamin.walker@intel.com>
2017-11-07 15:05:19 -0700

    bdev: Add a zero copy I/O path
    Add a ZCOPY operation to obtain buffers that represent
    data regions on the backing block device.
    
    Change-Id: Ie941c16ee051d0009e3888b52b8f41773bba47b3

--- lib/bdev/bdev.c
+++ lib/bdev/bdev.c
@@ -377,33 +377,40 @@ spdk_bdev_get_by_name(const char *bdev_name)
 	return NULL;
 }
 
-size_t
+int
 spdk_bdev_io_set_buf(struct spdk_bdev_io *bdev_io, void *buf, size_t len)
 {
-	struct iovec **iovs;
-	int *iovcnt;
+	struct iovec *iovs;
 	void *aligned_buf;
 
-	iovs = &bdev_io->u.bdev.iovs;
-	iovcnt = &bdev_io->u.bdev.iovcnt;
+	if (bdev_io->type == SPDK_BDEV_IO_TYPE_ZCOPY) {
+		/* Always use the internal iov array */
+		bdev_io->u.zcopy.iovs = &bdev_io->iov;
+		bdev_io->u.zcopy.iovcnt = 1;
 
-	if (*iovs == NULL || *iovcnt == 0) {
-		*iovs = &bdev_io->iov;
-		*iovcnt = 1;
+		iovs = bdev_io->u.zcopy.iovs;
+	} else {
+		/* The user must have provided some iovs */
+		iovs = bdev_io->u.bdev.iovs;
 	}
+
+	assert(iovs != NULL);
 
 	if (buf != NULL) {
 		aligned_buf = (void *)(((uintptr_t)buf + 511) & ~511UL);
-		len = len - ((uintptr_t)aligned_buf - (uintptr_t)buf);
-	} else {
-		aligned_buf = NULL;
-		assert(len == 0);
+		assert(aligned_buf == buf);
+		if (aligned_buf != buf) {
+			SPDK_ERRLOG("Buffer not properly aligned\n");
+			aligned_buf = NULL;
+			len = 0;
+			return -EINVAL;
+		}
 	}
 
-	(*iovs)[0].iov_base = aligned_buf;
-	(*iovs)[0].iov_len = len;
+	iovs[0].iov_base = buf;
+	iovs[0].iov_len = len;
 
-	return len;
+	return 0;
 }
 
 static void
@@ -436,11 +443,8 @@ spdk_bdev_io_put_buf(struct spdk_bdev_io *bdev_io)
 	} else {
 		tmp = STAILQ_FIRST(stailq);
 		STAILQ_REMOVE_HEAD(stailq, internal.buf_link);
-		len = spdk_bdev_io_set_buf(tmp, buf, len);
-		if (len < tmp->internal.buf_len) {
-			SPDK_ERRLOG("Unable to use buffer due to alignment\n");
+		if (spdk_bdev_io_set_buf(tmp, buf, len)) {
 			spdk_mempool_put(pool, buf);
-			spdk_bdev_io_set_buf(tmp, NULL, 0);
 			return;
 		}
 		tmp->internal.buf = buf;
@@ -454,6 +458,7 @@ spdk_bdev_io_get_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb, u
 	struct spdk_mempool *pool;
 	bdev_io_stailq_t *stailq;
 	void *buf = NULL;
+	void *aligned_buf = NULL;
 	struct spdk_bdev_mgmt_channel *mgmt_ch;
 	size_t buf_len;
 
@@ -486,13 +491,10 @@ spdk_bdev_io_get_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb, u
 	if (!buf) {
 		STAILQ_INSERT_TAIL(stailq, bdev_io, internal.buf_link);
 	} else {
-		size_t aligned_len;
+		aligned_buf = (void *)(((uintptr_t)buf + 511) & ~511UL);
 
-		aligned_len = spdk_bdev_io_set_buf(bdev_io, buf, buf_len);
-		if (aligned_len < len) {
-			SPDK_ERRLOG("Unable to use buffer after alignment calculations.\n");
+		if (spdk_bdev_io_set_buf(bdev_io, aligned_buf, buf_len)) {
 			spdk_mempool_put(pool, buf);
-			spdk_bdev_io_set_buf(bdev_io, NULL, 0);
 			STAILQ_INSERT_TAIL(stailq, bdev_io, internal.buf_link);
 			return;
 		}
@@ -1889,6 +1891,59 @@ spdk_bdev_writev_blocks(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 }
 
 int
+spdk_bdev_zcopy_start(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
+		      uint64_t offset_blocks, uint64_t num_blocks,
+		      bool populate,
+		      spdk_bdev_io_completion_cb cb, void *cb_arg)
+{
+	struct spdk_bdev *bdev = desc->bdev;
+	struct spdk_bdev_io *bdev_io;
+	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
+
+	if (!desc->write) {
+		return -EBADF;
+	}
+
+	if (!spdk_bdev_io_valid_blocks(bdev, offset_blocks, num_blocks)) {
+		return -EINVAL;
+	}
+
+	bdev_io = spdk_bdev_get_io(channel);
+	if (!bdev_io) {
+		return -ENOMEM;
+	}
+
+	bdev_io->internal.ch = channel;
+	bdev_io->type = SPDK_BDEV_IO_TYPE_ZCOPY;
+	bdev_io->u.zcopy.num_blocks = num_blocks;
+	bdev_io->u.zcopy.offset_blocks = offset_blocks;
+	bdev_io->u.zcopy.populate = populate;
+	bdev_io->u.zcopy.start = true;
+	spdk_bdev_io_init(bdev_io, bdev, cb_arg, cb);
+
+	spdk_bdev_io_submit(bdev_io);
+	return 0;
+}
+
+int
+spdk_bdev_zcopy_end(struct spdk_bdev_io *bdev_io, bool commit,
+		    spdk_bdev_io_completion_cb cb, void *cb_arg)
+{
+	struct spdk_bdev *bdev = bdev_io->bdev;
+
+	if (bdev_io->type != SPDK_BDEV_IO_TYPE_ZCOPY) {
+		return -EINVAL;
+	}
+
+	bdev_io->u.zcopy.commit = commit;
+	bdev_io->u.zcopy.start = false;
+	spdk_bdev_io_init(bdev_io, bdev, cb_arg, cb);
+
+	spdk_bdev_io_submit(bdev_io);
+	return 0;
+}
+
+int
 spdk_bdev_write_zeroes(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		       uint64_t offset, uint64_t len,
 		       spdk_bdev_io_completion_cb cb, void *cb_arg)
@@ -3144,12 +3199,13 @@ spdk_bdev_io_get_iovec(struct spdk_bdev_io *bdev_io, struct iovec **iovp, int *i
 
 	switch (bdev_io->type) {
 	case SPDK_BDEV_IO_TYPE_READ:
-		iovs = bdev_io->u.bdev.iovs;
-		iovcnt = bdev_io->u.bdev.iovcnt;
-		break;
 	case SPDK_BDEV_IO_TYPE_WRITE:
 		iovs = bdev_io->u.bdev.iovs;
 		iovcnt = bdev_io->u.bdev.iovcnt;
+		break;
+	case SPDK_BDEV_IO_TYPE_ZCOPY:
+		iovs = bdev_io->u.zcopy.iovs;
+		iovcnt = bdev_io->u.zcopy.iovcnt;
 		break;
 	default:
 		iovs = NULL;
