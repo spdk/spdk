57332684b56c941ca7632dec7e9ca9b011d90430
Hailiang Wang <hailiangx.e.wang@intel.com>
2017-11-08 18:06:22 +0800

    autotest: blobstore_test (porting from nightly to autotest)
    This patch is used to test blobstore automatically
    which based on examples/blob/cli/blobcli.
    
    Change-Id: I0309cf01d1561f309574a37aeaff8bfbc395f65c
    Signed-off-by: Hailiang Wang <hailiangx.e.wang@intel.com>

---
+++ test/blobstore/blobstore.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == "FreeBSD"* ]]; then
+    echo "Blobstore can not be tested in FreeBSD temporarily."
+    exit 0
+fi
+
+set -xe
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+source $rootdir/scripts/autotest_common.sh
+
+timing_enter blobstore
+
+set -e
+
+# Nvme0 target configuration
+$rootdir/scripts/gen_nvme.sh > $testdir/blobcli.conf
+
+$rootdir/examples/blob/cli/blobcli -c $testdir/blobcli.conf -b Nvme0n1 -T $testdir/test.bs
+
+rm -rf $testdir/blobcli.conf
+rm -rf $testdir/M.blob
+
+timing_exit blobstore
