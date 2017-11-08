a7297b171a26ce9629da41bc2bc37163c2581193
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2017-11-08 20:14:45 +0100

    test/vhost: Add vhost initiator tests for guest
    Change-Id: Id5ba51f76687150bfcc6164da30302d7fb7be911
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/vhost/initiator/guest_fio.sh
+++ test/vhost/initiator/guest_fio.sh
@@ -1,12 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
 fio_bdev_conf="$1"
 /root/spdk/scripts/setup.sh
 . /root/spdk/scripts/autotest_common.sh
+fio_job_conf="--section=fio_job_guest_randwrite"
 bdevs=$(discover_bdevs /root/spdk /root/bdev.conf | jq -r '.[] | select(.claimed == false)')
 virtio_bdevs=""
 for b in $(echo $bdevs | jq -r '.name') ; do
 	virtio_bdevs+="$b:"
 done
-sed -i "s|filename=|filename=$virtio_bdevs|g" /root/bdev.fio
-cat /root/bdev.fio
+
+if [ "$2" == "--nigthly" ]; then
+	fio_job_conf="--section=fio_job_guest_randwrite --section=fio_job_guest_randrw --section=fio_job_guest_write --section=fio_job_guest_rw"
+fi
+
+sed -i 's/\bfilename\b/&='$virtio_bdevs'/' /root/bdev.fio
 LD_PRELOAD=/root/spdk/examples/bdev/fio_plugin/fio_plugin /root/fio_src/fio \
-	--ioengine=spdk_bdev --runtime=10 /root/bdev.fio $fio_bdev_conf --spdk_mem=1024
+	--ioengine=spdk_bdev --runtime=10 /root/bdev.fio $fio_job_conf $fio_bdev_conf --spdk_mem=1024
