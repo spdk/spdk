cdd99c573197680f54489cb43287bcbfce3510e6
Daniel Verkamp <daniel.verkamp@intel.com>
2017-11-13 14:19:20 -0700

    nvme: normalize PCI addresses before comparing
    Use the env abstraction PCI functions to compare PCI addresses so that
    details like whether or not domain is specified or whether 0-padding is
    present don't affect the comparison.
    
    For example, 0000:01:00.0 should compare equal to 01:00.0.
    
    Change-Id: I9f3aaeb5f8fdbf3e246e31a41b4c09151288015e
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/unit/lib/nvme/nvme.c/nvme_ut.c
+++ test/unit/lib/nvme/nvme.c/nvme_ut.c
@@ -771,6 +771,25 @@ test_trid_parse_and_compare(void)
 	memset_trid(&trid1, &trid2);
 	ret = spdk_nvme_transport_id_compare(&trid1, &trid2);
 	CU_ASSERT(ret == 0);
+
+	/* Compare PCI addresses via spdk_pci_addr_compare (rather than as strings) */
+	memset_trid(&trid1, &trid2);
+	CU_ASSERT(spdk_nvme_transport_id_parse(&trid1, "trtype:PCIe traddr:0000:04:00.0") == 0);
+	CU_ASSERT(spdk_nvme_transport_id_parse(&trid2, "trtype:PCIe traddr:04:00.0") == 0);
+	MOCK_SET(spdk_pci_addr_compare, int, 0);
+	CU_ASSERT(spdk_nvme_transport_id_compare(&trid1, &trid2) == 0);
+
+	memset_trid(&trid1, &trid2);
+	CU_ASSERT(spdk_nvme_transport_id_parse(&trid1, "trtype:PCIe traddr:0000:05:00.0") == 0);
+	CU_ASSERT(spdk_nvme_transport_id_parse(&trid2, "trtype:PCIe traddr:04:00.0") == 0);
+	MOCK_SET(spdk_pci_addr_compare, int, 1);
+	CU_ASSERT(spdk_nvme_transport_id_compare(&trid1, &trid2) == 1);
+
+	memset_trid(&trid1, &trid2);
+	CU_ASSERT(spdk_nvme_transport_id_parse(&trid1, "trtype:PCIe traddr:0000:04:00.0") == 0);
+	CU_ASSERT(spdk_nvme_transport_id_parse(&trid2, "trtype:PCIe traddr:05:00.0") == 0);
+	MOCK_SET(spdk_pci_addr_compare, int, -1);
+	CU_ASSERT(spdk_nvme_transport_id_compare(&trid1, &trid2) == -1);
 }
 
 static void
