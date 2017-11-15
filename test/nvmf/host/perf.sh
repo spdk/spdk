29c2a7e11bbafa5b8ffd928c4c8a17c89f0d8b60
wenzhong wu <wenzhongx.wu@intel.com>
2017-11-16 07:14:14 +0800

    autotest: Add perf host with lvol backend and test different qd/io_size test in nightly
    To cover perf host with nvmf lvol_bdev backend and test differnet qd
    and io_sizes in nightly(Proting from DTS).
    
    Change-Id: I857fb66488d644ae0cc092dc1fff3370be09ba5e
    Signed-off-by: wenzhong wu <wenzhongx.wu@intel.com>

--- test/nvmf/host/perf.sh
+++ test/nvmf/host/perf.sh
@@ -23,7 +23,7 @@ timing_enter perf
 timing_enter start_nvmf_tgt
 
 cp $testdir/../nvmf.conf $testdir/nvmf.conf
-$rootdir/scripts/gen_nvme.sh >> $testdir/nvmf.conf
+#$rootdir/scripts/gen_nvme.sh >> $testdir/nvmf.conf
 
 local_nvme_trid=$(grep TransportID $testdir/nvmf.conf | head -n1 | awk -F"\"" '{print $2}')
 
@@ -40,8 +40,22 @@ bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SI
 if [ -n "$local_nvme_trid" ]; then
 	bdevs="$bdevs Nvme0n1"
 fi
+bdfs=$(lspci -mm -n | grep 0108 | tr -d '"' | awk -F " " '{print "0000:"$1}')
+bdfs_nums=$(lspci -mm -n | grep 0108 | tr -d '"' | awk -F " " '{print "0000:"$1}'| wc -l)
+
+if [ -n "$bdfs" ] then;
+$rpc_py construct_nvme_bdev -b Nvme0 -t PCIe -a $(echo $bdfs | awk '{ print $1 }')
+fi
 
 $rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" '' -a -s SPDK00000000000001 -n "$bdevs"
+
+#Create another nvme devices with nvmf lvol_bdev backend
+if [ -n "$bdfs" ] && [ $bdfs_nums -ge 2 ]; then
+        $rpc_py construct_nvme_bdev -b Nvme1 -t PCIe -a $(echo $bdfs | awk '{ print $2 }')
+        ls_guid=$($rpc_py construct_lvol_store Nvme1n1 lvs_0)
+        $rpc_py construct_lvol_bdev -u $ls_guid  lbd_nest 16000
+        $rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode2 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" "" -a -s SPDK00000000000002 -n "lvs_0/lbd_nest"
+fi
 
 # Test multi-process access to local NVMe device
 if [ -n "$local_nvme_trid" ]; then
@@ -49,8 +63,27 @@ if [ -n "$local_nvme_trid" ]; then
 fi
 
 $rootdir/examples/nvme/perf/perf -q 32 -s 4096 -w randrw -M 50 -t 1 -r "trtype:RDMA adrfam:IPv4 traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420"
+
+#Run perf host stress with differnet io_size and qd_depth in nightly
+if [ $RUN_NIGHTLY -eq 1 ]; then
+	qd_depth=("1" "64" "128")
+	io_size=("512" "4096" "131072")
+	for qd in ${qd_depth[@]}; do
+        	for s in ${io_size[@]}; do
+			$rootdir/examples/nvme/perf/perf -q $qd -s $s -w randrw -M 50 -t 60 -r "trtype:RDMA adrfam:IPv4 traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420"
+        	done
+	done
+fi
+
 sync
 $rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk:cnode1
+
+if [ -n "$bdfs" ] && [ $bdfs_nums -ge 2 ]; then
+        $rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk:cnode2
+        $rpc_py delete_bdev  "lvs_0/lbd_nest"
+        $rpc_py destroy_lvol_store -l lvs_0
+        $rpc_py delete_bdev "Nvme1n1"
+fi
 
 trap - SIGINT SIGTERM EXIT
 
