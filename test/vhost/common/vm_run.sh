f6d28a9d2311ce6a3b2d2ebd42d97a26f1ecf8f7
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-11-16 18:56:43 +0100

    test/vhost: rework commons to be usefull not only for testpool
    Currently using scripts form test/vhot/common for something other than
    autotests is not easy task. Lets change this to be more user friendly.
    As we are here cleanup some legacy code and fix things found during
    rework.
    
    test/vhost: fix negative tests after rework
    
    Change-Id: I148cafc257cb54bb359957f32938c6dbe7fa0559
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    
    tmp
    
    Change-Id: Ie1f9a60e54649fbf40776f8ae89a1388482dcf32
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- test/vhost/common/vm_run.sh
+++ test/vhost/common/vm_run.sh
@@ -1,49 +1,7 @@
 #!/usr/bin/env bash
 
-BASE_DIR=$(readlink -f $(dirname $0))
-[[ -z "$COMMON_DIR" ]] && COMMON_DIR="$(cd $BASE_DIR/../common && pwd)"
-[[ -z "$TEST_DIR" ]] && TEST_DIR="$(cd $BASE_DIR/../../../../ && pwd)"
+. $(readlink -f $(dirname $0))/common.sh || exit 1
 
-function usage()
-{
-	[[ ! -z $2 ]] && ( echo "$2"; echo ""; )
-	echo "Shortcut script for enabling VMs"
-	echo "Usage: $(basename $1) [OPTIONS] VM..."
-	echo
-	echo "-h, --help                print help and exit"
-	echo "    --work-dir=WORK_DIR   Where to find build file. Must exist. [default: ./..]"
-	echo "-a                        Run all VMs in WORK_DIR"
-	echo "-x                        set -x for script debug"
-	exit 0
-}
-run_all=false
-while getopts 'xah-:' optchar; do
-	case "$optchar" in
-		-)
-		case "$OPTARG" in
-			help) usage $0 ;;
-			work-dir=*) TEST_DIR="${OPTARG#*=}" ;;
-			*) usage $0 "Invalid argument '$OPTARG'" ;;
-		esac
-		;;
-	h) usage $0 ;;
-	a) run_all=true ;;
-	x) set -x ;;
-	*) usage $0 "Invalid argument '$OPTARG'"
-	esac
-done
+trap "trap - ERR; print_backtrace; error 'Failed to run VM'; exit 1" ERR
 
-. $COMMON_DIR/common.sh
-
-if [[ $EUID -ne 0 ]]; then
-	echo "Go away user come back as root"
-	exit 1
-fi
-
-if $run_all; then
-	vm_run -a
-else
-	shift $((OPTIND-1))
-	echo "INFO: running VMs: $@"
-	vm_run "$@"
-fi
+vm_run "${VHOST_TEST_ARGS[@]}"
