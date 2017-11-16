07a5d2467a7562a8fe21544f7d4aad7ca7dc0ea0
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2017-11-16 18:56:43 +0100

    test/vhost: rework commons to be usefull not only for testpool
    Currently using scripts form test/vhot/common for something other than
    autotests is not easy task. Lets change this to be more user friendly.
    As we are here cleanup some legacy code and fix things found during
    rework.
    
    test/vhost: fix negative tests after rework
    
    Change-Id: I148cafc257cb54bb359957f32938c6dbe7fa0559
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    
    tmp
    
    Change-Id: Ie1f9a60e54649fbf40776f8ae89a1388482dcf32
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- test/vhost/common/vm_ssh.sh
+++ test/vhost/common/vm_ssh.sh
@@ -1,59 +1,8 @@
 #!/usr/bin/env bash
 
-BASE_DIR=$(readlink -f $(dirname $0))
-[[ -z "$COMMON_DIR" ]] && COMMON_DIR="$(cd $BASE_DIR/../common && pwd)"
-[[ -z "$TEST_DIR" ]] && TEST_DIR="$(cd $BASE_DIR/../../../../ && pwd)"
+. $(readlink -f $(dirname $0))/common.sh || exit 1
 
-function usage()
-{
-	[[ ! -z $2 ]] && ( echo "$2"; echo ""; )
-	echo "Shortcut script for connecting to or executing command on selected VM"
-	echo "Usage: $(basename $1) [OPTIONS] VM_NUMBER"
-	echo
-	echo "-h, --help                print help and exit"
-	echo "    --work-dir=WORK_DIR   Where to find build file. Must exist. [default: $TEST_DIR]"
-	echo "-w                        Don't wait for vm to boot"
-	echo "-x                        set -x for script debug"
-	exit 0
-}
+# We don't know what command will do so don't trap errors here
+trap - ERR
 
-boot_wait=true
-while getopts 'xwh-:' optchar; do
-	case "$optchar" in
-		-)
-		case "$OPTARG" in
-			help)	usage $0 ;;
-			work-dir=*) TEST_DIR="${OPTARG#*=}" ;;
-			*) usage $0 "Invalid argument '$OPTARG'" ;;
-		esac ;;
-	h) usage $0 ;;
-	w) boot_wait=false ;;
-	x) set -x ;;
-	*) usage $0 "Invalid argument '$OPTARG'" ;;
-	esac
-done
-
-. $COMMON_DIR/common.sh
-
-shift $((OPTIND-1))
-vm_num="$1"
-shift
-
-
-if ! vm_num_is_valid $vm_num; then
-	usage $0 "Invalid VM num $vm_num"
-	exit 1
-fi
-
-if $boot_wait; then
-	while ! vm_os_booted $vm_num; do
-		if ! vm_is_running $vm_num; then
-			echo "ERROR: VM$vm_num is not running"
-				exit 1
-		fi
-		echo "INFO: waiting for VM$vm_num to boot"
-		sleep 1
-	done
-fi
-
-vm_ssh $vm_num "$@"
+vm_ssh "${VHOST_TEST_ARGS[@]}"
