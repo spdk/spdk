spdk/spdk

commit 4f163443564063d5f500af8bdfdc4f645b3bfa10

make: Update build artifact timestamps for clang support
Change-Id: Ibc165386b074d82969610467f4da73c73d8f4ea0
Signed-off-by: Ed rodriguez <edwinr@netapp.com>
Reviewed-on: https://review.gerrithub.io/386164
Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>

mk/spdk.common.mk
@@ -166,12 +166,12 @@ DEPFLAGS = -MMD -MP -MF $*.d.tmp
 COMPILE_C=\
 	$(Q)echo "  CC $S/$@"; \
 	$(CC) -o $@ $(DEPFLAGS) $(CFLAGS) -c $< && \
-	mv -f $*.d.tmp $*.d
+	mv -f $*.d.tmp $*.d;touch -c $@
 
 COMPILE_CXX=\
 	$(Q)echo "  CXX $S/$@"; \
 	$(CXX) -o $@ $(DEPFLAGS) $(CXXFLAGS) -c $< && \
-	mv -f $*.d.tmp $*.d
+	mv -f $*.d.tmp $*.d;touch -c $@
 
 # Link $(OBJS) and $(LIBS) into $@ (app)
 LINK_C=\

commit 12ae5f54e17dcc6a90993810f35d2c71d1748a42

build: only touch output if build is successful
Running the 'touch' command unconditionally causes the compile step to
return 0 even if the compilation failed.  Instead, only run it if the
compiler succeeded.

Fixes: 4f1634435640 ("make: Update build artifact timestamps for clang
support")

Change-Id: I46907851fde8bb9e3f73549276e489b6c30ed1fd
Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

mk/spdk.common.mk
@@ -166,12 +166,12 @@ DEPFLAGS = -MMD -MP -MF $*.d.tmp
 COMPILE_C=\
 	$(Q)echo "  CC $S/$@"; \
 	$(CC) -o $@ $(DEPFLAGS) $(CFLAGS) -c $< && \
-	mv -f $*.d.tmp $*.d;touch -c $@
+	mv -f $*.d.tmp $*.d && touch -c $@
 
 COMPILE_CXX=\
 	$(Q)echo "  CXX $S/$@"; \
 	$(CXX) -o $@ $(DEPFLAGS) $(CXXFLAGS) -c $< && \
-	mv -f $*.d.tmp $*.d;touch -c $@
+	mv -f $*.d.tmp $*.d && touch -c $@
 
 # Link $(OBJS) and $(LIBS) into $@ (app)
 LINK_C=\