bd3adf49a56d722059a781f0a86d6489619eb3d1
Ben Walker <benjamin.walker@intel.com>
2017-11-20 11:16:06 -0700

    nvmf: Add subsystem iterators
    Add functions to iterate the list of subsystems for a given
    target.
    
    Change-Id: Id3831f656033092224168b353788ae87238bf800
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/nvmf/subsystem.c
+++ lib/nvmf/subsystem.c
@@ -242,6 +242,43 @@ spdk_nvmf_delete_subsystem(struct spdk_nvmf_subsystem *subsystem)
 			      spdk_nvmf_subsystem_delete_done);
 }
 
+struct spdk_nvmf_subsystem *
+spdk_nvmf_subsystem_get_first(struct spdk_nvmf_tgt *tgt)
+{
+	struct spdk_nvmf_subsystem	*subsystem;
+	uint32_t sid;
+
+	for (sid = 0; sid < tgt->max_sid; sid++) {
+		subsystem = tgt->subsystems[sid];
+		if (subsystem) {
+			return subsystem;
+		}
+	}
+
+	return NULL;
+}
+
+struct spdk_nvmf_subsystem *
+spdk_nvmf_subsystem_get_next(struct spdk_nvmf_subsystem *subsystem)
+{
+	uint32_t sid;
+	struct spdk_nvmf_tgt *tgt;
+
+	if (!subsystem) {
+		return NULL;
+	}
+
+	tgt = subsystem->tgt;
+
+	for (sid = subsystem->id + 1; sid < tgt->max_sid; sid++) {
+		subsystem = tgt->subsystems[sid];
+		if (subsystem) {
+			return subsystem;
+		}
+	}
+
+	return NULL;
+}
 
 int
 spdk_nvmf_subsystem_add_host(struct spdk_nvmf_subsystem *subsystem, const char *hostnqn)
