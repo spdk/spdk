c952f3e0f474c7867df90776e297a75d0774f36c
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-11-20 19:24:24 +0100

    virtio: move virtio_dev allocation to upper layers
    This allows the to use a virtio_dev allocated as
    a part of bigger struct - possibly a SCSI/BLK
    specific wrapper.
    
    As a part of this refactor, multiple vdev-related
    functions have been renamed to keep the API clean.
    
    Change-Id: If84bf03e4a1642869c2f1ba93939fb32c8fb5d57
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/bdev/virtio/rte_virtio/virtio.c
+++ lib/bdev/virtio/rte_virtio/virtio.c
@@ -315,26 +315,18 @@ virtio_negotiate_features(struct virtio_dev *dev, uint64_t req_features)
 	return 0;
 }
 
-struct virtio_dev *
-	virtio_dev_construct(const struct virtio_dev_ops *ops, void *ctx)
+int
+virtio_dev_construct(struct virtio_dev *vdev, const struct virtio_dev_ops *ops, void *ctx)
 {
-	struct virtio_dev *vdev;
-
-	vdev = calloc(1, sizeof(*vdev));
-	if (vdev == NULL) {
-		SPDK_ERRLOG("virtio device calloc failed\n");
-		return NULL;
-	}
-
 	pthread_mutex_init(&vdev->mutex, NULL);
 	vdev->backend_ops = ops;
 	vdev->ctx = ctx;
 
-	return vdev;
+	return 0;
 }
 
 int
-virtio_dev_init(struct virtio_dev *dev, uint64_t req_features)
+virtio_dev_restart(struct virtio_dev *dev, uint64_t req_features)
 {
 	int ret;
 
@@ -358,12 +350,11 @@ virtio_dev_init(struct virtio_dev *dev, uint64_t req_features)
 }
 
 void
-virtio_dev_free(struct virtio_dev *dev)
+virtio_dev_destruct(struct virtio_dev *dev)
 {
 	virtio_free_queues(dev);
-	virtio_dev_backend_ops(dev)->free_vdev(dev);
+	virtio_dev_backend_ops(dev)->destruct_dev(dev);
 	pthread_mutex_destroy(&dev->mutex);
-	free(dev);
 }
 
 static void
