8b5ce6e01b7b7de5c2aa5c33ea73acaec022713e
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2017-11-23 13:35:15 +0100

    barrier: added spdk_smp_*mb to sync between cores
    The smp variants of memory barriers can
    be used in cases where just the sequential
    order of loads/stores between CPU cores
    is required.
    
    Change-Id: Ifbd187338bb441d4563672fa2f2afbe666607d76
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- include/spdk/barrier.h
+++ include/spdk/barrier.h
@@ -72,6 +72,44 @@ extern "C" {
 #error Unknown architecture
 #endif
 
+/** SMP memory barriers provide memory synchronization just between CPU cores. */
+
+/** Read memory barrier. */
+#ifdef __PPC64__
+#define spdk_smp_rmb()	__asm volatile("lwsync" ::: "memory")
+#elif defined(__aarch64__)
+#define spdk_smp_rmb()	__asm volatile("dmb ishld" ::: "memory")
+#elif defined(__i386__) || defined(__x86_64__)
+#define spdk_smp_rmb()	spdk_compiler_barrier()
+#else
+#define spdk_smp_rmb()
+#error Unknown architecture
+#endif
+
+/** Write memory barrier. */
+#ifdef __PPC64__
+#define spdk_smp_wmb()	__asm volatile("lwsync" ::: "memory")
+#elif defined(__aarch64__)
+#define spdk_smp_mb()	__asm volatile("dmb ishst" ::: "memory")
+#elif defined(__i386__) || defined(__x86_64__)
+#define spdk_smp_wmb()	spdk_compiler_barrier()
+#else
+#define spdk_smp_wmb()
+#error Unknown architecture
+#endif
+
+/** Full read/write memory barrier. */
+#ifdef __PPC64__
+#define spdk_smp_mb()	spdk_mb()
+#elif defined(__aarch64__)
+#define spdk_smp_mb()	__asm volatile("dmb ish" ::: "memory")
+#elif defined(__i386__) || defined(__x86_64__)
+#define spdk_smp_mb()	spdk_mb()
+#else
+#define spdk_smp_mb()
+#error Unknown architecture
+#endif
+
 #ifdef __cplusplus
 }
 #endif
