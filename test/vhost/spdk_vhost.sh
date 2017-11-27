b7e393bdcfd3b48f765b8531dc2760df052a947e
Chen Wang <chenx.wang@intel.com>
2017-11-28 00:47:15 +0800

    autotest: vhost_fio_test (porting from nightly to autotest)
    Add vhost-scsi and vhost-blk fio test cases which used more cores
    and controllers in nightly
    
    Change-Id: I51b47f6272c1b29dff890384087014627238dc68
    Signed-off-by: Chen Wang <chenx.wang@intel.com>

--- test/vhost/spdk_vhost.sh
+++ test/vhost/spdk_vhost.sh
@@ -8,17 +8,19 @@ case $1 in
 	-h|--help)
 		echo "usage: $(basename $0) TEST_TYPE"
 		echo "Test type can be:"
-		echo "  -i |--integrity             for running an integrity test with vhost scsi"
-		echo "  -fs|--fs-integrity-scsi     for running an integrity test with filesystem"
-		echo "  -fb|--fs-integrity-blk      for running an integrity test with filesystem"
-		echo "  -p |--performance           for running a performance test with vhost scsi"
-		echo "  -ib|--integrity-blk         for running an integrity test with vhost blk"
-		echo "  -pb|--performance-blk       for running a performance test with vhost blk"
-		echo "  -ils|--integrity-lvol-scsi  for running an integrity test with vhost scsi and lvol backends"
-		echo "  -ilb|--integrity-lvol-blk   for running an integrity test with vhost blk and lvol backends"
-		echo "  -hp|--hotplug               for running hotplug tests"
-		echo "  -ro|--readonly              for running readonly test for vhost blk"
-		echo "  -h |--help                  prints this message"
+		echo "  -i |--integrity                      for running an integrity test with vhost scsi"
+		echo "  -fs|--fs-integrity-scsi              for running an integrity test with filesystem"
+		echo "  -fb|--fs-integrity-blk               for running an integrity test with filesystem"
+		echo "  -p |--performance                    for running a performance test with vhost scsi"
+		echo "  -ib|--integrity-blk                  for running an integrity test with vhost blk"
+		echo "  -pb|--performance-blk                for running a performance test with vhost blk"
+		echo "  -ils|--integrity-lvol-scsi           for running an integrity test with vhost scsi and lvol backends"
+		echo "  -ilb|--integrity-lvol-blk            for running an integrity test with vhost blk and lvol backends"
+		echo "  -ilsn|--integrity-lvol-scsi-nightly  for running an nightly integrity test with vhost scsi and lvol backends"
+		echo "  -ilbn|--integrity-lvol-blk-nightly   for running an nightly integrity test with vhost blk and lvol backends"
+		echo "  -hp|--hotplug                        for running hotplug tests"
+		echo "  -ro|--readonly                       for running readonly test for vhost blk"
+		echo "  -h |--help                           prints this message"
 		echo ""
 		echo "Environment:"
 		echo "  VM_IMAGE        path to QCOW2 VM image used during test (default: $DEFAULT_VM_IMAGE)"
@@ -46,6 +48,8 @@ if [[ ! -r "${VM_IMAGE}" ]]; then
 	echo ""
 	exit 1
 fi
+
+DISKS_NUMBER=`lspci -mm -n | grep 0108 | tr -d '"' | awk -F " " '{print "0000:"$1}'| wc -l`
 
 WORKDIR=$(readlink -f $(dirname $0))
 cd $WORKDIR
@@ -102,6 +106,42 @@ case $1 in
 		echo 'Running lvol integrity suite...'
 		./lvol/lvol_test.sh -x --fio-bin=/home/sys_sgsw/fio_ubuntu \
 		--ctrl-type=vhost_blk
+		;;
+	-ilsn|--integrity-lvol-scsi-nightly)
+		if [[ $DISKS_NUMBER -ge 2 ]]; then
+			echo 'Running lvol integrity nightly suite with two cores and two controllers'
+			./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+			--ctrl-type=vhost_scsi --max-disks=2 --distribute-cores --vm-count=2
+
+			echo 'Running lvol integrity nightly suite with one core and two controllers'
+			./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+			--ctrl-type=vhost_scsi --max-disks=2 --vm-count=2
+		fi
+		echo 'Running lvol integrity nightly suite with one core and one controller'
+		./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+		--ctrl-type=vhost_scsi --max-disks=1
+
+		echo 'Running lvol integrity nightly suite with different os types'
+		./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+		--ctrl-type=vhost_scsi --vm-count=2 --multi-os
+		;;
+	-ilbn|--integrity-lvol-blk-nightly)
+		if [[ $DISKS_NUMBER -ge 2 ]]; then
+			echo 'Running lvol integrity nightly suite with two cores and two controllers'
+			./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+			--ctrl-type=vhost_blk --max-disks=2 --distribute-cores --vm-count=2
+
+			echo 'Running lvol integrity nightly suite with one core and two controllers'
+			./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+			--ctrl-type=vhost_blk --max-disks=2 --vm-count=2
+		fi
+		echo 'Running lvol integrity nightly suite with one core and one controller'
+		./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+		--ctrl-type=vhost_blk --max-disks=1
+
+		echo 'Running lvol integrity nightly suite with different os types'
+		./lvol/lvol_test.sh --fio-bin=/home/sys_sgsw/fio_ubuntu \
+		--ctrl-type=vhost_blk --vm-count=2 --multi-os
 		;;
 	-hp|--hotplug)
 		echo 'Running hotplug tests suite...'
