7ce0ae7ada20152bc23e56ef46ceaabcebffe592
Jim Harris <james.r.harris@intel.com>
2017-11-29 13:14:51 -0700

    blob: add spdk_bs_user_op_t
    This allows a channel's request_set resources to be
    used for queuing I/O requests.  This is needed
    for upcoming thin provisioning functionality,
    where we must queue I/O requests that need to
    allocate a cluster, if another cluster allocation
    is in progress.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Ie8d3e799afc0b56bc95ba5ecab11253d8bc8608f

--- lib/blob/request.h
+++ lib/blob/request.h
@@ -48,6 +48,8 @@ enum spdk_bs_cpl_type {
 	SPDK_BS_CPL_TYPE_NESTED_SEQUENCE,
 };
 
+enum spdk_blob_op_type;
+
 struct spdk_bs_request_set;
 
 /* Use a sequence to submit a set of requests serially */
@@ -55,6 +57,9 @@ typedef struct spdk_bs_request_set spdk_bs_sequence_t;
 
 /* Use a batch to submit a set of requests in parallel */
 typedef struct spdk_bs_request_set spdk_bs_batch_t;
+
+/* Use a user_op to queue a user operation for later execution */
+typedef struct spdk_bs_request_set spdk_bs_user_op_t;
 
 typedef void (*spdk_bs_nested_seq_complete)(void *cb_arg, spdk_bs_sequence_t *parent, int bserrno);
 
@@ -100,7 +105,7 @@ struct spdk_bs_cpl {
 typedef void (*spdk_bs_sequence_cpl)(spdk_bs_sequence_t *sequence,
 				     void *cb_arg, int bserrno);
 
-/* A generic request set. Can be a sequence or a batch. */
+/* A generic request set. Can be a sequence, batch or a user_op. */
 struct spdk_bs_request_set {
 	struct spdk_bs_cpl      cpl;
 
@@ -122,6 +127,17 @@ struct spdk_bs_request_set {
 			spdk_bs_sequence_cpl	cb_fn;
 			void			*cb_arg;
 		} batch;
+
+		struct spdk_bs_user_op_args {
+			int			type;
+			int			iovcnt;
+			struct spdk_blob	*blob;
+			uint64_t		offset;
+			uint64_t		length;
+			spdk_blob_op_complete	cb_fn;
+			void			*cb_arg;
+			void			*payload; /* cast to iov for readv/writev */
+		} user_op;
 	} u;
 
 	TAILQ_ENTRY(spdk_bs_request_set) link;
@@ -201,5 +217,13 @@ void spdk_bs_batch_close(spdk_bs_batch_t *batch);
 spdk_bs_batch_t *spdk_bs_sequence_to_batch(spdk_bs_sequence_t *seq,
 		spdk_bs_sequence_cpl cb_fn,
 		void *cb_arg);
+
+spdk_bs_user_op_t *spdk_bs_user_op_alloc(struct spdk_io_channel *channel, struct spdk_bs_cpl *cpl,
+		enum spdk_blob_op_type op_type, struct spdk_blob *blob,
+		void *payload, int iovcnt, uint64_t offset, uint64_t length);
+
+void spdk_bs_user_op_execute(spdk_bs_user_op_t *op);
+
+void spdk_bs_user_op_abort(spdk_bs_user_op_t *op);
 
 #endif
