412a79383812ecb4ca3a7824f2b6196b0247da22
Xiaodong Liu <xiaodong.liu@intel.com>
2017-11-29 23:51:35 -0500

    nbd: add nbd stop rpc
    Use soft-disconnection to stop nbd disk
    Examople RPC request:
    {
      "jsonrpc": "2.0",
      "id": 1,
      "method": "stop_nbd_disk",
      "params": {
        "nbd_device": "/dev/nbd0"
      }
    }
    
    Change-Id: I3d9161635b4422bbb9895914a03483dbaa6b6386
    Signed-off-by: Xiaodong Liu <xiaodong.liu@intel.com>

---
+++ lib/nbd/nbd_rpc.c
@@ -0,0 +1,128 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/rpc.h"
+#include "spdk/util.h"
+
+#include <linux/nbd.h>
+
+#include "nbd_internal.h"
+#include "spdk_internal/log.h"
+
+extern struct spdk_nbd_disk_globals g_spdk_nbd;
+
+static struct spdk_nbd_disk *
+spdk_nbd_disk_find_by_nbd_path(char *nbd_path)
+{
+	struct spdk_nbd_disk *nbd = NULL;
+	struct spdk_nbd_disk *nbd_idx;
+	struct spdk_nbd_disk *nbd_tmp;
+
+	pthread_mutex_lock(&g_spdk_nbd.mutex);
+	/*
+	 * check whether nbd has already been registered by nbd path.
+	 */
+	TAILQ_FOREACH_SAFE(nbd_idx, &g_spdk_nbd.disk_head, tailq, nbd_tmp) {
+		if (!strcmp(nbd_idx->nbd_path, nbd_path)) {
+			nbd = nbd_idx;
+			break;
+		}
+	}
+
+	pthread_mutex_unlock(&g_spdk_nbd.mutex);
+	return nbd;
+}
+
+struct rpc_stop_nbd_disk {
+	char *nbd_device;
+};
+
+static void
+free_rpc_stop_nbd_disk(struct rpc_stop_nbd_disk *req)
+{
+	free(req->nbd_device);
+}
+
+static const struct spdk_json_object_decoder rpc_stop_nbd_disk_decoders[] = {
+	{"nbd_device", offsetof(struct rpc_stop_nbd_disk, nbd_device), spdk_json_decode_string},
+};
+
+static void
+spdk_rpc_stop_nbd_disk(struct spdk_jsonrpc_request *request,
+		       const struct spdk_json_val *params)
+{
+	struct rpc_stop_nbd_disk req = {};
+	struct spdk_json_write_ctx *w;
+	struct spdk_nbd_disk *nbd;
+
+	if (spdk_json_decode_object(params, rpc_stop_nbd_disk_decoders,
+				    SPDK_COUNTOF(rpc_stop_nbd_disk_decoders),
+				    &req)) {
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		goto invalid;
+	}
+
+	if (req.nbd_device == NULL) {
+		goto invalid;
+	}
+
+	/* make sure nbd_device is registered */
+	nbd = spdk_nbd_disk_find_by_nbd_path(req.nbd_device);
+	if (!nbd) {
+		goto invalid;
+	}
+
+	/*
+	 * nbd soft-disconnection to terminate transmission phase.
+	 * After receiving this ioctl command, nbd kernel module will send
+	 * a NBD_CMD_DISC type io to nbd server in order to inform server.
+	 */
+	ioctl(nbd->dev_fd, NBD_DISCONNECT);
+
+	free_rpc_stop_nbd_disk(&req);
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w == NULL) {
+		return;
+	}
+
+	spdk_json_write_bool(w, true);
+	spdk_jsonrpc_end_result(request, w);
+	return;
+
+invalid:
+	free_rpc_stop_nbd_disk(&req);
+	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, "Invalid parameters");
+}
+
+SPDK_RPC_REGISTER("stop_nbd_disk", spdk_rpc_stop_nbd_disk)
