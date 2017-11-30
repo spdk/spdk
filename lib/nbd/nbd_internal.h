1666ef2f46f7f7253fa6f2f3ede77cb3dbffb143
Xiaodong Liu <xiaodong.liu@intel.com>
2017-11-30 05:10:42 -0500

    nbd: move nbd struct definition to nbd_internal.h
    Future nbd rpc and other features would like to know
    the details of nbd structure.
    
    Change-Id: Ib287d0139f4046c0c595fce1b0cc5baab31a6aea
    Signed-off-by: Xiaodong Liu <xiaodong.liu@intel.com>

---
+++ lib/nbd/nbd_internal.h
@@ -0,0 +1,85 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SPDK_NBD_INTERNAL_H
+#define SPDK_NBD_INTERNAL_H
+
+#include "spdk/stdinc.h"
+
+#include "spdk/queue.h"
+#include "spdk/bdev.h"
+#include "spdk/nbd.h"
+
+struct nbd_io {
+	enum spdk_bdev_io_type	type;
+	int			ref;
+	void			*payload;
+
+	/* NOTE: for TRIM, this represents number of bytes to trim. */
+	uint32_t		payload_size;
+
+	bool			payload_in_progress;
+
+	struct nbd_request	req;
+	bool			req_in_progress;
+
+	struct nbd_reply	resp;
+	bool			resp_in_progress;
+
+	/*
+	 * Tracks current progress on reading/writing a request,
+	 * response, or payload from the nbd socket.
+	 */
+	uint32_t		offset;
+};
+
+struct spdk_nbd_disk {
+	struct spdk_bdev	*bdev;
+	struct spdk_bdev_desc	*bdev_desc;
+	struct spdk_io_channel	*ch;
+	int			dev_fd;
+	int			kernel_sp_fd;
+	int			spdk_sp_fd;
+	struct nbd_io		io;
+	struct spdk_poller	*nbd_poller;
+	uint32_t		buf_align;
+
+	TAILQ_ENTRY(spdk_nbd_disk)	tailq;
+};
+
+struct spdk_nbd_disk_globals {
+	pthread_mutex_t		mutex;
+	TAILQ_HEAD(, spdk_nbd_disk)	disk_head;
+};
+
+#endif /* SPDK_NBD_INTERNAL_H */
