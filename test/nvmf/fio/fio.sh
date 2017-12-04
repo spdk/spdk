ee81ac717ad18c352d64872bf8e05087d7abe5b2
Liang Yan <liang.z.yan@intel.com>
2017-12-04 15:20:38 +0800

    scripts: refactor fio test scripts
    The iSCSI and NVMe-oF fio test scripts have a lot duplicated codes.
    So create the fiocommon.py file to have the codes used for both.
    And also create iscsi_fio.py and nvmf_fio.py files to test iSCSI
    and NVMe-oF with fio.
    
    Change-Id: Ib8eb65f55f5975a4a7c9155c2b2f78d65736b317
    Signed-off-by: Liang Yan <liang.z.yan@intel.com>

--- test/nvmf/fio/fio.sh
+++ test/nvmf/fio/fio.sh
@@ -9,6 +9,7 @@ MALLOC_BDEV_SIZE=64
 MALLOC_BLOCK_SIZE=512
 
 rpc_py="python $rootdir/scripts/rpc.py"
+fio_py="python $rootdir/scripts/nvmf_fio.py"
 
 set -e
 
@@ -39,15 +40,15 @@ $rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 "trtype:RDMA traddr:
 
 nvme connect -t rdma -n "nqn.2016-06.io.spdk:cnode1" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
 
-$testdir/nvmf_fio.py 4096 1 write 1 verify
-$testdir/nvmf_fio.py 4096 1 randwrite 1 verify
-$testdir/nvmf_fio.py 4096 128 write 1 verify
-$testdir/nvmf_fio.py 4096 128 randwrite 1 verify
+$fio_py 4096 1 write 1 verify
+$fio_py 4096 1 randwrite 1 verify
+$fio_py 4096 128 write 1 verify
+$fio_py 4096 128 randwrite 1 verify
 
 sync
 
 #start hotplug test case
-$testdir/nvmf_fio.py 4096 1 read 10 &
+$fio_py 4096 1 read 10 &
 fio_pid=$!
 
 sleep 3
