29367ed4be8c180d6827b46649027afb19ad70c6
Maciej Szwed <maciej.szwed@intel.com>
2017-12-04 12:38:13 +0100

    blob: cluster allocation/deallocation for thin provisioned blob
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Ib3470fbac49e92308ed14e20ccde6655354f2580

--- lib/blob/blobstore.h
+++ lib/blob/blobstore.h
@@ -240,6 +240,8 @@ struct spdk_blob_md_descriptor_extent {
 #define SPDK_BLOB_DATA_RO_FLAGS_MASK	SPDK_BLOB_READ_ONLY
 #define SPDK_BLOB_MD_RO_FLAGS_MASK	SPDK_BLOB_READ_ONLY
 
+#define spdk_blob_is_thin_provisioned(blob) (blob->invalid_flags & SPDK_BLOB_THIN_PROV)
+
 struct spdk_blob_md_descriptor_flags {
 	uint8_t		type;
 	uint32_t	length;
@@ -439,6 +441,22 @@ _spdk_bs_num_pages_to_cluster_boundary(struct spdk_blob_data *blob, uint32_t pag
 	pages_per_cluster = blob->bs->pages_per_cluster;
 
 	return pages_per_cluster - (page % pages_per_cluster);
+}
+
+/* Given a page offset into a blob, look up if it is from allocated cluster. */
+static inline bool
+_spdk_bs_blob_is_page_from_allocated_cluster(struct spdk_blob_data *blob, uint32_t page)
+{
+	uint64_t	lba;
+	uint32_t	pages_per_cluster;
+
+	pages_per_cluster = blob->bs->pages_per_cluster;
+
+	assert(page < blob->active.num_clusters * pages_per_cluster);
+
+	lba = blob->active.clusters[page / pages_per_cluster];
+
+	return (bool)(lba != 0);
 }
 
 #endif
