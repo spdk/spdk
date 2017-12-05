06749e28afc839c4f1744d00fb931fd7d8cdb92e
wenzhong wu <wenzhongx.wu@intel.com>
2017-12-06 06:07:48 +0800

    autotest:Add fio_plugin with nvmf lvol backend(Porting from DTS)
    To cover fio_plugin as host with nvmf lvol backend.
    
    Change-Id: I5c602d95421f1ab6d2dde660c1833c953c2e5c75
    Signed-off-by: wenzhong wu <wenzhongx.wu@intel.com>

--- test/nvmf/host/fio.sh
+++ test/nvmf/host/fio.sh
@@ -33,15 +33,44 @@ waitforlisten $nvmfpid
 timing_exit start_nvmf_tgt
 
 bdevs="$bdevs $($rpc_py construct_malloc_bdev 64 512)"
-$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" '' -a -s SPDK00000000000001 -n "$bdevs"
+$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk-cnode1 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" '' -a -s SPDK00000000000001 -n "$bdevs"
 
 PLUGIN_DIR=$rootdir/examples/nvme/fio_plugin
 
-LD_PRELOAD=$PLUGIN_DIR/fio_plugin /usr/src/fio/fio $PLUGIN_DIR/example_config.fio --filename="trtype=RDMA adrfam=IPv4 traddr=$NVMF_FIRST_TARGET_IP trsvcid=4420 ns=1"
+bdfs=$(lspci -mm -n | grep 0108 | tr -d '"' | awk -F " " '{print "0000:"$1}')
+#Configure nvme devices with nvmf lvol_bdev backend
+if [ -n "$bdfs" ]; then
+	$rpc_py construct_nvme_bdev -b Nvme0 -t PCIe -a $(echo $bdfs | awk '{ print $1 }')
+	ls_guid=$($rpc_py construct_lvol_store Nvme0n1 lvs_0)
+	lb_guid=$($rpc_py construct_lvol_bdev -u $ls_guid  lbd_0 16000)
+	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk-cnode2 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" "" -a -s SPDK00000000000002 -n "$lb_guid"
+	#Create lvol bdev for nested lvol stores
+	ls_nested_guid=$($rpc_py construct_lvol_store $lb_guid lvs_n_0)
+	lb_nested_guid=$($rpc_py construct_lvol_bdev -u $ls_nested_guid lbd_nest_0 2000)
+	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk-cnode3 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" "" -a -s SPDK00000000000003 -n "$lb_nested_guid"
+	#Test fio_plugin connect to nvmf subnqn with nvmf lvol and malloc backend
+	LD_PRELOAD=$PLUGIN_DIR/fio_plugin /usr/src/fio/fio $PLUGIN_DIR/example_config.fio --filename="trtype=RDMA adrfam=IPv4 \
+	traddr=$NVMF_FIRST_TARGET_IP subnqn=nqn.2016-06.io.spdk-cnode1 trsvcid=4420 ns=1" --filename="trtype=RDMA adrfam=IPv4 \
+	traddr=$NVMF_FIRST_TARGET_IP subnqn=nqn.2016-06.io.spdk-cnode2 trsvcid=4420 ns=1" --filename="trtype=RDMA adrfam=IPv4 \
+	traddr=$NVMF_FIRST_TARGET_IP subnqn=nqn.2016-06.io.spdk-cnode3 trsvcid=4420 ns=1"
+else
+	LD_PRELOAD=$PLUGIN_DIR/fio_plugin /usr/src/fio/fio $PLUGIN_DIR/example_config.fio --filename="trtype=RDMA adrfam=IPv4 \
+	traddr=$NVMF_FIRST_TARGET_IP subnqn=nqn.2016-06.io.spdk-cnode1 trsvcid=4420 ns=1"
+fi
 
 sync
+$rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk-cnode1
 
-$rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk:cnode1
+#Delete subsystems,lvol_bdev and destory lvol_store.
+if [ -n "$bdfs" ]; then
+	$rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk-cnode2
+	$rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk-cnode3
+	$rpc_py delete_bdev  "$lb_nested_guid"
+	$rpc_py destroy_lvol_store -l lvs_n_0
+	$rpc_py delete_bdev  "$lb_guid"
+	$rpc_py destroy_lvol_store -l lvs_0
+	$rpc_py delete_bdev "Nvme0n1"
+fi
 
 trap - SIGINT SIGTERM EXIT
 
