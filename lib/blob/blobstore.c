cc289c49655f81234b12f5ab6a9ac6f40201f2e6
Maciej Szwed <maciej.szwed@intel.com>
2017-12-06 14:06:13 +0100

    blob: add spdk_bs_md_create_blob_opts
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Iba33c55f129c60fad2d58f5254dec5c54ed56805
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -79,6 +79,12 @@ _spdk_bs_release_cluster(struct spdk_blob_store *bs, uint32_t cluster_num)
 	bs->num_free_clusters++;
 }
 
+void
+spdk_blob_opts_init(struct spdk_blob_opts *opts)
+{
+
+}
+
 static struct spdk_blob *
 _spdk_blob_alloc(struct spdk_blob_store *bs, spdk_blob_id id)
 {
@@ -2588,14 +2594,21 @@ _spdk_bs_md_create_blob_cpl(spdk_bs_sequence_t *seq, void *cb_arg, int bserrno)
 	spdk_bs_sequence_finish(seq, bserrno);
 }
 
-void spdk_bs_md_create_blob(struct spdk_blob_store *bs,
-			    spdk_blob_op_with_id_complete cb_fn, void *cb_arg)
+void spdk_bs_md_create_blob_ext(struct spdk_blob_store *bs, struct spdk_blob_opts *o,
+				spdk_blob_op_with_id_complete cb_fn, void *cb_arg)
 {
 	struct spdk_blob	*blob;
 	uint32_t		page_idx;
 	struct spdk_bs_cpl 	cpl;
+	struct spdk_blob_opts 	opts;
 	spdk_bs_sequence_t	*seq;
 	spdk_blob_id		id;
+
+	if (o) {
+		opts = *o;
+	} else {
+		spdk_blob_opts_init(&opts);
+	}
 
 	page_idx = spdk_bit_array_find_first_clear(bs->used_md_pages, 0);
 	if (page_idx >= spdk_bit_array_capacity(bs->used_md_pages)) {
@@ -2627,6 +2640,12 @@ void spdk_bs_md_create_blob(struct spdk_blob_store *bs,
 	}
 
 	_spdk_blob_persist(seq, blob, _spdk_bs_md_create_blob_cpl, blob);
+}
+
+void spdk_bs_md_create_blob(struct spdk_blob_store *bs,
+			    spdk_blob_op_with_id_complete cb_fn, void *cb_arg)
+{
+	spdk_bs_md_create_blob_ext(bs, NULL, cb_fn, cb_arg);
 }
 
 /* END spdk_bs_md_create_blob */
