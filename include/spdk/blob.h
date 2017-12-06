6970bbf9b4b5e382fd40bd921c8549ab03e54356
Maciej Szwed <maciej.szwed@intel.com>
2017-12-06 14:06:13 +0100

    blob: add spdk_bs_md_create_blob_opts
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Iba33c55f129c60fad2d58f5254dec5c54ed56805
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>

--- include/spdk/blob.h
+++ include/spdk/blob.h
@@ -218,7 +218,21 @@ uint64_t spdk_blob_get_num_pages(struct spdk_blob *blob);
 /* Return the number of clusters allocated to the blob */
 uint64_t spdk_blob_get_num_clusters(struct spdk_blob *blob);
 
-/* Create a new blob with initial size of 'sz' clusters. */
+struct spdk_blob_opts {
+	int	xattr_count;
+	char	**xattr_names;
+	void	(*get_xattr_value)(void *arg, const char *name,
+				   const void **value, size_t *value_len);
+};
+
+/* Initialize an spdk_blob_opts structure to the default blob option values. */
+void spdk_blob_opts_init(struct spdk_blob_opts *opts);
+
+/* Create a new blob with options and initial size of 0 clusters. */
+void spdk_bs_md_create_blob_ext(struct spdk_blob_store *bs, struct spdk_blob_opts *o,
+				spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
+
+/* Create a new blob with initial size of 0 clusters. */
 void spdk_bs_md_create_blob(struct spdk_blob_store *bs,
 			    spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
 
