bfc3523d695b5e08d5c2c748b947260fbfa9c02b
Chen Wang <chenx.wang@intel.com>
2017-12-07 19:14:29 +0800

    autotest: vhost_filesystem_test (porting from nightly to autotest)
    Add vhost-scsi and vhost-blk filesystem test cases with
    lvol and nvme back-end in nightly
    
    Change-Id: Id2cccd9a45aaa995bef90415855835b93447f2e9
    Signed-off-by: Chen Wang <chenx.wang@intel.com>

--- test/vhost/integrity/integrity_vm.sh
+++ test/vhost/integrity/integrity_vm.sh
@@ -16,56 +16,51 @@ else script=$blk_script;
 fi
 
 trap "exit 1" SIGINT SIGTERM EXIT
-for dev in $devs; do
-        mkfs_cmd="mkfs.$fs"
-        parted_cmd="parted -s /dev/${dev}"
-        if [ "ntfs" == $fs ]; then
-            mkfs_cmd+=" -f -F"
-        elif [ "btrfs" == $fs ]; then
-            mkfs_cmd+=" -f"
-        elif [ "fat" == $fs ]; then
-            mkfs_cmd+=" -I"
-        else
-            mkfs_cmd+=" -F"
-        fi
 
-        echo "INFO: Creating partition table on disk using: $parted_cmd mklabel gpt"
-        $parted_cmd mklabel gpt
-        $parted_cmd mkpart primary 2048s 100%
-        sleep 2
+for fs in $fs; do
+        for dev in $devs; do
+                mkfs_cmd="mkfs.$fs"
+                parted_cmd="parted -s /dev/${dev}"
 
-        mkfs_cmd+=" /dev/${dev}1"
-        echo "INFO: Creating filesystem using: $mkfs_cmd"
-        $mkfs_cmd
+                echo "INFO: Creating partition table on disk using: $parted_cmd mklabel gpt"
+                $parted_cmd mklabel gpt
+                $parted_cmd mkpart primary 2048s 100%
+                sleep 2
 
-        mkdir -p /mnt/${dev}dir
-        mount -o sync /dev/${dev}1 /mnt/${dev}dir
-        mkdir -p /mnt/${dev}dir/linux-src
-        tar xf $basedir/linux-src.tar.gz -C /mnt/${dev}dir/linux-src --strip-components=1
-        sleep 2
+                mkfs_cmd+=" /dev/${dev}1"
+                echo "INFO: Creating filesystem using: $mkfs_cmd"
+                wipefs -a /dev/${dev}1
+                $mkfs_cmd
 
-        # Now build SPDK
-        $MAKE -C /mnt/${dev}dir/linux-src defconfig
-        $MAKE -C /mnt/${dev}dir/linux-src
-        # Print out space consumed on target device
-        df -h /dev/$dev
-        rm -rf /mnt/${dev}dir/linux-src
-done
+                mkdir -p /mnt/${dev}dir
+                mount -o sync /dev/${dev}1 /mnt/${dev}dir
+                mkdir -p /mnt/${dev}dir/linux-src
+                tar xf $basedir/linux-src.tar.gz -C /mnt/${dev}dir/linux-src --strip-components=1
+                sleep 2
 
-for dev in $devs; do
-        umount /mnt/${dev}dir
-        rm -rf /mnt/${dev}dir
+                # Now build SPDK
+                $MAKE -C /mnt/${dev}dir/linux-src defconfig
+                $MAKE -C /mnt/${dev}dir/linux-src
+                # Print out space consumed on target device
+                df -h /dev/$dev
+                rm -rf /mnt/${dev}dir/linux-src
+        done
 
-        stats=( $(cat /sys/block/$dev/stat) )
-        echo ""
-        echo "$dev stats"
-        printf "READ  IO cnt: % 8u merges: % 8u sectors: % 8u ticks: % 8u\n" \
-                   ${stats[0]} ${stats[1]} ${stats[2]} ${stats[3]}
-        printf "WRITE IO cnt: % 8u merges: % 8u sectors: % 8u ticks: % 8u\n" \
-                   ${stats[4]} ${stats[5]} ${stats[6]} ${stats[7]}
-        printf "in flight: % 8u io ticks: % 8u time in queue: % 8u\n" \
-                   ${stats[8]} ${stats[9]} ${stats[10]}
-        echo ""
+        for dev in $devs; do
+                umount /mnt/${dev}dir
+                rm -rf /mnt/${dev}dir
+
+                stats=( $(cat /sys/block/$dev/stat) )
+                echo ""
+                echo "$dev stats"
+                printf "READ  IO cnt: % 8u merges: % 8u sectors: % 8u ticks: % 8u\n" \
+                           ${stats[0]} ${stats[1]} ${stats[2]} ${stats[3]}
+                printf "WRITE IO cnt: % 8u merges: % 8u sectors: % 8u ticks: % 8u\n" \
+                           ${stats[4]} ${stats[5]} ${stats[6]} ${stats[7]}
+                printf "in flight: % 8u io ticks: % 8u time in queue: % 8u\n" \
+                           ${stats[8]} ${stats[9]} ${stats[10]}
+                echo ""
+        done
 done
 
 trap - SIGINT SIGTERM EXIT
