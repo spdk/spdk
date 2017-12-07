94d6b2ee13ddb50dc75efb954b47cc04d0726207
Jim Harris <james.r.harris@intel.com>
2017-12-07 13:08:42 -0700

    blob: decouple spdk_blob from its actual data
    Moving forward, the spdk_blob returned to users will
    actually be an I/O channel - not the blob structure
    itself.  So rename the existing spdk_blob to spdk_blob_data.
    spdk_blob_data will continue to contain global state for
    the blob.  In the future spdk_blob will point to an
    I/O channel for the blob - for now it effectively still
    points to the spdk_blob_data, but by changing the
    structure names here it will reduce the code churn in
    future patches.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I7d0cbc0553f68f96c24173c833091a80d058eb89

--- examples/blob/cli/blobcli.c
+++ examples/blob/cli/blobcli.c
@@ -465,7 +465,7 @@ show_blob(struct cli_context_t *cli_context)
 	 * may be useful for debug of blobstore based applications.
 	 */
 	printf("\nBlob Private Info:\n");
-	switch (cli_context->blob->state) {
+	switch (__blob_to_data(cli_context->blob)->state) {
 	case SPDK_BLOB_STATE_DIRTY:
 		printf("state: DIRTY\n");
 		break;
@@ -483,7 +483,7 @@ show_blob(struct cli_context_t *cli_context)
 		break;
 	}
 	printf("open ref count: %d\n",
-	       cli_context->blob->open_ref);
+	       __blob_to_data(cli_context->blob)->open_ref);
 
 	spdk_xattr_names_free(names);
 }
