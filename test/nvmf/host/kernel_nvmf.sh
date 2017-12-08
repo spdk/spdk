44e5ee94429641f1b5b8f3d08489455e33a3da10
wenzhong wu <wenzhongx.wu@intel.com>
2017-12-09 05:59:07 +0800

    autotest: Add perf and fio_plugin as host with kernel nvmf-tgt
    To cover perf and fio_plugin with kernel nvmf-tgt.(Porting from DTS)
    
    Change-Id: Ib90606ca602cea0dfad2c6e4768626cd9ef57628
    Signed-off-by: wenzhong wu <wenzhongx.wu@intel.com>

---
+++ test/nvmf/host/kernel_nvmf.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../../..)
+source $rootdir/scripts/autotest_common.sh
+source $rootdir/test/nvmf/common.sh
+
+set -e
+
+RDMA_IP_LIST=$(get_available_rdma_ips)
+NVMF_FIRST_TARGET_IP=$(echo "$RDMA_IP_LIST" | head -n 1)
+if [ -z $NVMF_FIRST_TARGET_IP ]; then
+	echo "no NIC for nvmf test"
+	exit 0
+fi
+
+timing_enter identify_kernel_nvmf_tgt
+
+subsystemname=nqn.2016-06.io.spdk:testnqn
+
+modprobe null_blk nr_devices=1
+modprobe nvmet
+modprobe nvmet-rdma
+
+if [ ! -d /sys/kernel/config/nvmet/subsystems/$subsystemname ]; then
+	mkdir /sys/kernel/config/nvmet/subsystems/$subsystemname
+fi
+echo 1 > /sys/kernel/config/nvmet/subsystems/$subsystemname/attr_allow_any_host
+
+if [ ! -d /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1 ]; then
+	mkdir /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1
+fi
+
+echo -n /dev/nullb0 > /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1/device_path
+echo 1 > /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1/enable
+
+if [ ! -d /sys/kernel/config/nvmet/ports/1 ]; then
+	mkdir /sys/kernel/config/nvmet/ports/1
+fi
+
+echo -n rdma > /sys/kernel/config/nvmet/ports/1/addr_trtype
+echo -n ipv4 > /sys/kernel/config/nvmet/ports/1/addr_adrfam
+echo -n $NVMF_FIRST_TARGET_IP > /sys/kernel/config/nvmet/ports/1/addr_traddr
+echo -n $NVMF_PORT > /sys/kernel/config/nvmet/ports/1/addr_trsvcid
+
+ln -s /sys/kernel/config/nvmet/subsystems/$subsystemname /sys/kernel/config/nvmet/ports/1/subsystems/$subsystemname
+
+sleep 2
+
+$rootdir/examples/nvme/identify/identify -r "\
+	trtype:RDMA \
+	adrfam:IPv4 \
+	traddr:$NVMF_FIRST_TARGET_IP \
+	trsvcid:$NVMF_PORT \
+	subnqn:nqn.2014-08.org.nvmexpress.discovery" -t all
+
+$rootdir/examples/nvme/perf/perf -q 128 -s 4096 -w randrw -M 50 -r "\
+	trtype:RDMA \
+	adrfam:IPv4 \
+	traddr:$NVMF_FIRST_TARGET_IP \
+	trsvcid:$NVMF_PORT \
+	subnqn:nqn.2014-08.org.nvmexpress.discovery" -t 1
+
+LD_PRELOAD=$rootdir/examples/nvme/fio_plugin/fio_plugin /usr/src/fio/fio $rootdir/examples/nvme/fio_plugin/example_config.fio --filename="\
+	trtype=RDMA \
+	adrfam=IPv4 \
+	traddr=$NVMF_FIRST_TARGET_IP \
+	subnqn=nqn.2014-08.org.nvmexpress.discovery trsvcid=4420 ns=1"
+
+rm -rf /sys/kernel/config/nvmet/ports/1/subsystems/$subsystemname
+
+echo 0 > /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1/enable
+echo -n 0 > /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1/device_path
+
+rmdir --ignore-fail-on-non-empty /sys/kernel/config/nvmet/subsystems/$subsystemname/namespaces/1
+rmdir --ignore-fail-on-non-empty /sys/kernel/config/nvmet/subsystems/$subsystemname
+rmdir --ignore-fail-on-non-empty /sys/kernel/config/nvmet/ports/1
+
+rmmod nvmet-rdma
+rmmod null_blk
+rmmod nvmet
+
+timing_exit identify_kernel_nvmf_tgt
