bd289a5c291ec59cb24c1ab0c292277a22446fea
Sebastian Basierski <sebastianx.basierski@intel.com>
2017-12-12 12:06:36 +0100

    bdev: Logical volume rename - I part
    Code concerning Logical volume bdev rename.
    
    Change-Id: I293e5e1d97b0cbeb77e6dd7b91a59aec84122bd0
    Signed-off-by: Sebastian Basierski <sebastianx.basierski@intel.com>

--- lib/bdev/lvol/vbdev_lvol_rpc.c
+++ lib/bdev/lvol/vbdev_lvol_rpc.c
@@ -345,6 +345,86 @@ invalid:
 
 SPDK_RPC_REGISTER("construct_lvol_bdev", spdk_rpc_construct_lvol_bdev)
 
+struct rpc_rename_lvol_bdev {
+	char *uuid;
+	char *old_lvol_bdev_name;
+	char *new_lvol_bdev_name;
+};
+
+static void
+free_rpc_rename_lvol_bdev(struct rpc_rename_lvol_bdev *req)
+{
+	free(req->uuid);
+	free(req->old_lvol_bdev_name);
+	free(req->new_lvol_bdev_name);
+}
+
+static const struct spdk_json_object_decoder rpc_crename_lvol_bdev_decoders[] = {
+	{"uuid", offsetof(struct rpc_rename_lvol_bdev, uuid), spdk_json_decode_string, true},
+	{"old_lvol_bdev_name", offsetof(struct rpc_rename_lvol_bdev, old_lvol_bdev_name), spdk_json_decode_string, true},
+	{"new_lvol_bdev_name", offsetof(struct rpc_rename_lvol_bdev, new_lvol_bdev_name), spdk_json_decode_string, true},
+};
+
+static void
+_spdk_rpc_rename_lvol_bdev_cb(void *cb_arg, int lvolerrno)
+{
+	struct spdk_json_write_ctx *w;
+	struct spdk_jsonrpc_request *request = cb_arg;
+	char buf[64];
+
+	if (lvolerrno != 0) {
+		goto invalid;
+	}
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w == NULL) {
+		return;
+	}
+
+	spdk_json_write_bool(w, true);
+	spdk_jsonrpc_end_result(request, w);
+	return;
+
+invalid:
+	spdk_strerror_r(-lvolerrno, buf, sizeof(buf));
+	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, buf);
+}
+
+static void
+spdk_rpc_rename_lvol_bdev(struct spdk_jsonrpc_request *request,
+			  const struct spdk_json_val *params)
+{
+	struct rpc_rename_lvol_bdev req = {};
+	int rc;
+	char buf[64];
+
+	SPDK_INFOLOG(SPDK_LOG_LVOL_RPC, "Creating blob\n");
+
+	if (spdk_json_decode_object(params, rpc_crename_lvol_bdev_decoders,
+				    SPDK_COUNTOF(rpc_crename_lvol_bdev_decoders),
+				    &req)) {
+		SPDK_INFOLOG(SPDK_LOG_LVOL_RPC, "spdk_json_decode_object failed\n");
+		rc = -EINVAL;
+		goto invalid;
+	}
+
+	rc = vbdev_lvol_rename(req.old_lvol_bdev_name, req.new_lvol_bdev_name,
+			       _spdk_rpc_rename_lvol_bdev_cb, request);
+	if (rc < 0) {
+		goto invalid;
+	}
+
+	free_rpc_rename_lvol_bdev(&req);
+	return;
+
+invalid:
+	spdk_strerror_r(-rc, buf, sizeof(buf));
+	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, buf);
+	free_rpc_rename_lvol_bdev(&req);
+}
+
+SPDK_RPC_REGISTER("rename_lvol_bdev", spdk_rpc_rename_lvol_bdev)
+
 struct rpc_resize_lvol_bdev {
 	char *name;
 	uint64_t size;
