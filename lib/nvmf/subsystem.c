7f5864be205532fe530a57a320081d87f13eab64
Cunyin Chang <cunyin.chang@intel.com>
2017-12-14 10:38:18 +0800

    nvmf: Add public interface of remove ns from subsystem.
    Change-Id: I9c2746dd54a13f3dae0ac2bab1d5fced931e8591
    Signed-off-by: Cunyin Chang <cunyin.chang@intel.com>
    Reviewed-on: https://review.gerrithub.io/391699
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/nvmf/subsystem.c
+++ lib/nvmf/subsystem.c
@@ -427,6 +427,50 @@ spdk_nvmf_listener_get_trid(struct spdk_nvmf_listener *listener)
 }
 
 static void
+spdk_nvmf_remove_ns_done(struct spdk_io_channel_iter *i, int status)
+{
+	struct spdk_nvmf_ns *ns = spdk_io_channel_iter_get_ctx(i);
+	struct spdk_nvmf_subsystem *subsystem = ns->subsystem;
+
+	spdk_bdev_close(ns->desc);
+	ns->allocated = false;
+	subsystem->num_allocated_nsid--;
+}
+
+static void
+spdk_nvmf_subsystem_remove_ns_from_poll_group(struct spdk_io_channel_iter *i)
+{
+	struct spdk_nvmf_ns *ns = spdk_io_channel_iter_get_ctx(i);
+	struct spdk_io_channel *ch = spdk_io_channel_iter_get_channel(i);
+	struct spdk_nvmf_poll_group *group = spdk_io_channel_get_ctx(ch);
+	int rc;
+
+	rc = spdk_nvmf_poll_group_remove_ns(group, ns);
+	spdk_for_each_channel_continue(i, rc);
+}
+
+int
+spdk_nvmf_subsystem_remove_ns(struct spdk_nvmf_subsystem *subsystem, uint32_t nsid)
+{
+	struct spdk_nvmf_ns *ns;
+
+	if (nsid == 0 || nsid > subsystem->max_nsid) {
+		return -1;
+	}
+
+	ns = &subsystem->ns[nsid - 1];
+	if (ns->allocated == false) {
+		return -1;
+	}
+
+	spdk_for_each_channel(ns->subsystem->tgt,
+			      spdk_nvmf_subsystem_remove_ns_from_poll_group,
+			      ns,
+			      spdk_nvmf_remove_ns_done);
+	return 0;
+}
+
+static void
 spdk_nvmf_subsystem_add_ns_done(struct spdk_io_channel_iter *i, int status)
 {
 	return;
