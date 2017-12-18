d3679abc54364aa1ffed4911b2bfcd48d1769188
Pawel Kaminski <pawelx.kaminski@intel.com>
2017-12-18 16:02:24 +0100

    test/vhost: Scsi hot remove tests
    Change-Id: I7dac919f0ba331fe6a78855c0171c0798d48fade
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/vhost/hotplug/scsi_hotplug.sh
+++ test/vhost/hotplug/scsi_hotplug.sh
@@ -5,13 +5,28 @@ BASE_DIR=$(readlink -f $(dirname $0))
 
 . $BASE_DIR/common.sh
 
+current_driver="vfio"
+if $BASE_DIR/../../../scripts/setup.sh status | grep uio; then
+    current_driver="uio"
+fi
+
 # Add split section into vhost config
 function gen_config() {
     cp $BASE_DIR/vhost.conf.base $BASE_DIR/vhost.conf.in
     cat << END_OF_CONFIG >> $BASE_DIR/vhost.conf.in
 [Split]
   Split Nvme0n1 16
+  Split Nvme1n1 20
+  Split HotInNvme0n1 2
+  Split HotInNvme1n1 2
+  Split HotInNvme2n1 2
 END_OF_CONFIG
+}
+
+function pre_test_case() {
+    $SPDK_BUILD_DIR/scripts/rpc.py construct_malloc_bdev -b Malloc0 128 4096
+    $SPDK_BUILD_DIR/scripts/rpc.py construct_malloc_bdev -b Malloc1 128 4096
+    $SPDK_BUILD_DIR/scripts/rpc.py construct_malloc_bdev -b Malloc2 128 4096
 }
 
 # Run spdk by calling run_vhost from hotplug/common.sh.
@@ -61,15 +76,28 @@ function post_hot_attach_detach_test_case() {
 
 trap 'on_error_exit "${FUNCNAME}" "${LINENO}"' ERR
 gen_config
+if [ $current_driver == "vfio" ]; then
+    switch_to_uio
+fi
 run_vhost
 rm $BASE_DIR/vhost.conf.in
-pre_hot_attach_detach_test_case
-$BASE_DIR/scsi_hotattach.sh --fio-bin=$fio_bin &
-first_script=$!
-$BASE_DIR/scsi_hotdetach.sh --fio-bin=$fio_bin &
-second_script=$!
-wait $first_script
-wait $second_script
-vm_shutdown_all
-post_hot_attach_detach_test_case
-spdk_vhost_kill
+pre_test_case
+if [ $scsi_hot_remove_test == 0 ]; then
+    pre_hot_attach_detach_test_case
+    $BASE_DIR/scsi_hotattach.sh --fio-bin=$fio_bin &
+    first_script=$!
+    $BASE_DIR/scsi_hotdetach.sh --fio-bin=$fio_bin &
+    second_script=$!
+    wait $first_script
+    wait $second_script
+    vm_shutdown_all
+    post_hot_attach_detach_test_case
+fi
+vm_arg=""
+for vm in "${vms[@]}"; do
+    vm_arg+="--vm=$vm "
+done
+if [ $scsi_hot_remove_test == 1 ];then
+    $BASE_DIR/scsi_hotremove.sh --fio-bin=$fio_bin $vm_arg --test-type=spdk_vhost_scsi
+fi
+post_test_case
