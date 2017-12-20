872d628b8a6fcdc85551db6e9620594f3a35a270
Sebastian Basierski <sebastianx.basierski@intel.com>
2017-12-20 16:02:49 +0100

    bdev: Added functions allowing logical volume store rename.
    Change-Id: I564af4acb0c613f1da09bfe18d85e0820a60bce2
    Signed-off-by: Sebastian Basierski <sebastianx.basierski@intel.com>

--- lib/lvol/lvol.c
+++ lib/lvol/lvol.c
@@ -605,6 +605,37 @@ spdk_lvs_init(struct spdk_bs_dev *bs_dev, struct spdk_lvs_opts *o,
 }
 
 static void
+_spdk_lvs_rename_cb(void *cb_arg, int lvolerrno)
+{
+	struct spdk_lvol_req *req = cb_arg;
+
+	req->cb_fn(req->cb_arg, lvolerrno);
+	free(req);
+}
+
+int spdk_lvs_rename(struct spdk_lvol_store *lvs, const char *new_name,
+		    spdk_lvs_op_complete cb_fn, void *cb_arg)
+{
+	struct spdk_blob *blob = lvs->super_blob;
+	struct spdk_lvol_req *req;
+
+	req = calloc(1, sizeof(*req));
+	if (!req) {
+		SPDK_ERRLOG("Cannot alloc memory for lvol request pointer\n");
+		return -ENOMEM;
+	}
+	req->cb_fn = cb_fn;
+	req->cb_arg = cb_arg;
+
+	spdk_blob_set_xattr(blob, "name", new_name, strlen(new_name) + 1);
+
+	spdk_blob_sync_md(blob, _spdk_lvs_rename_cb, req);
+
+	return 0;
+
+}
+
+static void
 _lvs_unload_cb(void *cb_arg, int lvserrno)
 {
 	struct spdk_lvs_req *lvs_req = cb_arg;
