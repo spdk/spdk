05ddc14a7b6a727a6ef409b4fdb332652d35bd22
Sebastian Basierski <sebastianx.basierski@intel.com>
2017-12-20 16:02:49 +0100

    bdev: Added functions allowing logical volume store rename.
    Change-Id: I564af4acb0c613f1da09bfe18d85e0820a60bce2
    Signed-off-by: Sebastian Basierski <sebastianx.basierski@intel.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -357,6 +357,18 @@ p.add_argument('lvs_name', help='name for lvol store')
 p.add_argument('-c', '--cluster-sz', help='size of cluster (in bytes)', type=int, required=False)
 p.set_defaults(func=construct_lvol_store)
 
+def rename_lvol_store(args):
+    params = {
+        'old_lvs_name': args.old_lvs_name,
+        'new_lvs_name': args.new_lvs_name
+    }
+
+    print_array(jsonrpc_call('rename_lvol_store', params))
+p = subparsers.add_parser('rename_lvol_store', help='Change logical volume store name')
+p.add_argument('old_lvs_name', help='old lvs name')
+p.add_argument('new_lvs_name', help='new lvs name')
+p.set_defaults(func=rename_lvol_store)
+
 def construct_lvol_bdev(args):
     num_bytes = (args.size * 1024 * 1024)
     params = {'lvol_name': args.lvol_name, 'size': num_bytes}
