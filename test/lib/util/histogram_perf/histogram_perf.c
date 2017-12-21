ea17499c6c93dc44d50afe08fd583a123d5455bb
Jim Harris <james.r.harris@intel.com>
2017-12-21 12:27:19 -0700

    test: add util/histogram_perf test application
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I050ecd4f3bbccbd527387d080ce595fae16479d3
    
    Reviewed-on: https://review.gerrithub.io/392704
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

---
+++ test/lib/util/histogram_perf/histogram_perf.c
@@ -0,0 +1,97 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "spdk/env.h"
+#include "spdk/util.h"
+#include "spdk/histogram_data.h"
+
+/*
+ * This applications is a simple test app used to test the performance of
+ *  tallying datapoints with struct spdk_histogram_data.  It can be used
+ *  to measure the effect of changes to the spdk_histogram_data implementation.
+ *
+ * There are no command line parameters currently - it just tallies
+ *  datapoints for 10 seconds in a default-sized histogram structure and
+ *  then prints out the number of tallies performed.
+ */
+
+static void
+usage(const char *prog)
+{
+	printf("usage: %s\n", prog);
+	printf("Options:\n");
+}
+
+int
+main(int argc, char **argv)
+{
+	struct spdk_histogram_data h;
+	struct spdk_env_opts opts;
+	uint64_t tsc[128], t, end_tsc, count;
+	uint32_t i;
+	int ch;
+	int rc = 0;
+
+	while ((ch = getopt(argc, argv, "")) != -1) {
+		switch (ch) {
+		default:
+			usage(argv[0]);
+			return 1;
+		}
+	}
+
+	spdk_env_opts_init(&opts);
+	spdk_env_init(&opts);
+
+	for (i = 0; i < SPDK_COUNTOF(tsc); i++) {
+		tsc[i] = spdk_get_ticks();
+	}
+
+	end_tsc = spdk_get_ticks() + (10 * spdk_get_ticks_hz());
+	count = 0;
+	spdk_histogram_data_reset(&h);
+
+	while (true) {
+		t = spdk_get_ticks();
+		spdk_histogram_data_tally(&h, t - tsc[count % 128]);
+		count++;
+		if (t > end_tsc) {
+			break;
+		}
+	}
+
+	printf("count = %ju\n", count);
+	return rc;
+}
