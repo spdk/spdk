7830b0da53583a42f09077174ec2d112d4929133
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2017-12-21 15:18:37 +0100

    test/virtio: Add vhost initiator tests for pci mode
    Change-Id: If07645317020f9f0ba775de146b044fc2ed4f9ae
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/vhost/initiator/blockdev.sh
+++ test/vhost/initiator/blockdev.sh
@@ -3,6 +3,7 @@
 set -e
 BASE_DIR=$(readlink -f $(dirname $0))
 [[ -z "$COMMON_DIR" ]] && COMMON_DIR="$(cd $BASE_DIR/../common && pwd)"
+[[ -z "$TEST_DIR" ]] && TEST_DIR="$(cd $BASE_DIR/../../../../ && pwd)"
 ROOT_DIR=$(readlink -f $BASE_DIR/../../..)
 
 PLUGIN_DIR=$ROOT_DIR/examples/bdev/fio_plugin
@@ -11,14 +12,16 @@ FIO_BIN="/usr/src/fio/fio"
 BDEV_FIO="$BASE_DIR/bdev.fio"
 virtio_bdevs=""
 virtio_nvme_bdevs=""
+os_image="/home/sys_sgsw/vhost_vm_image.qcow2"
 
 function usage()
 {
 	[[ ! -z $2 ]] && ( echo "$2"; echo ""; )
 	echo "Script for running vhost initiator tests."
 	echo "Usage: $(basename $1) [-h|--help] [--fiobin=PATH]"
-	echo "-h, --help         Print help and exit"
-	echo "    --fiobin=PATH  Path to fio binary on host [default=/usr/src/fio/fio]"
+	echo "-h, --help           Print help and exit"
+	echo "    --fiobin=PATH    Path to fio binary on host [default=/usr/src/fio/fio]"
+	echo "    --vm_image=PATH  Path to vm image [default=/home/sys_sgsw/vhost_vm_image.qcow2]"
 }
 
 while getopts 'h-:' optchar; do
@@ -27,6 +30,7 @@ while getopts 'h-:' optchar; do
 		case "$OPTARG" in
 			help) usage $0 && exit 0 ;;
 			fiobin=*) FIO_BIN="${OPTARG#*=}" ;;
+			vm_img=*) os_image="${OPTARG#*=}" ;;
 			*) usage $0 echo "Invalid argument '$OPTARG'" && exit 1 ;;
 		esac
 		;;
@@ -38,7 +42,11 @@ done
 source $COMMON_DIR/common.sh
 
 if [ ! -x $FIO_BIN ]; then
-	error "Invalid path of fio binary"
+	error "Invalid path to fio binary"
+fi
+
+if [ ! -f $os_image ]; then
+	error "Invalid path to vm image"
 fi
 
 if [[ $EUID -ne 0 ]]; then
@@ -95,6 +103,19 @@ timing_exit create_bdev_config
 timing_enter run_spdk_fio
 run_spdk_fio $BDEV_FIO --filename=$virtio_bdevs --spdk_conf=$BASE_DIR/bdev.conf
 timing_exit run_spdk_fio
+
+vm_num="0"
+disk="Nvme0n1"
+setup_cmd="$COMMON_DIR/vm_setup.sh --work-dir=$TEST_DIR --test-type=spdk_vhost_scsi"
+setup_cmd+=" -f $vm_num --os=$os_image --disk=$disk"
+$setup_cmd
+$COMMON_DIR/vm_run.sh $x --work-dir=$TEST_DIR $vm_num
+vm_wait_for_boot 600 $vm_num
+vm_scp $vm_num -r $ROOT_DIR "127.0.0.1:/root/spdk"
+vm_ssh $vm_num " cd spdk ; make clean ; ./configure --with-fio=/root/fio_src ; make -j3"
+
+vm_ssh $vm_num "/root/spdk/test/vhost/initiator/test.sh"
+vm_ssh $vm_num "rm -f *.state"
 
 rm -f *.state
 timing_enter spdk_vhost_kill
