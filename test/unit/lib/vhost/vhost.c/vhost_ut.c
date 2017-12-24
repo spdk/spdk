6f38e5b75cb608217a23ca42b701e06f58e93048
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2017-12-25 08:26:52 +0900

    vhost: Remove DPDK dependency and simplify load balancing
    The latest patch for the iSCSI connection is applied to the vhost
    too.
    
    RTE_MAX_LCORE in the for loop is removed and the for loop is
    replaced by SPDK_ENV_FOREACH_CORE().
    
    When the cpumask is unexpectedly 0, not 0 but the first core is
    returned.
    
    Change-Id: I39cfc2219a3532eccc8c0ce59712102b947a76d7
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/unit/lib/vhost/vhost.c/vhost_ut.c
+++ test/unit/lib/vhost/vhost.c/vhost_ut.c
@@ -48,6 +48,8 @@ DEFINE_STUB(spdk_mem_register, int, (void *vaddr, size_t len), 0);
 DEFINE_STUB(spdk_mem_unregister, int, (void *vaddr, size_t len), 0);
 DEFINE_STUB(spdk_app_get_core_mask, uint64_t, (void), 0);
 DEFINE_STUB(spdk_app_parse_core_mask, int, (const char *mask, uint64_t *cpumask), 0);
+DEFINE_STUB(spdk_env_get_first_core, uint32_t, (void), 0);
+DEFINE_STUB(spdk_env_get_next_core, uint32_t, (uint32_t prev_core), 0);
 DEFINE_STUB_V(spdk_app_stop, (int rc));
 DEFINE_STUB_V(spdk_event_call, (struct spdk_event *event));
 DEFINE_STUB(spdk_poller_register, struct spdk_poller *, (spdk_poller_fn fn, void *arg,
