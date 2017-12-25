23db0b85fd2f5b83128a656d5a6837374b2bf33e
Yanbo Zhou <yanbo.zhou@intel.com>
2017-12-25 14:55:53 +0800

    include/fd.h: add comments for pubclic APIs
    Change-Id: I3a23e6c6b640428260ebf300ded9b8cdac501ef4
    Signed-off-by: Yanbo Zhou <yanbo.zhou@intel.com>
    Reviewed-on: https://review.gerrithub.io/392917
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: GangCao <gang.cao@intel.com>

--- include/spdk/fd.h
+++ include/spdk/fd.h
@@ -44,7 +44,22 @@
 extern "C" {
 #endif
 
+/**
+ * Get the file size.
+ *
+ * \param fd  File descriptor.
+ *
+ * \return    File size.
+ */
 uint64_t spdk_fd_get_size(int fd);
+
+/**
+ * Get the block size of the file.
+ *
+ * \param fd  File descriptor.
+ *
+ * \return    Block size.
+ */
 uint32_t spdk_fd_get_blocklen(int fd);
 
 #ifdef __cplusplus
