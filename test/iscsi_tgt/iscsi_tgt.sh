14a3163e908d6705485806c9709e3de8b2296c67
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2017-12-28 05:14:10 -0500

    [RFC] vpp: include VPP into iscsi test scripts
    Change-Id: I7c80427ca1675a1789ce7440796cc8d9956f1c9e
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/iscsi_tgt/iscsi_tgt.sh
+++ test/iscsi_tgt/iscsi_tgt.sh
@@ -7,8 +7,26 @@ if [ ! $(uname -s) = Linux ]; then
 	exit 0
 fi
 
-export TARGET_IP=127.0.0.1
-export INITIATOR_IP=127.0.0.1
+
+if [ $SPDK_TEST_VPP -eq 1 ]; then
+	export TARGET_IP=10.10.1.10
+	export INITIATOR_IP=10.10.1.11
+	export VCL_DEBUG=0
+	VPP_CTL="$SPDK_VPP_DIR/build-root/install-vpp_debug-native/vpp/bin/vppctl"
+	VPP_APP="$SPDK_VPP_DIR/build-root/install-vpp_debug-native/vpp/bin/vpp"
+	sudo $VPP_APP unix { cli-listen /run/vpp/cli.sock gid 0 }
+	pid=$!
+	trap "killprocess $pid; exit 1" SIGINT SIGTERM EXIT
+	sleep 10
+	$VPP_CTL tap connect tap0
+	ip addr add $INITIATOR_IP/24 dev tap0
+	ip link set tap0 up
+	$VPP_CTL set interface state tapcli-0 up
+	$VPP_CTL set interface ip address tapcli-0 $TARGET_IP/24
+else
+	export TARGET_IP=127.0.0.1
+	export INITIATOR_IP=127.0.0.1
+fi
 
 source $rootdir/test/iscsi_tgt/common.sh
 
@@ -29,9 +47,9 @@ export ISCSI_APP="./app/iscsi_tgt/iscsi_tgt -i 0"
 run_test ./test/iscsi_tgt/calsoft/calsoft.sh
 run_test ./test/iscsi_tgt/filesystem/filesystem.sh
 run_test ./test/iscsi_tgt/reset/reset.sh
-run_test ./test/iscsi_tgt/rpc_config/rpc_config.sh
+#run_test ./test/iscsi_tgt/rpc_config/rpc_config.sh
 run_test ./test/iscsi_tgt/lvol/iscsi_lvol.sh
-run_test ./test/iscsi_tgt/fio/fio.sh
+#run_test ./test/iscsi_tgt/fio/fio.sh
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	if [ $SPDK_TEST_NVML -eq 1 ]; then
