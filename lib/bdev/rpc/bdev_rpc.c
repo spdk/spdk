a5b971c9f31b50f59ebb2f7e6e0cb0c5fc930238
GangCao <gang.cao@intel.com>
2017-12-30 19:00:49 -0500

    bdev/qos: add the RPC support to adjust QoS setting at runtime
    This patch is to add the RPC method to adjust the QoS setting
    at the runtime. The bdev needs to be already QoS enabled.
    
    Otherwise, it needs to call the spdk_bdev_enable_qos() method
    first before calling this method.
    
    Change-Id: I84bdc88800924646913b83425eb04c8adbd0bfc7
    Signed-off-by: GangCao <gang.cao@intel.com>

--- lib/bdev/rpc/bdev_rpc.c
+++ lib/bdev/rpc/bdev_rpc.c
@@ -395,3 +395,102 @@ exit:
 }
 
 SPDK_RPC_REGISTER("disable_bdev_qos", spdk_rpc_disable_bdev_qos)
+
+
+struct rpc_adjust_bdev_qos {
+	char		*name;
+	uint64_t	ios_per_sec;
+};
+
+static void
+free_rpc_adjust_bdev_qos(struct rpc_adjust_bdev_qos *r)
+{
+	free(r->name);
+}
+
+static const struct spdk_json_object_decoder rpc_adjust_bdev_qos_decoders[] = {
+	{"name", offsetof(struct rpc_adjust_bdev_qos, name), spdk_json_decode_string},
+	{"ios_per_sec", offsetof(struct rpc_adjust_bdev_qos, ios_per_sec), spdk_json_decode_uint64},
+};
+
+static void
+_spdk_rpc_adjust_bdev_qos_complete(void *cb_arg, int bdeverrno)
+{
+	struct spdk_jsonrpc_request *request = cb_arg;
+	struct spdk_json_write_ctx *w;
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w == NULL) {
+		return;
+	}
+
+	spdk_json_write_bool(w, bdeverrno == 0);
+	spdk_jsonrpc_end_result(request, w);
+}
+
+static void
+spdk_rpc_adjust_bdev_qos(struct spdk_jsonrpc_request *request,
+			 const struct spdk_json_val *params)
+{
+	struct rpc_adjust_bdev_qos	req = {};
+	struct spdk_bdev		*bdev;
+	int				rc = 0;
+
+	if (spdk_json_decode_object(params, rpc_adjust_bdev_qos_decoders,
+				    sizeof(rpc_adjust_bdev_qos_decoders) /
+				    sizeof(*rpc_adjust_bdev_qos_decoders),
+				    &req)) {
+		SPDK_ERRLOG("%s %lu\n", req.name, req.ios_per_sec);
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		goto invalid;
+	}
+
+	if (req.name == NULL) {
+		SPDK_ERRLOG("missing name param\n");
+		goto invalid;
+	}
+
+	bdev = spdk_bdev_get_by_name(req.name);
+	if (bdev == NULL) {
+		SPDK_ERRLOG("bdev '%s' does not exist\n", req.name);
+		goto invalid;
+	}
+
+	if (bdev->ios_per_sec == 0) {
+		SPDK_ERRLOG("QoS not enabled on this bdev '%s'\n", req.name);
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_REQUEST,
+						 "QoS not enabled");
+		goto exit;
+	}
+
+	if (bdev->ios_per_sec == req.ios_per_sec) {
+		SPDK_ERRLOG("same QoS configuration enabled on this bdev '%s'\n", req.name);
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_REQUEST,
+						 "same QoS configuration");
+		goto exit;
+	}
+
+	if (req.ios_per_sec < 1000) {
+		SPDK_ERRLOG("At least 1000 for expected IOs per sec (%lu)\n", req.ios_per_sec);
+		goto invalid;
+	}
+
+	if (req.ios_per_sec % 1000) {
+		SPDK_ERRLOG("Expected IOs per sec (%lu) is not multiple of 1000\n", req.ios_per_sec);
+		goto invalid;
+	}
+
+	rc = spdk_bdev_adjust_qos(bdev, req.ios_per_sec);
+	_spdk_rpc_adjust_bdev_qos_complete(request, rc);
+	free_rpc_adjust_bdev_qos(&req);
+
+	return;
+
+invalid:
+	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, "Invalid parameters");
+
+exit:
+	free_rpc_adjust_bdev_qos(&req);
+}
+
+SPDK_RPC_REGISTER("adjust_bdev_qos", spdk_rpc_adjust_bdev_qos)
