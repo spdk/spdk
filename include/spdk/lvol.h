df110bb54940954a426295164c254999221c020f
Maciej Szwed <maciej.szwed@intel.com>
2018-01-04 15:00:00 +0100

    lvol: enable creation of thin provisioned lvols
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I03caa59b6818506f924ede4ad2d256431e139e65

--- include/spdk/lvol.h
+++ include/spdk/lvol.h
@@ -143,12 +143,13 @@ int spdk_lvs_destroy(struct spdk_lvol_store *lvol_store,
  * \param lvs Handle to lvolstore
  * \param name Name of lvol
  * \param sz size of lvol in bytes
+ * \param thin_provisioned Enables thin provisioning
  * \param cb_fn Completion callback
  * \param cb_arg Completion callback custom arguments
  * \return error
  */
 int spdk_lvol_create(struct spdk_lvol_store *lvs, const char *name, uint64_t sz,
-		     spdk_lvol_op_with_handle_complete cb_fn, void *cb_arg);
+		     bool thin_provisioned, spdk_lvol_op_with_handle_complete cb_fn, void *cb_arg);
 
 /**
  * \brief Closes lvol and removes information about lvol from its lvolstore.
