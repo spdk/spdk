2fd36c9b254b9fcb704aabdccee00da6c5416c9f
Jim Harris <james.r.harris@intel.com>
2018-01-04 12:42:41 -0700

    pkgdep.sh: add sg3_utils
    This is used for testing - not as part of the build - but we
    already have astyle and pep8 in pkgdep.sh so adding sg3_utils
    seems OK.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I3826fc0395c7d45f0308477e850d21ba87afec45
    
    Reviewed-on: https://review.gerrithub.io/393693
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>

--- scripts/pkgdep.sh
+++ scripts/pkgdep.sh
@@ -10,7 +10,8 @@ if [ -s /etc/redhat-release ]; then
 		yum --enablerepo=extras install -y epel-release
 	fi
 	yum install -y gcc gcc-c++ make CUnit-devel libaio-devel openssl-devel \
-		git astyle-devel python-pep8 lcov python clang-analyzer libuuid-devel
+		git astyle-devel python-pep8 lcov python clang-analyzer libuuid-devel \
+		sg3_utils
 	# Additional dependencies for NVMe over Fabrics
 	yum install -y libibverbs-devel librdmacm-devel
 	# Additional dependencies for DPDK
@@ -22,7 +23,7 @@ if [ -s /etc/redhat-release ]; then
 elif [ -f /etc/debian_version ]; then
 	# Includes Ubuntu, Debian
 	apt-get install -y gcc g++ make libcunit1-dev libaio-dev libssl-dev \
-		git astyle pep8 lcov clang uuid-dev
+		git astyle pep8 lcov clang uuid-dev sg3-utils
 	# Additional dependencies for NVMe over Fabrics
 	apt-get install -y libibverbs-dev librdmacm-dev
 	# Additional dependencies for DPDK
@@ -31,7 +32,7 @@ elif [ -f /etc/debian_version ]; then
 	apt-get install -y doxygen mscgen
 elif [ $SYSTEM = "FreeBSD" ] ; then
 	pkg install gmake cunit openssl git devel/astyle bash devel/pep8 \
-		python misc/e2fsprogs-libuuid
+		python misc/e2fsprogs-libuuid sysutils/sg3_utils
 	# Additional dependencies for building docs
 	pkg install doxygen mscgen
 else
