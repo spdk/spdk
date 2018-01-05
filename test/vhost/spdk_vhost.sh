65b75e79c2109c68319a0752f018ca5c5d4792a0
Karol Latecki <karol.latecki@intel.com>
2018-01-05 13:21:48 +0100

    test/vhost: run vhost tests with thin provisioned lvol bdevs
    Change-Id: I004e6ebae38ec923817410f7ad530dd707a307b1
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- test/vhost/spdk_vhost.sh
+++ test/vhost/spdk_vhost.sh
@@ -99,12 +99,12 @@ case $1 in
 	-ils|--integrity-lvol-scsi)
 		echo 'Running lvol integrity suite...'
 		./lvol/lvol_test.sh -x --fio-bin=$FIO_BIN \
-		--ctrl-type=spdk_vhost_scsi
+		--ctrl-type=spdk_vhost_scsi --thin-provisioning
 		;;
 	-ilb|--integrity-lvol-blk)
 		echo 'Running lvol integrity suite...'
 		./lvol/lvol_test.sh -x --fio-bin=$FIO_BIN \
-		--ctrl-type=spdk_vhost_blk
+		--ctrl-type=spdk_vhost_blk --thin-provisioning
 		;;
 	-ilsn|--integrity-lvol-scsi-nightly)
 		if [[ $DISKS_NUMBER -ge 2 ]]; then
