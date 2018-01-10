074f4d7627ea38cf74c1cd680429ba64c4757b76
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-01-10 16:46:10 +0100

    blob: set md_ro and data_ro fields only after persisting data
    Currently, there is no possibility to save read only blob to disk.
    This patch modifies behaviour so that read only flags are applied after syncing blob.
    This is analogy to resize, set xattr and remove xattr operations.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: Iffed601c78cb83231bb20e7ef05b73847dc3c95a
    Reviewed-on: https://review.gerrithub.io/394243
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>

--- include/spdk/blob.h
+++ include/spdk/blob.h
@@ -245,7 +245,11 @@ void spdk_bs_open_blob(struct spdk_blob_store *bs, spdk_blob_id blobid,
  * spdk_blob_sync_md() is called. */
 int spdk_blob_resize(struct spdk_blob *blob, size_t sz);
 
-/* Set blob as read only */
+/* Set blob as read only.
+ *
+ * These changes do not take effect until
+ * spdk_blob_sync_md() is called. */
+
 void spdk_blob_set_read_only(struct spdk_blob *blob);
 
 /* Sync a blob */
--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -3125,8 +3125,6 @@ void spdk_blob_set_read_only(struct spdk_blob *b)
 {
 	struct spdk_blob_data *blob = __blob_to_data(b);
 
-	blob->data_ro = true;
-	blob->md_ro = true;
 	blob->data_ro_flags |= SPDK_BLOB_READ_ONLY;
 
 	blob->state = SPDK_BLOB_STATE_DIRTY;
@@ -3138,6 +3136,13 @@ void spdk_blob_set_read_only(struct spdk_blob *b)
 static void
 _spdk_blob_sync_md_cpl(spdk_bs_sequence_t *seq, void *cb_arg, int bserrno)
 {
+	struct spdk_blob_data *blob = __blob_to_data(cb_arg);
+
+	if (bserrno == 0 && (blob->data_ro_flags & SPDK_BLOB_READ_ONLY)) {
+		blob->data_ro = true;
+		blob->md_ro = true;
+	}
+
 	spdk_bs_sequence_finish(seq, bserrno);
 }
 
