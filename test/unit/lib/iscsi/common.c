6c355cf66a37e041aeb1de0e811f7473a88b3682
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-01-10 14:55:53 +0900

    scsi: remove lun name
    There is no need to keep a lun name anymore - we always
    use the bdev name as the lun name so it is not providing any
    additional value. This also keeps us from associating
    the same bdev with different LUNs on different iSCSI target
    nodes or vhost-scsi controllers.
    
    Side effect of this change is:
    
    1) Use "bdev_name" across the APIs to make it more clear
       what these names refer to.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: I3d42fde22087352ce1d5dc80178bd8c5cac8cb7c

--- test/unit/lib/iscsi/common.c
+++ test/unit/lib/iscsi/common.c
@@ -113,7 +113,7 @@ spdk_event_allocate(uint32_t core, spdk_event_fn fn, void *arg1, void *arg2)
 }
 
 struct spdk_scsi_dev *
-	spdk_scsi_dev_construct(const char *name, char **lun_name_list,
+	spdk_scsi_dev_construct(const char *name, const char **bdev_name_list,
 			int *lun_id_list, int num_luns, uint8_t protocol_id,
 			void (*hotremove_cb)(const struct spdk_scsi_lun *, void *),
 			void *hotremove_ctx)
