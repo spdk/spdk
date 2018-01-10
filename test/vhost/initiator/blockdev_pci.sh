8dffe7beb31f792caacdee7df6af41f7b2b1f2c8
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-01-10 20:57:13 +0100

    test/virtio: Add vhost initiator tests for pci mode
    new tests are performed on guest vm where vhost scsi
    controllers are presented as a virtual pci device.
    some changes had to made to common.sh to allow to build
    spdk on vm and set queue number.
    
    Change-Id: I8289d6aede6020e958c9e5aae893591d844cdbcf
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

---
+++ test/vhost/initiator/blockdev_pci.sh
@@ -0,0 +1,23 @@
+
+virtio_bdevs=""
+/root/spdk/scripts/setup.sh
+. /root/spdk/scripts/autotest_common.sh
+
+vbdevs=$(discover_bdevs /root/spdk /root/spdk/test/vhost/initiator/bdev_pci.conf)
+virtio_bdevs=$(jq -r '[.[].name] | join(":")' <<< $vbdevs)
+virtio_with_unmap=$(jq -r '[.[] | select(.supported_io_types.unmap==true).name]
+ | join(":")' <<< $vbdevs)
+
+timing_enter run_spdk_fio
+LD_PRELOAD=/root/spdk/examples/bdev/fio_plugin/fio_plugin /root/fio_src/fio --ioengine=spdk_bdev \
+ /root/spdk/test/vhost/initiator/bdev.fio --filename=$virtio_bdevs --section=job_randwrite \
+ --section=job_randrw --section=job_write --section=job_rw \
+ --spdk_conf=/root/spdk/test/vhost/initiator/bdev_pci.conf --spdk_mem=1024
+timing_exit run_spdk_fio
+
+timing_enter run_spdk_fio_unmap
+LD_PRELOAD=/root/spdk/examples/bdev/fio_plugin/fio_plugin /root/fio_src/fio --ioengine=spdk_bdev \
+ /root/spdk/test/vhost/initiator/bdev.fio --filename=$virtio_with_unmap --section=job_randwrite \
+ --section=job_randrw --section=job_unmap_trim_sequential --section=job_unmap_trim_random \
+ --section=job_unmap_write --spdk_conf=/root/spdk/test/vhost/initiator/bdev_pci.conf --spdk_mem=1024
+timing_exit run_spdk_fio_unmap
