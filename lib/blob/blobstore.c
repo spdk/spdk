5f6ad5d10da687adb43bb59faa694a9e60a9867a
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-01-10 16:46:10 +0100

    blob: add clone
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: Iad67be79d0ddd8c498950c4f7b1b3203e47a7a41

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -3595,6 +3595,34 @@ void spdk_bs_create_blob_snapshot(struct spdk_blob *blob,
 }
 /* END spdk_bs_create_blob_snapshot */
 
+/* START spdk_bs_create_blob_clone */
+
+void spdk_bs_create_blob_clone(struct spdk_blob *blob,
+			       spdk_blob_op_with_id_complete cb_fn, void *cb_arg)
+{
+	struct spdk_blob_opts   opts;
+	struct spdk_bs_cpl		cpl;
+	struct spdk_blob_data	*snapshot = __blob_to_data(blob);
+	struct spdk_bs_dev		*bs_dev = NULL;
+
+	if (!snapshot->data_ro || !snapshot->md_ro) {
+		cb_fn(cb_arg, SPDK_BLOBID_INVALID, -EINVAL);
+		return;
+	}
+
+	bs_dev = _create_snapshot_bs_dev(__data_to_blob(snapshot));
+
+	spdk_blob_opts_init(&opts);
+	opts.thin_provision = true;
+	opts.num_clusters = spdk_blob_get_num_pages(blob);
+
+	cpl.u.blobid.cb_fn = cb_fn;
+	cpl.u.blobid.cb_arg = cb_arg;
+
+	_spdk_bs_create_blob(snapshot->bs, &opts, bs_dev, cb_fn, &cpl);
+}
+/* END spdk_bs_create_blob_clone */
+
 /* START spdk_blob_resize */
 int
 spdk_blob_resize(struct spdk_blob *_blob, uint64_t sz)
