a1a07f0866a8435c90ea038dd7e42ac58c1f029d
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-01-23 20:11:52 +0100

    test/virtio: test qemu virtio-scsi-pci device
    test emulated virtio-scsi-pci device
    
    Change-Id: Id7eaaabb5f7117cb72010345a9c892d81334e8cf
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/vhost/initiator/blockdev.sh
+++ test/vhost/initiator/blockdev.sh
@@ -104,7 +104,8 @@ timing_exit run_spdk_fio_unmap
 
 timing_enter setup_vm
 vm_no="0"
-vm_setup --disk-type=spdk_vhost_scsi --force=$vm_no --os=$os_image --disks="Nvme0n1:Malloc0:Malloc1" --queue_num=18 --memory=6144
+vm_setup --disk-type=spdk_vhost_scsi --force=$vm_no --os=$os_image --disks="Nvme0n1:Malloc0:Malloc1:,virtio" \
+ --queue_num=24 --memory=6144
 vm_run $vm_no
 vm_wait_for_boot 600 $vm_no
 vm_scp $vm_num -r $ROOT_DIR "127.0.0.1:/root/spdk"
