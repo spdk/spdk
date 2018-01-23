2622fa3f5db25708a13f0fca57835629e145db9c
Ben Walker <benjamin.walker@intel.com>
2018-01-23 15:03:38 -0700

    nvmf: Add an rpc to add allowed hosts
    Remove the requirement to set up hosts on subsystem
    creation. This is an API breaking change.
    
    Change-Id: I5c98658f949dad013165c04497cca49867022ba0
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/nvmf/shutdown/shutdown.sh
+++ test/nvmf/shutdown/shutdown.sh
@@ -34,8 +34,9 @@ timing_exit start_nvmf_tgt
 for i in `seq 1 10`
 do
 	bdevs="$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
-	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode${i} '' -a -s SPDK${i} -n "$bdevs"
+	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode${i} -s SPDK${i} -n "$bdevs"
 	$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode${i} -t RDMA -a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT
+	$rpc_py nvmf_subsystem_add_host nqn.2016-06.io.spdk:cnode${i} ANY
 done
 
 # Kill nvmf tgt without removing any subsystem to check whether it can shutdown correctly
