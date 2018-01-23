7220ae2a916a307c0f1f1b5c275c99c5e525dcc9
Seth Howell <seth.howell@intel.com>
2018-01-23 13:14:17 -0700

    test/nvmf: add null pointer check to subsystem test
    Fedora 27 ships with Clang 5 which throws an erroneous error in the
    add_ns subsystem test. It believes that subsystem.ns is a null pointer.
    In order to circumvent this error, I add an assert to bail out of the
    test if the pointer really is null and then only execute the final
    assert if the pointer is not null. This way our tests will still pass
    Scan-build tests on Fedora27.
    
    Change-Id: I54d3fae485d56033ba5eb23b0aa323480ae6a6e4
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/unit/lib/nvmf/subsystem.c/subsystem_ut.c
+++ test/unit/lib/nvmf/subsystem.c/subsystem_ut.c
@@ -210,7 +210,10 @@ test_spdk_nvmf_subsystem_add_ns(void)
 	/* NSID 1 is the first unused ID */
 	CU_ASSERT(nsid == 1);
 	CU_ASSERT(subsystem.max_nsid == 1);
-	CU_ASSERT(subsystem.ns[nsid - 1].bdev == &bdev1);
+	CU_ASSERT(subsystem.ns != NULL)
+	if (subsystem.ns != NULL) {
+		CU_ASSERT(subsystem.ns[nsid - 1].bdev == &bdev1);
+	}
 
 	/* Request a specific NSID */
 	nsid = spdk_nvmf_subsystem_add_ns(&subsystem, &bdev2, 5);
