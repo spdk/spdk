18a03979e30b35b48db5654e6b1ff26143397ac8
Maciej Szwed <maciej.szwed@intel.com>
2018-01-23 14:35:09 +0100

    blob: tweak for cluster_alloc_in_progress lock
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I777fc97b850446f5c2a601473358bf47b0c01790

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -1346,15 +1346,12 @@ static void
 _spdk_bs_allocate_and_copy_cluster(spdk_bs_sequence_t *seq, struct spdk_blob_data *blob,
 				   uint64_t lba)
 {
-	struct spdk_bs_request_set *set = (struct spdk_bs_request_set *)seq;
 	struct spdk_blob_copy_cluster_ctx *ctx;
 	uint64_t lfc = 0;
 	uint64_t page = _spdk_bs_dev_lba_to_page(blob->back_bs_dev, lba);
 	uint32_t cluster_start_page = _spdk_bs_page_to_cluster_start(blob, page);
 	uint64_t cluster_start_lba = _spdk_bs_dev_page_to_lba(blob->back_bs_dev, cluster_start_page);
 	int rc;
-
-	set->channel->cluster_alloc_in_progress = true;
 
 	rc = _spdk_bs_allocate_cluster(blob, _spdk_bs_page_to_cluster(blob->bs,
 				       cluster_start_lba * blob->back_bs_dev->blocklen / SPDK_BS_PAGE_SIZE), &lfc);
@@ -1716,10 +1713,13 @@ _spdk_blob_request_submit_rw_iov(struct spdk_blob *_blob, struct spdk_io_channel
 				cb_fn(cb_arg, -ENOMEM);
 			}
 			return;
+		} else if (!_spdk_bs_page_is_allocated(blob, offset) && !read) {
+			ch->cluster_alloc_in_progress = true;
 		}
 
 		seq = spdk_bs_sequence_start(_channel, &cpl);
 		if (!seq) {
+			ch->cluster_alloc_in_progress = false;
 			cb_fn(cb_arg, -ENOMEM);
 			return;
 		}
@@ -1735,6 +1735,7 @@ _spdk_blob_request_submit_rw_iov(struct spdk_blob *_blob, struct spdk_io_channel
 			if (_spdk_bs_page_is_allocated(blob, offset) == false) {
 				op = spdk_bs_user_op_alloc(_channel, &cpl, SPDK_BLOB_WRITEV, _blob, iov, iovcnt, offset, length);
 				if (op == NULL) {
+					ch->cluster_alloc_in_progress = false;
 					spdk_bs_sequence_finish(seq, -ENOMEM);
 					return;
 				}
