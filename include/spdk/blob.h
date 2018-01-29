de9d3d542b419df61d6d6d2c7db947875b48cb50
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-01-29 10:25:45 +0100

    blobstore: allow creating clones out of read-only snapshots
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>
    Change-Id: Iad67be79d0ddd8c498950c4f7b1b3203e47a7a41

--- include/spdk/blob.h
+++ include/spdk/blob.h
@@ -258,6 +258,14 @@ void spdk_bs_create_snapshot(struct spdk_blob_store *bs, spdk_blob_id blobid,
 			     const struct spdk_blob_xattr_opts *snapshot_xattrs,
 			     spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
 
+/* Create a clone of specified read-only blob.
+ * Structure clone_xattrs as well as anything it references (like e.g. names
+ * array) must be valid until the completion is called. */
+void spdk_bs_create_clone(struct spdk_blob_store *bs, spdk_blob_id blobid,
+			  const struct spdk_blob_xattr_opts *clone_xattrs,
+			  spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
+
+
 /* Delete an existing blob. */
 void spdk_bs_delete_blob(struct spdk_blob_store *bs, spdk_blob_id blobid,
 			 spdk_blob_op_complete cb_fn, void *cb_arg);
