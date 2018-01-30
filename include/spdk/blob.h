78e769601c6ae2280948002ca39131a8193532fb
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-01-30 13:07:49 +0100

    blobstore: add snapshot functionality
    This patch adds new feature of blobstore.
    New call creates a read-only snapshot of specified blob with provided options.
    
    NOTE:
    
    This patch doesn't cover recovery operation if snapshotting fails. This operation
    will be implemented and added later.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>
    Change-Id: I470ca13525638fa6df485d508b3adf71b6b69c0b

--- include/spdk/blob.h
+++ include/spdk/blob.h
@@ -248,6 +248,16 @@ void spdk_bs_create_blob_ext(struct spdk_blob_store *bs, const struct spdk_blob_
 void spdk_bs_create_blob(struct spdk_blob_store *bs,
 			 spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
 
+/* Create a read-only snapshot of specified blob with provided options.
+ * This will automatically sync specified blob.
+ * When operation is done, original blob is converted to the thin-provisioned
+ * blob with a newly created read-only snapshot set as a backing blob.
+ * Structure snapshot_xattrs as well as anything it references (like e.g. names
+ * array) must be valid until the completion is called. */
+void spdk_bs_create_snapshot(struct spdk_blob_store *bs, spdk_blob_id blobid,
+			     const struct spdk_blob_xattr_opts *snapshot_xattrs,
+			     spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
+
 /* Delete an existing blob. */
 void spdk_bs_delete_blob(struct spdk_blob_store *bs, spdk_blob_id blobid,
 			 spdk_blob_op_complete cb_fn, void *cb_arg);
