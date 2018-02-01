232b6d3510c91c02b811d39fed0d0bf1c1847397
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-02-01 17:50:52 +0900

    python/rpc: export/import JSON config file through JSON RPC
    This patch provides the basic function to dump SPDK config as
    JSON file from SPDK, and edit&import it to SPDK.
    
    This patch provides pretty print function too.
    
    Change-Id: Idd438d8e7aa3cf236af43c0e3807651111ed76b5
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

---
+++ scripts/rpc/jsonconf.py
@@ -0,0 +1,31 @@
+import json
+import os.path
+from client import print_dict
+
+def load(args):
+    filename = args.filename
+    with open(filename, 'r') as json_file:
+        conf = json.load(json_file)
+    params = conf.get('config', None)
+    method = conf.get('method', None)
+    args.client.call(method, params, verbose=args.verbose)
+
+
+def dump(args):
+    method = args.method
+    conf = args.client.call(method, verbose=args.verbose)
+    print_dict(conf)
+    filename = args.filename
+    if args.indent:
+        ind = int(args.indent)
+    else:
+        ind = None
+    if args.key_sep and args.item_sep:
+        ksep = args.key_sep
+        isep = args.item_sep
+        seps = (ksep, isep)
+    else:
+        seps = None
+    with open(filename, 'w') as json_file:
+        json.dump(conf, json_file, indent=ind, separators=seps)
+        
