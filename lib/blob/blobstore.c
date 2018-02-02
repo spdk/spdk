22189cdec0b9cf0f8af88137d8eb6be64d9053b9
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-02-02 12:56:14 +0100

    blobstore: [rfc] add inflate call
    This patch add inflate functionalty to blobstore.
    Inflate call can be used on thin provisioned blob or clones.
    Function allocates all unallocated clusters on specified blob,
    copies data from dependent blob (or fills clusters with zeros)
    and then removes that dependency.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: Ibff569e45b12068b2fb46557156be348b36c252b

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -3487,6 +3487,9 @@ _spdk_bs_cleanup_finish(void *cb_arg, int bserrno)
 	case SPDK_BS_CPL_TYPE_BLOBID:
 		cpl->u.blobid.cb_fn(cpl->u.blobid.cb_arg, cpl->u.blobid.blobid, ctx->bserrno);
 		break;
+	case SPDK_BS_CPL_TYPE_BLOB_BASIC:
+		cpl->u.blob_basic.cb_fn(cpl->u.blob_basic.cb_arg, ctx->bserrno);
+		break;
 	default:
 		assert(false);
 		break;
@@ -3824,6 +3827,90 @@ void spdk_bs_create_clone(struct spdk_blob_store *bs, spdk_blob_id blobid,
 }
 
 /* END spdk_bs_create_blob_clone */
+
+/* START spdk_bs_inflate_blob */
+
+static void
+_spdk_bs_inflate_blob_open_cpl(void *cb_arg, struct spdk_blob *_blob, int bserrno)
+{
+	struct spdk_double_blob_ctx	*ctx = (struct spdk_double_blob_ctx *)cb_arg;
+	struct spdk_blob_data		*orgblob = __blob_to_data(_blob);
+	uint64_t lfc; /* lowest free cluster */
+	uint64_t i;
+
+	if (bserrno != 0) {
+		ctx->bserrno = bserrno;
+		_spdk_bs_cleanup_finish(ctx, 0);
+		return;
+	}
+	ctx->orgblob.ptr = orgblob;
+
+	if (spdk_blob_is_thin_provisioned(orgblob) == false) {
+		ctx->bserrno = -EALREADY;
+		_spdk_bs_cleanup_orgblob(ctx, 0);
+		return;
+	}
+
+	/* Do two passes - one to verify that we can obtain enough clusters
+	 * and another to actually claim them.
+	 */
+	lfc = 0;
+	for (i = 0; i < orgblob->active.num_clusters; i++) {
+		if (orgblob->active.clusters[i] == 0) {
+			lfc = spdk_bit_array_find_first_clear(orgblob->bs->used_clusters, lfc);
+			if (lfc >= orgblob->bs->total_clusters) {
+				/* No more free clusters. Cannot satisfy the request */
+				ctx->bserrno = -ENOSPC;
+				_spdk_bs_cleanup_orgblob(ctx, 0);
+				return;
+			}
+			lfc++;
+		}
+	}
+
+	lfc = 0;
+	for (i = 0; i < orgblob->active.num_clusters; i++) {
+		if (orgblob->active.clusters[i] == 0) {
+			_spdk_bs_allocate_cluster(orgblob, i, &lfc, true);
+			/* TODO: copy contents of cluster */
+			/* use: _spdk_bs_allocate_and_copy_cluster()
+			 * with no additional i/o operations */
+		}
+	}
+	/* TODO: Postpone next operations to callback in _spdk_bs_allocate_and_copy_cluster() */
+
+	/* Unset thin provision */
+	orgblob->invalid_flags = orgblob->invalid_flags & ~SPDK_BLOB_THIN_PROV;
+
+	/* Destroy back_bs_dev */
+
+	orgblob->back_bs_dev->destroy(orgblob->back_bs_dev);
+	orgblob->back_bs_dev = NULL;
+
+	_spdk_bs_cleanup_orgblob(ctx, 0);
+
+}
+
+void spdk_bs_inflate_blob(struct spdk_blob_store *bs, spdk_blob_id blobid,
+			  spdk_blob_op_complete cb_fn, void *cb_arg)
+{
+	struct spdk_double_blob_ctx	*ctx = calloc(1, sizeof(*ctx));
+
+	if (!ctx) {
+		cb_fn(cb_arg, -ENOMEM);
+		return;
+	}
+	ctx->cpl.type = SPDK_BS_CPL_TYPE_BLOB_BASIC;
+	ctx->cpl.u.bs_basic.cb_fn = cb_fn;
+	ctx->cpl.u.bs_basic.cb_arg = cb_arg;
+	ctx->bserrno = 0;
+	ctx->orgblob.id = blobid;
+
+	spdk_bs_open_blob(bs, ctx->orgblob.id, _spdk_bs_inflate_blob_open_cpl, ctx);
+
+}
+
+/* END spdk_bs_inflate_blob */
 
 /* START spdk_blob_resize */
 int
