64d508e73f650e5b5fa525824e98e65804b48b01
Daniel Verkamp <daniel.verkamp@intel.com>
2018-02-02 13:23:08 -0700

    test/nvmf: connect kernel host in shutdown test
    Attempt to reproduce GitHub issue #233
    
    Change-Id: I6df334606e6db6c182eccac1f7a3d294dad990a9
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/nvmf/shutdown/shutdown.sh
+++ test/nvmf/shutdown/shutdown.sh
@@ -25,7 +25,7 @@ timing_enter start_nvmf_tgt
 $NVMF_APP -c $testdir/../nvmf.conf &
 pid=$!
 
-trap "killprocess $pid; exit 1" SIGINT SIGTERM EXIT
+trap "killprocess $pid; nvmfcleanup; exit 1" SIGINT SIGTERM EXIT
 
 waitforlisten $pid
 timing_exit start_nvmf_tgt
@@ -37,10 +37,18 @@ do
 	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode${i} "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:$NVMF_PORT" '' -a -s SPDK${i} -n "$bdevs"
 done
 
+# Connect kernel host to subsystems
+modprobe -v nvme-rdma
+for i in `seq 1 10`; do
+	nvme connect -t rdma -n "nqn.2016-06.io.spdk:cnode${i}" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
+done
+
 # Kill nvmf tgt without removing any subsystem to check whether it can shutdown correctly
 rm -f ./local-job0-0-verify.state
 
 trap - SIGINT SIGTERM EXIT
 
 killprocess $pid
+
+nvmfcleanup
 timing_exit shutdown
