ca88f46a2da828f47f231826cec9a748c8aa0e20
Daniel Verkamp <daniel.verkamp@intel.com>
2018-02-02 13:23:08 -0700

    test/nvmf: connect kernel host in shutdown test
    ATtempt to reproduce GitHub issue #233
    
    Change-Id: I6df334606e6db6c182eccac1f7a3d294dad990a9
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/398251
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/nvmf/shutdown/shutdown.sh
+++ test/nvmf/shutdown/shutdown.sh
@@ -25,16 +25,32 @@ timing_enter start_nvmf_tgt
 $NVMF_APP -c $testdir/../nvmf.conf &
 pid=$!
 
-trap "killprocess $pid; exit 1" SIGINT SIGTERM EXIT
+trap "killprocess $pid; nvmfcleanup; exit 1" SIGINT SIGTERM EXIT
 
 waitforlisten $pid
 timing_exit start_nvmf_tgt
 
-# Create 10 subsystems
-for i in `seq 1 10`
+num_subsystems=10
+# SoftRoce does not have enough queues available for
+# this test. Detect if we're using software RDMA.
+# If so, only use one subsystem.
+if check_ip_is_soft_roce "$NVMF_FIRST_TARGET_IP"; then
+	num_subsystems=1
+fi
+
+# Create subsystems
+for i in `seq 1 $num_subsystems`
 do
 	bdevs="$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
 	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode${i} "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:$NVMF_PORT" '' -a -s SPDK${i} -n "$bdevs"
+done
+
+modprobe -v nvme-rdma
+modprobe -v nvme-fabrics
+
+# Connect kernel host to subsystems
+for i in `seq 1 $num_subsystems`; do
+	nvme connect -t rdma -n "nqn.2016-06.io.spdk:cnode${i}" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
 done
 
 # Kill nvmf tgt without removing any subsystem to check whether it can shutdown correctly
@@ -43,4 +59,6 @@ rm -f ./local-job0-0-verify.state
 trap - SIGINT SIGTERM EXIT
 
 killprocess $pid
+
+nvmfcleanup
 timing_exit shutdown
