82453dc948bc3f3556a6a421cd42093376f26757
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-02-05 13:02:25 +0900

    iscsi: Consolidating checking uniqueness of IG into register/unregister
    Checking uniqueness of initiator group is done without mutex and
    before register/unregister. This is not thread-safe.
    
    This patch is a preparation to dynamic addition of initiator
    information to existing initiator groups.
    
    Change-Id: I434588765a49028bd71faec4d0b24465644bd6bf
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/iscsi/init_grp.h
+++ lib/iscsi/init_grp.h
@@ -61,8 +61,10 @@ struct spdk_iscsi_init_grp {
 int spdk_iscsi_init_grp_create_from_initiator_list(int tag,
 		int num_initiator_names, char **initiator_names,
 		int num_initiator_masks, char **initiator_masks);
-void spdk_iscsi_init_grp_release(struct spdk_iscsi_init_grp *ig);
+int spdk_iscsi_init_grp_register(struct spdk_iscsi_init_grp *ig);
+struct spdk_iscsi_init_grp *spdk_iscsi_init_grp_unregister(int tag);
 struct spdk_iscsi_init_grp *spdk_iscsi_init_grp_find_by_tag(int tag);
+void spdk_iscsi_init_grp_destroy(struct spdk_iscsi_init_grp *ig);
 int spdk_iscsi_init_grp_array_create(void);
 void spdk_iscsi_init_grp_array_destroy(void);
 
