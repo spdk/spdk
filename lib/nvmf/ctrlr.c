a4f82c476cf783505f1280b04fb6e652e7c20665
Changpeng Liu <changpeng.liu@intel.com>
2018-02-06 00:16:08 -0500

    nvmf: add Namespace attribute notice support
    Users can use RPC to add/remove a namespace to/from
    existing NVMe controller, SPDK NVMeoF target will
    generate an asynchronous event as an indication to
    host when asynchronous event request is available.
    
    While here, we also set the event with invalid log
    identifier, so that the host doesn't need to clear
    the event. Users can use Set Feature to disable
    such event.
    
    Change-Id: I93c4d752f552d3c86c53e80877aa61c093e167cc
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -146,12 +146,13 @@ spdk_nvmf_ctrlr_create(struct spdk_nvmf_subsystem *subsystem,
 
 	req->qpair->ctrlr = ctrlr;
 	TAILQ_INIT(&ctrlr->qpairs);
+	TAILQ_INIT(&ctrlr->notice_event_head);
 	ctrlr->num_qpairs = 0;
 	ctrlr->subsys = subsystem;
 	ctrlr->max_qpairs_allowed = tgt->opts.max_qpairs_per_ctrlr;
 
 	ctrlr->feat.keep_alive_timer.bits.kato = connect_cmd->kato;
-
+	ctrlr->feat.async_event_configuration.bits.ns_attr_notice = 1;
 	ctrlr->feat.volatile_write_cache.bits.wce = 1;
 
 	/* Subtract 1 for admin queue, 1 for 0's based */
@@ -780,6 +781,7 @@ spdk_nvmf_ctrlr_async_event_request(struct spdk_nvmf_request *req)
 {
 	struct spdk_nvmf_ctrlr *ctrlr = req->qpair->ctrlr;
 	struct spdk_nvme_cpl *rsp = &req->rsp->nvme_cpl;
+	struct spdk_nvmf_async_event *entry;
 
 	SPDK_DEBUGLOG(SPDK_LOG_NVMF, "Async Event Request\n");
 
@@ -792,6 +794,14 @@ spdk_nvmf_ctrlr_async_event_request(struct spdk_nvmf_request *req)
 	}
 
 	ctrlr->aer_req = req;
+	if (!TAILQ_EMPTY(&ctrlr->notice_event_head)) {
+		entry = TAILQ_FIRST(&ctrlr->notice_event_head);
+		TAILQ_REMOVE(&ctrlr->notice_event_head, entry, link);
+		rsp->cdw0 = entry->e.raw;
+		ctrlr->aer_req = NULL;
+		free(entry);
+		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+	}
 	return SPDK_NVMF_REQUEST_EXEC_STATUS_ASYNCHRONOUS;
 }
 
@@ -1333,4 +1343,54 @@ spdk_nvmf_ctrlr_process_fabrics_cmd(struct spdk_nvmf_request *req)
 		req->rsp->nvme_cpl.status.sc = SPDK_NVME_SC_INVALID_OPCODE;
 		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
 	}
+}
+
+int
+spdk_nvmf_ctrlr_async_event_ns_notice(struct spdk_nvmf_ctrlr *ctrlr)
+{
+	struct spdk_nvmf_request *req;
+	struct spdk_nvme_cpl *rsp;
+	struct spdk_nvmf_async_event *entry;
+	union spdk_nvme_async_event_completion event = {0};
+
+	/* Users may disable the event notification */
+	if (!ctrlr->feat.async_event_configuration.bits.ns_attr_notice) {
+		return 0;
+	}
+
+	event.bits.async_event_type = SPDK_NVME_ASYNC_EVENT_TYPE_NOTICE;
+	event.bits.async_event_info = SPDK_NVME_ASYNC_EVENT_NS_ATTR_CHANGED;
+	/* Alternatively, host may request Changed Namespace List log(04h)
+	 * to determine which namespaces have changed. While here, we
+	 * set invalid log page identifier to indicate that host doesn't
+	 * need to send such log page.
+	 */
+	event.bits.log_page_identifier = 0;
+
+	/* Queued the event */
+	if (!ctrlr->aer_req) {
+		/* Only Namespace attribute notice is
+		 * supported right now.
+		 */
+		if (!TAILQ_EMPTY(&ctrlr->notice_event_head)) {
+			return 0;
+		}
+		entry = calloc(1, sizeof(*entry));
+		if (!entry) {
+			return -1;
+		}
+		entry->e.raw = event.raw;
+		TAILQ_INSERT_TAIL(&ctrlr->notice_event_head, entry, link);
+		return 0;
+	}
+
+	req = ctrlr->aer_req;
+	rsp = &req->rsp->nvme_cpl;
+
+	rsp->cdw0 = event.raw;
+
+	spdk_nvmf_request_complete(req);
+	ctrlr->aer_req = NULL;
+
+	return 0;
 }
