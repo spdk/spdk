27ccabdb70e9128bbf7dc0363c409c28cdd6f8c8
chenlo2x <longx.o.chen@intel.com>
2018-02-09 14:28:30 +0800

    autotest: iscsi_fio_test (porting from nightly to autotest)
    This patch is used to test fio and hotplug cases over the nvme logical volume
    
    Change-Id: Ida8572c8c9ebe27bbbae9d79eaacb3cce4c3a10a
    Signed-off-by: chenlo2x <longx.o.chen@intel.com>

--- test/iscsi_tgt/fio/nvme_lvol.sh
+++ test/iscsi_tgt/fio/nvme_lvol.sh
@@ -32,6 +32,11 @@ function running_config() {
 	$fio_py 4096 1 randrw 5
 }
 
+function cleanup_lvol_config() {
+	$rpc_py delete_bdev "$lb_name" || true
+	$rpc_py destroy_lvol_store -l lvs0 || true
+}
+
 if [ -z "$TARGET_IP" ]; then
 	echo "TARGET_IP not defined in environment"
 	exit 1
@@ -51,8 +56,6 @@ PORT=3260
 INITIATOR_TAG=2
 INITIATOR_NAME=ANY
 NETMASK=$INITIATOR_IP/32
-MALLOC_BDEV_SIZE=64
-MALLOC_BLOCK_SIZE=4096
 
 rpc_py="python $rootdir/scripts/rpc.py"
 fio_py="python $rootdir/scripts/fio.py"
@@ -72,12 +75,9 @@ timing_exit start_iscsi_tgt
 
 $rpc_py add_portal_group 1 $TARGET_IP:$PORT
 $rpc_py add_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
-$rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE
-# "Malloc0:0" ==> use Malloc0 blockdev for LUN0
-# "1:2" ==> map PortalGroup1 to InitiatorGroup2
-# "64" ==> iSCSI queue depth 64
-# "-d" ==> disable CHAP authentication
-$rpc_py construct_target_node Target3 Target3_alias 'Malloc0:0' '1:2' 64 -d
+ls_guid=$($rpc_py construct_lvol_store HotInNvme0n1 lvs0)
+lb_name=$($rpc_py construct_lvol_bdev -u $ls_guid lbd0 8192)
+$rpc_py construct_target_node Target1 Target1_alias "$lb_name:0" 1:2 64 -d
 sleep 1
 
 iscsiadm -m discovery -t sendtargets -p $TARGET_IP:$PORT
@@ -105,25 +105,26 @@ fio_pid=$!
 
 sleep 3
 set +e
-$rpc_py delete_bdev 'Malloc0'
+$rpc_py delete_bdev "$lb_name"
 
 wait $fio_pid
 fio_status=$?
 
 if [ $fio_status -eq 0 ]; then
-       echo "iscsi hotplug test: fio successful - expected failure"
-       iscsicleanup
-       rm -f $testdir/iscsi.conf
-       killprocess $pid
-       exit 1
+	echo "iscsi hotplug test: fio successful - expected failure"
+	iscsicleanup
+	rm -f $testdir/iscsi.conf
+	killprocess $pid
+	exit 1
 else
-       echo "iscsi hotplug test: fio failed as expected"
+	echo "iscsi hotplug test: fio failed as expected"
 fi
 
 set -e
 
 iscsicleanup
-$rpc_py delete_target_node 'iqn.2016-06.io.spdk:Target3'
+$rpc_py delete_target_node 'iqn.2016-06.io.spdk:Target1'
+cleanup_lvol_config
 
 trap - SIGINT SIGTERM EXIT
 rm -f ./local-job*
