119ccada0a567852f76985a28f93bf00bab0ee24
Daniel Verkamp <daniel.verkamp@intel.com>
2018-02-12 15:46:36 -0700

    nvmf: add namespaces individually in construct
    This will allow us to specify individual namespace options in upcoming
    patches.
    
    Change-Id: Id3301a2313a1d7cbafac1f8a5bcd8d73eae4cd47
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/399522
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- app/nvmf_tgt/nvmf_tgt.h
+++ app/nvmf_tgt/nvmf_tgt.h
@@ -83,15 +83,10 @@ int spdk_nvmf_parse_conf(void);
 struct spdk_nvmf_subsystem *nvmf_tgt_create_subsystem(const char *name,
 		enum spdk_nvmf_subtype subtype, uint32_t num_ns);
 
-struct spdk_nvmf_ns_params {
-	char *bdev_name;
-	uint32_t nsid;
-};
-
 struct spdk_nvmf_subsystem *spdk_nvmf_construct_subsystem(const char *name,
 		int num_listen_addresses, struct rpc_listen_address *addresses,
 		int num_hosts, char *hosts[], bool allow_any_host,
-		const char *sn, size_t num_ns, struct spdk_nvmf_ns_params *ns_list);
+		const char *sn);
 
 int spdk_nvmf_tgt_start(struct spdk_app_opts *opts);
 
