spdk/spdk

commit 601bcbcf666516cd6a3e3a0b515d4ea477485187

util: extend cpumask to hold more than 64 cpus
Fixes github issue #218.

This patch introduces spdk_cpuset object to store and manipulate
the set of individual CPUs. The main objective of this object is
to replace cpumask declared as uint64_t and extend the limitation
of supported CPUs (lcores) above 64 CPUs.

spdk_cpuset is always allocated dynamically and accessed by opaque
pointer, what makes it easier to extend in the future without
breaking API/ABI.

This patch also extends parsing function allowing to set cpumask
using a list of cpus e.g. "[0-4,10,12]" sets mask of 0,1,2,3,4,10,12
as well as hexadecimal string with and without "0x" prefix.

Change-Id: I475c3ba7fab629021a22e03176e57e400dd24a49
Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>
Reviewed-on: https://review.gerrithub.io/390794
Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>

lib/vhost/vhost.c
@@ -495,7 +495,7 @@ spdk_vhost_dev_find(const char *ctrlr_name)
 }
 
 static int
-spdk_vhost_parse_core_mask(const char *mask, uint64_t *cpumask)
+spdk_vhost_parse_core_mask(const char *mask, struct spdk_cpuset *cpumask)
 {
 	int rc;
 
@@ -504,21 +504,19 @@ spdk_vhost_parse_core_mask(const char *mask, uint64_t *cpumask)
 	}
 
 	if (mask == NULL) {
-		*cpumask = spdk_app_get_core_mask();
+		spdk_cpuset_copy(cpumask, spdk_app_get_core_mask());
 		return 0;
 	}
 
-	*cpumask = 0;
-
 	rc = spdk_app_parse_core_mask(mask, cpumask);
-	if (rc != 0) {
+	if (rc < 0) {
 		SPDK_ERRLOG("invalid cpumask %s\n", mask);
 		return -1;
 	}
 
-	if (*cpumask == 0) {
-		SPDK_ERRLOG("no cpu is selected among reactor mask(=%jx)\n",
-			    spdk_app_get_core_mask());
+	if (spdk_cpuset_count(cpumask) == 0) {
+		SPDK_ERRLOG("no cpu is selected among reactor mask(=%s)\n",
+			    spdk_cpuset_fmt(spdk_app_get_core_mask()));
 		return -1;
 	}
 
@@ -532,7 +530,8 @@ spdk_vhost_dev_construct(struct spdk_vhost_dev *vdev, const char *name, const ch
 	unsigned ctrlr_num;
 	char path[PATH_MAX];
 	struct stat file_stat;
-	uint64_t cpumask;
+	struct spdk_cpuset *cpumask;
+	int rc;
 
 	assert(vdev);
 
@@ -541,15 +540,23 @@ spdk_vhost_dev_construct(struct spdk_vhost_dev *vdev, const char *name, const ch
 		return -EINVAL;
 	}
 
-	if (spdk_vhost_parse_core_mask(mask_str, &cpumask) != 0) {
-		SPDK_ERRLOG("cpumask %s is invalid (app mask is 0x%jx)\n",
-			    mask_str, spdk_app_get_core_mask());
-		return -EINVAL;
+	cpumask = spdk_cpuset_alloc();
+	if (!cpumask) {
+		SPDK_ERRLOG("spdk_cpuset_alloc failed\n");
+		return -1;
+	}
+
+	if (spdk_vhost_parse_core_mask(mask_str, cpumask) != 0) {
+		SPDK_ERRLOG("cpumask %s is invalid (app mask is 0x%s)\n",
+			    mask_str, spdk_cpuset_fmt(spdk_app_get_core_mask()));
+		rc = -EINVAL;
+		goto out;
 	}
 
 	if (spdk_vhost_dev_find(name)) {
 		SPDK_ERRLOG("vhost controller %s already exists.\n", name);
-		return -EEXIST;
+		rc = -EEXIST;
+		goto out;
 	}
 
 	for (ctrlr_num = 0; ctrlr_num < MAX_VHOST_DEVICES; ctrlr_num++) {
@@ -560,13 +567,15 @@ spdk_vhost_dev_construct(struct spdk_vhost_dev *vdev, const char *name, const ch
 
 	if (ctrlr_num == MAX_VHOST_DEVICES) {
 		SPDK_ERRLOG("Max controllers reached (%d).\n", MAX_VHOST_DEVICES);
-		return -ENOSPC;
+		rc = -ENOSPC;
+		goto out;
 	}
 
 	if (snprintf(path, sizeof(path), "%s%s", dev_dirname, name) >= (int)sizeof(path)) {
 		SPDK_ERRLOG("Resulting socket path for controller %s is too long: %s%s\n", name, dev_dirname,
 			    name);
-		return -EINVAL;
+		rc = -EINVAL;
+		goto out;
 	}
 
 	/* Register vhost driver to handle vhost messages. */
@@ -575,32 +584,37 @@ spdk_vhost_dev_construct(struct spdk_vhost_dev *vdev, const char *name, const ch
 			SPDK_ERRLOG("Cannot create a domain socket at path \"%s\": "
 				    "The file already exists and is not a socket.\n",
 				    path);
-			return -EIO;
+			rc = -EIO;
+			goto out;
 		} else if (unlink(path) != 0) {
 			SPDK_ERRLOG("Cannot create a domain socket at path \"%s\": "
 				    "The socket already exists and failed to unlink.\n",
 				    path);
-			return -EIO;
+			rc = -EIO;
+			goto out;
 		}
 	}
 
 	if (rte_vhost_driver_register(path, 0) != 0) {
 		SPDK_ERRLOG("Could not register controller %s with vhost library\n", name);
 		SPDK_ERRLOG("Check if domain socket %s already exists\n", path);
-		return -EIO;
+		rc = -EIO;
+		goto out;
 	}
 	if (rte_vhost_driver_set_features(path, backend->virtio_features) ||
 	    rte_vhost_driver_disable_features(path, backend->disabled_features)) {
 		SPDK_ERRLOG("Couldn't set vhost features for controller %s\n", name);
 
 		rte_vhost_driver_unregister(path);
-		return -EIO;
+		rc = -EIO;
+		goto out;
 	}
 
 	if (rte_vhost_driver_callback_register(path, &g_spdk_vhost_ops) != 0) {
 		rte_vhost_driver_unregister(path);
 		SPDK_ERRLOG("Couldn't register callbacks for controller %s\n", name);
-		return -EIO;
+		rc = -EIO;
+		goto out;
 	}
 
 	vdev->name = strdup(name);
@@ -628,6 +642,10 @@ spdk_vhost_dev_construct(struct spdk_vhost_dev *vdev, const char *name, const ch
 
 	SPDK_NOTICELOG("Controller %s: new controller added\n", vdev->name);
 	return 0;
+
+out:
+	spdk_cpuset_free(cpumask);
+	return rc;
 }
 
 int
@@ -662,6 +680,7 @@ spdk_vhost_dev_remove(struct spdk_vhost_dev *vdev)
 
 	free(vdev->name);
 	free(vdev->path);
+	spdk_cpuset_free(vdev->cpumask);
 	g_spdk_vhost_devices[ctrlr_num] = NULL;
 	return 0;
 }
@@ -687,15 +706,16 @@ spdk_vhost_dev_get_name(struct spdk_vhost_dev *vdev)
 	return vdev->name;
 }
 
-uint64_t
-spdk_vhost_dev_get_cpumask(struct spdk_vhost_dev *vdev)
+void
+spdk_vhost_dev_get_cpumask(struct spdk_vhost_dev *vdev, struct spdk_cpuset *cpumask)
 {
 	assert(vdev != NULL);
-	return vdev->cpumask;
+	assert(cpumask != NULL);
+	spdk_cpuset_copy(cpumask, vdev->cpumask);
 }
 
 static uint32_t
-spdk_vhost_allocate_reactor(uint64_t cpumask)
+spdk_vhost_allocate_reactor(struct spdk_cpuset *cpumask)
 {
 	uint32_t i, selected_core;
 	uint32_t min_ctrlrs;
@@ -704,7 +724,7 @@ spdk_vhost_allocate_reactor(uint64_t cpumask)
 	selected_core = spdk_env_get_first_core();
 
 	SPDK_ENV_FOREACH_CORE(i) {
-		if (!((1ULL << i) & cpumask)) {
+		if (!spdk_cpuset_get_cpu(cpumask, i)) {
 			continue;
 		}
 

include/spdk/cpuset.h
@@ -1,0 +1,155 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \file
+ * CPU set management functions
+ */
+
+#ifndef SPDK_CPUSET_H
+#define SPDK_CPUSET_H
+
+#include "spdk/stdinc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SPDK_CPUSET_SIZE 1024
+
+/**
+ * List of CPUs.
+ */
+struct spdk_cpuset;
+
+/**
+ * Allocate CPU set object.
+ *
+ * \return Allocated zeroed cpuset or NULL if fails.
+ */
+struct spdk_cpuset *spdk_cpuset_alloc(void);
+
+/**
+ * Free allocated CPU set.
+ *
+ * \param set CPU set to be freed.
+ */
+void spdk_cpuset_free(struct spdk_cpuset *set);
+
+/**
+ * Compare two CPU sets.
+ *
+ * \return True if both CPU sets are equal.
+ */
+bool spdk_cpuset_equal(const struct spdk_cpuset *set1, const struct spdk_cpuset *set2);
+
+/**
+ * Copy the content of CPU set to another.
+ *
+ * \param dst Destination CPU set
+ * \param src Source CPU set
+ */
+void spdk_cpuset_copy(struct spdk_cpuset *dst, const struct spdk_cpuset *src);
+
+/**
+ * Perform AND operation on two CPU sets. The result is stored in dst.
+ *
+ * \param dst First argument of operation. This value also stores the result of operation.
+ * \param src Second argument of operation.
+ */
+void spdk_cpuset_and(struct spdk_cpuset *dst, const struct spdk_cpuset *src);
+
+/**
+ * Perform OR operation on two CPU sets. The result is stored in dst.
+ *
+ * \param dst First argument of operation. This value also stores the result of operation.
+ * \param src Second argument of operation.
+ */
+void spdk_cpuset_or(struct spdk_cpuset *dst, const struct spdk_cpuset *src);
+
+/**
+ * Clear all CPUs in CPU set.
+ *
+ * \param set CPU set to be cleared.
+ */
+void spdk_cpuset_zero(struct spdk_cpuset *set);
+
+/**
+ * Set or clear CPU state in CPU set.
+ *
+ * \param set CPU set object.
+ * \param cpu CPU index to be set or cleared.
+ * \param state *true* to set cpu, *false* to clear.
+ */
+void spdk_cpuset_set_cpu(struct spdk_cpuset *set, uint32_t cpu, bool state);
+
+/**
+ * Get the state of CPU in CPU set.
+ *
+ * \param set CPU set object.
+ * \param cpu CPU index.
+ * \return State of selected CPU.
+ */
+bool spdk_cpuset_get_cpu(const struct spdk_cpuset *set, uint32_t cpu);
+
+/**
+ * Get the number of CPUs that are set in CPU set.
+ *
+ * \param set CPU set object.
+ * \return Number of CPUs.
+ */
+uint32_t spdk_cpuset_count(const struct spdk_cpuset *set);
+
+/**
+ * Convert a CPU set to hex string.
+ *
+ * \param CPU set.
+ * \return Pointer to hexadecimal representation of CPU set. Buffer to store a
+ * string is dynamically allocated internally and freed with CPU set object.
+ */
+char *spdk_cpuset_fmt(struct spdk_cpuset *set);
+
+/**
+ * Convert a string containing a CPU core mask into a CPU set.
+ *
+ * \param set
+ * \param mask String defining CPU set. By default hexadecimal value is used or
+ * as CPU list enclosed in square brackets defined as: 'c1[-c2][,c3[-c4],...]'
+ * \return Zero if success, non zero if fails.
+ */
+int spdk_cpuset_parse(struct spdk_cpuset *set, const char *mask);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* SPDK_CPUSET_H */

include/spdk/event.h
@@ -43,6 +43,7 @@
 
 #include "spdk/stdinc.h"
 
+#include "spdk/cpuset.h"
 #include "spdk/queue.h"
 #include "spdk/log.h"
 
@@ -140,12 +141,12 @@ int spdk_app_get_shm_id(void);
 /**
  * \brief Convert a string containing a CPU core mask into a bitmask
  */
-int spdk_app_parse_core_mask(const char *mask, uint64_t *cpumask);
+int spdk_app_parse_core_mask(const char *mask, struct spdk_cpuset *cpumask);
 
 /**
  * \brief Return a mask of the CPU cores active for this application
  */
-uint64_t spdk_app_get_core_mask(void);
+struct spdk_cpuset *spdk_app_get_core_mask(void);
 
 /**
  * \brief Return the number of CPU cores utilized by this application

include/spdk/vhost.h
@@ -100,15 +100,15 @@ typedef int (*spdk_vhost_event_fn)(struct spdk_vhost_dev *vdev, void *arg);
 const char *spdk_vhost_dev_get_name(struct spdk_vhost_dev *vdev);
 
 /**
- * Get cpumask of the vhost device.  The mask is constant
+ * Get cpuset of the vhost device.  The cpuset is constant
  * throughout the lifetime of a vdev. It is be a subset
- * of SPDK app cpumask vhost was started with.
+ * of SPDK app cpuset vhost was started with.
  *
  * \param dev vhost device
- * \return cpumask of the vdev. The mask is constructed as:
- * ((1 << cpu0) | (1 << cpu1) | ... | (1 << cpuN)).
+ * \param cpuset pointer to the cpuset of the vdev.
  */
-uint64_t spdk_vhost_dev_get_cpumask(struct spdk_vhost_dev *vdev);
+void spdk_vhost_dev_get_cpumask(struct spdk_vhost_dev *vdev,
+				struct spdk_cpuset *cpuset);
 
 /**
  * By default, events are generated when asked, but for high queue depth and

lib/env_dpdk/init.c
@@ -190,7 +190,19 @@ spdk_build_eal_cmdline(const struct spdk_env_opts *opts)
 	}
 
 	/* set the coremask */
-	args = spdk_push_arg(args, &argcount, _sprintf_alloc("-c %s", opts->core_mask));
+	/* NOTE: If coremask starts with '[' and ends with ']' it is a core list
+	 */
+	if (opts->core_mask[0] == '[') {
+		char *l_arg = _sprintf_alloc("-l %s", opts->core_mask + 1);
+		int len = strlen(l_arg);
+		if (l_arg[len - 1] == ']') {
+			l_arg[len - 1] = '\0';
+		}
+		args = spdk_push_arg(args, &argcount, l_arg);
+	} else {
+		args = spdk_push_arg(args, &argcount, _sprintf_alloc("-c %s", opts->core_mask));
+	}
+
 	if (args == NULL) {
 		return -1;
 	}

lib/event/app.c
@@ -91,7 +91,7 @@ spdk_app_get_shm_id(void)
 "  #  specifying a ReactorMask.  Default is to allow work items to run\n" \
 "  #  on all cores.  Core 0 must be set in the mask if one is specified.\n" \
 "  # Default: 0xFFFF (cores 0-15)\n" \
-"  ReactorMask \"0x%" PRIX64 "\"\n" \
+"  ReactorMask \"0x%s\"\n" \
 "\n" \
 "  # Tracepoint group mask for spdk trace buffers\n" \
 "  # Default: 0x0 (all tracepoint groups disabled)\n" \
@@ -102,12 +102,16 @@ spdk_app_get_shm_id(void)
 static void
 spdk_app_config_dump_global_section(FILE *fp)
 {
+	struct spdk_cpuset *coremask;
+
 	if (NULL == fp) {
 		return;
 	}
 
-	fprintf(fp, GLOBAL_CONFIG_TMPL,
-		spdk_app_get_core_mask(), spdk_trace_get_tpoint_group_mask());
+	coremask = spdk_app_get_core_mask();
+
+	fprintf(fp, GLOBAL_CONFIG_TMPL, spdk_cpuset_fmt(coremask),
+		spdk_trace_get_tpoint_group_mask());
 }
 
 int

lib/event/reactor.c
@@ -124,6 +124,8 @@ static void spdk_reactor_construct(struct spdk_reactor *w, uint32_t lcore,
 
 static struct spdk_mempool *g_spdk_event_mempool[SPDK_MAX_SOCKET];
 
+static struct spdk_cpuset *g_spdk_app_core_mask;
+
 static struct spdk_reactor *
 spdk_reactor_get(uint32_t lcore)
 {
@@ -559,46 +561,26 @@ spdk_app_get_current_core(void)
 }
 
 int
-spdk_app_parse_core_mask(const char *mask, uint64_t *cpumask)
+spdk_app_parse_core_mask(const char *mask, struct spdk_cpuset *cpumask)
 {
-	uint32_t i;
-	char *end;
-	uint64_t validmask;
+	int ret;
+	struct spdk_cpuset *validmask;
 
-	if (mask == NULL || cpumask == NULL) {
-		return -1;
+	ret = spdk_cpuset_parse(cpumask, mask);
+	if (ret < 0) {
+		return ret;
 	}
 
-	errno = 0;
-	*cpumask = strtoull(mask, &end, 16);
-	if (*end != '\0' || errno) {
-		return -1;
-	}
-
-	validmask = 0;
-	SPDK_ENV_FOREACH_CORE(i) {
-		if (i >= 64) {
-			break;
-		}
-		validmask |= 1ULL << i;
-	}
-
-	*cpumask &= validmask;
+	validmask = spdk_app_get_core_mask();
+	spdk_cpuset_and(cpumask, validmask);
 
 	return 0;
 }
 
-uint64_t
+struct spdk_cpuset *
 spdk_app_get_core_mask(void)
 {
-	uint32_t i;
-	uint64_t mask = 0;
-
-	SPDK_ENV_FOREACH_CORE(i) {
-		mask |= 1ULL << i;
-	}
-
-	return mask;
+	return g_spdk_app_core_mask;
 }
 
 
@@ -625,6 +607,7 @@ spdk_reactors_start(void)
 	int rc;
 
 	g_reactor_state = SPDK_REACTOR_STATE_RUNNING;
+	g_spdk_app_core_mask = spdk_cpuset_alloc();
 
 	current_core = spdk_env_get_current_core();
 	SPDK_ENV_FOREACH_CORE(i) {
@@ -637,6 +620,7 @@ spdk_reactors_start(void)
 				return;
 			}
 		}
+		spdk_cpuset_set_cpu(g_spdk_app_core_mask, i, true);
 	}
 
 	/* Start the master reactor */
@@ -646,6 +630,8 @@ spdk_reactors_start(void)
 	spdk_env_thread_wait_all();
 
 	g_reactor_state = SPDK_REACTOR_STATE_SHUTDOWN;
+	spdk_cpuset_free(g_spdk_app_core_mask);
+	g_spdk_app_core_mask = NULL;
 }
 
 void

lib/iscsi/conn.c
@@ -73,7 +73,7 @@ static pthread_mutex_t g_conns_mutex;
 
 static struct spdk_poller *g_shutdown_timer = NULL;
 
-static uint32_t spdk_iscsi_conn_allocate_reactor(uint64_t cpumask);
+static uint32_t spdk_iscsi_conn_allocate_reactor(const struct spdk_cpuset *cpumask);
 
 void spdk_iscsi_conn_login_do_work(void *arg);
 void spdk_iscsi_conn_full_feature_do_work(void *arg);
@@ -1421,7 +1421,7 @@ spdk_iscsi_conn_get_min_per_core(void)
 }
 
 static uint32_t
-spdk_iscsi_conn_allocate_reactor(uint64_t cpumask)
+spdk_iscsi_conn_allocate_reactor(const struct spdk_cpuset *cpumask)
 {
 	uint32_t i, selected_core;
 	int32_t num_pollers, min_pollers;
@@ -1429,9 +1429,8 @@ spdk_iscsi_conn_allocate_reactor(uint64_t cpumask)
 	min_pollers = INT_MAX;
 	selected_core = spdk_env_get_first_core();
 
-	/* we use u64 as CPU core mask */
 	SPDK_ENV_FOREACH_CORE(i) {
-		if (!((1ULL << i) & cpumask)) {
+		if (!spdk_cpuset_get_cpu(cpumask, i)) {
 			continue;
 		}
 

commit a809cbc9da5cdf600656a202077c21571b3aea7b

vhost: return proper rc on vhost_dev_register() alloc failure
This must've been overlooked in patch 601bcbcf.

Fixes: 601bcbcf6665 ("util: extend cpumask to hold more than 64 cpus")
Change-Id: Ia7e06fcacba5cd99770b81542678550e9fbc3ca2
Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

lib/vhost/vhost.c
@@ -628,7 +628,7 @@ spdk_vhost_dev_register(struct spdk_vhost_dev *vdev, const char *name, const cha
 	cpumask = spdk_cpuset_alloc();
 	if (!cpumask) {
 		SPDK_ERRLOG("spdk_cpuset_alloc failed\n");
-		return -1;
+		return -ENOMEM;
 	}
 
 	if (spdk_vhost_parse_core_mask(mask_str, cpumask) != 0) {