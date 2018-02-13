6fd20b062e690b0e39fade1229694facce16ee21
Ziye Yang <optimistyzy@gmail.com>
2018-02-13 13:09:49 +0800

    iscsi: create a iSCSI poll group for each core
    This patch adds a iSCSI poll group for each core,
    thus we can poll a group of connections instead of one
    by one on each core.
    
    Change-Id: I9cd82c8c0f7f6e7b3ee34034339638199bfca5da
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/iscsi/iscsi_subsystem.c
+++ lib/iscsi/iscsi_subsystem.c
@@ -815,6 +815,53 @@ spdk_iscsi_app_read_parameters(void)
 	return 0;
 }
 
+static void
+spdk_iscsi_poll_group_poll(void *ctx)
+{
+	struct spdk_iscsi_poll_group *group = ctx;
+	struct spdk_iscsi_conn *conn, *tmp;
+
+	STAILQ_FOREACH_SAFE(conn, &group->connections, link, tmp) {
+		conn->fn(conn);
+	}
+}
+
+static void
+iscsi_create_poll_group_done(void *ctx)
+{
+}
+
+static void
+iscsi_create_poll_group(void *ctx)
+{
+	struct spdk_iscsi_poll_group *pg;
+
+	pg = &g_spdk_iscsi.poll_group[spdk_env_get_current_core()];
+	pg->core = spdk_env_get_current_core();
+	assert(pg != NULL);
+
+	STAILQ_INIT(&pg->connections);
+	pg->poller = spdk_poller_register(spdk_iscsi_poll_group_poll, pg, 0);
+}
+
+static int
+spdk_initialize_iscsi_poll_group(void)
+{
+	size_t g_num_poll_groups = spdk_env_get_last_core() + 1;
+
+	assert(g_num_poll_groups > 0);
+	g_spdk_iscsi.poll_group = calloc(g_num_poll_groups, sizeof(struct spdk_iscsi_poll_group));
+	if (!g_spdk_iscsi.poll_group) {
+		SPDK_ERRLOG("Cannot allocate poll_group\n");
+		return -1;
+	}
+
+	/* Send a message to each thread and create a poll group */
+	spdk_for_each_thread(iscsi_create_poll_group, NULL, iscsi_create_poll_group_done);
+
+	return 0;
+}
+
 int
 spdk_iscsi_init(void)
 {
@@ -844,6 +891,12 @@ spdk_iscsi_init(void)
 		return -1;
 	}
 
+	rc = spdk_initialize_iscsi_poll_group();
+	if (rc < 0) {
+		SPDK_ERRLOG("spdk_initialize_iscsi_poll_group() failed\n");
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -868,6 +921,7 @@ spdk_iscsi_fini_done(void)
 	spdk_iscsi_portal_grp_array_destroy();
 	free(g_spdk_iscsi.authfile);
 	free(g_spdk_iscsi.nodebase);
+	free(g_spdk_iscsi.poll_group);
 
 	pthread_mutex_destroy(&g_spdk_iscsi.mutex);
 	g_fini_cb_fn(g_fini_cb_arg);
