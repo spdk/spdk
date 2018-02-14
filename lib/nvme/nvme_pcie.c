d507192be6127a4d622dfe2951584f808a03408a
Cunyin Chang <cunyin.chang@intel.com>
2018-02-14 08:25:23 +0800

    nvme: Fix the calculation of the offset of CMB after 2M align.
    Change-Id: Iff95ccbf60020dc1a4c2b9f9ef14f8b4a17a0823
    Signed-off-by: Cunyin Chang <cunyin.chang@intel.com>

--- lib/nvme/nvme_pcie.c
+++ lib/nvme/nvme_pcie.c
@@ -489,7 +489,6 @@ nvme_pcie_ctrlr_map_cmb(struct nvme_pcie_ctrlr *pctrlr)
 	pctrlr->cmb_bar_virt_addr = addr;
 	pctrlr->cmb_bar_phys_addr = bar_phys_addr;
 	pctrlr->cmb_size = size;
-	pctrlr->cmb_current_offset = offset;
 
 	if (!cmbsz.bits.sqs) {
 		pctrlr->ctrlr.opts.use_cmb_sqs = false;
@@ -516,7 +515,7 @@ nvme_pcie_ctrlr_map_cmb(struct nvme_pcie_ctrlr *pctrlr)
 		SPDK_ERRLOG("spdk_mem_register() failed\n");
 		goto exit;
 	}
-	pctrlr->cmb_current_offset = mem_register_start - ((uint64_t)pctrlr->cmb_bar_virt_addr + offset);
+	pctrlr->cmb_current_offset = 0;
 	pctrlr->cmb_io_data_supported = true;
 
 	return;
@@ -552,17 +551,18 @@ nvme_pcie_ctrlr_alloc_cmb(struct spdk_nvme_ctrlr *ctrlr, uint64_t length, uint64
 			  uint64_t *offset)
 {
 	struct nvme_pcie_ctrlr *pctrlr = nvme_pcie_ctrlr(ctrlr);
-	uint64_t round_offset;
+	uint64_t round_start;
+	uint64_t start_address;
 
-	round_offset = pctrlr->cmb_current_offset;
-	round_offset = (round_offset + (aligned - 1)) & ~(aligned - 1);
+	start_address = (uintptr_t)pctrlr->cmb_mem_register_addr + pctrlr->cmb_current_offset;
+	round_start = (start_address + (aligned - 1)) & ~(aligned - 1);
 
-	if (round_offset + length > pctrlr->cmb_size) {
+	if (round_start + length > (uintptr_t)pctrlr->cmb_mem_register_addr + pctrlr->cmb_mem_register_size) {
 		return -1;
 	}
 
-	*offset = round_offset;
-	pctrlr->cmb_current_offset = round_offset + length;
+	*offset = round_start - (uintptr_t)pctrlr->cmb_bar_virt_addr;
+	pctrlr->cmb_current_offset = round_start + length - start_address;
 
 	return 0;
 }
