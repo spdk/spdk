9366f1161a8e26460f5780665c8d7cd39dcd8c55
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-02-15 17:54:10 +0900

    iscsi/rpc: Adjust dump() and construct() format of target_node
    To support importing/exporting JSON config file, output of dump()
    and input to construct() must have the same format.
    
    This patch is for iSCSI target node.
    
    Change-Id: If097e41ada0f2fe3754691cee0a0774db97c4ebb
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/iscsi_tgt/rpc_config/rpc_config.py
+++ test/iscsi_tgt/rpc_config/rpc_config.py
@@ -301,22 +301,22 @@ def verify_target_nodes_rpc_methods(rpc_py, rpc_param):
     jsonvalues = json.loads(output)
     verify(len(jsonvalues) == 1, 1,
            "get_target_nodes returned {} nodes, expected 1".format(len(jsonvalues)))
-    bdev_name = jsonvalues[0]['luns'][0]['bdev_name']
+    bdev_name = jsonvalues[0]['bdev_names'][0]
     verify(bdev_name == "Malloc" + str(rpc_param['lun_total']), 1,
-           "bdev_name value is {}, expected Malloc{}".format(jsonvalues[0]['luns'][0]['bdev_name'], str(rpc_param['lun_total'])))
+           "bdev_name value is {}, expected Malloc{}".format(jsonvalues[0]['bdev_names'][0], str(rpc_param['lun_total'])))
     name = jsonvalues[0]['name']
     verify(name == nodebase + ":" + rpc_param['target_name'], 1,
            "target name value is {}, expected {}".format(name, nodebase + ":" + rpc_param['target_name']))
     verify(jsonvalues[0]['alias_name'] == rpc_param['alias_name'], 1,
            "target alias_name value is {}, expected {}".format(jsonvalues[0]['alias_name'], rpc_param['alias_name']))
-    verify(jsonvalues[0]['luns'][0]['id'] == 0, 1,
-           "lun id value is {}, expected 0".format(jsonvalues[0]['luns'][0]['id']))
-    verify(jsonvalues[0]['pg_ig_maps'][0]['ig_tag'] == int(initiator_tag), 1,
-           "initiator group tag value is {}, expected {}".format(jsonvalues[0]['pg_ig_maps'][0]['ig_tag'], initiator_tag))
+    verify(jsonvalues[0]['lun_ids'][0] == 0, 1,
+           "lun id value is {}, expected 0".format(jsonvalues[0]['lun_ids'][0]))
+    verify(jsonvalues[0]['ig_tags'][0] == int(initiator_tag), 1,
+           "initiator group tag value is {}, expected {}".format(jsonvalues[0]['ig_tags'][0], initiator_tag))
     verify(jsonvalues[0]['queue_depth'] == rpc_param['queue_depth'], 1,
            "queue depth value is {}, expected {}".format(jsonvalues[0]['queue_depth'], rpc_param['queue_depth']))
-    verify(jsonvalues[0]['pg_ig_maps'][0]['pg_tag'] == int(portal_tag), 1,
-           "portal group tag value is {}, expected {}".format(jsonvalues[0]['pg_ig_maps'][0]['pg_tag'], portal_tag))
+    verify(jsonvalues[0]['pg_tags'][0] == int(portal_tag), 1,
+           "portal group tag value is {}, expected {}".format(jsonvalues[0]['pg_tags'][0], portal_tag))
     verify(jsonvalues[0]['chap_disabled'] == rpc_param['chap_disable'], 1,
            "chap disable value is {}, expected {}".format(jsonvalues[0]['chap_disabled'], rpc_param['chap_disable']))
     verify(jsonvalues[0]['chap_mutual'] == rpc_param['chap_mutal'], 1,
@@ -333,10 +333,10 @@ def verify_target_nodes_rpc_methods(rpc_py, rpc_param):
     rpc.target_node_add_lun(name, bdev_name, "-i", lun_id)
     output = rpc.get_target_nodes()
     jsonvalues = json.loads(output)
-    verify(jsonvalues[0]['luns'][1]['bdev_name'] == "Malloc" + str(rpc_param['lun_total']), 1,
-           "bdev_name value is {}, expected Malloc{}".format(jsonvalues[0]['luns'][0]['bdev_name'], str(rpc_param['lun_total'])))
-    verify(jsonvalues[0]['luns'][1]['id'] == 1, 1,
-           "lun id value is {}, expected 1".format(jsonvalues[0]['luns'][1]['id']))
+    verify(jsonvalues[0]['bdev_names'][1] == "Malloc" + str(rpc_param['lun_total']), 1,
+           "bdev_name value is {}, expected Malloc{}".format(jsonvalues[0]['bdev_names'][1], str(rpc_param['lun_total'])))
+    verify(jsonvalues[0]['lun_ids'][1] == 1, 1,
+           "lun id value is {}, expected 1".format(jsonvalues[0]['lun_ids'][1]))
 
     rpc.delete_target_node(name)
     output = rpc.get_target_nodes()
