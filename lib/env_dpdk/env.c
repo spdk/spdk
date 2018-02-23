6834c531a8835b6a50caa7996f99fa15f9da962e
Chen Wang <chenx.wang@intel.com>
2018-02-23 14:43:28 +0800

    env: add malloc variants with DMA/shareable flags
    This will allow environment abstraction layers to provide different
    types of memory depending on whether it needs to be DMA-able and/or
    shared across multi-process boundaries.  For the DPDK environment, the
    flags can be ignored, since rte_malloc() supports both DMA-able and
    shared memory.
    
    Change-Id: I5ee894337dd9d6e24418848c0a35f131184383c8
    Signed-off-by: zkhatami88 <z.khatami88@gmail.com>

--- lib/env_dpdk/env.c
+++ lib/env_dpdk/env.c
@@ -63,7 +63,7 @@ virt_to_phys(void *vaddr)
 }
 
 void *
-spdk_dma_malloc_socket(size_t size, size_t align, uint64_t *phys_addr, int socket_id)
+spdk_malloc(size_t size, size_t align, uint64_t *phys_addr, int socket_id, uint32_t flags)
 {
 	void *buf = rte_malloc_socket(NULL, size, align, socket_id);
 	if (buf && phys_addr) {
@@ -73,13 +73,31 @@ spdk_dma_malloc_socket(size_t size, size_t align, uint64_t *phys_addr, int socke
 }
 
 void *
-spdk_dma_zmalloc_socket(size_t size, size_t align, uint64_t *phys_addr, int socket_id)
+spdk_zmalloc(size_t size, size_t align, uint64_t *phys_addr, int socket_id, uint32_t flags)
 {
-	void *buf = spdk_dma_malloc_socket(size, align, phys_addr, socket_id);
+	void *buf = spdk_malloc(size, align, phys_addr, socket_id, flags);
 	if (buf) {
 		memset(buf, 0, size);
 	}
 	return buf;
+}
+
+void
+spdk_free(void *buf)
+{
+	rte_free(buf);
+}
+
+void *
+spdk_dma_malloc_socket(size_t size, size_t align, uint64_t *phys_addr, int socket_id)
+{
+	return spdk_malloc(size, align, phys_addr, socket_id, (SPDK_MALLOC_DMA | SPDK_MALLOC_SHARE));
+}
+
+void *
+spdk_dma_zmalloc_socket(size_t size, size_t align, uint64_t *phys_addr, int socket_id)
+{
+	return spdk_zmalloc(size, align, phys_addr, socket_id, (SPDK_MALLOC_DMA | SPDK_MALLOC_SHARE));
 }
 
 void *
@@ -107,7 +125,7 @@ spdk_dma_realloc(void *buf, size_t size, size_t align, uint64_t *phys_addr)
 void
 spdk_dma_free(void *buf)
 {
-	rte_free(buf);
+	spdk_free(buf);
 }
 
 void *
