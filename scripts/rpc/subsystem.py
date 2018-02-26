0c8fdbbb7f1a58b08070c4a46a25ed2fd3eb9eb1
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-02-26 20:16:59 +0100

    subsystem: add per module configuration dump
    Change-Id: I5c4e51cd9cd97b05ab9a95cbe084ff2741f6ef58
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- scripts/rpc/subsystem.py
+++ scripts/rpc/subsystem.py
@@ -2,4 +2,10 @@ from client import print_dict, print_array, int_arg
 
 
 def get_subsystems(args):
-    print_dict(args.client.call('get_subsystems'))
+    params = {'no_config' : False}
+    if args.no_config:
+        params['no_config'] = args.no_config
+    if args.subsystems:
+        params['subsystems'] = args.subsystems.split(',')
+
+    print_dict(args.client.call('get_subsystems', params))
