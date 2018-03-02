4fa2317a73ba32fc0e539a242dbadc20a805ba43
SPDK Nightly Cron Job <sys_sgsw@intel.com>
2018-03-02 05:00:08 -0700

    SPDK NIGHTLY TEST RUN 03/02/18
    Change-Id: I2d6f1d3f0a4496f1ae5911195073301ca83da128
    Signed-off-by: SPDK Nightly Cron Job <sys_sgsw@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -9,7 +9,7 @@ set -e
 PS4=' \t	\$ '
 ulimit -c unlimited
 
-: ${RUN_NIGHTLY:=0}
+: ${RUN_NIGHTLY:=1}
 export RUN_NIGHTLY
 
 if [[ ! -z $1 ]]; then
@@ -540,3 +540,4 @@ function get_bdev_size()
 
 set -o errtrace
 trap "trap - ERR; print_backtrace >&2" ERR
+DATE='Fri Mar  2 05:00:08 MST 2018'
