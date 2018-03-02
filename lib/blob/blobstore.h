0847f27b54c82c29a67f779f6a464454015d448b
Daniel Verkamp <daniel.verkamp@intel.com>
2018-03-02 12:31:25 -0700

    Revert "blobstore: Add a blob_bs_dev that provides back_bs_dev for clones"
    This change wasn't correctly rebased and needs to be updated to compile
    against the current blobstore.
    
    This reverts commit c1174e6895ca44242d12b2e5f0742f84ccbf0b8f.
    
    Change-Id: I529608bee7323cb626d8c36dff15adc9ba24ad26
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/402352
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- lib/blob/blobstore.h
+++ lib/blob/blobstore.h
@@ -195,15 +195,6 @@ enum spdk_blob_op_type {
 	SPDK_BLOB_READV,
 };
 
-/* back bs_dev */
-
-#define BLOB_SNAPSHOT "SNAP"
-
-struct spdk_blob_bs_dev {
-	struct spdk_bs_dev bs_dev;
-	struct spdk_blob *blob;
-};
-
 /* On-Disk Data Structures
  *
  * The following data structures exist on disk.
@@ -338,7 +329,6 @@ SPDK_STATIC_ASSERT(sizeof(struct spdk_bs_super_block) == 0x1000, "Invalid super 
 #pragma pack(pop)
 
 struct spdk_bs_dev *spdk_bs_create_zeroes_dev(void);
-struct spdk_bs_dev *spdk_bs_create_blob_bs_dev(struct spdk_blob *blob);
 
 /* Unit Conversions
  *
