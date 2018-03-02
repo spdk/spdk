29c65ac6b99583ea00172eb1f248bd7a3b6846b7
Daniel Verkamp <daniel.verkamp@intel.com>
2018-03-02 14:19:08 -0700

    test/nvme-cli: skip test if nvme-cli not found
    No need to run the stub and other setup work if the nvme-cli repository
    is not found on the test agent.
    
    Change-Id: I5a3c0ab8ceb25e65f4693b8fc519812ebccf9ae7
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- test/lib/nvme/spdk_nvme_cli.sh
+++ test/lib/nvme/spdk_nvme_cli.sh
@@ -8,6 +8,11 @@ source $rootdir/scripts/common.sh
 source $rootdir/test/common/autotest_common.sh
 spdk_nvme_cli="/home/sys_sgsw/nvme-cli"
 
+if [ ! -d $spdk_nvme_cli ]; then
+	echo "nvme-cli repository not found at $spdk_nvme_cli; skipping tests."
+	exit 0
+fi
+
 timing_enter nvme_cli
 
 if [ `uname` = Linux ]; then
@@ -15,31 +20,28 @@ if [ `uname` = Linux ]; then
 	trap "kill_stub; exit 1" SIGINT SIGTERM EXIT
 fi
 
-if [ -d $spdk_nvme_cli ]; then
-	# Build against the version of SPDK under test
-	rm -f "$spdk_nvme_cli/spdk"
-	ln -sf "$rootdir" "$spdk_nvme_cli/spdk"
+# Build against the version of SPDK under test
+rm -f "$spdk_nvme_cli/spdk"
+ln -sf "$rootdir" "$spdk_nvme_cli/spdk"
 
-	bdfs=$(iter_pci_class_code 01 08 02)
-	bdf=$(echo $bdfs|awk '{ print $1 }')
-	cd $spdk_nvme_cli
-	make clean && make
-	sed -i 's/spdk=0/spdk=1/g' spdk.conf
-	sed -i 's/shm_id=1/shm_id=0/g' spdk.conf
-	./nvme list
-	./nvme id-ctrl $bdf
-	./nvme list-ctrl $bdf
-	./nvme get-ns-id $bdf
-	./nvme id-ns $bdf
-	./nvme fw-log $bdf
-	./nvme smart-log $bdf
-	./nvme error-log $bdf
-	./nvme list-ns $bdf -n 1
-	./nvme get-feature $bdf -n 1 -f 1 -s 1 -l 100
-	./nvme get-log $bdf -n 1 -i 1 -l 100
-	./nvme reset $bdf
-fi
-
+bdfs=$(iter_pci_class_code 01 08 02)
+bdf=$(echo $bdfs|awk '{ print $1 }')
+cd $spdk_nvme_cli
+make clean && make
+sed -i 's/spdk=0/spdk=1/g' spdk.conf
+sed -i 's/shm_id=1/shm_id=0/g' spdk.conf
+./nvme list
+./nvme id-ctrl $bdf
+./nvme list-ctrl $bdf
+./nvme get-ns-id $bdf
+./nvme id-ns $bdf
+./nvme fw-log $bdf
+./nvme smart-log $bdf
+./nvme error-log $bdf
+./nvme list-ns $bdf -n 1
+./nvme get-feature $bdf -n 1 -f 1 -s 1 -l 100
+./nvme get-log $bdf -n 1 -i 1 -l 100
+./nvme reset $bdf
 if [ `uname` = Linux ]; then
 	trap - SIGINT SIGTERM EXIT
 	kill_stub
