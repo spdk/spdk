771249485e7abec60a6157a91a4998c6e2c8f5d3
xuhuagen <huagenx.xu@intel.com>
2018-03-05 17:56:24 +0800

    NIGHTLY TEST RUN DEBUG FOR ISCSI FILESYSTEM
    Add iscsi filesystem test cases(run fio) with
    lvol and nvme back-end in nightly
    
    Change-Id: I3c36221b05a8de5b47df9ee71dfc8a2067952c73
    Signed-off-by: xuhuagen <huagenx.xu@intel.com>

--- test/iscsi_tgt/filesystem/filesystem.sh
+++ test/iscsi_tgt/filesystem/filesystem.sh
@@ -6,16 +6,22 @@ source $rootdir/test/common/autotest_common.sh
 source $rootdir/test/iscsi_tgt/common.sh
 
 timing_enter filesystem
-
+set -e
 # iSCSI target configuration
 PORT=3260
 INITIATOR_TAG=2
 INITIATOR_NAME=ANY
 NETMASK=$INITIATOR_IP/32
-MALLOC_BDEV_SIZE=256
-MALLOC_BLOCK_SIZE=512
-
 rpc_py="python $rootdir/scripts/rpc.py"
+
+function linux_iter_pci {
+	lspci -mm -n -D | grep $1 | tr -d '"' | awk -F " " '{print $1}'
+}
+
+function lvol_cleanup() {
+	$rpc_py delete_bdev $1
+	$rpc_py destroy_lvol_store -l lvs_0
+}
 
 timing_enter start_iscsi_tgt
 
@@ -29,32 +35,37 @@ waitforlisten $pid
 echo "iscsi_tgt is listening. Running tests..."
 
 timing_exit start_iscsi_tgt
+bdf=`linux_iter_pci 0108 | head -1`
 
 $rpc_py add_portal_group 1 $TARGET_IP:$PORT
 $rpc_py add_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
-$rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE
+$rpc_py construct_nvme_bdev -b "Nvme0" -t "pcie" -a $bdf
+
+ls_guid=$($rpc_py construct_lvol_store Nvme0n1 lvs_0)
+#free_mb=$(get_lvs_free_mb "$ls_guid")
+lb_guid=$($rpc_py construct_lvol_bdev -u $ls_guid lbd_0 2048)
+
 # "Malloc0:0" ==> use Malloc0 blockdev for LUN0
 # "1:2" ==> map PortalGroup1 to InitiatorGroup2
 # "64" ==> iSCSI queue depth 64
 # "1 0 0 0" ==> disable CHAP authentication
-$rpc_py construct_target_node Target3 Target3_alias 'Malloc0:0' '1:2' 256 -d
+$rpc_py construct_target_node Target1 Target1_alias "${lb_guid}:0" '1:2' 64 -d
 sleep 1
 
 iscsiadm -m discovery -t sendtargets -p $TARGET_IP:$PORT
+sleep 1
 iscsiadm -m node --login -p $TARGET_IP:$PORT
 
-trap "umount /mnt/device; rm -rf /mnt/device; iscsicleanup; killprocess $pid; exit 1" SIGINT SIGTERM EXIT
+trap "lvol_cleanup ${lb_guid}; [ -d /mnt/device ] && umount /mnt/device && rm -rf /mnt/device; iscsicleanup; killprocess $pid; exit 1" SIGINT SIGTERM EXIT
 
 sleep 1
 
 mkdir -p  /mnt/device
 
 dev=$(iscsiadm -m session -P 3 | grep "Attached scsi disk" | awk '{print $4}')
-
 parted -s /dev/$dev mklabel msdos
-parted -s /dev/$dev mkpart primary '0%' '100%'
+parted -s /dev/$dev mkpart primary '1%' '100%'
 sleep 1
-
 for fstype in "ext4" "btrfs" "xfs"; do
 
 	if [ "$fstype" == "ext4" ]; then
@@ -63,30 +74,40 @@ for fstype in "ext4" "btrfs" "xfs"; do
 		mkfs.${fstype} -f /dev/${dev}1
 	fi
 	mount /dev/${dev}1 /mnt/device
-	touch /mnt/device/aaa
-	umount /mnt/device
+	if [ $RUN_NIGHTLY -eq 0 ]; then
+		fio -filename=/mnt/device/test -direct=1 -iodepth 64 -thread=1 -invalidate=1 -rw=randwrite -ioengine=libaio -bs=4k \
+		 -size=128M -name=job0
+		fio -filename=/mnt/device/test -direct=1 -iodepth 1 -thread=1 -invalidate=1 -rw=randread -ioengine=libaio 			-bs=4k -runtime=20 -time_based=1 -name=job0
 
-	iscsiadm -m node --logout
-	sleep 1
-	iscsiadm -m node --login -p $TARGET_IP:$PORT
-	sleep 1
-	dev=$(iscsiadm -m session -P 3 | grep "Attached scsi disk" | awk '{print $4}')
-	mount -o rw /dev/${dev}1 /mnt/device
-
-	if [ -f "/mnt/device/aaa" ]; then
-		echo "File existed."
+		rm -rf /mnt/device/test
+		umount /mnt/device
 	else
-		echo "File doesn't exist."
-		exit 1
-	fi
+		touch /mnt/device/aaa
+		umount /mnt/device
 
-	rm -rf /mnt/device/aaa
-	umount /mnt/device
+		iscsiadm -m node --logout
+		sleep 1
+		iscsiadm -m node --login -p $TARGET_IP:$PORT
+		sleep 1
+		dev=$(iscsiadm -m session -P 3 | grep "Attached scsi disk" | awk '{print $4}')
+		mount -o rw /dev/${dev}1 /mnt/device
+
+		if [ -f "/mnt/device/aaa" ]; then
+			echo "File existed."
+		else
+			echo "File doesn't exist."
+			exit 1
+		fi
+
+		rm -rf /mnt/device/aaa
+		umount /mnt/device
+	fi
 done
 
 rm -rf /mnt/device
 
 trap - SIGINT SIGTERM EXIT
+
 
 iscsicleanup
 killprocess $pid
