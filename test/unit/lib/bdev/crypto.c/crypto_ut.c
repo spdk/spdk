cfb7a47d6328028f5071941bd7bdecd959e13b01
Paul Luse <paul.e.luse@intel.com>
2018-03-07 16:44:06 -0700

    [RFC] bdev: add crypto vbdev
    Ready for review but unable to remove RFC until the build system is updated at least for
    AESNI-MB.  QAT can be included in the code (but not makefile) and marked as experimental
    until we are ready to test in CI.  It works well on 2 systems but is a big PITA to get
    the HW setup and configured for use with DPDK (IMHO).  drv is driving patches to
    accomplosh the following build steps:
    
    Have tested (both aesni and QAT seprately and concurrrently) on underlying NVMe devices
    with bdevio and a bdevperf script that runs IOs from 512B to 128K each with Q depths from
    1 to 512 in powers of 2 for 30 seconds each run.
    
    * For it to build: (need to intgrate/automate)
    - Copy and build this library: https://github.com/01org/intel-ipsec-mb/archive/v0.48.zip
    - export AESNI_MULTI_BUFFER_LIB_PATH=/home/peluse/intel-ipsec-mb-0.48
    - cp ~/intel-ipsec-mb-0.48/libIPSec_MB.a /home/peluse/spdk/dpdk/build/lib/libIPSec_MB.a
    - edit dpdk/config/common_spdk
       CONFIG_RTE_LIBRTE_PMD_AESNI_MB=y
       CONFIG_RTE_LIBRTE_CRYPTODEV=y
       CONFIG_RTE_LIBRTE_REORDER=y
       CONFIG_RTE_LIBRTE_PMD_QAT=y
    
    Change-Id: If518c3df8e74e00efa18afdf194824c5e69778fc
    Signed-off-by: Paul Luse <paul.e.luse@intel.com>

---
+++ test/unit/lib/bdev/crypto.c/crypto_ut.c
@@ -0,0 +1,85 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk_cunit.h"
+
+#include "common/lib/test_env.c"
+#include "spdk_internal/mock.h"
+#include "unit/lib/json_mock.c"
+
+#include "bdev/crypto/vbdev_crypto.c"
+
+DEFINE_STUB(spdk_conf_find_section, struct spdk_conf_section *,
+(struct spdk_conf *cp, const char *name), NULL);
+DEFINE_STUB(spdk_conf_section_get_nval, char *,
+(struct spdk_conf_section *sp, const char *key, int idx), NULL);
+DEFINE_STUB(spdk_conf_section_get_nmval, char *,
+(struct spdk_conf_section *sp, const char *key, int idx1, int idx2), NULL);
+
+DEFINE_STUB(rte_cryptodev_count, unsigned, (void), 0);
+
+static void
+test_crypto_operation(void)
+{
+}
+
+int
+main(int argc, char **argv)
+{
+	CU_pSuite	suite = NULL;
+	unsigned int	num_failures;
+
+	if (CU_initialize_registry() != CUE_SUCCESS) {
+		return CU_get_error();
+	}
+
+	suite = CU_add_suite("crypto", NULL, NULL);
+	if (suite == NULL) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (
+		CU_add_test(suite, "ut_crypto_operation",
+			    test_crypto_operation) == NULL
+	) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	num_failures = CU_get_number_of_failures();
+	CU_cleanup_registry();
+	return num_failures;
+}
