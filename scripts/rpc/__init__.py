251e97f6d4816785e08d9fbfcedf03b37ac7d81c
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-03-09 21:19:34 +0100

    scripts/rpc: add configuration save and load
    Change-Id: I37f2174fd6c3092b25e127a4cd578d202ee8c98a
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- scripts/rpc/__init__.py
+++ scripts/rpc/__init__.py
@@ -10,7 +10,53 @@ import nvmf
 import pmem
 import subsystem
 import vhost
+import json
+import sys
 
 
 def get_rpc_methods(args):
     print_dict(args.client.call('get_rpc_methods'))
+
+
+def save_config(args):
+    config = {
+        'subsystems': []
+    }
+
+    for elem in args.client.call('get_subsystems'):
+        cfg = {
+            'subsystem': elem['subsystem'],
+            'config': args.client.call('get_subsystem_config', {"name": elem['subsystem']})
+        }
+        config['subsystems'].append(cfg)
+
+    indent = args.indent
+    if args.filename is None or args.filename == '-':
+        if indent is None and args.filename is None:
+            indent = 4
+        elif indent < 0:
+            indent = None
+        json.dump(config, sys.stdout, indent=indent)
+        sys.stdout.write('\n')
+    else:
+        if indent is not None and indent < 0:
+            indent = None
+        with open(args.filename, 'w') as file:
+            json.dump(config, file, indent=indent)
+            file.write('\n')
+
+
+def load_config(args):
+    if not args.filename or args.filename == '-':
+        config = json.load(sys.stdin)
+    else:
+        with open(args.filename, 'r') as file:
+            config = json.load(file)
+
+    for subsystem in config['subsystems']:
+        name = subsystem['subsystem']
+        config = subsystem['config']
+        if not config:
+            continue
+        for elem in subsystem['config']:
+            args.client.call(elem['method'], elem['params'])
