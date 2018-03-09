1275f1fd6a028e2f67b687c18ecf2dc09f8b97d5
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-03-09 21:19:34 +0100

    scripts/rpc: add configuration save and load
    Change-Id: I37f2174fd6c3092b25e127a4cd578d202ee8c98a
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- scripts/rpc/__init__.py
+++ scripts/rpc/__init__.py
@@ -10,7 +10,40 @@ import nvmf
 import pmem
 import subsystem
 import vhost
+import json
 
 
 def get_rpc_methods(args):
     print_dict(args.client.call('get_rpc_methods'))
+
+
+def save_config(args):
+    config = {
+        'subsystems': []
+    }
+
+    for elem in args.client.call('get_subsystems'):
+        rpc_params = {
+            "name": elem['subsystem']
+        }
+        cfg = {
+            'subsystem': elem['subsystem'],
+            'config': args.client.call('get_subsystem_config', rpc_params)
+        }
+        config['subsystems'].append(cfg)
+
+    with open(args.filename, 'w') as file:
+        json.dump(config, file, indent=2)
+
+
+def load_config(args):
+    with open(args.filename, 'r') as file:
+        config = json.load(file)
+
+    for subsystem in config['subsystems']:
+        name = subsystem['subsystem']
+        config = subsystem['config']
+        if not config:
+            continue
+        for elem in subsystem['config']:
+            args.client.call(elem['method'], elem['params'])
