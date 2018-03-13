1bbbf1124f6aad93b2f471e96afaca00a2f6b83c
Chen Wang <chenx.wang@intel.com>
2018-03-13 13:57:41 +0800

    test/vhost: add the message for vhost test cases.
    Change-Id: I7b6387019a861bd1c4f89b9a7712e53150aea8fa
    Signed-off-by: Chen Wang <chenx.wang@intel.com>

--- test/lvol/test_cases.py
+++ test/lvol/test_cases.py
@@ -71,13 +71,13 @@ def case_message(func):
             10000: 'SIGTERM',
         }
         num = int(func.__name__.strip('test_case')[:])
-        print("========================================================")
-        print("Test Case {num}: Start".format(num=num))
-        print("Test Name: {name}".format(name=test_name[num]))
-        print("========================================================")
+        print("************************************")
+        print("START TEST CASE {name}".format(name=test_name[num]))
+        print("************************************")
         fail_count = func(*args, **kwargs)
-        print("Test Case {num}: END\n".format(num=num))
-        print("========================================================")
+        print("************************************")
+        print("END TEST CASE {name}".format(name=test_name[num]))
+        print("************************************")
         return fail_count
     return inner
 
