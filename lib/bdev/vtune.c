74af349ddbe122108f455beac13409e238c0dd92
Daniel Verkamp <daniel.verkamp@intel.com>
2018-03-16 15:50:04 -0700

    bdev: disable new GCC 7 warning in VTune code
    The version of ittnotify_static.c shipped with VTune Amplifier XE 2017
    triggers the new GCC 7 -Wimplicit-fallthrough warning.
    
    Change-Id: I8a43beddf464853407639742b1eb89bcac39136d
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/404211
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- lib/bdev/vtune.c
+++ lib/bdev/vtune.c
@@ -38,6 +38,9 @@
 	__GNUC__ > 4 || \
 	(__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 #pragma GCC diagnostic ignored "-Wsign-compare"
+#if __GNUC__ >= 7
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#endif
 #endif
 
 #include "ittnotify_static.c"
