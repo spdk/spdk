d9621cec54e45b0f0fd08c196fc573e9cb40dbe3
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2018-03-19 12:30:51 +0100

    [RFC] lvol: display clone/snapshot status in get_bdevs
    Change-Id: I5df06798f3bd34bba89743aba896d129a0915d00
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut.c
+++ test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut.c
@@ -157,6 +157,43 @@ spdk_blob_close(struct spdk_blob *b, spdk_blob_op_complete cb_fn, void *cb_arg)
 {
 }
 
+int
+spdk_blob_get_clones(struct spdk_blob *snapshot, spdk_blob_id **ids, size_t *count)
+{
+	*count = 0;
+	return 0;
+}
+
+spdk_blob_id
+spdk_blob_get_snapshot(struct spdk_blob *blob)
+{
+	return 0;
+}
+
+bool
+spdk_blob_is_read_only(struct spdk_blob *blob)
+{
+	return false;
+}
+
+bool
+spdk_blob_is_snapshot(struct spdk_blob *blob)
+{
+	return false;
+}
+
+bool
+spdk_blob_is_clone(struct spdk_blob *blob)
+{
+	return false;
+}
+
+bool
+spdk_blob_is_thin_provisioned(struct spdk_blob *blob)
+{
+	return false;
+}
+
 static struct spdk_lvol *_lvol_create(struct spdk_lvol_store *lvs);
 
 void
