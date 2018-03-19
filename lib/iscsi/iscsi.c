7da5cc1659d97c4494f0f0a31023b250810c53c0
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-03-19 14:28:02 +0900

    iscsi: the result residual count check for read is always no
    In spdk_iscsi_transfer_in() residual count is checked.
    However, because
    - in spdk_bdev_scsi_read() task->length is substituted for
      task->data_transferred.
    - even for primary this check is done before accumulation of
      task->data_transferred is started in spdk_iscsi_send_datain(),
    the result of check is always no residual (data_len == transfer_len).
    
    Hence this check does not server any function.
    
    Remove this code as a refactoring to implement correct residual
    count handling.
    
    In spdk_iscsi_task_response() residual count is checked too. This
    is still under investigation.
    
    Change-Id: I3eca18e18e62dda07dd7b44b06bda541af309981
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/iscsi/iscsi.c
+++ lib/iscsi/iscsi.c
@@ -2745,7 +2745,6 @@ spdk_iscsi_transfer_in(struct spdk_iscsi_conn *conn,
 {
 	uint32_t DataSN;
 	int transfer_len;
-	int data_len;
 	int segment_len;
 	int offset;
 	int residual_len = 0;
@@ -2759,7 +2758,6 @@ spdk_iscsi_transfer_in(struct spdk_iscsi_conn *conn,
 
 	primary = spdk_iscsi_task_get_primary(task);
 	segment_len = conn->MaxRecvDataSegmentLength;
-	data_len = task->scsi.data_transferred;
 	transfer_len = task->scsi.length;
 
 	if (task->scsi.status != SPDK_SCSI_STATUS_GOOD) {
@@ -2780,24 +2778,7 @@ spdk_iscsi_transfer_in(struct spdk_iscsi_conn *conn,
 		return 0;
 	}
 
-	if (data_len < transfer_len) {
-		/* underflow */
-		SPDK_DEBUGLOG(SPDK_LOG_ISCSI, "Underflow %u/%u\n",
-			      data_len, transfer_len);
-		residual_len = transfer_len - data_len;
-		transfer_len = data_len;
-		datain_flag |= ISCSI_DATAIN_UNDERFLOW;
-	} else if (data_len > transfer_len) {
-		/* overflow */
-		SPDK_DEBUGLOG(SPDK_LOG_ISCSI, "Overflow %u/%u\n",
-			      data_len, transfer_len);
-		residual_len = data_len - transfer_len;
-		datain_flag |= ISCSI_DATAIN_OVERFLOW;
-	} else {
-		SPDK_DEBUGLOG(SPDK_LOG_ISCSI, "Transfer %u\n",
-			      transfer_len);
-		residual_len = 0;
-	}
+	SPDK_DEBUGLOG(SPDK_LOG_ISCSI, "Transfer %u\n", transfer_len);
 
 	DataSN = primary->datain_datasn;
 	sent_status = 0;
