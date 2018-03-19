b0d61a60a00bf8e6cad40fb04bc9a94eeb0cb49b
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2018-03-19 14:44:30 +0100

    test/common: don't hide bdev_svc output
    The bdev_svc can occasionally hang. Any TP
    will timeout and terminate the test, leaving no
    test output at all. bdev_svc is unlikely to
    fail, but it's still allowed to. Let's reenable
    it's output.
    
    Change-Id: I558065e5e492d19394b2ef12bb9a6464b0ce28c1
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -440,7 +440,7 @@ function discover_bdevs()
 	# Start the bdev service to query for the list of available
 	# bdevs.
 	$rootdir/test/app/bdev_svc/bdev_svc -r $rpc_server -i 0 -s 1024 -g \
-		-c $config_file &>/dev/null &
+		-c $config_file &
 	stubpid=$!
 	while ! [ -e /var/run/spdk_bdev0 ]; do
 		sleep 1
