98503f23ce0790030393e279a5b38c07fa076a33
Daniel Verkamp <daniel.verkamp@intel.com>
2018-03-19 17:37:58 -0700

    scripts/rpc.py: use relative imports
    This is necessary for rpc.py to work with Python 3.
    
    Change-Id: I4bd57dd366941b2c12dd52d2a2f812abb5bafac7
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- scripts/rpc/__init__.py
+++ scripts/rpc/__init__.py
@@ -1,16 +1,17 @@
-import app
-import bdev
-import iscsi
-import log
-import lvol
-import nbd
-import net
-import nvmf
-import pmem
-import subsystem
-import vhost
 import json
 import sys
+
+from . import app
+from . import bdev
+from . import iscsi
+from . import log
+from . import lvol
+from . import nbd
+from . import net
+from . import nvmf
+from . import pmem
+from . import subsystem
+from . import vhost
 
 
 def get_rpc_methods(args):
