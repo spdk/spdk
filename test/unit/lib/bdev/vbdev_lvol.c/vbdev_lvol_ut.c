adaeb7d37aabaad1eafaed69b96ac1060164e422
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2018-03-22 16:03:17 +0100

    blobstore: clone-snapshot blobstore relations
    This commit provides an API to obtain an information about
    snapshot and clone relations.
    
    The main objective is:
    
     1) Determinate if we can delete snapshot (if have some created
        clones),
    
     2) Provide an information about parent/children nodes to the upper
        layer (e.g. lvol)
    
    Realization:
    
     1) Structure parent-children is stored in the blob store object
        and updated on:
    
         a) blob store load,
    
         b) blob create/delete,
    
     2) Full information about parent-children is provided via new API:
        spdk_blob_get_parent() and spdk_blob_get_children(),
    
    Note:
    
        While we don't store an information about these relations in the
        blob store, we need to open all blobs on blob store load to create
        it. It should be considered that it have an impact on the blobstore
        loading performance.
    
    Change-Id: Ie0237fa5b93af01aa73d1f68ac1694e653fb75e5
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut.c
+++ test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut.c
@@ -163,6 +163,38 @@ spdk_blob_get_num_clusters(struct spdk_blob *b)
 	return 0;
 }
 
+int
+spdk_blob_get_clones(struct spdk_blob_store *bs, spdk_blob_id blobid, spdk_blob_id *ids,
+		     size_t *count)
+{
+	*count = 0;
+	return 0;
+}
+
+spdk_blob_id
+spdk_blob_get_parent_snapshot(struct spdk_blob_store *bs, spdk_blob_id blobid)
+{
+	return 0;
+}
+
+bool
+spdk_blob_is_snapshot(struct spdk_blob *blob)
+{
+	return false;
+}
+
+bool
+spdk_blob_is_clone(struct spdk_blob *blob)
+{
+	return false;
+}
+
+bool
+spdk_blob_is_thin_provisioned(struct spdk_blob *blob)
+{
+	return false;
+}
+
 static struct spdk_lvol *_lvol_create(struct spdk_lvol_store *lvs);
 
 void
