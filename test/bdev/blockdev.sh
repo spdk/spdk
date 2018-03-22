2eb5670fcdbcc10ba78a424cda808fafe8badb0b
Seth Howell <seth.howell@intel.com>
2018-03-22 12:43:32 -0700

    test: move the bdev directory up one level
    Change-Id: I6e291c3361b9b8529e4ad44c4bc41bdb4b3b2b47
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/bdev/blockdev.sh
+++ test/bdev/blockdev.sh
@@ -3,7 +3,7 @@
 set -e
 
 testdir=$(readlink -f $(dirname $0))
-rootdir=$(readlink -f $testdir/../../..)
+rootdir=$(readlink -f $testdir/../..)
 plugindir=$rootdir/examples/bdev/fio_plugin
 
 function run_fio()
