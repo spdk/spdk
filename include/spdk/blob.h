b0e6ba09c47b59b2c7d36433ef7518920758a336
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2018-03-22 16:03:17 +0100

    blobstore: clone-snapshot blobstore relations
    This commit provides an API to obtain an information about
    snapshot and clone relations.
    
    The main objective is:
    
     1) Determinate if we can delete snapshot (if have some created
        clones),
    
     2) Provide an information about parent/children nodes to the upper
        layer (e.g. lvol)
    
    Realization:
    
     1) Structure parent-children is stored in the blob store object
        and updated on:
    
         a) blob store load,
    
         b) blob create/delete,
    
     2) Full information about parent-children is provided via new API:
        spdk_blob_get_parent() and spdk_blob_get_children(),
    
    Note:
    
        While we don't store an information about these relations in the
        blob store, we need to open all blobs on blob store load to create
        it. It should be considered that it have an impact on the blobstore
        loading performance.
    
    Change-Id: Ie0237fa5b93af01aa73d1f68ac1694e653fb75e5
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- include/spdk/blob.h
+++ include/spdk/blob.h
@@ -391,6 +391,29 @@ void spdk_bs_create_clone(struct spdk_blob_store *bs, spdk_blob_id blobid,
 			  spdk_blob_op_with_id_complete cb_fn, void *cb_arg);
 
 /**
+ * Provide table with blob id's of clones are dependent on specified snapshot.
+ *
+ * \param bs blobstore.
+ * \param blobid Snapshots blob id.
+ * \param ids Array of the clone ids.
+ * \param count Number of clones in ids array.
+ *
+ * \return -EINVAL if specified blob is not a snapshot.
+ */
+int spdk_blob_get_clones(struct spdk_blob_store *bs, spdk_blob_id blobid, spdk_blob_id *ids,
+			   size_t *count);
+
+/**
+ * Get snapshot blob id of the clone.
+ *
+ * \param bs blobstore.
+ * \param blobid Clones blob id.
+ *
+ * \return blob id of parent blob or SPDK_BLOBID_INVALID if have no parent
+ */
+spdk_blob_id spdk_blob_get_parent_snapshot(struct spdk_blob_store *bs, spdk_blob_id blobid);
+
+/**
  * Check if blob is read only.
  *
  * \param blob Blob.
@@ -407,6 +430,15 @@ bool spdk_blob_is_read_only(struct spdk_blob *blob);
  * \return true if blob is a snapshot.
  */
 bool spdk_blob_is_snapshot(struct spdk_blob *blob);
+
+/**
+ * Check if blob is a clone.
+ *
+ * \param blob Blob.
+ *
+ * \return true if blob is a clone.
+ */
+bool spdk_blob_is_clone(struct spdk_blob *blob);
 
 /**
  * Check if blob is thin-provisioned.
