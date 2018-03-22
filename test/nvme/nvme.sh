e2965d8d8aa331bdc6160de039146bc3b882a531
Seth Howell <seth.howell@intel.com>
2018-03-22 15:18:32 -0700

    test: move lib/nvme up one directory
    Change-Id: I40c29b30c7098e34f2aa860612eeb82f2fb01ff3
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/nvme/nvme.sh
+++ test/nvme/nvme.sh
@@ -140,7 +140,7 @@ $testdir/e2edp/nvme_dp
 timing_exit e2edp
 
 timing_enter overhead
-$rootdir/test/lib/nvme/overhead/overhead -s 4096 -t 1 -H
+$testdir/overhead/overhead -s 4096 -t 1 -H
 timing_exit overhead
 
 timing_enter arbitration
