3cbb5526b8831127b4936fc08569db5b5cb6ea72
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2018-03-22 16:25:38 +0100

    blobstore: opened clone-snapshot relations
    This patch adds the relations between opened snapshots and
    clones.
    
    Change-Id: Iabb9d0c501b1db49c34caafde1c97a7d3d908a0f
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -184,6 +184,8 @@ _spdk_blob_alloc(struct spdk_blob_store *bs, spdk_blob_id id)
 	TAILQ_INIT(&blob->xattrs);
 	TAILQ_INIT(&blob->xattrs_internal);
 
+	TAILQ_INIT(&blob->clones);
+
 	return blob;
 }
 
@@ -810,6 +812,10 @@ _spdk_blob_load_snapshot_cpl(void *cb_arg, struct spdk_blob *snapshot, int bserr
 		bserrno = -ENOMEM;
 		goto error;
 	}
+
+	blob->snapshot = snapshot;
+
+	TAILQ_INSERT_TAIL(&snapshot->clones, blob, next_clone);
 
 	_spdk_blob_load_final(ctx, bserrno);
 	return;
@@ -3669,6 +3675,8 @@ _spdk_bs_snapshot_origblob_sync_cpl(void *cb_arg, int bserrno)
 		return;
 	}
 
+	TAILQ_INSERT_TAIL(&newblob->clones, ctx->original.blob, next_clone);
+
 	spdk_blob_set_read_only(newblob);
 
 	/* sync snapshot metadata */
@@ -3703,6 +3711,9 @@ _spdk_bs_snapshot_newblob_sync_cpl(void *cb_arg, int bserrno)
 
 	/* set clone blob as thin provisioned */
 	_spdk_blob_set_thin_provision(origblob);
+
+	origblob->snapshot = newblob;
+	//TAILQ_INSERT_TAIL(&newblob->clones, origblob, next_clone);
 
 	/* Zero out origblob cluster map */
 	memset(origblob->active.clusters, 0,
@@ -4008,6 +4019,10 @@ _spdk_bs_delete_open_cpl(void *cb_arg, struct spdk_blob *blob, int bserrno)
 		return;
 	}
 
+	if (spdk_blob_is_clone(blob)) {
+		TAILQ_REMOVE(&blob->snapshot->clones, blob, next_clone);
+	}
+
 	/*
 	 * Remove the blob from the blob_store list now, to ensure it does not
 	 *  get returned after this point by _spdk_blob_lookup().
@@ -4264,6 +4279,9 @@ _spdk_blob_close_cpl(spdk_bs_sequence_t *seq, void *cb_arg, int bserrno)
 			if (blob->active.num_pages > 0) {
 				TAILQ_REMOVE(&blob->bs->blobs, blob, link);
 			}
+			if (blob->snapshot != NULL /* && !TAILQ_EMPTY(&blob->snapshot->clones) */) {
+				TAILQ_REMOVE(&blob->snapshot->clones, blob, next_clone);
+			}
 			_spdk_blob_free(blob);
 		}
 	}
@@ -4283,6 +4301,12 @@ void spdk_blob_close(struct spdk_blob *blob, spdk_blob_op_complete cb_fn, void *
 	if (blob->open_ref == 0) {
 		cb_fn(cb_arg, -EBADF);
 		return;
+	}
+
+	/* Check if blob have opened clones */
+	if ((blob->open_ref == 1) && !TAILQ_EMPTY(&blob->clones)) {
+		//cb_fn(cb_arg, -EBUSY);
+		//return;
 	}
 
 	cpl.type = SPDK_BS_CPL_TYPE_BLOB_BASIC;
