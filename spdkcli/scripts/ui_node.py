a55a311cfc380b2dd717f50344aff6993f5f8cfc
Karol Latecki <karol.latecki@intel.com>
2018-03-23 11:40:31 +0100

    [RFC] spdkcli: initial version
    Initial basic version with CLI and Malloc bdev management.
    
    Change-Id: I1a03d7660dad0335e25734b8ffb90592a5b337c2
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

---
+++ spdkcli/scripts/ui_node.py
@@ -0,0 +1,94 @@
+from configshell_fb import ConfigNode, ExecutionError
+from .bk_end import BKRoot
+
+
+class UINode(ConfigNode):
+    def __init__(self, name, parent=None, shell=None):
+        ConfigNode.__init__(self, name, parent, shell)
+
+    def refresh(self):
+        for child in self.children:
+            child.refresh()
+
+    def ui_command_refresh(self):
+        self.refresh()
+
+    def ui_command_ll(self, path=None, depth=None):
+        self.ui_command_ls(path, depth)
+
+
+class UIBdevs(UINode):
+    def __init__(self, parent):
+        UINode.__init__(self, 'bdevs', parent)
+        self.refresh()
+
+    def refresh(self):
+        self._children = set([])
+        BKRoot.list_bdevs()
+        UIMallocBdev(self)
+
+    def ui_command_refresh(self):
+        self.refresh()
+
+    def ui_command_delete(self, name):
+        BKRoot.delete_bdev(name=name)
+        self.refresh()
+
+
+class UIBdev(UINode):
+    def __init__(self, name, parent):
+        UINode.__init__(self, name, parent)
+        self.refresh()
+
+    def refresh(self):
+        self._children = set([])
+
+        for bdev in BKRoot.get_bdevs(self.name):
+            UIBdevObj(bdev, self)
+
+    def ui_command_delete(self, name):
+        BKRoot.delete_bdev(name=name)
+        self.refresh()
+
+    def ui_command_refresh(self):
+        self.refresh()
+
+
+class UIMallocBdev(UIBdev):
+    def __init__(self, parent):
+        UIBdev.__init__(self, "Malloc", parent)
+
+    def summary(self):
+        bdevs = len(self.children)
+        return "Malloc bdevs: %d" % bdevs, None
+
+    def ui_command_create(self, size, block_size, name=None, uuid=None):
+        BKRoot.create_malloc_bdev(total_size=int(size),
+                                  block_size=int(block_size),
+                                  name=name, uuid=uuid)
+        self.refresh()
+
+
+class UIAIOBdev(UIBdev):
+    def __init__(self, parent):
+        UIBdev.__init__(self, "AIO", parent)
+
+    def summary(self):
+        bdevs = len(self.children)
+        return "AIO bdevs: %d" % bdevs, None
+
+
+class UIBdevObj(UINode):
+    def __init__(self, bdev, parent):
+        UINode.__init__(self, bdev.name, parent)
+        self.bdev = bdev
+
+    def summary(self):
+        # TODO: Size always in MB's for now, add util function for that
+        # and discover when to use kB's, MB's or GB's
+        size = self.bdev.block_size * self.bdev.num_blocks / 1024 / 1024
+        in_use = "FREE"
+        if bool(self.bdev.claimed):
+            in_use = "IN USE"
+
+        return "Size=%sM, %s" % (size, in_use), True
