a445cfbb6fd6f6038119fb66b243d784d496cb01
Slawomir Mrozowicz <slawomirx.mrozowicz@intel.com>
2018-03-26 11:39:58 +0200

    [RFC] vpp: include VPP into iscsi test scripts
    Change-Id: I7c80427ca1675a1789ce7440796cc8d9956f1c9e
    Signed-off-by: Slawomir Mrozowicz <slawomirx.mrozowicz@intel.com>
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/iscsi_tgt/pmem/iscsi_pmem.sh
+++ test/iscsi_tgt/pmem/iscsi_pmem.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-export TARGET_IP=127.0.0.1
-export INITIATOR_IP=127.0.0.1
-
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../../..)
 source $rootdir/test/common/autotest_common.sh
