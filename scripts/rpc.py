0d7df5d54b6cb6603d8d613ce300a7faaa22ed88
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-03-27 15:45:10 +0200

    bdev/virito: add JSON config dump and generic create RPC
    The new construct_virtio_dev allow creating virtio SCSI and blk for both
    PCI and user transports.
    
    Change-Id: Ibd79c4fb75e3cbd993b46227d86e915c1b740a18
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -759,6 +759,23 @@ if __name__ == "__main__":
     p.add_argument('ctrlr', help='controller name')
     p.set_defaults(func=remove_vhost_controller)
 
+    def construct_virtio_dev(args):
+        print_dict(rpc.vhost.construct_virtio_dev(args))
+
+    p = subparsers.add_parser('construct_virtio_dev', help="""Construct new virtio device using provided
+    transport type and device type. In case of SCSI device type this imply scan and add bdevs offered by
+    remote side. Result is array of added bdevs.""")
+    p.add_argument('name', help="Use this name as base for new created bdevs")
+    p.add_argument('-t', '--trtype',
+                   help='Virtio target transport type: pci or user', required=True)
+    p.add_argument('-a', '--traddr',
+                   help='Transport type specifig target address: e.g. UNIX domain socket path or BDF', required=True)
+    p.add_argument('-d', '--dev-type',
+                   help='Device type: blk or scsi', required=True)
+    p.add_argument('--vq-count', help='Number of virtual queues to be used.', type=int)
+    p.add_argument('--vq-size', help='Size of each queue', type=int)
+    p.set_defaults(func=construct_virtio_dev)
+
     def construct_virtio_user_scsi_bdev(args):
         print_dict(rpc.vhost.construct_virtio_user_scsi_bdev(args))
 
