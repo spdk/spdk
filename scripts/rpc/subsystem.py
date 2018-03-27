673500ad0d72ce4ceebc1bed527bddeee176dbcf
Daniel Verkamp <daniel.verkamp@intel.com>
2018-03-27 14:04:40 -0700

    scripts/rpc.py: pass named args to subsystem.py
    As an example of how we want the Python wrappers for the RPC interface
    to look, convert the methods in scripts/rpc/subsystem.py to have
    explicitly named arguments and pass the JSONRPCClient object explicitly.
    
    Change-Id: I9d2e194ce7fde535d323383925f7825ab93909de
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- scripts/rpc/subsystem.py
+++ scripts/rpc/subsystem.py
@@ -1,7 +1,7 @@
-def get_subsystems(args):
-    return args.client.call('get_subsystems')
+def get_subsystems(client):
+    return client.call('get_subsystems')
 
 
-def get_subsystem_config(args):
-    params = {'name': args.name}
-    return args.client.call('get_subsystem_config', params)
+def get_subsystem_config(client, name):
+    params = {'name': name}
+    return client.call('get_subsystem_config', params)
