80d2e164ba43c8328d1dd741af237a02b3d85088
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2018-03-27 09:56:16 -0400

    test/iscsi: parametrize PORTAL_TAG
    Change-Id: I58b53634fea07db219b20ee5b74cf31e708279a1
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-on: https://review.gerrithub.io/405548
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/iscsi_tgt/nvme_remote/fio_remote_nvme.sh
+++ test/iscsi_tgt/nvme_remote/fio_remote_nvme.sh
@@ -53,7 +53,7 @@ echo "iSCSI target has started."
 timing_exit start_iscsi_tgt
 
 echo "Creating an iSCSI target node."
-$rpc_py -s "$iscsi_rpc_addr" add_portal_group 1 $TARGET_IP:$ISCSI_PORT
+$rpc_py -s "$iscsi_rpc_addr" add_portal_group $PORTAL_TAG $TARGET_IP:$ISCSI_PORT
 $rpc_py -s "$iscsi_rpc_addr" add_initiator_group 1 ANY $INITIATOR_IP/32
 if [ $1 -eq 0 ]; then
 	$rpc_py -s "$iscsi_rpc_addr" construct_nvme_bdev -b "Nvme0" -t "rdma" -f "ipv4" -a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT -n nqn.2016-06.io.spdk:cnode1
