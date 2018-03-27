9d0721f4d5dd8d92b6b7916e1ac9c79728adbe56
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2018-03-27 10:44:08 -0400

    [RFC] test/iscsi: use veth interfaces instead of loopback
    At this time all iSCSI tests were using local address
    127.0.0.1 for both sides of connection.
    
    This patch changes iSCSI tests to use separate
    veth (virtual ethernet) interfaces.
    It will allow to actually verify functionality when using
    different target and initiator IPs.
    Veth are used as most closely resembling real enviroment
    in single host and without any network hardware required.
    
    Network namespaces are used to assure that kernel
    is not routing packets within host stack,
    but they go through veth interfaces.
    
    This patch serves as a base for future VPP test changes,
    where veth interfaces are used as well.
    
    Change-Id: Ida8fce107e8262bef94b2161e0197c45f6e3f070
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/iscsi_tgt/common.sh
+++ test/iscsi_tgt/common.sh
@@ -1,10 +1,39 @@
+# Network configuration
+TARGET_INTERFACE="spdk_tgt_int"
+INITIATOR_INTERFACE="spdk_init_int"
+TARGET_NAMESPACE="spdk_iscsi_ns"
+TARGET_NS_CMD="ip netns exec $TARGET_NAMESPACE"
+
 # iSCSI target configuration
-TARGET_IP=127.0.0.1
-INITIATOR_IP=127.0.0.1
+TARGET_IP=10.11.12.1
+INITIATOR_IP=10.11.12.2
 ISCSI_PORT=3260
 NETMASK=$INITIATOR_IP/32
 INITIATOR_TAG=2
 INITIATOR_NAME=ANY
 PORTAL_TAG=1
-ISCSI_APP="./app/iscsi_tgt/iscsi_tgt -i 0"
+ISCSI_APP="$TARGET_NS_CMD ./app/iscsi_tgt/iscsi_tgt -i 0"
 ISCSI_TEST_CORE_MASK=0xFF
+
+function create_veth_interfaces() {
+	# Create veth (Virtual ethernet) interface pair
+	ip link add $INITIATOR_INTERFACE type veth peer name $TARGET_INTERFACE
+	ip addr add $INITIATOR_IP dev $INITIATOR_INTERFACE
+	ip link set $INITIATOR_INTERFACE up
+
+	# Create and add interface for target to network namespace
+	ip netns add $TARGET_NAMESPACE
+	ip link set $TARGET_INTERFACE netns $TARGET_NAMESPACE
+
+	$TARGET_NS_CMD ip addr add $TARGET_IP dev $TARGET_INTERFACE
+	$TARGET_NS_CMD ip link set $TARGET_INTERFACE up
+
+	trap "cleanup_veth_interfaces; exit 1" SIGINT SIGTERM EXIT
+}
+
+function cleanup_veth_interfaces() {
+	# Cleanup veth interfaces and network namespace
+	# Note: removing one veth, removes the pair
+	ip link delete $INITIATOR_INTERFACE
+	ip netns del $TARGET_NAMESPACE
+}
