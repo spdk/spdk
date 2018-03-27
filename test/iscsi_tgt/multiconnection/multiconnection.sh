6e1a41e6681c75537f9dfd990ed9ed35565e8925
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2018-03-27 09:56:16 -0400

    test/iscsi: parametrize PORTAL_TAG
    Change-Id: I58b53634fea07db219b20ee5b74cf31e708279a1
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/iscsi_tgt/multiconnection/multiconnection.sh
+++ test/iscsi_tgt/multiconnection/multiconnection.sh
@@ -56,7 +56,7 @@ trap "remove_backends; iscsicleanup; killprocess $iscsipid; exit 1" SIGINT SIGTE
 waitforlisten $iscsipid
 timing_exit start_iscsi_tgt
 
-$rpc_py add_portal_group 1 $TARGET_IP:$ISCSI_PORT
+$rpc_py add_portal_group $PORTAL_TAG $TARGET_IP:$ISCSI_PORT
 $rpc_py add_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
 
 echo "Creating an iSCSI target node."
