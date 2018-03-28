c9253b8f237ae709a07341cbb0a464824dcb4117
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-03-28 19:20:18 +0200

    test/vhost: shorten tests by reducing file size on fio tests
    There are timeouts in NIGHTLY_TEST mainly caused by long duration
    of spdk_vhost.sh --fs-integrity-blk test. It could be shorten by
    reducing file size in fio test
    
    Change-Id: Ic99f6f3ce52ed8bf1fafc75bb0f908d9df0d0d4e
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
    Reviewed-on: https://review.gerrithub.io/405587
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/vhost/integrity/integrity_vm.sh
+++ test/vhost/integrity/integrity_vm.sh
@@ -43,7 +43,7 @@ for fs in $fs; do
 		fio --name="integrity" --bsrange=4k-512k --iodepth=128 --numjobs=1 --direct=1 \
 		--thread=1 --group_reporting=1 --rw=randrw --rwmixread=70 \
 		--filename=/mnt/${dev}dir/test_file --verify=md5 --do_verify=1 \
-		--verify_backlog=1024 --fsync_on_close=1 --runtime=20 --time_based=1 --size=1G
+		--verify_backlog=1024 --fsync_on_close=1 --runtime=20 --time_based=1 --size=512m
 
 		# Print out space consumed on target device
 		df -h /dev/$dev
