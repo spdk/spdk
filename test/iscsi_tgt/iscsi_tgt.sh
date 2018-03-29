06073f0fe127be4f47a3d9726af17e08cd9dd54d
Slawomir Mrozowicz <slawomirx.mrozowicz@intel.com>
2018-03-29 15:28:58 +0200

    [RFC] vpp: include VPP into iscsi test scripts
    Change-Id: I7c80427ca1675a1789ce7440796cc8d9956f1c9e
    Signed-off-by: Slawomir Mrozowicz <slawomirx.mrozowicz@intel.com>
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/iscsi_tgt/iscsi_tgt.sh
+++ test/iscsi_tgt/iscsi_tgt.sh
@@ -11,6 +11,17 @@ source $rootdir/test/iscsi_tgt/common.sh
 
 timing_enter iscsi_tgt
 
+if [ $SPDK_TEST_VPP -eq 1 ]; then
+	export TARGET_IP=10.10.1.10
+	export INITIATOR_IP=10.10.1.11
+
+	start_vpp
+else
+	export TARGET_IP=127.0.0.1
+	export INITIATOR_IP=127.0.0.1
+fi
+
+
 # ISCSI_TEST_CORE_MASK is the biggest core mask specified by
 #  any of the iscsi_tgt tests.  Using this mask for the stub
 #  ensures that if this mask spans CPU sockets, that we will
@@ -19,7 +30,11 @@ timing_enter iscsi_tgt
 #  core 0) so there is no impact to the iscsi_tgt tests by
 #  specifying the bigger core mask.
 start_stub "-s 2048 -i 0 -m $ISCSI_TEST_CORE_MASK"
-trap "kill_stub; exit 1" SIGINT SIGTERM EXIT
+if [ $SPDK_TEST_VPP -eq 1 ]; then
+	trap "kill_stub; kill_vpp; exit 1" SIGINT SIGTERM EXIT
+else
+	trap "kill_stub; exit 1" SIGINT SIGTERM EXIT
+fi
 
 run_test ./test/iscsi_tgt/calsoft/calsoft.sh
 run_test ./test/iscsi_tgt/filesystem/filesystem.sh
@@ -58,6 +73,10 @@ fi
 
 if [ $SPDK_TEST_ISCSI_INITIATOR -eq 1 ]; then
 	run_test ./test/iscsi_tgt/initiator/initiator.sh
+fi
+
+if [ $SPDK_TEST_VPP -eq 1 ]; then
+	kill_vpp
 fi
 
 timing_exit iscsi_tgt
