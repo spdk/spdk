3570c0c63345a62ca49699e33a88d76956fd46aa
Daniel Verkamp <daniel.verkamp@intel.com>
2018-03-30 13:04:08 -0700

    env_dpdk: use public function to check VFIO status
    DPDK 17.11 added a public API, rte_vfio_is_enabled(), that we can use
    instead of declaring and using pci_vfio_is_enabled().  This removes one
    of the remaining non-public DPDK symbols we are currently using, getting
    us closer to building against the shared library version of DPDK.
    
    Change-Id: Idf4ee66d4868cf542521fa2896ed8c609d42ee29
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/env_dpdk/vtophys.c
+++ lib/env_dpdk/vtophys.c
@@ -53,9 +53,6 @@
 #define SPDK_VFIO_ENABLED 1
 #include <linux/vfio.h>
 
-/* Internal DPDK function forward declaration */
-int pci_vfio_is_enabled(void);
-
 struct spdk_vfio_dma_map {
 	struct vfio_iommu_type1_dma_map map;
 	TAILQ_ENTRY(spdk_vfio_dma_map) tailq;
@@ -372,6 +369,20 @@ spdk_vtophys_notify(void *cb_ctx, struct spdk_mem_map *map,
 }
 
 #if SPDK_VFIO_ENABLED
+
+static bool
+spdk_vfio_enabled(void)
+{
+#if RTE_VERSION >= RTE_VERSION_NUM(17, 11, 0, 3)
+	return rte_vfio_is_enabled("vfio_pci");
+#else
+	/* Internal DPDK function forward declaration */
+	int pci_vfio_is_enabled(void);
+
+	return pci_vfio_is_enabled();
+#endif
+}
+
 static void
 spdk_vtophys_iommu_init(void)
 {
@@ -381,7 +392,7 @@ spdk_vtophys_iommu_init(void)
 	DIR *dir;
 	struct dirent *d;
 
-	if (!pci_vfio_is_enabled()) {
+	if (!spdk_vfio_enabled()) {
 		return;
 	}
 
