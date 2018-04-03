f017619a9f10c43e3315f4fa568485a511594f65
Ben Walker <benjamin.walker@intel.com>
2018-04-03 11:35:04 -0700

    nvmf: Make spdk_nvmf_tgt_listen asynchronous
    This was internally asynchronous already, but make it
    explicitly asynchronous so other code can properly
    wait on the operation to complete.
    
    This fixes an intermittent CI crash.
    
    Change-Id: I81c9b19673566047dcffa94796236ca9fd7fa7d0
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/nvmf/nvmf.c
+++ lib/nvmf/nvmf.c
@@ -215,14 +215,32 @@ spdk_nvmf_tgt_destroy(struct spdk_nvmf_tgt *tgt)
 }
 
 struct spdk_nvmf_tgt_listen_ctx {
+	struct spdk_nvmf_tgt *tgt;
 	struct spdk_nvmf_transport *transport;
 	struct spdk_nvme_transport_id trid;
+
+	listen_done_fn cb_fn;
+	void *cb_arg;
 };
 
 static void
 spdk_nvmf_tgt_listen_done(struct spdk_io_channel_iter *i, int status)
 {
-	void *ctx = spdk_io_channel_iter_get_ctx(i);
+	struct spdk_nvmf_tgt_listen_ctx *ctx = spdk_io_channel_iter_get_ctx(i);
+	int rc;
+
+	if (!status) {
+		rc = spdk_nvmf_transport_listen(ctx->transport, &ctx->trid);
+		if (rc < 0) {
+			SPDK_ERRLOG("Unable to listen on address '%s'\n", ctx->trid.traddr);
+			ctx->cb_fn(ctx->cb_arg, rc);
+			return;
+		}
+
+		ctx->tgt->discovery_genctr++;
+	}
+
+	ctx->cb_fn(ctx->cb_arg, status);
 
 	free(ctx);
 }
@@ -239,9 +257,11 @@ spdk_nvmf_tgt_listen_add_transport(struct spdk_io_channel_iter *i)
 	spdk_for_each_channel_continue(i, rc);
 }
 
-int
+void
 spdk_nvmf_tgt_listen(struct spdk_nvmf_tgt *tgt,
-		     struct spdk_nvme_transport_id *trid)
+		     struct spdk_nvme_transport_id *trid,
+		     listen_done_fn cb_fn,
+		     void *cb_arg)
 {
 	struct spdk_nvmf_transport *transport;
 	int rc;
@@ -253,39 +273,43 @@ spdk_nvmf_tgt_listen(struct spdk_nvmf_tgt *tgt,
 		transport = spdk_nvmf_transport_create(tgt, trid->trtype);
 		if (!transport) {
 			SPDK_ERRLOG("Transport initialization failed\n");
-			return -EINVAL;
+			cb_fn(cb_arg, -EINVAL);
+			return;
 		}
 		TAILQ_INSERT_TAIL(&tgt->transports, transport, link);
 
 		ctx = calloc(1, sizeof(*ctx));
 		if (!ctx) {
-			return -ENOMEM;
+			cb_fn(cb_arg, -ENOMEM);
+			return;
 		}
 
-		ctx->trid = *trid;
+		ctx->tgt = tgt;
 		ctx->transport = transport;
+		ctx->trid = *trid;
+		ctx->cb_fn = cb_fn;
+		ctx->cb_arg = cb_arg;
 
-		/* Send a message to each poll group to notify it that a new transport
-		 * is available.
-		 * TODO: This call does not currently allow the user to wait for these
-		 * messages to propagate. It also does not protect against two calls
-		 * to this function overlapping
-		 */
 		spdk_for_each_channel(tgt,
 				      spdk_nvmf_tgt_listen_add_transport,
 				      ctx,
 				      spdk_nvmf_tgt_listen_done);
+
+		return;
 	}
 
 	rc = spdk_nvmf_transport_listen(transport, trid);
 	if (rc < 0) {
 		SPDK_ERRLOG("Unable to listen on address '%s'\n", trid->traddr);
-		return rc;
+		cb_fn(cb_arg, rc);
+		return;
 	}
 
 	tgt->discovery_genctr++;
 
-	return 0;
+	cb_fn(cb_arg, 0);
+
+	return;
 }
 
 struct spdk_nvmf_subsystem *
