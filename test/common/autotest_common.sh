fcdd68356a7606b695a63881d668d15ed12a1bbd
Jim Harris <james.r.harris@intel.com>
2018-04-03 16:07:39 -0700

    bdev/pmem: change all NVML strings to PMDK
    NVML (Non Volatile Memory Library) changed its name to
    PMDK (Persistent Memory Development Kit), so make the
    necessary changes to the SPDK repository.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Id07a87eafb4e9a3099603030c58c0e927bafa608

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -39,7 +39,7 @@ fi
 : ${SPDK_TEST_EVENT=1}; export SPDK_TEST_EVENT
 : ${SPDK_TEST_BLOBFS=1}; export SPDK_TEST_BLOBFS
 : ${SPDK_TEST_VHOST_INIT=1}; export SPDK_TEST_VHOST_INIT
-: ${SPDK_TEST_NVML=1}; export SPDK_TEST_NVML
+: ${SPDK_TEST_PMDK=1}; export SPDK_TEST_PMDK
 : ${SPDK_TEST_LVOL=1}; export SPDK_TEST_LVOL
 : ${SPDK_RUN_ASAN=1}; export SPDK_RUN_ASAN
 : ${SPDK_RUN_UBSAN=1}; export SPDK_RUN_UBSAN
@@ -106,7 +106,10 @@ if [ -f /usr/include/infiniband/verbs.h ]; then
 fi
 
 if [ -f /usr/include/libpmemblk.h ]; then
-	config_params+=' --with-nvml'
+	config_params+=' --with-pmdk'
+else
+	# PMDK not installed so disable PMDK tests explicitly here
+	SPDK_TEST_PMDK=0; export SPDK_TEST_PMDK
 fi
 
 if [ -d /usr/src/fio ]; then
