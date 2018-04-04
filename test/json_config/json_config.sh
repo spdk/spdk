6f2b9ec8c7dd7e84c4f89124a3f24424d507eff5
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-04-04 12:38:39 -0400

    json/rpc: Tests for bdev subsystem
    Change-Id: I89226bc9c05880e73523fb189116e7433a513244
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/json_config/json_config.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+set -e
+cd ../vhost/common
+. ./common.sh
+cd ../../json_config
+BASE_DIR=$(readlink -f $(dirname $0))
+
+vhost_rpc_py="python $SPDK_BUILD_DIR/scripts/rpc.py -s $(get_vhost_dir)/rpc.sock"
+nvme_rpc_py="python $SPDK_BUILD_DIR/scripts/rpc.py -s /var/tmp/spdk.sock"
+base_json_config=$BASE_DIR/base_config.json
+last_json_config=$BASE_DIR/last_config.json
+base_bdevs=$BASE_DIR/bdevs_base.txt
+last_bdevs=$BASE_DIR/bdevs_last.txt
+
+function run_vhost() {
+    notice "==============="
+    notice ""
+    notice "running SPDK"
+    notice ""
+    spdk_vhost_run --conf-path=$BASE_DIR
+    notice ""
+}
+
+# Add split section into vhost config
+function gen_config() {
+    cp $BASE_DIR/vhost.conf.base $BASE_DIR/vhost.conf.in
+    cat << END_OF_CONFIG >> $BASE_DIR/vhost.conf.in
+[Split]
+  Split Nvme0n1 2
+  Split Nvme1n1 2
+END_OF_CONFIG
+}
+
+function test_json_config() {
+    $rpc_py get_bdevs >> $base_bdevs
+    $rpc_py save_config -f $base_json_config
+    $rpc_py clear_config
+    $rpc_py load_config --filename $base_json_config
+    $rpc_py get_bdevs >> $last_bdevs
+    $rpc_py save_config -f $last_json_config
+    diff $base_json_config $last_json_config
+    diff $base_bdevs $last_bdevs
+    rm $last_bdevs $base_bdevs || true
+    rm $last_json_config $base_json_config || true
+}
+
+function upload_vhost() {
+    $rpc_py construct_null_bdev Null0 32 512
+    $rpc_py construct_malloc_bdev 128 512 --name Malloc0
+    $rpc_py construct_malloc_bdev 64 4096 --name Malloc1
+    $rpc_py construct_malloc_bdev 8 1024 --name Malloc2
+    $rpc_py construct_error_bdev Malloc2
+    $rpc_py construct_aio_bdev /root/sample_aio aio_disk 1024
+}
+
+function upload_nvmf() {
+    bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
+    bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
+    echo "BDEVS: $bdevs"
+
+    RDMA_IP_LIST=$(get_available_rdma_ips)
+    NVMF_FIRST_TARGET_IP=$(echo "$RDMA_IP_LIST" | head -n 1)
+    if [ -z $NVMF_FIRST_TARGET_IP ]; then
+        echo "no NIC for nvmf test"
+        return
+    fi
+
+    $rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 '' '' -a -s SPDK00000000000001 -n "$bdevs"
+    $rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t RDMA -a $NVMF_FIRST_TARGET_IP -s "$NVMF_PORT"
+}
+
+function upload_iscsi() {
+    # iSCSI target configuration
+    PORT=3260
+    INITIATOR_TAG=1
+    INITIATOR_NAME=ANY
+    NETMASK=$INITIATOR_IP/32
+    NVMF_PORT=4420
+    PMEM_SIZE=32
+    PMEM_BLOCK_SIZE=512
+    TGT_NR=3
+    PMEM_PER_TGT=1
+    rdma_device_init
+    RDMA_IP_LIST=$(get_available_rdma_ips)
+    NVMF_FIRST_TARGET_IP=$(echo "$RDMA_IP_LIST" | head -n 1)
+
+    if [ $NVMF_FIRST_TARGET_IP ]; then
+        $rpc_py construct_nvme_bdev -b "Nvme0" -t "rdma" -f "ipv4" -a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT -n nqn.2016-06.io.spdk:cnode1
+        $rpc_py construct_target_node Target1 Target1_alias 'Nvme0n1:0' '1:1' 64 -d
+    fi
+    for i in `seq 1 $TGT_NR`; do
+        INITIATOR_TAG=$((i+1))
+        $rpc_py add_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
+
+        luns=""
+        for j in `seq 1 $PMEM_PER_TGT`; do
+            $rpc_py create_pmem_pool /tmp/pool_file${i}_${j} $PMEM_SIZE $PMEM_BLOCK_SIZE
+            bdevs_name="$($rpc_py construct_pmem_bdev -n pmem${i}_${j} /tmp/pool_file${i}_${j})"
+            PMEM_BDEVS+="$bdevs_name "
+            luns+="$bdevs_name:$((j-1)) "
+        done
+        $rpc_py construct_target_node Target$i Target${i}_alias "$luns" "1:$INITIATOR_TAG " 256 -d
+    done
+    rbd_setup
+    $rpc_py construct_rbd_bdev $RBD_POOL $RBD_NAME 4096
+}
+
+function clean_upload_vhost() {
+    echo ""
+}
+
+function clean_upload_nvmf() {
+    echo ""
+}
+
+function clean_upload_iscsi() {
+    rbd_cleanup
+    PMEM_PER_TGT=1
+    TGT_NR=3
+
+    for i in `seq 2 $TGT_NR`; do
+        INITIATOR_TAG=$((i+1))
+        for j in `seq 1 $PMEM_PER_TGT`; do
+            $rpc_py delete_pmem_pool /tmp/pool_file${i}_${j}
+        done
+    done
+}
+
+function test_vhost() {
+    run_vhost
+
+    rpc_py="$vhost_rpc_py"
+    upload_vhost
+    test_json_config
+    clean_upload_vhost
+    spdk_vhost_kill
+}
+
+function test_nvmf() {
+    . $BASE_DIR/../nvmf/common.sh
+    MALLOC_BDEV_SIZE=64
+    MALLOC_BLOCK_SIZE=512
+
+    $BASE_DIR/../../app/nvmf_tgt/nvmf_tgt -c $BASE_DIR/../nvmf/nvmf.conf &
+    nvmfpid=$!
+    trap "killprocess $nvmfpid; exit 1" SIGINT SIGTERM EXIT
+    waitforlisten $nvmfpid
+
+    rpc_py="$nvme_rpc_py"
+    upload_nvmf
+    test_json_config
+    clean_upload_nvmf
+
+    #nvmfcleanup
+    killprocess $nvmfpid
+}
+
+function test_iscsi() {
+    . $BASE_DIR/../common/autotest_common.sh
+    . $BASE_DIR/../iscsi_tgt/common.sh
+    . $BASE_DIR/../nvmf/common.sh
+    $BASE_DIR/../../app/iscsi_tgt/iscsi_tgt -i 0 -c $BASE_DIR/../iscsi_tgt/calsoft/iscsi.conf -m 0x1 &
+    iscsipid=$!
+    trap "killprocess $iscsipid; exit 1 " SIGINT SIGTERM EXIT
+    waitforlisten $iscsipid
+
+    rpc_py="$nvme_rpc_py"
+    upload_iscsi
+    test_json_config
+    clean_upload_iscsi
+
+    killprocess $iscsipid
+}
+
+function on_error_exit() {
+    set +e
+    echo "Error on $1 - $2"
+    rm $last_bdevs $base_bdevs || true
+    rm $last_json_config $base_json_config || true
+    rm $BASE_DIR/vhost.conf.in || true
+    print_backtrace
+    exit 1
+}
+
+trap 'on_error_exit "${FUNCNAME}" "${LINENO}"' ERR
+gen_config
+modprobe nbd
+
+test_vhost
+test_nvmf
+test_iscsi
+
+rmmod nbd
