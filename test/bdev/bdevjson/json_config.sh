cd58939a915176b81f932aec4a7beaa29f694d0a
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-04-04 12:38:39 -0400

    json/rpc: Tests for bdev subsystem
    Change-Id: I89226bc9c05880e73523fb189116e7433a513244
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/bdev/bdevjson/json_config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -ex
+BDEV_JSON_DIR=$(readlink -f $(dirname $0))
+. $BDEV_JSON_DIR/../../json_config/common.sh
+
+function test_subsystems() {
+	# Export flag to skip the known bug that exists in librados
+	export ASAN_OPTIONS=new_delete_type_mismatch=0
+	run_spdk_tgt
+	rootdir=$(readlink -f $BDEV_JSON_DIR/../..)
+
+	rpc_py="$spdk_rpc_py"
+	clear_config_py="$spdk_clear_config_py"
+	create_bdev_subsystem_config
+	test_json_config
+
+	clean_bdev_subsystem_config
+	test_params
+	kill_targets
+}
+
+trap 'on_error_exit "${FUNCNAME}" "${LINENO}"' ERR
+modprobe nbd
+
+test_subsystems
+
+rmmod nbd
