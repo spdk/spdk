d729fd87144cb2c3f366399d3aadda6ebbdd3c2d
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-04-05 11:57:09 -0400

    test/json: Tests for virtio bdevs
    Change-Id: Ibb94abdfc6881aa3e8e6dceaf70e494f0146e385
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/json_config/clear_config.py
+++ test/json_config/clear_config.py
@@ -114,7 +114,16 @@ def clear_interface_subsystem(args, interface_config):
 
 
 def clear_vhost_subsystem(args, vhost_config):
-    pass
+    for vhost in reversed(vhost_config):
+        if 'method' in vhost:
+            method = vhost['method']
+            if method in ['add_vhost_scsi_lun']:
+                args.client.call("remove_vhost_scsi_target",
+                                 {"ctrlr": vhost['params']['ctrlr'],
+                                  "scsi_target_num": vhost['params']['scsi_target_num']})
+            elif method in ['construct_vhost_scsi_controller', 'construct_vhost_blk_controller',
+                            'construct_vhost_nvme_controller']:
+                args.client.call("remove_vhost_controller", {'ctrlr': vhost['params']['ctrlr']})
 
 
 def call_test_cmd(func):
