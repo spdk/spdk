spdk/spdk

commit 4c7733618a796069f7632e42b977a114dae7dc48

json/rpc: Fix. Support not fully implemented subsystems.
Change-Id: I3cbc052dd4732a48c0d182b90e0c1b12a2f218c6
Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>
Reviewed-on: https://review.gerrithub.io/406317
Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>

scripts/rpc/__init__.py
@@ -59,4 +59,6 @@ def load_config(client, args):
         if not config:
             continue
         for elem in subsystem['config']:
+            if not elem or 'method' not in config:
+                continue
             client.call(elem['method'], elem['params'])

commit a7b324790cca710c73b553fb5d270d0422a810b9

scripts/rpc.py: fix load_config method check
The "method" check was looking in the wrong object, so it would always
fail and not call any of the methods.

Change-Id: I4c91428523256912f47dfced95ff53cc1630284a
Fixes: 4c7733618a79 ("json/rpc: Fix. Support not fully implemented subsystems.")
Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

scripts/rpc/__init__.py
@@ -59,6 +59,6 @@ def load_config(client, args):
         if not config:
             continue
         for elem in subsystem['config']:
-            if not elem or 'method' not in config:
+            if not elem or 'method' not in elem:
                 continue
             client.call(elem['method'], elem['params'])