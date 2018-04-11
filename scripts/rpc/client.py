3142d978c35e1091b3959429236ec4d73296e1ab
Karol Latecki <karol.latecki@intel.com>
2018-04-11 14:42:58 +0200

    scripts/rpc.py: Handle socket connection error
    Handle socket exception, print it and exit cleanly.
    
    Change-Id: I7ac899dfe329c7512339549991abea8a6cb09608
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-on: https://review.gerrithub.io/407308
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- scripts/rpc/client.py
+++ scripts/rpc/client.py
@@ -16,17 +16,21 @@ class JSONRPCClient(object):
     def __init__(self, addr, port=None, verbose=False, timeout=60.0):
         self.verbose = verbose
         self.timeout = timeout
-        if addr.startswith('/'):
-            self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            self.sock.connect(addr)
-        elif ':' in addr:
-            for res in socket.getaddrinfo(addr, port, socket.AF_INET6, socket.SOCK_STREAM, socket.SOL_TCP):
-                af, socktype, proto, canonname, sa = res
-            self.sock = socket.socket(af, socktype, proto)
-            self.sock.connect(sa)
-        else:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((addr, port))
+        try:
+            if addr.startswith('/'):
+                self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                self.sock.connect(addr)
+            elif ':' in addr:
+                for res in socket.getaddrinfo(addr, port, socket.AF_INET6, socket.SOCK_STREAM, socket.SOL_TCP):
+                    af, socktype, proto, canonname, sa = res
+                self.sock = socket.socket(af, socktype, proto)
+                self.sock.connect(sa)
+            else:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.connect((addr, port))
+        except socket.error as ex:
+            raise JSONRPCException("Error while connecting to %s\n"
+                                   "Error details: %s" % (addr, ex))
 
     def __del__(self):
         self.sock.close()
