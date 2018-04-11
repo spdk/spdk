b6f502cba798b517b00d0635a53ebee24eefcb5c
Seth Howell <seth.howell@intel.com>
2018-04-10 17:07:17 -0700

    subsystem.c: make add_ns asynchronous.
    This function will now update the thread local information about
    namespaces as they are added to a subsystem. This requires it to be
    asynchronous and take a callback function.
    
    Change-Id: Ib2a7c8b049d2b9b84f9d21114ffa9bc03e844b4d
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvmf/subsystem.c
+++ lib/nvmf/subsystem.c
@@ -984,8 +984,8 @@ spdk_nvmf_ns_opts_get_defaults(struct spdk_nvmf_ns_opts *opts, size_t opts_size)
 }
 
 uint32_t
-spdk_nvmf_subsystem_add_ns(struct spdk_nvmf_subsystem *subsystem, struct spdk_bdev *bdev,
-			   const struct spdk_nvmf_ns_opts *user_opts, size_t opts_size)
+spdk_nvmf_subsystem_initialize_ns(struct spdk_nvmf_subsystem *subsystem, struct spdk_bdev *bdev,
+				  const struct spdk_nvmf_ns_opts *user_opts, size_t opts_size)
 {
 	struct spdk_nvmf_ns_opts opts;
 	struct spdk_nvmf_ns *ns;
@@ -1088,6 +1088,31 @@ spdk_nvmf_subsystem_add_ns(struct spdk_nvmf_subsystem *subsystem, struct spdk_bd
 	spdk_nvmf_subsystem_ns_changed(subsystem, opts.nsid);
 
 	return opts.nsid;
+}
+
+uint32_t
+spdk_nvmf_subsystem_add_ns(struct spdk_nvmf_subsystem *subsystem, struct spdk_bdev *bdev,
+			   const struct spdk_nvmf_ns_opts *user_opts, size_t opts_size,
+			   spdk_nvmf_subsystem_state_change_done cb_fn, void *cb_arg)
+{
+	uint32_t nsid;
+	struct subsystem_modify_ctx *ctx;
+
+	nsid = spdk_nvmf_subsystem_initialize_ns(subsystem, bdev, user_opts, opts_size);
+
+	if (nsid == 0) {
+		return nsid;
+	}
+
+	ctx = calloc(1, sizeof(*ctx));
+
+	ctx->subsystem = subsystem;
+	ctx->cb_fn = cb_fn;
+	ctx->cb_arg = cb_arg;
+
+	spdk_nvmf_subsystem_modify(subsystem, subsystem_modify_done, ctx);
+
+	return nsid;
 }
 
 static uint32_t
