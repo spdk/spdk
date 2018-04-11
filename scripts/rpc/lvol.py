7d45cfc3ccd9a2231ba1bb3240b1eff22c25f75e
Daniel Verkamp <daniel.verkamp@intel.com>
2018-04-11 14:31:09 -0700

    scripts/rpc.py: pass named args to lvol.py
    Also add docstrings to all lvol.py methods.
    
    Change-Id: I2fb0c0622450a8862d2d6182f1f800da4d15a780
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/407372
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: John Kariuki <John.K.Kariuki@intel.com>

--- scripts/rpc/lvol.py
+++ scripts/rpc/lvol.py
@@ -1,84 +1,150 @@
-def construct_lvol_store(client, args):
-    params = {'bdev_name': args.bdev_name, 'lvs_name': args.lvs_name}
-    if args.cluster_sz:
-        params['cluster_sz'] = args.cluster_sz
+def construct_lvol_store(client, bdev_name, lvs_name, cluster_sz=None):
+    """Construct a logical volume store.
+
+    Args:
+        bdev_name: bdev on which to construct logical volume store
+        lvs_name: name of the logical volume store to create
+        cluster_sz: cluster size of the logical volume store in bytes (optional)
+
+    Returns:
+        UUID of created logical volume store.
+    """
+    params = {'bdev_name': bdev_name, 'lvs_name': lvs_name}
+    if cluster_sz:
+        params['cluster_sz'] = cluster_sz
     return client.call('construct_lvol_store', params)
 
 
-def rename_lvol_store(client, args):
+def rename_lvol_store(client, old_name, new_name):
+    """Rename a logical volume store.
+
+    Args:
+        old_name: existing logical volume store name
+        new_name: new logical volume store name
+    """
     params = {
-        'old_name': args.old_name,
-        'new_name': args.new_name
+        'old_name': old_name,
+        'new_name': new_name
     }
     return client.call('rename_lvol_store', params)
 
 
-def construct_lvol_bdev(client, args):
-    num_bytes = (args.size * 1024 * 1024)
-    params = {'lvol_name': args.lvol_name, 'size': num_bytes}
-    if args.thin_provision:
-        params['thin_provision'] = args.thin_provision
-    if (args.uuid and args.lvs_name) or (not args.uuid and not args.lvs_name):
-        print("You need to specify either uuid or name of lvolstore")
-    else:
-        if args.uuid:
-            params['uuid'] = args.uuid
-        if args.lvs_name:
-            params['lvs_name'] = args.lvs_name
-        return client.call('construct_lvol_bdev', params)
+def construct_lvol_bdev(client, lvol_name, size, thin_provision=False, uuid=None, lvs_name=None):
+    """Create a logical volume on a logical volume store.
+
+    Args:
+        lvol_name: name of logical volume to create
+        size: desired size of logical volume in bytes (will be rounded up to a multiple of cluster size)
+        thin_provision: True to enable thin provisioning
+        uuid: UUID of logical volume store to create logical volume on (optional)
+        lvs_name: name of logical volume store to create logical volume on (optional)
+
+    Either uuid or lvs_name must be specified, but not both.
+    """
+    if (uuid and lvs_name) or (not uuid and not lvs_name):
+        raise ValueError("Either uuid or lvs_name must be specified, but not both")
+
+    params = {'lvol_name': lvol_name, 'size': size}
+    if thin_provision:
+        params['thin_provision'] = thin_provision
+    if uuid:
+        params['uuid'] = uuid
+    if lvs_name:
+        params['lvs_name'] = lvs_name
+    return client.call('construct_lvol_bdev', params)
 
 
-def snapshot_lvol_bdev(client, args):
+def snapshot_lvol_bdev(client, lvol_name, snapshot_name):
+    """Capture a snapshot of the current state of a logical volume.
+
+    Args:
+        lvol_name: logical volume to create a snapshot from
+        snapshot_name: name for the newly created snapshot
+    """
     params = {
-        'lvol_name': args.lvol_name,
-        'snapshot_name': args.snapshot_name
+        'lvol_name': lvol_name,
+        'snapshot_name': snapshot_name
     }
     return client.call('snapshot_lvol_bdev', params)
 
 
-def clone_lvol_bdev(client, args):
+def clone_lvol_bdev(client, snapshot_name, clone_name):
+    """Create a logical volume based on a snapshot.
+
+    Args:
+        snapshot_name: snapshot to clone
+        clone_name: name of logical volume to create
+    """
     params = {
-        'snapshot_name': args.snapshot_name,
-        'clone_name': args.clone_name
+        'snapshot_name': snapshot_name,
+        'clone_name': clone_name
     }
     return client.call('clone_lvol_bdev', params)
 
 
-def rename_lvol_bdev(client, args):
+def rename_lvol_bdev(client, old_name, new_name):
+    """Rename a logical volume.
+
+    Args:
+        old_name: existing logical volume name
+        new_name: new logical volume name
+    """
     params = {
-        'old_name': args.old_name,
-        'new_name': args.new_name
+        'old_name': old_name,
+        'new_name': new_name
     }
     return client.call('rename_lvol_bdev', params)
 
 
-def resize_lvol_bdev(client, args):
-    num_bytes = (args.size * 1024 * 1024)
+def resize_lvol_bdev(client, name, size):
+    """Resize a logical volume.
+
+    Args:
+        name: name of logical volume to resize
+        size: desired size of logical volume in bytes (will be rounded up to a multiple of cluster size)
+    """
     params = {
-        'name': args.name,
-        'size': num_bytes,
+        'name': name,
+        'size': size,
     }
     return client.call('resize_lvol_bdev', params)
 
 
-def destroy_lvol_store(client, args):
+def destroy_lvol_store(client, uuid=None, lvs_name=None):
+    """Destroy a logical volume store.
+
+    Args:
+        uuid: UUID of logical volume store to destroy (optional)
+        lvs_name: name of logical volume store to destroy (optional)
+
+    Either uuid or lvs_name must be specified, but not both.
+    """
+    if (uuid and lvs_name) or (not uuid and not lvs_name):
+        raise ValueError("Exactly one of uuid or lvs_name must be specified")
+
     params = {}
-    if (args.uuid and args.lvs_name) or (not args.uuid and not args.lvs_name):
-        print("You need to specify either uuid or name of lvolstore")
-    else:
-        if args.uuid:
-            params['uuid'] = args.uuid
-        if args.lvs_name:
-            params['lvs_name'] = args.lvs_name
-        return client.call('destroy_lvol_store', params)
+    if uuid:
+        params['uuid'] = uuid
+    if lvs_name:
+        params['lvs_name'] = lvs_name
+    return client.call('destroy_lvol_store', params)
 
 
-def get_lvol_stores(client, args):
+def get_lvol_stores(client, uuid=None, lvs_name=None):
+    """List logical volume stores.
+
+    Args:
+        uuid: UUID of logical volume store to retrieve information about (optional)
+        lvs_name: name of logical volume store to retrieve information about (optional)
+
+    Either uuid or lvs_name may be specified, but not both.
+    If both uuid and lvs_name are omitted, information about all logical volume stores is returned.
+    """
+    if (uuid and lvs_name):
+        raise ValueError("Exactly one of uuid or lvs_name may be specified")
     params = {}
-    if (args.uuid and args.lvs_name):
-        print("You can only specify either uuid or name of lvolstore")
-    if args.uuid:
-        params['uuid'] = args.uuid
-    if args.lvs_name:
-        params['lvs_name'] = args.lvs_name
+    if uuid:
+        params['uuid'] = uuid
+    if lvs_name:
+        params['lvs_name'] = lvs_name
     return client.call('get_lvol_stores', params)
