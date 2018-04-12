b5008e813425aeaa9c7c011f7bd9efeb2673b0c9
Maciej Szwed <maciej.szwed@intel.com>
2018-04-12 10:10:49 +0200

    lvol: Creating 2 lvols simultaneously with same name
    This patch fixes issue where user creates 2 lvols
    with the same name simultaneously. New solution
    builds list of names of lvols that are currently
    being created and when new request comes it compares
    name against existing lvols and lvols that are in the
    process of creation.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I31b59ee13b5b9bae531866925dd409b143f08ad4

--- include/spdk_internal/lvolstore.h
+++ include/spdk_internal/lvolstore.h
@@ -89,6 +89,7 @@ struct spdk_lvol_store {
 	int				lvols_opened;
 	bool				destruct;
 	TAILQ_HEAD(, spdk_lvol)		lvols;
+	TAILQ_HEAD(, spdk_lvol)		pending_lvols;
 	bool				on_list;
 	TAILQ_ENTRY(spdk_lvol_store)	link;
 	char				name[SPDK_LVS_NAME_MAX];
