668b6453b58509accf3da52cd81f55a2eef22469
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2018-04-12 07:34:07 +0200

    ut/vhost: remove backend-specific tests
    They were initially developed to bump code coverage,
    but now with upcoming vhost changes they are too much
    of a wart to keep updating. They heavily rely on vhost
    internals and are not particularly useful for testing.
    
    Right now these tests allocate a vhost target struct,
    hack its internals and try to call *_unregister() on it,
    hoping to trigger some expected error.
    
    One way to properly test the Vhost library would be to
    mock rte_vhost_* functions and use the spdk/vhost.h API
    to manage targets/devices. However, this is a greater
    refactor and would more likely fit in a multi-threaded
    unit test environment which might appear for vhost
    in future.
    
    Change-Id: Ia6cb0e1f7d0d57aef35050427396f895fb3bd7e1
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- test/unit/unittest.sh
+++ test/unit/unittest.sh
@@ -116,8 +116,6 @@ $valgrind $testdir/lib/util/string.c/string_ut
 
 if [ $(uname -s) = Linux ]; then
 $valgrind $testdir/lib/vhost/vhost.c/vhost_ut
-$valgrind $testdir/lib/vhost/vhost_scsi.c/vhost_scsi_ut
-$valgrind $testdir/lib/vhost/vhost_blk.c/vhost_blk_ut
 fi
 
 # local unit test coverage
