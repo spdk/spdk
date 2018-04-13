ecc0651b0ca80f069ecb35d72961ba7819f05898
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-04-13 10:40:32 +0900

    rpc: Filter RPCs only for subsystem initialization by using state mask
    This is to differentiate between RPCs that is allowed before subsystem
    initialization and RPCs that is allowed after subsystem initialization.
    
    Former are existing JSON RPCs and later are new for subsystem initialization.
    
    The following is a short description of the design.
    
    Add state_mask to struct spdk_rpc_method and add g_rpc_state to the RPC server.
    
    Define two bits for the state_mask:
    - RPC_STATE_PRE_SUBSYSTEM_INIT  0x1
    - RPC_STATE_POST_SUBSYSTEM_INIT 0x2
    
    g_rpc_state is set to either one by spdk_rpc_set_state(state_mask).
    
    If the command line option '-S' is specified, g_rpc_state is initialized to
    RPC_STATE_PRE_SUBSYSTEM_INIT. Otherwise, g_rpc_state is initialized to
    RPC_STATE_POST_SUBSYSTEM_INIT.
    
    When g_rpc_state is initialized to RPC_STATE_PRE_SUBSYSTEM_INIT, SPDK start
    RPC server and wait for start_subsystem_initialization RPC.
    
    When SPDK receive start_subsystem_init RPC, SPDK initializes
    subsystems. After completing initialization of subsystems, SPDK return
    response to the RPC.
    
    After that, when SPDK receive complete_subsystem_init RPC, SPDK change
    g_rpc_state to RPC_STATE_POST_SUBSYSTEM_INIT and then start application.
    
    When g_rpc_state is initialized to RPC_STATE_POST_SUBSYSTEM_INIT, SPDK
    initialize subsystems. After competing initialization of subsystems, SPDK
    start RPC server and then start application.
    
    Upcoming new RPCs to initialize options of NVMf-tgt and iSCSI-tgt will be
    able to be allowed before start_subsystems_init RPC is called.
    
    Change-Id: I39d8a0116d86015f11b9ef76d9185f363e65b016
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -67,6 +67,13 @@ if __name__ == "__main__":
     p.add_argument('--filename', help="""JSON Configuration file.""")
     p.set_defaults(func=load_config)
 
+    @call_cmd
+    def start_subsystems_init(args):
+        rpc.start_subsystems_init(args.client)
+
+    p = subparsers.add_parser('start_subsystems_init', help='Start initialization of subsystems')
+    p.set_defaults(func=start_subsystems_init)
+
     # app
     @call_cmd
     def kill_instance(args):
