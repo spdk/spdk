25c4050ab3a5e5ffb7de1033b7acaff6abfe0afa
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2018-04-15 16:46:03 +0200

    vhost: untie targets from lcores
    Vhost targets are now accessible from all cores.
    All the target struct data is already read-only,
    so it can be safely accessed from both vhost pollers
    and external threads. The global vhost mutex assures
    the target doesn't get removed while someone is
    accessing it.
    
    This allows vhost external events to be fully
    synchronous. From now on, it will be vhost internals'
    responsibility to propagate proper actions (like target
    hotplug) to all devices on a vhost target. Other
    external events like listing all vhost controllers can
    now be done entirely on a single thread. Only device-
    specific data has to be accesed by a single, specific
    CPU core.
    
    This change will allow us to start new devices of the
    same target on different CPU cores.
    
    Change-Id: Iddfd7d2cab2bdc6e6bc4c7bc7f8bceb7b42bf019
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- lib/vhost/vhost_internal.h
+++ lib/vhost/vhost_internal.h
@@ -93,6 +93,18 @@
 #define SPDK_VHOST_DISABLED_FEATURES ((1ULL << VIRTIO_RING_F_EVENT_IDX) | \
 	(1ULL << VIRTIO_F_NOTIFY_ON_EMPTY))
 
+struct spdk_vhost_dev;
+
+/**
+ * Synchronized vhost device event used for backend callbacks.
+ *
+ * \param vdev vhost device.
+ * \param arg user-provided parameter.
+ *
+ * \return 0 on success, -1 on failure.
+ */
+typedef int (*spdk_vhost_dev_fn)(struct spdk_vhost_dev *vdev, void *arg);
+
 struct spdk_vhost_virtqueue {
 	struct rte_vhost_vring vring;
 	void *tasks;
@@ -122,12 +134,12 @@ struct spdk_vhost_tgt_backend {
 
 	/**
 	 * Callbacks for starting and pausing the device.
-	 * The first param is struct spdk_vhost_tgt *.
+	 * The first param is struct spdk_vhost_dev *.
 	 * The second one is event context that has to be
 	 * passed to spdk_vhost_dev_backend_event_done().
 	 */
-	spdk_vhost_event_fn start_device;
-	spdk_vhost_event_fn stop_device;
+	spdk_vhost_dev_fn start_device;
+	spdk_vhost_dev_fn stop_device;
 
 	int (*vhost_get_config)(struct spdk_vhost_tgt *vtgt, uint8_t *config, uint32_t len);
 	int (*vhost_set_config)(struct spdk_vhost_tgt *vtgt, uint8_t *config,
@@ -178,6 +190,7 @@ struct spdk_vhost_dev {
 	/* rte_vhost device ID. */
 	int vid;
 
+	/* Logical core ID this device is polling on. -1 if not polling */
 	int32_t lcore;
 
 	int task_cnt;
@@ -281,6 +294,7 @@ int spdk_vhost_scsi_controller_construct(void);
 int spdk_vhost_blk_controller_construct(void);
 void spdk_vhost_dump_info_json(struct spdk_vhost_tgt *vtgt, struct spdk_json_write_ctx *w);
 void spdk_vhost_dev_backend_event_done(void *event_ctx, int response);
+void spdk_vhost_tgt_foreach_vdev(struct spdk_vhost_tgt *vtgt, spdk_vhost_dev_fn fn, void *arg);
 void spdk_vhost_lock(void);
 void spdk_vhost_unlock(void);
 int spdk_remove_vhost_controller(struct spdk_vhost_dev *vdev);
