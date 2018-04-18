4bcc441f365d13889f1da9e494f72b81267584e7
Yanbo Zhou <yanbo.zhou@intel.com>
2018-04-18 02:13:51 -0400

    bdev: a new public function to get the I/O statistics of bdevs
    Add a new function and its RPC caller. By using it, we can
    get the statistics of all the bdevs or the specified bdev.
    
    Meanwhile, with this patch, the open source tool 'sysstat/iostat'
    can support for SPDK. The 'iostat' tool can call this function to
    get the statistics of all the SPDK managed devices via the rpc
    interface.
    
    Change-Id: Iae816a0143b9641e4796e9c112f9d375f5bd9f99
    Signed-off-by: Yanbo Zhou <yanbo.zhou@intel.com>

--- lib/bdev/rpc/bdev_rpc.c
+++ lib/bdev/rpc/bdev_rpc.c
@@ -38,6 +38,147 @@
 
 #include "spdk_internal/bdev.h"
 
+struct rpc_get_bdevs_iostat_ctx {
+	bool get_all;
+	struct spdk_jsonrpc_request *request;
+	struct spdk_json_write_ctx *w;
+};
+
+static void
+spdk_rpc_get_bdevs_iostat_cb(struct spdk_bdev *bdev,
+			     const struct spdk_bdev_io_stat *stat, void *cb_arg, int rc)
+{
+	struct rpc_get_bdevs_iostat_ctx *ctx = cb_arg;
+	struct spdk_jsonrpc_request *request = ctx->request;
+	struct spdk_json_write_ctx *w = ctx->w;
+	struct spdk_bdev *next_bdev;
+	const char *bdev_name;
+
+	if (rc != 0) {
+		goto done;
+	}
+
+	bdev_name = spdk_bdev_get_name(bdev);
+	if (bdev_name != NULL) {
+		spdk_json_write_object_begin(w);
+
+		spdk_json_write_name(w, "name");
+		spdk_json_write_string(w, bdev_name);
+
+		spdk_json_write_name(w, "bytes_read");
+		spdk_json_write_uint64(w, stat->bytes_read);
+
+		spdk_json_write_name(w, "num_read_ops");
+		spdk_json_write_uint64(w, stat->num_read_ops);
+
+		spdk_json_write_name(w, "bytes_written");
+		spdk_json_write_uint64(w, stat->bytes_written);
+
+		spdk_json_write_name(w, "num_write_ops");
+		spdk_json_write_uint64(w, stat->num_write_ops);
+
+		spdk_json_write_object_end(w);
+	}
+
+done:
+	if (bdev != NULL && ctx->get_all) {
+		next_bdev = spdk_bdev_next(bdev);
+		if (next_bdev != NULL) {
+			return spdk_bdev_get_device_stat(next_bdev, spdk_rpc_get_bdevs_iostat_cb, ctx);
+		}
+	}	
+
+	spdk_json_write_array_end(w);
+	spdk_jsonrpc_end_result(request, w);
+	free(ctx);
+}
+
+struct rpc_get_bdevs_iostat {
+	char *name;
+};
+
+static void
+free_rpc_get_bdevs_iostat(struct rpc_get_bdevs_iostat *r)
+{
+	free(r->name);
+}
+
+static const struct spdk_json_object_decoder rpc_get_bdevs_iostat_decoders[] = {
+	{"name", offsetof(struct rpc_get_bdevs_iostat, name), spdk_json_decode_string},
+};
+
+static void
+spdk_rpc_get_bdevs_iostat(struct spdk_jsonrpc_request *request,
+			  const struct spdk_json_val *params)
+{
+	struct rpc_get_bdevs_iostat req = {};
+	struct spdk_bdev *bdev = NULL;
+	struct spdk_json_write_ctx *w;
+	struct rpc_get_bdevs_iostat_ctx *ctx;
+
+	if (params != NULL) {
+		if (spdk_json_decode_object(params, rpc_get_bdevs_iostat_decoders,
+					    SPDK_COUNTOF(rpc_get_bdevs_iostat_decoders),
+					    &req)) {
+			SPDK_ERRLOG("spdk_json_decode_object failed\n");
+			goto invalid;
+		} else {
+			if (req.name == NULL) {
+				SPDK_ERRLOG("missing name param\n");
+				goto invalid;
+			}
+
+			bdev = spdk_bdev_get_by_name(req.name);
+			if (bdev == NULL) {
+				SPDK_ERRLOG("bdev '%s' does not exist\n", req.name);
+				goto invalid;
+			}
+
+			free_rpc_get_bdevs_iostat(&req);
+		}
+	}
+
+	ctx = calloc(1, sizeof(struct rpc_get_bdevs_iostat_ctx));
+	if (ctx == NULL) {
+		SPDK_ERRLOG("Failed to allocate rpc_get_bdevs_iostat_ctx struct\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR, "No memory left");
+		return;
+	}
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w == NULL) {
+		free(ctx);
+		return;
+	}
+
+	if (bdev != NULL) {
+		ctx->get_all = false;
+	} else {
+		ctx->get_all = true;
+		bdev = spdk_bdev_first();
+	}
+	ctx->request = request;
+	ctx->w = w;
+
+	spdk_json_write_array_begin(w);
+
+	if (bdev != NULL) {
+		spdk_bdev_get_device_stat(bdev, spdk_rpc_get_bdevs_iostat_cb, ctx);
+	} else {
+		spdk_json_write_array_end(w);
+		spdk_jsonrpc_end_result(request, w);
+		free(ctx);
+	}
+
+	return;
+
+invalid:
+	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, "Invalid parameters");
+
+	free_rpc_get_bdevs_iostat(&req);
+}
+SPDK_RPC_REGISTER("get_bdevs_iostat", spdk_rpc_get_bdevs_iostat)
+
 static void
 spdk_rpc_dump_bdev_info(struct spdk_json_write_ctx *w,
 			struct spdk_bdev *bdev)
