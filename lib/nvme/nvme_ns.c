a221fbee7b3cd7f0b059fdbb8a8a3ca4038efda4
Changpeng Liu <changpeng.liu@intel.com>
2018-04-18 21:52:55 -0400

    nvme: update the NS identify data according to AER
    Number of Namespaces of controller identify structure
    defines the maximum number of namespaces supported by
    this controller, for physical NVMe controllers, the NN
    is a fixed number, while here, we set the same rule for
    NVMeoF controllers.
    
    After NVMe driver got namespace notice event, it should
    update the namespace identify data structure for NS
    attach/detach commands.
    
    Change-Id: Id72a2600a2ce9492fa2d6e09924667acbb77ae43
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/nvme/nvme_ns.c
+++ lib/nvme/nvme_ns.c
@@ -58,14 +58,7 @@ int nvme_ns_identify_update(struct spdk_nvme_ns *ns)
 			&ns->ctrlr->ctrlr_lock)) {
 		/* This can occur if the namespace is not active. Simply zero the
 		 * namespace data and continue. */
-		memset(nsdata, 0, sizeof(*nsdata));
-		ns->sector_size = 0;
-		ns->extended_lba_size = 0;
-		ns->md_size = 0;
-		ns->pi_type = 0;
-		ns->sectors_per_max_io = 0;
-		ns->sectors_per_stripe = 0;
-		ns->flags = 0;
+		nvme_ns_destruct(ns);
 		return 0;
 	}
 
@@ -300,5 +293,19 @@ int nvme_ns_construct(struct spdk_nvme_ns *ns, uint32_t id,
 
 void nvme_ns_destruct(struct spdk_nvme_ns *ns)
 {
+	struct spdk_nvme_ns_data *nsdata;
 
+	if (!ns->id) {
+		return;
+	}
+
+	nsdata = _nvme_ns_get_data(ns);
+	memset(nsdata, 0, sizeof(*nsdata));
+	ns->sector_size = 0;
+	ns->extended_lba_size = 0;
+	ns->md_size = 0;
+	ns->pi_type = 0;
+	ns->sectors_per_max_io = 0;
+	ns->sectors_per_stripe = 0;
+	ns->flags = 0;
 }
