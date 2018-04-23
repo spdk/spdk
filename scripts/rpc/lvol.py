2055c9aa360ca256684060d08eded3fc4cb3350f
Maciej Szwed <maciej.szwed@intel.com>
2018-04-23 15:53:21 +0200

    lvol: add lvol inflate RPC call
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I15d6d89bc6fabd87b458113c777a66e3bf1dcecc

--- scripts/rpc/lvol.py
+++ scripts/rpc/lvol.py
@@ -110,6 +110,18 @@ def resize_lvol_bdev(client, name, size):
     return client.call('resize_lvol_bdev', params)
 
 
+def inflate_lvol_bdev(client, name):
+    """Inflate a logical volume.
+
+    Args:
+        name: name of logical volume to inflate
+    """
+    params = {
+        'name': name,
+    }
+    return client.call('inflate_lvol_bdev', params)
+
+
 def destroy_lvol_store(client, uuid=None, lvs_name=None):
     """Destroy a logical volume store.
 
