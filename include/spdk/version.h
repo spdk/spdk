9eb54cae6aca7ddefb501fe6e26d125d877d59c9
Daniel Verkamp <daniel.verkamp@intel.com>
2018-04-27 09:50:36 -0700

    SPDK 18.04
    Change-Id: I67ad1bb09b77834bb35a56b2d6982cf10d59152f
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>
    Reviewed-on: https://review.gerrithub.io/409339
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

--- include/spdk/version.h
+++ include/spdk/version.h
@@ -59,7 +59,7 @@
 /**
  * Version string suffix.
  */
-#define SPDK_VERSION_SUFFIX	"-pre"
+#define SPDK_VERSION_SUFFIX	""
 
 /**
  * Single numeric value representing a version number for compile-time comparisons.
