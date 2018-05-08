66d1900410479c015b7b9bbb56b76ebfb322eb4c
Kunal Sablok <kunal.sablok@intel.com>
2018-05-08 07:30:29 -0400

    bdev: add pooled volume module
    Pooled volume module:
    ====================
    - SPDK pooled volume (PVOL) module is a new bdev module which is
      responsible for striping various NVMe devices and expose the pooled
    volume to bdev layer which would enhance the performance and capacity.
    - It can support theoretically 256 base devices (currently it is being
      tested max upto 8 base devices)
    - Multiple strip sizes like 32KB, 64KB, 128KB, 256KB, 512KB etc is
      supported. Most of the current testing is focused on 64KB strip size.
    - New RPC commands like "create pvol", "destroy pvol" and "get pvols"
      are introduced to configure pooled volume dynamically in a running
    SPDK system.
    - Currently pooled volume configuration parameters are persisted in the
      current SPDK configuration file for across reboot support. DDF will be
    introduced later.
    
    High level testing done:
    =======================
    - Pooled volume is created with 8 base NVMe devices via configuration
      file and is exposed to initiator via existing methods. Initiator is
    able to see a single NVMe namespace with capacity equal to sum of the
    minimum capacities of 8 devices. Initiator was able to run raw
    read/write workload, file system workload etc (tested with XFS file
    system workload).
    - Multiple pooled volumes are also created and exposed to initiator and
      tested with file system and other workloads for read/write IO. Maximum
    pooled volumes tested is 4 (with each having 2 base NVMe drives
    striped).
    - LVS / LVOL are created over pooled volume and exposed to initiator.
      Testing was done for raw read/write workloads and XFS file system
    workloads.
    - RPC testing is done where on the running SPDK system pooled volumes
      are created out of NVMe base devices. These pooled volumes (and LVOLs
    over pooled volumes) are then exposed to initiator and IO workload was
    tested for raw read/write and XFS file system workload.
    - RPC testing is done for delete pooled volumes where all pooled volumes
      are deleted in running SPDK system.
    - RPC testing is done for get pooled volumes where existing list of
      pooled volume names is printed (it can be all pooled volumes or only
    online or only configuring or only offline).
    - RPC testing is done where pooled volume and underlying NVMe devices
      relationship was returned in JSON RPC commands
    
    Some UT improvements submit
    Some autobuild.sh fixes
    Some UT improvements submit
    .gitignore file submit
    Some UT fixes for double free
    Use spdk_thread_send_msg and remove event
    
    PatchSet16 review comments
    
    Rebase to latest repository and changes to adjust bdev IO wait queue
    entry
    Build fixes
    Test case fix
    
    Patchset 23
    Patchset 24
    Patchset 25
    
    Waitq entry embedded,
    change pvol to raid in file contents
    change pvol to raid in file names
    
    Change-Id: I10ae1266f8f2cca3c106e4df8c1c0993ddf435d8
    Signed-off-by: Kunal Sablok <kunal.sablok@intel.com>

--- scripts/rpc/bdev.py
+++ scripts/rpc/bdev.py
@@ -74,6 +74,49 @@ def delete_null_bdev(client, name):
     return client.call('delete_null_bdev', params)
 
 
+def get_raid_bdevs(client, category):
+    """Get list of raid bdevs based on category
+
+    Args:
+        category: any one of all or online or configuring or offline
+
+    Returns:
+        List of raid bdev names
+    """
+    params = {'category': category}
+    return client.call('get_raid_bdevs', params)
+
+
+def construct_raid_bdev(client, name, strip_size, raid_level, base_bdevs):
+    """Construct pooled device
+
+    Args:
+        name: user defined raid bdev name
+        strip_size: strip size of raid bdev in KB, supported values like 8, 16, 32, 64, 128, 256, 512, 1024 etc
+        raid_level: raid level of raid bdev, supported values 0
+        base_bdevs: Space separated names of Nvme bdevs in double quotes, like "Nvme0n1 Nvme1n1 Nvme2n1"
+
+    Returns:
+        None
+    """
+    params = {'name': name, 'strip_size': strip_size, 'raid_level': raid_level, 'base_bdevs': base_bdevs}
+
+    return client.call('construct_raid_bdev', params)
+
+
+def destroy_raid_bdev(client, name):
+    """Destroy pooled device
+
+    Args:
+        name: raid bdev name
+
+    Returns:
+        None
+    """
+    params = {'name': name}
+    return client.call('destroy_raid_bdev', params)
+
+
 def construct_aio_bdev(client, filename, name, block_size=None):
     """Construct a Linux AIO block device.
 
