8107d04bfbccbd25d49356f14f815186b56f3734
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-05-10 07:55:12 +0900

    iscsi: Add JSON config/info dump for initiator group.
    Support JSON config/info dump for initiator group. These are added
    to init_grp.c. Text config dump is moved to init_grp.c too.
    
    Add accesses to g_spdk_iscsi.ig_head are consolidated into
    init_grp.c to extract ig_head from g_spdk_iscsi.
    
    Change-Id: I88131d248ed13e42e4da56aed8ae0edc01a65715
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-on: https://review.gerrithub.io/407849
    Tested-by: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Reviewed-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/iscsi/iscsi_subsystem.c
+++ lib/iscsi/iscsi_subsystem.c
@@ -163,56 +163,6 @@ spdk_iscsi_config_dump_portal_groups(FILE *fp)
 	}
 }
 
-/* Initiator Groups */
-static const char *initiator_group_section = \
-		"\n"
-		"# Users must change the InitiatorGroup section(s) to match the IP\n"
-		"#  addresses and initiator configuration in their environment.\n"
-		"# Netmask can be used to specify a single IP address or a range of IP addresses\n"
-		"#  Netmask 192.168.1.20   <== single IP address\n"
-		"#  Netmask 192.168.1.0/24 <== IP range 192.168.1.*\n";
-
-#define INITIATOR_GROUP_TMPL \
-"[InitiatorGroup%d]\n" \
-"  Comment \"Initiator Group%d\"\n"
-
-#define INITIATOR_TMPL \
-"  InitiatorName "
-
-#define NETMASK_TMPL \
-"  Netmask "
-
-static void
-spdk_iscsi_config_dump_initiator_groups(FILE *fp)
-{
-	struct spdk_iscsi_init_grp *ig;
-	struct spdk_iscsi_initiator_name *iname;
-	struct spdk_iscsi_initiator_netmask *imask;
-
-	/* Create initiator group section */
-	fprintf(fp, "%s", initiator_group_section);
-
-	/* Dump initiator groups */
-	TAILQ_FOREACH(ig, &g_spdk_iscsi.ig_head, tailq) {
-		if (NULL == ig) { continue; }
-		fprintf(fp, INITIATOR_GROUP_TMPL, ig->tag, ig->tag);
-
-		/* Dump initiators */
-		fprintf(fp, INITIATOR_TMPL);
-		TAILQ_FOREACH(iname, &ig->initiator_head, tailq) {
-			fprintf(fp, "%s ", iname->name);
-		}
-		fprintf(fp, "\n");
-
-		/* Dump netmasks */
-		fprintf(fp, NETMASK_TMPL);
-		TAILQ_FOREACH(imask, &ig->netmask_head, tailq) {
-			fprintf(fp, "%s ", imask->mask);
-		}
-		fprintf(fp, "\n");
-	}
-}
-
 static void
 spdk_mobj_ctor(struct spdk_mempool *mp, __attribute__((unused)) void *arg,
 	       void *_m, __attribute__((unused)) unsigned i)
@@ -1012,7 +962,7 @@ spdk_iscsi_config_text(FILE *fp)
 {
 	spdk_iscsi_globals_config_text(fp);
 	spdk_iscsi_config_dump_portal_groups(fp);
-	spdk_iscsi_config_dump_initiator_groups(fp);
+	spdk_iscsi_init_grps_config_text(fp);
 	spdk_iscsi_tgt_nodes_config_text(fp);
 }
 
@@ -1020,6 +970,7 @@ void
 spdk_iscsi_config_json(struct spdk_json_write_ctx *w)
 {
 	spdk_json_write_array_begin(w);
+	spdk_iscsi_init_grps_config_json(w);
 	spdk_iscsi_tgt_nodes_config_json(w);
 	spdk_json_write_array_end(w);
 }
