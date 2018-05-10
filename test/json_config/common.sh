c11618502873662e5332e5be98047ec7db728109
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-05-10 12:39:55 -0400

    test/json: Test for vhost subsystem.
    Change-Id: I913d378fe3f324043e8e86c1df209ad19680c60b
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/json_config/common.sh
+++ test/json_config/common.sh
@@ -40,6 +40,17 @@ function run_initiator() {
 	waitforlisten $virtio_pid /var/tmp/virtio.sock
 }
 
+function upload_vhost() {
+	$rpc_py construct_split_vbdev Nvme0n1 8
+	$rpc_py construct_vhost_scsi_controller sample1
+	$rpc_py add_vhost_scsi_lun sample1 0 Nvme0n1p3
+	$rpc_py add_vhost_scsi_lun sample1 1 Nvme0n1p4
+	$rpc_py set_vhost_controller_coalescing sample1 1 100
+	$rpc_py construct_vhost_blk_controller sample2 Nvme0n1p5
+	$rpc_py construct_vhost_nvme_controller sample3 16
+	$rpc_py add_vhost_nvme_ns sample3 Nvme0n1p6
+}
+
 function kill_targets() {
 	if [ ! -z $virtio_pid ]; then
 		killprocess $virtio_pid
