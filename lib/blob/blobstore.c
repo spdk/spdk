d7886d50580b4789ecd6620faa83250894684cb6
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-05-10 14:12:36 +0200

    blobstore: Save the original size of the disk
    Save the original size of the disk to metadata whenit is first created.
    On load verify that the disk did not change size.
    
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I535940ee188425ee3b394effd99653cc073d541e

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -2899,6 +2899,14 @@ _spdk_bs_load_super_cpl(spdk_bs_sequence_t *seq, void *cb_arg, int bserrno)
 		return;
 	}
 
+	if (ctx->super->blockcnt == 0) {
+		/* Write number of blocks for blobstore that haven't done that before */
+		ctx->super->blockcnt = ctx->bs->dev->blockcnt;
+	} else if (ctx->super->blockcnt != ctx->bs->dev->blockcnt) {
+		_spdk_bs_load_ctx_fail(seq, ctx, -ENOMEDIUM);
+		return;
+	}
+
 	/* Parse the super block */
 	ctx->bs->cluster_sz = ctx->super->cluster_size;
 	ctx->bs->total_clusters = ctx->bs->dev->blockcnt / (ctx->bs->cluster_sz / ctx->bs->dev->blocklen);
@@ -3171,6 +3179,8 @@ spdk_bs_init(struct spdk_bs_dev *dev, struct spdk_bs_opts *o,
 	num_md_pages += bs->md_len;
 
 	num_md_lba = _spdk_bs_page_to_lba(bs, num_md_pages);
+
+	ctx->super->blockcnt = dev->blockcnt;
 
 	ctx->super->crc = _spdk_blob_md_page_calc_crc(ctx->super);
 
