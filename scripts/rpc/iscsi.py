f5de4d4b6d1e81ccafee0bbcbda4bb3afeccb924
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-05-11 12:13:28 +0900

    subsystem/iscsi: Add set_iscsi_options RPC to set global params
    An new RPC set_iscsi_options allocates and set options dynamically.
    Initialization of iSCSI subsystem skips initialization of options
    if it is already allocated.
    
    To use and test this RPC easily, add python script too.
    
    Change-Id: I71e252da6495a194ae9a1a9e3aaae4feb543487a
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- scripts/rpc/iscsi.py
+++ scripts/rpc/iscsi.py
@@ -1,3 +1,41 @@
+def set_iscsi_options(client, args):
+    params = {}
+
+    if args.auth_file:
+        params['auth_file'] = args.auth_file
+    if args.node_base:
+        params['node_base'] = args.node_base
+    if args.nop_timeout:
+        params['nop_timeout'] = args.nop_timeout
+    if args.nop_in_interval:
+        params['nop_in_interval'] = args.nop_in_interval
+    if args.no_discovery_auth:
+        params['no_discovery_auth'] = args.no_discovery_auth
+    if args.req_discovery_auth:
+        params['req_discovery_auth'] = args.req_discovery_auth
+    if args.req_discovery_auth_mutual:
+        params['req_discovery_auth_mutual'] = args.req_discovery_auth_mutual
+    if args.discovery_auth_group:
+        params['discovery_auth_group'] = args.discovery_auth_group
+    if args.max_sessions:
+        params['max_sessions'] = args.max_sessions
+    if args.max_connections_per_session:
+        params['max_connections_per_session'] = args.max_connections_per_session
+    if args.default_time2wait:
+        params['default_time2wait'] = args.default_time2wait
+    if args.default_time2retain:
+        params['default_time2retain'] = args.default_time2retain
+    if args.immediate_data:
+        params['immediate_data'] = args.immediate_data
+    if args.error_recovery_level:
+        params['error_recovery_level'] = args.error_recovery_level
+    if args.allow_duplicated_isid:
+        params['allow_duplicated_isid'] = args.allow_duplicated_isid
+    if args.min_connections_per_session:
+        params['min_connections_per_session'] = args.min_connections_per_session
+    return client.call('set_iscsi_options', params)
+
+
 def get_portal_groups(client, args):
     return client.call('get_portal_groups')
 
