71dcbb9cf06238dad27fcdc30627e3c486be1f2b
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-05-15 08:39:02 +0900

    subsystem/nvmf: Add set_nvmf_target_options/config RPCs
    Two new RPCs allocate and initialize options and conf dynamically.
    Initialization of NVMf subsystem skips initialization of them
    if they are already allocated.
    
    To use and test them, add python script too.
    
    Change-Id: I3be83c33edf9960cb4340d52abb47b7331a6fb92
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/event/subsystems/nvmf/nvmf_tgt.c
+++ lib/event/subsystems/nvmf/nvmf_tgt.c
@@ -273,7 +273,7 @@ nvmf_tgt_advance_state(void)
 		}
 		case NVMF_TGT_INIT_START_ACCEPTOR:
 			g_acceptor_poller = spdk_poller_register(acceptor_poll, g_spdk_nvmf_tgt,
-					    g_spdk_nvmf_tgt_conf.acceptor_poll_rate);
+					    g_spdk_nvmf_tgt_conf->acceptor_poll_rate);
 			SPDK_INFOLOG(SPDK_LOG_NVMF, "Acceptor running\n");
 			g_tgt_state = NVMF_TGT_RUNNING;
 			break;
@@ -304,6 +304,7 @@ nvmf_tgt_advance_state(void)
 			break;
 		case NVMF_TGT_FINI_FREE_RESOURCES:
 			spdk_nvmf_tgt_destroy(g_spdk_nvmf_tgt);
+			free(g_spdk_nvmf_tgt_conf);
 			g_tgt_state = NVMF_TGT_STOPPED;
 			break;
 		case NVMF_TGT_STOPPED:
