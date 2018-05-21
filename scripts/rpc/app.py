293558e20e5c31fb5f2150d4733f75d3cbb1c704
Daniel Verkamp <daniel.verkamp@intel.com>
2018-05-21 13:07:12 -0700

    scripts/rpc.py: pass named args to app.py
    Also add docstrings to all app.py methods.
    
    Change-Id: Ib234014630e8b47c55f8d96bede509952fe653c5
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- scripts/rpc/app.py
+++ scripts/rpc/app.py
@@ -1,12 +1,23 @@
-def kill_instance(client, args):
-    params = {'sig_name': args.sig_name}
+def kill_instance(client, sig_name):
+    """Send a signal to the SPDK process.
+
+    Args:
+        sig_name: signal to send ("SIGINT", "SIGTERM", "SIGQUIT", "SIGHUP", or "SIGKILL")
+    """
+    params = {'sig_name': sig_name}
     return client.call('kill_instance', params)
 
 
-def context_switch_monitor(client, args):
+def context_switch_monitor(client, enabled=None):
+    """Query or set state of context switch monitoring.
+
+    Args:
+        enabled: True to enable monitoring; False to disable monitoring; None to query (optional)
+
+    Returns:
+        Current context switch monitoring state (after applying enabled flag).
+    """
     params = {}
-    if args.enable:
-        params['enabled'] = True
-    if args.disable:
-        params['enabled'] = False
+    if enabled is not None:
+        params['enabled'] = enabled
     return client.call('context_switch_monitor', params)
