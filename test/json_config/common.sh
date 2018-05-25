54c721cddf39b3fcdfc142ec3888d7ae66d45edb
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-05-25 18:41:17 +0200

    [RFC]json/rpc: Tests for nvmf subsystem
    Change-Id: I70044bfc01c77a367cf4634d245a58546273f40b
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/json_config/common.sh
+++ test/json_config/common.sh
@@ -3,6 +3,7 @@ set -ex
 JSON_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 SPDK_BUILD_DIR=$JSON_DIR/../../
 . $JSON_DIR/../common/autotest_common.sh
+. $JSON_DIR/../nvmf/common.sh
 
 spdk_rpc_py="python $SPDK_BUILD_DIR/scripts/rpc.py -s /var/tmp/spdk.sock"
 spdk_clear_config_py="$JSON_DIR/clear_config.py -s /var/tmp/spdk.sock"
@@ -80,6 +81,22 @@ function create_bdev_subsystem_config() {
 	# Uncomment after bug on librados will be fixed
 	# rbd_setup 127.0.0.1
 	# $rpc_py construct_rbd_bdev $RBD_POOL $RBD_NAME 4096
+}
+
+function create_nvmf_subsystem_config() {
+	rdma_device_init
+	MALLOC_BDEV_SIZE=64
+	MALLOC_BLOCK_SIZE=512
+	RDMA_IP_LIST=$(get_available_rdma_ips)
+	NVMF_FIRST_TARGET_IP=$(echo "$RDMA_IP_LIST" | head -n 1)
+	if [ -z $NVMF_FIRST_TARGET_IP ]; then
+		echo "no NIC for nvmf test"
+		return
+	fi
+	bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE --name Malloc3)"
+	bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE --name Malloc4)"
+	$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 '' '' -a -s SPDK00000000000001 -n "$bdevs"
+	$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t RDMA -a $NVMF_FIRST_TARGET_IP -s "$NVMF_PORT"
 }
 
 function clean_bdev_subsystem_config() {
---
+++ test/nvmf/jsonnvmf/json_config.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+NVMF_JSON_DIR=$(readlink -f $(dirname $0))
+. $NVMF_JSON_DIR/../../json_config/common.sh
+
+function test_subsystems() {
+	# Export flag to skip the known bug that exists in librados
+	export ASAN_OPTIONS=new_delete_type_mismatch=0
+	run_spdk_tgt
+	rootdir=$(readlink -f $NVMF_JSON_DIR/../..)
+
+	rpc_py="$spdk_rpc_py"
+	clear_config_py="$spdk_clear_config_py"
+	create_bdev_subsystem_config
+	test_json_config
+
+	kill_targets
+}
+
+trap 'on_error_exit "${FUNCNAME}" "${LINENO}"' ERR
+modprobe nbd
+
+test_subsystems
+
+rmmod nbd
