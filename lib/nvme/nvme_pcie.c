451b0ad7a53c677f44d4c423ea0c513c1bcdb19e
Daniel Verkamp <daniel.verkamp@intel.com>
2018-06-05 16:01:52 -0700

    nvme: ignore requests submitted before timeout set
    If requests are in flight at the time a timeout callback is configured,
    we can't retroactively get the submission time of those requests; treat
    them as not having a timeout.
    
    Change-Id: Ic589e874a0f2c8c7f4ce352afa7c3aea33b01fae
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/nvme/nvme_pcie.c
+++ lib/nvme/nvme_pcie.c
@@ -1170,6 +1170,8 @@ nvme_pcie_qpair_submit_tracker(struct spdk_nvme_qpair *qpair, struct nvme_tracke
 	req->timed_out = false;
 	if (spdk_unlikely(pctrlr->ctrlr.timeout_enabled)) {
 		req->submit_tick = spdk_get_ticks();
+	} else {
+		req->submit_tick = 0;
 	}
 
 	pqpair->tr[tr->cid].active = true;
@@ -1998,7 +2000,7 @@ nvme_pcie_qpair_check_timeout(struct spdk_nvme_qpair *qpair)
 
 	t02 = spdk_get_ticks();
 	TAILQ_FOREACH_SAFE(tr, &pqpair->outstanding_tr, tq_list, tmp) {
-		if (tr->req->timed_out) {
+		if (tr->req->timed_out || tr->req->submit_tick == 0) {
 			continue;
 		}
 
