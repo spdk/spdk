a92a162a4cda260c96934c1692e604e2598b0983
Jim Harris <james.r.harris@intel.com>
2018-06-07 06:23:49 -0700

    blob: add bsdump utility
    Add spdk_bs_dump which dumps low level blobstore metadata
    information to a specified FILE.
    
    Also add new bsdump utility which dumps blobstore metadata
    information associated with a given bdev.  This utility also
    has helper functions for printing out BlobFS and Lvolstore
    xattrs.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Iad018b70f8caa4f950d55dd308b9000d55d885ae

--- include/spdk/blob.h
+++ include/spdk/blob.h
@@ -198,6 +198,11 @@ void spdk_bs_load(struct spdk_bs_dev *dev, struct spdk_bs_opts *opts,
 void spdk_bs_init(struct spdk_bs_dev *dev, struct spdk_bs_opts *opts,
 		  spdk_bs_op_with_handle_complete cb_fn, void *cb_arg);
 
+typedef void (*spdk_bs_dump_print_xattr)(FILE *fp, const char *bstype, const char *name,
+		const void *value, size_t value_length);
+
+void spdk_bs_dump(struct spdk_bs_dev *dev, FILE *fp, spdk_bs_dump_print_xattr print_xattr_fn,
+		  spdk_bs_op_complete cb_fn, void *cb_arg);
 /**
  * Destroy the blobstore.
  *
