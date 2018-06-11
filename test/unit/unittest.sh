094a0ac2cb3ada25fc5d02766d4d78673b0b9f1a
Jakub Radtke <jakub.radtke@intel.com>
2018-06-11 15:13:55 -0700

    ocssd: nvme driver public API extension for Open-Channel
    This patch adds to nvme driver new public API dedicated for
    Open-Channel:
    * spdk_nvme_ocssd_ctrlr_cmd_geometry
    
    Change-Id: I69592618d9bcc280346258e771195683d5b33893
    Signed-off-by: Jakub Radtke <jakub.radtke@intel.com>

--- test/unit/unittest.sh
+++ test/unit/unittest.sh
@@ -71,6 +71,7 @@ $valgrind $testdir/lib/net/sock.c/sock_ut
 $valgrind $testdir/lib/nvme/nvme.c/nvme_ut
 $valgrind $testdir/lib/nvme/nvme_ctrlr.c/nvme_ctrlr_ut
 $valgrind $testdir/lib/nvme/nvme_ctrlr_cmd.c/nvme_ctrlr_cmd_ut
+$valgrind $testdir/lib/nvme/nvme_ctrlr_ocssd_cmd.c/nvme_ctrlr_ocssd_cmd_ut
 $valgrind $testdir/lib/nvme/nvme_ns.c/nvme_ns_ut
 $valgrind $testdir/lib/nvme/nvme_ns_cmd.c/nvme_ns_cmd_ut
 $valgrind $testdir/lib/nvme/nvme_qpair.c/nvme_qpair_ut
