dcd47af0bf7a18267f0ac895cf060344184f618a
Daniel Verkamp <daniel.verkamp@intel.com>
2018-06-12 10:11:45 -0700

    nvmf/rpc: add max_namespaces to get_nvmf_subsystems
    This is one of the construct_nvmf_subsystems parameters, so we need a
    public API to retrieve it for informational purposes and for the
    JSON-RPC configuration dump.
    
    Change-Id: I8a7cafa487209311e964ea9ff1b8e09ee8b23c07
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- include/spdk/nvmf.h
+++ include/spdk/nvmf.h
@@ -567,6 +567,15 @@ struct spdk_nvmf_ns *spdk_nvmf_subsystem_get_ns(struct spdk_nvmf_subsystem *subs
 		uint32_t nsid);
 
 /**
+ * Get the maximum number of namespaces allowed in a subsystem.
+ *
+ * \param subsystem Subsystem to query.
+ *
+ * \return Maximum number of namespaces allowed in the subsystem, or 0 for unlimited.
+ */
+uint32_t spdk_nvmf_subsystem_get_max_namespaces(const struct spdk_nvmf_subsystem *subsystem);
+
+/**
  * Get a namespace's NSID.
  *
  * \param ns Namespace to query.
