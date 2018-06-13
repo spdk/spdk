240608d0c069376a26942fd8b3ba722e5d4a83cc
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-06-13 15:57:26 +0200

    nvmf: change session to ctrlr in python scripts
    Change-Id: I5e1ed10012849b8aed8cc3c808d180e7b09451da
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- scripts/rpc/nvmf.py
+++ scripts/rpc/nvmf.py
@@ -4,7 +4,7 @@ def set_nvmf_target_options(client, args):
     if args.max_queue_depth:
         params['max_queue_depth'] = args.max_queue_depth
     if args.max_qpairs_per_session:
-        params['max_qpairs_per_session'] = args.max_qpairs_per_session
+        params['max_qpairs_per_ctrlr'] = args.max_qpairs_per_ctrlr
     if args.in_capsule_data_size:
         params['in_capsule_data_size'] = args.in_capsule_data_size
     if args.max_io_size:
