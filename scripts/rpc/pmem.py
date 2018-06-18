2c46a037be1669f06c7ef882bf263c51854db52a
Karol Latecki <karol.latecki@intel.com>
2018-06-18 14:09:44 +0200

    scripts/rpc.py: pass named arguments to pmem.py
    Also move "num_blocks" parameter calculation
    outside of the create_pmem_pool function to keep it
    consistent with rest of similar functions.
    Add docstrings while at it.
    
    Change-Id: I024abf0ed450f51d67a6acdb3927b06e205102d8
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- scripts/rpc/pmem.py
+++ scripts/rpc/pmem.py
@@ -1,17 +1,29 @@
-def create_pmem_pool(client, args):
-    # truncate to the nearest block.
-    num_blocks = int((args.total_size * 1024 * 1024) / args.block_size)
-    params = {'pmem_file': args.pmem_file,
+def create_pmem_pool(client, pmem_file, num_blocks, block_size):
+    """Create pmem pool at specified path.
+    Args:
+        pmem_file: path at which to create pmem pool
+        num_blocks: number of blocks for created pmem pool file
+        block_size: block size for pmem pool file
+    """
+    params = {'pmem_file': pmem_file,
               'num_blocks': num_blocks,
-              'block_size': args.block_size}
+              'block_size': block_size}
     return client.call('create_pmem_pool', params)
 
 
-def pmem_pool_info(client, args):
-    params = {'pmem_file': args.pmem_file}
+def pmem_pool_info(client, pmem_file):
+    """Get details about pmem pool.
+    Args:
+        pmem_file: path to pmem pool
+    """
+    params = {'pmem_file': pmem_file}
     return client.call('pmem_pool_info', params)
 
 
-def delete_pmem_pool(client, args):
-    params = {'pmem_file': args.pmem_file}
+def delete_pmem_pool(client, pmem_file):
+    """Delete pmem pool.
+    Args:
+        pmem_file: path to pmem pool
+    """
+    params = {'pmem_file': pmem_file}
     return client.call('delete_pmem_pool', params)
