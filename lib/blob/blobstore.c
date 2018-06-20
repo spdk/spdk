ab88725718ba48224a9be1e0940dab3123ff58b0
Daniel Verkamp <daniel.verkamp@intel.com>
2018-06-20 09:20:25 -0700

    blob: change lba to uint64_t in serialize_extent
    Make sure we don't truncate the LBA when using it to serialize the
    cluster array into an extent list.
    
    Change-Id: If4e65ed86550e39dfa39826930dfafac158d519c
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -664,8 +664,8 @@ _spdk_blob_serialize_extent(const struct spdk_blob *blob,
 {
 	struct spdk_blob_md_descriptor_extent *desc;
 	size_t cur_sz;
-	uint64_t i, extent_idx;
-	uint32_t lba, lba_per_cluster, lba_count;
+	uint64_t i, extent_idx, lba;
+	uint32_t lba_per_cluster, lba_count;
 
 	/* The buffer must have room for at least one extent */
 	cur_sz = sizeof(struct spdk_blob_md_descriptor) + sizeof(desc->extents[0]);
