37c87d15b814817cf95a2969ed028254828b0788
dongx.yi <dongx.yi@intel.com>
2018-06-21 07:10:06 -0400

    scripts/cleanup: Add cleanup.sh to clear shared memory files
    Add cleanup.sh to clear shared memory files,
    and we can also add other action to clean up if we need.
    execute ./scripts/cleanup.sh to clear.
    
    Change-Id: I200be25572a468dcc932472a648e0c182b65da9b
    Signed-off-by: dongx.yi <dongx.yi@intel.com>

---
+++ scripts/cleanup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -x
+
+rootdir=$(readlink -f $(dirname $0))
+
+# we can add some actions to clean up files which no need anymore, such as share memory overflow...
+if [ `uname` = Linux ] ;then
+        echo "Delete leftover SPDK generated files from terminated processes."
+	# serach sensible file
+	for file in `ls /dev/shm/* |grep trace.pid`
+        do
+	   #confirm is a file
+           if test -f $file
+           then
+               FileName=`echo $file`
+               PID=`echo ${FileName#*trace.pid}`
+               #confirm PID is a  number
+               if [ -n "$(echo $PID| sed -n "/^[0-9]\+$/p")" ] ;then
+		    if [[ $PID -eq 0 ]] ;then
+                        continue
+                    else
+                        kill -0 $PID
+                        #Found the target PID
+                        if [[ $? -eq 0 ]] ;then
+                                echo "Target $PID is running, skip deleting this file."
+                        else
+                                # Delete leftover SPDK generated files from terminated processes.
+                                echo "Delete trace.pid$PID file"
+                                rm -rf /dev/shm/*trace.pid$PID
+                                # only one spdk_iscsi_conns file, no need to delete.
+                                #rm -rf /dev/shm/spdk_iscsi_conns.-1
+                        fi
+                    fi
+		else
+		#skip non number PID
+			continue
+                fi
+	    fi
+        done
+elif [ `uname` =  FreeBSD ] ;then
+        echo " FreeBSD need to add extra cleanup behavior check !"
+else
+        echo "Not support!!"
+fi
