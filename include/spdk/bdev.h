cda9fe12ff490fa6b531cde9beb9048e3c326532
GangCao <gang.cao@intel.com>
2018-06-21 22:15:02 -0400

    QoS/Bdev: add the RPC support for the bandwidth rate limit
    This patch added the support of RPC method to enable,
    adjust, disable the bandwidth rate limit on the bdev.
    
    Also it can work together with the IOPS rate limit.
    
    Related UT codes have also been updated.
    
    New PRC method has been consolidated to support both IOPS
    and bandwidth rate limits as below:
    
    usage:
    rpc.py set_bdev_qos_limit [-h]
                              [--rw_ios_per_sec RW_IOS_PER_SEC]
                              [--rw_mbytes_per_sec RW_MBYTES_PER_SEC]
                              name
    
    positional arguments:
      name       Blockdev name to set QoS. Example: Malloc0
    
    optional arguments:
      -h, --help show this help message and exit
      --rw_ios_per_sec RW_IOS_PER_SEC
                 R/W IOs per second limit (>=10000, example: 20000).
                 -1 means unlimited.
      --rw_mbytes_per_sec RW_MBYTES_PER_SEC
                 R/W megabytes per second limit (>=10, example: 100).
                 -1 means unlimited.
    
    Change-Id: I9c03cd635280add01801a81c6a6c02f0cf85bee1
    Signed-off-by: GangCao <gang.cao@intel.com>

--- include/spdk/bdev.h
+++ include/spdk/bdev.h
@@ -106,6 +106,26 @@ enum spdk_bdev_io_type {
 	SPDK_BDEV_NUM_IO_TYPES /* Keep last */
 };
 
+/** bdev QoS rate limit type */
+enum spdk_bdev_qos_type {
+	/** Keep first */
+	SPDK_BDEV_QOS_INVALID_RATE_LIMIT = 0,
+	/** IOPS rate limit for both read and write */
+	SPDK_BDEV_QOS_RW_IOPS_RATE_LIMIT = 1 << 0,
+	/** Byte per second rate limit for both read and write */
+	SPDK_BDEV_QOS_RW_BPS_RATE_LIMIT = 1 << 1,
+	/** Keep last */
+	SPDK_BDEV_QOS_NUM_TYPES = 1 << 2
+};
+
+/** bdev QoS rate limit value */
+struct spdk_bdev_qos_rate_limits {
+	/** Read/Write I/Os per second */
+	int32_t rw_ios_per_sec;
+	/** Read/Write megabytes per second */
+	int32_t rw_mbytes_per_sec;
+};
+
 /**
  * Block device completion callback.
  *
@@ -340,14 +360,17 @@ uint64_t spdk_bdev_get_num_blocks(const struct spdk_bdev *bdev);
 uint64_t spdk_bdev_get_qos_ios_per_sec(struct spdk_bdev *bdev);
 
 /**
- * Set an IOPS-based quality of service rate limit on a bdev.
+ * Set the quality of service rate limit on a bdev.
  *
  * \param bdev Block device.
- * \param ios_per_sec I/O per second limit.
+ * \param limits Pointer to the QoS rate limits structure which holding the limits.
+ * \param types Types of rate limit, for example R/W IOPS limit type.
  * \param cb_fn Callback function to be called when the QoS limit has been updated.
  * \param cb_arg Argument to pass to cb_fn.
  */
-void spdk_bdev_set_qos_limit_iops(struct spdk_bdev *bdev, uint64_t ios_per_sec,
+void spdk_bdev_set_qos_rate_limit(struct spdk_bdev *bdev,
+				  struct spdk_bdev_qos_rate_limits *limits,
+				  uint32_t types,
 				  void (*cb_fn)(void *cb_arg, int status), void *cb_arg);
 
 /**
