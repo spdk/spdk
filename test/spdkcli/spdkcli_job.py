e4637666ad939441f24760ab26cd881bae365912
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-06-22 21:27:31 +0200

    test/spdkcli: Add test for SPDK CLI
    Change-Id: I640548858b5e2666c78ff5e1b2b2d0d8066a0ba6
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/spdkcli/spdkcli_job.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.5
+import pexpect
+import os
+import sys
+
+
+def execute_command(cmd, element=None, element_exists=False):
+    child.sendline(cmd)
+    child.expect("/>")
+    print("before: %s" % child.before.decode())
+    if "error response" in child.before.decode():
+        print("Error in cmd: %s" % cmd)
+        exit(1)
+    ls_tree = cmd.split(" ")[0]
+    if ls_tree and element:
+        child.sendline("ls %s" % ls_tree)
+        child.expect("/>")
+        print("child: %s" % child.before.decode())
+        if element_exists:
+            if element not in child.before.decode():
+                print("Element %s not in list" % element)
+                exit(1)
+        else:
+            if element in child.before.decode():
+                print("Element %s is in list" % element)
+                exit(1)
+
+
+if __name__ == "__main__":
+    testdir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    child = pexpect.spawn(os.path.join(testdir, "../../scripts/spdkcli.py"))
+    child.expect(">")
+    child.sendline("cd /")
+    child.expect("/>")
+
+    execute_command(*sys.argv[1:])
