4c655a4cbbf3150deff25c83a082ad6393c619a6
Maciej Szwed <maciej.szwed@intel.com>
2018-06-22 09:42:57 +0200

    bdev: add delete_null_bdev call
    Since delete_bdev should be used only for debug purpose,
    this patch adds delete call specific for null bdev.
    
    Changes in spdkcli done accordingly.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I576322257e5cf70ec03d6b20f8ba43bfce222907

--- scripts/spdkcli/ui_root.py
+++ scripts/spdkcli/ui_root.py
@@ -72,6 +72,9 @@ class UIRoot(UINode):
         response = rpc.bdev.construct_null_bdev(self.client, **kwargs)
         return self.print_array(response)
 
+    def delete_null_bdev(self, **kwargs):
+        rpc.bdev.delete_null_bdev(self.client, **kwargs)
+
     def create_error_bdev(self, **kwargs):
         response = rpc.bdev.construct_error_bdev(self.client, **kwargs)
 
