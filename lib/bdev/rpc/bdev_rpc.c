8afc119c8ea02dd9e47487f0f7c078589bcab1db
GangCao <gang.cao@intel.com>
2018-06-21 22:15:02 -0400

    QoS/Bdev: add RPC support for the bandwidth rate limit
    This patch added the support of RPC method to enable,
    adjust, disable the bandwidth rate limit on the bdev.
    
    And it can work together with the existing IOPS rate limit.
    
    The PRC method has been consolidated to support both IOPS
    and bandwidth rate limits as below:
    
    usage:
    rpc.py set_bdev_qos_limit [-h]
                              [--rw_ios_per_sec RW_IOS_PER_SEC]
                              [--rw_mbytes_per_sec RW_MBYTES_PER_SEC]
                              name
    
    positional arguments:
      name       Blockdev name to set QoS. Example: Malloc0
    
    optional arguments:
      -h, --help show this help message and exit
      --rw_ios_per_sec RW_IOS_PER_SEC
                 R/W IOs per second limit (>=10000, example: 20000).
                 0 means unlimited.
      --rw_mbytes_per_sec RW_MBYTES_PER_SEC
                 R/W megabytes per second limit (>=10, example: 100).
                 0 means unlimited.
    
    Change-Id: I9c03cd635280add01801a81c6a6c02f0cf85bee1
    Signed-off-by: GangCao <gang.cao@intel.com>

--- lib/bdev/rpc/bdev_rpc.c
+++ lib/bdev/rpc/bdev_rpc.c
@@ -219,6 +219,8 @@ spdk_rpc_dump_bdev_info(struct spdk_json_write_ctx *w,
 			struct spdk_bdev *bdev)
 {
 	struct spdk_bdev_alias *tmp;
+	uint64_t qos_limits[SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES];
+	int i;
 
 	spdk_json_write_object_begin(w);
 
@@ -250,8 +252,14 @@ spdk_rpc_dump_bdev_info(struct spdk_json_write_ctx *w,
 		spdk_json_write_named_string(w, "uuid", uuid_str);
 	}
 
-	spdk_json_write_name(w, "qos_ios_per_sec");
-	spdk_json_write_uint64(w, spdk_bdev_get_qos_ios_per_sec(bdev));
+	spdk_json_write_name(w, "assigned_rate_limits");
+	spdk_json_write_object_begin(w);
+	spdk_bdev_get_qos_rate_limits(bdev, qos_limits);
+	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
+		spdk_json_write_name(w, spdk_bdev_get_qos_rpc_param(i));
+		spdk_json_write_uint64(w, qos_limits[i]);
+	}
+	spdk_json_write_object_end(w);
 
 	spdk_json_write_name(w, "claimed");
 	spdk_json_write_bool(w, (bdev->internal.claim_module != NULL));
@@ -544,31 +552,40 @@ SPDK_RPC_REGISTER("set_bdev_qd_sampling_period",
 		  spdk_rpc_set_bdev_qd_sampling_period,
 		  SPDK_RPC_RUNTIME)
 
-struct rpc_set_bdev_qos_limit_iops {
+struct rpc_set_bdev_qos_limit {
 	char		*name;
-	uint64_t	ios_per_sec;
+	uint64_t	limits[SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES];
 };
 
 static void
-free_rpc_set_bdev_qos_limit_iops(struct rpc_set_bdev_qos_limit_iops *r)
+free_rpc_set_bdev_qos_limit(struct rpc_set_bdev_qos_limit *r)
 {
 	free(r->name);
 }
 
-static const struct spdk_json_object_decoder rpc_set_bdev_qos_limit_iops_decoders[] = {
-	{"name", offsetof(struct rpc_set_bdev_qos_limit_iops, name), spdk_json_decode_string},
-	{"ios_per_sec", offsetof(struct rpc_set_bdev_qos_limit_iops, ios_per_sec), spdk_json_decode_uint64},
+static const struct spdk_json_object_decoder rpc_set_bdev_qos_limit_decoders[] = {
+	{"name", offsetof(struct rpc_set_bdev_qos_limit, name), spdk_json_decode_string},
+	{
+		"rw_ios_per_sec", offsetof(struct rpc_set_bdev_qos_limit,
+					   limits[SPDK_BDEV_QOS_RW_IOPS_RATE_LIMIT]),
+		spdk_json_decode_uint64, true
+	},
+	{
+		"rw_mbytes_per_sec", offsetof(struct rpc_set_bdev_qos_limit,
+					      limits[SPDK_BDEV_QOS_RW_BPS_RATE_LIMIT]),
+		spdk_json_decode_uint64, true
+	},
 };
 
 static void
-spdk_rpc_set_bdev_qos_limit_iops_complete(void *cb_arg, int status)
+spdk_rpc_set_bdev_qos_limit_complete(void *cb_arg, int status)
 {
 	struct spdk_jsonrpc_request *request = cb_arg;
 	struct spdk_json_write_ctx *w;
 
 	if (status != 0) {
 		spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
-						     "Failed to configure IOPS limit: %s",
+						     "Failed to configure rate limit: %s",
 						     spdk_strerror(-status));
 		return;
 	}
@@ -583,14 +600,16 @@ spdk_rpc_set_bdev_qos_limit_iops_complete(void *cb_arg, int status)
 }
 
 static void
-spdk_rpc_set_bdev_qos_limit_iops(struct spdk_jsonrpc_request *request,
-				 const struct spdk_json_val *params)
+spdk_rpc_set_bdev_qos_limit(struct spdk_jsonrpc_request *request,
+			    const struct spdk_json_val *params)
 {
-	struct rpc_set_bdev_qos_limit_iops req = {};
+	struct rpc_set_bdev_qos_limit req = {NULL, {UINT64_MAX, UINT64_MAX}};
 	struct spdk_bdev *bdev;
+	bool valid_limit = false;
+	int i;
 
-	if (spdk_json_decode_object(params, rpc_set_bdev_qos_limit_iops_decoders,
-				    SPDK_COUNTOF(rpc_set_bdev_qos_limit_iops_decoders),
+	if (spdk_json_decode_object(params, rpc_set_bdev_qos_limit_decoders,
+				    SPDK_COUNTOF(rpc_set_bdev_qos_limit_decoders),
 				    &req)) {
 		SPDK_ERRLOG("spdk_json_decode_object failed\n");
 		goto invalid;
@@ -599,17 +618,32 @@ spdk_rpc_set_bdev_qos_limit_iops(struct spdk_jsonrpc_request *request,
 	bdev = spdk_bdev_get_by_name(req.name);
 	if (bdev == NULL) {
 		SPDK_ERRLOG("bdev '%s' does not exist\n", req.name);
-		goto invalid;
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "Bdev does not exist");
+		goto exit;
 	}
 
-	free_rpc_set_bdev_qos_limit_iops(&req);
-	spdk_bdev_set_qos_limit_iops(bdev, req.ios_per_sec,
-				     spdk_rpc_set_bdev_qos_limit_iops_complete, request);
+	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
+		if (req.limits[i] != UINT64_MAX) {
+			valid_limit = true;
+		}
+	}
+
+	if (valid_limit == false) {
+		SPDK_ERRLOG("invalid rate limits set\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "Invalid rate limits");
+		goto exit;
+	}
+
+	free_rpc_set_bdev_qos_limit(&req);
+	spdk_bdev_set_qos_rate_limits(bdev, req.limits, spdk_rpc_set_bdev_qos_limit_complete, request);
 	return;
 
 invalid:
 	spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS, "Invalid parameters");
-	free_rpc_set_bdev_qos_limit_iops(&req);
+exit:
+	free_rpc_set_bdev_qos_limit(&req);
 }
 
-SPDK_RPC_REGISTER("set_bdev_qos_limit_iops", spdk_rpc_set_bdev_qos_limit_iops, SPDK_RPC_RUNTIME)
+SPDK_RPC_REGISTER("set_bdev_qos_limit", spdk_rpc_set_bdev_qos_limit, SPDK_RPC_RUNTIME)
