e060c96c2c10192738faf6f8ade795b8169a421f
GangCao <gang.cao@intel.com>
2018-06-21 22:15:02 -0400

    QoS/Bdev: add the RPC support for Read/Write separate IOPS limit
    This patch adds the RPC support for the Read/Write separate
    IOPS limit control. The basic usage as following:
    
    usage:
    rpc.py set_bdev_qos_limit [-h] [--rw_ios_per_sec RW_IOS_PER_SEC]
                                   [--r_ios_per_sec R_IOS_PER_SEC]
                                   [--w_ios_per_sec W_IOS_PER_SEC]
                                   [--rw_mbytes_per_sec RW_MBYTES_PER_SEC]
                                   name
    
    positional arguments:
      name       Blockdev name to set QoS. Example: Malloc0
    
    optional arguments:
      -h, --help show this help message and exit
      --rw_ios_per_sec RW_IOS_PER_SEC
                 R/W IOs per second limit (>=10000, example: 20000).
                 0 means unlimited.
      --r_ios_per_sec R_IOS_PER_SEC
                 Read IOs per second limit (>=10000, example: 20000).
                 0 means unlimited.
      --w_ios_per_sec W_IOS_PER_SEC
                 Write IOs per second limit (>=10000, example: 20000).
                 0 means unlimited.
      --rw_mbytes_per_sec RW_MBYTES_PER_SEC
                 R/W megabytes per second limit (>=10, example: 100).
                 0 means unlimited.
    
    Change-Id: I16bfecffa591e3b2460e550c762d841ffce63529
    Signed-off-by: GangCao <gang.cao@intel.com>

--- test/unit/lib/bdev/mt/bdev.c/bdev_ut.c
+++ test/unit/lib/bdev/mt/bdev.c/bdev_ut.c
@@ -1102,9 +1102,27 @@ qos_dynamic_enable(void)
 
 	set_thread(0);
 
-	/* Enable QoS: IOPS rate limit */
+	/* Enable QoS: Read/Write IOPS rate limit */
 	status = -1;
 	limits[SPDK_BDEV_QOS_RW_IOPS_RATE_LIMIT] = 10000;
+	spdk_bdev_set_qos_rate_limits(bdev, limits, qos_dynamic_enable_done, &status);
+	poll_threads();
+	CU_ASSERT(status == 0);
+	CU_ASSERT((bdev_ch[0]->flags & BDEV_CH_QOS_ENABLED) != 0);
+	CU_ASSERT((bdev_ch[1]->flags & BDEV_CH_QOS_ENABLED) != 0);
+
+	/* Enable QoS: Read only IOPS rate limit */
+	status = -1;
+	limits[SPDK_BDEV_QOS_R_IOPS_RATE_LIMIT] = 10000;
+	spdk_bdev_set_qos_rate_limits(bdev, limits, qos_dynamic_enable_done, &status);
+	poll_threads();
+	CU_ASSERT(status == 0);
+	CU_ASSERT((bdev_ch[0]->flags & BDEV_CH_QOS_ENABLED) != 0);
+	CU_ASSERT((bdev_ch[1]->flags & BDEV_CH_QOS_ENABLED) != 0);
+
+	/* Enable QoS: Write only IOPS rate limit */
+	status = -1;
+	limits[SPDK_BDEV_QOS_W_IOPS_RATE_LIMIT] = 10000;
 	spdk_bdev_set_qos_rate_limits(bdev, limits, qos_dynamic_enable_done, &status);
 	poll_threads();
 	CU_ASSERT(status == 0);
@@ -1152,9 +1170,27 @@ qos_dynamic_enable(void)
 	CU_ASSERT(bdev_io_status[1] == SPDK_BDEV_IO_STATUS_PENDING);
 	poll_threads();
 
-	/* Disable QoS: IOPS rate limit */
+	/* Disable QoS: Read/Write IOPS rate limit */
 	status = -1;
 	limits[SPDK_BDEV_QOS_RW_IOPS_RATE_LIMIT] = 0;
+	spdk_bdev_set_qos_rate_limits(bdev, limits, qos_dynamic_enable_done, &status);
+	poll_threads();
+	CU_ASSERT(status == 0);
+	CU_ASSERT((bdev_ch[0]->flags & BDEV_CH_QOS_ENABLED) != 0);
+	CU_ASSERT((bdev_ch[1]->flags & BDEV_CH_QOS_ENABLED) != 0);
+
+	/* Disable QoS: Read only IOPS rate limit */
+	status = -1;
+	limits[SPDK_BDEV_QOS_R_IOPS_RATE_LIMIT] = 0;
+	spdk_bdev_set_qos_rate_limits(bdev, limits, qos_dynamic_enable_done, &status);
+	poll_threads();
+	CU_ASSERT(status == 0);
+	CU_ASSERT((bdev_ch[0]->flags & BDEV_CH_QOS_ENABLED) != 0);
+	CU_ASSERT((bdev_ch[1]->flags & BDEV_CH_QOS_ENABLED) != 0);
+
+	/* Disable QoS: Write only IOPS rate limit */
+	status = -1;
+	limits[SPDK_BDEV_QOS_W_IOPS_RATE_LIMIT] = 0;
 	spdk_bdev_set_qos_rate_limits(bdev, limits, qos_dynamic_enable_done, &status);
 	poll_threads();
 	CU_ASSERT(status == 0);
