641f52a93d1b3b3ea43236f9ab9cc3695f0cfac5
GangCao <gang.cao@intel.com>
2018-06-21 22:15:02 -0400

    QoS/Bdev: add RPC support for Read/Write separate bandwidth limits
    This patch adds the RPC support for the Read/Write separate
    bandwidth limit control. The basic usage as following:
    
    usage:
    rpc.py set_bdev_qos_limit [-h] [--rw_ios_per_sec RW_IOS_PER_SEC]
                                   [--rw_mbytes_per_sec RW_MBYTES_PER_SEC]
                                   [--r_mbytes_per_sec R_MBYTES_PER_SEC]
                                   [--w_mbytes_per_sec W_MBYTES_PER_SEC]
                                   name
    
    positional arguments:
      name       Blockdev name to set QoS. Example: Malloc0
    
    optional arguments:
      -h, --help show this help message and exit
      --rw_ios_per_sec RW_IOS_PER_SEC
                 R/W IOs per second limit (>=10000, example: 20000).
                 0 means unlimited.
      --rw_mbytes_per_sec RW_MBYTES_PER_SEC
                 R/W megabytes per second limit (>=10, example: 100).
                 0 means unlimited.
      --r_mbytes_per_sec R_MBYTES_PER_SEC
                 Read megabytes per second limit (>=10, example: 100).
                 0 means unlimited.
      --w_mbytes_per_sec W_MBYTES_PER_SEC
                 Write megabytes per second limit (>=10, example: 100).
                 0 means unlimited.
    
    Change-Id: I822ec4814d21adff9826ce03a6af3783b1b98f44
    Signed-off-by: GangCao <gang.cao@intel.com>

--- scripts/rpc/bdev.py
+++ scripts/rpc/bdev.py
@@ -518,13 +518,21 @@ def set_bdev_qd_sampling_period(client, name, period):
     return client.call('set_bdev_qd_sampling_period', params)
 
 
-def set_bdev_qos_limit(client, name, rw_ios_per_sec=None, rw_mbytes_per_sec=None):
+def set_bdev_qos_limit(
+        client,
+        name,
+        rw_ios_per_sec=None,
+        rw_mbytes_per_sec=None,
+        r_mbytes_per_sec=None,
+        w_mbytes_per_sec=None):
     """Set QoS rate limit on a block device.
 
     Args:
         name: name of block device
         rw_ios_per_sec: R/W IOs per second limit (>=10000, example: 20000). 0 means unlimited.
         rw_mbytes_per_sec: R/W megabytes per second limit (>=10, example: 100). 0 means unlimited.
+        r_mbytes_per_sec: Read megabytes per second limit (>=10, example: 100). 0 means unlimited.
+        w_mbytes_per_sec: Write megabytes per second limit (>=10, example: 100). 0 means unlimited.
     """
     params = {}
     params['name'] = name
@@ -532,6 +540,10 @@ def set_bdev_qos_limit(client, name, rw_ios_per_sec=None, rw_mbytes_per_sec=None
         params['rw_ios_per_sec'] = rw_ios_per_sec
     if rw_mbytes_per_sec is not None:
         params['rw_mbytes_per_sec'] = rw_mbytes_per_sec
+    if r_mbytes_per_sec is not None:
+        params['r_mbytes_per_sec'] = r_mbytes_per_sec
+    if w_mbytes_per_sec is not None:
+        params['w_mbytes_per_sec'] = w_mbytes_per_sec
     return client.call('set_bdev_qos_limit', params)
 
 
