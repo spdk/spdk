2772cc67b4ea3644975011472cfdeaa8265c0672
GangCao <gang.cao@intel.com>
2018-06-21 22:15:02 -0400

    QoS/Bdev: add RPC support for Read/Write separate IOPS limits
    This patch adds the RPC support for the Read/Write separate
    IOPS limit control. The basic usage as following:
    
    usage:
    rpc.py set_bdev_qos_limit [-h] [--rw_ios_per_sec RW_IOS_PER_SEC]
                                   [--r_ios_per_sec R_IOS_PER_SEC]
                                   [--w_ios_per_sec W_IOS_PER_SEC]
                                   [--rw_mbytes_per_sec RW_MBYTES_PER_SEC]
                                   [--r_mbytes_per_sec R_MBYTES_PER_SEC]
                                   [--w_mbytes_per_sec W_MBYTES_PER_SEC]
                                   name
    
    positional arguments:
      name                  Blockdev name to set QoS. Example: Malloc0
    
    optional arguments:
      -h, --help            show this help message and exit
      --rw_ios_per_sec RW_IOS_PER_SEC
                            R/W IOs per second limit (>=10000, example: 20000). 0
                            means unlimited.
      --r_ios_per_sec R_IOS_PER_SEC
                            Read IOs per second limit (>=10000, example: 20000). 0
                            means unlimited.
      --w_ios_per_sec W_IOS_PER_SEC
                            Write IOs per second limit (>=10000, example: 20000).
                            0 means unlimited.
      --rw_mbytes_per_sec RW_MBYTES_PER_SEC
                            R/W megabytes per second limit (>=10, example: 100). 0
                            means unlimited.
      --r_mbytes_per_sec R_MBYTES_PER_SEC
                            Read megabytes per second limit (>=10, example: 100).
                            0 means unlimited.
      --w_mbytes_per_sec W_MBYTES_PER_SEC
                            Write megabytes per second limit (>=10, example: 100).
                            0 means unlimited.
    
    Change-Id: I16bfecffa591e3b2460e550c762d841ffce63529
    Signed-off-by: GangCao <gang.cao@intel.com>

--- test/iscsi_tgt/qos/qos.sh
+++ test/iscsi_tgt/qos/qos.sh
@@ -61,6 +61,7 @@ timing_enter qos
 MALLOC_BDEV_SIZE=64
 MALLOC_BLOCK_SIZE=512
 IOPS_LIMIT=20000
+READ_IOPS_LIMIT=50000
 BANDWIDTH_LIMIT=20
 READ_BANDWIDTH_LIMIT=10
 LIMIT_TYPE=IOPS
@@ -101,14 +102,15 @@ check_qos_works_well true $IOPS_LIMIT Malloc0
 $rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec 0
 check_qos_works_well false $IOPS_LIMIT Malloc0
 
-# Limit the I/O rate again.
+# Limit the I/O rate again together against read only I/O.
 $rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec $IOPS_LIMIT
+$rpc_py set_bdev_qos_limit Malloc0 --r_ios_per_sec $READ_IOPS_LIMIT
 check_qos_works_well true $IOPS_LIMIT Malloc0
 echo "I/O rate limiting tests successful"
 
 # Limit the I/O bandwidth rate by RPC, then confirm the observed rate matches.
 LIMIT_TYPE=BANDWIDTH
-$rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec 0 --rw_mbytes_per_sec $BANDWIDTH_LIMIT
+$rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec 0 --r_ios_per_sec 0 --rw_mbytes_per_sec $BANDWIDTH_LIMIT
 check_qos_works_well true $BANDWIDTH_LIMIT Malloc0
 
 # Now disable the bandwidth rate limiting, and confirm the observed rate is not limited anymore.
