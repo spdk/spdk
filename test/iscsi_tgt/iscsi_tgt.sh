319e7cc1cce3402f1fd079cd584c3dd3c57d47f9
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2018-06-25 08:44:59 -0400

    vpp: include VPP into iSCSI test scripts
    Tests that were thus far performed using posix net framework
    can now be run with VPP. This patch adds network interface
    configuration for VPP to work in iSCSI tests.
    
    Some tests are disabled on purpose:
    - RBD and NVMe-oF due to their tests lacking network namespace support
    - rpc_config adding/deleting IP, as VPP has separate utility for that
    
    calsoft.sh, filesystem.sh and reset.sh still have pending compatibility
    issues and are temporarily disabled when run with VPP.
    
    Change-Id: I7c80427ca1675a1789ce7440796cc8d9956f1c9e
    Signed-off-by: Slawomir Mrozowicz <slawomirx.mrozowicz@intel.com>
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- test/iscsi_tgt/iscsi_tgt.sh
+++ test/iscsi_tgt/iscsi_tgt.sh
@@ -25,14 +25,18 @@ create_veth_interfaces $TEST_TYPE
 trap "cleanup_veth_interfaces $TEST_TYPE; exit 1" SIGINT SIGTERM EXIT
 
 run_test suite ./test/iscsi_tgt/sock/sock.sh
-run_test suite ./test/iscsi_tgt/calsoft/calsoft.sh
+run_test suite ./test/iscsi_tgt/fio/fio.sh
+run_test suite ./test/iscsi_tgt/rpc_config/rpc_config.sh $TEST_TYPE
+if [ "$TEST_TYPE" == "posix" ]; then
+	run_test suite ./test/iscsi_tgt/calsoft/calsoft.sh
+fi
 run_test suite ./test/iscsi_tgt/filesystem/filesystem.sh
 run_test suite ./test/iscsi_tgt/reset/reset.sh
-run_test suite ./test/iscsi_tgt/rpc_config/rpc_config.sh $TEST_TYPE
 run_test suite ./test/iscsi_tgt/lvol/iscsi_lvol.sh
-run_test suite ./test/iscsi_tgt/fio/fio.sh
 run_test suite ./test/iscsi_tgt/qos/qos.sh
-run_test suite ./test/iscsi_tgt/ip_migration/ip_migration.sh
+if [ "$TEST_TYPE" == "posix" ]; then
+	run_test suite ./test/iscsi_tgt/ip_migration/ip_migration.sh
+fi
 run_test suite ./test/iscsi_tgt/trace_record/trace_record.sh
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
@@ -43,14 +47,22 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 	run_test suite ./test/iscsi_tgt/digests/digests.sh
 fi
 if [ $SPDK_TEST_RBD -eq 1 ]; then
-	run_test suite ./test/iscsi_tgt/rbd/rbd.sh
+	# RBD tests do not support network namespaces,
+	# they can only be run on posix sockets.
+	if [ "$TEST_TYPE" == "posix" ]; then
+		run_test suite ./test/iscsi_tgt/rbd/rbd.sh
+	fi
 fi
 
 trap "cleanup_veth_interfaces $TEST_TYPE; exit 1" SIGINT SIGTERM EXIT
 
 if [ $SPDK_TEST_NVMF -eq 1 ]; then
-	# Test configure remote NVMe device from rpc and conf file
-	run_test suite ./test/iscsi_tgt/nvme_remote/fio_remote_nvme.sh
+	# NVMe-oF tests do not support network namespaces,
+	# they can only be run on posix sockets.
+	if [ "$TEST_TYPE" == "posix" ]; then
+		# Test configure remote NVMe device from rpc and conf file
+		run_test suite ./test/iscsi_tgt/nvme_remote/fio_remote_nvme.sh
+	fi
 fi
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
@@ -58,8 +70,11 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 fi
 
 if [ $SPDK_TEST_ISCSI_INITIATOR -eq 1 ]; then
-	run_test suite ./test/iscsi_tgt/initiator/initiator.sh
 	run_test suite ./test/iscsi_tgt/bdev_io_wait/bdev_io_wait.sh
+	# TODO: fix initiator test for VPP
+	if [ "$TEST_TYPE" == "posix" ]; then
+		run_test suite ./test/iscsi_tgt/initiator/initiator.sh
+	fi
 fi
 
 cleanup_veth_interfaces $TEST_TYPE
