4c1fbb1120cee78042557af03b8b3d14021e9303
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-06-25 20:57:06 +0200

    test/json: Tests for iscsi subsystems
    Change-Id: I7053dca9cc6f2ef09566ba2bf8338f7f4c92ebf0
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

---
+++ test/iscsi_tgt/iscsijson/json_config.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -ex
+ISCSI_JSON_DIR=$(readlink -f $(dirname $0))
+. $ISCSI_JSON_DIR/../../json_config/common.sh
+base_iscsi_config=$JSON_DIR/base_iscsi_config.json
+last_iscsi_config=$JSON_DIR/last_iscsi_config.json
+
+function test_subsystems() {
+	run_spdk_tgt
+
+	rootdir=$(readlink -f $ISCSI_JSON_DIR/../..)
+	rpc_py="$spdk_rpc_py"
+	clear_config_py="$spdk_clear_config_py"
+	load_nvme
+
+	create_iscsi_subsystem_config
+	$rpc_py save_config -f $base_iscsi_config
+	test_json_config
+
+	clear_iscsi_subsystem_config
+	kill_targets
+
+	run_spdk_tgt
+	$rpc_py load_config -f $base_iscsi_config
+	$rpc_py save_config -f $last_iscsi_config
+
+	diff $base_iscsi_config $last_iscsi_config
+
+	clear_iscsi_subsystem_config
+	kill_targets
+	remove_config_files_after_test_json_config
+	rm -f $base_iscsi_config $last_iscsi_config
+}
+
+trap 'on_error_exit "${FUNCNAME}" "${LINENO}"; rm -f $base_iscsi_config $last_iscsi_config' ERR
+
+timing_enter iscsi_json_config
+test_subsystems
+timing_exit iscsi_json_config
+
+report_test_completion iscsi_json_config
