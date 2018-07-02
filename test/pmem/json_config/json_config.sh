af7258b1e15fdccec5d9f4be7f7347873eb60bc4
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-02 17:17:50 -0400

    test/json: Add pmem bdev test
    Change-Id: I50a99f029f10f334370013627b75f61e7eecdcf5
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/pmem/json_config/json_config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -ex
+VHOST_JSON_DIR=$(readlink -f $(dirname $0))
+. $VHOST_JSON_DIR/../../json_config/common.sh
+
+function test_subsystems() {
+	run_spdk_tgt
+	rootdir=$(readlink -f $VHOST_JSON_DIR/../../..)
+
+	rpc_py="$spdk_rpc_py"
+	clear_config_py="$spdk_clear_config_py"
+	load_nvme
+
+	create_pmem_bdev_subsytem_config
+	test_json_config
+	clear_pmem_bdev_subsystem_config
+
+	kill_targets
+}
+
+trap 'on_error_exit "${FUNCNAME}" "${LINENO}"' ERR
+
+test_subsystems
