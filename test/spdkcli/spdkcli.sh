16f0c875724296b5f7702b5e6a2e9092f46bb3d4
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-04 16:24:49 -0400

    test/spdkcli: Test for rbd bdev
    Change-Id: Ic27ee112511241d4d46d5b1d3eeb5ff4be19bee8
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/spdkcli/spdkcli.sh
+++ test/spdkcli/spdkcli.sh
@@ -115,6 +115,19 @@ function run_pmem_test() {
 	timing_exit spdkcli_pmem
 }
 
+function run_rbd_test() {
+	timing_enter spdkcli_rbd
+	trap 'rbd_cleanup; on_error_exit' ERR
+	rootdir=$(readlink -f $SPDK_BUILD_DIR)
+	rbd_setup 127.0.0.1
+
+	$spdkcli_job "/bdevs/rbd create rbd foo 512" "Ceph0" True
+	$spdkcli_job "/bdevs/rbd delete Ceph0" "Ceph0"
+
+	rbd_cleanup
+	timing_exit spdkcli_rbd
+}
+
 timing_enter spdk_cli
 trap 'on_error_exit' ERR
 
@@ -122,7 +135,7 @@ run_spdk_tgt
 
 case $1 in
 	-h|--help)
-		echo "usage: $(basename $0) TEST_TYPE"
+		echo "Usage: $(basename $0) TEST_TYPE"
 		echo "Test type can be:"
 		echo "	--vhost"
 		echo "	--pmem"
@@ -132,9 +145,12 @@ case $1 in
 		;;
 	-p|--pmem)
 		run_pmem_test
+                ;;
+	-r|--rbd)
+		run_rbd_test
 		;;
 	*)
-		echo "unknown test type: $1"
+		echo "Unknown test type: $1"
 		exit 1
 	;;
 esac
