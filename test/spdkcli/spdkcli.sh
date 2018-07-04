5ac1d81798911c6a300d08c0b919a598d691c00d
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-04 11:30:59 -0400

    test/spdkcli: Test for pmem bdev.
    Change-Id: I1917ca3002f34b90d74ce360987fc63b3064a20f
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/spdkcli/spdkcli.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -xe
+
+testdir=$(readlink -f $(dirname $0))
+SPDKCLI_BUILD_DIR=$(readlink -f $testdir/../..)
+spdkcli_job="python3 $SPDKCLI_BUILD_DIR/test/spdkcli/spdkcli_job.py"
+. $SPDKCLI_BUILD_DIR/test/common/autotest_common.sh
+
+function on_error_exit() {
+	set +e
+	killprocess $spdk_tgt_pid
+	rm -f $testdir/${MATCH_FILE} $testdir/spdkcli_details_vhost.test /tmp/sample_aio
+	print_backtrace
+	exit 1
+}
+
+function run_spdk_tgt() {
+	$SPDKCLI_BUILD_DIR/app/spdk_tgt/spdk_tgt -m 0x3 -p 0 -s 1024 &
+	spdk_tgt_pid=$!
+	waitforlisten $spdk_tgt_pid
+}
+
+function check_match() {
+	python3 $SPDKCLI_BUILD_DIR/scripts/spdkcli.py ll > $testdir/${MATCH_FILE}
+	$SPDKCLI_BUILD_DIR/test/app/match/match -v $testdir/${MATCH_FILE}.match
+	rm -f $testdir/${MATCH_FILE}
+}
