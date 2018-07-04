067603aa54c87f392b88a03630fe093d43446cc1
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-04 11:30:59 -0400

    test/spdkcli: Test for pmem bdev
    Change-Id: I1917ca3002f34b90d74ce360987fc63b3064a20f
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -191,7 +191,7 @@ if [ $SPDK_TEST_VHOST -eq 1 ]; then
 	timing_exit integrity_lvol_blk
 
 	timing_enter spdk_cli
-	run_test ./test/spdkcli/spdkcli.sh
+	run_test ./test/spdkcli/spdkcli.sh --vhost
 	timing_exit spdk_cli
 
 	timing_exit vhost
@@ -217,6 +217,7 @@ fi
 if [ $SPDK_TEST_PMDK -eq 1 ]; then
 	run_test ./test/pmem/pmem.sh -x
 	run_test ./test/pmem/json_config/json_config.sh
+	run_test ./test/spdkcli/spdkcli.sh --pmem
 fi
 
 if [ $SPDK_TEST_RBD -eq 1 ]; then
