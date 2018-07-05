b5f145d7969e4be67b1b231b52c9657c1d021e80
Liu Xiaodong <xiaodong.liu@intel.com>
2018-07-05 03:46:48 -0400

    scripts/rpc.py: add method "send_nvme_cmd"
    An example to send nvme_cmd rpc request:
    scripts/rpc.py send_nvme_cmd -n Nvme0 -t admin -r c2h \
    -c <base64_urlsafe encoded cmdbuf> -D 4096
    
    nvme-rpc will be processed internally by bdev_nvme.
    
    Change-Id: I6e731b76be0f503d48154a8b34a1e81b4b454396
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>

--- lib/bdev/nvme/bdev_nvme.c
+++ lib/bdev/nvme/bdev_nvme.c
@@ -132,6 +132,30 @@ static int bdev_nvme_io_passthru_md(struct nvme_bdev *nbdev, struct spdk_io_chan
 				    struct spdk_nvme_cmd *cmd, void *buf, size_t nbytes, void *md_buf, size_t md_len);
 static int nvme_ctrlr_create_bdev(struct nvme_ctrlr *nvme_ctrlr, uint32_t nsid);
 
+struct spdk_nvme_qpair *
+spdk_bdev_nvme_get_io_qpair(struct spdk_io_channel *ctrlr_io_ch)
+{
+	struct nvme_io_channel *nvme_ch;
+
+	nvme_ch =  spdk_io_channel_get_ctx(ctrlr_io_ch);
+
+	return nvme_ch->qpair;
+}
+
+struct nvme_ctrlr *
+spdk_bdev_nvme_lookup_ctrlr(const char *ctrlr_name)
+{
+	struct nvme_ctrlr *_nvme_ctrlr;
+
+	TAILQ_FOREACH(_nvme_ctrlr, &g_nvme_ctrlrs, tailq) {
+		if (strcmp(ctrlr_name, _nvme_ctrlr->name) == 0) {
+			return _nvme_ctrlr;
+		}
+	}
+
+	return NULL;
+}
+
 static int
 bdev_nvme_get_ctx_size(void)
 {
@@ -148,6 +172,20 @@ static struct spdk_bdev_module nvme_if = {
 
 };
 SPDK_BDEV_MODULE_REGISTER(&nvme_if)
+
+struct nvme_bdev *
+spdk_bdev_nvme_lookup_bdev(const char *ns_name)
+{
+	struct spdk_bdev *bdev;
+
+	bdev = spdk_bdev_get_by_name(ns_name);
+
+	if (bdev && bdev->module == &nvme_if) {
+		return (struct nvme_bdev *)bdev->ctxt;
+	}
+
+	return NULL;
+}
 
 static int
 bdev_nvme_readv(struct nvme_bdev *nbdev, struct spdk_io_channel *ch,
