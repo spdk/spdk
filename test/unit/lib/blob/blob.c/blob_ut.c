b4414105c9b46701f7ed01ca0e4b8bad632865e8
Ben Walker <benjamin.walker@intel.com>
2018-07-10 10:47:14 -0700

    test: Simplify the mock library
    Unify the regular mocks and the pointer mocks. Simplify
    several of the #defines.
    
    Change-Id: Ica8c69dbb70a685a55b5961b73fd7872f451c305
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/unit/lib/blob/blob.c/blob_ut.c
+++ test/unit/lib/blob/blob.c/blob_ut.c
@@ -1541,7 +1541,7 @@ blob_rw_verify_iov_nomem(void)
 	spdk_blob_io_writev(blob, channel, iov_write, 3, 250, 10, blob_op_complete, NULL);
 	CU_ASSERT(g_bserrno = -ENOMEM);
 	CU_ASSERT(req_count == bs_channel_get_req_count(channel));
-	MOCK_SET(calloc, void *, (void *)MOCK_PASS_THRU);
+	MOCK_CLEAR(calloc);
 
 	spdk_blob_close(blob, blob_op_complete, NULL);
 	CU_ASSERT(g_bserrno == 0);
@@ -4527,7 +4527,7 @@ blob_relations(void)
 	spdk_blob_id blobid, cloneid, snapshotid, cloneid2, snapshotid2;
 	int rc;
 	size_t count;
-	spdk_blob_id ids[10];
+	spdk_blob_id ids[10] = {};
 
 	dev = init_dev();
 	spdk_bs_opts_init(&bs_opts);
