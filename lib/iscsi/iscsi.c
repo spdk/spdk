9d9a9b2656cb56253b3eb9964add9f24e89ca6f1
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-07-11 14:24:10 +0900

    iscsi: Fix the issue that queued iSCSI tasks are not migrated when clearing tasks
    When multiple LUNs are attached and spdk_clear_all_transfer_tasks() is called with
    a LUN, all tasks whose target is the LUN are removed and there will be room to start
    queued tasks for other LUNs. However currently no migration is done.
    
    Add UT code to verify the fix too.
    
    Change-Id: I082d370ab86a46e5b4a74a16293a572fae663add
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/iscsi/iscsi.c
+++ lib/iscsi/iscsi.c
@@ -3516,9 +3516,24 @@ spdk_add_transfer_task(struct spdk_iscsi_conn *conn,
 	return SPDK_SUCCESS;
 }
 
-void spdk_del_transfer_task(struct spdk_iscsi_conn *conn, uint32_t task_tag)
+static void
+spdk_start_queued_transfer_tasks(struct spdk_iscsi_conn *conn)
 {
 	struct spdk_iscsi_task *task, *tmp;
+
+	TAILQ_FOREACH_SAFE(task, &conn->queued_r2t_tasks, link, tmp) {
+		if (conn->pending_r2t < DEFAULT_MAXR2T) {
+			TAILQ_REMOVE(&conn->queued_r2t_tasks, task, link);
+			spdk_add_transfer_task(conn, task);
+		} else {
+			break;
+		}
+	}
+}
+
+void spdk_del_transfer_task(struct spdk_iscsi_conn *conn, uint32_t task_tag)
+{
+	struct spdk_iscsi_task *task;
 	int i;
 
 	for (i = 0; i < conn->pending_r2t; i++) {
@@ -3541,14 +3556,7 @@ void spdk_del_transfer_task(struct spdk_iscsi_conn *conn, uint32_t task_tag)
 	 *  sure each of the tasks will fit without the connection's allotment
 	 *  for total R2T tasks.
 	 */
-	TAILQ_FOREACH_SAFE(task, &conn->queued_r2t_tasks, link, tmp) {
-		if (conn->pending_r2t < DEFAULT_MAXR2T) {
-			TAILQ_REMOVE(&conn->queued_r2t_tasks, task, link);
-			spdk_add_transfer_task(conn, task);
-		} else {
-			break;
-		}
-	}
+	spdk_start_queued_transfer_tasks(conn);
 }
 
 static void
@@ -3604,6 +3612,11 @@ void spdk_clear_all_transfer_task(struct spdk_iscsi_conn *conn,
 
 	spdk_del_connection_queued_task(&conn->active_r2t_tasks, lun);
 	spdk_del_connection_queued_task(&conn->queued_r2t_tasks, lun);
+
+	/* When more than one LUN are attached and only R2T tasks for the specific LUN
+	 * are cleared, start queued R2T tasks for other LUNs.
+	 */
+	spdk_start_queued_transfer_tasks(conn);
 }
 
 /* This function is used to handle the r2t snack */
