7a2ffeebdca4fb6d15fcf6c9607f270106f7cafd
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-07-12 21:26:27 +0200

    test/iscsi: replace config files from iscsi tests with json and rpc calls
    fio.sh still uses conf file because hot remove is not yet imlemented
    in rpc calls
    
    Change-Id: If9066e6b24e1e3d381876909fb9e00000a302e02
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/iscsi_tgt/pmem/iscsi_pmem.sh
+++ test/iscsi_tgt/pmem/iscsi_pmem.sh
@@ -18,13 +18,15 @@ fio_py="python $rootdir/scripts/fio.py"
 timing_enter iscsi_pmem
 
 timing_enter start_iscsi_target
-$ISCSI_APP -c $testdir/iscsi.conf -m $ISCSI_TEST_CORE_MASK &
+$ISCSI_APP -m $ISCSI_TEST_CORE_MASK -w &
 pid=$!
 echo "Process pid: $pid"
 
 trap "iscsicleanup; killprocess $pid; rm -f /tmp/pool_file*; exit 1" SIGINT SIGTERM EXIT
 
 waitforlisten $pid
+$rpc_py set_iscsi_options -o 30 -a 16
+$rpc_py start_subsystem_init
 echo "iscsi_tgt is listening. Running tests..."
 timing_exit start_iscsi_target
 
