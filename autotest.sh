9fddb421aa456e2a65dc5e7ebaf9b52cf305ddd0
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-12 10:22:17 -0400

    test/sharedctrlr: Add test for connecting to the same controller                   by two different applications
    Change-Id: I2c2a96a709c567f5c910715a5b3d85a4e28bcb97
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -141,6 +141,10 @@ if [ $SPDK_TEST_VHOST -eq 1 ]; then
 	run_test ./test/vhost/spdk_vhost.sh --negative
 	timing_exit negative
 
+	timing_enter shared_controllers
+	run_test ./test/vhost/spdk_vhost.sh --shared
+	timing_exit shared_controllers
+
 	timing_enter vhost_json_config
 	run_test ./test/vhost/json_config/json_config.sh
 	timing_exit vhost_json_config
