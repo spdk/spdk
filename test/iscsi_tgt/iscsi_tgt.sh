b6b238be85d7ab1e5137683f236b5a37ea524c95
Seth Howell <seth.howell@intel.com>
2018-07-12 16:53:47 -0700

    test/iscsi_tgt: always run fio_remote_nvme tests.
    These tests were previously hidden behind a flag check for NVMe-oF
    tests. I removed this check to avoid interdependencies between test
    flags.
    
    Change-Id: I540856ce204d86248797746db72c98e94d25f781
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/iscsi_tgt/iscsi_tgt.sh
+++ test/iscsi_tgt/iscsi_tgt.sh
@@ -55,12 +55,10 @@ fi
 trap "cleanup_veth_interfaces $TEST_TYPE; exit 1" SIGINT SIGTERM EXIT
 kill_stub
 
-if [ $SPDK_TEST_NVMF -eq 1 ]; then
-	# TODO: enable remote NVMe controllers with multi-process so that
-	#  we can use the stub for this test
-	# Test configure remote NVMe device from rpc and conf file
-	run_test ./test/iscsi_tgt/nvme_remote/fio_remote_nvme.sh
-fi
+# TODO: enable remote NVMe controllers with multi-process so that
+#  we can use the stub for this test
+# Test configure remote NVMe device from rpc and conf file
+run_test ./test/iscsi_tgt/nvme_remote/fio_remote_nvme.sh
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	run_test ./test/iscsi_tgt/multiconnection/multiconnection.sh
