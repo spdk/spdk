06dc3c5b14e4520e7b4f2440bc0939b6c5dd34df
Seth Howell <seth.howell@intel.com>
2018-07-13 16:23:23 -0700

    subsystem: defer channel iter until pg functions return
    The poll group pause, resume, remove, and add functions are only called
    from the subsystem_state_change_on_pg function. Previously, they would
    return immediately and the state change would move on to the next
    channel. However, some of these functions (specifically remove) kick off
    asynchronous APIs and we should not iterate past them until those
    asynchronous operations complete.
    
    Change-Id: I78804273b39f2d171ba26ac4478ad515356833f3
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/unit/lib/nvmf/ctrlr_discovery.c/ctrlr_discovery_ut.c
+++ test/unit/lib/nvmf/ctrlr_discovery.c/ctrlr_discovery_ut.c
@@ -170,32 +170,32 @@ spdk_nvmf_poll_group_update_subsystem(struct spdk_nvmf_poll_group *group,
 	return 0;
 }
 
-int
+void
 spdk_nvmf_poll_group_add_subsystem(struct spdk_nvmf_poll_group *group,
-				   struct spdk_nvmf_subsystem *subsystem)
+				   struct spdk_nvmf_subsystem *subsystem,
+				   spdk_nvmf_poll_group_mod_done cb_fn, void *cb_arg)
 {
-	return 0;
 }
 
-int
+void
 spdk_nvmf_poll_group_remove_subsystem(struct spdk_nvmf_poll_group *group,
-				      struct spdk_nvmf_subsystem *subsystem)
+				      struct spdk_nvmf_subsystem *subsystem,
+				      spdk_nvmf_poll_group_mod_done cb_fn, void *cb_arg)
 {
-	return 0;
 }
 
-int
+void
 spdk_nvmf_poll_group_pause_subsystem(struct spdk_nvmf_poll_group *group,
-				     struct spdk_nvmf_subsystem *subsystem)
+				     struct spdk_nvmf_subsystem *subsystem,
+				     spdk_nvmf_poll_group_mod_done cb_fn, void *cb_arg)
 {
-	return 0;
 }
 
-int
+void
 spdk_nvmf_poll_group_resume_subsystem(struct spdk_nvmf_poll_group *group,
-				      struct spdk_nvmf_subsystem *subsystem)
+				      struct spdk_nvmf_subsystem *subsystem,
+				      spdk_nvmf_poll_group_mod_done cb_fn, void *cb_arg)
 {
-	return 0;
 }
 
 static void
