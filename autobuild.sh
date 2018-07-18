5faf35bedf868013b1d2ea936fde4342060c4c5b
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2018-07-18 07:44:23 +0200

    dpdk: update submodule to spdk-18.05 branch
    Make DPDK 18.05 the default version bundled with SPDK.
    Only legacy memory allocation mode is supported so far.
    
    Change-Id: Ie7ae20f47fce1ce7300b2b2b64b64368af2c14cb
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -73,7 +73,12 @@ timing_exit "$make_timing_label"
 
 # Check for generated files that are not listed in .gitignore
 timing_enter generated_files_check
-if [ `git status --porcelain | wc -l` -ne 0 ]; then
+cd dpdk
+git status --porcelain
+cd app/test-bbdev
+ls -l
+cd ../../..
+if [ `git status --porcelain --ignore-submodules | wc -l` -ne 0 ]; then
 	echo "Generated files missing from .gitignore:"
 	git status --porcelain
 	exit 1
