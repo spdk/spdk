854b71c7cbfedfd2d3452fd1f662f498f7602a7e
Seth Howell <seth.howell@intel.com>
2018-07-20 16:18:30 -0700

    nvmf/rdma: drain queue before deletion.
    Previously, we simply deleted the queue pair without clearing all of the
    pending requests. This could lead to undefined behavior in the deletion
    path, such as receiving completions on the completion queue for a wr on
    a deleted queue. By tracing all of the completions through the
    different states, we can avoid accessing a qpair after it has been
    deleted.
    
    Change-Id: I4935627944bb8c10a1dc7ff0ac1301260491c61b
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvmf/nvmf.c
+++ lib/nvmf/nvmf.c
@@ -693,8 +693,8 @@ _spdk_nvmf_ctrlr_free_from_qpair(void *ctx)
 	free(qpair_ctx);
 }
 
-static void
-_spdk_nvmf_qpair_destroy(void *ctx, int status)
+void
+spdk_nvmf_qpair_destroy_cb(void *ctx)
 {
 	struct nvmf_qpair_disconnect_ctx *qpair_ctx = ctx;
 	struct spdk_nvmf_qpair *qpair = qpair_ctx->qpair;
@@ -703,11 +703,6 @@ _spdk_nvmf_qpair_destroy(void *ctx, int status)
 	uint32_t count;
 
 	spdk_nvmf_poll_group_remove(qpair->group, qpair);
-
-	assert(qpair->state == SPDK_NVMF_QPAIR_DEACTIVATING);
-	qpair->state = SPDK_NVMF_QPAIR_INACTIVE;
-
-	spdk_nvmf_transport_qpair_fini(qpair);
 
 	if (!ctrlr) {
 		if (qpair_ctx->cb_fn) {
@@ -736,6 +731,18 @@ _spdk_nvmf_qpair_destroy(void *ctx, int status)
 }
 
 static void
+_spdk_nvmf_qpair_destroy(void *ctx, int status)
+{
+	struct nvmf_qpair_disconnect_ctx *qpair_ctx = ctx;
+	struct spdk_nvmf_qpair *qpair = qpair_ctx->qpair;
+
+	assert(qpair->state == SPDK_NVMF_QPAIR_DEACTIVATING);
+	qpair->state = SPDK_NVMF_QPAIR_INACTIVE;
+
+	spdk_nvmf_transport_qpair_fini(qpair, qpair_ctx);
+}
+
+static void
 _spdk_nvmf_qpair_deactivate(void *ctx)
 {
 	struct nvmf_qpair_disconnect_ctx *qpair_ctx = ctx;
@@ -754,7 +761,6 @@ _spdk_nvmf_qpair_deactivate(void *ctx)
 	}
 
 	assert(qpair->state == SPDK_NVMF_QPAIR_ACTIVE);
-	qpair->state = SPDK_NVMF_QPAIR_DEACTIVATING;
 
 	/* Check for outstanding I/O */
 	if (!TAILQ_EMPTY(&qpair->outstanding)) {
@@ -763,6 +769,8 @@ _spdk_nvmf_qpair_deactivate(void *ctx)
 		spdk_nvmf_qpair_free_aer(qpair);
 		return;
 	}
+
+	qpair->state = SPDK_NVMF_QPAIR_DEACTIVATING;
 
 	_spdk_nvmf_qpair_destroy(qpair_ctx, 0);
 }
@@ -780,7 +788,7 @@ spdk_nvmf_qpair_disconnect(struct spdk_nvmf_qpair *qpair, nvmf_qpair_disconnect_
 	/* If we get a qpair in the uninitialized state, we can just destroy it immediately */
 	if (qpair->state == SPDK_NVMF_QPAIR_UNINITIALIZED) {
 		free(qpair_ctx);
-		spdk_nvmf_transport_qpair_fini(qpair);
+		spdk_nvmf_transport_qpair_fini(qpair, NULL);
 		if (cb_fn) {
 			cb_fn(ctx);
 		}
