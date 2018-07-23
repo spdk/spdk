d747a00bc18247d9b2ebe88f2c76cfdb5af467a5
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-07-23 13:49:13 +0900

    bdev/raid: Use "stripe" for variables related with stripe
    Change-Id: I2c77f6e6e27976193dee2c39e4ee6a54f2b2c2dd
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -989,12 +989,12 @@ if __name__ == "__main__":
 
         rpc.bdev.construct_raid_bdev(args.client,
                                      name=args.name,
-                                     strip_size=args.strip_size,
+                                     stripe_size=args.stripe_size,
                                      raid_level=args.raid_level,
                                      base_bdevs=base_bdevs)
     p = subparsers.add_parser('construct_raid_bdev', help='Construct new raid bdev')
     p.add_argument('-n', '--name', help='raid bdev name', required=True)
-    p.add_argument('-s', '--strip-size', help='strip size in KB', type=int, required=True)
+    p.add_argument('-s', '--stripe-size', help='stripe size in KB', type=int, required=True)
     p.add_argument('-r', '--raid-level', help='raid level, only raid level 0 is supported', type=int, required=True)
     p.add_argument('-b', '--base-bdevs', help='base bdevs name, whitespace separated list in quotes', required=True)
     p.set_defaults(func=construct_raid_bdev)
