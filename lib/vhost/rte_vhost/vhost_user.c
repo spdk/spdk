95332d3674fc28dd3ec0465d99a29d59f8006a23
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-07-25 20:09:03 +0200

    vhost: clear desc, avail and used addresses on setting new mem table
    This fixes queue handling for QEMU 2.12 which is sending new memory
    table but resetting only some queues
    
    Fixes #339
    
    Change-Id: Ic971725261720d7459e49a4f14bc15c2f2a77b1a
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- lib/vhost/rte_vhost/vhost_user.c
+++ lib/vhost/rte_vhost/vhost_user.c
@@ -584,6 +584,7 @@ vhost_setup_mem_table(struct virtio_net *dev)
 {
 	struct VhostUserMemory memory = dev->mem_table;
 	struct rte_vhost_mem_region *reg;
+	struct vhost_virtqueue *vq;
 	void *mmap_addr;
 	uint64_t mmap_size;
 	uint64_t mmap_offset;
@@ -595,6 +596,17 @@ vhost_setup_mem_table(struct virtio_net *dev)
 		free_mem_region(dev);
 		rte_free(dev->mem);
 		dev->mem = NULL;
+	}
+
+	for (i = 0; i < dev->nr_vring; i++) {
+		vq = dev->virtqueue[i];
+		/* Those addresses won't be valid anymore in host address space
+		 * after setting new mem table. Initiator need to resend these
+		 * addresses.
+		 */
+		vq->desc = NULL;
+		vq->avail = NULL;
+		vq->used = NULL;
 	}
 
 	dev->nr_guest_pages = 0;
