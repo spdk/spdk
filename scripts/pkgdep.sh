37c8f82378666b0c06d1eb6639b9066d118dfed7
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-25 21:32:05 -0400

    scripts: add config converter
    Add a new script to enable converting
    old INI config files to the new JSON-RPC
    config file format.
    This prepares for deprecating the INI
    config file file format in the future.
    
    Change-Id: I0f2bfa9a585ce3537772b662d8e4028ab08a554d
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- scripts/pkgdep.sh
+++ scripts/pkgdep.sh
@@ -39,6 +39,8 @@ if [ -s /etc/redhat-release ]; then
 	yum install -y libpmemblk-devel || true
 	# Additional dependencies for SPDK CLI
 	yum install -y python-configshell python-pexpect python3-configshell python3-pexpect
+	# Additional dependencies for config converter
+	yum install -y python-configparser
 elif [ -f /etc/debian_version ]; then
 	# Includes Ubuntu, Debian
 	apt-get install -y gcc g++ make libcunit1-dev libaio-dev libssl-dev \
@@ -55,6 +57,8 @@ elif [ -f /etc/debian_version ]; then
 	apt-get install -y python-pip python3-pip
 	pip install configshell_fb pexpect
 	pip3 install configshell_fb pexpect
+	# Additional dependencies for config converter
+	apt-get install python-configparser
 elif [ -f /etc/SuSE-release ]; then
 	zypper install -y gcc gcc-c++ make cunit-devel libaio-devel libopenssl-devel \
 		git-core lcov python-base python-pep8 libuuid-devel sg3_utils pciutils
