a761704764d40b97fe8429d68d3cbffa7292ecaa
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-07-25 21:32:05 -0400

    test/json: Config converter
    Change-Id: I0f2bfa9a585ce3537772b662d8e4028ab08a554d
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- scripts/pkgdep.sh
+++ scripts/pkgdep.sh
@@ -23,7 +23,8 @@ if [ -s /etc/redhat-release ]; then
 	# Additional dependencies for building pmem based backends
 	yum install -y libpmemblk-devel || true
 	# Additional dependencies for SPDK CLI
-	yum install -y python-configshell python-pexpect python3-configshell python3-pexpect
+	yum install -y python-configshell python-pexpect python3-configshell python3-pexpect\
+		python-configparser python3-configparser
 elif [ -f /etc/debian_version ]; then
 	# Includes Ubuntu, Debian
 	apt-get install -y gcc g++ make libcunit1-dev libaio-dev libssl-dev \
@@ -38,8 +39,8 @@ elif [ -f /etc/debian_version ]; then
 	apt-get install -y doxygen mscgen graphviz
 	# Additional dependencies for SPDK CLI
 	apt-get install -y python-pip python3-pip
-	pip install configshell_fb pexpect
-	pip3 install configshell_fb pexpect
+	pip install configshell_fb pexpect configparser
+	pip3 install configshell_fb pexpect configparser
 elif [ -f /etc/SuSE-release ]; then
 	zypper install -y gcc gcc-c++ make cunit-devel libaio-devel libopenssl-devel \
 		git-core lcov python-base python-pep8 libuuid-devel sg3_utils
