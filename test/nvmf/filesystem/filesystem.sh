567261bc4725e537714e52dc6a79c80192bc40d3
Changpeng Liu <changpeng.liu@intel.com>
2018-07-26 01:34:52 -0400

    nvmf: claim each bdev when constructing new Namespace
    Claim the block device when adding it to a new Namespace,
    and prevent the block device to be added twice for other
    modules and Namespaces.  Also remove the test that using
    same block device over different Namespaces.
    
    Fix issue #371.
    
    Change-Id: Ib7ce18e9fde4a15c0f19ce9e28e69145e54570e0
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- test/nvmf/filesystem/filesystem.sh
+++ test/nvmf/filesystem/filesystem.sh
@@ -32,8 +32,8 @@ for incapsule in 0 4096; do
 	$rpc_py set_nvmf_target_options -u 8192 -p 4 -c $incapsule
 	$rpc_py start_subsystem_init
 
-	bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
-	bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
+	bdevs="$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
+	bdevs+=" $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
 
 	modprobe -v nvme-rdma
 
