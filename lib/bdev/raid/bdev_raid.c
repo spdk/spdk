32821ba6339ec57ba90b0c5a5a982321a03038a2
Jim Harris <james.r.harris@intel.com>
2018-07-31 13:09:19 -0700

    bdev/raid: release claim and close descriptors during destruct
    During system shutdown, we want each module to release its
    claim and close any open descriptors when one of its bdevs
    is unregistered.
    
    This prepares for a future patch which will unregister
    bdevs at shutdown in reverse order - i.e the RAID bdev
    will be unregistered before its member disks.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Ib4c2b9d25463e420dbc4ec5d3d7030e3d6e8d981

--- lib/bdev/raid/bdev_raid.c
+++ lib/bdev/raid/bdev_raid.c
@@ -215,8 +215,7 @@ raid_bdev_destruct(void *ctxt)
 		 * Close all base bdev descriptors for which call has come from below
 		 * layers
 		 */
-		if ((raid_bdev->base_bdev_info[i].base_bdev_remove_scheduled == true) &&
-		    (raid_bdev->base_bdev_info[i].base_bdev != NULL)) {
+		if (raid_bdev->base_bdev_info[i].base_bdev != NULL) {
 			spdk_bdev_module_release_bdev(raid_bdev->base_bdev_info[i].base_bdev);
 			spdk_bdev_close(raid_bdev->base_bdev_info[i].base_bdev_desc);
 			raid_bdev->base_bdev_info[i].base_bdev_desc = NULL;
@@ -226,12 +225,20 @@ raid_bdev_destruct(void *ctxt)
 		}
 	}
 
-	if (raid_bdev->num_base_bdevs_discovered == 0) {
-		/* Free raid_bdev when there no base bdevs left */
-		SPDK_DEBUGLOG(SPDK_LOG_BDEV_RAID, "raid bdev base bdevs is 0, going to free all in destruct\n");
-		raid_bdev_cleanup(raid_bdev_ctxt);
+	assert(raid_bdev->num_base_bdevs_discovered == 0);
+	if (raid_bdev->state == RAID_BDEV_STATE_ONLINE) {
+		TAILQ_REMOVE(&g_spdk_raid_bdev_configured_list, raid_bdev, link_specific_list);
+		raid_bdev->state = RAID_BDEV_STATE_OFFLINE;
+		TAILQ_INSERT_TAIL(&g_spdk_raid_bdev_offline_list, raid_bdev, link_specific_list);
+	} else if (raid_bdev->state == RAID_BDEV_STATE_CONFIGURING) {
+		TAILQ_REMOVE(&g_spdk_raid_bdev_configured_list, raid_bdev, link_specific_list);
+		raid_bdev->state = RAID_BDEV_STATE_OFFLINE;
+		TAILQ_INSERT_TAIL(&g_spdk_raid_bdev_offline_list, raid_bdev, link_specific_list);
+	} else {
+		assert(raid_bdev->state == RAID_BDEV_STATE_OFFLINE);
 	}
 
+	raid_bdev_cleanup(raid_bdev_ctxt);
 	return 0;
 }
 
