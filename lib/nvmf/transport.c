2adeb3578149f02ffa5ae8a164eb55728b510877
Ziye Yang <optimistyzy@gmail.com>
2018-08-02 10:21:45 +0800

    nvme: add the NVMe over fabrics TCP/IP transport support
    It is the first patch to follow the NVMe over fabrics
    spec and implmenent the TCP/IP transport. It can be
    divided into work in the host and target sides:
    
    Host side: add the TCP/IP transport in nvme lib (lib/nvme).
    Target side: add the TCP/IP transport in nvmf lib (lib/nvmf)
    
    Change-Id: Idc4f93750df676354f6c2ea8ecdb234e3638fd44
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/nvmf/transport.c
+++ lib/nvmf/transport.c
@@ -46,6 +46,7 @@ static const struct spdk_nvmf_transport_ops *const g_transport_ops[] = {
 #ifdef SPDK_CONFIG_RDMA
 	&spdk_nvmf_transport_rdma,
 #endif
+	&spdk_nvmf_transport_tcp,
 };
 
 #define NUM_TRANSPORTS (SPDK_COUNTOF(g_transport_ops))
@@ -233,4 +234,14 @@ spdk_nvmf_transport_opts_init(enum spdk_nvme_transport_type type,
 
 	ops->opts_init(opts);
 	return true;
+}
+
+int
+spdk_nvmf_transport_qpair_set_sqsize(struct spdk_nvmf_qpair *qpair)
+{
+	if (qpair->transport->ops->qpair_set_sqsize) {
+		return qpair->transport->ops->qpair_set_sqsize(qpair);
+	}
+
+	return 0;
 }
