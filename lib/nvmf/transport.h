e8ab15931d010459183c4e70011b440545c8082d
Ziye Yang <optimistyzy@gmail.com>
2018-08-02 10:21:45 +0800

    nvme: Add the NVMe over fabrics TCP/IP transport support
    It is the first patch to follow the NVMe over fabrics
    spec and implmenent the TCP/IP transport. It can be
    divided into work in the host and target sides:
    
    Host side: add the TCP/IP transport in nvme lib (lib/nvme).
    Target side: add the TCP/IP transport in nvmf lib (lib/nvmf)
    
    Change-Id: Idc4f93750df676354f6c2ea8ecdb234e3638fd44
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/nvmf/transport.h
+++ lib/nvmf/transport.h
@@ -153,6 +153,11 @@ struct spdk_nvmf_transport_ops {
 	 */
 	int (*qpair_get_listen_trid)(struct spdk_nvmf_qpair *qpair,
 				     struct spdk_nvme_transport_id *trid);
+
+	/*
+	 * set the submission queue size of the queue pair
+	 */
+	int (*qpair_set_sqsize)(struct spdk_nvmf_qpair *qpair);
 };
 
 
@@ -191,10 +196,12 @@ int spdk_nvmf_transport_qpair_get_local_trid(struct spdk_nvmf_qpair *qpair,
 
 int spdk_nvmf_transport_qpair_get_listen_trid(struct spdk_nvmf_qpair *qpair,
 		struct spdk_nvme_transport_id *trid);
+int spdk_nvmf_transport_qpair_set_sqsize(struct spdk_nvmf_qpair *qpair);
 
 bool spdk_nvmf_transport_opts_init(enum spdk_nvme_transport_type type,
 				   struct spdk_nvmf_transport_opts *opts);
 
 extern const struct spdk_nvmf_transport_ops spdk_nvmf_transport_rdma;
+extern const struct spdk_nvmf_transport_ops spdk_nvmf_transport_tcp;
 
 #endif /* SPDK_NVMF_TRANSPORT_H */
