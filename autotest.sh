fbc5ab382954fd02246bc74f0a535e0956bbf752
Karol Latecki <karol.latecki@intel.com>
2018-08-03 14:06:13 +0200

    autotest: move vhost test script execution to separate file
    Keep run_test inside a designated file instead of
    calling them directly in autotest.sh
    In similar way how nvmf and iscsi scripts are composed.
    
    Change-Id: I19f70678a85b5764d34808f8f7a9e735556474f9
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -165,68 +165,8 @@ if [ $SPDK_TEST_NVMF -eq 1 ]; then
 fi
 
 if [ $SPDK_TEST_VHOST -eq 1 ]; then
-	timing_enter vhost
-	timing_enter negative
-	run_test suite ./test/vhost/spdk_vhost.sh --negative
-	timing_exit negative
-
-	timing_enter vhost_json_config
-	run_test suite ./test/vhost/json_config/json_config.sh
-	timing_exit vhost_json_config
-
-	timing_enter vhost_boot
-	run_test suite ./test/vhost/spdk_vhost.sh --boot
-	timing_exit vhost_boot
-
-	if [ $RUN_NIGHTLY -eq 1 ]; then
-		timing_enter integrity_blk
-		run_test suite ./test/vhost/spdk_vhost.sh --integrity-blk
-		timing_exit integrity_blk
-
-		timing_enter integrity
-		run_test suite ./test/vhost/spdk_vhost.sh --integrity
-		timing_exit integrity
-
-		timing_enter fs_integrity_scsi
-		run_test suite ./test/vhost/spdk_vhost.sh --fs-integrity-scsi
-		timing_exit fs_integrity_scsi
-
-		timing_enter fs_integrity_blk
-		run_test suite ./test/vhost/spdk_vhost.sh --fs-integrity-blk
-		timing_exit fs_integrity_blk
-
-		timing_enter integrity_lvol_scsi_nightly
-		run_test suite ./test/vhost/spdk_vhost.sh --integrity-lvol-scsi-nightly
-		timing_exit integrity_lvol_scsi_nightly
-
-		timing_enter integrity_lvol_blk_nightly
-		run_test suite ./test/vhost/spdk_vhost.sh --integrity-lvol-blk-nightly
-		timing_exit integrity_lvol_blk_nightly
-
-		# timing_enter readonly
-		# run_test suite ./test/vhost/spdk_vhost.sh --readonly
-		# timing_exit readonly
-	fi
-
-	if [ $RUN_NIGHTLY_FAILING -eq 1 ]; then
-		timing_enter vhost_migration
-		run_test suite ./test/vhost/spdk_vhost.sh --migration
-		timing_exit vhost_migration
-	fi
-
-	timing_enter integrity_lvol_scsi
-	run_test suite ./test/vhost/spdk_vhost.sh --integrity-lvol-scsi
-	timing_exit integrity_lvol_scsi
-
-	timing_enter integrity_lvol_blk
-	run_test suite ./test/vhost/spdk_vhost.sh --integrity-lvol-blk
-	timing_exit integrity_lvol_blk
-
-	timing_enter spdk_cli
-	run_test suite ./test/spdkcli/vhost.sh
-	timing_exit spdk_cli
-
-	timing_exit vhost
+	run_test suite ./test/vhost/vhost.sh
+	report_test_completion "vhost"
 fi
 
 if [ $SPDK_TEST_LVOL -eq 1 ]; then
