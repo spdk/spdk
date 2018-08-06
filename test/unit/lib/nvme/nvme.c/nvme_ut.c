e3b996eb36455b7a3b1bd152545de2f4f968944a
zkhatami88 <z.khatami88@gmail.com>
2018-08-06 16:27:13 -0700

    nvme: Add mechanism to override RDMA pd/mr behavior
    Add a mechanism to modify the RDMA transport's behavior
    when creating protection domains and registering memory.
    This is entirely optional.
    
    Change-Id: I7cd850e76a673bf5521ca4815b779c53ab9567e8
    Signed-off-by: zkhatami88 <z.khatami88@gmail.com>

--- test/unit/lib/nvme/nvme.c/nvme_ut.c
+++ test/unit/lib/nvme/nvme.c/nvme_ut.c
@@ -84,6 +84,9 @@ DEFINE_STUB(spdk_nvme_qpair_process_completions, int32_t,
 	    (struct spdk_nvme_qpair *qpair,
 	     uint32_t max_completions), 0);
 
+DEFINE_STUB_V(nvme_transport_init_hooks,
+	      (enum spdk_nvme_transport_type trtype, struct spdk_nvme_hooks *hooks));
+
 static bool ut_destruct_called = false;
 void
 nvme_ctrlr_destruct(struct spdk_nvme_ctrlr *ctrlr)
