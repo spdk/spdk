75408adc3db70285a7ab308c7c50af2ff048b006
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2018-08-09 03:06:27 -0400

    test/nvmf: add bdev_io_wait tests
    Those tests check behaviour of nvmf layer when there is no remaining
    bdev_io buffers in pool.
    
    Change-Id: Ic10534108b9d71c00d534407348f1672a74926c4
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-on: https://review.gerrithub.io/421696
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Chandler-Test-Pool: SPDK Automated Test System <sys_sgsw@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/nvmf/nvmf.sh
+++ test/nvmf/nvmf.sh
@@ -30,6 +30,7 @@ if [ $SPDK_TEST_NVME_CLI -eq 1 ]; then
 fi
 run_test suite test/nvmf/lvol/nvmf_lvol.sh
 run_test suite test/nvmf/shutdown/shutdown.sh
+run_test suite test/nvmf/bdev_io_wait/bdev_io_wait.sh
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	run_test suite test/nvmf/multiconnection/multiconnection.sh
