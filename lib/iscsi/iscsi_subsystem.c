ac5010e212bb0c31bfec1b70e702255c6dbba19b
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-08-09 14:11:25 +0900

    iscsi: Add CHAP authentication group configuration to JSON config dump
    Adding CHAP authentication group configuration to JSON config dump.
    This addition is done unconditionally because the path to the CHAP secret file
    have to be specified explicitly now.
    
    Change-Id: Ibd4085222a6767dae3a74023f1d6bc270ef4abac
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/iscsi/iscsi_subsystem.c
+++ lib/iscsi/iscsi_subsystem.c
@@ -1422,6 +1422,20 @@ spdk_iscsi_auth_group_info_json(struct spdk_iscsi_auth_group *group,
 	spdk_json_write_object_end(w);
 }
 
+static void
+spdk_iscsi_auth_group_config_json(struct spdk_iscsi_auth_group *group,
+				  struct spdk_json_write_ctx *w)
+{
+	spdk_json_write_object_begin(w);
+
+	spdk_json_write_named_string(w, "method", "add_iscsi_auth_group");
+
+	spdk_json_write_name(w, "params");
+	spdk_iscsi_auth_group_info_json(group, w);
+
+	spdk_json_write_object_end(w);
+}
+
 void
 spdk_iscsi_auth_groups_info_json(struct spdk_json_write_ctx *w)
 {
@@ -1429,6 +1443,16 @@ spdk_iscsi_auth_groups_info_json(struct spdk_json_write_ctx *w)
 
 	TAILQ_FOREACH(group, &g_spdk_iscsi.auth_group_head, tailq) {
 		spdk_iscsi_auth_group_info_json(group, w);
+	}
+}
+
+static void
+spdk_iscsi_auth_groups_config_json(struct spdk_json_write_ctx *w)
+{
+	struct spdk_iscsi_auth_group *group;
+
+	TAILQ_FOREACH(group, &g_spdk_iscsi.auth_group_head, tailq) {
+		spdk_iscsi_auth_group_config_json(group, w);
 	}
 }
 
@@ -1453,6 +1477,7 @@ spdk_iscsi_config_json(struct spdk_json_write_ctx *w)
 	spdk_iscsi_portal_grps_config_json(w);
 	spdk_iscsi_init_grps_config_json(w);
 	spdk_iscsi_tgt_nodes_config_json(w);
+	spdk_iscsi_auth_groups_config_json(w);
 	spdk_json_write_array_end(w);
 }
 
