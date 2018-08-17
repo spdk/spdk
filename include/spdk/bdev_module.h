9f2dd0c4f80f28969c5768221193859b2218b828
Jim Harris <james.r.harris@intel.com>
2018-08-17 15:06:26 -0700

    bdev: save the bdev_desc specified when submitting the I/O
    This will be needed for using this same descriptor when
    splitting an I/O.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Idec759df7ab27f8de567d3c8a4214e25dbe173f5
    
    Reviewed-on: https://review.gerrithub.io/423022
    Chandler-Test-Pool: SPDK Automated Test System <sys_sgsw@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- include/spdk/bdev_module.h
+++ include/spdk/bdev_module.h
@@ -401,6 +401,9 @@ struct spdk_bdev_io {
 		/** The bdev I/O channel that this was submitted on. */
 		struct spdk_bdev_channel *io_submit_ch;
 
+		/** The bdev descriptor that was used when submitting this I/O. */
+		struct spdk_bdev_desc *desc;
+
 		/** User function that will be called when this completes */
 		spdk_bdev_io_completion_cb cb;
 
