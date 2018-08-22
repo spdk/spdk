e213032c50b8b6ad0fe2643e37359f17d79246e0
Chen Wang <chenx.wang@intel.com>
2018-08-22 13:44:46 +0800

    test/iscsi: add fio and lvol test cases with raid bdev
    1. Use the Raid-0 bdev which based two malloc bdevs to instead of
       one malloc blockbdev.
    2. Create the lvol store with Raid-0 bdev which based two malloc bdevs
       when the number of Malloc bdevs is equal to 1.
    
    Change-Id: I8b584ad09011386b405e4756c04273834b24f029
    Signed-off-by: Chen Wang <chenx.wang@intel.com>

--- test/iscsi_tgt/lvol/iscsi_lvol.sh
+++ test/iscsi_tgt/lvol/iscsi_lvol.sh
@@ -10,10 +10,10 @@ timing_enter iscsi_lvol
 MALLOC_BDEV_SIZE=128
 MALLOC_BLOCK_SIZE=512
 if [ $RUN_NIGHTLY -eq 1 ]; then
-	NUM_MALLOC=10
+	NUM_LVS=10
 	NUM_LVOL=10
 else
-	NUM_MALLOC=2
+	NUM_LVS=2
 	NUM_LVOL=2
 fi
 
@@ -37,10 +37,21 @@ timing_exit start_iscsi_tgt
 
 timing_enter setup
 $rpc_py add_portal_group $PORTAL_TAG $TARGET_IP:$ISCSI_PORT
-for i in $(seq 1 $NUM_MALLOC); do
+#Create the first LVS from a Raid-0 bdev, which is created from two malloc bdevs
+#Create remaining LVSs from a malloc bdev, respectively
+for i in $(seq 1 $NUM_LVS); do
 	INITIATOR_TAG=$((i + 2))
 	$rpc_py add_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
-	bdev=$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)
+	if [ $i -eq 1 ]; then
+		# construct RAID bdev and put its name in $bdev
+		malloc_bdevs="$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE) "
+		malloc_bdevs+="$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
+		$rpc_py construct_raid_bdev -n raid0 -s 64 -r 0 -b "$malloc_bdevs"
+		bdev="raid0"
+	else
+		# construct malloc bdev and put its name in $bdev
+		bdev=$($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)
+	fi
 	ls_guid=$($rpc_py construct_lvol_store $bdev lvs_$i -c 1048576)
 	LUNs=""
 	for j in $(seq 1 $NUM_LVOL); do
