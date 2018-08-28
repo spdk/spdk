84f5788674eba271e887db28a19c026d4de3d790
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-08-28 21:58:23 +0200

    test/nvme: Test case 2 of NVME performance tests
    Nvme I/O cores scaling
    
    Change-Id: Ibb5131ea8701537e8e85cc2d97dcb325845197e3
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/nvme/perf/common.sh
+++ test/nvme/perf/common.sh
@@ -6,6 +6,22 @@ PLUGIN_DIR_BDEV=$ROOTDIR/examples/bdev/fio_plugin
 . $ROOTDIR/scripts/common.sh || exit 1
 . $ROOTDIR/test/common/autotest_common.sh
 NVME_FIO_RESULTS=$BASE_DIR/result.json
+BASE_DIR=$(readlink -f $(dirname $0))
+ROOTDIR=$(readlink -f $BASE_DIR/../../..)
+
+PRECONDITIONING=true
+FIO_BIN="/usr/src/fio/fio"
+RUNTIME=600
+PLUGIN="nvme"
+RAMP_TIME=30
+BLK_SIZE=4096
+RW=randrw
+MIX=(100 70 0)
+TYPE=("randread" "randrw" "randwrite")
+IODEPTH=(256 256 32)
+DISKNO=1
+CPU_MASKS=(0x02 0x06 0x1E 0x7E 0x1FE 0x7FE)
+CPUs=(1 2 4 6 8 10)
 
 function get_disks(){
 	if [ "$1" = "nvme" ]; then
@@ -16,7 +32,7 @@ function get_disks(){
 			fi
 		done
 	elif [ "$1" = "bdev" ]; then
-		bdevs=$(discover_bdevs $ROOTDIR $BASE_DIR/bdev.conf)
+		local bdevs=$(discover_bdevs $ROOTDIR $BASE_DIR/bdev.conf)
 		echo $(jq -r '.[].name' <<< $bdevs)
 	fi
 }
@@ -116,6 +132,42 @@ function run_spdk_nvme_fio(){
 	sleep 1
 }
 
+function usage()
+{
+	set +x
+	[[ ! -z $2 ]] && ( echo "$2"; echo ""; )
+	echo "Run Nvme PMD/BDEV performance test. Change options for easier debug and setup configuration"
+	echo "Usage: $(basename $1) [options]"
+	echo "-h, --help                Print help and exit"
+	echo "    --run-time=TIME[s]    Tell fio to terminate processing after the specified period of time. [default=$RUNTIME]"
+	echo "    --ramp-time=TIME[s]   Fio will run the specified workload for this amount of time before logging any performance numbers. [default=$RAMP_TIME]"
+	echo "    --fio-bin=PATH        Path to fio binary. [default=$FIO_BIN]"
+	echo "    --fio-plugin=STR      Use bdev or nvme fio_plugin. [default=$PLUGIN]"
+	echo "    --disk-no=INT,ALL     Number of disks to test on, if =ALL then test on all found disk. [default=$DISKNO]"
+	echo "    --no-preconditioning  Skip preconditioning"
+	set -x
+}
+
+while getopts 'h-:' optchar; do
+	case "$optchar" in
+		-)
+		case "$OPTARG" in
+			help) usage $0; exit 0 ;;
+			run-time=*) RUNTIME="${OPTARG#*=}" ;;
+			ramp-time=*) RAMP_TIME="${OPTARG#*=}" ;;
+			fio-bin=*) FIO_BIN="${OPTARG#*=}" ;;
+			disk-no=*) DISKNO="${OPTARG#*=}" ;;
+			fio-plugin=*) PLUGIN="${OPTARG#*=}" ;;
+			no-preconditioning) PRECONDITIONING=false ;;
+			*) usage $0 echo "Invalid argument '$OPTARG'"; exit 1 ;;
+		esac
+		;;
+		h) usage $0; exit 0 ;;
+		*) usage $0 "Invalid argument '$optchar'"; exit 1 ;;
+	esac
+done
+
+trap 'rm -f *.state $NVME_FIO_RESULTS $BASE_DIR/bdev.conf; print_backtrace' ERR SIGTERM SIGABRT
 mkdir -p $BASE_DIR/results
 date="$(date +'%m_%d_%Y_%H%M%S')"
 disks=($(get_disks nvme))
--- test/nvme/perf/perf_tc1.sh
+++ test/nvme/perf/perf_tc1.sh
@@ -3,59 +3,10 @@
 BASE_DIR=$(readlink -f $(dirname $0))
 ROOTDIR=$(readlink -f $BASE_DIR/../../..)
 
-PRECONDITIONING=true
-FIO_BIN="/usr/src/fio/fio"
-RUNTIME=600
-PLUGIN="nvme"
-RAMP_TIME=30
-BLK_SIZE=4096
-RW=randrw
-MIX=(100 70 0)
-TYPE=("randread" "randrw" "randwrite")
-IODEPTH=(256 256 32)
-CPUMASK=0x02
-DISKNO=1
-
-function usage()
-{
-	set +x
-	[[ ! -z $2 ]] && ( echo "$2"; echo ""; )
-	echo "Run Nvme PMD/BDEV performance test. Change options for easier debug and setup configuration"
-	echo "Usage: $(basename $1) [options]"
-	echo "-h, --help                Print help and exit"
-	echo "    --run-time=TIME[s]    Tell fio to terminate processing after the specified period of time. [default=$RUNTIME]"
-	echo "    --ramp-time=TIME[s]   Fio will run the specified workload for this amount of time before logging any performance numbers. [default=$RAMP_TIME]"
-	echo "    --fio-bin=PATH        Path to fio binary. [default=$FIO_BIN]"
-	echo "    --fio-plugin=STR      Use bdev or nvme fio_plugin. [default=$PLUGIN]"
-	echo "    --disk-no=INT,ALL     Number of disks to test on, if =ALL then test on all found disk. [default=$DISKNO]"
-	echo "    --no-preconditioning  Skip preconditioning"
-	set -x
-}
-
-while getopts 'h-:' optchar; do
-	case "$optchar" in
-		-)
-		case "$OPTARG" in
-			help) usage $0; exit 0 ;;
-			run-time=*) RUNTIME="${OPTARG#*=}" ;;
-			ramp-time=*) RAMP_TIME="${OPTARG#*=}" ;;
-			fio-bin=*) FIO_BIN="${OPTARG#*=}" ;;
-			disk-no=*) DISKNO="${OPTARG#*=}" ;;
-			fio-plugin=*) PLUGIN="${OPTARG#*=}" ;;
-			no-preconditioning) PRECONDITIONING=false ;;
-			*) usage $0 echo "Invalid argument '$OPTARG'"; exit 1 ;;
-		esac
-		;;
-		h) usage $0; exit 0 ;;
-		*) usage $0 "Invalid argument '$optchar'"; exit 1 ;;
-	esac
-done
-
-trap "rm -f *.state $NVME_FIO_RESULTS $BASE_DIR/bdev.conf; print_backtrace" ERR SIGTERM SIGABRT
 . $BASE_DIR/common.sh
+CPUMASK=0x02
 
 if [ $PLUGIN = "bdev" ]; then
-	echo "[Nvme]" >> $BASE_DIR/bdev.conf
 	$ROOTDIR/scripts/gen_nvme.sh >> $BASE_DIR/bdev.conf
 fi
 
