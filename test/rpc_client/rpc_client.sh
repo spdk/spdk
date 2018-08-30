b925c87f9784913aae120b66d0ef033814bc7eae
Liu Xiaodong <xiaodong.liu@intel.com>
2018-08-30 07:05:39 -0400

    jsonrpc-client: add new C client library
    It's a C libary for client to call rpc method.
    
    Change-Id: I5378747bd9dab83a41801225ba794b3910d1f5a5
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>

---
+++ test/rpc_client/rpc_client.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+
+source $rootdir/test/common/autotest_common.sh
+
+function rpc_client_test() {
+	local conf=$rootdir/test/bdev/bdev.conf.in
+
+	if [ ! -e $conf ]; then
+		return 1
+	fi
+
+	$rootdir/test/app/bdev_svc/bdev_svc -i 0 -c ${conf} &
+	svc_pid=$!
+	echo "Process bdev_svc pid: $svc_pid"
+	waitforlisten $svc_pid
+
+	$rootdir/test/rpc_client/rpc_client_test
+
+	killprocess $svc_pid
+
+	return 0
+}
+
+timing_enter rpc_client
+rpc_client_test
+timing_exit rpc_client
