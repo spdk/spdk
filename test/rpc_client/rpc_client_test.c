3ec69556415bed6124c960d2076c336deb292888
Liu Xiaodong <xiaodong.liu@intel.com>
2018-08-30 07:05:39 -0400

    jsonrpc-client: add new C client library
    It's a C libary for client to call rpc method.
    
    Change-Id: I5378747bd9dab83a41801225ba794b3910d1f5a5
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>

---
+++ test/rpc_client/rpc_client_test.c
@@ -0,0 +1,61 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+#include "spdk/event.h"
+#include "spdk/rpc_client.h"
+
+const char *rpcsock_addr = SPDK_DEFAULT_RPC_ADDR;
+
+int main(int argc, char **argv)
+{
+	struct spdk_rpc_client_conn *conn;
+	int rc;
+	char *method_name = "get_rpc_methods";
+
+	rc = spdk_rpc_client_check_rpc_method(rpcsock_addr, method_name);
+	if (rc) {
+		return rc;
+	}
+
+	conn = spdk_rpc_client_connect(rpcsock_addr);
+	if (!conn) {
+		return -1;
+	}
+
+	rc = _spdk_rpc_client_check_rpc_method(conn, method_name);
+
+	spdk_rpc_client_close(conn);
+
+	return rc;
+}
