97c2eaacb15f904ea4489b6faba87eaa161c0400
Liu Xiaodong <xiaodong.liu@intel.com>
2018-08-30 07:05:39 -0400

    jsonrpc-client: add new C client library
    It's a C libary for client to call rpc method.
    
    Change-Id: I5378747bd9dab83a41801225ba794b3910d1f5a5
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>

---
+++ include/spdk/jsonrpc_client_cmd.h
@@ -0,0 +1,66 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \file
+ * SPDK JSON-RPC commands
+ */
+
+#ifndef SPDK_JSONRPC_CLIENT_CMD_H_
+#define SPDK_JSONRPC_CLIENT_CMD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct spdk_jsonrpc_client_conn;
+
+struct spdk_jsonrpc_client_conn *spdk_jsonrpc_client_connect(const char *listen_addr);
+void spdk_jsonrpc_client_close(struct spdk_jsonrpc_client_conn *conn);
+
+
+/**
+ * This function can be used to check jsonrpc workability
+ *
+ * \param rpc_sock_addr Connecting address.
+ *
+ * \return 0 on success.
+ */
+int spdk_rpc_client_get_rpc_methods(const char *rpc_sock_addr);
+int _spdk_rpc_client_get_rpc_methods(struct spdk_jsonrpc_client_conn *conn);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
