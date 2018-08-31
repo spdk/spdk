65ec2a4f3a647596605998549f7a68bcbe3a495c
Ben Walker <benjamin.walker@intel.com>
2018-08-31 13:39:11 -0700

    nbd: Encapsulate spdk_event usage in subsystem module
    Events are part of the SPDK event framework and should be
    isolated inside lib/event.
    
    Change-Id: I8e9a5250bcbb9dd891f98df3177a6b5370a65fe3
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/event/subsystems/nbd/nbd.c
+++ lib/event/subsystems/nbd/nbd.c
@@ -54,12 +54,20 @@ spdk_nbd_subsystem_fini(void)
 	spdk_subsystem_fini_next();
 }
 
+static void
+spdk_nbd_subsystem_write_config_json(struct spdk_json_write_ctx *w,
+				     struct spdk_event *done_ev)
+{
+	spdk_nbd_write_config_json(w);
+	spdk_event_call(done_ev);
+}
+
 static struct spdk_subsystem g_spdk_subsystem_nbd = {
 	.name = "nbd",
 	.init = spdk_nbd_subsystem_init,
 	.fini = spdk_nbd_subsystem_fini,
 	.config = NULL,
-	.write_config_json = spdk_nbd_write_config_json,
+	.write_config_json = spdk_nbd_subsystem_write_config_json,
 };
 
 SPDK_SUBSYSTEM_REGISTER(g_spdk_subsystem_nbd);
