052a9695185bc725aa0578c16bd20e45ed718579
Ben Walker <benjamin.walker@intel.com>
2018-08-31 10:58:36 -0700

    thread: Update unit test to use new threading logic
    No longer override the send_msg implementation.
    
    This updates ut_multithread.c, which has minor ripple
    effects into bdev_ut.c. But that unit test is otherwise
    unchanged.
    
    Change-Id: I2fd30a1010bdff0a810d376d985ab1b8a2b22fb9
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/unit/lib/thread/thread.c/thread_ut.c
+++ test/unit/lib/thread/thread.c/thread_ut.c
@@ -36,14 +36,7 @@
 #include "spdk_cunit.h"
 
 #include "thread/thread.c"
-#include "common/lib/test_env.c"
 #include "common/lib/ut_multithread.c"
-
-static void
-_send_msg(spdk_thread_fn fn, void *ctx, void *thread_ctx)
-{
-	fn(ctx);
-}
 
 static void
 thread_alloc(void)
@@ -116,7 +109,7 @@ thread_poller(void)
 	allocate_threads(1);
 
 	set_thread(0);
-	reset_time();
+	MOCK_SET(spdk_get_ticks, 0);
 	/* Register a poller with no-wait time and test execution */
 	poller = spdk_poller_register(poller_run_done, &poller_run, 0);
 	CU_ASSERT(poller != NULL);
@@ -135,16 +128,15 @@ thread_poller(void)
 	poll_threads();
 	CU_ASSERT(poller_run == false);
 
-	increment_time(1000);
+	spdk_delay_us(1000);
 	poll_threads();
 	CU_ASSERT(poller_run == true);
 
-	reset_time();
 	poller_run = false;
 	poll_threads();
 	CU_ASSERT(poller_run == false);
 
-	increment_time(1000);
+	spdk_delay_us(1000);
 	poll_threads();
 	CU_ASSERT(poller_run == true);
 
@@ -246,6 +238,7 @@ for_each_channel_remove(void)
 	 */
 	set_thread(0);
 	spdk_put_io_channel(ch0);
+	poll_threads();
 	spdk_for_each_channel(&io_target, channel_msg, &count, channel_cpl);
 	poll_threads();
 
@@ -350,7 +343,7 @@ thread_name(void)
 	const char *name;
 
 	/* Create thread with no name, which automatically generates one */
-	spdk_allocate_thread(_send_msg, NULL, NULL, NULL, NULL);
+	spdk_allocate_thread(NULL, NULL, NULL, NULL, NULL);
 	thread = spdk_get_thread();
 	SPDK_CU_ASSERT_FATAL(thread != NULL);
 	name = spdk_thread_get_name(thread);
@@ -358,7 +351,7 @@ thread_name(void)
 	spdk_free_thread();
 
 	/* Create thread named "test_thread" */
-	spdk_allocate_thread(_send_msg, NULL, NULL, NULL, "test_thread");
+	spdk_allocate_thread(NULL, NULL, NULL, NULL, "test_thread");
 	thread = spdk_get_thread();
 	SPDK_CU_ASSERT_FATAL(thread != NULL);
 	name = spdk_thread_get_name(thread);
@@ -414,10 +407,12 @@ destroy_cb_2(void *io_device, void *ctx_buf)
 static void
 channel(void)
 {
+	struct spdk_thread *thread;
 	struct spdk_io_channel *ch1, *ch2;
 	void *ctx;
 
-	spdk_allocate_thread(_send_msg, NULL, NULL, NULL, "thread0");
+	thread = spdk_allocate_thread(NULL, NULL, NULL, NULL, "thread0");
+	SPDK_CU_ASSERT_FATAL(thread != NULL);
 	spdk_io_device_register(&device1, create_cb_1, destroy_cb_1, sizeof(ctx1), NULL);
 	spdk_io_device_register(&device2, create_cb_2, destroy_cb_2, sizeof(ctx2), NULL);
 
@@ -434,6 +429,7 @@ channel(void)
 
 	g_destroy_cb_calls = 0;
 	spdk_put_io_channel(ch2);
+	while (spdk_thread_poll(thread, 0) > 0) {}
 	CU_ASSERT(g_destroy_cb_calls == 0);
 
 	g_create_cb_calls = 0;
@@ -447,17 +443,21 @@ channel(void)
 
 	g_destroy_cb_calls = 0;
 	spdk_put_io_channel(ch1);
+	while (spdk_thread_poll(thread, 0) > 0) {}
 	CU_ASSERT(g_destroy_cb_calls == 1);
 
 	g_destroy_cb_calls = 0;
 	spdk_put_io_channel(ch2);
+	while (spdk_thread_poll(thread, 0) > 0) {}
 	CU_ASSERT(g_destroy_cb_calls == 1);
 
 	ch1 = spdk_get_io_channel(&device3);
 	CU_ASSERT(ch1 == NULL);
 
 	spdk_io_device_unregister(&device1, NULL);
+	while (spdk_thread_poll(thread, 0) > 0) {}
 	spdk_io_device_unregister(&device2, NULL);
+	while (spdk_thread_poll(thread, 0) > 0) {}
 	CU_ASSERT(TAILQ_EMPTY(&g_io_devices));
 	spdk_free_thread();
 	CU_ASSERT(TAILQ_EMPTY(&g_threads));
