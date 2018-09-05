e7a13fc6ae3ca7861ec48fd7e572a5bc8be4920d
chenlo2x <longx.o.chen@intel.com>
2018-09-05 10:10:05 +0800

    Autotest: refactor iscsi qos iops case and add new iscsi qos bandwidth cases
    Change-Id: I1041ac282ecdf43784b86782ae06ecac79885e71
    Signed-off-by: chenlo2x <longx.o.chen@intel.com>

--- test/iscsi_tgt/qos/qos.sh
+++ test/iscsi_tgt/qos/qos.sh
@@ -6,29 +6,49 @@ source $rootdir/test/common/autotest_common.sh
 source $rootdir/test/iscsi_tgt/common.sh
 
 function check_qos_works_well() {
-	local enable_limit=$1
-	local iops_limit=$2
-	local retval=0
+	local bdev=$1
+	local lsize=$3
 
-	start_io_count=$($rpc_py get_bdevs_iostat -b $3 | jq -r '.[1].num_read_ops')
-	$fio_py 512 64 randread 5
-	end_io_count=$($rpc_py get_bdevs_iostat -b $3 | jq -r '.[1].num_read_ops')
+	local start_rio_count=$($rpc_py get_bdevs_iostat -b $bdev | jq -r '.[1].num_read_ops')
+	local start_wio_count=$($rpc_py get_bdevs_iostat -b $bdev | jq -r '.[1].num_write_ops')
 
-	read_iops=$(((end_io_count-start_io_count)/5))
-
-	if [ $enable_limit = true ]; then
-		retval=$(echo "$iops_limit*0.9 < $read_iops && $read_iops < $iops_limit*1.01" | bc)
-		if [ $retval -eq 0 ]; then
-			echo "Failed to limit the io read rate of malloc bdev by qos"
-			exit 1
-		fi
-	else
-		retval=$(echo "$read_iops > $iops_limit" | bc)
-		if [ $retval -eq 0 ]; then
-			echo "$read_iops less than $iops_limit - expected greater than"
-			exit 1
-		fi
+	local qos_ope=randwrite
+	if [[ "$2"  =~ r_ios || "$2"  =~ r_mbytes ]]; then
+		qos_ope=randread
+	elif [[ "$2"  =~ rw_ios || "$2"  =~ rw_mbytes ]]; then
+		qos_ope=randrw
 	fi
+	$fio_py 4096 64 $qos_ope 5
+
+	local end_rio_count=$($rpc_py get_bdevs_iostat -b $bdev | jq -r '.[1].num_read_ops')
+	local end_wio_count=$($rpc_py get_bdevs_iostat -b $bdev | jq -r '.[1].num_write_ops')
+
+	local delta_io_count=$(($end_rio_count+$end_wio_count-$start_rio_count-$start_wio_count))
+	local iops=$(($delta_io_count/5))
+	local mbps=$(($iops*4096/1048576))
+
+	local retval qos_type
+	if [[ "$2" =~ ios ]]; then
+		retval=$(echo "$lsize*0.95 < $iops && $iops < $lsize*1.05" | bc)
+		qos_type=iops
+	else
+		retval=$(echo "$lsize*0.95 < $mbps && $mbps < $lsize*1.05" | bc)
+		qos_type=bandwidth
+	fi
+	if [ $retval -eq 0 ]; then
+		echo "Failed to limit the $qos_ope $qos_type of malloc bdev by qos"
+		exit 1
+	fi
+}
+
+function test_qos_case() {
+	local bdev=$1
+	local type=$2
+	local lsize=$3
+
+	$rpc_py set_bdev_qos_limit $bdev "--$type" $lsize
+	check_qos_works_well $bdev $type $lsize
+	$rpc_py set_bdev_qos_limit $bdev "--$type" 0
 }
 
 if [ -z "$TARGET_IP" ]; then
@@ -43,12 +63,11 @@ fi
 
 timing_enter qos
 
+BDEV=Malloc0
 MALLOC_BDEV_SIZE=64
 MALLOC_BLOCK_SIZE=512
-IOPS_LIMIT=20000
 rpc_py="$rootdir/scripts/rpc.py"
 fio_py="$rootdir/scripts/fio.py"
-READ_IOPS_LIMIT=50000
 
 timing_enter start_iscsi_tgt
 
@@ -68,7 +87,7 @@ $rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE
 # "1:2" ==> map PortalGroup1 to InitiatorGroup2
 # "64" ==> iSCSI queue depth 64
 # "-d" ==> disable CHAP authentication
-$rpc_py construct_target_node Target1 Target1_alias 'Malloc0:0' $PORTAL_TAG:$INITIATOR_TAG 64 -d
+$rpc_py construct_target_node Target1 Target1_alias "$BDEV:0" $PORTAL_TAG:$INITIATOR_TAG 64 -d
 sleep 1
 
 iscsiadm -m discovery -t sendtargets -p $TARGET_IP:$ISCSI_PORT
@@ -76,19 +95,18 @@ iscsiadm -m node --login -p $TARGET_IP:$ISCSI_PORT
 
 trap "iscsicleanup; killprocess $pid; exit 1" SIGINT SIGTERM EXIT
 
-# Limit the I/O rate by RPC, then confirm the observed rate matches.
-$rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec $IOPS_LIMIT
-check_qos_works_well true $IOPS_LIMIT Malloc0
+declare -A QOS_PARAM
+QOS_PARAM[rw_ios_per_sec]=20000
+QOS_PARAM[r_ios_per_sec]=20000
+QOS_PARAM[w_ios_per_sec]=20000
+QOS_PARAM[rw_mbytes_per_sec]=50
+QOS_PARAM[r_mbytes_per_sec]=50
+QOS_PARAM[w_mbytes_per_sec]=50
 
-# Now disable the rate limiting, and confirm the observed rate is not limited anymore.
-$rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec 0
-check_qos_works_well false $IOPS_LIMIT Malloc0
-
-# Limit the I/O rate again together against read only I/O.
-$rpc_py set_bdev_qos_limit Malloc0 --rw_ios_per_sec $IOPS_LIMIT
-$rpc_py set_bdev_qos_limit Malloc0 --r_ios_per_sec $READ_IOPS_LIMIT
-check_qos_works_well true $IOPS_LIMIT Malloc0
-echo "I/O rate limiting tests successful"
+for key in ${!QOS_PARAM[@]}; do
+	test_qos_case $BDEV $key ${QOS_PARAM[$key]}
+done
+echo "All the iops & bandwidth limiting tests successful"
 
 iscsicleanup
 $rpc_py delete_target_node 'iqn.2016-06.io.spdk:Target1'
