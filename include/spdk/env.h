205d072db73d0008a53eaea0ec1e8eb7b045f46b
Seth Howell <seth.howell@intel.com>
2018-09-06 13:07:42 -0700

    nvme_rdma: Check mr size when creating a request.
    This is in preparation for enabling multiple SGLs in our requests. If
    an MR is not large enough to handle the request, we will need to split
    the request on MR boundaries.
    
    Change-Id: I8686c166ec956507f5ae55cf602341281482cb89
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- include/spdk/env.h
+++ include/spdk/env.h
@@ -1032,12 +1032,13 @@ int spdk_mem_map_clear_translation(struct spdk_mem_map *map, uint64_t vaddr, uin
  *
  * \param map Memory map.
  * \param vaddr Virtual address.
- * \param size Size of memory region.
+ * \param size Pointer to the desired size of the region.
  *
  * \return the translation of vaddr stored in the map, or default_translation
  * as specified in spdk_mem_map_alloc() if vaddr is not present in the map.
+ * The size of the region described by this virtual address is copied to size.
  */
-uint64_t spdk_mem_map_translate(const struct spdk_mem_map *map, uint64_t vaddr, uint64_t size);
+uint64_t spdk_mem_map_translate(const struct spdk_mem_map *map, uint64_t vaddr, uint64_t *size);
 
 /**
  * Register the specified memory region for address translation.
--- lib/env_dpdk/vtophys.c
+++ lib/env_dpdk/vtophys.c
@@ -339,7 +339,7 @@ spdk_vtophys_notify(void *cb_ctx, struct spdk_mem_map *map,
 		    void *vaddr, size_t len)
 {
 	int rc = 0, pci_phys = 0;
-	uint64_t paddr;
+	uint64_t paddr, buffer_len;
 
 	if ((uintptr_t)vaddr & ~MASK_256TB) {
 		DEBUG_PRINT("invalid usermode virtual address %p\n", vaddr);
@@ -405,7 +405,10 @@ spdk_vtophys_notify(void *cb_ctx, struct spdk_mem_map *map,
 				 * we need to unmap the range from the IOMMU
 				 */
 				if (g_vfio.enabled) {
-					paddr = spdk_mem_map_translate(map, (uint64_t)vaddr, VALUE_2MB);
+					paddr = spdk_mem_map_translate(map, (uint64_t)vaddr, &buffer_len);
+					if (buffer_len < VALUE_2MB) {
+						return -EINVAL;
+					}
 					rc = vtophys_iommu_unmap_dma(paddr, VALUE_2MB);
 					if (rc) {
 						return -EFAULT;
@@ -619,7 +622,7 @@ spdk_vtophys(void *buf)
 
 	vaddr = (uint64_t)buf;
 
-	paddr_2mb = spdk_mem_map_translate(g_vtophys_map, vaddr, VALUE_2MB);
+	paddr_2mb = spdk_mem_map_translate(g_vtophys_map, vaddr, NULL);
 
 	/*
 	 * SPDK_VTOPHYS_ERROR has all bits set, so if the lookup returned SPDK_VTOPHYS_ERROR,
