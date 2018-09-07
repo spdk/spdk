bd924f2f8d6828fa478f3cc006288bee85b165b5
GangCao <gang.cao@intel.com>
2018-09-07 18:16:59 -0400

    QoS/Bdev: add the pass through of IO opts
    Change-Id: Ie395583d97b7aa3af5d94c28909c952cbc7be56b
    Signed-off-by: GangCao <gang.cao@intel.com>

--- include/spdk/bdev.h
+++ include/spdk/bdev.h
@@ -123,6 +123,20 @@ enum spdk_bdev_qos_rate_limit_type {
 	SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES
 };
 
+/** bdev IO priority */
+enum spdk_bdev_io_priority {
+	SPDK_BDEV_IO_NORMAL_PRIORITY = 0,
+	SPDK_BDEV_IO_URGENT_PRIORITY
+};
+
+/** bdev IO additional opts */
+struct spdk_bdev_io_opts {
+	/** User specified I/O priority */
+	enum spdk_bdev_io_priority	priority;
+	/** Indicate to count this I/O */
+	bool				count_io;
+};
+
 /**
  * Block device completion callback.
  *
@@ -679,6 +693,7 @@ int spdk_bdev_write_blocks(struct spdk_bdev_desc *desc, struct spdk_io_channel *
  * \param iovcnt The number of elements in iov.
  * \param offset The offset, in bytes, from the start of the block device.
  * \param len The size of data to write.
+ * \param opts Additional opts of the I/O.
  * \param cb Called when the request is complete.
  * \param cb_arg Argument passed to cb.
  *
@@ -692,6 +707,7 @@ int spdk_bdev_write_blocks(struct spdk_bdev_desc *desc, struct spdk_io_channel *
 int spdk_bdev_writev(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 		     struct iovec *iov, int iovcnt,
 		     uint64_t offset, uint64_t len,
+		     struct spdk_bdev_io_opts *opts,
 		     spdk_bdev_io_completion_cb cb, void *cb_arg);
 
 /**
@@ -709,6 +725,7 @@ int spdk_bdev_writev(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
  * \param iovcnt The number of elements in iov.
  * \param offset_blocks The offset, in blocks, from the start of the block device.
  * \param num_blocks The number of blocks to write.
+ * \param opts Additional opts of the I/O.
  * \param cb Called when the request is complete.
  * \param cb_arg Argument passed to cb.
  *
@@ -722,6 +739,7 @@ int spdk_bdev_writev(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 int spdk_bdev_writev_blocks(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 			    struct iovec *iov, int iovcnt,
 			    uint64_t offset_blocks, uint64_t num_blocks,
+			    struct spdk_bdev_io_opts *opts,
 			    spdk_bdev_io_completion_cb cb, void *cb_arg);
 
 /**
