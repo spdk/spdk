b845bc9d897a8c031c2f3defc900af4b343939e5
Ben Walker <benjamin.walker@intel.com>
2018-09-10 12:04:31 -0700

    test: No longer use construct_nvmf_subsystem
    Use nvmf_subsystem_create instead.
    
    Change-Id: Idcfa6526ef47e2839f69acd5d7dc08e65e5150b5
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/nvmf/bdev_io_wait/bdev_io_wait.sh
+++ test/nvmf/bdev_io_wait/bdev_io_wait.sh
@@ -42,7 +42,11 @@ modprobe -v nvme-rdma
 
 bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
 
-$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 "trtype:RDMA traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420" '' -a -s SPDK00000000000001 -n "$bdevs"
+$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
+$rpc_py nvmf_subsystem_add_listener  nqn.2016-06.io.spdk:cnode1 -t rdma -a $NVMF_FIRST_TARGET_IP -s 4420
+for bdev in $bdevs; do
+	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 $bdev
+done
 
 echo "[Nvme]" > $testdir/bdevperf.conf
 echo "  TransportID \"trtype:RDMA adrfam:IPv4 subnqn:nqn.2016-06.io.spdk:cnode1 traddr:$NVMF_FIRST_TARGET_IP trsvcid:4420\" Nvme0" >> $testdir/bdevperf.conf
--- test/nvmf/host/identify.sh
+++ test/nvmf/host/identify.sh
@@ -33,8 +33,9 @@ timing_exit start_nvmf_tgt
 
 bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
 
-$rpc_py construct_nvmf_subsystem nqn.2016-06.io.spdk:cnode1 '' '' -a -s SPDK00000000000001
+$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
 $rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t RDMA -a $NVMF_FIRST_TARGET_IP -s 4420
+
 
 for bdev in $bdevs; do
 	# NOTE: This will assign the same NGUID and EUI64 to all bdevs,
