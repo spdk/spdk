5623a9d400a44959dca14efb2abf28694668ba6c
Ben Walker <benjamin.walker@intel.com>
2018-09-10 11:27:39 -0700

    nvmf: Dump new-style configuration RPCs
    Avoid using the deprecated construct_nvmf_subsystem
    when dumping configuration.
    
    Change-Id: I908d87bdd77a8b2a8e54baeb7b73e8b52c4912ee
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/json_config/clear_config.py
+++ test/json_config/clear_config.py
@@ -81,7 +81,7 @@ def clear_bdev_subsystem(args, bdev_config):
 
 
 def get_nvmf_destroy_method(nvmf):
-    destroy_method_map = {'construct_nvmf_subsystem': "delete_nvmf_subsystem",
+    destroy_method_map = {'nvmf_subsystem_create': "delete_nvmf_subsystem",
                           'set_nvmf_target_config': None,
                           'set_nvmf_target_options': None,
                           'nvmf_create_transport': None
