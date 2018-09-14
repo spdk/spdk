e9bab0c1ea51d4b6b1b3a296e3a3990756ec651a
Changpeng Liu <changpeng.liu@intel.com>
2018-09-14 02:20:55 -0400

    nvme: make identify ns can be executed asynchronously
    Change-Id: I189ad8889c74937bf43bcf2c3029416ddb94976d
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- test/unit/lib/nvme/nvme_ctrlr.c/nvme_ctrlr_ut.c
+++ test/unit/lib/nvme/nvme_ctrlr.c/nvme_ctrlr_ut.c
@@ -69,6 +69,7 @@ DEFINE_STUB(nvme_ctrlr_cmd_set_host_id, int,
 DEFINE_STUB(nvme_ctrlr_identify_ns, int, (struct spdk_nvme_ns *ns, uint32_t id,
 		struct spdk_nvme_ctrlr *ctrlr), 0);
 DEFINE_STUB(nvme_ctrlr_identify_id_desc, int, (struct spdk_nvme_ns *ns), 0);
+DEFINE_STUB_V(nvme_ns_identify_data_update, (struct spdk_nvme_ns *ns));
 
 struct spdk_nvme_ctrlr *nvme_transport_ctrlr_construct(const struct spdk_nvme_transport_id *trid,
 		const struct spdk_nvme_ctrlr_opts *opts,
