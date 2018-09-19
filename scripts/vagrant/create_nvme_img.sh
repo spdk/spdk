b4041a477d90714f31bda88da91cf8bb3160114d
Karol Latecki <karol.latecki@intel.com>
2018-09-19 11:31:00 +0200

    scripts/vagrant: allow adjustable nvme img size
    Allow specifying size for created NVMe image file.
    Default is 1024M but some tests suites (vhost_initator
    for example) need more.
    
    Change-Id: I4bf7a5241a0e9c2320cd473b1787c0be25d9a8e4
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- scripts/vagrant/create_nvme_img.sh
+++ scripts/vagrant/create_nvme_img.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 SYSTEM=`uname -s`
+size="1024M"
+
+# NVMe img size example format: 2048M
+if [ -n $1 ]; then
+	size=$1
+fi
 
 if [ ! "${SYSTEM}" = "FreeBSD" ]; then
 	WHICH_OS=`lsb_release -i | awk '{print $3}'`
 	nvme_disk='/var/lib/libvirt/images/nvme_disk.img'
 
-	qemu-img create -f raw $nvme_disk 1024M
+	qemu-img create -f raw $nvme_disk ${size}
 	#Change SE Policy on Fedora
 	if [ $WHICH_OS == "Fedora" ]; then
 		sudo chcon -t svirt_image_t $nvme_disk
