6e50b0acdaabf1ad03a5899574e5ed2447027e42
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-09-20 16:54:49 +0000

    CAS: add CAS module
    Add CAS module based on OCF meta-library
    Open CAS Framwework (OCF) is high performance block storage caching meta-library
    It is open-source, published at https://github.com/Open-CAS/ocf
    
    With this patch OCF-enabled device is represented in SPDK as virtual bdev
      having core and caching devices as its base devices
    
    This patch includes implementation of:
      * CAS top adapter          (vbdev_cas.c)
      * CAS bottom adapter       (vbdev_cas_dobj.c, vbdev_cas_data.c)
      * Adaptation layer for OCF (lib/cas/ocfenv/)
      * CAS context abstractions (ctx.c)
    
    Adaptation layer and context abstractions are not dependent on SPDK bdev
    
    CAS bdev supports reads and writes, configured at startup
    Other features will be added with separate patches
    
    Change-Id: Ic2dcab378c8238d16f1e4b64d4374bdf257565bc
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

---
+++ lib/bdev/cas/vbdev_cas_data.c
@@ -0,0 +1,128 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <ocf/ocf.h>
+
+#include "spdk/bdev.h"
+#include "spdk/log.h"
+
+#include "ocf_env.h"
+#include "data.h"
+#include "vbdev_cas.h"
+
+struct bdev_ocf_data *
+opencas_data_alloc(uint32_t iovcnt)
+{
+	struct bdev_ocf_data *data;
+
+	data = env_malloc(sizeof(*data), ENV_MEM_NOIO);
+	if (!data) {
+		return NULL;
+	}
+
+	data->seek = 0;
+
+	if (iovcnt) {
+		data->iovs = env_malloc(sizeof(*data->iovs) * iovcnt, ENV_MEM_NOIO);
+		if (!data->iovs) {
+			env_free(data);
+			return NULL;
+		}
+	}
+
+	data->iovcnt = 0;
+	data->iovalloc = iovcnt;
+
+	return data;
+}
+
+void
+opencas_data_free(struct bdev_ocf_data *data)
+{
+	if (!data) {
+		return;
+	}
+
+	if (data->iovalloc != 0) {
+		env_free(data->iovs);
+	}
+
+	env_free(data);
+}
+
+void
+_opencas_iovs_add(struct bdev_ocf_data *data, void *base, size_t len)
+{
+	assert(NULL != data);
+	assert(data->iovalloc != -1);
+
+	if (data->iovcnt == data->iovalloc) {
+		/* TODO: Realloc iovs */
+		SPDK_ERRLOG("IOV error");
+	}
+
+	data->iovs[data->iovcnt].iov_base = base;
+	data->iovs[data->iovcnt].iov_len = len;
+	data->iovcnt++;
+}
+
+struct bdev_ocf_data *
+opencas_data_from_spdk_io(void *io_ptr)
+{
+	struct spdk_bdev_io *bdev_io = io_ptr;
+	struct bdev_ocf_data *data;
+
+	switch (bdev_io->type) {
+	case SPDK_BDEV_IO_TYPE_WRITE:
+	case SPDK_BDEV_IO_TYPE_READ:
+		assert(bdev_io->u.bdev.iovs);
+		break;
+	case SPDK_BDEV_IO_TYPE_FLUSH:
+		break;
+	default:
+		SPDK_ERRLOG("Unsupported IO type %d\n", bdev_io->type);
+		return NULL;
+	}
+
+	data = opencas_data_alloc(0);
+	if (data == NULL) {
+		SPDK_ERRLOG("Memory allocation error\n");
+		return NULL;
+	}
+
+	data->iovs = bdev_io->u.bdev.iovs;
+	data->iovcnt = bdev_io->u.bdev.iovcnt;
+	data->size = bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen;
+
+	return data;
+}
---
+++ lib/cas/ocfenv/ocf_env_headers.h
@@ -0,0 +1,47 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __OCF_ENV_HEADERS_H__
+#define __OCF_ENV_HEADERS_H__
+
+#include "spdk/stdinc.h"
+
+#define OCF_VERSION_MAIN 3
+#define OCF_VERSION_MAJOR 8
+#define OCF_VERSION_MINOR 0
+
+#define OCF_LOGO "Intel(R) OCF"
+#define OCF_PREFIX_SHORT "[" OCF_LOGO "] "
+#define OCF_PREFIX_LONG "Intel(R) Cache Acceleration Software"
+
+#endif /* __OCF_ENV_HEADERS_H__ */
