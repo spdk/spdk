385e55fe312e5ade04d3ecf1a90c9d9d254c99c1
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-09-20 16:54:49 +0000

    CAS: initial
    Add cache module based on OCF metalibrary
    This patch includes both top-adapter implementation (lib/bdev/cache)
      and bottom adapter implementation (lib/cache)
    
    Change-Id: Ic2dcab378c8238d16f1e4b64d4374bdf257565bc
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/cache/stats.h
+++ lib/cache/stats.h
@@ -30,68 +30,25 @@
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifndef OPENCAS_STATS_H
+#define OPENCAS_STATS_H
 
-/** \file
- * Standard C headers
- *
- * This file is intended to be included first by all other SPDK files.
- */
+#include <ocf/ocf_types.h>
+#include <ocf/ocf_stats_builder.h>
+#include <ocf/ocf_stats.h>
 
-#ifndef SPDK_STDINC_H
-#define SPDK_STDINC_H
+struct cache_stats {
+	struct ocf_stats_usage usage;
+	struct ocf_stats_requests reqs;
+	struct ocf_stats_blocks blocks;
+	struct ocf_stats_errors errors;
+};
 
-#ifdef __cplusplus
-extern "C" {
+void print_usage_stats(struct ocf_stats_usage *usage);
+void print_reqs_stats(struct ocf_stats_requests *reqs);
+void print_blocks_stats(struct ocf_stats_blocks *blks);
+void print_errors_stats(struct ocf_stats_errors *errs);
+
+int cache_get_stats(int cache_id, int core_id, struct cache_stats *stats);
+
 #endif
-
-/* Standard C */
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-/* POSIX */
-#include <arpa/inet.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <ifaddrs.h>
-#include <netdb.h>
-#include <poll.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <signal.h>
-#include <syslog.h>
-#include <termios.h>
-#include <unistd.h>
-#include <net/if.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/resource.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <sys/un.h>
-#include <sys/user.h>
-#include <sys/wait.h>
-
-/* GNU extension */
-#include <getopt.h>
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* SPDK_STDINC_H */
