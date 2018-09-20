e254033cd1ff0c05736de1bff1ce2ac0ac174203
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-09-20 16:54:49 +0000

    CAS: initial
    Add cache module based on OCF metalibrary
    This patch includes both top-adapter implementation (lib/bdev/cache)
      and bottom adapter implementation (lib/cache)
    
    Change-Id: Ic2dcab378c8238d16f1e4b64d4374bdf257565bc
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/cache/utils.h
+++ lib/cache/utils.h
@@ -31,67 +31,12 @@
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** \file
- * Standard C headers
- *
- * This file is intended to be included first by all other SPDK files.
- */
+#ifndef OPENCAS_UTILS_H
+#define OPENCAS_UTILS_H
 
-#ifndef SPDK_STDINC_H
-#define SPDK_STDINC_H
+#include <ocf/ocf.h>
 
-#ifdef __cplusplus
-extern "C" {
+int ocf_get_cache_mode(const char *cache_mode);
+const char *ocf_get_cache_modename(int mode);
+
 #endif
-
-/* Standard C */
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-/* POSIX */
-#include <arpa/inet.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <ifaddrs.h>
-#include <netdb.h>
-#include <poll.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <signal.h>
-#include <syslog.h>
-#include <termios.h>
-#include <unistd.h>
-#include <net/if.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/resource.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <sys/un.h>
-#include <sys/user.h>
-#include <sys/wait.h>
-
-/* GNU extension */
-#include <getopt.h>
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* SPDK_STDINC_H */
