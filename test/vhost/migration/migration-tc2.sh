766e4877c47eb38446e459dd98403cd32d86e6ba
Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>
2018-09-24 17:08:44 +0200

    test/vhost: fix vhost live migration tc2 & tc3
    Change made in https://review.gerrithub.io/#/c/spdk/spdk/+/425187/
    mistakenly change name of rpc_nvmf variable.
    
    Change-Id: Ie973581777ec426d58d8b3cae12b747b4c3bd8c8
    Signed-off-by: Pawel Niedzwiecki <pawelx.niedzwiecki@intel.com>

--- test/vhost/migration/migration-tc2.sh
+++ test/vhost/migration/migration-tc2.sh
@@ -118,9 +118,9 @@ function migration_tc2_configure_vhost()
 	notice "Configuring nvmf_tgt, vhost devices & controllers via RPC ..."
 
 	# Construct shared bdevs and controllers
-	$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
-	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Nvme0n1
-	$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t rdma -a $nvmf_target_ip -s 4420
+	$rpc_nvmf nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
+	$rpc_nvmf nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Nvme0n1
+	$rpc_nvmf nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t rdma -a $nvmf_target_ip -s 4420
 
 	$rpc_0 construct_nvme_bdev -b Nvme0 -t rdma -f ipv4 -a $nvmf_target_ip -s 4420 -n "nqn.2016-06.io.spdk:cnode1"
 	$rpc_0 construct_vhost_scsi_controller $incoming_vm_ctrlr
