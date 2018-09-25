b2a243bfe6da922b104621d2ea17bfd0f306b3d1
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-09-25 14:32:23 +0200

    mk: get rid of genconfig.py
    We don't need it as config.h can be generated using couple of sed
    commands. This also void need of detecting python version before calling
    genconfig.py.
    
    Change-Id: I83f3628195ef59219562c593e2e6cdf3f3282fb3
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- mk/spdk.common.mk
+++ mk/spdk.common.mk
@@ -80,7 +80,7 @@ ifeq ($(TARGET_MACHINE),x86_64)
 COMMON_CFLAGS += -march=native
 endif
 
-COMMON_CFLAGS += -include $(SPDK_ROOT_DIR)/config.h
+COMMON_CFLAGS += -include $(SPDK_ROOT_DIR)/include/spdk/config.h
 
 ifeq ($(CONFIG_WERROR), y)
 COMMON_CFLAGS += -Werror
