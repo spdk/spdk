2dcbc448cc5099843982b4dcb629c98327c88efb
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2018-09-26 03:57:32 -0400

    net/vpp: switch to session.api
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I94ce2735c3d15dd7ee5e4ad33280e9996740e244

--- mk/spdk.modules.mk
+++ mk/spdk.modules.mk
@@ -80,11 +80,9 @@ SOCK_MODULES_LIST = sock_posix
 ifeq ($(CONFIG_VPP),y)
 SYS_LIBS += -Wl,--whole-archive
 ifneq ($(CONFIG_VPP_DIR),)
-SYS_LIBS += -l:libvppinfra.a -l:libsvm.a -l:libvapiclient.a
-SYS_LIBS += -l:libvppcom.a -l:libvlibmemoryclient.a
-else
-SYS_LIBS += -lvppcom
+SYS_LIBS += -L$(CONFIG_VPP_DIR)/lib
 endif
+SYS_LIBS += -lvppinfra -lsvm -lvlibmemoryclient
 SYS_LIBS += -Wl,--no-whole-archive
 SOCK_MODULES_LIST += sock_vpp
 endif
