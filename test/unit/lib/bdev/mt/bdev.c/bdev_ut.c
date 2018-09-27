c4fee1e970ab48f45848f3653a024382dfd9cdfa
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-09-27 21:38:15 +0200

    mk: don't use '-include spdk/config.h'
    Each file that need to check SPDK_CONFIG_* options need to include
    spdk/config.h explicitly.
    
    Change-Id: If9f2a91ac4c2b1a300dcf88ec3e2a12714ad344a
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/427221
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Chandler-Test-Pool: SPDK Automated Test System <sys_sgsw@intel.com>

--- test/unit/lib/bdev/mt/bdev.c/bdev_ut.c
+++ test/unit/lib/bdev/mt/bdev.c/bdev_ut.c
@@ -37,6 +37,7 @@
 #include "common/lib/ut_multithread.c"
 #include "unit/lib/json_mock.c"
 
+#include "spdk/config.h"
 /* HACK: disable VTune integration so the unit test doesn't need VTune headers and libs to build */
 #undef SPDK_CONFIG_VTUNE
 
