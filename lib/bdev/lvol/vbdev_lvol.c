caf7a1d62a571a6f19d2145ac97397f0dacda60e
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-10-04 16:59:54 +0200

    vbdev_lvol: set optimal_io_boundary to cluster size
    This patch sets optimal_io_boundary to cluster size, so that splitting
    happens in bdev layer rather than blobstore layer.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I0230cb4a188d605845a709e9c3c9061e822ef0f5

--- lib/bdev/lvol/vbdev_lvol.c
+++ lib/bdev/lvol/vbdev_lvol.c
@@ -958,6 +958,8 @@ _create_lvol_disk(struct spdk_lvol *lvol, bool destroy)
 	bdev->blockcnt = total_size / bdev->blocklen;
 	bdev->uuid = lvol->uuid;
 	bdev->need_aligned_buffer = lvs_bdev->bdev->need_aligned_buffer;
+	bdev->split_on_optimal_io_boundary = true;
+	bdev->optimal_io_boundary = spdk_bs_get_cluster_size(lvol->lvol_store->blobstore) / bdev->blocklen;
 
 	bdev->ctxt = lvol;
 	bdev->fn_table = &vbdev_lvol_fn_table;
