c6366e72e057262e5ee541903ff048c00f31f371
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-10-08 07:42:51 -0400

    test: fix nvmf connect/disconnect
    Add missing wait for test/nvme/shutdown/shutdown.sh
    Not waiting after connecting nvme device resulted in undefined behavior.
    
    Change-Id: Idb29c7f6b40ae4b3fa0eb68f7b8d5f86d9a4149f
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- test/nvmf/shutdown/shutdown.sh
+++ test/nvmf/shutdown/shutdown.sh
@@ -63,7 +63,9 @@ for ((x=0; x<5;x++)); do
 	# Connect kernel host to subsystems
 	for i in `seq 1 $num_subsystems`; do
 		nvme connect -t rdma -n "nqn.2016-06.io.spdk:cnode${i}" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
+		waitforblk "nvme$((i - 1))n1"
 	done
+
 	# Disconnect the subsystems in reverse order
 	for i in `seq $num_subsystems -1 1`; do
 		nvme disconnect -n nqn.2016-06.io.spdk:cnode${i}
