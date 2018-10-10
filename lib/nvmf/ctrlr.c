88e272e634400ec5c781fc09b985f44be44ae658
Seth Howell <seth.howell@intel.com>
2018-10-10 15:55:29 -0700

    nvmf: Report that we support more than one SGL element
    Change-Id: Idf5aeb1fa3d6a3a83042bd699e0099b95e34f5b9
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -1525,7 +1525,12 @@ spdk_nvmf_ctrlr_identify_ctrlr(struct spdk_nvmf_ctrlr *ctrlr, struct spdk_nvme_c
 		cdata->nvmf_specific.iorcsz = sizeof(struct spdk_nvme_cpl) / 16;
 		cdata->nvmf_specific.icdoff = 0; /* offset starts directly after SQE */
 		cdata->nvmf_specific.ctrattr.ctrlr_model = SPDK_NVMF_CTRLR_MODEL_DYNAMIC;
-		cdata->nvmf_specific.msdbd = 1; /* target supports single SGL in capsule */
+		/* The RDMA transport supports up to SPDK_NVMF_MAX_SGL_ENTRIES descriptors. */
+		if (transport->ops->type == SPDK_NVME_TRANSPORT_RDMA) {
+			cdata->nvmf_specific.msdbd = SPDK_NVMF_MAX_SGL_ENTRIES;
+		} else {
+			cdata->nvmf_specific.msdbd = 1;
+		}
 
 		/* TODO: this should be set by the transport */
 		cdata->nvmf_specific.ioccsz += transport->opts.in_capsule_data_size / 16;
