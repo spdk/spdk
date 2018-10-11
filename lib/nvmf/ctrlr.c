6b40be11194f31ca57514c9126530683d97bfa79
yidong0635 <dongx.yi@intel.com>
2018-10-11 16:46:35 -0400

    nvmf/ctrlr: add debug logs for volatile write cache
    Add debug logs in set feature,
    spdk_nvmf_ctrlr_set_features_volatile_write_cache
    to indicate the volatile write cache is disabled or enabled
    according to the conditon.
    
    Change-Id: Idc0a7fb461e2bbf1371d4a3faf5d839c7370bb65
    Signed-off-by: yidong0635 <dongx.yi@intel.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -796,6 +796,11 @@ spdk_nvmf_ctrlr_set_features_volatile_write_cache(struct spdk_nvmf_request *req)
 	ctrlr->feat.volatile_write_cache.raw = cmd->cdw11;
 	ctrlr->feat.volatile_write_cache.bits.reserved = 0;
 
+	if (!ctrlr->feat.volatile_write_cache.bits.wce) {
+		SPDK_DEBUGLOG(SPDK_LOG_NVMF, "Set Features - Volatile Write Cache  is disabled\n");
+	} else {
+		SPDK_DEBUGLOG(SPDK_LOG_NVMF, "Set Features - Volatile Write Cache  is enabled\n");
+	}
 	return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
 }
 
