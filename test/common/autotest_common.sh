aa285c8f0b8f2c11eccab25edab248e38b735dd2
SPDK Nightly Cron Job <sys_sgsw@intel.com>
2018-10-12 05:00:09 -0700

    SPDK NIGHTLY TEST RUN 10/12/18 - RUN_NIGHTLY - master
    Change-Id: I2d6f1d3f0a4496f1ae5911195073301ca83da128
    Signed-off-by: SPDK Nightly Cron Job <sys_sgsw@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -13,7 +13,7 @@ export ASAN_OPTIONS=new_delete_type_mismatch=0
 PS4=' \t	\$ '
 ulimit -c unlimited
 
-: ${RUN_NIGHTLY:=0}
+: ${RUN_NIGHTLY:=1}
 export RUN_NIGHTLY
 
 : ${RUN_NIGHTLY_FAILING:=0}
@@ -696,3 +696,4 @@ function freebsd_update_contigmem_mod()
 
 set -o errtrace
 trap "trap - ERR; print_backtrace >&2" ERR
+DATE='Fri Oct 12 05:00:09 MST 2018'
