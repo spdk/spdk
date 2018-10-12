f0cb7b871ec66328e5199dbb04e5433f8c115774
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2018-10-12 09:19:54 +0200

    autorun: passthrough WITH_DPDK_DIR to autotest.sh
    This variable will be used for something more than
    just building SPDK, namely installing the freebsd
    contigmem kernel module.
    
    Note: Installing a module requires root priviledges
    and can't be done as a part of autobuild.
    
    Change-Id: I45cc797493cc4ff22c1f8d0dd5e4e56642d54d11
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-on: https://review.gerrithub.io/429186
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Chandler-Test-Pool: SPDK Automated Test System <sys_sgsw@intel.com>

--- autorun.sh
+++ autorun.sh
@@ -8,5 +8,5 @@ conf=~/autorun-spdk.conf
 
 # Runs agent scripts
 $rootdir/autobuild.sh "$conf"
-sudo $rootdir/autotest.sh "$conf"
+sudo WITH_DPDK_DIR="$WITH_DPDK_DIR" $rootdir/autotest.sh "$conf"
 $rootdir/autopackage.sh "$conf"
