62e54464e137c2d98fe908390201b992ddd15e3f
paul luse <paul.e.luse@intel.com>
2018-10-15 16:46:41 -0400

    bdev/compress: Add configure option and build dependencies
    Includes the required DPDK dependencies for SPDK block Reduce aka
    Compression. Installation of ISA-L follows same model as
    ipsec-lib for crypto.
    
    Updated pkg_dep compression and crypto dependency check/install
    to provide more user output.
    
    Change-Id: Ic1ea3cbeb9373a7700f6f0c2a3194d65d6a34a41
    Signed-off-by: paul luse <paul.e.luse@intel.com>

--- autorun.sh
+++ autorun.sh
@@ -4,6 +4,12 @@ set -e
 
 rootdir=$(readlink -f $(dirname $0))
 
+cd $rootdir
+git submodule update --init
+cd -
+sudo $rootdir/scripts/pkgdep.sh -r
+exit 0
+
 conf=~/autorun-spdk.conf
 
 # Runs agent scripts
