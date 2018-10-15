528c717a8b7794426a53b7bf0a63b2afcf2fa4fc
Seth Howell <seth.howell@intel.com>
2018-10-15 14:38:49 -0700

    test/bdev: change crypto device conf based on environment
    Change-Id: I2868839f40931b964d49a90a86dfb391e2247e30
    Signed-off-by: Seth Howell <seth.howell@intel.com>

---
+++ scripts/gen_crypto.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+rootdir=$(readlink -f $(dirname $0))/..
+source "$rootdir/scripts/common.sh"
+
+base_bdev=$1
+
+if [ -n $base_bdev ]; then
+        echo
+        echo "[crypto]"
+        if [ $(lspci -d:37c8 | wc -l) -eq 0 ]; then
+                echo "  CRY $base_bdev crypto_ram 0123456789123456 crypto_aesni_mb"
+        else
+                echo "  CRY $base_bdev crypto_ram 0123456789123456 crypto_qat"
+        fi
+fi
