14948e40fb67876bae38c9d73d5a1894d4a15582
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-10-24 09:02:31 +0200

    bdev: clean spdk_bdev_get_buf_align code
    This patch cleans code before introducing patch with universal
    alignment.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I5329b9fe26ef2417bc7beae86518cc643b263f97

--- lib/bdev/bdev.c
+++ lib/bdev/bdev.c
@@ -2033,12 +2033,9 @@ spdk_bdev_get_qos_rate_limits(struct spdk_bdev *bdev, uint64_t *limits)
 size_t
 spdk_bdev_get_buf_align(const struct spdk_bdev *bdev)
 {
-	/* TODO: push this logic down to the bdev modules */
-	if (bdev->need_aligned_buffer) {
-		return bdev->blocklen;
-	}
+	assert(bdev->required_alignment < 32);
 
-	return 1;
+	return 1U << bdev->required_alignment;
 }
 
 uint32_t
