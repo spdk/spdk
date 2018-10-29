155dd85f7a002c4f966da73e23762f182e054074
Wojciech Malikowski <wojciech.malikowski@intel.com>
2018-10-29 10:40:34 -0400

    bdev_ftl: Added FTL bdev functional tests
    This patch introduces functional tests for FTL bdev.
    
    The tests cover various I/O workflows and check data integrity. Several
    scripts have been added to test the FTL library:
     * generate_config.sh - prepares configuration scripts for specified
     device
     * restore.sh - tests restoring device's state from the SSD
     * fio.sh - runs tests based on fio and fio_plugin
    
    The tests are run from autotest.sh when the SPDK_TEST_BDEV_FTL flag is
    set.
    
    Change-Id: I561d99ed35fe91eadd3756789cc99afe2da8c1db
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -61,6 +61,7 @@ fi
 : ${SPDK_RUN_INSTALLED_DPDK=1}; export SPDK_RUN_INSTALLED_DPDK
 : ${SPDK_TEST_CRYPTO=1}; export SPDK_TEST_CRYPTO
 : ${SPDK_TEST_FTL=0}; export SPDK_TEST_FTL
+: ${SPDK_TEST_BDEV_FTL=0}; export SPDK_TEST_BDEV_FTL
 
 if [ -z "$DEPENDENCY_DIR" ]; then
 	export DEPENDENCY_DIR=/home/sys_sgsw
@@ -252,7 +253,7 @@ function timing_finish() {
 }
 
 function create_test_list() {
-	grep -rsh --exclude="autotest_common.sh" --exclude="$rootdir/test/common/autotest_common.sh" -e "report_test_completion" $rootdir | sed 's/report_test_completion//g; s/[[:blank:]]//g; s/"//g;' > $output_dir/all_tests.txt || true
+	grep -rshI --exclude="autotest_common.sh" --exclude="$rootdir/test/common/autotest_common.sh" -e "report_test_completion" $rootdir | sed 's/report_test_completion//g; s/[[:blank:]]//g; s/"//g;' > $output_dir/all_tests.txt || true
 }
 
 function report_test_completion() {
