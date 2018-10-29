ea49768339ed6e0f6a6f1a021f5e584fe95b2fa0
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-10-29 20:23:51 +0000

    CAS: add RPC
    Add rpc methods for vbdev_cas:
      * construct_cas_bdev
      * delete_cas_bdev
    
    Change-Id: Ie75a2b4bf4f88462c00651d195d22d156478abf8
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

---
+++ lib/bdev/cas/vbdev_cas_rpc.c
@@ -0,0 +1,174 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "vbdev_cas.h"
+#include "spdk/log.h"
+#include "spdk/rpc.h"
+#include "spdk/string.h"
+
+/* Structure to hold the parameters for this RPC method. */
+struct rpc_construct_cas_bdev {
+	char *name;             /* master vbdev */
+	char *mode;             /* CAS mode (choose one) */
+	char *cache_bdev_name;  /* sub bdev */
+	char *core_bdev_name;   /* sub bdev */
+};
+
+static void
+free_rpc_construct_cas_bdev(struct rpc_construct_cas_bdev *r)
+{
+	free(r->name);
+	free(r->core_bdev_name);
+	free(r->cache_bdev_name);
+	free(r->mode);
+}
+
+/* Structure to decode the input parameters for this RPC method. */
+static const struct spdk_json_object_decoder rpc_construct_cas_bdev_decoders[] = {
+	{"name", offsetof(struct rpc_construct_cas_bdev, name), spdk_json_decode_string},
+	{"mode", offsetof(struct rpc_construct_cas_bdev, mode), spdk_json_decode_string},
+	{"cache_bdev_name", offsetof(struct rpc_construct_cas_bdev, cache_bdev_name), spdk_json_decode_string},
+	{"core_bdev_name", offsetof(struct rpc_construct_cas_bdev, core_bdev_name), spdk_json_decode_string},
+};
+
+static void
+spdk_rpc_construct_cas_bdev(struct spdk_jsonrpc_request *request,
+			    const struct spdk_json_val *params)
+{
+	int ret = 0;
+	struct rpc_construct_cas_bdev req = {NULL};
+	struct spdk_json_write_ctx *w;
+
+	ret = spdk_json_decode_object(params, rpc_construct_cas_bdev_decoders,
+				      SPDK_COUNTOF(rpc_construct_cas_bdev_decoders),
+				      &req);
+	if (ret) {
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "Invalid parameters");
+		goto end;
+	}
+
+	ret = vbdev_cas_construct(req.name, req.mode, req.cache_bdev_name, req.core_bdev_name);
+	if (ret) {
+		spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						     "Could not create CAS vbdev: %s",
+						     spdk_strerror(-ret));
+		goto end;
+	}
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w) {
+		spdk_json_write_string(w, req.name);
+		spdk_jsonrpc_end_result(request, w);
+	}
+
+end:
+	free_rpc_construct_cas_bdev(&req);
+}
+SPDK_RPC_REGISTER("construct_cas_bdev", spdk_rpc_construct_cas_bdev, SPDK_RPC_RUNTIME)
+
+/* Structure to hold the parameters for this RPC method. */
+struct rpc_delete_cas_bdev {
+	char *name;             /* master vbdev name */
+};
+
+static void
+free_rpc_delete_cas_bdev(struct rpc_delete_cas_bdev *r)
+{
+	free(r->name);
+}
+
+/* Structure to decode the input parameters for this RPC method. */
+static const struct spdk_json_object_decoder rpc_delete_cas_bdev_decoders[] = {
+	{"name", offsetof(struct rpc_delete_cas_bdev, name), spdk_json_decode_string},
+};
+
+static void
+rpc_delete_cb(int rc, void *opaque)
+{
+	struct spdk_jsonrpc_request *request = opaque;
+	struct spdk_json_write_ctx *w;
+
+	if (rc) {
+		spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						     "Could not delete CAS vbdev: %s",
+						     spdk_strerror(-rc));
+	} else {
+		w = spdk_jsonrpc_begin_result(request);
+		if (w == NULL) {
+			return;
+		}
+
+		spdk_json_write_bool(w, true);
+		spdk_jsonrpc_end_result(request, w);
+	}
+}
+
+static void
+spdk_rpc_delete_cas_bdev(struct spdk_jsonrpc_request *request,
+			 const struct spdk_json_val *params)
+{
+	struct rpc_delete_cas_bdev req = {NULL};
+	struct vbdev_cas *vbdev;
+	int status;
+
+	status = spdk_json_decode_object(params, rpc_delete_cas_bdev_decoders,
+					 SPDK_COUNTOF(rpc_delete_cas_bdev_decoders),
+					 &req);
+	if (status) {
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "Invalid parameters");
+		goto end;
+	}
+
+	vbdev = vbdev_cas_get_by_name(req.name);
+	if (vbdev == NULL) {
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 spdk_strerror(ENODEV));
+		goto end;
+	}
+
+	status = vbdev_cas_delete(vbdev, rpc_delete_cb, request);
+	if (status) {
+		spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						     "Could not delete CAS vbdev: %s",
+						     spdk_strerror(-status));
+		goto end;
+	}
+
+end:
+	free_rpc_delete_cas_bdev(&req);
+}
+SPDK_RPC_REGISTER("delete_cas_bdev", spdk_rpc_delete_cas_bdev, SPDK_RPC_RUNTIME)
