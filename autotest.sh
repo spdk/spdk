e205322c7ad566ead07aa64d65ae0ac50dc1a65a
Wojciech Malikowski <wojciech.malikowski@intel.com>
2018-10-29 10:40:34 -0400

    ftl: Added OCSSD functional tests
    This patch introduces functional tests for OCSSD bdev.
    
    The tests cover various I/O workflows and check data integrity. Several
    scripts have been added to test the FTL library:
     * generate_config.sh - prepares configuration scripts for specified
     device
     * restore.sh - tests restoring device's state from the SSD
     * fio.sh - runs tests based on fio and fio_plugin
     * vdbench.sh - runs tests based on vdbench and nbd
    
    The tests are run from autotest.sh when the SPDK_TEST_OCSSD_FTL flag is
    set.
    
    Change-Id: I561d99ed35fe91eadd3756789cc99afe2da8c1db
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -203,6 +203,10 @@ if [ $SPDK_TEST_RBD -eq 1 ]; then
 	run_test suite ./test/spdkcli/rbd.sh
 fi
 
+if [ $SPDK_TEST_BDEV_OCSSD -eq 1 ]; then
+	run_test suite ./test/ocssd/ocssd.sh
+fi
+
 timing_enter cleanup
 autotest_cleanup
 timing_exit cleanup
