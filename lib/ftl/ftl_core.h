4734942acc93bd55a564f4e61d5607aeabcd3fba
Wojciech Malikowski <wojciech.malikowski@intel.com>
2018-10-29 09:06:35 -0400

    ftl: Added relocate module
    This patch implements module responsible for data relocation due to
    defragmentation, wear-leveling, read disturb and/or background data
    refresh.
    
    The submodule supports moving whole bands, chunks, as well as
    single logical blocks.
    
    Change-Id: Iaef37f50e0bb93986a21e495174f6d5ef8a1f30d
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Signed-off-by: Jakub Radtke <jakub.radtke@intel.com>

--- lib/ftl/ftl_core.h
+++ lib/ftl/ftl_core.h
@@ -55,6 +55,7 @@ struct ftl_io;
 struct ftl_restore;
 struct ftl_wptr;
 struct ftl_flush;
+struct ftl_reloc;
 
 struct ftl_stats {
 	/* Number of writes scheduled directly by the user */
@@ -206,6 +207,9 @@ struct spdk_ftl_dev {
 
 	/* Inflight io operations */
 	atomic_ulong				num_inflight;
+
+	/* Manages data relocation */
+	struct ftl_reloc			*reloc;
 
 	/* Task array */
 	struct ftl_task				tasks[FTL_TASK_ID_MAX];
