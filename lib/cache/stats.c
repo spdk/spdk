ac127c86efef99d6fa1303f20b41051e9d0c949e
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-10-29 20:23:51 +0000

    CAS: add RPC
    Add rpc methods to vbdev_cache module:
    - construct_cache_bdev
    - delete_cache_bdev
    - get_cas_stats
    
    Change-Id: Ie75a2b4bf4f88462c00651d195d22d156478abf8
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/cache/stats.c
+++ lib/cache/stats.c
@@ -36,115 +36,139 @@
 #include "ctx.h"
 #include "stats.h"
 
-static float percentage(uint64_t percent)
+#define MAX_STAT_LEN 10000
+
+static float
+percentage(uint64_t percent)
 {
 	return percent / 10.;
 }
 
-
-void print_usage_stats(struct ocf_stats_usage *usage)
+void
+cache_stats_write_usage(struct ocf_stats_usage *usage, cache_get_stats_callback_t callback,
+			void  *ctx)
 {
-	printf("╔══════════════════╤══════════╤═══════╤═════════════╗\n");
-	printf("║ Usage statistics │  Count   │   %%   │   Units     ║\n");
-	printf("╠══════════════════╪══════════╪═══════╪═════════════╣\n");
-	printf("║ Occupancy        │ %8lu │ %5.1f │ 4KiB blocks ║\n", usage->occupancy.value,
-	       percentage(usage->occupancy.percent));
-	printf("║ Free             │ %8lu │ %5.1f │ 4KiB blocks ║\n", usage->free.value,
-	       percentage(usage->free.percent));
-	printf("║ Clean            │ %8lu │ %5.1f │ 4KiB blocks ║\n", usage->clean.value,
-	       percentage(usage->clean.percent));
-	printf("║ Dirty            │ %8lu │ %5.1f │ 4KiB blocks ║\n", usage->dirty.value,
-	       percentage(usage->dirty.percent));
-	printf("╚══════════════════╧══════════╧═══════╧═════════════╝\n");
+	char buff[MAX_STAT_LEN];
+	snprintf(buff, MAX_STAT_LEN,
+		 "╔══════════════════╤══════════╤═══════╤═════════════╗\n"
+		 "║ Usage statistics │  Count   │   %%   │   Units     ║\n"
+		 "╠══════════════════╪══════════╪═══════╪═════════════╣\n"
+		 "║ Occupancy        │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Free             │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Clean            │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Dirty            │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "╚══════════════════╧══════════╧═══════╧═════════════╝\n"
+		 , usage->occupancy.value, percentage(usage->occupancy.percent)
+		 , usage->free.value, percentage(usage->free.percent)
+		 , usage->clean.value, percentage(usage->clean.percent)
+		 , usage->dirty.value, percentage(usage->dirty.percent)
+		);
+	callback(buff, ctx);
 }
 
-
-void print_reqs_stats(struct ocf_stats_requests *reqs)
+void
+cache_stats_write_reqs(struct ocf_stats_requests *reqs, cache_get_stats_callback_t callback,
+		       void *ctx)
 {
-	printf("╔══════════════════════╤══════════╤═══════╤══════════╗\n");
-	printf("║ Request statistics   │  Count   │   %%   │ Units    ║\n");
-	printf("╠══════════════════════╪══════════╪═══════╪══════════╣\n");
-	printf("║ Read hits            │ %8lu │ %5.1f │ Requests ║\n", reqs->rd_hits.value,
-	       percentage(reqs->rd_hits.percent));
-	printf("║ Read partial misses  │ %8lu │ %5.1f │ Requests ║\n",
-	       reqs->rd_partial_misses.value, percentage(reqs->rd_partial_misses.percent));
-	printf("║ Read full misses     │ %8lu │ %5.1f │ Requests ║\n", reqs->rd_full_misses.value,
-	       percentage(reqs->rd_full_misses.percent));
-	printf("║ Read total           │ %8lu │ %5.1f │ Requests ║\n", reqs->rd_total.value,
-	       percentage(reqs->rd_total.percent));
-	printf("╟──────────────────────┼──────────┼───────┼──────────╢\n");
-	printf("║ Write hits           │ %8lu │ %5.1f │ Requests ║\n", reqs->wr_hits.value,
-	       percentage(reqs->wr_hits.percent));
-	printf("║ Write partial misses │ %8lu │ %5.1f │ Requests ║\n",
-	       reqs->wr_partial_misses.value, percentage(reqs->wr_partial_misses.percent));
-	printf("║ Write full misses    │ %8lu │ %5.1f │ Requests ║\n", reqs->wr_full_misses.value,
-	       percentage(reqs->wr_full_misses.percent));
-	printf("║ Write total          │ %8lu │ %5.1f │ Requests ║\n", reqs->wr_total.value,
-	       percentage(reqs->wr_total.percent));
-	printf("╟──────────────────────┼──────────┼───────┼──────────╢\n");
-	printf("║ Pass-Through reads   │ %8lu │ %5.1f │ Requests ║\n", reqs->rd_pt.value,
-	       percentage(reqs->rd_pt.percent));
-	printf("║ Pass-Through writes  │ %8lu │ %5.1f │ Requests ║\n", reqs->wr_pt.value,
-	       percentage(reqs->wr_pt.percent));
-	printf("║ Serviced requests    │ %8lu │ %5.1f │ Requests ║\n", reqs->serviced.value,
-	       percentage(reqs->serviced.percent));
-	printf("╟──────────────────────┼──────────┼───────┼──────────╢\n");
-	printf("║ Total requests       │ %8lu │ %5.1f │ Requests ║\n", reqs->total.value,
-	       percentage(reqs->total.percent));
-	printf("╚══════════════════════╧══════════╧═══════╧══════════╝\n");
+	char buff[MAX_STAT_LEN];
+	snprintf(buff, MAX_STAT_LEN,
+		 "╔══════════════════════╤══════════╤═══════╤══════════╗\n"
+		 "║ Request statistics   │  Count   │   %%   │ Units    ║\n"
+		 "╠══════════════════════╪══════════╪═══════╪══════════╣\n"
+		 "║ Read hits            │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Read partial misses  │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Read full misses     │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Read total           │ %8lu │ %5.1f │ Requests ║\n"
+		 "╟──────────────────────┼──────────┼───────┼──────────╢\n"
+		 "║ Write hits           │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Write partial misses │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Write full misses    │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Write total          │ %8lu │ %5.1f │ Requests ║\n"
+		 "╟──────────────────────┼──────────┼───────┼──────────╢\n"
+		 "║ Pass-Through reads   │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Pass-Through writes  │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Serviced requests    │ %8lu │ %5.1f │ Requests ║\n"
+		 "╟──────────────────────┼──────────┼───────┼──────────╢\n"
+		 "║ Total requests       │ %8lu │ %5.1f │ Requests ║\n"
+		 "╚══════════════════════╧══════════╧═══════╧══════════╝\n"
+		 , reqs->rd_hits.value, percentage(reqs->rd_hits.percent)
+		 , reqs->rd_partial_misses.value, percentage(reqs->rd_partial_misses.percent)
+		 , reqs->rd_full_misses.value, percentage(reqs->rd_full_misses.percent)
+		 , reqs->rd_total.value, percentage(reqs->rd_total.percent)
+		 , reqs->wr_hits.value, percentage(reqs->wr_hits.percent)
+		 , reqs->wr_partial_misses.value, percentage(reqs->wr_partial_misses.percent)
+		 , reqs->wr_full_misses.value, percentage(reqs->wr_full_misses.percent)
+		 , reqs->wr_total.value, percentage(reqs->wr_total.percent)
+		 , reqs->rd_pt.value, percentage(reqs->rd_pt.percent)
+		 , reqs->wr_pt.value, percentage(reqs->wr_pt.percent)
+		 , reqs->serviced.value, percentage(reqs->serviced.percent)
+		 , reqs->total.value, percentage(reqs->total.percent)
+		);
+	callback(buff, ctx);
 }
 
-void print_blocks_stats(struct ocf_stats_blocks *blks)
+void
+cache_stats_write_blocks(struct ocf_stats_blocks *blks, cache_get_stats_callback_t callback,
+			 void  *ctx)
 {
-	printf("╔════════════════════════════════════╤══════════╤═══════╤═════════════╗\n");
-	printf("║ Block statistics                   │  Count   │   %%   │   Units     ║\n");
-	printf("╠════════════════════════════════════╪══════════╪═══════╪═════════════╣\n");
-	printf("║ Reads from core data object(s)     │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->core_obj_rd.value, percentage(blks->core_obj_rd.percent));
-	printf("║ Writes to core data object(s)      │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->core_obj_wr.value, percentage(blks->core_obj_wr.percent));
-	printf("║ Total to/from core data object (s) │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->core_obj_total.value, percentage(blks->core_obj_total.percent));
-	printf("╟────────────────────────────────────┼──────────┼───────┼─────────────╢\n");
-	printf("║ Reads from cache data object       │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->cache_obj_rd.value, percentage(blks->cache_obj_rd.percent));
-	printf("║ Writes to cache data object        │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->cache_obj_wr.value, percentage(blks->cache_obj_wr.percent));
-	printf("║ Total to/from cache data object    │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->cache_obj_total.value, percentage(blks->cache_obj_total.percent));
-	printf("╟────────────────────────────────────┼──────────┼───────┼─────────────╢\n");
-	printf("║ Reads from volume                  │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->volume_rd.value, percentage(blks->volume_rd.percent));
-	printf("║ Writes to volume                   │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->volume_wr.value, percentage(blks->volume_wr.percent));
-	printf("║ Total to/from volume               │ %8lu │ %5.1f │ 4KiB blocks ║\n",
-	       blks->volume_total.value, percentage(blks->volume_total.percent));
-	printf("╚════════════════════════════════════╧══════════╧═══════╧═════════════╝\n");
-
+	char buff[MAX_STAT_LEN];
+	snprintf(buff, MAX_STAT_LEN,
+		 "╔════════════════════════════════════╤══════════╤═══════╤═════════════╗\n"
+		 "║ Block statistics                   │  Count   │   %%   │   Units     ║\n"
+		 "╠════════════════════════════════════╪══════════╪═══════╪═════════════╣\n"
+		 "║ Reads from core data object(s)     │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Writes to core data object(s)      │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Total to/from core data object (s) │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "╟────────────────────────────────────┼──────────┼───────┼─────────────╢\n"
+		 "║ Reads from cache data object       │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Writes to cache data object        │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Total to/from cache data object    │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "╟────────────────────────────────────┼──────────┼───────┼─────────────╢\n"
+		 "║ Reads from volume                  │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Writes to volume                   │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "║ Total to/from volume               │ %8lu │ %5.1f │ 4KiB blocks ║\n"
+		 "╚════════════════════════════════════╧══════════╧═══════╧═════════════╝\n"
+		 , blks->core_obj_rd.value, percentage(blks->core_obj_rd.percent)
+		 , blks->core_obj_wr.value, percentage(blks->core_obj_wr.percent)
+		 , blks->core_obj_total.value, percentage(blks->core_obj_total.percent)
+		 , blks->cache_obj_rd.value, percentage(blks->cache_obj_rd.percent)
+		 , blks->cache_obj_wr.value, percentage(blks->cache_obj_wr.percent)
+		 , blks->cache_obj_total.value, percentage(blks->cache_obj_total.percent)
+		 , blks->volume_rd.value, percentage(blks->volume_rd.percent)
+		 , blks->volume_wr.value, percentage(blks->volume_wr.percent)
+		 , blks->volume_total.value, percentage(blks->volume_total.percent)
+		);
+	callback(buff, ctx);
 }
 
-void print_errors_stats(struct ocf_stats_errors *errs)
+void
+cache_stats_write_errors(struct ocf_stats_errors *errs, cache_get_stats_callback_t callback,
+			 void  *ctx)
 {
-	printf("╔════════════════════╤══════════╤═══════╤══════════╗\n");
-	printf("║ Error statistics   │  Count   │   %%   │ Units    ║\n");
-	printf("╠════════════════════╪══════════╪═══════╪══════════╣\n");
-	printf("║ Cache read errors  │ %8lu │ %5.1f │ Requests ║\n", errs->core_obj_rd.value,
-	       percentage(errs->core_obj_rd.percent));
-	printf("║ Cache write errors │ %8lu │ %5.1f │ Requests ║\n", errs->core_obj_wr.value,
-	       percentage(errs->core_obj_wr.percent));
-	printf("║ Cache total errors │ %8lu │ %5.1f │ Requests ║\n", errs->core_obj_total.value,
-	       percentage(errs->core_obj_total.percent));
-	printf("╟────────────────────┼──────────┼───────┼──────────╢\n");
-	printf("║ Core read errors   │ %8lu │ %5.1f │ Requests ║\n", errs->cache_obj_rd.value,
-	       percentage(errs->cache_obj_rd.percent));
-	printf("║ Core write errors  │ %8lu │ %5.1f │ Requests ║\n", errs->cache_obj_wr.value,
-	       percentage(errs->cache_obj_wr.percent));
-	printf("║ Core total errors  │ %8lu │ %5.1f │ Requests ║\n", errs->cache_obj_total.value,
-	       percentage(errs->cache_obj_total.percent));
-	printf("╟────────────────────┼──────────┼───────┼──────────╢\n");
-	printf("║ Total errors       │ %8lu │ %5.1f │ Requests ║\n", errs->total.value,
-	       percentage(errs->total.percent));
-	printf("╚════════════════════╧══════════╧═══════╧══════════╝\n");
+	char buff[MAX_STAT_LEN];
+	snprintf(buff, MAX_STAT_LEN,
+		 "╔════════════════════╤══════════╤═══════╤══════════╗\n"
+		 "║ Error statistics   │  Count   │   %%   │ Units    ║\n"
+		 "╠════════════════════╪══════════╪═══════╪══════════╣\n"
+		 "║ Cache read errors  │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Cache write errors │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Cache total errors │ %8lu │ %5.1f │ Requests ║\n"
+		 "╟────────────────────┼──────────┼───────┼──────────╢\n"
+		 "║ Core read errors   │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Core write errors  │ %8lu │ %5.1f │ Requests ║\n"
+		 "║ Core total errors  │ %8lu │ %5.1f │ Requests ║\n"
+		 "╟────────────────────┼──────────┼───────┼──────────╢\n"
+		 "║ Total errors       │ %8lu │ %5.1f │ Requests ║\n"
+		 "╚════════════════════╧══════════╧═══════╧══════════╝\n"
+		 , errs->cache_obj_rd.value, percentage(errs->cache_obj_rd.percent)
+		 , errs->cache_obj_wr.value, percentage(errs->cache_obj_wr.percent)
+		 , errs->cache_obj_total.value, percentage(errs->cache_obj_total.percent)
+		 , errs->core_obj_rd.value, percentage(errs->core_obj_rd.percent)
+		 , errs->core_obj_wr.value, percentage(errs->core_obj_wr.percent)
+		 , errs->core_obj_total.value, percentage(errs->core_obj_total.percent)
+		 , errs->total.value, percentage(errs->total.percent)
+		);
+	callback(buff, ctx);
 }
 
 int
