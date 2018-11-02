b2bbc990d0fa5adaf92c5fde9792f686317aa7da
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-11-02 00:00:24 +0000

    CAS: test: add functional tests
    Add SPDK_TEST_CAS suite
    Add basic integrity tests
    
    Change-Id: If2916558894d9f7efd9628eb99f7338c90b312b1
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -66,6 +66,11 @@ fi
 
 fail=0
 ./configure $config_params
+
+# Need to cmpile OCF files first
+# because they do not pass scanbuild
+$MAKE $MAKEFLAGS -C lib/cas/ocfenv &> /dev/null || true
+
 time $scanbuild $MAKE $MAKEFLAGS || fail=1
 if [ $fail -eq 1 ]; then
 	if [ -d $out/scan-build-tmp ]; then
