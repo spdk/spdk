1bcb9983926b3bbfdf5468ab7bc887c29f5ff352
Seth Howell <seth.howell@intel.com>
2018-11-02 11:22:35 -0700

    bdev_nvme: handle lost controller in reset
    We have handling for when pcie NVMe drives are hotplugged, but if the
    target application crashes in NVMe-oF, it is possible that we will not
    get any notification about the drives being removed from the fabric. In
    attempting to reset a "hotplugged" NVMe-oF controller, we attempt to
    read some information from it which results in admin commands being sent
    over the wire. In the case of a crashed target, these requests simply
    never get a response and block the target from shutting down cleanly.
    
    Change-Id: I6265da77d2e67454943ff2827b188fa4fb04df9b
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_transport.c
+++ lib/nvme/nvme_transport.c
@@ -110,6 +110,12 @@ nvme_transport_ctrlr_destruct(struct spdk_nvme_ctrlr *ctrlr)
 }
 
 int
+nvme_transport_ctrlr_confirm_existence(struct spdk_nvme_ctrlr *ctrlr)
+{
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_confirm_existence, (ctrlr));
+}
+
+int
 nvme_transport_ctrlr_enable(struct spdk_nvme_ctrlr *ctrlr)
 {
 	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_enable, (ctrlr));
