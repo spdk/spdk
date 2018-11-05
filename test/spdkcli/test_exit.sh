81ce14bf702a8fe18e5eae36d1edf83a7bbcfb6e
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-11-05 15:20:09 +0100

    test: Do not merge. Check if job fails.
    Change-Id: Ia4d32c661e7aa2b6e6f37ed22874ef51f6f6d13a
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/spdkcli/test_exit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CURRENT_DIR=$(readlink -f $(dirname $0))
+#. $CURRENT_DIR/../vhost/common/common.sh
+. $CURRENT_DIR/../common/autotest_common.sh
+
+function on_error_exit() {
+        echo "Error on $1 - $2"
+        print_backtrace
+        set +e
+        exit 1
+}
+
+#trap 'on_error_exit "${FUNCNAME}" "${LINENO}"' ERR
+
+fake_command
