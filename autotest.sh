14539a55a2edf3065716b29da24f364b95572a54
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-11-05 15:20:09 +0100

    test: Do not merge. Check if job fails.
    Change-Id: Ia4d32c661e7aa2b6e6f37ed22874ef51f6f6d13a
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -251,6 +251,10 @@ if [ $SPDK_TEST_RBD -eq 1 ]; then
 	run_test suite ./test/spdkcli/rbd.sh
 fi
 
+if [ $SPDK_FEDORA_PACKAGING -eq 1 ]; then
+	run_test suite ./test/spdkcli/test_exit.sh
+fi
+
 timing_enter cleanup
 autotest_cleanup
 timing_exit cleanup
