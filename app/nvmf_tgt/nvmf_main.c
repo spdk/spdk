50503c3403aee43b3d79711f238c33e6b4e128e1
Ben Walker <benjamin.walker@intel.com>
2018-11-08 16:09:56 -0700

    nvmf: Change max_delay_us to 0
    This sets the maximum tolerated additional latency
    on an I/O, which potentially allows a thread to
    sleep and save power. However, today if a poller
    is registered on the thread it will never sleep,
    which is always true for the nvmf_tgt.
    
    Further, if this were fixed such that the thread could
    sleep as long as the pollers all reported idle, this
    max latency ends up breaking the QoS test because the
    service provided is more than 10% away from the target
    number.
    
    Change-Id: Ic9decbd92ad4e0c19eb81eea18a59207d6028367
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- app/nvmf_tgt/nvmf_main.c
+++ app/nvmf_tgt/nvmf_main.c
@@ -64,7 +64,7 @@ main(int argc, char **argv)
 	/* default value in opts */
 	spdk_app_opts_init(&opts);
 	opts.name = "nvmf";
-	opts.max_delay_us = 1000; /* 1 ms */
+	opts.max_delay_us = 0;
 	if ((rc = spdk_app_parse_args(argc, argv, &opts, "", NULL,
 				      nvmf_parse_arg, nvmf_usage)) !=
 	    SPDK_APP_PARSE_ARGS_SUCCESS) {
