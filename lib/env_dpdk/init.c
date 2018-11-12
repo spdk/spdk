c1a7ea1d7599b9a960fb69110be9235304e611b7
Ziye Yang <ziye.yang@intel.com>
2018-11-12 21:03:38 +0800

    env_dpdk: set hugepage_single_segments to true as default value.
    With this default setting, it can save the time
    to start the application when the app is specifying
    the huage page memory size.
    
    Though we provide the -g parameter, but if this configuration
    is set to true as default, which could be better.
    
    Change-Id: I2d7e075cc527921398d55b1471c0e188a7032f0b
    Signed-off-by: Lizhou Gao <lizhou.gao@ucloud.cn>
    Signed-off-by: Ziye Yang <ziye.yang@intel.com>

--- lib/env_dpdk/init.c
+++ lib/env_dpdk/init.c
@@ -40,12 +40,13 @@
 #include <rte_config.h>
 #include <rte_eal.h>
 
-#define SPDK_ENV_DPDK_DEFAULT_NAME		"spdk"
-#define SPDK_ENV_DPDK_DEFAULT_SHM_ID		-1
-#define SPDK_ENV_DPDK_DEFAULT_MEM_SIZE		-1
-#define SPDK_ENV_DPDK_DEFAULT_MASTER_CORE	-1
-#define SPDK_ENV_DPDK_DEFAULT_MEM_CHANNEL	-1
-#define SPDK_ENV_DPDK_DEFAULT_CORE_MASK		"0x1"
+#define SPDK_ENV_DPDK_DEFAULT_NAME			"spdk"
+#define SPDK_ENV_DPDK_DEFAULT_SHM_ID			-1
+#define SPDK_ENV_DPDK_DEFAULT_MEM_SIZE			-1
+#define SPDK_ENV_DPDK_DEFAULT_MASTER_CORE		-1
+#define SPDK_ENV_DPDK_DEFAULT_MEM_CHANNEL		-1
+#define SPDK_ENV_DPDK_DEFAULT_CORE_MASK			"0x1"
+#define SPDK_ENV_DPDK_DEFAULT_HUGEPAGE_SINGLE_SEGEMENTS	true
 
 static char **eal_cmdline;
 static int eal_cmdline_argcount;
@@ -132,6 +133,7 @@ spdk_env_opts_init(struct spdk_env_opts *opts)
 	opts->mem_size = SPDK_ENV_DPDK_DEFAULT_MEM_SIZE;
 	opts->master_core = SPDK_ENV_DPDK_DEFAULT_MASTER_CORE;
 	opts->mem_channel = SPDK_ENV_DPDK_DEFAULT_MEM_CHANNEL;
+	opts->hugepage_single_segments = SPDK_ENV_DPDK_DEFAULT_HUGEPAGE_SINGLE_SEGEMENTS;
 }
 
 static void
