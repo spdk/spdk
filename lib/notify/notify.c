cd6c1b574c3e4ad73833dc3a976f1e1e655caf1b
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-11-14 09:36:25 +0100

    notify: add notification library
    This patch adds library that will provide ability to notify about events.
    On one side each spdk library can specify what kind of notification
    it can produce and on the other side libraries can subscribe to
    those notifications.
    
    Example is bdev module, which lib notify about added or removed bdev.
    RPC library is a consumer of those events and will send those to the
    connected clients.
    
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: Ia95e564a8a43400b2745d9de8217b9cc84cd1b16

---
+++ lib/notify/notify.c
@@ -0,0 +1,162 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/file.h>
+
+#include "spdk/stdinc.h"
+#include "spdk/notify.h"
+#include "spdk/util.h"
+#include "spdk/queue.h"
+#include "spdk/json.h"
+
+static size_t g_notify_types_count = 0;
+struct spdk_notify_type_list g_notify_types = TAILQ_HEAD_INITIALIZER(
+			g_notify_types);
+
+struct spdk_notify_client_list g_notify_clients = TAILQ_HEAD_INITIALIZER(
+			g_notify_clients);
+
+
+struct spdk_mempool *g_notify_pool;
+
+void
+spdk_notify_register_type(const char *name,
+			  spdk_notify_get_info get_object_cb, spdk_notify_get_info get_uuid_cb)
+{
+	struct spdk_notify_type *type;
+	type = calloc(1, sizeof(struct spdk_notify_type));
+	assert(type != NULL);
+
+	type->name = strdup(name);
+	assert(type->name != NULL);
+
+	TAILQ_INSERT_TAIL(&g_notify_types, type, tailq);
+	g_notify_types_count++;
+}
+
+struct spdk_notify_type *
+spdk_notify_first(void)
+{
+	struct spdk_notify_type *type;
+
+	type = TAILQ_FIRST(&g_notify_types);
+
+	return type;
+}
+
+struct spdk_notify_type *
+spdk_notify_next(struct spdk_notify_type *prev)
+{
+	struct spdk_notify_type *type;
+
+	type = TAILQ_NEXT(prev, tailq);
+
+	return type;
+}
+
+int
+spdk_notify_listen(spdk_notify_handler cb,
+		   void *ctx)
+{
+	struct spdk_notify_client *client;
+
+	client = calloc(1, sizeof(*client));
+	if (client == NULL) {
+		return -ENOMEM;
+	}
+
+	client->cb = cb;
+	client->ctx = ctx;
+	TAILQ_INSERT_TAIL(&g_notify_clients, client, tailq);
+	return 0;
+}
+
+int
+spdk_notify_unlisten(spdk_notify_handler cb,
+		     void *ctx)
+{
+	struct spdk_notify_client *client;
+
+	TAILQ_FOREACH(client, &g_notify_clients, tailq) {
+		if (client->cb == cb && client->ctx == ctx) {
+			TAILQ_REMOVE(&g_notify_clients, client, tailq);
+			free(client);
+			return 0;
+		}
+
+	}
+	return -ENOENT;
+}
+
+void
+spdk_notify_send(struct spdk_notify *notify)
+{
+	struct spdk_notify_client *client;
+
+	client = TAILQ_FIRST(&g_notify_clients);
+	TAILQ_FOREACH(client, &g_notify_clients, tailq) {
+		client->cb(notify, client->ctx);
+	}
+}
+
+struct spdk_notify *
+spdk_nofify_alloc(void)
+{
+	struct spdk_notify *notify;
+
+	notify = calloc(1, sizeof(*notify));
+	return notify;
+}
+
+void
+spdk_nofify_get(struct spdk_notify *notify)
+{
+	notify->refcnt++;
+}
+
+void
+spdk_nofify_put(struct spdk_notify *notify)
+{
+	assert(notify != NULL);
+	assert(notify->refcnt > 0);
+
+	notify->refcnt--;
+	if (notify->refcnt == 0) {
+		free(notify);
+	}
+}
+
+void spdk_notify_write_json(struct spdk_json_write_ctx *w, struct spdk_notify *notify)
+{
+	notify->write_json_cb(w, notify, notify->ctx);
+}
