c6fb6a5349891fc22460b59b7b7fcca509cd59b5
Liu Xiaodong <xiaodong.liu@intel.com>
2018-11-14 22:01:51 -0500

    lib/trace: add trace_record tool
    trace_record is used to poll the spdk trace shm file
    and store new entries from it to another specified trace file.
    This could help retain the trace_entires from the overlay of
    trace circular buffer
    
    A usage sample is:
    ./spdk_trace_record -s bdev_svc -p <spdk app pid> -f trace.tmp -q
    
    Change-Id: If073a05022ec9c1b45923c38ba407a873be8741b
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>

--- test/iscsi_tgt/iscsi_tgt.sh
+++ test/iscsi_tgt/iscsi_tgt.sh
@@ -33,6 +33,7 @@ run_test suite ./test/iscsi_tgt/lvol/iscsi_lvol.sh
 run_test suite ./test/iscsi_tgt/fio/fio.sh
 run_test suite ./test/iscsi_tgt/qos/qos.sh
 run_test suite ./test/iscsi_tgt/ip_migration/ip_migration.sh
+run_test suite ./test/iscsi_tgt/trace_record/trace_record.sh
 
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	if [ $SPDK_TEST_PMDK -eq 1 ]; then
