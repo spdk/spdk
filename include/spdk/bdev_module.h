10a88aafd5032b478bedb21c7dc88d51f49cb05c
Piotr Pelplinski <piotr.pelplinski@intel.com>
2018-11-19 14:31:19 +0100

    histograms: add histograms to bdev layer
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I677f0b368447fc928cba318c2771e64156ace416

--- include/spdk/bdev_module.h
+++ include/spdk/bdev_module.h
@@ -356,6 +356,10 @@ struct spdk_bdev {
 
 		/** accumulated I/O statistics for previously deleted channels of this bdev */
 		struct spdk_bdev_io_stat stat;
+
+		/** histogram enabled on this bdev */
+		bool	histogram_enabled;
+		bool	histogram_in_progress;
 	} internal;
 };
 
