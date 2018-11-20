c2b793a31134f011ae4cadc5addf627fca019555
Jim Harris <james.r.harris@intel.com>
2018-11-20 14:28:09 -0700

    build: remove xx_MODULES_LINKER_ARGS and xx_MODULES_FILES
    Application Makefiles can now just add xx_MODULES_LIB_LIST
    to SPDK_LIB_LIST.  This is possible now since all
    SPDK libraries are linked with --whole-archive, so there
    is no need to differentiate between "modules" libraries
    and other SPDK libraries.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Iabf81a388b72d3b2a2f48287a8491ddc977722ac

--- lib/rocksdb/spdk.rocksdb.mk
+++ lib/rocksdb/spdk.rocksdb.mk
@@ -53,10 +53,10 @@ ifeq ($(CONFIG_ASAN),y)
 CXXFLAGS += -fno-sanitize=address
 endif
 
-SPDK_LIB_LIST = event_bdev event_copy
+SPDK_LIB_LIST = $(ALL_MODULES_LIST)
+SPDK_LIB_LIST += event_bdev event_copy
 SPDK_LIB_LIST += bdev copy event util conf trace log jsonrpc json rpc sock thread
 
-AM_LINK += $(ALL_MODULES_LINKER_ARGS)
 AM_LINK += $(SPDK_LIB_LINKER_ARGS) $(ENV_LINKER_ARGS)
 AM_LINK += $(SYS_LIBS)
 
