997b3f4af11ba11d39c0b9d4b15d4655ca03c2d6
Jim Harris <james.r.harris@intel.com>
2018-11-20 15:11:50 -0700

    build: add mk/spdk.app_vars.mk
    This is a step towards moving a lot of the boilerplate
    rules in various application Makefiles into mk/spdk.app.mk.
    We will need to add a mk/spdk.app_cxx.mk for our C++
    applications, but that will need to share these same
    variables.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Iaee73a4fa47ff2ee54f1a1ea3c0d2097900df218

--- mk/spdk.app_vars.mk
+++ mk/spdk.app_vars.mk
@@ -31,6 +31,20 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-include $(SPDK_ROOT_DIR)/mk/spdk.app_vars.mk
+SPDK_LIB_FILES = $(call spdk_lib_list_to_static_libs,$(SPDK_LIB_LIST))
+SPDK_LIB_LINKER_ARGS = \
+	-L$(SPDK_ROOT_DIR)/build/lib \
+	-Wl,--whole-archive \
+	$(SPDK_LIB_LIST:%=-lspdk_%) \
+	-Wl,--no-whole-archive
 
-install: all
+# This is primarily used for unit tests to ensure they link when shared library
+# build is enabled.  Shared libraries can't get their mock implementation from
+# the unit test file.  Note that even for unittests, we must include the mock
+# library with whole-archive, to keep its functions from getting stripped out
+# when LTO is enabled.
+SPDK_STATIC_LIB_LINKER_ARGS = \
+	$(SPDK_LIB_LIST:%=$(SPDK_ROOT_DIR)/build/lib/libspdk_%.a) \
+	-Wl,--whole-archive \
+	$(SPDK_ROOT_DIR)/build/lib/libspdk_ut_mock.a \
+	-Wl,--no-whole-archive
