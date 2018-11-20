d18657629f61434bfee0777fac187c8dc71cb011
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2018-11-20 15:05:13 +0100

    pci: keep device information in the spdk_pci_device struct
    It's handy to store the SPDK structs within the device
    structure. The subsequent patch will make us use
    spdk_pci_addr much more frequently, so it makes sense
    to keep it around rather than build it up from rte_pci_addr
    everytime.
    
    The upcoming VMD driver will also benefit from this patch
    by being able to fill the spdk_pci_device struct with any
    custom PCI details.
    
    Change-Id: I236a19e28beba9a593b29f23b79b1b0b92ef1fa7
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- lib/env_dpdk/env_internal.h
+++ lib/env_dpdk/env_internal.h
@@ -73,6 +73,9 @@ extern struct rte_pci_bus rte_pci_bus;
 
 struct spdk_pci_device {
 	struct rte_pci_device		*dev_handle;
+	struct spdk_pci_addr		addr;
+	struct spdk_pci_id		id;
+	int				socket_id;
 	bool				attached;
 	TAILQ_ENTRY(spdk_pci_device)	tailq;
 };
