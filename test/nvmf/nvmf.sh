347539c05a6b8d0391250017efeaacda311ae744
Ben Walker <benjamin.walker@intel.com>
2018-11-21 10:22:19 -0700

    nvmf: Pre-allocate memory in all tests.
    Change-Id: I4ae4275bfedeb53f6e2b0189ce22a206f20b446b
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/nvmf/nvmf.sh
+++ test/nvmf/nvmf.sh
@@ -22,7 +22,7 @@ start_stub "-s 2048 -i 0 -m $NVMF_TEST_CORE_MASK"
 trap "kill_stub; exit 1" SIGINT SIGTERM EXIT
 
 export NVMF_APP_SHM_ID="0"
-export NVMF_APP="./app/nvmf_tgt/nvmf_tgt -i $NVMF_APP_SHM_ID -e 0xFFFF"
+export NVMF_APP="./app/nvmf_tgt/nvmf_tgt -s 512 -i $NVMF_APP_SHM_ID -e 0xFFFF"
 
 run_test suite test/nvmf/filesystem/filesystem.sh
 run_test suite test/nvmf/discovery/discovery.sh
