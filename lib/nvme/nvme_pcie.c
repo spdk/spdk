b4bff4cff5083375fff1973d8595d70801de4138
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2018-11-22 17:19:47 +0100

    nvme/pcie: don't allow constructing a controller from secondary process
    With various possibilities to leak the rte_pci_device in the
    primary process, we could technically construct the controller
    in secondary. The nvme stack is not prepared for this and
    will fail to initialize the device, but nevertheless will leak
    the newly created device.
    
    This patch adds an extra check to prevent any controller from
    being constructed in secondary process. This will get handy
    with the upcoming DPDK 18.11 hotplug functionality.
    
    Change-Id: I772f42b541c5db53310362b6595cebf9a30e8491
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- lib/nvme/nvme_pcie.c
+++ lib/nvme/nvme_pcie.c
@@ -703,15 +703,16 @@ pcie_nvme_enum_cb(void *ctx, struct spdk_pci_device *pci_dev)
 	trid.trtype = SPDK_NVME_TRANSPORT_PCIE;
 	spdk_pci_addr_fmt(trid.traddr, sizeof(trid.traddr), &pci_addr);
 
-	/* Verify that this controller is not already attached */
 	ctrlr = spdk_nvme_get_ctrlr_by_trid_unsafe(&trid);
-	if (ctrlr) {
-		if (spdk_process_is_primary()) {
-			/* Already attached */
-			return 0;
-		} else {
-			return nvme_ctrlr_add_process(ctrlr, pci_dev);
+	if (!spdk_process_is_primary()) {
+		if (!ctrlr) {
+			/* We're trying to construct a controller from
+			 * a secondary process, which is not supported yet.
+			 */
+			return -1;
 		}
+
+		return nvme_ctrlr_add_process(ctrlr, pci_dev);
 	}
 
 	/* check whether user passes the pci_addr */
