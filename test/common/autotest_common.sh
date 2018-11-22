b53c15b9cbfa7bbb91ae5037292175ee521e7acf
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2018-11-22 09:34:58 +0100

    [debug] freebsd waitforlisten
    Change-Id: I37f6a32b1affe6a100f0494f886205c3c437f48e
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -323,9 +323,16 @@ function waitforlisten() {
 	echo "Waiting for process to start up and listen on UNIX domain socket $rpc_addr..."
 	# turn off trace for this loop
 	local shell_restore_x="$( [[ "$-" =~ x ]] && echo 'set -x' )"
-	set +x
-	local ret=0
 
+	if [[ "$(uname -s)" != "Linux" ]]; then
+		local is_linux=false
+		set -x
+	else
+		local is_linux=true
+		set -x
+	fi
+
+	local ret=0
 	while sleep 0.5; do
 		# if the process is no longer running, then exit the script
 		#  since it means the application crashed
@@ -347,6 +354,12 @@ function waitforlisten() {
 				break
 			fi
 		else
+			if ! $is_linux; then
+				echo "=== waitforlisten: DEBUG START ==="
+				$ns_cmd netstat -an
+				echo "=== waitforlisten: DEBUG END ==="
+			fi
+
 			# if system doesn't have ss, just assume it has netstat
 			if $ns_cmd netstat -an | grep -iw LISTENING | egrep -q "\s+$rpc_addr\$"; then
 				break
