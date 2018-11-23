6068ad307e97e08a4b30b6b6f1673cbc956c17a1
Changpeng Liu <changpeng.liu@intel.com>
2018-11-23 00:14:25 -0500

    scsi: add persistent reservation out release support
    Change-Id: If0df3b3fc4e89ffa78d827e2b31cf5c61aa149db
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/scsi/scsi_bdev.c
+++ lib/scsi/scsi_bdev.c
@@ -1017,6 +1017,62 @@ conflict:
 }
 
 static int
+spdk_scsi_pr_out_release(struct spdk_scsi_task *task,
+			 enum spdk_scsi_pr_type_code type, uint64_t rkey)
+{
+	struct spdk_scsi_lun *lun = task->lun;
+	struct spdk_scsi_dev *dev = lun->dev;
+	struct spdk_scsi_pr_registrant *reg;
+	bool all_regs = false;
+
+	SPDK_DEBUGLOG(SPDK_LOG_SCSI, "PR OUT RELEASE: rkey 0x%"PRIx64", "
+		      "reservation type %u\n", rkey, type);
+
+	reg = spdk_scsi_pr_get_registrant(dev, task->initiator_port, task->target_port);
+	if (!reg) {
+		SPDK_ERRLOG("RELEASE: no registration\n");
+		return -EINVAL;
+	}
+
+	/* no reservation holder */
+	if (!dev->holder) {
+		SPDK_DEBUGLOG(SPDK_LOG_SCSI, "RELEASE: no reservation holder\n");
+		return 0;
+	}
+
+	if (dev->type != type || rkey != dev->crkey) {
+		SPDK_ERRLOG("RELEASE: type or reservation key doesn't match\n");
+		goto check_condition;
+	}
+
+	if (dev->type == SPDK_SCSI_PR_WRITE_EXCLUSIVE_ALL_REGS ||
+	    dev->type == SPDK_SCSI_PR_EXCLUSIVE_ACCESS_ALL_REGS) {
+		all_regs = true;
+	}
+	/* I_T nexus is not a persistent reservation holder */
+	if (!all_regs && (dev->holder != reg)) {
+		SPDK_DEBUGLOG(SPDK_LOG_SCSI, "RELEASE: current I_T nexus is not holder\n");
+		return 0;
+	}
+
+	/* TODO: Unit Attention to other initiator ports for
+	 * type registrants only or all registrants
+	 */
+	dev->type = 0;
+	dev->crkey = 0;
+	dev->holder = NULL;
+
+	return 0;
+
+check_condition:
+	spdk_scsi_task_set_status(task, SPDK_SCSI_STATUS_CHECK_CONDITION,
+				  SPDK_SCSI_SENSE_ILLEGAL_REQUEST,
+				  SPDK_SCSI_ASC_INVALID_FIELD_IN_CDB,
+				  SPDK_SCSI_ASCQ_CAUSE_NOT_REPORTABLE);
+	return -EINVAL;
+}
+
+static int
 spdk_scsi_pr_out(struct spdk_scsi_task *task,
 		 uint8_t *cdb, uint8_t *data,
 		 uint16_t data_len)
@@ -1063,6 +1119,9 @@ spdk_scsi_pr_out(struct spdk_scsi_task *task,
 	case SPDK_SCSI_PR_OUT_RESERVE:
 		rc = spdk_scsi_pr_out_reserve(task, type, rkey,
 					      spec_i_pt, all_tg_pt, aptpl);
+		break;
+	case SPDK_SCSI_PR_OUT_RELEASE:
+		rc = spdk_scsi_pr_out_release(task, type, rkey);
 		break;
 	default:
 		SPDK_ERRLOG("Invalid service action code %u\n", action);
