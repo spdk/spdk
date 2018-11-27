c35662df9bc91f1592b6a1751b732f3453110bd7
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-11-27 13:58:25 -0500

    test/spdkcli: Run different parts of tests with given spdk target.
    Use nvmf_tgt for nvmf tests.
    Use iscsi_tgt for spdkcli iscsi tests.
    Use vhost target for spdkcli vhost tests.
    Fixes #511.
    
    Change-Id: I5f084bccf87d32b0ede210c62ccc4a2f7926d716
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- scripts/spdkcli/ui_node.py
+++ scripts/spdkcli/ui_node.py
@@ -804,7 +804,7 @@ class UIVhostBlk(UIVhost):
 
     def refresh(self):
         self._children = set([])
-        for ctrlr in self.get_root().get_vhost_ctrlrs(self.name):
+        for ctrlr in self.get_root().get_vhost_controllers(ctrlr_type=self.name):
             UIVhostBlkCtrlObj(ctrlr, self)
 
     def ui_command_create(self, name, bdev, cpumask=None, readonly=False):
@@ -838,7 +838,7 @@ class UIVhostScsi(UIVhost):
 
     def refresh(self):
         self._children = set([])
-        for ctrlr in self.get_root().get_vhost_ctrlrs(self.name):
+        for ctrlr in self.get_root().get_vhost_controllers(ctrlr_type=self.name):
             UIVhostScsiCtrlObj(ctrlr, self)
 
     def ui_command_create(self, name, cpumask=None):
@@ -898,7 +898,7 @@ class UIVhostScsiCtrlObj(UIVhostCtrl):
         try:
             self.get_root().remove_vhost_scsi_target(ctrlr=self.ctrlr.ctrlr,
                                                      scsi_target_num=int(target_num))
-            for ctrlr in self.get_root().get_vhost_ctrlrs("scsi"):
+            for ctrlr in self.get_root().get_vhost_controllers(ctrlr_type="scsi"):
                 if ctrlr.ctrlr == self.ctrlr.ctrlr:
                     self.ctrlr = ctrlr
         except JSONRPCException as e:
@@ -921,7 +921,7 @@ class UIVhostScsiCtrlObj(UIVhostCtrl):
             self.get_root().add_vhost_scsi_lun(ctrlr=self.ctrlr.ctrlr,
                                                scsi_target_num=int(target_num),
                                                bdev_name=bdev_name)
-            for ctrlr in self.get_root().get_vhost_ctrlrs("scsi"):
+            for ctrlr in self.get_root().get_vhost_controllers(ctrlr_type="scsi"):
                 if ctrlr.ctrlr == self.ctrlr.ctrlr:
                     self.ctrlr = ctrlr
         except JSONRPCException as e:
--- test/spdkcli/common.sh
+++ test/spdkcli/common.sh
@@ -7,7 +7,18 @@ spdkcli_job="$SPDKCLI_BUILD_DIR/test/spdkcli/spdkcli_job.py"
 
 function on_error_exit() {
 	set +e
-	killprocess $spdk_tgt_pid
+	if [ ! -z $spdk_tgt_pid ]; then
+		killprocess $spdk_tgt_pid
+	fi
+	if [ ! -z $nvmf_tgt_pid ]; then
+		killprocess $nvmf_tgt_pid
+	fi
+	if [ ! -z $iscsi_tgt_pid ]; then
+		killprocess $iscsi_tgt_pid
+	fi
+	if [ ! -z $vhost_tgt_pid ]; then
+		killprocess $vhost_tgt_pid
+	fi
 	rm -f $testdir/${MATCH_FILE} $testdir/match_files/spdkcli_details_vhost.test /tmp/sample_aio /tmp/sample_pmem
 	print_backtrace
 	exit 1
@@ -17,6 +28,24 @@ function run_spdk_tgt() {
 	$SPDKCLI_BUILD_DIR/app/spdk_tgt/spdk_tgt -m 0x3 -p 0 -s 4096 &
 	spdk_tgt_pid=$!
 	waitforlisten $spdk_tgt_pid
+}
+
+function run_nvmf_tgt() {
+	$SPDKCLI_BUILD_DIR/app/nvmf_tgt/nvmf_tgt -m 0x3 -p 0 -s 4096 &
+	nvmf_tgt_pid=$!
+	waitforlisten $nvmf_tgt_pid
+}
+
+function run_iscsi_tgt() {
+	$SPDKCLI_BUILD_DIR/app/iscsi_tgt/iscsi_tgt -m 0x3 -p 0 -s 4096 &
+	iscsi_tgt_pid=$!
+	waitforlisten $iscsi_tgt_pid
+}
+
+function run_vhost_tgt() {
+	$SPDKCLI_BUILD_DIR/app/vhost/vhost -m 0x3 -p 0 -s 4096 &
+	vhost_tgt_pid=$!
+	waitforlisten $vhost_tgt_pid
 }
 
 function check_match() {
