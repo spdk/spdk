a6471791ffb7060a765834c56f2fb283d6519904
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-11-27 14:23:38 +0900

    dif: Generate DIF for extended LBA payload
    DIF and DIX will be supported in SPDK throughout, e.g., NVMe
    driver, NVMe-oF initiator and target, NVMe block device, malloc
    block device, SCSI, iSCSI target, FIO plugin, and Perf.
    
    Generic and common APIs to generate and verify DIF and inject
    bit flip error to any field will be helpful for them.
    
    This patch is the first in the patch series.
    
    This patch adds APIs to generate and verify DIF for SGL extended
    LBA payload as byte alignement and granularity.
    
    Change-Id: Ie6588d960113761f10efbf2d2a3cae004af37ce8
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

---
+++ lib/util/dif.c
@@ -0,0 +1,462 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/dif.h"
+#include "spdk/crc16.h"
+#include "spdk/endian.h"
+#include "spdk/log.h"
+#include "spdk/util.h"
+
+static bool
+_are_iovs_bytes_multiple(struct iovec *iovs, int iovcnt, uint32_t bytes)
+{
+	int i;
+
+	for (i = 0; i < iovcnt; i++) {
+		if (iovs[i].iov_len % bytes) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+static uint32_t
+_dif_get_guard_interval(uint32_t block_size, uint32_t md_size, bool dif_start)
+{
+	if (dif_start) {
+		/* For metadata formats with more than 8 bytes, if the DIF is
+		 * contained in the last 8 bytes of metadata, then the CRC
+		 * covers all metadata up to but excluding these last 8 bytes.
+		 */
+		return block_size - sizeof(struct spdk_dif);
+	} else {
+		/* For metadata formats with more than 8 bytes, if the DIF is
+		 * contained in the first 8 bytes of metadata, then the CRC
+		 * does not cover any metadata.
+		 */
+		return block_size - md_size;
+	}
+}
+
+static void
+_dif_generate(void *_dif, void *data_buf, uint32_t guard_interval,
+	      uint32_t dif_flags, uint32_t ref_tag, uint16_t app_tag)
+{
+	struct spdk_dif *dif = _dif;
+	uint16_t guard;
+
+	if (dif_flags & SPDK_DIF_GUARD_CHECK) {
+		/* Compute CRC over the logical block data. */
+		guard = spdk_crc16_t10dif(data_buf, guard_interval);
+		to_be16(&dif->guard, guard);
+	}
+
+	if (dif_flags & SPDK_DIF_APPTAG_CHECK) {
+		to_be16(&dif->app_tag, app_tag);
+	}
+
+	if (dif_flags & SPDK_DIF_REFTAG_CHECK) {
+		to_be32(&dif->ref_tag, ref_tag);
+	}
+}
+
+static int
+dif_generate(struct iovec *iovs, int iovcnt, uint32_t block_size,
+	     uint32_t guard_interval, int dif_type, uint32_t dif_flags,
+	     uint32_t init_ref_tag, uint16_t app_tag)
+{
+	uint32_t iov_offset, ref_tag;
+	int iovpos;
+	void *buf;
+
+	iov_offset = 0;
+	iovpos = 0;
+	ref_tag = init_ref_tag;
+
+	while (iovpos < iovcnt) {
+		buf = iovs[iovpos].iov_base + iov_offset;
+
+		_dif_generate(buf + guard_interval, buf, guard_interval,
+			      dif_flags, ref_tag, app_tag);
+
+		/* For type 1 and 2, the reference tag is incremented for each
+		 * subsequent logical block. For type 3, the reference tag is
+		 * remains the same as the initial reference tag.
+		 */
+		if (dif_type != 3) {
+			ref_tag++;
+		}
+
+		iov_offset += block_size;
+		if (iov_offset == iovs[iovpos].iov_len) {
+			iovpos++;
+			iov_offset = 0;
+		}
+	}
+
+	return 0;
+}
+
+static int
+dif_generate_split(struct iovec *iovs, int iovcnt, uint32_t block_size,
+		   uint32_t guard_interval, int dif_type, uint32_t dif_flags,
+		   uint32_t init_ref_tag, uint16_t app_tag)
+{
+	uint32_t payload_offset, offset_blocks, offset_in_block;
+	uint32_t iov_offset, buf_len, ref_tag;
+	int iovpos;
+	void *buf;
+	uint8_t *contig_buf, contig_dif[sizeof(struct spdk_dif)] = {0};
+
+	contig_buf = calloc(1, guard_interval);
+	if (!contig_buf) {
+		SPDK_ERRLOG("calloc() failed for contiguous data block buffer.\n");
+		return -ENOMEM;
+	}
+
+	payload_offset = 0;
+	iov_offset = 0;
+	iovpos = 0;
+
+	while (iovpos < iovcnt) {
+		offset_blocks = payload_offset / block_size;
+		offset_in_block = payload_offset % block_size;
+
+		/* For type 1 and 2, the reference tag is incremented for each
+		 * subsequent logical block. For type 3, the reference tag is
+		 * remains the same as the initial reference tag.
+		 */
+		if (dif_type != 3) {
+			ref_tag = init_ref_tag + offset_blocks;
+		} else {
+			ref_tag = init_ref_tag;
+		}
+
+		buf = iovs[iovpos].iov_base + iov_offset;
+		buf_len = iovs[iovpos].iov_len - iov_offset;
+
+		/* Logical block data over which CRC is computed. */
+		if (offset_in_block < guard_interval) {
+			buf_len = spdk_min(guard_interval - offset_in_block, buf_len);
+			if (dif_flags & SPDK_DIF_GUARD_CHECK) {
+				/* Copy the split logical block data to the temporary
+				 * contiguous buffer to compute CRC.
+				 */
+				memcpy(&contig_buf[offset_in_block], buf, buf_len);
+			}
+
+			if (offset_in_block + buf_len == guard_interval) {
+				/* If a whole logical block data is parsed, generate DIF and
+				 * save it to the temporary contiguous buffer.
+				 */
+				_dif_generate(contig_dif, contig_buf, guard_interval,
+					      dif_flags, ref_tag, app_tag);
+			}
+
+		/* DIF field located to the logical block data. */
+		} else if (offset_in_block < guard_interval + sizeof(struct spdk_dif)) {
+			/* Copy generated DIF to the split DIF field. */
+			buf_len = spdk_min(guard_interval + sizeof(struct spdk_dif) - offset_in_block,
+					   buf_len);
+			memcpy(buf, &contig_dif[offset_in_block - guard_interval], buf_len);
+
+		/* Metadata field after DIF field when metadata size is more than 8 bytes. */
+		} else {
+			buf_len = spdk_min(block_size - offset_in_block, buf_len);
+		}
+
+		payload_offset += buf_len;
+		iov_offset += buf_len;
+		if (iov_offset == iovs[iovpos].iov_len) {
+			iovpos++;
+			iov_offset = 0;
+		}
+	}
+
+	free(contig_buf);
+	return 0;
+}
+
+int
+spdk_dif_generate(struct iovec *iovs, int iovcnt, uint32_t block_size,
+		  uint32_t md_size, bool dif_start, int dif_type,
+		  uint32_t dif_flags, uint32_t init_ref_tag, uint16_t app_tag)
+{
+	uint32_t guard_interval;
+
+	if (md_size == 0) {
+		return -EINVAL;
+	}
+
+	guard_interval = _dif_get_guard_interval(block_size, md_size, dif_start);
+
+	if (_are_iovs_bytes_multiple(iovs, iovcnt, block_size)) {
+		return dif_generate(iovs, iovcnt, block_size, guard_interval,
+				    dif_type, dif_flags, init_ref_tag, app_tag);
+	} else {
+		return dif_generate_split(iovs, iovcnt, block_size, guard_interval,
+					  dif_type, dif_flags, init_ref_tag, app_tag);
+	}
+}
+
+static int
+_dif_verify(void *_dif, void *data_buf, uint32_t guard_interval, int dif_type,
+	    uint32_t dif_flags, uint32_t ref_tag, uint16_t apptag_mask, uint16_t app_tag)
+{
+	struct spdk_dif *dif = _dif;
+	uint16_t guard, _guard, _app_tag;
+	uint32_t _ref_tag;
+
+	switch (dif_type) {
+	case 1:
+	case 2:
+		/* If Type 1 or 2 is used, then all DIF checks are disabled when
+		 * the Application Tag is 0xFFFF.
+		 */
+		if (dif->app_tag == 0xFFFF) {
+			return 0;
+		}
+		break;
+	case 3:
+		/* If Type 3 is used, then all DIF checks are disabled when the
+		 * Application Tag is 0xFFFF and the Reference Tag is 0xFFFFFFFF.
+		 */
+		if (dif->app_tag == 0xFFFF && dif->ref_tag == 0xFFFFFFFF) {
+			return 0;
+		}
+	}
+
+	if (dif_flags & SPDK_DIF_GUARD_CHECK) {
+		/* Compare the DIF Guard field to the CRC computed over the logical
+		 * block data.
+		 */
+		guard = spdk_crc16_t10dif(data_buf, guard_interval);
+		_guard = from_be16(&dif->guard);
+		if (_guard != guard) {
+			SPDK_ERRLOG("Failed to compare Guard: LBA=%" PRIu32 "," \
+				    "  Expected=%x, Actual=%x\n",
+				    ref_tag, _guard, guard);
+			return -1;
+		}
+	}
+
+	if (dif_flags & SPDK_DIF_APPTAG_CHECK) {
+		/* Compare unmasked bits in the DIF Application Tag field to the
+		 * passed Application Tag.
+		 */
+		_app_tag = from_be16(&dif->app_tag);
+		if ((_app_tag & apptag_mask) != app_tag) {
+			SPDK_ERRLOG("Failed to compare App Tag: LBA=%" PRIu32 "," \
+				    "  Expected=%x, Actual=%x\n",
+				    ref_tag, app_tag, (_app_tag & apptag_mask));
+			return -1;
+		}
+	}
+
+	if (dif_flags & SPDK_DIF_REFTAG_CHECK) {
+		switch (dif_type) {
+		case 1:
+		case 2:
+			/* Compare the DIF Reference Tag field to the passed Reference Tag.
+			 * The passed Reference Tag will be the least significant 4 bytes
+			 * of the LBA when Type 1 is used, and application specific value
+			 * if Type 2 is used, */
+			_ref_tag = from_be32(&dif->ref_tag);
+			if (_ref_tag != ref_tag) {
+				SPDK_ERRLOG("Failed to compare Ref Tag: LBA=%" PRIu32 "," \
+					    " Expected=%x, Actual=%x\n",
+					    ref_tag, ref_tag, _ref_tag);
+				return -1;
+			}
+			break;
+		case 3:
+			/* If Type 3 is used, ignore this check for now. */
+			break;
+		}
+	}
+
+	return 0;
+}
+
+static int
+dif_verify(struct iovec *iovs, int iovcnt, uint32_t block_size,
+	   uint32_t guard_interval, int dif_type, uint32_t dif_flags,
+	   uint32_t init_ref_tag, uint16_t apptag_mask, uint16_t app_tag)
+{
+	uint32_t iov_offset, ref_tag;
+	int iovpos, rc;
+	void *buf;
+
+	ref_tag = init_ref_tag;
+	iov_offset = 0;
+	iovpos = 0;
+
+	while (iovpos < iovcnt) {
+		buf = iovs[iovpos].iov_base + iov_offset;
+
+		rc = _dif_verify(buf + guard_interval, buf, guard_interval,
+				 dif_type, dif_flags, ref_tag, apptag_mask, app_tag);
+		if (rc != 0) {
+			return rc;
+		}
+
+		/* For type 1 and 2, the reference tag is incremented for each
+		 * subsequent logical block. For type 3, the reference tag is
+		 * remains the same as the initial reference tag.
+		 */
+		if (dif_type != 3) {
+			ref_tag++;
+		}
+
+		iov_offset += block_size;
+		if (iov_offset == iovs[iovpos].iov_len) {
+			iovpos++;
+			iov_offset = 0;
+		}
+	}
+
+	return 0;
+}
+
+static int
+dif_verify_split(struct iovec *iovs, int iovcnt, uint32_t block_size,
+		 uint32_t guard_interval, int dif_type, uint32_t dif_flags,
+		 uint32_t init_ref_tag, uint16_t apptag_mask, uint16_t app_tag)
+{
+	uint32_t payload_offset, offset_blocks, offset_in_block;
+	uint32_t iov_offset, buf_len, ref_tag;
+	int iovpos, rc;
+	void *buf;
+	uint8_t *contig_buf, contig_dif[8] = {0};
+
+	contig_buf = calloc(1, guard_interval);
+	if (!contig_buf) {
+		SPDK_ERRLOG("calloc() failed for contiguous data block buffer\n");
+		return -ENOMEM;
+	}
+
+	payload_offset = 0;
+	iov_offset = 0;
+	iovpos = 0;
+
+	while (iovpos < iovcnt) {
+		offset_blocks = payload_offset / block_size;
+		offset_in_block = payload_offset % block_size;
+
+		/* For type 1 and 2, the reference tag is incremented for each
+		 * subsequent logical block. For type 3, the reference tag is
+		 * remains the same as the initial reference tag.
+		 */
+		if (dif_type != 3) {
+			ref_tag = init_ref_tag + offset_blocks;
+		} else {
+			ref_tag = init_ref_tag;
+		}
+
+		buf = iovs[iovpos].iov_base + iov_offset;
+		buf_len = iovs[iovpos].iov_len - iov_offset;
+
+		/* Logical block data over which CRC is computed. */
+		if (offset_in_block < guard_interval) {
+			buf_len = spdk_min(guard_interval - offset_in_block, buf_len);
+			if (dif_flags & SPDK_DIF_GUARD_CHECK) {
+				/* Copy the split logical block data to the temporary
+				 * contiguous buffer to compute CRC.
+				 */
+				memcpy(&contig_buf[offset_in_block], buf, buf_len);
+			}
+
+		/* DIF field located to the logical block data. */
+		} else if (offset_in_block < guard_interval + sizeof(struct spdk_dif)) {
+			buf_len = spdk_min(guard_interval + sizeof(struct spdk_dif) - offset_in_block,
+					   buf_len);
+			/* Copy the split DIF field to the temporary contiguous buffer */
+			memcpy(&contig_dif[offset_in_block - guard_interval], buf, buf_len);
+			if (offset_in_block + buf_len == guard_interval + sizeof(struct spdk_dif)) {
+				/* If a whole DIF field is parsed, compare the parsed DIF field
+				 * and computed or passed values.
+				 */
+				rc = _dif_verify(contig_dif, contig_buf, guard_interval,
+						 dif_type, dif_flags, ref_tag, apptag_mask, app_tag);
+				if (rc != 0) {
+					free(contig_buf);
+					return rc;
+				}
+			}
+
+		/* Metadata field after DIF field when metadata size is more than 8 bytes. */
+		} else {
+			buf_len = spdk_min(buf_len, block_size - offset_in_block);
+		}
+
+		payload_offset += buf_len;
+		iov_offset += buf_len;
+		if (iov_offset == iovs[iovpos].iov_len) {
+			iovpos++;
+			iov_offset = 0;
+		}
+	}
+
+	free(contig_buf);
+	return 0;
+}
+
+int
+spdk_dif_verify(struct iovec *iovs, int iovcnt, uint32_t block_size,
+		uint32_t md_size, bool dif_start, int dif_type, uint32_t dif_flags,
+		uint32_t init_ref_tag, uint16_t apptag_mask, uint16_t app_tag)
+{
+	uint32_t guard_interval;
+
+	if (md_size == 0) {
+		SPDK_ERRLOG("Metadata size is 0\n");
+		return -EINVAL;
+	}
+
+	if (dif_type != 1 && dif_type != 2 && dif_type != 3) {
+		SPDK_ERRLOG("DIF Type is %d and not 1, 2, or 3\n", dif_type);
+		return -EINVAL;
+	}
+
+	guard_interval = _dif_get_guard_interval(block_size, md_size, dif_start);
+
+	if (_are_iovs_bytes_multiple(iovs, iovcnt, block_size)) {
+		return dif_verify(iovs, iovcnt, block_size, guard_interval, dif_type,
+				  dif_flags, init_ref_tag, apptag_mask, app_tag);
+	} else {
+		return dif_verify_split(iovs, iovcnt, block_size, guard_interval,
+					dif_type, dif_flags, init_ref_tag,
+					apptag_mask, app_tag);
+	}
+}
---
+++ test/unit/lib/util/dif.c/dif_ut.c
@@ -0,0 +1,483 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "spdk_cunit.h"
+
+#include "util/dif.c"
+
+#define DATA_PATTERN	0xAB
+
+static void
+_data_pattern_generate(struct iovec *iovs, int iovcnt,
+		       uint32_t block_size, uint32_t md_size)
+{
+	uint32_t payload_offset, offset_in_block;
+	uint32_t iov_offset, buf_len;
+	int iovpos;
+	void *buf;
+
+	payload_offset = 0;
+	iov_offset = 0;
+	iovpos = 0;
+
+	while (iovpos < iovcnt) {
+		offset_in_block = payload_offset % block_size;
+
+		buf = iovs[iovpos].iov_base + iov_offset;
+		buf_len = iovs[iovpos].iov_len - iov_offset;
+
+		if (offset_in_block < block_size - md_size) {
+			buf_len = spdk_min(block_size - md_size - offset_in_block,
+					   buf_len);
+			memset(buf, DATA_PATTERN, buf_len);
+		} else {
+			buf_len = spdk_min(block_size - offset_in_block, buf_len);
+			memset(buf, 0, buf_len);
+		}
+
+		payload_offset += buf_len;
+		iov_offset += buf_len;
+		if (iov_offset == iovs[iovpos].iov_len) {
+			iovpos++;
+			iov_offset = 0;
+		}
+	}
+}
+
+static int
+_data_pattern_verify(struct iovec *iovs, int iovcnt,
+		     uint32_t block_size, uint32_t md_size)
+{
+	uint32_t payload_offset, offset_in_block;
+	uint32_t iov_offset, buf_len, i;
+	int iovpos;
+	uint8_t *buf;
+
+	payload_offset = 0;
+	iov_offset = 0;
+	iovpos = 0;
+
+	while (iovpos < iovcnt) {
+		offset_in_block = payload_offset % block_size;
+
+		buf = (uint8_t *)(iovs[iovpos].iov_base + iov_offset);
+		buf_len = iovs[iovpos].iov_len - iov_offset;
+
+		if (offset_in_block < block_size - md_size) {
+			buf_len = spdk_min(block_size - md_size - offset_in_block,
+					   buf_len);
+			for (i = 0; i < buf_len; i++) {
+				if (buf[i] != DATA_PATTERN) {
+					return -1;
+				}
+			}
+		} else {
+			buf_len = spdk_min(block_size - offset_in_block, buf_len);
+		}
+
+		payload_offset += buf_len;
+		iov_offset += buf_len;
+		if (iov_offset == iovs[iovpos].iov_len) {
+			iovpos++;
+			iov_offset = 0;
+		}
+	}
+
+	return 0;
+}
+
+static void
+_iov_alloc_buf(struct iovec *iov, uint32_t len)
+{
+	iov->iov_base = calloc(1, len);
+	iov->iov_len = len;
+	SPDK_CU_ASSERT_FATAL(iov->iov_base != NULL);
+}
+
+static void
+_iov_free_buf(struct iovec *iov)
+{
+	free(iov->iov_base);
+}
+
+static void
+dif_generate_and_verify_test(void)
+{
+	struct iovec iov;
+	uint32_t dif_flags;
+	int rc;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	_iov_alloc_buf(&iov, 4096 + 128);
+
+	_data_pattern_generate(&iov, 1, 4096 + 128, 128);
+
+	_dif_generate(iov.iov_base + 4096, iov.iov_base, 4096, dif_flags, 22, 0x22);
+
+	rc = _dif_verify(iov.iov_base + 4096, iov.iov_base, 4096, 1, dif_flags,
+			 22, 0xFFFF, 0x22);
+	CU_ASSERT(rc == 0);
+
+	rc = _data_pattern_verify(&iov, 1, 4096 + 128, 128);
+	CU_ASSERT(rc == 0);
+
+	_iov_free_buf(&iov);
+}
+
+static void
+sec_512_md_0_error_test(void)
+{
+	struct iovec iov = {0};
+	int rc;
+
+	rc = spdk_dif_generate(&iov, 1, 512, 0, true, 1, 0, 0, 0);
+	CU_ASSERT(rc != 0);
+
+	rc = spdk_dif_verify(&iov, 1, 512, 0, true, 1, 0, 0, 0, 0);
+	CU_ASSERT(rc != 0);
+}
+
+static void
+dif_generate_and_verify(struct iovec *iovs, int iovcnt,
+			uint32_t block_size, uint32_t md_size,
+			bool dif_start, uint32_t dif_type, uint32_t dif_flags,
+			uint32_t ref_tag, uint16_t apptag_mask, uint16_t app_tag)
+{
+	int rc;
+
+	_data_pattern_generate(iovs, iovcnt, block_size, md_size);
+
+	rc = spdk_dif_generate(iovs, iovcnt, block_size, md_size, dif_start,
+			       dif_type, dif_flags, ref_tag, app_tag);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_verify(iovs, iovcnt, block_size, md_size,
+			     dif_start, dif_type, dif_flags,
+			     ref_tag, apptag_mask, app_tag);
+	CU_ASSERT(rc == 0);
+
+	rc = _data_pattern_verify(iovs, iovcnt, block_size, md_size);
+	CU_ASSERT(rc == 0);
+}
+
+static void
+sec_512_md_8_prchk_0_single_iov_test(void)
+{
+	struct iovec iov;
+
+	_iov_alloc_buf(&iov, (512 + 8) * 4);
+
+	dif_generate_and_verify(&iov, 1, 512 + 8, 8, true, 1, 0, 0, 0, 0);
+
+	_iov_free_buf(&iov);
+}
+
+static void
+sec_512_md_8_prchk_0_1_2_4_multi_iovs_test(void)
+{
+	struct iovec iovs[4];
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		_iov_alloc_buf(&iovs[i], (512 + 8) * (i + 1));
+	}
+
+	dif_generate_and_verify(iovs, 4, 512 + 8, 8, true, 1, 0, 22, 0xFFFF, 0x22);
+
+	dif_generate_and_verify(iovs, 4, 512 + 8, 8, true, 1, SPDK_DIF_GUARD_CHECK,
+				22, 0xFFFF, 0x22);
+
+	dif_generate_and_verify(iovs, 4, 512 + 8, 8, true, 1, SPDK_DIF_APPTAG_CHECK,
+				22, 0xFFFF, 0x22);
+
+	dif_generate_and_verify(iovs, 4, 512 + 8, 8, true, 1, SPDK_DIF_REFTAG_CHECK,
+				22, 0xFFFF, 0x22);
+
+	for (i = 0; i < 4; i++) {
+		_iov_free_buf(&iovs[i]);
+	}
+}
+
+static void
+sec_4096_md_128_prchk_7_multi_iovs_test(void)
+{
+	struct iovec iovs[4];
+	int i;
+	uint32_t dif_flags;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	for (i = 0; i < 4; i++) {
+		_iov_alloc_buf(&iovs[i], (4096 + 128) * (i + 1));
+	}
+
+	dif_generate_and_verify(iovs, 4, 4096 + 128, 128, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	for (i = 0; i < 4; i++) {
+		_iov_free_buf(&iovs[i]);
+	}
+}
+
+static void
+sec_512_md_8_prchk_7_multi_iovs_split_data_and_md_test(void)
+{
+	struct iovec iovs[2];
+	uint32_t dif_flags;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	_iov_alloc_buf(&iovs[0], 512);
+	_iov_alloc_buf(&iovs[1], 8);
+
+	dif_generate_and_verify(iovs, 2, 512 + 8, 8, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	_iov_free_buf(&iovs[0]);
+	_iov_free_buf(&iovs[1]);
+}
+
+static void
+sec_512_md_8_prchk_7_multi_iovs_split_data_test(void)
+{
+	struct iovec iovs[2];
+	uint32_t dif_flags;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	_iov_alloc_buf(&iovs[0], 256);
+	_iov_alloc_buf(&iovs[1], 264);
+
+	dif_generate_and_verify(iovs, 2, 512 + 8, 8, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	_iov_free_buf(&iovs[0]);
+	_iov_free_buf(&iovs[1]);
+}
+
+static void
+sec_512_md_8_prchk_7_multi_iovs_split_guard_test(void)
+{
+	struct iovec iovs[2];
+	uint32_t dif_flags;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	_iov_alloc_buf(&iovs[0], 513);
+	_iov_alloc_buf(&iovs[1], 7);
+
+	dif_generate_and_verify(iovs, 2, 512 + 8, 8, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	_iov_free_buf(&iovs[0]);
+	_iov_free_buf(&iovs[1]);
+}
+
+static void
+sec_512_md_8_prchk_7_multi_iovs_split_apptag_test(void)
+{
+	struct iovec iovs[2];
+	uint32_t dif_flags;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	_iov_alloc_buf(&iovs[0], 515);
+	_iov_alloc_buf(&iovs[1], 5);
+
+	dif_generate_and_verify(iovs, 2, 512 + 8, 8, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	_iov_free_buf(&iovs[0]);
+	_iov_free_buf(&iovs[1]);
+}
+
+static void
+sec_512_md_8_prchk_7_multi_iovs_split_reftag_test(void)
+{
+	struct iovec iovs[2];
+	uint32_t dif_flags;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	_iov_alloc_buf(&iovs[0], 518);
+	_iov_alloc_buf(&iovs[1], 2);
+
+	dif_generate_and_verify(iovs, 2, 512 + 8, 8, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	_iov_free_buf(&iovs[0]);
+	_iov_free_buf(&iovs[1]);
+}
+
+static void
+sec_512_md_8_prchk_7_multi_iovs_complex_splits_test(void)
+{
+	struct iovec iovs[9];
+	uint32_t dif_flags;
+	int i;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	/* data[0][255:0] */
+	_iov_alloc_buf(&iovs[0], 256);
+
+	/* data[0][511:256], guard[0][0] */
+	_iov_alloc_buf(&iovs[1], 256 + 1);
+
+	/* guard[0][1], apptag[0][0] */
+	_iov_alloc_buf(&iovs[2], 1 + 1);
+
+	/* apptag[0][1], reftag[0][0] */
+	_iov_alloc_buf(&iovs[3], 1 + 1);
+
+	/* reftag[0][3:1], data[1][255:0] */
+	_iov_alloc_buf(&iovs[4], 3 + 256);
+
+	/* data[1][511:256], guard[1][0] */
+	_iov_alloc_buf(&iovs[5], 256 + 1);
+
+	/* guard[1][1], apptag[1][0] */
+	_iov_alloc_buf(&iovs[6], 1 + 1);
+
+	/* apptag[1][1], reftag[1][0] */
+	_iov_alloc_buf(&iovs[7], 1 + 1);
+
+	/* reftag[1][3:1] */
+	_iov_alloc_buf(&iovs[8], 3);
+
+	dif_generate_and_verify(iovs, 9, 512 + 8, 8, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	for (i = 0; i < 9; i++) {
+		_iov_free_buf(&iovs[i]);
+	}
+}
+
+static void
+sec_4096_md_128_prchk_7_multi_iovs_complex_splits_test(void)
+{
+	struct iovec iovs[11];
+	uint32_t dif_flags;
+	int i;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	/* data[0][1000:0] */
+	_iov_alloc_buf(&iovs[0], 1000);
+
+	/* data[0][3095:1000], guard[0][0] */
+	_iov_alloc_buf(&iovs[1], 3096 + 1);
+
+	/* guard[0][1], apptag[0][0] */
+	_iov_alloc_buf(&iovs[2], 1 + 1);
+
+	/* apptag[0][1], reftag[0][0] */
+	_iov_alloc_buf(&iovs[3], 1 + 1);
+
+	/* reftag[0][3:1], ignore[0][59:0] */
+	_iov_alloc_buf(&iovs[4], 3 + 60);
+
+	/* ignore[119:60], data[1][3050:0] */
+	_iov_alloc_buf(&iovs[5], 60 + 3051);
+
+	/* data[1][4095:3050], guard[1][0] */
+	_iov_alloc_buf(&iovs[6], 1045 + 1);
+
+	/* guard[1][1], apptag[1][0] */
+	_iov_alloc_buf(&iovs[7], 1 + 1);
+
+	/* apptag[1][1], reftag[1][0] */
+	_iov_alloc_buf(&iovs[8], 1 + 1);
+
+	/* reftag[1][3:1], ignore[1][9:0] */
+	_iov_alloc_buf(&iovs[9], 3 + 10);
+
+	/* ignore[1][127:9] */
+	_iov_alloc_buf(&iovs[10], 118);
+
+	dif_generate_and_verify(iovs, 11, 4096 + 128, 128, true, 1, dif_flags, 22, 0xFFFF, 0x22);
+
+	for (i = 0; i < 11; i++) {
+		_iov_free_buf(&iovs[i]);
+	}
+}
+
+int
+main(int argc, char **argv)
+{
+	CU_pSuite	suite = NULL;
+	unsigned int	num_failures;
+
+	if (CU_initialize_registry() != CUE_SUCCESS) {
+		return CU_get_error();
+	}
+
+	suite = CU_add_suite("dif", NULL, NULL);
+	if (suite == NULL) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (
+		CU_add_test(suite, "dif_generate_and_verify_test", dif_generate_and_verify_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_0_error_test", sec_512_md_0_error_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_0_single_iov_test",
+			    sec_512_md_8_prchk_0_single_iov_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_0_1_2_4_multi_iovs_test",
+			    sec_512_md_8_prchk_0_1_2_4_multi_iovs_test) == NULL ||
+		CU_add_test(suite, "sec_4096_md_128_prchk_7_multi_iovs_test",
+			    sec_4096_md_128_prchk_7_multi_iovs_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_7_multi_iovs_split_data_and_md_test",
+			    sec_512_md_8_prchk_7_multi_iovs_split_data_and_md_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_7_multi_iovs_split_data_test",
+			    sec_512_md_8_prchk_7_multi_iovs_split_data_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_7_multi_iovs_split_guard_test",
+			    sec_512_md_8_prchk_7_multi_iovs_split_guard_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_7_multi_iovs_split_apptag_test",
+			    sec_512_md_8_prchk_7_multi_iovs_split_apptag_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_7_multi_iovs_split_reftag_test",
+			    sec_512_md_8_prchk_7_multi_iovs_split_reftag_test) == NULL ||
+		CU_add_test(suite, "sec_512_md_8_prchk_7_multi_iovs_complex_splits_test",
+			    sec_512_md_8_prchk_7_multi_iovs_complex_splits_test) == NULL ||
+		CU_add_test(suite, "sec_4096_md_128_prchk_7_multi_iovs_complex_splits_test",
+			    sec_4096_md_128_prchk_7_multi_iovs_complex_splits_test) == NULL
+	) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+
+	CU_basic_run_tests();
+
+	num_failures = CU_get_number_of_failures();
+	CU_cleanup_registry();
+
+	return num_failures;
+}
