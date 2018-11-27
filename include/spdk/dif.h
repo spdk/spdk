2db1c7d0759a3b5bd1c1dbedba247a4721ea6970
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-11-27 14:23:38 +0900

    dif: Generate DIF for extended LBA payload
    DIF and DIX will be supported in SPDK throughout, e.g., NVMe
    driver, NVMe-oF initiator and target, NVMe block device, malloc
    block device, SCSI, iSCSI target, FIO plugin, and Perf.
    
    Generic and common APIs to generate and verify DIF and inject
    bit flip error to any field will be helpful for them.
    
    This patch is the first in the patch series.
    
    This patch adds APIs to generate and verify DIF for SGL extended
    LBA payload as byte alignement and granularity.
    
    Change-Id: Ie6588d960113761f10efbf2d2a3cae004af37ce8
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

---
+++ include/spdk/dif.h
@@ -0,0 +1,98 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SPDK_DIF_H
+#define SPDK_DIF_H
+
+#include "spdk/stdinc.h"
+#include "spdk/assert.h"
+
+#define SPDK_DIF_GUARD_CHECK	0x1
+#define SPDK_DIF_APPTAG_CHECK	0x2
+#define SPDK_DIF_REFTAG_CHECK	0x4
+
+enum spdk_dif_type {
+	SPDK_DIF_TYPE1 = 1,
+	SPDK_DIF_TYPE2,
+	SPDK_DIF_TYPE3,
+};
+
+struct spdk_dif {
+	uint16_t guard;
+	uint16_t app_tag;
+	uint32_t ref_tag;
+};
+SPDK_STATIC_ASSERT(sizeof(struct spdk_dif) == 8, "Incorrect size");
+
+/**
+ * Generate DIF for extended LBA payload.
+ *
+ * \param iovs A scatter gather list of buffers to be written from.
+ * \param iovcnt The number of elements in iovs.
+ * \param block_size The block size in a block.
+ * \param md_size The metadata size in a block.
+ * \param dif_start DIF is set in the first eight bytes of metadata if true,
+ * or in the last eight bytes of metadata otherwise.
+ * \param dif_type The type of DIF.
+ * \param dif_flags The flag to specify the DIF action.
+ * \param init_ref_tag Initial Reference Tag.
+ * \param app_tag Application Tag.
+ *
+ * \return 0 on success and negated errno otherwise.
+ */
+int spdk_dif_generate(struct iovec *iovs, int iovcnt, uint32_t block_size,
+		      uint32_t md_size, bool dif_start, enum spdk_dif_type dif_type,
+		      uint32_t dif_flags, uint32_t init_ref_tag, uint16_t app_tag);
+
+/**
+ * Verify DIF for extended LBA payload.
+ *
+ * \param iovs A scatter gather list of buffers to be read to.
+ * \param iovcnt The number of elements in iovs.
+ * \param block_size The block size in a block.
+ * \param md_size The metadata size in a block.
+ * \param dif_start DIF is set in the first eight bytes of metadata if true,
+ * or in the last eight bytes of metadata otherwise.
+ * \param dif_type The type of DIF.
+ * \param dif_flags The flag to specify the DIF action.
+ * \param init_ref_tag Initial Reference Tag.
+ * \param apptag_mask Application Tag Mask.
+ * \param app_tag Application Tag.
+ *
+ * \return 0 on success and negated errno otherwise.
+ */
+int spdk_dif_verify(struct iovec *iovs, int iovcnt, uint32_t block_size,
+		    uint32_t md_size, bool dif_start, enum spdk_dif_type dif_type,
+		    uint32_t dif_flags, uint32_t init_ref_tag, uint16_t apptag_mask,
+		    uint16_t app_tag);
+#endif /* SPDK_DIF_H */
