d65e310cc21b745f2f97949343752fc35a1f91ca
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-11-29 10:18:50 +0900

    scsi: Revert submission queue and suspend submission during LUN reset
    Submission queue deleted by https://review.gerrithub.io/393911
    is necessary to support proper LUN reset.
    
    Hence revert the removal in this patch.
    
    And using the submission queue, suspend IO task submission during
    LUN reset to limit completion time of LUN reset.
    
    Change-Id: I3cdc4f0165fe845637112c2900407d6b4a09df79
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/unit/lib/scsi/lun.c/lun_ut.c
+++ test/unit/lib/scsi/lun.c/lun_ut.c
@@ -245,7 +245,8 @@ lun_task_mgmt_execute_abort_task_not_supported(void)
 	task.lun = lun;
 	task.cdb = cdb;
 
-	spdk_scsi_lun_execute_task(lun, &task);
+	spdk_scsi_lun_append_task(lun, &task);
+	spdk_scsi_lun_execute_tasks(lun);
 
 	/* task should now be on the tasks list */
 	CU_ASSERT(!TAILQ_EMPTY(&lun->tasks));
@@ -289,7 +290,8 @@ lun_task_mgmt_execute_abort_task_all_not_supported(void)
 	task.lun = lun;
 	task.cdb = cdb;
 
-	spdk_scsi_lun_execute_task(lun, &task);
+	spdk_scsi_lun_append_task(lun, &task);
+	spdk_scsi_lun_execute_tasks(lun);
 
 	/* task should now be on the tasks list */
 	CU_ASSERT(!TAILQ_EMPTY(&lun->tasks));
@@ -453,7 +455,8 @@ lun_execute_scsi_task_pending(void)
 	 */
 	CU_ASSERT(TAILQ_EMPTY(&lun->tasks));
 
-	spdk_scsi_lun_execute_task(lun, &task);
+	spdk_scsi_lun_append_task(lun, &task);
+	spdk_scsi_lun_execute_tasks(lun);
 
 	/* Assert the task has been successfully added to the tasks queue */
 	CU_ASSERT(!TAILQ_EMPTY(&lun->tasks));
@@ -490,7 +493,8 @@ lun_execute_scsi_task_complete(void)
 	 */
 	CU_ASSERT(TAILQ_EMPTY(&lun->tasks));
 
-	spdk_scsi_lun_execute_task(lun, &task);
+	spdk_scsi_lun_append_task(lun, &task);
+	spdk_scsi_lun_execute_tasks(lun);
 
 	/* Assert the task has not been added to the tasks queue */
 	CU_ASSERT(TAILQ_EMPTY(&lun->tasks));
