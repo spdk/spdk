21afb2d9f7440ded4ce35019418374414ed94e98
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-11-29 10:23:07 +0900

    scsi: LUN reset waits for completion of all prior tasks
    IO submissions to backends have to complete first in LUN reset.
    Previous patch makes the waiting time limited.
    
    LUN reset don't use timeout for now because even if LUN reset reports
    timeout, error handling of the initiator will be escalated and the
    initiator will issue stronger command and will wait eventually
    until all outstanding tasks completed.
    
    Besides, not using timeout in LUN reset will make implementation simple.
    
    If timeout becomes necessary, I'll do it in the separate patch later.
    
    Change-Id: Ie9e4502068c19b1727ea65dc773ddf08cedec7c4
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/scsi/scsi_internal.h
+++ lib/scsi/scsi_internal.h
@@ -44,6 +44,8 @@
 
 #include "spdk_internal/log.h"
 
+#define SPDK_SCSI_MGMT_TASK_TIMEOUT	30	/* seconds */
+
 enum {
 	SPDK_SCSI_TASK_UNKNOWN = -1,
 	SPDK_SCSI_TASK_COMPLETE,
@@ -125,6 +127,8 @@ struct spdk_scsi_lun {
 	/** pending management tasks */
 	TAILQ_HEAD(pending_mgmt_tasks, spdk_scsi_task) pending_mgmt_tasks;
 
+	/** poller to check completion of tasks prior to reset */
+	struct spdk_poller *reset_poller;
 };
 
 struct spdk_lun_db_entry {
