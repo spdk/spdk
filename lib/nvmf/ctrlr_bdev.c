38d3a18695c5c82398bdc45d58639506b073378e
Changpeng Liu <changpeng.liu@intel.com>
2018-12-03 04:32:28 -0500

    NVMoF: add reservation release command support
    The reservation holder may release the reservation on
    a namespace, release notification feature is supported
    in following patches.
    
    Change-Id: If5d3158e691fcc782f7cf0b67a326bf62edf0531
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/nvmf/ctrlr_bdev.c
+++ lib/nvmf/ctrlr_bdev.c
@@ -616,6 +616,21 @@ nvmf_ns_reservation_all_registrants_type(struct spdk_nvmf_ns *ns)
 	return false;
 }
 
+static uint32_t
+nvmf_ctrlr_remove_all_registrants(struct spdk_nvmf_subsystem *subsystem)
+{
+	struct spdk_nvmf_registrant *tmp, *tmp1;
+	uint32_t count = 0;
+
+	TAILQ_FOREACH_SAFE(tmp, &subsystem->reg_head, link, tmp1) {
+		TAILQ_REMOVE(&subsystem->reg_head, tmp, link);
+		free(tmp);
+		count++;
+		subsystem->regctl--;
+	}
+	return count;
+}
+
 static int
 nvmf_ns_reservation_register(struct spdk_nvmf_request *req,
 			     struct spdk_nvmf_ctrlr *ctrlr,
@@ -882,6 +897,98 @@ conflict:
 	return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
 }
 
+/* TODO: reservation notificaton */
+static void
+nvme_ns_reservation_notification(struct spdk_nvmf_ctrlr *ctrlr)
+{
+	return;
+}
+
+static int
+nvmf_ns_reservation_release(struct spdk_nvmf_request *req,
+			    struct spdk_nvmf_ctrlr *ctrlr,
+			    struct spdk_nvmf_ns *ns)
+{
+	struct spdk_nvme_cmd *cmd = &req->cmd->nvme_cmd;
+	struct spdk_nvmf_subsystem *subsystem = ctrlr->subsys;
+	uint8_t rrela, iekey, rtype;
+	struct spdk_nvmf_registrant *reg;
+	uint64_t crkey;
+
+	rrela = cmd->cdw10 & 0x7u;
+	iekey = (cmd->cdw10 >> 3) & 0x1u;
+	rtype = (cmd->cdw10 >> 8) & 0xffu;
+	memcpy(&crkey, req->data, sizeof(crkey));
+
+	SPDK_DEBUGLOG(SPDK_LOG_NVMF, "RELEASE: RRELA %u, IEKEY %u, RTYPE %u, "
+		      "CRKEY 0x%"PRIx64"\n",  rrela, iekey, rtype, crkey);
+
+	pthread_mutex_lock(&subsystem->reservation_lock);
+
+	if (iekey) {
+		SPDK_ERRLOG("Ignore existing key field set to 1\n");
+		goto invalid;
+	}
+
+	reg = nvmf_ctrlr_get_registrant(subsystem, ctrlr);
+	if (!reg || reg->rkey != crkey) {
+		SPDK_ERRLOG("No registrant or current key doesn't match "
+			    "with existing registrant key\n");
+		goto conflict;
+	}
+
+	switch (rrela) {
+	case SPDK_NVME_RESERVE_RELEASE:
+		if (!ns->holder) {
+			SPDK_DEBUGLOG(SPDK_LOG_NVMF, "RELEASE: no holder\n");
+			goto success;
+		}
+		if (ns->rtype != rtype) {
+			SPDK_ERRLOG("Type doesn't match\n");
+			goto invalid;
+		}
+		if (!nvmf_ns_registrant_is_hodler(ns, reg)) {
+			/* not the reservation holder, this isn't an error */
+			goto success;
+		}
+		ns->rtype = 0;
+		ns->crkey = 0;
+		ns->holder = NULL;
+
+		/* TODO: release notification */
+		if (rtype != SPDK_NVME_RESERVE_WRITE_EXCLUSIVE ||
+		    rtype != SPDK_NVME_RESERVE_EXCLUSIVE_ACCESS) {
+			nvme_ns_reservation_notification(ctrlr);
+		}
+		break;
+	case SPDK_NVME_RESERVE_CLEAR:
+		ns->rtype = 0;
+		ns->crkey = 0;
+		ns->holder = NULL;
+		nvmf_ctrlr_remove_all_registrants(subsystem);
+		/* TODO: reservation preempted notificaton */
+		break;
+	default:
+		goto invalid;
+	}
+
+success:
+	req->rsp->nvme_cpl.status.sct = SPDK_NVME_SCT_GENERIC;
+	req->rsp->nvme_cpl.status.sc = SPDK_NVME_SC_SUCCESS;
+	pthread_mutex_unlock(&subsystem->reservation_lock);
+	return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+invalid:
+	req->rsp->nvme_cpl.status.sct = SPDK_NVME_SCT_GENERIC;
+	req->rsp->nvme_cpl.status.sc = SPDK_NVME_SC_INVALID_FIELD;
+	pthread_mutex_unlock(&subsystem->reservation_lock);
+	return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+conflict:
+	req->rsp->nvme_cpl.status.sct = SPDK_NVME_SCT_GENERIC;
+	req->rsp->nvme_cpl.status.sc = SPDK_NVME_SC_RESERVATION_CONFLICT;
+	pthread_mutex_unlock(&subsystem->reservation_lock);
+	return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+}
+
 int
 spdk_nvmf_ctrlr_process_io_cmd(struct spdk_nvmf_request *req)
 {
@@ -939,6 +1046,8 @@ spdk_nvmf_ctrlr_process_io_cmd(struct spdk_nvmf_request *req)
 		return nvmf_ns_reservation_register(req, ctrlr, ns);
 	case SPDK_NVME_OPC_RESERVATION_ACQUIRE:
 		return nvmf_ns_reservation_acquire(req, ctrlr, ns);
+	case SPDK_NVME_OPC_RESERVATION_RELEASE:
+		return nvmf_ns_reservation_release(req, ctrlr, ns);
 	default:
 		return nvmf_bdev_ctrlr_nvme_passthru_io(bdev, desc, ch, req);
 	}
