1fc7d357c1e6b13fc0f1a1e9b5d89b79ddbc7479
Pawel Kaminski <pawelx.kaminski@intel.com>
2018-12-03 13:41:25 -0500

    test/spdkcli: Reduce execution time for spdkcli tests
    Pass many spdkcli commands to spdkcli_job.py
    
    Change-Id: I0cf5840fdf7183febff1872c7fc8d1cb303a06f1
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/spdkcli/virtio.sh
+++ test/spdkcli/virtio.sh
@@ -18,19 +18,21 @@ waitforlisten $virtio_pid /var/tmp/virtio.sock
 timing_exit run_spdk_virtio
 
 timing_enter spdkcli_create_virtio_pci_config
-$spdkcli_job "/bdevs/malloc create 32 512 Malloc0" "Malloc0" True
-$spdkcli_job "/bdevs/malloc create 32 512 Malloc1" "Malloc1" True
+$spdkcli_job "'/bdevs/malloc create 32 512 Malloc0' 'Malloc0' True
+'/bdevs/malloc create 32 512 Malloc1' 'Malloc1' True
+"
 pci_blk=$(lspci -nn -D | grep '1af4:1001' | head -1 | awk '{print $1;}')
 if [ ! -z $pci_blk ]; then
-	$spdkcli_job "/bdevs/virtioblk_disk create virtioblk_pci pci $pci_blk" "virtioblk_pci" True
+	$spdkcli_job "'/bdevs/virtioblk_disk create virtioblk_pci pci $pci_blk' 'virtioblk_pci' True"
 fi
 pci_scsi=$(lspci -nn -D | grep '1af4:1004' | head -1 | awk '{print $1;}')
 if [ ! -z $pci_scsi ]; then
-	$spdkcli_job "/bdevs/virtioscsi_disk create virtioscsi_pci pci $pci_scsi" "virtioscsi_pci" True
+	$spdkcli_job "'/bdevs/virtioscsi_disk create virtioscsi_pci pci $pci_scsi' 'virtioscsi_pci' True"
 fi
-$spdkcli_job "/vhost/scsi create sample_scsi" "sample_scsi" True
-$spdkcli_job "/vhost/scsi/sample_scsi add_lun 0 Malloc0" "Malloc0" True
-$spdkcli_job "/vhost/block create sample_block Malloc1" "Malloc1" True
+$spdkcli_job "'/vhost/scsi create sample_scsi' 'sample_scsi' True
+'/vhost/scsi/sample_scsi add_lun 0 Malloc0' 'Malloc0' True
+'/vhost/block create sample_block Malloc1' 'Malloc1' True
+"
 timing_exit spdkcli_create_virtio_pci_config
 
 timing_enter spdkcli_check_match
@@ -42,8 +44,9 @@ fi
 timing_exit spdkcli_check_match
 
 timing_enter spdkcli_create_virtio_user_config
-$spdkcli_job "/bdevs/virtioblk_disk create virtioblk_user user $testdir/../../sample_block" "virtioblk_user" True "/var/tmp/virtio.sock"
-$spdkcli_job "/bdevs/virtioscsi_disk create virtioscsi_user user $testdir/../../sample_scsi" "virtioscsi_user" True "/var/tmp/virtio.sock"
+$spdkcli_job "'/bdevs/virtioblk_disk create virtioblk_user user $testdir/../../sample_block' 'virtioblk_user' True
+'/bdevs/virtioscsi_disk create virtioscsi_user user $testdir/../../sample_scsi' 'virtioscsi_user' True
+" /var/tmp/virtio.sock
 timing_exit spdkcli_create_virtio_user_config
 
 timing_enter spdkcli_check_match_user_config
@@ -53,19 +56,22 @@ check_match
 timing_exit spdkcli_check_match_user_config
 
 timing_enter spdkcli_clear_virtio_config
-$spdkcli_job "/bdevs/virtioscsi_disk delete virtioscsi_user" "" False "/var/tmp/virtio.sock"
-$spdkcli_job "/bdevs/virtioblk_disk delete virtioblk_user" "" False "/var/tmp/virtio.sock"
-$spdkcli_job "/vhost/block delete sample_block" "sample_block"
-$spdkcli_job "/vhost/scsi/sample_scsi remove_target 0" "Malloc0"
-$spdkcli_job "/vhost/scsi delete sample_scsi" " sample_scsi"
+$spdkcli_job "'/bdevs/virtioscsi_disk delete virtioscsi_user' '' False
+'/bdevs/virtioblk_disk delete virtioblk_user' '' False
+" /var/tmp/virtio.sock
+$spdkcli_job "'/vhost/block delete sample_block' 'sample_block'
+'/vhost/scsi/sample_scsi remove_target 0' 'Malloc0'
+'/vhost/scsi delete sample_scsi' 'sample_scsi'
+"
 if [ ! -z $pci_blk ]; then
-	$spdkcli_job "/bdevs/virtioblk_disk delete virtioblk_pci" "virtioblk_pci"
+	$spdkcli_job "'/bdevs/virtioblk_disk delete virtioblk_pci' 'virtioblk_pci'"
 fi
 if [ ! -z $pci_scsi ]; then
-	$spdkcli_job "/bdevs/virtioscsi_disk delete virtioscsi_pci" "virtioscsi_pci"
+	$spdkcli_job "'/bdevs/virtioscsi_disk delete virtioscsi_pci' 'virtioscsi_pci'"
 fi
-$spdkcli_job "/bdevs/malloc delete Malloc0" "Malloc0"
-$spdkcli_job "/bdevs/malloc delete Malloc1" "Malloc1"
+$spdkcli_job "'/bdevs/malloc delete Malloc0' 'Malloc0'
+'/bdevs/malloc delete Malloc1' 'Malloc1'
+"
 timing_exit spdkcli_clear_virtio_config
 
 killprocess $virtio_pid
