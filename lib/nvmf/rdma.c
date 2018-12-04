97803aaa76482a27ba4a886618915568b7c7f1ed
Ziye Yang <ziye.yang@intel.com>
2018-12-05 02:42:07 +0800

    nvmf: fix the error path for shared data buffer free.
    Since we use aligned buffer, I think that the error handling
    path here is not correct, the address is wrong.
    
    Change-Id: I5bcb7f050199496423f861fd6aea65e0fe48c804
    Signed-off-by: Ziye Yang <ziye.yang@intel.com>

--- lib/nvmf/rdma.c
+++ lib/nvmf/rdma.c
@@ -1177,7 +1177,7 @@ spdk_nvmf_rdma_request_fill_iovs(struct spdk_nvmf_rdma_transport *rtransport,
 nomem:
 	while (i) {
 		i--;
-		spdk_mempool_put(rtransport->data_buf_pool, rdma_req->req.iov[i].iov_base);
+		spdk_mempool_put(rtransport->data_buf_pool, rdma_req->data.buffers[i]);
 		rdma_req->req.iov[i].iov_base = NULL;
 		rdma_req->req.iov[i].iov_len = 0;
 
