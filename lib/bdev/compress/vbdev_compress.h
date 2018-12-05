ec2a438ab58301da421299aa0204929f6cc8abf1
paul luse <paul.e.luse@intel.com>
2018-12-05 18:35:02 -0500

    bdev/compress: add some tests
    Add unit test shell, rpc tests and update reduce testflag in at least
    one case.  Also fix one smll bug in the load callback (too much cleanup).
    
    UT for this will be big enough to warrant another patch in the series.
    
    Change-Id: Ic76f0e2fd12e48782b048fb8785902b1098cf352
    Signed-off-by: paul luse <paul.e.luse@intel.com>

--- lib/bdev/compress/vbdev_compress.h
+++ lib/bdev/compress/vbdev_compress.h
@@ -47,7 +47,7 @@ typedef void (*spdk_delete_compress_complete)(void *cb_arg, int bdeverrno);
  * \param vbdev_name Vbdev name.
  * \return 0 on success, other on failure.
  */
-int create_compress_disk(const char *bdev_name, const char *vbdev_name, const char *comp_pmd);
+int create_compress_bdev(const char *bdev_name, const char *vbdev_name, const char *comp_pmd);
 
 /**
  * Delete compress bdev.
@@ -56,7 +56,7 @@ int create_compress_disk(const char *bdev_name, const char *vbdev_name, const ch
  * \param cb_fn Function to call after deletion.
  * \param cb_arg Argument to pass to cb_fn.
  */
-void delete_compress_disk(struct spdk_bdev *bdev, spdk_delete_compress_complete cb_fn,
+void delete_compress_bdev(struct spdk_bdev *bdev, spdk_delete_compress_complete cb_fn,
 			  void *cb_arg);
 
 #endif /* SPDK_VBDEV_compress_H */
