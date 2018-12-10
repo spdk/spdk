b3ae24bc90b511db22b4778b2f088273b153b9a0
Ben Walker <benjamin.walker@intel.com>
2018-12-10 14:42:37 -0700

    rocksdb: Use new threading API calls
    Change-Id: I6ed98605512b675e1b6bde5b9644d6160802b893
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/rocksdb/env_spdk.cc
+++ lib/rocksdb/env_spdk.cc
@@ -577,20 +577,10 @@ public:
 	}
 };
 
-static void
-_spdk_send_msg(__attribute__((unused)) spdk_thread_fn fn,
-	       __attribute__((unused)) void *ctx,
-	       __attribute__((unused)) void *thread_ctx)
-{
-	/* Not supported */
-	assert(false);
-}
-
 void SpdkInitializeThread(void)
 {
 	if (g_fs != NULL) {
-		/* TODO: Add an event lib call to dynamically register a thread */
-		spdk_allocate_thread(_spdk_send_msg, NULL, NULL, NULL, "spdk_rocksdb");
+		spdk_allocate_thread(NULL, NULL, NULL, NULL, "spdk_rocksdb");
 		g_sync_args.channel = spdk_fs_alloc_io_channel_sync(g_fs);
 	}
 }
