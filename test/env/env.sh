1574dc00fd9d5ac1f3dc083c58d0f73fa7a9afbb
Jim Harris <james.r.harris@intel.com>
2018-12-11 16:34:09 -0700

    test: add mem_callbacks unit test
    This will be helpful during development of upstream
    DPDK patches to ensure that buffers do not cross
    memory callback allocation boundaries, and that memory
    freed by DPDK is in the same units as it was allocated.
    
    Since DPDK currently will fail these tests, do not
    plumb them into unittest.sh - but still build it by
    default.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I5b68e9f0864304962b9189b9f5e58f828f0d0e54

--- test/env/env.sh
+++ test/env/env.sh
@@ -20,5 +20,12 @@ timing_enter pci
 $testdir/pci/pci_ut
 timing_exit pci
 
+# Do not enable this test yet - it doesn't work with current DPDK.
+# We will use this test for fixing DPDK, then enable it once those
+# changes are merged upstream.
+#timing_enter mem_callbacks
+#$testdir/mem_callbacks/mem_callbacks
+#timing_exit mem_callbacks
+
 report_test_completion "env"
 timing_exit env
