9f6cb9a5aa3cdec256e366ac4bfced9116e9cd62
Richael Zhuang <richael.zhuang@arm.com>
2018-12-13 15:24:48 +0800

    Update spdk crc32c.c with ARM CRC32 intrinsics
    Implement spdk_crc32c_update() with ARM CRC32 intrinsics.
    
    Change-Id: I1daf7f21012aab02290f88a65bbae619eedf5087
    Signed-off-by: Richael Zhuang <richael.zhuang@arm.com>

--- lib/util/crc32c.c
+++ lib/util/crc32c.c
@@ -33,8 +33,7 @@
 
 #include "spdk/crc32.h"
 
-#if defined(__x86_64__) && defined(__SSE4_2__)
-#include <x86intrin.h>
+#ifdef SPDK_HAVE_SSE4_2
 
 uint32_t
 spdk_crc32c_update(const void *buf, size_t len, uint32_t crc)
@@ -64,6 +63,34 @@ spdk_crc32c_update(const void *buf, size_t len, uint32_t crc)
 	count = len & 7;
 	while (count--) {
 		crc = _mm_crc32_u8(crc, *(const uint8_t *)buf);
+		buf++;
+	}
+
+	return crc;
+}
+
+#elif defined(SPDK_HAVE_ARM_CRC)
+
+uint32_t
+spdk_crc32c_update(const void *buf, size_t len, uint32_t crc)
+{
+	uint64_t crc_tmp64;
+	size_t count;
+
+	crc_tmp64 = crc;
+
+	count = len / 8;
+	while (count--) {
+		uint64_t block;
+		memcpy(&block, buf, sizeof(block));
+		crc_tmp64 = __crc32cd(crc_tmp64, block);
+		buf += sizeof(block);
+	}
+	crc = (uint32_t)crc_tmp64;
+
+	count = len & 7;
+	while (count--) {
+		crc = __crc32cb(crc, *(const uint8_t *)buf);
 		buf++;
 	}
 
