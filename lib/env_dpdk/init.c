5bf165cdf738758d40ecd10835171c47b0408748
Xiaodong Liu <xiaodong.liu@intel.com>
2018-12-13 23:54:01 +0800

    env: add spdk_env_dpdk_post_init
    Based on requirement of initializing SPDK env
    from a DPDK application, relative to spdk_env_init,
    add spdk_env_dpdk_post_init for calling after rte_env_init.
    More details, visit
    https://github.com/spdk/spdk/issues/529
    
    Change-Id: I6fda1593e0296ef93b705e31cc76bcd0d248673a
    Signed-off-by: Xiaodong Liu <xiaodong.liu@intel.com>

--- lib/env_dpdk/init.c
+++ lib/env_dpdk/init.c
@@ -36,6 +36,7 @@
 #include "env_internal.h"
 
 #include "spdk/version.h"
+#include "spdk/env_dpdk.h"
 
 #include <rte_config.h>
 #include <rte_eal.h>
@@ -342,7 +343,25 @@ spdk_build_eal_cmdline(const struct spdk_env_opts *opts)
 	return argcount;
 }
 
-int spdk_env_init(const struct spdk_env_opts *opts)
+int
+spdk_env_dpdk_post_init(void)
+{
+	spdk_pci_init();
+
+	if (spdk_mem_map_init() < 0) {
+		fprintf(stderr, "Failed to allocate mem_map\n");
+		return -1;
+	}
+	if (spdk_vtophys_init() < 0) {
+		fprintf(stderr, "Failed to initialize vtophys\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+int
+spdk_env_init(const struct spdk_env_opts *opts)
 {
 	char **dpdk_args = NULL;
 	int i, rc;
@@ -396,16 +415,5 @@ int spdk_env_init(const struct spdk_env_opts *opts)
 		spdk_env_unlink_shared_files();
 	}
 
-	spdk_pci_init();
-
-	if (spdk_mem_map_init() < 0) {
-		fprintf(stderr, "Failed to allocate mem_map\n");
-		return -1;
-	}
-	if (spdk_vtophys_init() < 0) {
-		fprintf(stderr, "Failed to initialize vtophys\n");
-		return -1;
-	}
-
-	return 0;
+	return spdk_env_dpdk_post_init();
 }
