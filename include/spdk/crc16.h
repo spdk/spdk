eb9ccf2fd12b7a598bb7f8d1777c01b3b9e1a044
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-12-17 17:34:45 +0900

    util/crc16: Add spdk_crc16_t10dif_copy to use in read strip and write insert
    For DIF and DIX, read strip and write insert operation will copy
    data together with DIF generation and verification.
    
    This patch adds spdk_crc16_t10dif_copy for those cases.
    
    Change-Id: I9a77fa8b367486fe0b6704d58dcdf95d5210c875
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- include/spdk/crc16.h
+++ include/spdk/crc16.h
@@ -59,6 +59,18 @@ extern "C" {
  * \return CRC-16 value.
  */
 uint16_t spdk_crc16_t10dif(uint16_t init_crc, const void *buf, size_t len);
+
+/**
+ * Calculate T10-DIF CRC-16 checksum and copy data.
+ *
+ * \param init_crc Initial CRC-16 value.
+ * \param dst Destination data buffer for copy.
+ * \param src Source data buffer for CRC calculation and copy.
+ * \param len Length of buffer in bytes.
+ * \return CRC-16 value.
+ */
+uint16_t spdk_crc16_t10dif_copy(uint16_t init_crc, uint8_t *dst, uint8_t *src,
+				size_t len);
 #ifdef __cplusplus
 }
 #endif
