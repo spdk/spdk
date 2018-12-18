668f7851788dc9d63ff258a412da106d33d8253d
Changpeng Liu <changpeng.liu@intel.com>
2018-12-18 05:27:09 -0500

    scsi: add persistent reservation in command with read keys support
    Change-Id: Ia8e4ad31e84a1a97c83d4f49680bb7090ee4e041
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/scsi/scsi_internal.h
+++ lib/scsi/scsi_internal.h
@@ -196,5 +196,7 @@ extern struct spdk_scsi_globals g_spdk_scsi;
 
 int spdk_scsi_pr_out(struct spdk_scsi_task *task, uint8_t *cdb,
 		     uint8_t *data, uint16_t data_len);
+int spdk_scsi_pr_in(struct spdk_scsi_task *task, uint8_t *cdb,
+		    uint8_t *data, uint16_t data_len);
 
 #endif /* SPDK_SCSI_INTERNAL_H */
