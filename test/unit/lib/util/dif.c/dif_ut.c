8779565637b3d4e45425fb7ef40a55739f500526
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-12-18 10:57:08 +0900

    dif: Inject bit-flip error for copied extended LBA payload
    This patch adds API to inject bit flip error into any field of
    the copied extended LBA payload.
    
    Change-Id: I9f7ac1332fe63cb206c692d0876c8de41ef9286f
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/unit/lib/util/dif.c/dif_ut.c
+++ test/unit/lib/util/dif.c/dif_ut.c
@@ -876,6 +876,112 @@ dif_copy_sec_512_md_8_prchk_7_multi_iovs_complex_splits(void)
 	_iov_free_buf(&bounce_iov);
 }
 
+static void
+_dif_copy_inject_error_and_verify(struct iovec *iovs, int iovcnt, struct iovec *bounce_iov,
+				  uint32_t block_size, uint32_t md_size, uint32_t num_blocks,
+				  uint32_t inject_flags, bool dif_loc)
+{
+	uint32_t dif_flags;
+	int rc;
+
+	dif_flags = SPDK_DIF_GUARD_CHECK | SPDK_DIF_APPTAG_CHECK | SPDK_DIF_REFTAG_CHECK;
+
+	rc = ut_data_pattern_generate(iovs, iovcnt, block_size - md_size, 0, num_blocks);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_generate_copy(iovs, iovcnt, bounce_iov, block_size, md_size, num_blocks,
+				    dif_loc, SPDK_DIF_TYPE1, dif_flags, 88, 0x88);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_inject_error(bounce_iov, 1, block_size, md_size, num_blocks,
+				   dif_loc, inject_flags);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_verify_copy(iovs, iovcnt, bounce_iov, block_size, md_size, num_blocks,
+				  dif_loc, SPDK_DIF_TYPE1, dif_flags, 88, 0xFFFF, 0x88);
+	CU_ASSERT(rc != 0);
+}
+
+static void
+dif_copy_inject_error_and_verify(struct iovec *iovs, int iovcnt, struct iovec *bounce_iov,
+				 uint32_t block_size, uint32_t md_size, uint32_t num_blocks,
+				 uint32_t inject_flags)
+{
+	/* The case that DIF is contained in the first 8 bytes of metadata. */
+	_dif_copy_inject_error_and_verify(iovs, iovcnt, bounce_iov,
+					  block_size, md_size, num_blocks,
+					  inject_flags, false);
+
+	/* The case that DIF is contained in the last 8 bytes of metadata. */
+	_dif_copy_inject_error_and_verify(iovs, iovcnt, bounce_iov,
+					  block_size, md_size, num_blocks,
+					  inject_flags, true);
+}
+
+static void
+dif_copy_sec_4096_md_128_inject_1_2_4_8_multi_iovs_test(void)
+{
+	struct iovec iovs[4], bounce_iov;
+	int i, num_blocks;
+
+	num_blocks = 0;
+
+	for (i = 0; i < 4; i++) {
+		_iov_alloc_buf(&iovs[i], 4096 * (i + 1));
+		num_blocks += i + 1;
+	}
+
+	_iov_alloc_buf(&bounce_iov, (4096 + 128) * num_blocks);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 num_blocks, SPDK_DIF_GUARD_ERROR);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 num_blocks, SPDK_DIF_APPTAG_ERROR);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 num_blocks, SPDK_DIF_REFTAG_ERROR);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 num_blocks, SPDK_DIF_DATA_ERROR);
+
+	for (i = 0; i < 4; i++) {
+		_iov_free_buf(&iovs[i]);
+	}
+	_iov_free_buf(&bounce_iov);
+}
+
+static void
+dif_copy_sec_4096_md_128_inject_1_2_4_8_multi_iovs_split_test(void)
+{
+	struct iovec iovs[4], bounce_iov;
+	int i;
+
+	_iov_alloc_buf(&iovs[0], 2048);
+	_iov_alloc_buf(&iovs[1], 2048);
+	_iov_alloc_buf(&iovs[2], 1);
+	_iov_alloc_buf(&iovs[3], 4095);
+
+	_iov_alloc_buf(&bounce_iov, (4096 + 128) * 2);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 2, SPDK_DIF_GUARD_ERROR);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 2, SPDK_DIF_APPTAG_ERROR);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 2, SPDK_DIF_REFTAG_ERROR);
+
+	dif_copy_inject_error_and_verify(iovs, 4, &bounce_iov, 4096 + 128, 128,
+					 2, SPDK_DIF_DATA_ERROR);
+
+	for (i = 0; i < 4; i++) {
+		_iov_free_buf(&iovs[i]);
+	}
+	_iov_free_buf(&bounce_iov);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -938,7 +1044,11 @@ main(int argc, char **argv)
 		CU_add_test(suite, "dif_copy_sec_512_md_8_prchk_7_multi_iovs_split_data",
 			    dif_copy_sec_512_md_8_prchk_7_multi_iovs_split_data) == NULL ||
 		CU_add_test(suite, "dif_copy_sec_512_md_8_prchk_7_multi_iovs_complex_splits",
-			    dif_copy_sec_512_md_8_prchk_7_multi_iovs_complex_splits) == NULL
+			    dif_copy_sec_512_md_8_prchk_7_multi_iovs_complex_splits) == NULL ||
+		CU_add_test(suite, "dif_copy_sec_4096_md_128_inject_1_2_4_8_multi_iovs_test",
+			    dif_copy_sec_4096_md_128_inject_1_2_4_8_multi_iovs_test) == NULL ||
+		CU_add_test(suite, "dif_copy_sec_4096_md_128_inject_1_2_4_8_multi_iovs_split_test",
+			    dif_copy_sec_4096_md_128_inject_1_2_4_8_multi_iovs_split_test) == NULL
 	) {
 		CU_cleanup_registry();
 		return CU_get_error();
