88997d120c24fb0e037ed8adea7146e71f8272b8
Ziye Yang <optimistyzy@gmail.com>
2018-12-20 21:50:18 +0800

    nvmf: set the qpair state to active in new_qpair function.
    It is not exactly correct to set the state in
    spdk_nvmf_poll_group_poll function. The reason is that this
    function is called by spdk_nvmf_poll_group_poll with event
    function.
    
    And see the logic in spdk_nvmf_poll_group_poll,
    the state SPDK_NVMF_QPAIR_ACTIVATING is useless. so I think
    that put this state in new_qpair function is useful,
    we can use this state to track the qpair state, if the qpair
    is in SPDK_NVMF_QPAIR_ACTIVATING state, which means that
    the qpair is not added into the related group yet. And if
    there is disconnect operation when the qpair is in
    SPDK_NVMF_QPAIR_ACTIVATING state, we can use this state to
    defer the disconnect of this qpair.
    
    Also set the qpair->group later after it is really added into
    the group.
    
    Change-Id: I88ad724c898ce27e157df709f669c0e7d288a761
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/nvmf/nvmf.c
+++ lib/nvmf/nvmf.c
@@ -76,16 +76,6 @@ struct nvmf_qpair_disconnect_many_ctx {
 	void *cpl_ctx;
 };
 
-static void
-spdk_nvmf_qpair_set_state(struct spdk_nvmf_qpair *qpair,
-			  enum spdk_nvmf_qpair_state state)
-{
-	assert(qpair != NULL);
-	assert(qpair->group->thread == spdk_get_thread());
-
-	qpair->state = state;
-}
-
 static int
 spdk_nvmf_poll_group_poll(void *ctx)
 {
@@ -651,9 +641,6 @@ spdk_nvmf_poll_group_add(struct spdk_nvmf_poll_group *group,
 	struct spdk_nvmf_transport_poll_group *tgroup;
 
 	TAILQ_INIT(&qpair->outstanding);
-	qpair->group = group;
-	spdk_nvmf_qpair_set_state(qpair, SPDK_NVMF_QPAIR_ACTIVATING);
-
 	TAILQ_FOREACH(tgroup, &group->tgroups, link) {
 		if (tgroup->transport == qpair->transport) {
 			rc = spdk_nvmf_transport_poll_group_add(tgroup, qpair);
@@ -663,6 +650,7 @@ spdk_nvmf_poll_group_add(struct spdk_nvmf_poll_group *group,
 
 	/* We add the qpair to the group only it is succesfully added into the tgroup */
 	if (rc == 0) {
+		qpair->group = group;
 		TAILQ_INSERT_TAIL(&group->qpairs, qpair, link);
 		spdk_nvmf_qpair_set_state(qpair, SPDK_NVMF_QPAIR_ACTIVE);
 	}
