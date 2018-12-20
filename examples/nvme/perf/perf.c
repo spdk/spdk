ec70c3a8d380f1f3c18605592a3d066c60bfe3f2
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-12-20 11:06:14 +0900

    nvme_perf: Enable SGL for I/O commands.
    Most use cases of NVMe I/O commands have used SGL now, and
    upcoming DIF/DIX support will use it too. Hence this patch supports
    SGL and adds an option to enable SGL.
    
    Change-Id: I2ae185f7819964d6125e26fe27f5d02ffa2cac2e
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- examples/nvme/perf/perf.c
+++ examples/nvme/perf/perf.c
@@ -135,6 +135,8 @@ struct ns_worker_ctx {
 struct perf_task {
 	struct ns_worker_ctx	*ns_ctx;
 	void			*buf;
+	struct iovec		iov;
+	uint32_t		iov_offset;
 	uint64_t		submit_tsc;
 	uint16_t		appmask;
 	uint16_t		apptag;
@@ -183,6 +185,7 @@ static bool g_no_pci;
 static bool g_warn;
 static bool g_header_digest;
 static bool g_data_digest;
+static bool g_enable_iov;
 
 static const char *g_core_mask;
 
@@ -623,6 +626,31 @@ static void io_complete(void *ctx, const struct spdk_nvme_cpl *completion);
 
 static __thread unsigned int seed = 0;
 
+static void
+task_reset_sgl(void *ref, uint32_t sgl_offset)
+{
+	struct perf_task *task = ref;
+
+	task->iov_offset = sgl_offset;
+}
+
+static int
+task_next_sge(void *ref, void **address, uint32_t *length)
+{
+	struct perf_task *task = ref;
+
+	*address = task->iov.iov_base;
+	*length = task->iov.iov_len;
+
+	if (task->iov_offset) {
+		assert(task->iov_offset <= task->iov.iov_len);
+		*address += task->iov_offset;
+		*length -= task->iov_offset;
+	}
+
+	return 0;
+}
+
 static int
 submit_single_read_io(struct perf_task *task, struct ns_worker_ctx *ns_ctx,
 		      struct ns_entry *entry, uint64_t offset_in_ios)
@@ -640,12 +668,21 @@ submit_single_read_io(struct perf_task *task, struct ns_worker_ctx *ns_ctx,
 					   entry->io_size_blocks, false);
 		task->is_read = true;
 
-		rc = spdk_nvme_ns_cmd_read_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair,
-						   task->buf, NULL,
-						   task->lba,
-						   entry->io_size_blocks, io_complete,
-						   task, entry->io_flags,
-						   task->appmask, task->apptag);
+		if (!g_enable_iov) {
+			rc = spdk_nvme_ns_cmd_read_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair,
+							   task->buf, NULL,
+							   task->lba,
+							   entry->io_size_blocks, io_complete,
+							   task, entry->io_flags,
+							   task->appmask, task->apptag);
+		} else {
+			rc = spdk_nvme_ns_cmd_readv_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair,
+							    task->lba, entry->io_size_blocks,
+							    io_complete, task, entry->io_flags,
+							    task_reset_sgl, task_next_sge, NULL,
+							    task->appmask, task->apptag);
+		}
+
 	}
 
 	return rc;
@@ -667,12 +704,20 @@ submit_single_write_io(struct perf_task *task, struct ns_worker_ctx *ns_ctx,
 		task_extended_lba_setup_pi(entry, task, task->lba,
 					   entry->io_size_blocks, true);
 
-		rc = spdk_nvme_ns_cmd_write_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair,
-						    task->buf, NULL,
-						    task->lba,
-						    entry->io_size_blocks, io_complete,
-						    task, entry->io_flags,
-						    task->appmask, task->apptag);
+		if (!g_enable_iov) {
+			rc = spdk_nvme_ns_cmd_write_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair,
+							    task->buf, NULL,
+							    task->lba,
+							    entry->io_size_blocks, io_complete,
+							    task, entry->io_flags,
+							    task->appmask, task->apptag);
+		} else {
+			rc = spdk_nvme_ns_cmd_writev_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair,
+							     task->lba, entry->io_size_blocks,
+							     io_complete, task, entry->io_flags,
+							     task_reset_sgl, task_next_sge, NULL,
+							     task->appmask, task->apptag);
+		}
 	}
 
 	return rc;
@@ -800,6 +845,10 @@ allocate_task(struct ns_worker_ctx *ns_ctx, int queue_depth)
 		exit(1);
 	}
 	memset(task->buf, queue_depth % 8 + 1, max_io_size_bytes);
+
+	task->iov.iov_base = task->buf;
+	task->iov.iov_len = max_io_size_bytes;
+	task->iov_offset = 0;
 
 	task->ns_ctx = ns_ctx;
 
@@ -974,6 +1023,7 @@ static void usage(char *program_name)
 	printf("\t[-m max completions per poll]\n");
 	printf("\t\t(default: 0 - unlimited)\n");
 	printf("\t[-i shared memory group ID]\n");
+	printf("\t[-v enable SGL for I/O commands]\n");
 }
 
 static void
@@ -1350,6 +1400,9 @@ parse_args(int argc, char **argv)
 			break;
 		case 't':
 			g_time_in_sec = atoi(optarg);
+			break;
+		case 'v':
+			g_enable_iov = 1;
 			break;
 		case 'w':
 			workload_type = optarg;
