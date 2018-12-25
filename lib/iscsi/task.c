db371318ee17076ac0d9800e77dd397ebf38bf03
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2018-12-26 07:43:59 +0900

    test_only: investigate github issue 544
    Change-Id: I42d577fe0d676c1cd07039c39be59673dfe6ea49
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/iscsi/task.c
+++ lib/iscsi/task.c
@@ -48,6 +48,7 @@ spdk_iscsi_task_free(struct spdk_scsi_task *scsi_task)
 	}
 
 	spdk_iscsi_task_disassociate_pdu(task);
+	assert(task->mgmt_poller != NULL);
 	assert(task->conn->pending_task_cnt > 0);
 	task->conn->pending_task_cnt--;
 	spdk_mempool_put(g_spdk_iscsi.task_pool, (void *)task);
