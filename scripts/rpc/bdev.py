f1672c4015375ded37b2f81cd83647330951a910
Vitaliy Mysak <vitaliy.mysak@intel.com>
2018-12-28 18:59:40 +0000

    OCF: rpc: add stats method
    Add get_ocf_stats rpc method that
      returns available OCF statistics for specific OCF bdev
    
    Change-Id: If043a18c847fbeeddd8fdde1af9397e24bd90718
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- scripts/rpc/bdev.py
+++ scripts/rpc/bdev.py
@@ -71,6 +71,20 @@ def delete_ocf_bdev(client, name):
     return client.call('delete_ocf_bdev', params)
 
 
+def get_ocf_stats(client, name):
+    """Get statistics of chosen OCF block device
+
+    Args:
+        name: name of OCF bdev
+
+    Returns:
+        Statistics as json object
+    """
+    params = {'name': name}
+
+    return client.call('get_ocf_stats', params)
+
+
 def construct_malloc_bdev(client, num_blocks, block_size, name=None, uuid=None):
     """Construct a malloc block device.
 
