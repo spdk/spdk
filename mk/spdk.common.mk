9ee526395d7df7b9aa98afa27ff171a6f43d2225
Piotr Pelplinski <piotr.pelplinski@intel.com>
2019-01-02 10:28:02 +0100

    Makefile: Add possibility to uninstall spdk
    Please do not abandon
    Fixes #464
    
    This patch adds 'uninstall' target to makefile.
    'make uninstall' will remove spdk_tgt app, headers, libraries
    and shared libraries from system directories defined by $DESTDIR.
    Additionaly, if there will be any empty directories left after
    this operation, they will be removed as well.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I7b07fb4b81081d3914ff09165991fbe3a26b9067
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- mk/spdk.common.mk
+++ mk/spdk.common.mk
@@ -290,6 +290,12 @@ INSTALL_LIB=\
 	install -d -m 755 "$(DESTDIR)$(libdir)"; \
 	install -m 644 "$(LIB)" "$(DESTDIR)$(libdir)/"
 
+# Uninstall a library
+UNINSTALL_LIB=\
+	$(Q)echo "  UNINSTALL STATIC LIBRARIES at $(DESTDIR)$(libdir)/";\
+	rm -f $(DESTDIR)$(libdir)/libspdk_*.a; \
+	if [ $$(ls -A "$(DESTDIR)$(libdir)" | wc -l) -eq 0 ]; then rm -rf $(DESTDIR)$(libdir); fi
+
 ifeq ($(OS),FreeBSD)
 INSTALL_REL_SYMLINK := install -l rs
 else
@@ -311,22 +317,48 @@ INSTALL_SHARED_LIB=\
 	install -m $$perm_mode "$(SHARED_REALNAME_LIB)" "$(DESTDIR)$(libdir)/"; \
 	$(call spdk_install_lib_symlink,$(notdir $(SHARED_REALNAME_LIB)),$(notdir $(SHARED_LINKED_LIB)));
 
+# Uninstall an shared library
+UNINSTALL_SHARED_LIB=\
+	$(Q)echo "  UNINSTALL SHARED LIB $(DESTDIR)$(libdir)/$(notdir $(SHARED_LINKED_LIB))"; \
+	rm -f "$(DESTDIR)$(libdir)/$(notdir $(SHARED_LINKED_LIB))"; \
+	rm -f "$(DESTDIR)$(libdir)/$(notdir $(SHARED_REALNAME_LIB))"; \
+	if [ $$(ls -A "$(DESTDIR)$(libdir)" | wc -l) -eq 0 ]; then rm -rf $(DESTDIR)$(libdir); fi
+
+
 # Install an app binary
 INSTALL_APP=\
 	$(Q)echo "  INSTALL $(DESTDIR)$(bindir)/$(APP)"; \
 	install -d -m 755 "$(DESTDIR)$(bindir)"; \
 	install -m 755 "$(APP)" "$(DESTDIR)$(bindir)/"
 
+# Uninstall an app binary
+UNINSTALL_APP=\
+        $(Q)echo "  UNINSTALL BINARY $(DESTDIR)$(bindir)/$(APP)"; \
+	rm -f "$(DESTDIR)$(bindir)/$(APP)"; \
+	if [ $$(ls -A "$(DESTDIR)$(bindir)" | wc -l) -eq 0 ]; then rm -rf $(DESTDIR)$(bindir); fi
+
 INSTALL_EXAMPLE=\
 	$(Q)echo "  INSTALL $(DESTDIR)$(bindir)/spdk_$(strip $(subst /,_,$(subst $(SPDK_ROOT_DIR)/examples/, ,$(CURDIR))))"; \
 	install -d -m 755 "$(DESTDIR)$(bindir)"; \
 	install -m 755 "$(APP)" "$(DESTDIR)$(bindir)/spdk_$(strip $(subst /,_,$(subst $(SPDK_ROOT_DIR)/examples/, ,$(CURDIR))))"
 
+# Uninstall an example binary
+UNINSTALL_EXAMPLE=\
+	$(Q)echo "  UNINSTALL EXAMPLE $(DESTDIR)$(bindir)/spdk_$(strip $(subst /,_,$(subst $(SPDK_ROOT_DIR)/examples/, ,$(CURDIR))))"; \
+	rm -f "$(DESTDIR)$(bindir)/spdk_$(strip $(subst /,_,$(subst $(SPDK_ROOT_DIR)/examples/, ,$(CURDIR))))"; \
+	if [ $$(ls -A "$(DESTDIR)$(bindir)" | wc -l) -eq 0 ]; then rm -rf $(DESTDIR)$(bindir); fi
+
 # Install a header
 INSTALL_HEADER=\
 	$(Q)echo "  INSTALL $@"; \
 	install -d -m 755 "$(DESTDIR)$(includedir)/$(dir $(patsubst $(DESTDIR)$(includedir)/%,%,$@))"; \
-	install -m 644 "$(patsubst $(DESTDIR)$(includedir)/%,%,$@)" "$(DESTDIR)$(includedir)/$(dir $(patsubst $(DESTDIR)$(includedir)/%,%,$@))/"
+	install -m 644 "$(patsubst $(DESTDIR)$(includedir)/%,%,$@)" "$(DESTDIR)$(includedir)/$(dir $(patsubst $(DESTDIR)$(includedir)/%,%,$@))"
+
+# Uninstall a header
+UNINSTALL_HEADERS=\
+	$(Q)echo "  UNINSTALL HEADERS AT $(DESTDIR)$(includedir)/spdk"; \
+	rm -rf "$(DESTDIR)$(includedir)/spdk"; \
+	if [ $$(ls -A "$(DESTDIR)$(includedir)" | wc -l) -eq 0 ]; then rm -rf $(DESTDIR)$(includedir); fi
 
 %.o: %.c %.d $(MAKEFILE_LIST)
 	$(COMPILE_C)
