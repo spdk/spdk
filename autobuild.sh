a86af59ee6e55b4efb79350d95d592231060a3d7
Piotr Pelplinski <piotr.pelplinski@intel.com>
2019-01-02 10:28:02 +0100

    Makefile: Add possibility to uninstall spdk
    Please do not abandon
    Fixes #464
    
    This patch adds 'uninstall' target to makefile.
    'make uninstall' will remove spdk_tgt app, headers, libraries
    and shared libraries from system directories defined by $DESTDIR.
    Additionaly, if there will be any empty directories left after
    this operation, they will be removed as well.
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I7b07fb4b81081d3914ff09165991fbe3a26b9067
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -121,9 +121,21 @@ timing_enter make_install
 rm -rf /tmp/spdk
 mkdir /tmp/spdk
 $MAKE $MAKEFLAGS install DESTDIR=/tmp/spdk prefix=/usr
-ls -lR /tmp/spdk
-rm -rf /tmp/spdk
 timing_exit make_install
+
+# Test 'make uninstall'
+timing_enter make_uninstall
+touch /tmp/spdk/usr/lib/sample_xyz.a
+$MAKE $MAKEFLAGS uninstall DESTDIR=/tmp/spdk prefix=/usr
+ls -A /tmp/spdk/usr
+if [[ $(ls -A /tmp/spdk/usr/lib/ | wc -l) -ne 1 ]]; then
+	rm -rf /tmp/spdk
+	echo "Make uninstall failed"
+	exit 1
+else
+	rm -rf /tmp/spdk
+fi
+timing_exit make_uninstall
 
 timing_enter doxygen
 if [ $SPDK_BUILD_DOC -eq 1 ] && hash doxygen; then
