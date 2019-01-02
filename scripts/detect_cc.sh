627367c1178037ac268872b6aff21f1b9f134df2
Konrad Sztyber <konrad.sztyber@intel.com>
2019-01-02 13:47:47 +0100

    build: use linker specified by the LD env variable
    Modifed scripts/detect_cc.sh to take additional parameter specifing
    the linker to use. Default to LLD on FreeBSD systems.
    
    Change-Id: Idf97e9676a144028c0803d272ae6f0e903b0dd1f
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- scripts/detect_cc.sh
+++ scripts/detect_cc.sh
@@ -19,12 +19,13 @@ function usage()
 	err " -h, --help                Display this help and exit"
 	err " --cc=path                 C compiler to use"
 	err " --cxx=path                C++ compiler to use"
+	err " --ld=path                 Linker to use"
 	err " --lto=[y|n]               Attempt to configure for LTO"
-
 }
 
 CC=cc
 CXX=c++
+LD=ld
 LTO=n
 
 for i in "$@"; do
@@ -42,6 +43,9 @@ for i in "$@"; do
 		--lto=*)
 			LTO="${i#*=}"
 			;;
+		--ld=*)
+			LD="${i#*=}"
+			;;
 		--)
 			break
 			;;
@@ -54,12 +58,26 @@ done
 
 CC_TYPE=$($CC -v 2>&1 | grep -o -E '\w+ version' | head -1 | awk '{ print $1 }')
 CXX_TYPE=$($CXX -v 2>&1 | grep -o -E '\w+ version' | head -1 | awk '{ print $1 }')
-LD_TYPE=$(ld -v 2>&1 | awk '{print $2}')
-
 if [ "$CC_TYPE" != "$CXX_TYPE" ]; then
 	err "C compiler is $CC_TYPE but C++ compiler is $CXX_TYPE"
 	err "This may result in errors"
 fi
+
+LD_TYPE=$($LD --version 2>&1 | head -n1 | awk '{print $1, $2}')
+case "$LD_TYPE" in
+	"GNU ld"*)
+		LD_TYPE=bfd
+		;;
+	"GNU gold"*)
+		LD_TYPE=gold
+		;;
+	"LLD"*)
+		LD_TYPE=lld
+		;;
+	*)
+		err "Unsupported linker: $LD"
+		exit 1
+esac
 
 CCAR="ar"
 if [ "$LTO" = "y" ]; then
@@ -68,13 +86,23 @@ if [ "$LTO" = "y" ]; then
 			err "Using LTO with clang requires the gold linker."
 			exit 1
 		fi
-		 CCAR="llvm-ar"
+		CCAR="llvm-ar"
 	else
 		CCAR="gcc-ar"
 	fi
 fi
 
-echo "CC?=$CC"
-echo "CXX?=$CXX"
+function set_default() {
+	echo "ifeq (\$(origin $1),default)"
+	echo "$1 = $2"
+	echo "endif"
+	echo ""
+}
+
+set_default CC $CC
+set_default CXX $CXX
+set_default LD $LD
+
 echo "CCAR=$CCAR"
 echo "CC_TYPE=$CC_TYPE"
+echo "LD_TYPE=$LD_TYPE"
