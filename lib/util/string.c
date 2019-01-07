66457e3f27b59f41eaf5b7f73fd7e06ef6c864df
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-01-07 10:39:01 +0900

    util/string: Use vsnprintf to determine buffer size in sprintf_alloc
    This patch applies the idea used in sprintf_realloc to the existing
    sprintf_alloc.
    
    Using vsnprintf to determine the necessary buffer size eliminates
    complicated parts in sprintf_alloc.
    
    Change-Id: Ib1fcfff6e46405aa3b03a50a1a73b3abae593763
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/util/string.c
+++ lib/util/string.c
@@ -54,43 +54,29 @@ spdk_vsprintf_alloc(const char *format, va_list args)
 {
 	va_list args_copy;
 	char *buf;
-	size_t bufsize;
-	int rc;
+	int bufsize, rc;
 
-	/* Try with a small buffer first. */
-	bufsize = 32;
-
-	/* Limit maximum buffer size to something reasonable so we don't loop forever. */
-	while (bufsize <= 1024 * 1024) {
-		buf = malloc(bufsize);
-		if (buf == NULL) {
-			return NULL;
-		}
-
-		va_copy(args_copy, args);
-		rc = vsnprintf(buf, bufsize, format, args_copy);
-		va_end(args_copy);
-
-		/*
-		 * If vsnprintf() returned a count within our current buffer size, we are done.
-		 * The count does not include the \0 terminator, so rc == bufsize is not OK.
-		 */
-		if (rc >= 0 && (size_t)rc < bufsize) {
-			return buf;
-		}
-
-		/*
-		 * vsnprintf() should return the required space, but some libc versions do not
-		 * implement this correctly, so just double the buffer size and try again.
-		 *
-		 * We don't need the data in buf, so rather than realloc(), use free() and malloc()
-		 * again to avoid a copy.
-		 */
-		free(buf);
-		bufsize *= 2;
+	/* Necessary buffer size */
+	bufsize = get_buffer_size(format, args);
+	if (bufsize < 0) {
+		return NULL;
 	}
 
-	return NULL;
+	buf = malloc(bufsize);
+	if (buf == NULL) {
+		return NULL;
+	}
+
+	va_copy(args_copy, args);
+	rc = vsnprintf(buf, bufsize, format, args_copy);
+	va_end(args_copy);
+
+	if (rc < 0 || rc > bufsize) {
+		free(buf);
+		return NULL;
+	}
+
+	return buf;
 }
 
 char *
