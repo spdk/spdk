f2ec8ee513a69304fba83b81d61eabe2e33b51df
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-01-07 21:17:47 +0000

    OCF: tests: add construct-delete tests
    Add tests for creation and deletion of OCF bdev
    We explicitly check 'claimed' property of base devices
    Other issues like segfaults or custom SPDK errors
      also can be revealed by these tests
    
    Change-Id: Ia1d79df8db2e734e8d868a733655f69a0348de8e
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

---
+++ test/ocf/management/create-destruct.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+curdir=$(dirname $(readlink -f "$BASH_SOURCE"))
+rootdir=$(readlink -f $curdir/../../..)
+source $rootdir/test/common/autotest_common.sh
+
+rpc_py=$rootdir/scripts/rpc.py
+
+$rootdir/app/iscsi_tgt/iscsi_tgt -c "$curdir/environment.conf" &
+spdk_pid=$!
+
+trap "killprocess $spdk_pid; exit 1" SIGINT SIGTERM EXIT
+
+sleep 1
+
+$rpc_py construct_ocf_bdev PartCache wt Malloc0 NonExisting
+
+if ! bdev_check_claimed Malloc0; then
+	>&2 echo "Base device expected to be claimed now"
+	exit 1
+fi
+
+$rpc_py delete_ocf_bdev PartCache
+if bdev_check_claimed Malloc0; then
+	>&2 echo "Base device is not expected to be claimed now"
+	exit 1
+fi
+
+$rpc_py construct_ocf_bdev FullCache wt Malloc0 Malloc1
+
+if ! (bdev_check_claimed Malloc0 && bdev_check_claimed Malloc1); then
+	>&2 echo "Base devices expected to be claimed now"
+	exit 1
+fi
+
+$rpc_py delete_ocf_bdev FullCache
+if bdev_check_claimed Malloc0 && bdev_check_claimed Malloc1; then
+	>&2 echo "Base devices are not expected to be claimed now"
+	exit 1
+fi
+
+# check if shutdown of running CAS bdev is ok
+$rpc_py construct_ocf_bdev PartCache wt NonExisting Malloc1
+
+trap - SIGINT SIGTERM EXIT
+
+killprocess $spdk_pid
