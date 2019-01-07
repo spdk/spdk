3bce77bb8ece4a4d2928db097de6660a3e650990
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-01-07 21:17:47 +0000

    CAS: tests: add construct-delete tests
    Add tests for creation and deletion of CAS bdev
    We explicitly check 'claimed' property of base devices
    Other issues like segfaults or custom SPDK errors
      also can be revealed by these tests
    
    Change-Id: Ia1d79df8db2e734e8d868a733655f69a0348de8e
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

---
+++ test/cas/management/create-destruct.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+curdir=$(dirname $(readlink -f "$BASH_SOURCE"))
+rootdir=$(readlink -f $curdir/../../..)
+source $curdir/common.sh
+
+function check_claimed()
+{
+	status=$($rpc_py get_bdevs)
+	claimed=$(echo $status |  jq "map(select(.name == \"$1\")) | .[0].claimed")
+	if [[ $claimed == true ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function partial()
+{
+	sleep 1
+
+	$rpc_py construct_cas_bdev TestCache wt Malloc0 NonExisting
+
+	if ! check_claimed Malloc0; then
+		>&2 echo "Base device expected to be claimed now"
+		return 1
+	fi
+
+	$rpc_py delete_cas_bdev TestCache
+	if check_claimed Malloc0; then
+		>&2 echo "Base device is not expected to be claimed now"
+		return 1
+	fi
+
+	# check if shutdown of partial CAS bdev is ok
+	$rpc_py construct_cas_bdev TestCache wt NonExisting Malloc0
+}
+
+function full()
+{
+	sleep 1
+
+	$rpc_py construct_cas_bdev TestCache wt Malloc0 Malloc1
+
+	if ! (check_claimed Malloc0 && check_claimed Malloc1); then
+		>&2 echo "Base devices expected to be claimed now"
+		return 1
+	fi
+
+
+	$rpc_py delete_cas_bdev TestCache
+	if check_claimed Malloc0 && check_claimed Malloc1; then
+		>&2 echo "Base devices are not expected to be claimed now"
+		return 1
+	fi
+
+	# check if shutdown of running CAS bdev is ok
+	$rpc_py construct_cas_bdev TestCache wt Malloc0 Malloc1
+}
+
+with_spdk partial
+with_spdk full
