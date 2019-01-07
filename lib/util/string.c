6ad240fb57563c7a012fb19c516e118f943770a7
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-01-07 10:39:01 +0900

    util/string: Use vsnprintf to determine buffer size in sprintf_alloc
    This patch applies the idea used in sprintf_realloc to the existing
    sprintf_alloc.
    
    Using vsnprintf to determine the necessary buffer size eliminates
    complicated parts in sprintf_alloc.
    
    Change-Id: Ib1fcfff6e46405aa3b03a50a1a73b3abae593763
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/util/string.c
+++ lib/util/string.c
@@ -41,42 +41,24 @@ spdk_vsprintf_alloc(const char *format, va_list args)
 	va_list args_copy;
 	char *buf;
 	size_t bufsize;
-	int rc;
 
-	/* Try with a small buffer first. */
-	bufsize = 32;
+	va_copy(args_copy, args);
 
-	/* Limit maximum buffer size to something reasonable so we don't loop forever. */
-	while (bufsize <= 1024 * 1024) {
-		buf = malloc(bufsize);
-		if (buf == NULL) {
-			return NULL;
-		}
+	/* Determine necessary buffer size. */
+	bufsize = vsnprintf(NULL, 0, format, args_copy) + 1;
 
-		va_copy(args_copy, args);
-		rc = vsnprintf(buf, bufsize, format, args_copy);
+	buf = malloc(bufsize);
+	if (buf == NULL) {
 		va_end(args_copy);
 
-		/*
-		 * If vsnprintf() returned a count within our current buffer size, we are done.
-		 * The count does not include the \0 terminator, so rc == bufsize is not OK.
-		 */
-		if (rc >= 0 && (size_t)rc < bufsize) {
-			return buf;
-		}
-
-		/*
-		 * vsnprintf() should return the required space, but some libc versions do not
-		 * implement this correctly, so just double the buffer size and try again.
-		 *
-		 * We don't need the data in buf, so rather than realloc(), use free() and malloc()
-		 * again to avoid a copy.
-		 */
-		free(buf);
-		bufsize *= 2;
+		return NULL;
 	}
 
-	return NULL;
+	vsnprintf(buf, bufsize, format, args_copy);
+
+	va_end(args_copy);
+
+	return buf;
 }
 
 char *
