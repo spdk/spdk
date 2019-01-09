9d71562dd2ebf6737bdacde5f81f0968df9732fd
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-01-09 16:50:40 +0000

    CAS: tests: add flush test with bdevperf
    Add test for flush IO type
    We test correctness with bdevperf -w flush
      by watching if any errors produced during its run
    
    Change-Id: I07dcda5b976aa0d23e75ef2a312dd9780c6c39f3
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

---
+++ test/cas/integrity/bdevperf-iotypes.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curdir=$(dirname $(readlink -f "$BASH_SOURCE"))
+rootdir=$(readlink -f $curdir/../../..)
+source $curdir/common.sh
+
+bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -t 4 -w flush
