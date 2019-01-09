2cbc98864c590af225ad35dcd47977ca7652c9a4
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-01-09 16:50:40 +0000

    CAS: tests: add flush test with bdevperf
    Add test for flush IO type
    We test correctness with bdevperf -w flush
      by watching if any errors produced during its run
    
    Change-Id: I07dcda5b976aa0d23e75ef2a312dd9780c6c39f3
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- test/cas/integrity/common.sh
+++ test/cas/integrity/common.sh
@@ -2,6 +2,10 @@
 source $rootdir/test/common/autotest_common.sh
 plugindir=$rootdir/examples/bdev/fio_plugin
 
+function bdevperf(){
+	$rootdir/test/bdev/bdevperf/bdevperf $@
+}
+
 function fio_verify(){
 	sudo LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio $curdir/test.fio --ioengine=spdk_bdev $@
 }
