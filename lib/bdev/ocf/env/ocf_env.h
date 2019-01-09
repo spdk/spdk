0b3485c96871a39cc54489a5820889d2d2e1f7dc
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-01-09 18:29:05 +0000

    OCF: use mempool for env_allocators
    Use mempool for allocating OCF requests with constant size
    Previous method was using mallocs which is significantly slower
    
    Change-Id: I539ff22efc18fbd353ceb2687ea211d2baaa7523
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/bdev/ocf/env/ocf_env.h
+++ lib/bdev/ocf/env/ocf_env.h
@@ -155,7 +155,7 @@ static inline uint64_t env_get_free_memory(void)
 
 #define OCF_ALLOCATOR_NAME_MAX 128
 
-typedef struct _env_allocator env_allocator;
+typedef struct spdk_mempool env_allocator;
 
 env_allocator *env_allocator_create(uint32_t size, const char *name);
 
