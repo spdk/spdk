e5845e081faa00fe038efcc3f26bb189bfd47ba1
Piotr Pelplinski <piotr.pelplinski@intel.com>
2019-01-16 08:59:00 +0100

    ocf: unit tests
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I6b74e997bd4a4f14b465dcc1788a571c8424bac1

---
+++ test/unit/lib/bdev/vbdev_ocf.c/vbdev_ocf_ut.c
@@ -0,0 +1,95 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk_cunit.h"
+#include "spdk/string.h"
+#include "bdev/ocf/vbdev_ocf.c"
+
+int g_rc;
+
+static void delete_cb(int rc, void *cb_arg)
+{
+	g_rc = rc;
+}
+
+static void ut_ocf_init(void)
+{
+	struct vbdev_ocf *vbdev;
+	struct vbdev_ocf_base *base;
+	const char *vbdev_name = "cache1";
+	const char *cache_mode_name = "wt";
+	const char *cache_name = "fast";
+	const char *core_name = "slow";
+	int rc;
+
+	rc = vbdev_ocf_construct(vbdev_name, cache_mode_name, cache_name, core_name);
+	SPDK_CU_ASSERT_FATAL(rc != 0);
+
+	vbdev = vbdev_ocf_get_by_name(vbdev_name);
+	SPDK_CU_ASSERT_FATAL(vbdev != NULL);
+
+	base = vbdev_ocf_get_base_by_name(vbdev_name);
+	SPDK_CU_ASSERT_FATAL(rc != NULL);
+
+	rc = vbdev_ocf_delete(vbdev);
+	SPDK_CU_ASSERT_FATAL(rc != 0);
+}
+
+int main(int argc, char **argv)
+{
+	CU_pSuite	suite = NULL;
+	unsigned int	num_failures;
+
+	if (CU_initialize_registry() != CUE_SUCCESS) {
+		return CU_get_error();
+	}
+
+	suite = CU_add_suite("ocf", NULL, NULL);
+	if (suite == NULL) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (
+		CU_add_test(suite, "ut_ocf_init", ut_ocf_init) == NULL
+	) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	num_failures = CU_get_number_of_failures();
+	CU_cleanup_registry();
+	return num_failures;
+}
