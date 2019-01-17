67bed57dc665ed0baeb281b285ce55c71b7a59ac
Ziye Yang <optimistyzy@gmail.com>
2019-01-17 22:42:49 +0800

    nvmf/tcp: fix the issues of tcp requests recycling.
    For tcp requests are in TCP_REQUEST_STATE_TRANSFERRING_CONTROLLER_TO_HOST
    are not totally freed, because they may still in queued_c2h_data_tcp_req list.
    (Only part of data may be sent).
    
    Currrently, We only cleanup the list, but we did not free those reqs yet.
    So It is still need to call: spdk_nvmf_tcp_drain_state_queue on this state
    again.
    
    Moreover, if the tqpair is in EXITED state, we do not need to handle
    the queued_c2h_data_tcp_req, to avoid the recursive invoke when invoke
    pdu->cb_fn.
    
    Change-Id: I5856c3421019ec49d576d3dae4c62fefbb3925ca
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- lib/nvmf/tcp.c
+++ lib/nvmf/tcp.c
@@ -459,7 +459,9 @@ spdk_nvmf_tcp_cleanup_all_states(struct nvme_tcp_qpair *tqpair)
 	struct nvme_tcp_pdu *pdu, *tmp_pdu;
 
 	/* For the requests in TCP_REQUEST_STATE_TRANSFERRING_CONTROLLER_TO_HOST,
-	 * they can be refreed via the pdu in the send_queue
+	 * they can be freed via the folowing cases:
+	 * (1) The pdu in the send_queue;
+	 * (2) The requests are in queued_c2h_data_tcp_req.
 	  */
 	TAILQ_FOREACH_SAFE(pdu, &tqpair->send_queue, tailq, tmp_pdu) {
 		TAILQ_REMOVE(&tqpair->send_queue, pdu, tailq);
@@ -467,6 +469,11 @@ spdk_nvmf_tcp_cleanup_all_states(struct nvme_tcp_qpair *tqpair)
 		pdu->cb_fn(pdu->cb_arg);
 		spdk_nvmf_tcp_pdu_put(pdu);
 	}
+
+	TAILQ_FOREACH_SAFE(tcp_req, &tqpair->queued_c2h_data_tcp_req, link, req_tmp) {
+		TAILQ_REMOVE(&tqpair->queued_c2h_data_tcp_req, tcp_req, link);
+	}
+	spdk_nvmf_tcp_drain_state_queue(tqpair, TCP_REQUEST_STATE_TRANSFERRING_CONTROLLER_TO_HOST);
 
 	spdk_nvmf_tcp_drain_state_queue(tqpair, TCP_REQUEST_STATE_NEW);
 
@@ -480,11 +487,6 @@ spdk_nvmf_tcp_cleanup_all_states(struct nvme_tcp_qpair *tqpair)
 
 	spdk_nvmf_tcp_drain_state_queue(tqpair, TCP_REQUEST_STATE_NEED_BUFFER);
 	spdk_nvmf_tcp_drain_state_queue(tqpair, TCP_REQUEST_STATE_EXECUTING);
-
-	TAILQ_FOREACH_SAFE(tcp_req, &tqpair->queued_c2h_data_tcp_req, link, req_tmp) {
-		TAILQ_REMOVE(&tqpair->queued_c2h_data_tcp_req, tcp_req, link);
-	}
-
 	spdk_nvmf_tcp_drain_state_queue(tqpair, TCP_REQUEST_STATE_TRANSFERRING_HOST_TO_CONTROLLER);
 }
 
@@ -2388,6 +2390,11 @@ static void
 spdk_nvmf_tcp_handle_pending_c2h_data_queue(struct nvme_tcp_qpair *tqpair)
 {
 	struct nvme_tcp_req *tcp_req;
+
+	/* Do not handling the c2h_data_queue if the tqpair is in EXITED state */
+	if (spdk_unlikely(tqpair->state == NVME_TCP_QPAIR_STATE_EXITED)) {
+		return;
+	}
 
 	while (!TAILQ_EMPTY(&tqpair->queued_c2h_data_tcp_req) &&
 	       (tqpair->c2h_data_pdu_cnt < NVMF_TCP_QPAIR_MAX_C2H_PDU_NUM)) {
