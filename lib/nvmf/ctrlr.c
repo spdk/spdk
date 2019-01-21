e0af43cfeec28069b1e3e6293ac04b77ab760403
Changpeng Liu <changpeng.liu@intel.com>
2019-01-21 01:24:57 -0500

    nvmf: add reservation report command support
    Change-Id: Ib4de22c7020dbd8294f448f23c0c5c8c142629dd
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -2130,6 +2130,7 @@ spdk_nvmf_ctrlr_process_io_cmd(struct spdk_nvmf_request *req)
 	case SPDK_NVME_OPC_RESERVATION_REGISTER:
 	case SPDK_NVME_OPC_RESERVATION_ACQUIRE:
 	case SPDK_NVME_OPC_RESERVATION_RELEASE:
+	case SPDK_NVME_OPC_RESERVATION_REPORT:
 		spdk_thread_send_msg(ctrlr->subsys->thread, spdk_nvmf_subsystem_reservation_request, req);
 		return SPDK_NVMF_REQUEST_EXEC_STATUS_ASYNCHRONOUS;
 	default:
