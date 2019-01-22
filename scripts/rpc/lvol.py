92227425619ddad466ed6d753850a90469fcd3de
Maciej Szwed <maciej.szwed@intel.com>
2019-01-22 09:47:24 +0100

    lvol: add option to change default data erase method
    Some users require to do write zeroes operation when
    erasing data on lvol. Currently the default method is
    unmap. This patch adds flag to spdk_rpc_construct_lvol_bdev
    call that changes default erase method. This is also a base
    implementation for possible future function for erasing
    data on lvol bdev.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I8964f170b13c2268fe3c18104f7956c32be96040

--- scripts/rpc/lvol.py
+++ scripts/rpc/lvol.py
@@ -29,7 +29,7 @@ def rename_lvol_store(client, old_name, new_name):
     return client.call('rename_lvol_store', params)
 
 
-def construct_lvol_bdev(client, lvol_name, size, thin_provision=False, uuid=None, lvs_name=None):
+def construct_lvol_bdev(client, lvol_name, size, thin_provision=False, uuid=None, lvs_name=None, clear_method=None):
     """Create a logical volume on a logical volume store.
 
     Args:
@@ -54,6 +54,8 @@ def construct_lvol_bdev(client, lvol_name, size, thin_provision=False, uuid=None
         params['uuid'] = uuid
     if lvs_name:
         params['lvs_name'] = lvs_name
+    if clear_method:
+        params['clear_method'] = clear_method
     return client.call('construct_lvol_bdev', params)
 
 
