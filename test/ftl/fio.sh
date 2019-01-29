4018ed0e36aed8f106bc272d97e671e58caa8850
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-01-29 04:54:46 -0500

    ftl: extended nightly tests
    Change-Id: I8dc3e895f6b360f3ad81f157db502dd58629c5e6
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- test/ftl/fio.sh
+++ test/ftl/fio.sh
@@ -4,19 +4,36 @@ set -e
 
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
+plugindir=$rootdir/examples/bdev/fio_plugin
 
 source $rootdir/test/common/autotest_common.sh
 
-tests=(randw-verify randw-verify-j2 randw-verify-depth128)
-plugindir=$rootdir/examples/bdev/fio_plugin
+declare -A suite
+suite['basic']='randw-verify randw-verify-j2 randw-verify-depth128'
+suite['extended']='drive-prep randw-verify-qd128-ext randw randr randrw'
+
 device=$1
+tests=${suite[$2]}
+uuid=$3
 
 if [ ! -d /usr/src/fio ]; then
 	echo "FIO not available"
 	exit 1
 fi
 
-$rootdir/scripts/gen_ftl.sh -a $device -n nvme0 -l 0-3
+if [ -z "$tests" ]; then
+	echo "Invalid test suite '$2'"
+	exit 1
+fi
+
+export FTL_BDEV_CONF=$testdir/config/ftl.conf
+export FTL_BDEV_NAME=nvme0
+
+if [ -z "$uuid" ]; then
+	$rootdir/scripts/gen_ftl.sh -a $device -n nvme0 -l 0-3 > $FTL_BDEV_CONF
+else
+	$rootdir/scripts/gen_ftl.sh -a $device -n nvme0 -l 0-3 -u $uuid > $FTL_BDEV_CONF
+fi
 
 for test in ${tests[@]}; do
 	timing_enter $test
