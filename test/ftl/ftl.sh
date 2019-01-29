0699216fac06f5eb1aa27e164b4ecabd5c39c9e9
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-01-29 04:54:46 -0500

    ftl: extended nightly tests
    Change-Id: I8dc3e895f6b360f3ad81f157db502dd58629c5e6
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/442878
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- test/ftl/ftl.sh
+++ test/ftl/ftl.sh
@@ -4,12 +4,9 @@ set -e
 
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
+rpc_py=$rootdir/scripts/rpc.py
 
 source $rootdir/test/common/autotest_common.sh
-
-function ftl_kill() {
-	rm -f $testdir/.testfile_*
-}
 
 vendor_id='0x1d1d'
 device_id='0x1f1f'
@@ -20,20 +17,32 @@ if [ -z "$device" ]; then
 	exit 0
 fi
 
-trap "ftl_kill; exit 1" SIGINT SIGTERM EXIT
-
 timing_enter ftl
 timing_enter fio
 
-run_test suite $testdir/fio.sh $device
+run_test suite $testdir/fio.sh $device basic
 
 timing_exit fio
 
 timing_enter restore
-run_test suite $testdir/restore.sh $device $uuid
+run_test suite $testdir/restore.sh $device
 timing_exit restore
 
-timing_exit ftl
+if [ $SPDK_TEST_FTL_EXTENDED -eq 1 ]; then
+	$rootdir/test/app/bdev_svc/bdev_svc &
+	bdev_svc_pid=$!
 
-trap - SIGINT SIGTERM EXIT
-ftl_kill
+	trap "killprocess $bdev_svc_pid; exit 1" SIGINT SIGTERM EXIT
+
+	waitforlisten $bdev_svc_pid
+	uuid=$($rpc_py construct_ftl_bdev -b nvme0 -a $device -l 0-3 | jq -r '.uuid')
+	killprocess $bdev_svc_pid
+
+	trap - SIGINT SIGTERM EXIT
+
+	timing_enter fio_extended
+	run_test suite $testdir/fio.sh $device extended $uuid
+	timing_exit fio_extended
+fi
+
+timing_exit ftl
