d304f675cf3ea66f56fc2ffc04e06bcef1e85ff6
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-01-29 04:54:46 -0500

    ftl: extended nightly tests
    Change-Id: I8dc3e895f6b360f3ad81f157db502dd58629c5e6
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- scripts/gen_ftl.sh
+++ scripts/gen_ftl.sh
@@ -5,26 +5,18 @@ set -e
 rootdir=$(readlink -f $(dirname $0))/..
 
 function usage {
-	echo "Replaces FTL_* variables in config files inside the config/ directory."
-	echo "The following varaibles are replaced:"
-	echo "- FTL_CONF_DIR - config directory"
-	echo "- FTL_TRANSPORT_ADDR - SSD's PCIe address (defaults to first lnvm device)"
-	echo "- FTL_BDEV_NAME - name of the bdev"
-	echo "- FTL_BDEV_PUNITS - bdev's parallel unit range (e.g. 0-3)"
-	echo "- FTL_BDEV_UUID - bdev's uuid (used when in restore mode)"
-	echo
 	echo "Usage: $0 -a TRANSPORT_ADDR -n BDEV_NAME -l PUNITS [-u UUID]"
 	echo "UUID is required when restoring device state"
+	echo
+	echo "TRANSPORT_ADDR - SSD's PCIe address"
+	echo "BDEV_NAME - name of the bdev"
+	echo "PUNITS - bdev's parallel unit range (e.g. 0-3)"
+	echo "UUID - bdev's uuid (used when in restore mode)"
 }
 
 function generate_config {
-	fname=$1
-	output=${1%.in}
-
-	cp $fname $output
-	for var in ${!vmap[@]}; do
-		sed -i "s,$var,${vmap[$var]},g" $output
-	done
+	echo "[Ftl]"
+	echo "  TransportID \"trtype:PCIe traddr:$1\" $2 $3 $4"
 }
 
 while getopts ":a:n:l:m:u:" arg; do
@@ -45,13 +37,4 @@ if [[ -z "$addr" || -z "$name" || -z "$punits" ]]; then
 	exit 1
 fi
 
-declare -A vmap
-vmap[FTL_CONF_DIR]=$rootdir/test/ftl/config
-vmap[FTL_TRANSPORT_ADDR]=$addr
-vmap[FTL_BDEV_NAME]=$name
-vmap[FTL_BDEV_PUNITS]=$punits
-vmap[FTL_BDEV_UUID]=${uuid:-}
-
-for file in $(find $rootdir/test/ftl/config -type f -iname "*.in"); do
-	generate_config $file
-done
+generate_config $addr $name $punits $uuid
