0b8ee623f97eb8a52fb7390f14fb9bc5aaee2bcd
Ziye Yang <optimistyzy@gmail.com>
2019-01-29 22:50:41 +0800

    [Test] Debug the ceph issue with ISA-L enabling
    Change-Id: Ia04e92a94857d0a8431e5bca18454974bfea1ea4
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- mk/spdk.common.mk
+++ mk/spdk.common.mk
@@ -146,7 +146,7 @@ IPSEC_MB_DIR=$(SPDK_ROOT_DIR)/intel-ipsec-mb
 
 ISAL_DIR=$(SPDK_ROOT_DIR)/isa-l
 ifeq ($(CONFIG_ISAL), y)
-SYS_LIBS += -L$(ISAL_DIR)/.libs -lisal
+SYS_LIBS += -Wl,-rpath,$(ISAL_DIR)/.libs/libisal.a
 COMMON_CFLAGS += -I$(ISAL_DIR)/..
 endif
 
