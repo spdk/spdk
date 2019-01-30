df9fb7006c4ec46ec490e30e197a9630a16fd483
Seth Howell <seth.howell@intel.com>
2019-01-30 13:40:29 -0700

    nvmf: use io unit size in transport buffer pools
    When this structure was brought up to the generic layer, the tcp
    transport was using max_io_size and the rdma transport was using
    io_unit_size. In the interest of conserving memory, we should use
    io_unit_size instead of max_io_size.
    
    Change-Id: I2633306fcbfd8c3d557445959c745cb2d9a0999e
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvmf/transport.c
+++ lib/nvmf/transport.c
@@ -122,7 +122,7 @@ spdk_nvmf_transport_create(enum spdk_nvme_transport_type type,
 
 	transport->data_buf_pool = spdk_mempool_create(spdk_mempool_name,
 				   opts->num_shared_buffers,
-				   opts->max_io_size + NVMF_DATA_BUFFER_ALIGNMENT,
+				   opts->io_unit_size + NVMF_DATA_BUFFER_ALIGNMENT,
 				   SPDK_MEMPOOL_DEFAULT_CACHE_SIZE,
 				   SPDK_ENV_SOCKET_ID_ANY);
 
