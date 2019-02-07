2cb8b95481613018526019bd9f54428cd692aeb5
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-02-07 11:39:22 +0100

    [PoC] Add asynch events listener to rpc client
    Change-Id: I43f5797f6b529fd79ddcc6622d0e0d5b7b98235f
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- scripts/rpc/client.py
+++ scripts/rpc/client.py
@@ -2,6 +2,9 @@ import json
 import socket
 import time
 import os
+import threading
+
+from threading import Condition
 
 
 def print_dict(d):
@@ -18,6 +21,9 @@ class JSONRPCClient(object):
         self.verbose = verbose
         self.timeout = timeout
         self.request_id = 0
+        self.responses = {}
+        self.condition = Condition()
+        self.is_running = False
         try:
             if os.path.exists(addr):
                 self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -34,26 +40,45 @@ class JSONRPCClient(object):
             raise JSONRPCException("Error while connecting to %s\n"
                                    "Error details: %s" % (addr, ex))
 
+
     def __del__(self):
         self.sock.close()
 
-    def call(self, method, params={}, verbose=False):
-        self.request_id += 1
-        req = {}
-        req['jsonrpc'] = '2.0'
-        req['method'] = method
-        req['id'] = self.request_id
-        if (params):
-            req['params'] = params
-        reqstr = json.dumps(req)
 
-        verbose = verbose or self.verbose
+    def start(self):
+        self.thread = threading.Thread(target = self._recv_handler)
+        self.thread.start()
 
-        if verbose:
-            print("request:")
-            print(json.dumps(req, indent=2))
 
-        self.sock.sendall(reqstr.encode("utf-8"))
+    def stop(self):
+        self.is_running = False
+        self.condition.acquire()
+        self.condition.notify()
+        self.condition.release()
+        self.thread.join()
+
+
+    def add_handler(self, handler):
+        """ Add new listener for asynchronious RPC
+
+        NOTE: This implementation allows to add only one listener
+        """
+        self.handler = handler
+        if not self.is_running:
+            self.start()
+
+
+    def remove_handler(self, handler):
+        self.handler = False
+        self.stop()
+
+
+    def fire(self, response):
+        if self.handler:
+            self.handler(response)
+
+
+    def recv_response(self):
         buf = ''
         closed = False
         response = {}
@@ -78,19 +103,72 @@ class JSONRPCClient(object):
                 continue  # incomplete response; keep buffering
             break
 
+        if response:
+            self.fire(response)
+
+        return response
+
+
+    def _recv_handler(self):
+        self.is_running = True
+        while self.is_running:
+            response = self.recv_response()
+            if response:
+                self.condition.acquire()
+                self.responses[response['id']] = response
+                self.condition.notify()
+                self.condition.release()
+
+
+    def get_response(self, id=0, wait=True):
+        """
+        Get response with specified id
+        """
+
+        if not self.is_running:
+            return self.recv_response()
+
+        while self.is_running and wait:
+            self.condition.acquire()
+            response = self.responses.pop(id, False)
+            if response:
+                self.condition.release()
+                return response
+
+            self.condition.wait(self.timeout)
+            self.condition.release()
+        return False
+
+
+    def call(self, method, params={}, verbose=False):
+        self.request_id += 1
+        req = {}
+        req['jsonrpc'] = '2.0'
+        req['method'] = method
+        req['id'] = self.request_id
+        if (params):
+            req['params'] = params
+        reqstr = json.dumps(req)
+
+        verbose = verbose or self.verbose
+
+        if verbose:
+            print("request:")
+            print(json.dumps(req, indent=2))
+
+        self.sock.sendall(reqstr.encode("utf-8"))
+
+        response = self.get_response(req['id'])
+
         if not response:
             if method == "kill_instance":
                 return {}
-            if closed:
-                msg = "Connection closed with partial response:"
+            #if closed:
+            #    msg = "Connection closed with partial response:"
             else:
                 msg = "Timeout while waiting for response:"
-            msg = "\n".join([msg, buf])
+            #msg = "\n".join([msg, buf])
             raise JSONRPCException(msg)
-
-        if verbose:
-            print("response:")
-            print(json.dumps(response, indent=2))
 
         if 'error' in response:
             msg = "\n".join(["Got JSON-RPC error response",
