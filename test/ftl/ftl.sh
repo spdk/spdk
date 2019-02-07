a0b8ea29c30335950890513575cdb2c826c5232b
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2019-02-07 11:35:21 +0100

    [RFC] test/ftl: use OCSSD instead first NVMe like
    Change-Id: I175bebb68ea1752fda6fe80932cd27c30cf3dcff
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

--- test/ftl/ftl.sh
+++ test/ftl/ftl.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 
-set -e
-
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 
+source $rootdir/scripts/common.sh
 source $rootdir/test/common/autotest_common.sh
 
 function ftl_kill() {
 	rm -f $testdir/.testfile_*
 }
 
-vendor_id='0x1d1d'
-device_id='0x1f1f'
-device=$(lspci -d ${vendor_id}:${device_id} | cut -d' ' -f 1)
-
-if [ -z "$device" ]; then
-	echo "Could not find FTL device. Tests skipped."
-	exit 0
+if [[ -z "$OCSSD_PCI_DEVICES" -eq 0 ]]; then
+	echo "OCSSD device list is empty." 
+	echo "This test require that OCSSD_PCI_DEVICES environment variable to be set"
+	echo "and point to OCSSD devices PCI BDF. You can specify multiple space
+	echo "separated BDFs in this case first one will be used."
+	exit 1
 fi
+
+set -e
 
 trap "ftl_kill; exit 1" SIGINT SIGTERM EXIT
 
 timing_enter ftl
 timing_enter fio
 
+read device _ <<< "$OCSSD_PCI_DEVICES"
 run_test suite $testdir/fio.sh $device
 
 timing_exit fio
