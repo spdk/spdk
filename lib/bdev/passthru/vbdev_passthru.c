0d4c9ea375722c187193d3f5d638bf1098cb604b
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-02-12 17:16:29 +0900

    bdev/passthru: Use vbdev's name as io_device's name instead of base bdev's name
    Using passthru virtual bdev's name instead of base bdev's name as
    io_device's name will be meaningful. This patch doesn't change any
    behavior.
    
    Change-Id: I33f7aa78c60cd1d9f6a7b36280441bc559f44857
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/bdev/passthru/vbdev_passthru.c
+++ lib/bdev/passthru/vbdev_passthru.c
@@ -598,7 +598,7 @@ vbdev_passthru_register(struct spdk_bdev *bdev)
 
 		spdk_io_device_register(pt_node, pt_bdev_ch_create_cb, pt_bdev_ch_destroy_cb,
 					sizeof(struct pt_io_channel),
-					name->bdev_name);
+					name->vbdev_name);
 		SPDK_NOTICELOG("io_device created at: 0x%p\n", pt_node);
 
 		rc = spdk_bdev_open(bdev, true, vbdev_passthru_base_bdev_hotremove_cb,
