815f82b17bda83084c5b8153143c3d701d9db2d1
lorneli <lorneli@163.com>
2019-02-12 21:13:19 +0800

    nvme: mv submit_tick assignments to generic qpair code
    Move req->submit_tick assignments from specific transports to generic
    qpair code.
    
    Check whether submit_tick has been assigned before doing the actual
    assignment, because a request may be submitted several times and the
    original submit_tick shouldn't be covered.
    
    Change-Id: I2de8018dc21763eb5a19bb9d48dfbdef764b036e
    Signed-off-by: lorneli <lorneli@163.com>
    Reviewed-on: https://review.gerrithub.io/c/444702
    Reviewed-by: Ziye Yang <ziye.yang@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-by: Seth Howell <seth.howell5141@gmail.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -541,6 +541,16 @@ nvme_qpair_submit_request(struct spdk_nvme_qpair *qpair, struct nvme_request *re
 		}
 	}
 
+	/* assign submit_tick before submitting req to specific transport */
+	if (spdk_unlikely(ctrlr->timeout_enabled)) {
+		if (req->submit_tick == 0) { /* req submitted for the first time */
+			req->submit_tick = spdk_get_ticks();
+			req->timed_out = false;
+		}
+	} else {
+		req->submit_tick = 0;
+	}
+
 	return nvme_transport_qpair_submit_request(qpair, req);
 }
 
