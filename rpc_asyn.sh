d9376d0ce3a4d6bf0f8fce7b180fade91498fe0e
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2019-02-13 16:51:04 +0100

    [RFC][DO NOT MERGE] notfications simple test
    Just to share the idea.
    
    Change-Id: Ie5701d7864d91cfa70caee92f3e2a37a83fb8f14
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

---
+++ rpc_asyn.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -xe
+
+rootdir=$(readlink -f $(dirname $0))
+
+shopt -s expand_aliases
+
+function on_exit() {
+	trap - EXIT
+
+	if [[ -n "$n_pid" ]]; then
+		kill $n_pid;
+	fi
+
+
+}
+
+trap "on_exit" EXIT ERR
+
+: ${socket=$rootdir/../sandbox/vhost0/vhost.sock}
+
+alias rpc="$rootdir/scripts/rpc.py -s $socket"
+
+rpc -v get_notification_types
+sleep 1
+
+# TODO: max-count test
+rpc -v get_notifications -n 5 -t 100000 -m 2 &
+n_pid=$!
+
+sleep 0.5
+rpc -t 3600 construct_malloc_bdev -b Malloc2 32 512
+rpc -t 3600 construct_malloc_bdev -b Malloc2 32 512 || true
+rpc -t 3600 delete_malloc_bdev Malloc2
+rpc -t 3600 construct_malloc_bdev -b Malloc3 32 512
+rpc -t 3600 construct_malloc_bdev -b Malloc4 32 512
+rpc -t 3600 construct_malloc_bdev -b Malloc5 32 512
+rpc -t 3600 construct_malloc_bdev -b Malloc6 32 512
+rpc -t 3600 construct_malloc_bdev -b Malloc7 32 512
+rpc -t 3600 construct_malloc_bdev -b Malloc8 32 512
+
+
+
+#sleep 1
+rpc -t 3600 delete_malloc_bdev Malloc3
+rpc -t 3600 delete_malloc_bdev Malloc4
+
+sleep 1
+#rpc -t 3600 construct_malloc_bdev -b Malloc2 32 512
+#rpc -t 3600 delete_malloc_bdev Malloc2
+
+wait $n_pid
+
+trap - EXIT ERR
+echo "DONE"
