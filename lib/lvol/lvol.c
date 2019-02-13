7af70eaff3c2835d7d10b0cbf8c1afe123b7b53c
Maciej Szwed <maciej.szwed@intel.com>
2019-02-13 10:20:57 +0100

    blobstore: Make possible to remove snapshot if there is only one clone
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I800724b981af894e01e1912d0077c5b34a2ae634

--- lib/lvol/lvol.c
+++ lib/lvol/lvol.c
@@ -890,7 +890,7 @@ spdk_lvol_deletable(struct spdk_lvol *lvol)
 	size_t count;
 
 	spdk_blob_get_clones(lvol->lvol_store->blobstore, lvol->blob_id, NULL, &count);
-	return (count == 0);
+	return (count <= 2);
 }
 
 static void
