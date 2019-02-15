072c7362c3e3473eb8e997694e4658fe47e144d7
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-02-15 16:26:01 +0900

    dif: Insert DIF into stream data directly without using bounce buffer
    Traditionally DIF insertion and strip have been done by hardware.
    Hardware can insert and strip DIF from stream data efficiently
    without using another bounce buffer.
    
    This patch emulates that operation for DIF insertion.
    The next patch will emulate that operation for DIF strip.
    
    They will be utilized in transparent DIF support in iSCSI target,
    and then will be utilized in NVMe-oF target.
    
    Change-Id: I99d7c1194fb9d91c04a800efb674680e2d4ae3ae
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- include/spdk/dif.h
+++ include/spdk/dif.h
@@ -257,4 +257,20 @@ int spdk_dix_verify(struct iovec *iovs, int iovcnt, struct iovec *md_iov,
 int spdk_dix_inject_error(struct iovec *iovs, int iovcnt, struct iovec *md_iov,
 			  uint32_t num_blocks, const struct spdk_dif_ctx *ctx,
 			  uint32_t inject_flags, uint32_t *inject_offset);
+
+/**
+ * Insert DIF into stream payload. From the tail of the newly added data,
+ * shift block data, and generate and insert DIF among block data.
+ *
+ * \param buf Data buffer to which DIF is inserted.
+ * \param parsed_bytes Number of parsed bytes.
+ * \param new_bytes Number of newly stored bytes.
+ * \param ctx DIF context.
+ *
+ * return Number of parsed bytes after this call on success and negated errono otherwise.
+ * Return value should be used as the input to the next call on success.
+ */
+int spdk_dif_generate_stream(void *buf, uint32_t parsed_bytes, uint32_t new_bytes,
+			     const struct spdk_dif_ctx *ctx);
+
 #endif /* SPDK_DIF_H */
