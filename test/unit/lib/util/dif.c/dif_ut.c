b4421d3b56502e526437120234c10c12acdfb38c
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-02-18 09:41:16 +0900

    dif: Strip DIF from stream data directly without using bounce buffer
    Traditionally DIF insertion and strip have been done by hardware.
    Hardware can insert and strip DIF from stream data efficiently
    without using another bounce buffer.
    
    This patch emulates that operation for DIF strip.
    The previous patch will emulate that operation for DIF strip.
    
    ASAN complains about memcpy address overlapping and fix that by
    using memmove instead of memcpy.
    
    They will be utilized in transparent DIF support in iSCSI target,
    and then will be utilized in NVMe-oF target.
    
    Change-Id: I877eb5608b7bcaa7302542169c9a62f1127c394f
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/unit/lib/util/dif.c/dif_ut.c
+++ test/unit/lib/util/dif.c/dif_ut.c
@@ -1441,6 +1441,42 @@ dif_sec_512_md_8_stream_dif_insert_test(void)
 	_iov_free_buf(&iov);
 }
 
+static void
+dif_sec_512_md_8_stream_dif_strip_test(void)
+{
+	struct iovec iov;
+	struct spdk_dif_ctx dif_ctx;
+	struct spdk_dif_error err_blk = {};
+	uint32_t dif_flags;
+	int rc;
+
+	_iov_alloc_buf(&iov, 520 * 6);
+
+	dif_flags = SPDK_DIF_FLAGS_REFTAG_CHECK | SPDK_DIF_FLAGS_APPTAG_CHECK |
+		    SPDK_DIF_FLAGS_GUARD_CHECK;
+
+	rc = spdk_dif_ctx_init(&dif_ctx, 520, 8, true, false, SPDK_DIF_TYPE1,
+			       dif_flags, 22, 0xFFFF, 0x22, 0);
+	CU_ASSERT(rc == 0);
+
+	rc = ut_data_pattern_generate(&iov, 1, 520, 8, 5);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_generate(&iov, 1, 5, &dif_ctx);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_verify(&iov, 1, 5, &dif_ctx, &err_blk);
+	CU_ASSERT(rc == 0);
+
+	rc = spdk_dif_verify_strip(&iov, 1, 520 * 5, &dif_ctx, &err_blk);
+	CU_ASSERT(rc == 512 * 5);
+
+	rc = ut_data_pattern_verify(&iov, 1, 512, 0, 5);
+	CU_ASSERT(rc == 0);
+
+	_iov_free_buf(&iov);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -1526,7 +1562,9 @@ main(int argc, char **argv)
 		CU_add_test(suite, "dix_sec_4096_md_128_inject_1_2_4_8_multi_iovs_split_test",
 			    dix_sec_4096_md_128_inject_1_2_4_8_multi_iovs_split_test) == NULL ||
 		CU_add_test(suite, "dif_sec_512_md_8_stream_dif_insert_test",
-			    dif_sec_512_md_8_stream_dif_insert_test) == NULL
+			    dif_sec_512_md_8_stream_dif_insert_test) == NULL ||
+		CU_add_test(suite, "dif_sec_512_md_8_stream_dif_strip_test",
+			    dif_sec_512_md_8_stream_dif_strip_test) == NULL
 	) {
 		CU_cleanup_registry();
 		return CU_get_error();
