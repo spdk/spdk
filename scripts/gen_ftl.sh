cd3be1dc23dfca97769c74f3b87ac237bae68454
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-02-19 05:06:12 -0500

    bdev/ftl: Add json config generation
    Change-Id: I0baa6cfb4e4cb2a5ae68de730edb75011bd8e49d
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- scripts/gen_ftl.sh
+++ scripts/gen_ftl.sh
@@ -8,6 +8,7 @@ function usage {
 	echo "Usage: $0 -a TRANSPORT_ADDR -n BDEV_NAME -l PUNITS [-u UUID] [-c CACHE]"
 	echo "UUID is required when restoring device state"
 	echo
+	echo "-j json format"
 	echo "TRANSPORT_ADDR - SSD's PCIe address"
 	echo "BDEV_NAME - name of the bdev"
 	echo "PUNITS - bdev's parallel unit range (e.g. 0-3)"
@@ -15,15 +16,40 @@ function usage {
 	echo "CACHE - name of the bdev to be used as write buffer cache"
 }
 
-function generate_config {
+function create_classic_config {
 	echo "[Ftl]"
 	echo "  TransportID \"trtype:PCIe traddr:$1\" $2 $3 $4 $5"
 }
 
+function create_json_config()
+{
+	echo "{"
+	echo '"subsystem": "bdev",'
+	echo '"config": ['
+	echo '{'
+	echo '"method": "construct_ftl_bdev",'
+	echo '"params": {'
+	echo "\"name\": \"$2\","
+	echo '"trtype": "PCIe",'
+	echo "\"traddr\": \"$1\","
+	echo "\"punits\": \"$3\","
+	if [ -n "$5" ]; then
+		echo "\"uuid\": \"$4\","
+		echo "\"cache\": \"$5\""
+	else
+		echo "\"uuid\": \"$4\""
+	fi
+	echo '}'
+	echo '}'
+	echo ']'
+	echo '}'
+}
+
 uuid=00000000-0000-0000-0000-000000000000
 
-while getopts ":a:n:l:m:u:c:" arg; do
+while getopts "ja:n:l:m:u:c:" arg; do
 	case "$arg" in
+		j)	json=1		;;
 		a)	addr=$OPTARG	;;
 		n)	name=$OPTARG	;;
 		l)	punits=$OPTARG	;;
@@ -41,4 +67,8 @@ if [[ -z "$addr" || -z "$name" || -z "$punits" ]]; then
 	exit 1
 fi
 
-generate_config $addr $name $punits $uuid $cache
+if [ -n "$json" ]; then
+	create_json_config $addr $name $punits $uuid $cache
+else
+	create_classic_config $addr $name $punits $uuid $cache
+fi
