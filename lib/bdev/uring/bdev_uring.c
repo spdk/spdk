c74b11dd1cf419f930706a3b76bac74bec4b5db1
Vishal Verma <vishal4.verma@intel.com>
2019-02-19 10:06:19 -0700

    [RFC] bdev: Add io_uring bdev module
    This adds bdev io_uring module. This is WIP.
    
    Change-Id: I9a8171d7c871673b189bff59a89d06679da4e191
    Signed-off-by: Vishal Verma <vishal4.verma@intel.com>

---
+++ lib/bdev/uring/bdev_uring.c
@@ -0,0 +1,711 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "bdev_uring.h"
+
+#include "spdk/stdinc.h"
+
+#include "spdk/barrier.h"
+#include "spdk/bdev.h"
+#include "spdk/conf.h"
+#include "spdk/env.h"
+#include "spdk/fd.h"
+#include "spdk/likely.h"
+#include "spdk/thread.h"
+#include "spdk/json.h"
+#include "spdk/util.h"
+#include "spdk/string.h"
+
+#include "spdk_internal/log.h"
+
+#include "liburing.h"
+
+struct bdev_uring_io_channel {
+	uint64_t				io_inflight;
+	struct bdev_uring_group_channel		*group_ch;
+};
+
+struct bdev_uring_group_channel {
+	struct spdk_poller			*poller;
+	struct io_uring				uring;
+};
+
+struct bdev_uring_task {
+	struct io_uring			uring;
+	uint64_t			len;
+	struct bdev_uring_io_channel	*ch;
+	TAILQ_ENTRY(bdev_uring_task)	link;
+};
+
+struct file_disk {
+	struct bdev_uring_task	*reset_task;
+	struct spdk_poller	*reset_retry_timer;
+	struct spdk_bdev	disk;
+	char			*filename;
+	int			fd;
+	TAILQ_ENTRY(file_disk)  link;
+	bool			block_size_override;
+};
+
+/* For user space reaping of completions */
+struct spdk_uring_ring {
+	uint32_t id;
+	uint32_t size;
+	uint32_t head;
+	uint32_t tail;
+
+	uint32_t version;
+	uint32_t compat_features;
+	uint32_t incompat_features;
+	uint32_t header_length;
+};
+
+#define SPDK_AIO_RING_VERSION	0xa10a10a1
+
+static int bdev_uring_initialize(void);
+static void bdev_uring_fini(void);
+static void uring_free_disk(struct file_disk *fdisk);
+static void bdev_uring_get_spdk_running_config(FILE *fp);
+static TAILQ_HEAD(, file_disk) g_uring_disk_head;
+
+#define SPDK_URING_QUEUE_DEPTH 128
+#define MAX_EVENTS_PER_POLL 32
+
+static int
+bdev_uring_get_ctx_size(void)
+{
+	return sizeof(struct bdev_uring_task);
+}
+
+static struct spdk_bdev_module uring_if = {
+	.name		= "uring",
+	.module_init	= bdev_uring_initialize,
+	.module_fini	= bdev_uring_fini,
+	.config_text	= bdev_uring_get_spdk_running_config,
+	.get_ctx_size	= bdev_uring_get_ctx_size,
+};
+
+SPDK_BDEV_MODULE_REGISTER(uring, &uring_if)
+
+static int
+bdev_uring_open(struct file_disk *disk)
+{
+	int fd;
+
+	fd = open(disk->filename, O_NOATIME | O_DIRECT);
+	if (fd < 0) {
+		/* Try without O_DIRECT for non-disk files */
+		fd = open(disk->filename, O_RDWR);
+		if (fd < 0) {
+			SPDK_ERRLOG("open() failed (file:%s), errno %d: %s\n",
+				    disk->filename, errno, spdk_strerror(errno));
+			disk->fd = -1;
+			return -1;
+		}
+	}
+
+	disk->fd = fd;
+
+	return 0;
+}
+
+static int
+bdev_uring_close(struct file_disk *disk)
+{
+	int rc;
+
+	if (disk->fd == -1) {
+		return 0;
+	}
+
+	rc = close(disk->fd);
+	if (rc < 0) {
+		SPDK_ERRLOG("close() failed (fd=%d), errno %d: %s\n",
+			    disk->fd, errno, spdk_strerror(errno));
+		return -1;
+	}
+
+	disk->fd = -1;
+
+	return 0;
+}
+
+static int64_t
+bdev_uring_readv(struct file_disk *fdisk, struct spdk_io_channel *ch,
+		 struct bdev_uring_task *uring_task,
+		 struct iovec *iov, int iovcnt, uint64_t nbytes, uint64_t offset)
+{
+	struct bdev_uring_io_channel *uring_ch = spdk_io_channel_get_ctx(ch);
+	struct io_uring *uring = &uring_ch->group_ch->uring;
+	int rc;
+	struct io_uring_sqe *sqe;
+
+	sqe = io_uring_get_sqe(uring);
+	io_uring_prep_readv(sqe, fdisk->fd, iov, iovcnt, offset);
+	io_uring_sqe_set_data(sqe, uring_task);
+	uring_task->len = nbytes;
+	uring_task->ch = uring_ch;
+
+	SPDK_DEBUGLOG(SPDK_LOG_URING, "read %d iovs size %lu to off: %#lx\n",
+		      iovcnt, nbytes, offset);
+
+	rc = io_uring_submit(uring);
+	if (rc < 0) {
+		if (rc == -EAGAIN) {
+			spdk_bdev_io_complete(spdk_bdev_io_from_ctx(uring_task), SPDK_BDEV_IO_STATUS_NOMEM);
+		} else {
+			spdk_bdev_io_complete(spdk_bdev_io_from_ctx(uring_task), SPDK_BDEV_IO_STATUS_FAILED);
+			SPDK_ERRLOG("%s: io_submit returned %d\n", __func__, rc);
+		}
+		return -1;
+	}
+	uring_ch->io_inflight++;
+	return nbytes;
+}
+
+static int64_t
+bdev_uring_writev(struct file_disk *fdisk, struct spdk_io_channel *ch,
+		  struct bdev_uring_task *uring_task,
+		  struct iovec *iov, int iovcnt, size_t len, uint64_t offset)
+{
+	struct io_uring *uring = &uring_task->uring;
+	struct bdev_uring_io_channel *uring_ch = spdk_io_channel_get_ctx(ch);
+	int rc;
+	struct io_uring_sqe *sqe;
+
+	sqe = io_uring_get_sqe(uring);
+	io_uring_prep_writev(sqe, fdisk->fd, iov, iovcnt, offset);
+	io_uring_sqe_set_data(sqe, uring_task);
+	uring_task->ch = uring_ch;
+
+	SPDK_DEBUGLOG(SPDK_LOG_URING, "write %d iovs size %lu from off: %#lx\n",
+		      iovcnt, len, offset);
+
+	rc = io_uring_submit(&uring_ch->group_ch->uring);
+	if (rc < 0) {
+		if (rc == -EAGAIN) {
+			spdk_bdev_io_complete(spdk_bdev_io_from_ctx(uring_task), SPDK_BDEV_IO_STATUS_NOMEM);
+		} else {
+			spdk_bdev_io_complete(spdk_bdev_io_from_ctx(uring_task), SPDK_BDEV_IO_STATUS_FAILED);
+			SPDK_ERRLOG("%s: io_submit returned %d\n", __func__, rc);
+		}
+		return -1;
+	}
+	uring_ch->io_inflight++;
+	return len;
+}
+
+static void
+bdev_uring_flush(struct file_disk *fdisk, struct bdev_uring_task *uring_task)
+{
+	int rc = fsync(fdisk->fd);
+
+	spdk_bdev_io_complete(spdk_bdev_io_from_ctx(uring_task),
+			      rc == 0 ? SPDK_BDEV_IO_STATUS_SUCCESS : SPDK_BDEV_IO_STATUS_FAILED);
+}
+
+static int
+bdev_uring_destruct(void *ctx)
+{
+	struct file_disk *fdisk = ctx;
+	int rc = 0;
+
+	TAILQ_REMOVE(&g_uring_disk_head, fdisk, link);
+	rc = bdev_uring_close(fdisk);
+	if (rc < 0) {
+		SPDK_ERRLOG("bdev_uring_close() failed\n");
+	}
+	spdk_io_device_unregister(fdisk, NULL);
+	uring_free_disk(fdisk);
+	return rc;
+}
+
+static int
+bdev_uring_group_poll(void *arg)
+{
+	struct bdev_uring_group_channel *group_ch = arg;
+	int nr;
+	enum spdk_bdev_io_status status;
+	struct bdev_uring_task *uring_task;
+	struct io_uring_cqe *cqe;
+
+	nr = io_uring_wait_completion(&group_ch->uring, &cqe);
+	if (nr < 0) {
+		return -1;
+	}
+
+	uring_task = (struct bdev_uring_task *)cqe->user_data;
+	if (cqe->res != (signed) uring_task->len) {
+		status = SPDK_BDEV_IO_STATUS_FAILED;
+	} else {
+		status = SPDK_BDEV_IO_STATUS_SUCCESS;
+	}
+
+	spdk_bdev_io_complete(spdk_bdev_io_from_ctx(uring_task), status);
+	uring_task->ch->io_inflight--;
+
+	return 1;
+}
+
+static void
+_bdev_uring_get_io_inflight(struct spdk_io_channel_iter *i)
+{
+	struct spdk_io_channel *ch = spdk_io_channel_iter_get_channel(i);
+	struct bdev_uring_io_channel *uring_ch = spdk_io_channel_get_ctx(ch);
+
+	if (uring_ch->io_inflight) {
+		spdk_for_each_channel_continue(i, -1);
+		return;
+	}
+
+	spdk_for_each_channel_continue(i, 0);
+}
+
+static int bdev_uring_reset_retry_timer(void *arg);
+
+static void
+_bdev_uring_get_io_inflight_done(struct spdk_io_channel_iter *i, int status)
+{
+	struct file_disk *fdisk = spdk_io_channel_iter_get_ctx(i);
+
+	if (status == -1) {
+		fdisk->reset_retry_timer = spdk_poller_register(bdev_uring_reset_retry_timer, fdisk, 500);
+		return;
+	}
+
+	spdk_bdev_io_complete(spdk_bdev_io_from_ctx(fdisk->reset_task), SPDK_BDEV_IO_STATUS_SUCCESS);
+}
+
+static int
+bdev_uring_reset_retry_timer(void *arg)
+{
+	struct file_disk *fdisk = arg;
+
+	if (fdisk->reset_retry_timer) {
+		spdk_poller_unregister(&fdisk->reset_retry_timer);
+	}
+
+	spdk_for_each_channel(fdisk,
+			      _bdev_uring_get_io_inflight,
+			      fdisk,
+			      _bdev_uring_get_io_inflight_done);
+
+	return -1;
+}
+
+static void
+bdev_uring_reset(struct file_disk *fdisk, struct bdev_uring_task *uring_task)
+{
+	fdisk->reset_task = uring_task;
+
+	bdev_uring_reset_retry_timer(fdisk);
+}
+
+static void bdev_uring_get_buf_cb(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io)
+{
+	switch (bdev_io->type) {
+	case SPDK_BDEV_IO_TYPE_READ:
+		bdev_uring_readv((struct file_disk *)bdev_io->bdev->ctxt,
+				 ch,
+				 (struct bdev_uring_task *)bdev_io->driver_ctx,
+				 bdev_io->u.bdev.iovs,
+				 bdev_io->u.bdev.iovcnt,
+				 bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen,
+				 bdev_io->u.bdev.offset_blocks * bdev_io->bdev->blocklen);
+		break;
+	case SPDK_BDEV_IO_TYPE_WRITE:
+		bdev_uring_writev((struct file_disk *)bdev_io->bdev->ctxt,
+				  ch,
+				  (struct bdev_uring_task *)bdev_io->driver_ctx,
+				  bdev_io->u.bdev.iovs,
+				  bdev_io->u.bdev.iovcnt,
+				  bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen,
+				  bdev_io->u.bdev.offset_blocks * bdev_io->bdev->blocklen);
+		break;
+	default:
+		SPDK_ERRLOG("Wrong io type\n");
+		break;
+	}
+}
+
+static int _bdev_uring_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io)
+{
+	switch (bdev_io->type) {
+	/* Read and write operations must be performed on buffers aligned to
+	 * bdev->required_alignment. If user specified unaligned buffers,
+	 * get the aligned buffer from the pool by calling spdk_bdev_io_get_buf. */
+	case SPDK_BDEV_IO_TYPE_READ:
+	case SPDK_BDEV_IO_TYPE_WRITE:
+		spdk_bdev_io_get_buf(bdev_io, bdev_uring_get_buf_cb,
+				     bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
+		return 0;
+	case SPDK_BDEV_IO_TYPE_FLUSH:
+		bdev_uring_flush((struct file_disk *)bdev_io->bdev->ctxt,
+				 (struct bdev_uring_task *)bdev_io->driver_ctx);
+		return 0;
+
+	case SPDK_BDEV_IO_TYPE_RESET:
+		bdev_uring_reset((struct file_disk *)bdev_io->bdev->ctxt,
+				 (struct bdev_uring_task *)bdev_io->driver_ctx);
+		return 0;
+	default:
+		return -1;
+	}
+}
+
+static void bdev_uring_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io)
+{
+	if (_bdev_uring_submit_request(ch, bdev_io) < 0) {
+		spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_FAILED);
+	}
+}
+
+static bool
+bdev_uring_io_type_supported(void *ctx, enum spdk_bdev_io_type io_type)
+{
+	switch (io_type) {
+	case SPDK_BDEV_IO_TYPE_READ:
+	case SPDK_BDEV_IO_TYPE_WRITE:
+	case SPDK_BDEV_IO_TYPE_FLUSH:
+	case SPDK_BDEV_IO_TYPE_RESET:
+		return true;
+
+	default:
+		return false;
+	}
+}
+
+static int
+bdev_uring_create_cb(void *io_device, void *ctx_buf)
+{
+	struct bdev_uring_io_channel *ch = ctx_buf;
+
+	ch->group_ch = spdk_io_channel_get_ctx(spdk_get_io_channel(&uring_if));
+
+	return 0;
+}
+
+static void
+bdev_uring_destroy_cb(void *io_device, void *ctx_buf)
+{
+	struct bdev_uring_io_channel *ch = ctx_buf;
+
+	spdk_put_io_channel(spdk_io_channel_from_ctx(ch->group_ch));
+}
+
+static struct spdk_io_channel *
+bdev_uring_get_io_channel(void *ctx)
+{
+	struct file_disk *fdisk = ctx;
+
+	return spdk_get_io_channel(fdisk);
+}
+
+
+static const struct spdk_bdev_fn_table uring_fn_table = {
+	.destruct		= bdev_uring_destruct,
+	.submit_request		= bdev_uring_submit_request,
+	.io_type_supported	= bdev_uring_io_type_supported,
+	.get_io_channel		= bdev_uring_get_io_channel,
+};
+
+static void uring_free_disk(struct file_disk *fdisk)
+{
+	if (fdisk == NULL) {
+		return;
+	}
+	free(fdisk->filename);
+	free(fdisk->disk.name);
+	free(fdisk);
+}
+
+static int
+bdev_uring_group_create_cb(void *io_device, void *ctx_buf)
+{
+	struct bdev_uring_group_channel *ch = ctx_buf;
+
+	if (io_uring_queue_init(SPDK_URING_QUEUE_DEPTH, &ch->uring, IORING_SETUP_IOPOLL) < 0) {
+		SPDK_ERRLOG("uring I/O context setup failure\n");
+		return -1;
+	}
+
+	ch->poller = spdk_poller_register(bdev_uring_group_poll, ch, 0);
+	return 0;
+}
+
+static void
+bdev_uring_group_destroy_cb(void *io_device, void *ctx_buf)
+{
+	struct bdev_uring_group_channel *ch = ctx_buf;
+
+	close(ch->uring.ring_fd);
+	io_uring_queue_exit(&ch->uring);
+
+	spdk_poller_unregister(&ch->poller);
+}
+
+struct spdk_bdev *
+create_uring_disk(const char *name, const char *filename, uint32_t block_size)
+{
+	struct file_disk *fdisk;
+	uint32_t detected_block_size;
+	uint64_t disk_size;
+	int rc;
+
+	fdisk = calloc(1, sizeof(*fdisk));
+	if (!fdisk) {
+		SPDK_ERRLOG("Unable to allocate enough memory for uring backend\n");
+		return NULL;
+	}
+
+	fdisk->filename = strdup(filename);
+	if (!fdisk->filename) {
+		goto error_return;
+	}
+
+	if (bdev_uring_open(fdisk)) {
+		SPDK_ERRLOG("Unable to open file %s. fd: %d errno: %d\n", filename, fdisk->fd, errno);
+		goto error_return;
+	}
+
+	disk_size = spdk_fd_get_size(fdisk->fd);
+
+	fdisk->disk.name = strdup(name);
+	if (!fdisk->disk.name) {
+		goto error_return;
+	}
+	fdisk->disk.product_name = "URING disk";
+	fdisk->disk.module = &uring_if;
+
+	fdisk->disk.write_cache = 1;
+
+	detected_block_size = spdk_fd_get_blocklen(fdisk->fd);
+	if (block_size == 0) {
+		/* User did not specify block size - use autodetected block size. */
+		if (detected_block_size == 0) {
+			SPDK_ERRLOG("Block size could not be auto-detected\n");
+			goto error_return;
+		}
+		fdisk->block_size_override = false;
+		block_size = detected_block_size;
+	} else {
+		if (block_size < detected_block_size) {
+			SPDK_ERRLOG("Specified block size %" PRIu32 " is smaller than "
+				    "auto-detected block size %" PRIu32 "\n",
+				    block_size, detected_block_size);
+			goto error_return;
+		} else if (detected_block_size != 0 && block_size != detected_block_size) {
+			SPDK_WARNLOG("Specified block size %" PRIu32 " does not match "
+				     "auto-detected block size %" PRIu32 "\n",
+				     block_size, detected_block_size);
+		}
+		fdisk->block_size_override = true;
+	}
+
+	if (block_size < 512) {
+		SPDK_ERRLOG("Invalid block size %" PRIu32 " (must be at least 512).\n", block_size);
+		goto error_return;
+	}
+
+	if (!spdk_u32_is_pow2(block_size)) {
+		SPDK_ERRLOG("Invalid block size %" PRIu32 " (must be a power of 2.)\n", block_size);
+		goto error_return;
+	}
+
+	fdisk->disk.blocklen = block_size;
+	fdisk->disk.required_alignment = spdk_u32log2(block_size);
+
+	if (disk_size % fdisk->disk.blocklen != 0) {
+		SPDK_ERRLOG("Disk size %" PRIu64 " is not a multiple of block size %" PRIu32 "\n",
+			    disk_size, fdisk->disk.blocklen);
+		goto error_return;
+	}
+
+	fdisk->disk.blockcnt = disk_size / fdisk->disk.blocklen;
+	fdisk->disk.ctxt = fdisk;
+
+	fdisk->disk.fn_table = &uring_fn_table;
+
+	spdk_io_device_register(fdisk, bdev_uring_create_cb, bdev_uring_destroy_cb,
+				sizeof(struct bdev_uring_io_channel),
+				fdisk->disk.name);
+	rc = spdk_bdev_register(&fdisk->disk);
+	if (rc) {
+		spdk_io_device_unregister(fdisk, NULL);
+		goto error_return;
+	}
+
+	TAILQ_INSERT_TAIL(&g_uring_disk_head, fdisk, link);
+	return &fdisk->disk;
+
+error_return:
+	bdev_uring_close(fdisk);
+	uring_free_disk(fdisk);
+	return NULL;
+}
+
+struct delete_uring_disk_ctx {
+	spdk_delete_uring_complete cb_fn;
+	void *cb_arg;
+};
+
+static void
+uring_bdev_unregister_cb(void *arg, int bdeverrno)
+{
+	struct delete_uring_disk_ctx *ctx = arg;
+
+	ctx->cb_fn(ctx->cb_arg, bdeverrno);
+	free(ctx);
+}
+
+void
+delete_uring_disk(struct spdk_bdev *bdev, spdk_delete_uring_complete cb_fn, void *cb_arg)
+{
+	struct delete_uring_disk_ctx *ctx;
+
+	if (!bdev || bdev->module != &uring_if) {
+		cb_fn(cb_arg, -ENODEV);
+		return;
+	}
+
+	ctx = calloc(1, sizeof(*ctx));
+	if (ctx == NULL) {
+		cb_fn(cb_arg, -ENOMEM);
+		return;
+	}
+
+	ctx->cb_fn = cb_fn;
+	ctx->cb_arg = cb_arg;
+	spdk_bdev_unregister(bdev, uring_bdev_unregister_cb, ctx);
+}
+
+static int
+bdev_uring_initialize(void)
+{
+	size_t i;
+	struct spdk_conf_section *sp;
+	struct spdk_bdev *bdev;
+
+	TAILQ_INIT(&g_uring_disk_head);
+	spdk_io_device_register(&uring_if, bdev_uring_group_create_cb, bdev_uring_group_destroy_cb,
+				sizeof(struct bdev_uring_group_channel),
+				"uring_module");
+
+	sp = spdk_conf_find_section(NULL, "URING");
+	if (!sp) {
+		return 0;
+	}
+
+	i = 0;
+	while (true) {
+		const char *file;
+		const char *name;
+		const char *block_size_str;
+		uint32_t block_size = 0;
+		long int tmp;
+
+		file = spdk_conf_section_get_nmval(sp, "URING", i, 0);
+		if (!file) {
+			break;
+		}
+
+		name = spdk_conf_section_get_nmval(sp, "URING", i, 1);
+		if (!name) {
+			SPDK_ERRLOG("No name provided for URING disk with file %s\n", file);
+			i++;
+			continue;
+		}
+
+		block_size_str = spdk_conf_section_get_nmval(sp, "URING", i, 2);
+		if (block_size_str) {
+			tmp = spdk_strtol(block_size_str, 10);
+			if (tmp < 0) {
+				SPDK_ERRLOG("Invalid block size for URING disk with file %s\n", file);
+				i++;
+				continue;
+			}
+			block_size = (uint32_t)tmp;
+		}
+
+		bdev = create_uring_disk(name, file, block_size);
+		if (!bdev) {
+			SPDK_ERRLOG("Unable to create URING bdev from file %s\n", file);
+			i++;
+			continue;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+static void
+bdev_uring_fini(void)
+{
+	spdk_io_device_unregister(&uring_if, NULL);
+}
+
+static void
+bdev_uring_get_spdk_running_config(FILE *fp)
+{
+	char			*file;
+	char			*name;
+	uint32_t		block_size;
+	struct file_disk	*fdisk;
+
+	fprintf(fp,
+		"\n"
+		"# Users must change this section to match the /dev/sdX devices to be\n"
+		"# exported as iSCSI LUNs. The devices are accessed using Linux IO_URING.\n"
+		"# The format is:\n"
+		"# URING <file name> <bdev name> [<block size>]\n"
+		"# The file name is the backing device\n"
+		"# The bdev name can be referenced from elsewhere in the configuration file.\n"
+		"# Block size may be omitted to automatically detect the block size of a disk.\n"
+		"[URING]\n");
+
+	TAILQ_FOREACH(fdisk, &g_uring_disk_head, link) {
+		file = fdisk->filename;
+		name = fdisk->disk.name;
+		block_size = fdisk->disk.blocklen;
+		fprintf(fp, "  URING %s %s ", file, name);
+		if (fdisk->block_size_override) {
+			fprintf(fp, "%d", block_size);
+		}
+		fprintf(fp, "\n");
+	}
+	fprintf(fp, "\n");
+}
+
+SPDK_LOG_REGISTER_COMPONENT("uring", SPDK_LOG_URING)
