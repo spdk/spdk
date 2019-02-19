df295f9adcbaa18099c08f4138107665cc536d05
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-02-19 05:06:12 -0500

    bdev/ftl: Add json config generation
    Change-Id: I0baa6cfb4e4cb2a5ae68de730edb75011bd8e49d
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- scripts/gen_ftl.sh
+++ scripts/gen_ftl.sh
@@ -5,22 +5,47 @@ set -e
 rootdir=$(readlink -f $(dirname $0))/..
 
 function usage {
-	echo "Usage: $0 -a TRANSPORT_ADDR -n BDEV_NAME -l PUNITS [-u UUID]"
+	echo "Usage: $0 [-j] -a TRANSPORT_ADDR -n BDEV_NAME -l PUNITS [-u UUID]"
 	echo "UUID is required when restoring device state"
 	echo
+	echo "-j json format"
 	echo "TRANSPORT_ADDR - SSD's PCIe address"
 	echo "BDEV_NAME - name of the bdev"
 	echo "PUNITS - bdev's parallel unit range (e.g. 0-3)"
 	echo "UUID - bdev's uuid (used when in restore mode)"
 }
 
-function generate_config {
+function create_classic_config {
 	echo "[Ftl]"
 	echo "  TransportID \"trtype:PCIe traddr:$1\" $2 $3 $4"
 }
 
-while getopts ":a:n:l:m:u:" arg; do
+function create_json_config()
+{
+	echo "{"
+	echo '"subsystem": "bdev",'
+	echo '"config": ['
+	echo '{'
+	echo '"method": "construct_ftl_bdev",'
+	echo '"params": {'
+	echo "\"name\": \"$2\","
+	echo '"trtype": "PCIe",'
+	echo "\"traddr\": \"$1\","
+	if [ -n "$4" ]; then
+		echo "\"punits\": \"$3\","
+		echo "\"uuid\": \"$4\""
+	else
+		echo "\"punits\": \"$3\""
+	fi
+	echo '}'
+	echo '}'
+	echo ']'
+	echo '}'
+}
+
+while getopts "ja:n:l:m:u:" arg; do
 	case "$arg" in
+		j)	json=1		;;
 		a)	addr=$OPTARG	;;
 		n)	name=$OPTARG	;;
 		l)	punits=$OPTARG	;;
@@ -37,4 +62,8 @@ if [[ -z "$addr" || -z "$name" || -z "$punits" ]]; then
 	exit 1
 fi
 
-generate_config $addr $name $punits $uuid
+if [ -n "$json" ]; then
+	create_json_config $addr $name $punits $uuid
+else
+	create_classic_config $addr $name $punits $uuid
+fi
---
+++ test/ftl/json.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+rpc_py=$rootdir/scripts/rpc.py
+
+source $rootdir/test/common/autotest_common.sh
+
+device=$1
+FTL_BDEV_CONF=$testdir/config/ftl.json
+
+json_kill() {
+	killprocess $svcpid
+}
+
+trap "json_kill; exit 1" SIGINT SIGTERM EXIT
+
+$rootdir/test/app/bdev_svc/bdev_svc & svcpid=$!
+waitforlisten $svcpid
+
+# Create new bdev from json configuration
+$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme0 -l 0-3 | $rpc_py load_subsystem_config
+
+uuid=$($rpc_py get_bdevs | jq -r '.[0].uuid')
+
+$rpc_py delete_ftl_bdev -b nvme0
+
+# Restore bdev from json configuration
+$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme0 -l 0-3 -u $uuid | $rpc_py load_subsystem_config
+# Create new bdev from json configuration
+$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme1 -l 4-5 | $rpc_py load_subsystem_config
+# Create new bdev from RPC
+$rpc_py construct_ftl_bdev -b nvme2 -a $device -l 7-7
+
+$rpc_py delete_ftl_bdev -b nvme2
+$rpc_py delete_ftl_bdev -b nvme0
+$rpc_py delete_ftl_bdev -b nvme1
+
+# TODO: add negative test cases
+
+trap - SIGINT SIGTERM EXIT
+json_kill
