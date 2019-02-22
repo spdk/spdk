3353811f33634318e9c73a2ce7f51e228df67de8
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-02-22 10:48:05 +0100

    jsonrpc: don't check returned value from spdk_jsonrpc_begin_result()
    As spdk_jsonrpc_begin_result() is not allowed to return NULL we can
    remove these checks. We didn't have any tests cases that goes this path
    anyway.
    
    Change-Id: I0894e76c0162591e550e70b172566b9060a6dd5f
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- lib/rpc/rpc.c
+++ lib/rpc/rpc.c
@@ -366,10 +366,6 @@ spdk_rpc_get_methods(struct spdk_jsonrpc_request *request, const struct spdk_jso
 	}
 
 	w = spdk_jsonrpc_begin_result(request);
-	if (w == NULL) {
-		return;
-	}
-
 	spdk_json_write_array_begin(w);
 	SLIST_FOREACH(m, &g_rpc_methods, slist) {
 		if (req.current && ((m->state_mask & g_rpc_state) != g_rpc_state)) {
