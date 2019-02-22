d058328e12f4b769a7828c53243c50ef9d93a921
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-02-22 06:14:53 -0500

    test/unit/ftl: Add unit tests for metadata serialization
    Change-Id: I1f96efc289d4804677b081a1cc6866983ca8eb77
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/445875
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

---
+++ test/unit/lib/ftl/ftl_md/ftl_md_ut.c
@@ -0,0 +1,175 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "spdk_cunit.h"
+#include "common/lib/test_env.c"
+
+#include "ftl/ftl_band.c"
+#include "../common/utils.c"
+
+static struct spdk_ocssd_geometry_data g_geo = {
+	.num_grp	= 4,
+	.num_pu		= 3,
+	.num_chk	= 1500,
+	.clba		= 100,
+	.ws_opt		= 16,
+	.ws_min		= 4,
+};
+
+static struct spdk_ftl_punit_range g_range = {
+	.begin		= 2,
+	.end		= 9,
+};
+
+static void
+setup_band(struct ftl_band **band, const struct spdk_ocssd_geometry_data *geo,
+	   const struct spdk_ftl_punit_range *range)
+{
+	int rc;
+	struct spdk_ftl_dev *dev;
+
+	dev = test_init_ftl_dev(geo, range);
+	*band = test_init_ftl_band(dev, 0);
+	rc = ftl_band_alloc_md(*band);
+	CU_ASSERT_EQUAL_FATAL(rc, 0);
+}
+
+static void
+cleanup_band(struct ftl_band *band)
+{
+	struct spdk_ftl_dev *dev = band->dev;
+
+	test_free_ftl_band(band);
+	test_free_ftl_dev(dev);
+}
+
+static void
+test_md_unpack(void)
+{
+	struct ftl_band *band;
+	struct ftl_md *md;
+	void *data;
+
+	setup_band(&band, &g_geo, &g_range);
+
+	md = &band->md;
+
+	data = malloc(ftl_tail_md_num_lbks(band->dev) * FTL_BLOCK_SIZE);
+	SPDK_CU_ASSERT_FATAL(data);
+
+	ftl_pack_head_md(band->dev, md, data);
+	CU_ASSERT_EQUAL(ftl_unpack_head_md(band->dev, md, data), FTL_MD_SUCCESS);
+
+	ftl_pack_tail_md(band->dev, md, data);
+	CU_ASSERT_EQUAL(ftl_unpack_tail_md(band->dev, md, data), FTL_MD_SUCCESS);
+
+	free(data);
+	cleanup_band(band);
+}
+
+static void
+test_md_unpack_fail(void)
+{
+	struct ftl_band *band;
+	struct ftl_md *md;
+	struct ftl_md_hdr *hdr;
+	void *data;
+
+	setup_band(&band, &g_geo, &g_range);
+
+	md = &band->md;
+
+	data = malloc(ftl_tail_md_num_lbks(band->dev) * FTL_BLOCK_SIZE);
+	SPDK_CU_ASSERT_FATAL(data);
+
+	/* check crc */
+	ftl_pack_tail_md(band->dev, md, data);
+	/* flip last bit of lba_map */
+	*((char *)data + ftl_tail_md_num_lbks(band->dev) * FTL_BLOCK_SIZE - 1) ^= 0x1;
+	CU_ASSERT_EQUAL(ftl_unpack_tail_md(band->dev, md, data), FTL_MD_INVALID_CRC);
+
+	/* check invalid version */
+	hdr = data;
+	ftl_pack_tail_md(band->dev, md, data);
+	hdr->ver++;
+	CU_ASSERT_EQUAL(ftl_unpack_tail_md(band->dev, md, data), FTL_MD_INVALID_VER);
+
+	/* check wrong UUID */
+	ftl_pack_head_md(band->dev, md, data);
+	hdr->uuid.u.raw[0] ^= 0x1;
+	CU_ASSERT_EQUAL(ftl_unpack_head_md(band->dev, md, data), FTL_MD_NO_MD);
+
+	/* check invalid size */
+	ftl_pack_tail_md(band->dev, md, data);
+	band->dev->geo.clba--;
+	CU_ASSERT_EQUAL(ftl_unpack_tail_md(band->dev, md, data), FTL_MD_INVALID_SIZE);
+
+	free(data);
+	cleanup_band(band);
+}
+
+int
+main(int argc, char **argv)
+{
+	CU_pSuite suite = NULL;
+	unsigned int num_failures;
+
+	if (CU_initialize_registry() != CUE_SUCCESS) {
+		return CU_get_error();
+	}
+
+	suite = CU_add_suite("ftl_meta_suite", NULL, NULL);
+	if (!suite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (
+		CU_add_test(suite, "test_md_unpack",
+			    test_md_unpack) == NULL
+		|| CU_add_test(suite, "test_md_unpack_fail",
+			       test_md_unpack_fail) == NULL
+	) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	num_failures = CU_get_number_of_failures();
+	CU_cleanup_registry();
+
+	return num_failures;
+}
