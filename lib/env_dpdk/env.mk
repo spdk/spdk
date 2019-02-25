f4d67a75aca4e1810d7f2e1a09b46578c7e26a4b
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-02-25 01:57:46 +0100

    configure: add option to use the internal rte_vhost copy
    It's currently enabled by default. One of the features
    missing in the upstream rte_vhost lib is vhost-nvme,
    so all its references in the code were wrapped with
    CONFIG_VHOST_INTERNAL_LIB ifdef.
    
    Change-Id: Ic8bc5497c4d77bfef77c57f3d5a1f8681ffb6d1f
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- lib/env_dpdk/env.mk
+++ lib/env_dpdk/env.mk
@@ -37,6 +37,8 @@
 # ENV_LIBS
 # ENV_LINKER_ARGS
 
+include $(SPDK_ROOT_DIR)/mk/config.mk
+
 DPDK_DIR = $(CONFIG_DPDK_DIR)
 
 export DPDK_ABS_DIR = $(abspath $(DPDK_DIR))
@@ -98,6 +100,15 @@ endif
 
 ifneq (, $(wildcard $(DPDK_ABS_DIR)/lib/librte_kvargs.*))
 DPDK_LIB_LIST += rte_kvargs
+endif
+
+ifneq ($(CONFIG_VHOST_INTERNAL_LIB),y)
+ifneq (, $(wildcard $(DPDK_ABS_DIR)/lib/librte_vhost.*))
+DPDK_LIB_LIST += rte_vhost rte_net rte_hash rte_mbuf
+ifneq ($(DPDK_FRAMEWORK),y)
+DPDK_LIB_LIST += rte_cryptodev
+endif
+endif
 endif
 
 DPDK_LIB = $(DPDK_LIB_LIST:%=$(DPDK_ABS_DIR)/lib/lib%$(DPDK_LIB_EXT))
