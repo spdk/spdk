spdk/spdk

commit c5c102ce5551223b945280ea2215422f279674b2

lib/ftl: Fix band picking for write pointer
Removing band from "free list" is moved from FTL_BAND_STATE_OPENING
to FTL_BAND_STATE_PREP state's change actions.
This will fix race condition when one band is prepared (erased)
and write pointer is trying to get next active band.

Change-Id: I9e4fe9482a01ee732271736e4a0e6fcedf2582d8
Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
Reviewed-on: https://review.gerrithub.io/c/445118
Reviewed-by: Jakub Radtke <jakub.radtke@intel.com>
Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

lib/ftl/ftl_band.c
@@ -224,13 +224,14 @@ _ftl_band_set_free(struct ftl_band *band)
 }
 
 static void
-_ftl_band_set_opening(struct ftl_band *band)
+_ftl_band_set_preparing(struct ftl_band *band)
 {
 	struct spdk_ftl_dev *dev = band->dev;
 	struct ftl_md *md = &band->md;
 
 	/* Verify band's previous state */
-	assert(band->state == FTL_BAND_STATE_PREP);
+	assert(band->state == FTL_BAND_STATE_FREE);
+	/* Remove band from free list */
 	LIST_REMOVE(band, list_entry);
 
 	md->wr_cnt++;
@@ -467,8 +468,8 @@ ftl_band_set_state(struct ftl_band *band, enum ftl_band_state state)
 		_ftl_band_set_free(band);
 		break;
 
-	case FTL_BAND_STATE_OPENING:
-		_ftl_band_set_opening(band);
+	case FTL_BAND_STATE_PREP:
+		_ftl_band_set_preparing(band);
 		break;
 
 	case FTL_BAND_STATE_CLOSED:

commit b0f99a6bde193d3b3be58f5801f3eec8e42152cc

ftl: fix the assert in the unit test
Commit c5c102ce5 introduced a assertion with master branch,
remove the error state, since it's set via last function.

Change-Id: If838a9d1d42bcaf37aacbbde47d6d2822122277a
Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

lib/ftl/ftl_band.c
@@ -185,6 +185,7 @@ _ftl_band_set_free(struct ftl_band *band)
 
 	/* Verify band's previous state */
 	assert(band->state == FTL_BAND_STATE_CLOSED);
+	band->state = FTL_BAND_STATE_FREE;
 
 	if (band == dev->df_band) {
 		dev->df_band = NULL;
@@ -231,6 +232,7 @@ _ftl_band_set_preparing(struct ftl_band *band)
 
 	/* Verify band's previous state */
 	assert(band->state == FTL_BAND_STATE_FREE);
+	band->state = FTL_BAND_STATE_PREP;
 	/* Remove band from free list */
 	LIST_REMOVE(band, list_entry);
 

test/unit/lib/ftl/ftl_wptr/ftl_wptr_ut.c
@@ -147,7 +147,6 @@ test_wptr(void)
 	for (i = 0; i < ftl_dev_num_bands(dev); ++i) {
 		wptr = LIST_FIRST(&dev->wptr_list);
 		band = wptr->band;
-		ftl_band_set_state(band, FTL_BAND_STATE_PREP);
 		ftl_band_set_state(band, FTL_BAND_STATE_OPENING);
 		ftl_band_set_state(band, FTL_BAND_STATE_OPEN);
 		io.band = band;