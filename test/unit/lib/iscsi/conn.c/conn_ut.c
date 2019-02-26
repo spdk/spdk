6a8473d4f38db427c28138300cd19bdbcacf896e
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-02-26 10:09:05 +0900

    iscsi: Unify calculate mapped length and build iovecs to flush PDUs
    To know the mapped length by iovecs, pdu length was got first and
    then the size of partial written was reduced separately.
    
    This patch unifies these two operations into spdk_iscsi_build_iovs().
    
    Change-Id: Ic6f5eecc902b8e209ef00c010915f476ca16c002
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/unit/lib/iscsi/conn.c/conn_ut.c
+++ test/unit/lib/iscsi/conn.c/conn_ut.c
@@ -139,7 +139,7 @@ DEFINE_STUB_V(spdk_clear_all_transfer_task,
 
 DEFINE_STUB(spdk_iscsi_build_iovs, int,
 	    (struct spdk_iscsi_conn *conn, struct iovec *iov,
-	     struct spdk_iscsi_pdu *pdu),
+	     struct spdk_iscsi_pdu *pdu, uint32_t *mapped_length),
 	    0);
 
 DEFINE_STUB(spdk_iscsi_is_deferred_free_pdu, bool,
