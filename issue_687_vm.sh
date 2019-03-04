79bffbce2f74c3d22e23badca3d4ed24421ca560
Pawel Wodkowski <pawelx.wodkowski@intel.com>
2019-03-04 12:21:31 +0100

    [RFC] [DO NOT MERGE] scripts to replicate issue 687
    Change-Id: Id525d4515f26e4e897b5083c31394d596d7dc922
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>

---
+++ issue_687_vm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -xe
+
+rootdir=$(readlink -f $(dirname $0))
+
+shopt -s expand_aliases
+
+function on_exit() {
+	pkill -9 -F /home/pwodkowx/Private/sandbox/vms/0/qemu.pid
+	trap - EXIT
+}
+
+trap "on_exit" EXIT ERR
+
+: ${socket=$rootdir/../sandbox/vhost0/vhost.sock}
+alias rpc='$rootdir/scripts/rpc.py -s ${socket}'
+
+function start_vm() {
+	/home/pwodkowx/Private/sandbox/root/bin/qemu-system-x86_64 \
+	-snapshot -m 512 --enable-kvm -cpu host -smp 4 -vga std -vnc :100 -daemonize \
+	-object memory-backend-file,id=mem,size=512M,mem-path=/dev/hugepages,share=on,prealloc=yes,host-nodes=1,policy=bind \
+	-monitor telnet:127.0.0.1:10002,server,nowait \
+	-numa node,memdev=mem \
+	-pidfile /home/pwodkowx/Private/sandbox/vms/0/qemu.pid \
+	-serial file:/home/pwodkowx/Private/sandbox/vms/0/serial.log \
+	-D /home/pwodkowx/Private/sandbox/vms/0/qemu.log \
+	-drive file=/home/pwodkowx/Private/sandbox/vms/0/os.qcow2,if=none,id=os_disk \
+	-device ide-hd,drive=os_disk,bootindex=0 \
+	-chardev socket,id=vhost0,path=/home/pwodkowx/Private/sandbox/vhost0/vhost.0 \
+	-device vhost-user-scsi-pci,id=scsi0,chardev=vhost0
+}
+
+while true; do
+	start_vm
+	sleep 5
+	pkill -9 -F /home/pwodkowx/Private/sandbox/vms/0/qemu.pid
+done
+
+trap - EXIT ERR
+echo "DONE"
