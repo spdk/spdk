880b8b8af39f55e37c70d29ab595b65a0bc58c96
Ben Walker <benjamin.walker@intel.com>
2019-03-05 14:18:44 -0700

    event: spdk_subsystem_config_json is no longer asynchronous
    Nothing needed this to be asynchronous.
    
    Change-Id: Ic67167d5c1214e832b77ab7fa44aa693026d868a
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/447115
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/event/rpc/subsystem_rpc.c
+++ lib/event/rpc/subsystem_rpc.c
@@ -86,22 +86,12 @@ static const struct spdk_json_object_decoder rpc_get_subsystem_config[] = {
 };
 
 static void
-rpc_get_subsystem_config_done(void *arg1, void *arg2)
-{
-	struct spdk_jsonrpc_request *request = arg1;
-	struct spdk_json_write_ctx *w = arg2;
-
-	spdk_jsonrpc_end_result(request, w);
-}
-
-static void
 spdk_rpc_get_subsystem_config(struct spdk_jsonrpc_request *request,
 			      const struct spdk_json_val *params)
 {
 	struct rpc_get_subsystem_config req = {};
 	struct spdk_json_write_ctx *w;
 	struct spdk_subsystem *subsystem;
-	struct spdk_event *ev;
 
 	if (spdk_json_decode_object(params, rpc_get_subsystem_config,
 				    SPDK_COUNTOF(rpc_get_subsystem_config), &req)) {
@@ -121,8 +111,8 @@ spdk_rpc_get_subsystem_config(struct spdk_jsonrpc_request *request,
 
 	w = spdk_jsonrpc_begin_result(request);
 	if (w) {
-		ev = spdk_event_allocate(spdk_env_get_current_core(), rpc_get_subsystem_config_done, request, w);
-		spdk_subsystem_config_json(w, subsystem, ev);
+		spdk_subsystem_config_json(w, subsystem);
+		spdk_jsonrpc_end_result(request, w);
 	}
 }
 
