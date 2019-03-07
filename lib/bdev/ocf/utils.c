9bbe37d1070b1101429f187c723ce3b4237de83c
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-03-07 03:42:42 +0000

    [RFC] ocf: add utility functions for async procedures
    Add "continuable poller" interface for usage
      in OCF bdev.
    
    Change-Id: If791bbde0db09afea999d0c798e59da3436e3ae3
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/bdev/ocf/utils.c
+++ lib/bdev/ocf/utils.c
@@ -32,6 +32,7 @@
  */
 
 #include "spdk/stdinc.h"
+#include "spdk/thread.h"
 
 #include "utils.h"
 
@@ -65,4 +66,234 @@ ocf_get_cache_modename(ocf_cache_mode_t mode)
 	} else {
 		return NULL;
 	}
+}
+
+struct spdk_cont_poller {
+	struct spdk_poller            *poller;
+	int                            period;
+	bool                           done;
+	cont_poller_fn                 fn;
+	bool                           is_poller;
+	int                            status;
+	void                          *ctx;
+
+	/* This structure is a tree, meaning that
+	 * every poller have list of pollers, and
+	 * it iself an element of such list. */
+	TAILQ_HEAD(, spdk_cont_poller) continuations;
+	TAILQ_ENTRY(spdk_cont_poller)  tailq;
+
+	struct spdk_cont_poller       *parent;
+};
+
+static void poller_done_iter(struct spdk_cont_poller *arg);
+
+int
+spdk_cont_poller_parent_status(struct spdk_cont_poller *current)
+{
+	if (current->parent) {
+		return current->parent->status;
+	} else {
+		return 0;
+	}
+}
+
+static int
+cont_poller_poll(void *opaque)
+{
+	struct spdk_cont_poller *arg = opaque;
+
+	arg->done = true;
+	arg->status = arg->fn(arg, arg->ctx);
+
+	if (arg->done) {
+		spdk_poller_unregister(&arg->poller);
+		poller_done_iter(arg);
+		return 1;
+	}
+
+	return 0;
+}
+
+int
+spdk_cont_poller_repeat(struct spdk_cont_poller *poller)
+{
+	assert(poller->is_poller);
+	if (poller->is_poller) {
+		poller->done = false;
+	}
+	return 0;
+}
+
+static void
+procedure_callback(void *ctx)
+{
+	struct spdk_cont_poller *current = ctx;
+
+	if (current->fn) {
+		current->status = current->fn(current, current->ctx);
+	}
+
+	poller_done_iter(current);
+}
+
+static void
+start(struct spdk_cont_poller *current)
+{
+	if (current->is_poller) {
+		current->poller = spdk_poller_register(cont_poller_poll, current, current->period);
+		if (current->poller == NULL) {
+			SPDK_ERRLOG("Could not register a poller\n");
+			poller_done_iter(current);
+			return;
+		}
+	} else {
+		spdk_thread_send_msg(spdk_get_thread(), procedure_callback, current);
+	}
+}
+
+static void
+poller_done_iter(struct spdk_cont_poller *arg)
+{
+	struct spdk_cont_poller *current = TAILQ_FIRST(&arg->continuations);
+	if (current) {
+		TAILQ_REMOVE(&arg->continuations, current, tailq);
+		start(current);
+	} else {
+		if (arg->parent) {
+			poller_done_iter(arg->parent);
+		}
+		free(arg);
+	}
+}
+
+static struct spdk_cont_poller *
+init(struct spdk_cont_poller *parent, bool is_poller, cont_poller_fn fn, void *ctx, int period)
+{
+	struct spdk_cont_poller *ret;
+
+	ret = malloc(sizeof(*ret));
+	if (ret == NULL) {
+		return NULL;
+	}
+
+	ret->fn            = fn;
+	ret->period        = period;
+	ret->is_poller     = is_poller;
+	ret->ctx           = ctx;
+	ret->parent        = parent;
+	ret->status        = 0;
+	TAILQ_INIT(&ret->continuations);
+
+	if (parent) {
+		TAILQ_INSERT_TAIL(&parent->continuations, ret, tailq);
+	}
+
+	return ret;
+}
+
+struct spdk_cont_poller *
+spdk_cont_poller_register(cont_poller_fn fn, void *ctx, int period)
+{
+	struct spdk_cont_poller *arg;
+
+	if (fn == NULL || period < 0) {
+		return NULL;
+	}
+
+	arg = init(NULL, true, fn, ctx, period);
+	start(arg);
+	return arg;
+}
+
+struct spdk_cont_poller *
+spdk_cont_poller_noop(void)
+{
+	struct spdk_cont_poller *ret = init(NULL, false, NULL, NULL, 0);
+
+	if (ret == NULL) {
+		SPDK_ERRLOG("Poller memory allocation failed\n");
+		return NULL;
+	}
+
+	start(ret);
+	return ret;
+}
+
+int spdk_cont_poller_append(struct spdk_cont_poller *parent, cont_poller_fn fn, void *ctx)
+{
+	struct spdk_cont_poller *ret;
+
+	if (fn == NULL || parent == NULL) {
+		return -EFAULT;
+	}
+
+	ret = init(parent, false, fn, ctx, 0);
+	if (ret == NULL) {
+		SPDK_ERRLOG("Poller memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+int spdk_cont_poller_append_poller(struct spdk_cont_poller *parent, cont_poller_fn fn, void *ctx,
+				   int period)
+{
+	struct spdk_cont_poller *ret;
+
+	if (fn == NULL || parent == NULL) {
+		return -EFAULT;
+	}
+
+	ret = init(parent, true, fn, ctx, period);
+	if (ret == NULL) {
+		SPDK_ERRLOG("Poller memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+
+struct common_ctx {
+	spdk_callback_fn cb;
+	void            *ctx;
+};
+
+static int common_cb(struct spdk_cont_poller *actx, void *ctx)
+{
+	struct common_ctx *cctx = ctx;
+
+	cctx->cb(spdk_cont_poller_parent_status(actx), cctx->ctx);
+	free(cctx);
+	return 0;
+}
+
+int spdk_cont_poller_append_finish(struct spdk_cont_poller *parent, spdk_callback_fn cb, void *ctx)
+{
+	struct spdk_cont_poller *ret;
+	struct common_ctx *cctx;
+
+	if (cb == NULL || parent == NULL) {
+		return -EFAULT;
+	}
+
+	cctx = malloc(sizeof(*cctx));
+	if (cctx == NULL) {
+		SPDK_ERRLOG("Poller memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	cctx->cb  = cb;
+	cctx->ctx = ctx;
+
+	ret = init(parent, false, common_cb, cctx, 0);
+	if (ret == NULL) {
+		free(cctx);
+		SPDK_ERRLOG("Poller memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	return 0;
 }
--- lib/bdev/ocf/utils.h
+++ lib/bdev/ocf/utils.h
@@ -39,4 +39,26 @@
 ocf_cache_mode_t ocf_get_cache_mode(const char *cache_mode);
 const char *ocf_get_cache_modename(ocf_cache_mode_t mode);
 
+/* Poller that supports continuations */
+struct spdk_cont_poller;
+
+/* Common continuation type for asynchronous OCF procedures */
+typedef int (*cont_poller_fn)(struct spdk_cont_poller *, void *);
+
+struct spdk_cont_poller *spdk_cont_poller_noop(void);
+struct spdk_cont_poller *spdk_cont_poller_register(cont_poller_fn fn, void *ctx, int period);
+int spdk_cont_poller_append(struct spdk_cont_poller *poller, cont_poller_fn fn, void *ctx);
+int spdk_cont_poller_append_poller(struct spdk_cont_poller *poller, cont_poller_fn fn, void *ctx,
+				   int period);
+
+/* Common SPDK callback function that is not composable */
+typedef void (*spdk_callback_fn)(int, void *);
+int spdk_cont_poller_append_finish(struct spdk_cont_poller *poller, spdk_callback_fn cb, void *ctx);
+
+/* Prevents poller from unregistering after function was called */
+int spdk_cont_poller_repeat(struct spdk_cont_poller *poller);
+
+/* Status returned by previus poller */
+int spdk_cont_poller_parent_status(struct spdk_cont_poller *current);
+
 #endif
