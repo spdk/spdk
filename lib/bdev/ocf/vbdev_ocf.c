6a38c961a8e186025bc665cc326cc1edf38a1363
Piotr Pelplinski <piotr.pelplinski@intel.com>
2019-03-08 09:09:23 +0100

    vbdev_ocf: add examine_disk
    This patch utilizies ocf_metadata_probe to identify if disk is ocf disk
    
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Change-Id: I4f95626a9c1cc76bef1a63f6417077a859f8f573

--- lib/bdev/ocf/vbdev_ocf.c
+++ lib/bdev/ocf/vbdev_ocf.c
@@ -964,6 +964,69 @@ vbdev_ocf_examine(struct spdk_bdev *bdev)
 	spdk_bdev_module_examine_done(&ocf_if);
 }
 
+static
+int vbdev_core_visitor(ocf_core_t core, void *cntx)
+{
+	struct vbdev_ocf *vbdev = (struct vbdev_ocf *)cntx;
+	char *data;
+	int rc;
+
+	vbdev->ocf_core = core;
+	rc = ocf_core_get_user_metadata(core, &data, NAME_MAX);
+	strdup(vbdev->name, data);
+
+	register_vbdev(vbdev);
+	return rc;
+}
+
+/* This function is called to determine if device was
+ * previously initialized by ocf, so that spdk can
+ * load it automatically and registers all core devices.
+ */
+static void
+vbdev_ocf_examine_disk(struct spdk_bdev *bdev)
+{
+	const char *cache_name = spdk_bdev_get_name(bdev);
+	struct vbdev_ocf *vbdev;
+	ocf_volume_t volume;
+
+	bool clean_shutdown;
+	bool cache_dirty;
+	struct ocf_volume_uuid uuid;
+	int rc;
+
+	uuid.data = cache_name;
+	uuid.size = strlen(cache_name) + 1;
+
+	rc = ocf_ctx_volume_create(vbdev_ocf_ctx, &volume, &uuid, SPDK_OBJECT);
+
+	if (rc) {
+		goto done;
+	}
+
+	rc = ocf_metadata_probe(vbdev_ocf_ctx, volume, &clean_shutdown, &cache_dirty);
+	if (rc || !clean_shutdown || cache_dirty) {
+		goto done;
+	}
+
+	vbdev = calloc(1, sizeof(*vbdev));
+	if (!vbdev) {
+		SPDK_ERRLOG("Failed to start cache instance\n");
+		goto done;
+	}
+
+	rc = ocf_mngt_cache_start(vbdev_ocf_ctx, &vbdev->ocf_cache, &vbdev->cfg.cache);
+	if (rc) {
+		SPDK_ERRLOG("Failed to start cache instance\n");
+		goto done;
+	}
+
+	ocf_core_visit(vbdev->ocf_cache, vbdev_core_visitor, &vbdev, false);
+
+done:
+	spdk_bdev_module_examine_done(&ocf_if);
+}
+
 static int
 vbdev_ocf_get_ctx_size(void)
 {
@@ -980,6 +1043,7 @@ static struct spdk_bdev_module ocf_if = {
 	.config_text = NULL,
 	.get_ctx_size = vbdev_ocf_get_ctx_size,
 	.examine_config = vbdev_ocf_examine,
+	.examine_disk = vbdev_ocf_examine_disk,
 };
 SPDK_BDEV_MODULE_REGISTER(ocf, &ocf_if);
 
