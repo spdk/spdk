6daf7c4920d09003bbb76ecaa60fc392b95684c1
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-03-15 13:24:09 +0100

    nvme: don't rely on phys_addr retrieved from spdk_malloc()
    The phys_addr param in spdk_*malloc() is about to be
    deprecated, so use a separate spdk_vtophys() call to
    retrieve physical addresses.
    
    This patch also adds error checks against SPDK_VTOPHYS_ERROR.
    The error handling paths are already there to account for
    spdk_*malloc() failures themselves, so reuse them in case
    of vtophys failures.
    
    Change-Id: I377636e66b8c570d013c1bb2021f04bce4e6c0ce
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- test/common/lib/test_env.c
+++ test/common/lib/test_env.c
@@ -150,6 +150,7 @@ spdk_dma_realloc(void *buf, size_t size, size_t align, uint64_t *phys_addr)
 	return realloc(buf, size);
 }
 
+DEFINE_RETURN_MOCK(spdk_free, void *);
 void
 spdk_free(void *buf)
 {
