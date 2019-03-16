64d76e50cc587a460b327cead12c533a1fc3eead
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-03-17 00:50:51 +0100

    vhost: call session_event_done() always under the global vhost lock
    In the next patch we will put much more responsibility
    on spdk_vhost_session_event_done(), so here we make
    sure it's always called under the global vhost mutex.
    
    Specifically, spdk_vhost_session_event_done() will set
    vsession->lcore, which any other thread might try to
    concurrently access via spdk_vhost_dev_foreach_session().
    
    Change-Id: I7a5fde4be4e8bdfdbbb24ac955af964f516bdb68
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/448227
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/vhost/vhost_nvme.c
+++ lib/vhost/vhost_nvme.c
@@ -1164,6 +1164,10 @@ destroy_device_poller_cb(void *arg)
 
 	/* FIXME wait for pending I/Os to complete */
 
+	if (spdk_vhost_trylock() != 0) {
+		return -1;
+	}
+
 	for (i = 0; i < nvme->num_ns; i++) {
 		ns_dev = &nvme->ns[i];
 		if (ns_dev->bdev_io_channel) {
@@ -1184,6 +1188,7 @@ destroy_device_poller_cb(void *arg)
 	spdk_poller_unregister(&nvme->stop_poller);
 	spdk_vhost_session_event_done(nvme->vsession, 0);
 
+	spdk_vhost_unlock();
 	return -1;
 }
 
