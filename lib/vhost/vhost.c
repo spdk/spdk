d3f4c42d138c873ebb7836d085213ebed861424f
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-03-17 01:57:42 +0100

    vhost: add spdk_vhost_trylock()
    We'll make use of it inside the vhost device backend
    code. The function itself is generic enough to be put
    in the public vhost.h header rather than vhost_internal.h.
    
    Change-Id: I60602c61d8bba665dcf9c6d27af2e910c208a7be
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- lib/vhost/vhost.c
+++ lib/vhost/vhost.c
@@ -1396,6 +1396,12 @@ spdk_vhost_lock(void)
 	pthread_mutex_lock(&g_spdk_vhost_mutex);
 }
 
+int
+spdk_vhost_trylock(void)
+{
+	return -pthread_mutex_trylock(&g_spdk_vhost_mutex);
+}
+
 void
 spdk_vhost_unlock(void)
 {
