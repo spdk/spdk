spdk/spdk

commit 2725b5191eccae3d444cb98497dc29bbcdea6c21

virtio/user: don't send SET_VRING_ADDR after updating the memory table
Our internal rte_vhost lib requires SET_VRING_ADDR to
flush a pending SET_MEM_TABLE. On the other hand, the
upstream rte_vhost will invalidate the entire queue
upon receiving SET_VRING_ADDR message, so we mustn't
send it at runtime. Both behaviors are strictly
implementation specific, but this message isn't needed
by the spec, so send it only if vhost was compiled
with our internal lib.

Change-Id: I200b99a1dd5124bdebd104e69ae1b012a974ab6f
Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/446087
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>

lib/virtio/virtio_user.c
@@ -165,13 +165,19 @@ virtio_user_map_notify(void *cb_ctx, struct spdk_mem_map *map,
 		return ret;
 	}
 
-	/* We have to send SET_VRING_ADDR to make rte_vhost flush a pending
-	 * SET_MEM_TABLE...
+#ifdef SPDK_CONFIG_VHOST_INTERNAL_LIB
+	/* Our internal rte_vhost lib requires SET_VRING_ADDR to flush a pending
+	 * SET_MEM_TABLE. On the other hand, the upstream rte_vhost will invalidate
+	 * the entire queue upon receiving SET_VRING_ADDR message, so we mustn't
+	 * send it here. Both behaviors are strictly implementation specific, but
+	 * this message isn't needed from the point of the spec, so send it only
+	 * if vhost is compiled with our internal lib.
 	 */
 	ret = virtio_user_queue_setup(vdev, virtio_user_set_vring_addr);
 	if (ret < 0) {
 		return ret;
 	}
+#endif
 
 	/* Since we might want to use that mapping straight away, we have to
 	 * make sure the guest has already processed our SET_MEM_TABLE message.

commit 2654283a2f3d6c1e40644792aa3927dd216a5b7f

virtio/user: fix memory hotplug with the internal rte_vhost lib
This was broken in commit 2725b5191e [1], which added
an extra #ifdef SPDK_CONFIG_VHOST_INTERNAL_LIB without
including config.h anywhere within the same file.

Currently the virtio library always behaves as if it was
compiled with upstream rte_vhost. Fix this by including
spdk/config.h where necessary.

[1] virtio/user: don't send SET_VRING_ADDR after updating
    the memory table

Change-Id: Idadc53e20d615ca1dcef5b58a854f6b39d14511b
Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

lib/virtio/virtio_user.c
@@ -43,6 +43,7 @@
 
 #include "virtio_user/vhost.h"
 #include "spdk/string.h"
+#include "spdk/config.h"
 
 #include "spdk_internal/virtio.h"
 