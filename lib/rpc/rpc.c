5828fe431818eba64564c7309e8e06217e56c34c
Chunyang Hui <chunyang.hui@intel.com>
2019-03-21 18:02:04 -0400

    rpc: add get_spdk_version rpc method
    Change-Id: Iacc1766a60fb1e15a1013475f83f7f13cd45fe12
    Signed-off-by: Chunyang Hui <chunyang.hui@intel.com>

--- lib/rpc/rpc.c
+++ lib/rpc/rpc.c
@@ -41,6 +41,7 @@
 #include "spdk/log.h"
 #include "spdk/string.h"
 #include "spdk/util.h"
+#include "spdk/version.h"
 
 #define RPC_DEFAULT_PORT	"5260"
 
@@ -309,3 +310,38 @@ spdk_rpc_get_rpc_methods(struct spdk_jsonrpc_request *request,
 	spdk_jsonrpc_end_result(request, w);
 }
 SPDK_RPC_REGISTER("get_rpc_methods", spdk_rpc_get_rpc_methods, SPDK_RPC_STARTUP | SPDK_RPC_RUNTIME)
+
+static void
+spdk_rpc_get_version(struct spdk_jsonrpc_request *request, const struct spdk_json_val *params)
+{
+	struct spdk_json_write_ctx *w;
+
+	if (params != NULL) {
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "get_spdk_version method requires no parameters");
+	}
+
+	w = spdk_jsonrpc_begin_result(request);
+	if (w == NULL) {
+		return;
+	}
+
+	spdk_json_write_object_begin(w);
+
+	spdk_json_write_named_string_fmt(w, "version", "%s", SPDK_VERSION_STRING);
+
+	spdk_json_write_named_object_begin(w, "fields");
+	spdk_json_write_named_uint32(w, "major", SPDK_VERSION_MAJOR);
+	spdk_json_write_named_uint32(w, "minor", SPDK_VERSION_MINOR);
+
+	spdk_json_write_named_uint32(w, "patch", SPDK_VERSION_PATCH);
+
+	spdk_json_write_named_string_fmt(w, "suffix", "%s", SPDK_VERSION_SUFFIX);
+
+	spdk_json_write_object_end(w);
+
+	spdk_json_write_object_end(w);
+
+	spdk_jsonrpc_end_result(request, w);
+}
+SPDK_RPC_REGISTER("get_spdk_version", spdk_rpc_get_version, SPDK_RPC_STARTUP | SPDK_RPC_RUNTIME)
