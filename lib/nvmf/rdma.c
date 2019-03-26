6f7cc6b85b74043abb42729fbf75d7a2f873ccae
Seth Howell <seth.howell@intel.com>
2019-03-26 13:36:50 -0700

    rdma: add an stailq for qpairs pending recv
    This will help us not iterate through the whole list of connections when
    only some of them have pending recvs.
    
    Change-Id: I681bc98befbdda4e77ef333b7a086c08b2708eb3
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvmf/rdma.c
+++ lib/nvmf/rdma.c
@@ -385,6 +385,8 @@ struct spdk_nvmf_rdma_qpair {
 
 	TAILQ_ENTRY(spdk_nvmf_rdma_qpair)	link;
 
+	STAILQ_ENTRY(spdk_nvmf_rdma_qpair)	recv_link;
+
 	STAILQ_ENTRY(spdk_nvmf_rdma_qpair)	send_link;
 
 	/* IBV queue pair attributes: they are used to manage
@@ -426,6 +428,8 @@ struct spdk_nvmf_rdma_poller {
 	struct spdk_nvmf_rdma_resources		*resources;
 
 	TAILQ_HEAD(, spdk_nvmf_rdma_qpair)	qpairs;
+
+	STAILQ_HEAD(, spdk_nvmf_rdma_qpair)	qpairs_pending_recv;
 
 	STAILQ_HEAD(, spdk_nvmf_rdma_qpair)	qpairs_pending_send;
 
@@ -1053,6 +1057,9 @@ nvmf_rdma_qpair_queue_recv_wrs(struct spdk_nvmf_rdma_qpair *rqpair, struct ibv_r
 	if (rqpair->resources->recvs_to_post.first == NULL) {
 		rqpair->resources->recvs_to_post.first = first;
 		rqpair->resources->recvs_to_post.last = last;
+		if (rqpair->srq == NULL) {
+			STAILQ_INSERT_TAIL(&rqpair->poller->qpairs_pending_recv, rqpair, recv_link);
+		}
 	} else {
 		rqpair->resources->recvs_to_post.last->next = first;
 		rqpair->resources->recvs_to_post.last = last;
@@ -2972,6 +2979,7 @@ spdk_nvmf_rdma_poll_group_create(struct spdk_nvmf_transport *transport)
 
 		TAILQ_INIT(&poller->qpairs);
 		STAILQ_INIT(&poller->qpairs_pending_send);
+		STAILQ_INIT(&poller->qpairs_pending_recv);
 
 		TAILQ_INSERT_TAIL(&rgroup->pollers, poller, link);
 		if (device->attr.max_srq != 0) {
@@ -3401,7 +3409,8 @@ spdk_nvmf_rdma_poll_group_poll(struct spdk_nvmf_transport_poll_group *group)
 				rpoller->resources->recvs_to_post.last = NULL;
 			}
 		} else {
-			TAILQ_FOREACH(rqpair, &rpoller->qpairs, link) {
+			while (!STAILQ_EMPTY(&rpoller->qpairs_pending_recv)) {
+				rqpair = STAILQ_FIRST(&rpoller->qpairs_pending_recv);
 				if (rqpair->resources->recvs_to_post.first) {
 					rc = ibv_post_recv(rqpair->cm_id->qp, rqpair->resources->recvs_to_post.first, &bad_recv_wr);
 					if (rc) {
@@ -3415,9 +3424,10 @@ spdk_nvmf_rdma_poll_group_poll(struct spdk_nvmf_transport_poll_group *group)
 							spdk_nvmf_rdma_start_disconnect(rqpair);
 						}
 					}
-					rqpair->resources->recvs_to_post.first = NULL;
-					rqpair->resources->recvs_to_post.last = NULL;
 				}
+				rqpair->resources->recvs_to_post.first = NULL;
+				rqpair->resources->recvs_to_post.last = NULL;
+				STAILQ_REMOVE_HEAD(&rpoller->qpairs_pending_recv, recv_link);
 			}
 		}
 
