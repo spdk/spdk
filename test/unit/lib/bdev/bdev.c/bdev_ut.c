9bfd93472f0194b773b440b0dbe49f266a1d00bc
Piotr Pelplinski <piotr.pelplinski@intel.com>
2019-03-29 16:04:48 +0100

    notify: add basic unit tests for notifications
    Signed-off-by: Piotr Pelplinski <piotr.pelplinski@intel.com>
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Change-Id: Ia6cc45c1c539cdc7d25fbe1a562afb21783dde4c

--- test/unit/lib/bdev/bdev.c/bdev_ut.c
+++ test/unit/lib/bdev/bdev.c/bdev_ut.c
@@ -58,6 +58,9 @@ DEFINE_STUB_V(spdk_trace_register_description, (const char *name, const char *sh
 		uint8_t arg1_is_ptr, const char *arg1_name));
 DEFINE_STUB_V(_spdk_trace_record, (uint64_t tsc, uint16_t tpoint_id, uint16_t poller_id,
 				   uint32_t size, uint64_t object_id, uint64_t arg1));
+DEFINE_STUB(spdk_notify_send, uint64_t, (const char *type, const char *ctx), 0);
+DEFINE_STUB(spdk_notify_type_register, struct spdk_notify_type *, (const char *type), NULL);
+
 
 int g_status;
 int g_count;
