7b661426bdd4ee8fc0796e8349bae4aee55b4eee
Changpeng Liu <changpeng.liu@intel.com>
2019-03-29 01:43:02 -0400

    nvme: don't poll the CQ after CQ is deleted
    Change-Id: I7985907298ca714df82426f240c8b32b0139be6f
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/nvme/nvme_pcie.c
+++ lib/nvme/nvme_pcie.c
@@ -1704,9 +1704,6 @@ nvme_pcie_ctrlr_delete_io_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_
 
 free:
 	if (qpair->no_deletion_notification_needed == 0) {
-		/* Complete any I/O in the completion queue */
-		nvme_pcie_qpair_process_completions(qpair, 0);
-
 		/* Abort the rest of the I/O */
 		nvme_pcie_qpair_abort_trackers(qpair, 1);
 	}
