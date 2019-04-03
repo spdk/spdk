9751fe10df03853565a9818fe47f8e9cefc43758
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-04-03 20:43:15 +0000

    ocf: make vbdev_ocf_construct function asynchronous
    vbdev_ocf_construct now accepts callback.
    The function is not in fact asynchronous yet, but will be after adopting
      "trylock" API
    
    Most of the changes are related to making examine work with asycnrhonous
      bdev startup. We need to count references of bdevs that are being
      examined.
    
    Functions examine_end() is implemented for
      trully asynchronous version of register_vbdev,
      so interpreted it as placeholder until next patch that
      will implement the asynchronous register.
    
    Change-Id: I6ac091f2dd48462e74aa89cf54acb0306c30673b
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/bdev/ocf/vbdev_ocf.h
+++ lib/bdev/ocf/vbdev_ocf.h
@@ -160,11 +160,13 @@ struct vbdev_ocf {
 	TAILQ_ENTRY(vbdev_ocf)       tailq;
 };
 
-int vbdev_ocf_construct(
+void vbdev_ocf_construct(
 	const char *vbdev_name,
 	const char *cache_mode_name,
 	const char *cache_name,
-	const char *core_name);
+	const char *core_name,
+	void (*cb)(int, void *),
+	void *cb_arg);
 
 /* If vbdev is online, return its object */
 struct vbdev_ocf *vbdev_ocf_get_by_name(const char *name);
