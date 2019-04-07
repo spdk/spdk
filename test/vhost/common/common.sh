27328cd13f596de8540fd3d48fd2c127a1b73ae0
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-04-07 17:05:55 -0400

    test/vhost: windows scsi compliance test.
    We don't provide windows executable for this test.
    
    Change-Id: Iedaafba06662f776c102528f8073b6c3d56bb323
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/vhost/common/common.sh
+++ test/vhost/common/common.sh
@@ -260,6 +260,25 @@ function assert_number()
 	return 1;
 }
 
+# Run command on vm with given password
+# First argument - vm number
+# Second argument - ssh password for vm
+#
+function vm_sshpass()
+{
+	vm_num_is_valid $1 || return 1
+
+	local ssh_cmd="sshpass -p $2 ssh \
+		-o UserKnownHostsFile=/dev/null \
+		-o StrictHostKeyChecking=no \
+		-o User=root \
+		-p $(vm_ssh_socket $1) $VM_SSH_OPTIONS 127.0.0.1"
+
+	shift 2
+	$ssh_cmd "$@"
+}
+
+
 # Helper to validate VM number
 # param $1 VM number
 #
