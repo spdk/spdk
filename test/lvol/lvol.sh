396efc16aa02e99c4a34f2029a83374616763799
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-04-08 08:37:09 -0400

    test/lvol: Check if snapshot with snapshot can be deleted
    Change-Id: If4458e998abb6fc3415ebbd46c5e5081cddb9180
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/lvol/lvol.sh
+++ test/lvol/lvol.sh
@@ -72,6 +72,7 @@ function usage() {
                                     759: 'clone_decouple_parent_rw',
                                     760: 'set_read_only',
                                     761: 'delete_snapshot',
+                                    762: 'delete_snapshot_with_snapshot',
                                     800: 'rename_positive',
                                     801: 'rename_lvs_nonexistent',
                                     802: 'rename_lvs_EEXIST',
