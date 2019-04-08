421b73fb6369bcca4e9401bdc67830fc950e4f36
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-04-08 23:51:12 +0200

    iscsi: allocate sessions array using regular calloc
    spdk_dma_malloc() is not required here, as the session
    array is nether DMA-able nor shared between processes.
    
    Change-Id: Ia9218d00eea8a3207abf07c1d78eaa585ffcad2c
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- lib/iscsi/iscsi_subsystem.c
+++ lib/iscsi/iscsi_subsystem.c
@@ -1289,9 +1289,9 @@ iscsi_parse_globals(void)
 		return rc;
 	}
 
-	g_spdk_iscsi.session = spdk_dma_zmalloc(sizeof(void *) * g_spdk_iscsi.MaxSessions, 0, NULL);
+	g_spdk_iscsi.session = calloc(1, sizeof(void *) * g_spdk_iscsi.MaxSessions);
 	if (!g_spdk_iscsi.session) {
-		SPDK_ERRLOG("spdk_dma_zmalloc() failed for session array\n");
+		SPDK_ERRLOG("calloc() failed for session array\n");
 		return -1;
 	}
 
@@ -1306,12 +1306,14 @@ iscsi_parse_globals(void)
 	rc = iscsi_initialize_all_pools();
 	if (rc != 0) {
 		SPDK_ERRLOG("spdk_initialize_all_pools() failed\n");
+		free(g_spdk_iscsi.session);
 		return -1;
 	}
 
 	rc = spdk_initialize_iscsi_conns();
 	if (rc < 0) {
 		SPDK_ERRLOG("spdk_initialize_iscsi_conns() failed\n");
+		free(g_spdk_iscsi.session);
 		return rc;
 	}
 
