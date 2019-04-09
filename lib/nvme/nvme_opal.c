13d0081d3db7416b566bd37a6173630575793970
Chunyang Hui <chunyang.hui@intel.com>
2019-04-10 00:17:56 +0800

    Opal: Avoid misalignment for undefined behaviour
    Change-Id: I9e773a0ae5ac2275340bd4ce379f995125eb804c
    Signed-off-by: Chunyang Hui <chunyang.hui@intel.com>

--- lib/nvme/nvme_opal.c
+++ lib/nvme/nvme_opal.c
@@ -373,8 +373,8 @@ opal_discovery0_end(struct spdk_opal_dev *dev)
 	cpos += sizeof(*hdr); /* current position on buffer */
 
 	while (cpos < epos) {
-		const union spdk_discovery0_features *body =
-				(const union spdk_discovery0_features *)cpos;
+		union spdk_discovery0_features *body = calloc(1, sizeof(union spdk_discovery0_features));
+		memcpy(body, (union spdk_discovery0_features *)cpos, sizeof(union spdk_discovery0_features));
 		uint16_t feature_code = from_be16(&(body->tper.feature_code));
 
 		switch (feature_code) {
@@ -407,6 +407,7 @@ opal_discovery0_end(struct spdk_opal_dev *dev)
 			SPDK_NOTICELOG("Unknow feature code: %d\n", feature_code);
 		}
 		cpos += body->tper.length + 4;
+		free(body);
 	}
 
 	if (supported == false) {
