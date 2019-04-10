d09eff6a0f4ba5ddd3619419f26f61d86a390d5c
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-04-10 17:01:43 +0900

    lib/iscsi: Make two helper functions from private to public in iSCSI library
    These change will be used in the next patch.
    
    Change-Id: Ifdb4ccf20049b46e850122a4021cbbe7441e1270
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/450736
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/unit/lib/iscsi/common.c
+++ test/unit/lib/iscsi/common.c
@@ -175,6 +175,10 @@ DEFINE_STUB(spdk_scsi_dev_delete_port, int,
 
 DEFINE_STUB_V(spdk_shutdown_iscsi_conns, (void));
 
+DEFINE_STUB_V(spdk_iscsi_conns_start_exit, (struct spdk_iscsi_tgt_node *target));
+
+DEFINE_STUB(spdk_iscsi_get_active_conns, int, (struct spdk_iscsi_tgt_node *target), 0);
+
 void
 spdk_iscsi_task_cpl(struct spdk_scsi_task *scsi_task)
 {
