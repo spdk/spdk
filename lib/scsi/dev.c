1d0ae2d274f2288238e96cc569149f459e37f92e
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-04-11 18:10:47 +0900

    lib/scsi: Make spdk_scsi_dev_destruct asynchronous
    This is the end of the patch series. After this patch,
    delete_target_node RPC will wait for the completion of
    removal of the SCSI device and then free the iSCSI target.
    
    SCSI device holds passed callback and calls it in free_dev().
    free_dev() is ensured to be called after all iSCSI sessions
    are closed. So iSCSI target resource can be freed safely
    after that.
    
    Change-Id: I25921b4014207092b7b3845dfeae58bcdffa2edc
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/scsi/dev.c
+++ lib/scsi/dev.c
@@ -68,19 +68,37 @@ free_dev(struct spdk_scsi_dev *dev)
 	assert(dev->removed == true);
 
 	dev->is_allocated = 0;
+
+	if (dev->remove_cb) {
+		dev->remove_cb(dev->remove_ctx, 0);
+		dev->remove_cb = NULL;
+	}
 }
 
 void
-spdk_scsi_dev_destruct(struct spdk_scsi_dev *dev)
+spdk_scsi_dev_destruct(struct spdk_scsi_dev *dev,
+		       spdk_scsi_dev_destruct_cb_t cb_fn, void *cb_arg)
 {
 	int lun_cnt;
 	int i;
 
-	if (dev == NULL || dev->removed) {
+	if (dev == NULL) {
+		if (cb_fn) {
+			cb_fn(cb_arg, -EINVAL);
+		}
+		return;
+	}
+
+	if (dev->removed) {
+		if (cb_fn) {
+			cb_fn(cb_arg, -EBUSY);
+		}
 		return;
 	}
 
 	dev->removed = true;
+	dev->remove_cb = cb_fn;
+	dev->remove_ctx = cb_arg;
 	lun_cnt = 0;
 
 	for (i = 0; i < SPDK_SCSI_DEV_MAX_LUN; i++) {
@@ -236,7 +254,7 @@ spdk_scsi_dev_construct(const char *name, const char *bdev_name_list[],
 		rc = spdk_scsi_dev_add_lun(dev, bdev_name_list[i], lun_id_list[i],
 					   hotremove_cb, hotremove_ctx);
 		if (rc < 0) {
-			spdk_scsi_dev_destruct(dev);
+			spdk_scsi_dev_destruct(dev, NULL, NULL);
 			return NULL;
 		}
 	}
