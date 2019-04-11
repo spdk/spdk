43a0cd6f029266b4e01a657c498540cc106dbf39
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-04-11 08:20:20 -0400

    app/spdk_lspci: Simple app for printing available SPDK PCI devices
    Change-Id: I2e73bc62c0a282b673486fa4527ff4b8319d47a2
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

---
+++ app/spdk_lspci/spdk_lspci.c
@@ -0,0 +1,107 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+#include "spdk/env.h"
+#include "spdk/vmd.h"
+
+static void
+usage(void)
+{
+	printf("Usage: spkd_lspci\n");
+	printf("Print available SPDK PCI devices supported by NVMe driver.\n");
+}
+
+static int
+pci_enum_cb(void *ctx, struct spdk_pci_device *dev)
+{
+	return 0;
+}
+
+static void
+print_pci_dev(struct spdk_pci_device *dev)
+{
+	struct spdk_pci_addr pci_addr = spdk_pci_device_get_addr(dev);
+	char addr[32] = { 0 };
+
+	spdk_pci_addr_fmt(addr, sizeof(addr), &pci_addr);
+
+	printf("%s (%x %x)\n", addr,
+	       spdk_pci_device_get_vendor_id(dev),
+	       spdk_pci_device_get_device_id(dev));
+}
+
+int
+main(int argc, char **argv)
+{
+	int op;
+	struct spdk_env_opts opts;
+	struct spdk_pci_device *dev;
+
+	while ((op = getopt(argc, argv, "h")) != -1) {
+		switch (op) {
+		case 'h':
+		default:
+			usage();
+			return 1;
+		}
+	}
+
+	spdk_env_opts_init(&opts);
+	opts.name = "spdk_lspci";
+
+	if (spdk_env_init(&opts) < 0) {
+		fprintf(stderr, "Unable to initialize SPDK env\n");
+		return 1;
+	}
+
+	spdk_vmd_init();
+
+	if (spdk_pci_enumerate(spdk_pci_nvme_get_driver(), pci_enum_cb, NULL)) {
+		fprintf(stderr, "Unable to enumerate PCI nvme driver\n");
+		return 1;
+	}
+
+	dev = spdk_pci_get_first_device();
+	if (!dev) {
+		printf("\nLack of PCI devices available for SPDK!\n");
+	}
+
+	printf("\nList of available PCI devices:\n");
+	while (dev) {
+		print_pci_dev(dev);
+		dev = spdk_pci_get_next_device(dev);
+	}
+
+	return 0;
+}
