d6f0613d193ba119d7bc43df7d3195ebf61fcf1e
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-04-12 18:28:13 +0000

    ocf: test: use all cache modes
    Modify FIO test to use all cache modes that we support,
      including WriteBack
    
    New test config uses Nvme bdevs instead of mallocs because
      memory is an issue when testing OCF
    
    Change-Id: I3abec9605b61791f8ebaaaf08b88a011a50d3f26
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/451022
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

---
+++ test/ocf/integrity/fio-modes.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+curdir=$(dirname $(readlink -f "$BASH_SOURCE"))
+rootdir=$(readlink -f $curdir/../../..)
+plugindir=$rootdir/examples/bdev/fio_plugin
+
+source $rootdir/test/common/autotest_common.sh
+
+function fio_verify(){
+	LD_PRELOAD=$plugindir/fio_plugin /usr/src/fio/fio $curdir/test.fio --aux-path=/tmp/ --ioengine=spdk_bdev $@
+}
+
+function cleanup(){
+	rm -f $curdir/modes.conf
+}
+
+trap "cleanup; exit 1" SIGINT SIGTERM EXIT
+
+nvme_cfg=$($rootdir/scripts/gen_nvme.sh)
+
+config="
+$nvme_cfg
+
+[Split]
+  Split Nvme0n1 8 101
+
+[OCF]
+  OCF PT_Nvme  pt Nvme0n1p0 Nvme0n1p1
+  OCF WT_Nvme  wt Nvme0n1p2 Nvme0n1p3
+  OCF WB_Nvme0 wb Nvme0n1p4 Nvme0n1p5
+  OCF WB_Nvme1 wb Nvme0n1p6 Nvme0n1p7
+"
+echo "$config" > $curdir/modes.conf
+
+fio_verify --filename=PT_Nvme:WT_Nvme:WB_Nvme0:WB_Nvme1 --spdk_conf=$curdir/modes.conf
+
+trap - SIGINT SIGTERM EXIT
+cleanup
