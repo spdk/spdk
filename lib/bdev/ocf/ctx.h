bc7e88b04333f3a2210ff4a9d7f948276a8f982b
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-04-17 18:02:54 +0000

    ocf: add shared cache instance context
    Introduce cache context structure that is going to be used
      for sharing per cache info. For example: management queue,
      cleaner_channel, cleaner_thread.
    
    Lifetime of this structure ends with last vbdev that gets unregistered
      and NOT when cache stops because cache does not have to be freed
      there.
    
    Change-Id: I66252084d7efda92edd10fb737c8e9c8169b4f6d
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/bdev/ocf/ctx.h
+++ lib/bdev/ocf/ctx.h
@@ -42,6 +42,14 @@ extern ocf_ctx_t vbdev_ocf_ctx;
 
 #define SPDK_OBJECT 1
 
+/* Context of cache instance */
+struct vbdev_ocf_cache_ctx {
+	env_atomic                   refcnt;
+};
+
+void vbdev_ocf_cache_ctx_put(struct vbdev_ocf_cache_ctx *ctx);
+void vbdev_ocf_cache_ctx_get(struct vbdev_ocf_cache_ctx *ctx);
+
 int vbdev_ocf_ctx_init(void);
 void vbdev_ocf_ctx_cleanup(void);
 
