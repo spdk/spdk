eb95f4cd206e5e4ce1b72d2b8c9fcaa6eaa90e0e
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-04-17 18:41:37 +0000

    ocf: implement mngt_queue for asynchronous management
    Implement management queue for asynchronous management that is provided
      in new OCF API. This is a neccessery step to be able to implement
      metadata support.
    
    OCF submodule was updated to get management queue functionality
    It has to be done in this patch because OCF module will not work
    otherwise.
    
    Change-Id: Id19c2e5bd6a5d26fee41752b62720e408dc082e8
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- lib/bdev/ocf/ctx.h
+++ lib/bdev/ocf/ctx.h
@@ -44,6 +44,7 @@ extern ocf_ctx_t vbdev_ocf_ctx;
 
 /* Context of cache instance */
 struct vbdev_ocf_cache_ctx {
+	ocf_queue_t                  mngt_queue;
 	pthread_mutex_t              lock;
 	env_atomic                   refcnt;
 };
