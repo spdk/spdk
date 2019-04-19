5d75a943391a6ee430e86302d66e2bbecaf94c4c
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-04-19 21:33:57 +0000

    ocf: remove scanbuild workaround
    With 13.01 release OCF passes scanbuild tests so we don't have to build
      it separately in our autobuild.sh script.
    
    This patch just removes all code related to building OCF library.
    
    Change-Id: I7845fa8abd8c0fc3e3a792ed9b70e9e7e77cede2
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -14,17 +14,6 @@ cd $rootdir
 date -u
 git describe --tags
 
-if [ "$SPDK_TEST_OCF" -eq 1 ]; then
-	# We compile OCF sources ourselves
-	# They don't need to be checked with scanbuild and code coverage is not applicable
-	# So we precompile OCF now for further use as standalone static library
-	./configure $(echo $config_params | sed 's/--enable-coverage//g')
-	$MAKE $MAKEFLAGS include/spdk/config.h
-	CC=gcc CCAR=ar $MAKE $MAKEFLAGS -C lib/bdev/ocf/env exportlib O=$rootdir/build/ocf.a
-	# Set config to use precompiled library
-	config_params="$config_params --with-ocf=/$rootdir/build/ocf.a"
-fi
-
 ./configure $config_params
 
 # Print some test system info out for the log
