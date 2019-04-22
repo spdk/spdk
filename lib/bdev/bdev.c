83837a10248140387bd3c0a10701804ac76d8ff0
Changpeng Liu <changpeng.liu@intel.com>
2019-04-22 18:36:50 -0400

    bdev: get io buffer when splitting on io boundary
    Existing code only process the READ requests, and WRITE requests also
    need same process.
    
    Change-Id: I3b009d5f21f41b72eb2c9722ffe92fc82a6ed202
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/bdev/bdev.c
+++ lib/bdev/bdev.c
@@ -454,6 +454,9 @@ _are_iovs_aligned(struct iovec *iovs, int iovcnt, uint32_t alignment)
 		if ((iov_base & (alignment - 1)) != 0) {
 			return false;
 		}
+		if (iovs[i].iov_len % alignment) {
+			return false;
+		}
 	}
 
 	return true;
@@ -1720,12 +1723,8 @@ spdk_bdev_io_submit(struct spdk_bdev_io *bdev_io)
 	assert(bdev_io->internal.status == SPDK_BDEV_IO_STATUS_PENDING);
 
 	if (bdev->split_on_optimal_io_boundary && _spdk_bdev_io_should_split(bdev_io)) {
-		if (bdev_io->type == SPDK_BDEV_IO_TYPE_READ) {
-			spdk_bdev_io_get_buf(bdev_io, _spdk_bdev_io_split_get_buf_cb,
-					     bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
-		} else {
-			_spdk_bdev_io_split(NULL, bdev_io);
-		}
+		spdk_bdev_io_get_buf(bdev_io, _spdk_bdev_io_split_get_buf_cb,
+				     bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
 		return;
 	}
 
