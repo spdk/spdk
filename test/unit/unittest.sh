d9e9fca661bdb8097348668e97b61b8213a16bfa
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-04-22 18:28:02 +0900

    NVMe-oF Target: Add FC transport.
      - New files and updates to existing SPDK files to add the NVMf-FC transport.
      - Low level FC driver abstraction API. This driver is not part of SPDK repository.
      - Makefile updates to build FC transport (using CONFIG_FC) along with low-level
        FC driver files, which must be placed into the "fc" directory at SPDK root.
      - Update configure script for FC build.
      - New fc_adm_api lib. for FC admin and event management.
      - New FC unit test for FC-LS commands.
      - Update unittest.sh to run FC unit test (when built).
    
    Signed-off-by: John Barnard <john.barnard@broadcom.com>
    Signed-off-by: Anil Veerabhadrappa <anil.veerabhadrappa@broadcom.com>
    Change-Id: If31d4d25feab76c2dbe90a7faf71d465c2c3a354

--- test/unit/unittest.sh
+++ test/unit/unittest.sh
@@ -152,7 +152,15 @@ $valgrind $testdir/lib/util/string.c/string_ut
 $valgrind $testdir/lib/util/dif.c/dif_ut
 
 if [ $(uname -s) = Linux ]; then
-$valgrind $testdir/lib/vhost/vhost.c/vhost_ut
+	$valgrind $testdir/lib/vhost/vhost.c/vhost_ut
+fi
+
+if [ -e $testdir/lib/nvmf/fc.c/fc_ut ]; then
+	$valgrind $testdir/lib/nvmf/fc.c/fc_ut
+fi
+
+if [ -e $testdir/lib/nvmf/fc_ls.c/fc_ls_ut ]; then
+	$valgrind $testdir/lib/nvmf/fc_ls.c/fc_ls_ut
 fi
 
 if grep -q '#define SPDK_CONFIG_FTL 1' $rootdir/include/spdk/config.h; then
