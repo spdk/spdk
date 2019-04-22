7c4e1f6aa9d195af04cd316274adfe4e774491d0
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-04-22 18:28:02 +0900

    NVMe-oF Target: Add FC transport.
      - New files and updates to existing SPDK files to add the NVMf-FC transport.
      - Low level FC driver abstraction API. This driver is not part of SPDK repository.
      - Makefile updates to build FC transport (using CONFIG_FC) along with low-level
        FC driver files, which must be placed into the "fc" directory at SPDK root.
      - Update configure script for FC build.
      - New fc_adm_api lib. for FC admin and event management.
      - New FC unit test for FC-LS commands.
      - Update unittest.sh to run FC unit test (when built).
    
    Signed-off-by: John Barnard <john.barnard@broadcom.com>
    Signed-off-by: Anil Veerabhadrappa <anil.veerabhadrappa@broadcom.com>
    Change-Id: If31d4d25feab76c2dbe90a7faf71d465c2c3a354

--- lib/nvmf/transport.c
+++ lib/nvmf/transport.c
@@ -47,6 +47,9 @@ static const struct spdk_nvmf_transport_ops *const g_transport_ops[] = {
 	&spdk_nvmf_transport_rdma,
 #endif
 	&spdk_nvmf_transport_tcp,
+#ifdef SPDK_CONFIG_FC
+	&spdk_nvmf_transport_fc,
+#endif
 };
 
 #define NUM_TRANSPORTS (SPDK_COUNTOF(g_transport_ops))
@@ -87,8 +90,9 @@ spdk_nvmf_transport_create(enum spdk_nvme_transport_type type,
 
 	ops = spdk_nvmf_get_transport_ops(type);
 	if (!ops) {
-		SPDK_ERRLOG("Transport type %s unavailable.\n",
-			    spdk_nvme_transport_id_trtype_str(type));
+		const char *type_str = spdk_nvme_transport_id_trtype_str(type);
+		SPDK_ERRLOG("Transport type '%s' unavailable.\n",
+			    type_str ? type_str : "unknown type");
 		return NULL;
 	}
 
