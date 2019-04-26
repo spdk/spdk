d339b5921664838d477460d85da57f817df36281
Mike Carlin <mikefcarlin@protonmail.com>
2019-04-26 10:30:50 -0700

    spdkcli: Add support for raid devices
    Adds support to spdkcli.py for raid devices via UIRaidBdev with
    functionality to construct and destroy raid bdevs.
    
    Information regarding the raid can be viewed via 'show_details' on a
    given pooled_device child in the tree.
    
    Signed-off-by: Mike Carlin <mikefcarlin@protonmail.com>
    Change-Id: I2ca89e641b201c09b3d4db070eac6cc0c2cdec73

---
+++ test/spdkcli/raid.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -xe
+
+MATCH_FILE="spdkcli_raid.test"
+SPDKCLI_BRANCH="/bdevs"
+testdir=$(readlink -f $(dirname $0))
+. $testdir/common.sh
+
+timing_enter spdkcli_raid
+trap 'on_error_exit;' ERR
+
+timing_enter run_spdk_tgt
+run_spdk_tgt
+timing_exit run_spdk_tgt
+
+timing_enter spdkcli_create_malloc
+$spdkcli_job "'/bdevs/malloc create 8 512 Malloc1' 'Malloc1' True
+'/bdevs/malloc create 8 512 Malloc2' 'Malloc2' True
+"
+timing_exit spdkcli_create_malloc
+
+timing_enter spdkcli_create_raid
+$spdkcli_job "'/bdevs/raid_volume create testraid 0 \"Malloc1 Malloc2\" 4' 'testraid' True
+"
+timing_exit spdkcli_create_raid
+
+timing_enter spdkcli_check_match
+check_match
+timing_exit spdkcli_check_match
+
+timing_enter spdkcli_delete_raid
+$spdkcli_job "'/bdevs/raid_volume delete testraid' '' True
+"
+timing_exit spdkcli_delete_raid
+
+timing_enter spdkcli_delete_malloc
+$spdkcli_job "'/bdevs/malloc delete Malloc1' '' True
+'/bdevs/malloc delete Malloc2' '' True
+"
+timing_exit spdkcli_delete_malloc
+
+killprocess $spdk_tgt_pid
+timing_exit spdkcli_raid
+report_test_completion spdk_cli
