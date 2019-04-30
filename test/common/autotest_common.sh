3080b95e97ca928220994308723ca2df52946de8
Jim Harris <james.r.harris@intel.com>
2019-04-30 15:57:29 -0700

    bdev/nvme: always enable FTL
    FTL doesn't have any kind of special package requirements.
    It is getting pretty good traction in the community, so
    let's enable it by default.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I15525b6c4e6ee52f49adf74d55f9484fe08a6dcc

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -197,10 +197,6 @@ if [ ! -d "${DEPENDENCY_DIR}/nvme-cli" ]; then
 	export SPDK_TEST_NVME_CLI=0
 fi
 
-if [ $SPDK_TEST_FTL -eq 1 ]; then
-	config_params+=' --with-ftl'
-fi
-
 if [ $SPDK_TEST_ISAL -eq 0 ]; then
 	config_params+=' --without-isal'
 fi
