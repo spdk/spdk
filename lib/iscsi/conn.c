945793a7506480eff05c9a952c4e6601f95576fa
Jim Harris <james.r.harris@intel.com>
2019-04-30 13:45:07 -0700

    trace: shorten max name from 44 to 24 characters
    This restriction helps reduce the amount of padding when
    printing out the event trace, allowing it to fit in a
    small number of columns.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: Ifa31e5a6967c7b9bc7028069effb71533f80596f

--- lib/iscsi/conn.c
+++ lib/iscsi/conn.c
@@ -1581,11 +1581,11 @@ SPDK_TRACE_REGISTER_FN(iscsi_conn_trace, "iscsi_conn", TRACE_GROUP_ISCSI)
 {
 	spdk_trace_register_owner(OWNER_ISCSI_CONN, 'c');
 	spdk_trace_register_object(OBJECT_ISCSI_PDU, 'p');
-	spdk_trace_register_description("ISCSI_READ_FROM_SOCKET_DONE", TRACE_ISCSI_READ_FROM_SOCKET_DONE,
+	spdk_trace_register_description("ISCSI_READ_DONE", TRACE_ISCSI_READ_FROM_SOCKET_DONE,
 					OWNER_ISCSI_CONN, OBJECT_NONE, 0, 0, "");
-	spdk_trace_register_description("ISCSI_FLUSH_WRITEBUF_START", TRACE_ISCSI_FLUSH_WRITEBUF_START,
+	spdk_trace_register_description("ISCSI_WRITE_START", TRACE_ISCSI_FLUSH_WRITEBUF_START,
 					OWNER_ISCSI_CONN, OBJECT_NONE, 0, 0, "iovec: ");
-	spdk_trace_register_description("ISCSI_FLUSH_WRITEBUF_DONE", TRACE_ISCSI_FLUSH_WRITEBUF_DONE,
+	spdk_trace_register_description("ISCSI_WRITE_DONE", TRACE_ISCSI_FLUSH_WRITEBUF_DONE,
 					OWNER_ISCSI_CONN, OBJECT_NONE, 0, 0, "");
 	spdk_trace_register_description("ISCSI_READ_PDU", TRACE_ISCSI_READ_PDU,
 					OWNER_ISCSI_CONN, OBJECT_ISCSI_PDU, 1, 0, "opc:   ");
