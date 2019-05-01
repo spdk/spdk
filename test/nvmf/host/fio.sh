59955106a66affac456e8ebcf30253133b82c90d
Jim Harris <james.r.harris@intel.com>
2019-05-01 09:21:59 -0700

    test/nvmf: simplify host/fio.sh
    We don't need to use the UUIDs for lvolstores and
    lvols in the nightly test - just use the names that
    we've specified.
    
    Also remove the bdev loops - there's only one namespace
    per subsystem.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I684b72ca4472009dd8bc49f5221a7937769f1ab8
    
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/452772
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- test/nvmf/host/fio.sh
+++ test/nvmf/host/fio.sh
@@ -38,11 +38,9 @@ waitforlisten $nvmfpid
 $rpc_py nvmf_create_transport -t RDMA -u 8192 -p 4
 timing_exit start_nvmf_tgt
 
-bdevs="$bdevs $($rpc_py construct_malloc_bdev 64 512)"
+$rpc_py construct_malloc_bdev 64 512 -b Malloc1
 $rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
-for bdev in $bdevs; do
-	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 $bdev
-done
+$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Malloc1
 $rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t rdma -a $NVMF_FIRST_TARGET_IP -s 4420
 
 PLUGIN_DIR=$rootdir/examples/nvme/fio_plugin
@@ -60,26 +58,22 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 	# Test fio_plugin as host with nvme lvol backend
 	bdfs=$(iter_pci_class_code 01 08 02)
 	$rpc_py construct_nvme_bdev -b Nvme0 -t PCIe -a $(echo $bdfs | awk '{ print $1 }') -i $NVMF_FIRST_TARGET_IP
-	ls_guid=$($rpc_py construct_lvol_store Nvme0n1 lvs_0)
-	get_lvs_free_mb $ls_guid
-	lb_guid=$($rpc_py construct_lvol_bdev -u $ls_guid lbd_0 $free_mb)
+	$rpc_py construct_lvol_store Nvme0n1 lvs_0
+	get_lvs_free_mb lvs_0
+	$rpc_py construct_lvol_bdev -l lvs_0 lbd_0 $free_mb
 	$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode2 -a -s SPDK00000000000001
-	for bdev in $lb_guid; do
-		$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode2 $bdev
-	done
+	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode2 lvs_0/lbd_0
 	$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode2 -t rdma -a $NVMF_FIRST_TARGET_IP -s 4420
 	LD_PRELOAD=$PLUGIN_DIR/fio_plugin /usr/src/fio/fio $PLUGIN_DIR/example_config.fio --filename="trtype=RDMA adrfam=IPv4 \
 	traddr=$NVMF_FIRST_TARGET_IP trsvcid=4420 ns=1"
 	$rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk:cnode2
 
 	# Test fio_plugin as host with nvme lvol nested backend
-	ls_nested_guid=$($rpc_py construct_lvol_store $lb_guid lvs_n_0)
-	get_lvs_free_mb $ls_nested_guid
-	lb_nested_guid=$($rpc_py construct_lvol_bdev -u $ls_nested_guid lbd_nest_0 $free_mb)
+	$rpc_py construct_lvol_store lvs_0/lbd_0 lvs_n_0
+	get_lvs_free_mb lvs_n_0
+	$rpc_py construct_lvol_bdev -l lvs_n_0 lbd_nest_0 $free_mb
 	$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode3 -a -s SPDK00000000000001
-	for bdev in $lb_nested_guid; do
-		$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode3 $bdev
-	done
+	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode3 lbd_nest_0/lvs_n_0
 	$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode3 -t rdma -a $NVMF_FIRST_TARGET_IP -s 4420
 	LD_PRELOAD=$PLUGIN_DIR/fio_plugin /usr/src/fio/fio $PLUGIN_DIR/example_config.fio --filename="trtype=RDMA adrfam=IPv4 \
 	traddr=$NVMF_FIRST_TARGET_IP trsvcid=4420 ns=1"
@@ -87,9 +81,9 @@ if [ $RUN_NIGHTLY -eq 1 ]; then
 
 	sync
 	# Delete lvol_bdev and destroy lvol_store.
-	$rpc_py destroy_lvol_bdev "$lb_nested_guid"
+	$rpc_py destroy_lvol_bdev lbd_nest_0/lvs_n_0
 	$rpc_py destroy_lvol_store -l lvs_n_0
-	$rpc_py destroy_lvol_bdev "$lb_guid"
+	$rpc_py destroy_lvol_bdev lvs_0/lbd_0
 	$rpc_py destroy_lvol_store -l lvs_0
 	$rpc_py delete_nvme_controller Nvme0
 fi
