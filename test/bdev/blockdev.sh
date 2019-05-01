4b6280d84dbd4841c50d0b9b68ce26e1dbbda4b3
paul luse <paul.e.luse@intel.com>
2019-05-01 19:36:14 -0400

    test: only run hello_world bdev test nightly
    May not be the long pole in the tent right now but it is 11 secs
    to make sure our example app is working, it adds no nvme coverage
    so move it to nightly instead of per patch.
    
    Change-Id: I779a9f2a6951b3c51a82403c8cf085a251fb955c
    Signed-off-by: paul luse <paul.e.luse@intel.com>

--- test/bdev/blockdev.sh
+++ test/bdev/blockdev.sh
@@ -79,11 +79,13 @@ if hash pmempool; then
 	echo "  Blk /tmp/spdk-pmem-pool Pmem0" >> $testdir/bdev.conf
 fi
 
-timing_enter hello_bdev
-if grep -q Nvme0 $testdir/bdev.conf; then
-	$rootdir/examples/bdev/hello_world/hello_bdev -c $testdir/bdev.conf -b Nvme0n1
+if [ $RUN_NIGHTLY -eq 1 ]; then
+	timing_enter hello_bdev
+	if grep -q Nvme0 $testdir/bdev.conf; then
+		$rootdir/examples/bdev/hello_world/hello_bdev -c $testdir/bdev.conf -b Nvme0n1
+	fi
+	timing_exit hello_bdev
 fi
-timing_exit hello_bdev
 
 timing_enter bounds
 if [ $(uname -s) = Linux ]; then
