879a03d91f76002863b3d6aa7d0da476561a8f2a
Jim Harris <james.r.harris@intel.com>
2019-05-01 16:13:01 -0700

    test/nvmf: simplify subsystem creation in bdev_io_wait test
    We only create one namespace, so this can be simpler.  We
    can also just specify the name of the malloc bdev explicitly.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I04141cb8013605f9c54777147852a53d22ae68cf

--- test/nvmf/bdev_io_wait/bdev_io_wait.sh
+++ test/nvmf/bdev_io_wait/bdev_io_wait.sh
@@ -40,12 +40,9 @@ timing_exit start_nvmf_tgt
 
 modprobe -v nvme-rdma
 
-bdevs="$bdevs $($rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE)"
-
+$rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc0
 $rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
-for bdev in $bdevs; do
-	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 $bdev
-done
+$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Malloc0
 $rpc_py nvmf_subsystem_add_listener  nqn.2016-06.io.spdk:cnode1 -t rdma -a $NVMF_FIRST_TARGET_IP -s 4420
 
 echo "[Nvme]" > $testdir/bdevperf.conf
