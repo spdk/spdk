d79febaed31b2b6fe8cb9b201e05d9b783a7e24b
Changpeng Liu <changpeng.liu@intel.com>
2019-05-05 01:05:37 -0400

    bdev: split requests first before getting the bounce buffer
    For those backend devices which need IO boundary, the bdev layer
    should split the large IO request first before getting the
    bounce buffer, because existing code can only support up to
    64KiB data length for bounce buffer.
    
    Change-Id: I67661f5fa4c3c7c561b45c86146759aa3477adbf
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/bdev/bdev.c
+++ lib/bdev/bdev.c
@@ -1665,18 +1665,6 @@ _spdk_bdev_io_split(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io)
 	_spdk_bdev_io_split_with_payload(bdev_io);
 }
 
-static void
-_spdk_bdev_io_split_get_buf_cb(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io,
-			       bool success)
-{
-	if (!success) {
-		spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_FAILED);
-		return;
-	}
-
-	_spdk_bdev_io_split(ch, bdev_io);
-}
-
 /* Explicitly mark this inline, since it's used as a function pointer and otherwise won't
  *  be inlined, at least on some compilers.
  */
@@ -1728,12 +1716,7 @@ spdk_bdev_io_submit(struct spdk_bdev_io *bdev_io)
 	assert(bdev_io->internal.status == SPDK_BDEV_IO_STATUS_PENDING);
 
 	if (bdev->split_on_optimal_io_boundary && _spdk_bdev_io_should_split(bdev_io)) {
-		if (bdev_io->type == SPDK_BDEV_IO_TYPE_READ) {
-			spdk_bdev_io_get_buf(bdev_io, _spdk_bdev_io_split_get_buf_cb,
-					     bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
-		} else {
-			_spdk_bdev_io_split(NULL, bdev_io);
-		}
+		_spdk_bdev_io_split(NULL, bdev_io);
 		return;
 	}
 
