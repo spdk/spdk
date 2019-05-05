187e10284c1835d332a73c5a1e5331ba1325be4f
Changpeng Liu <changpeng.liu@intel.com>
2019-05-05 01:05:37 -0400

    bdev: split requests first before getting the bounce buffer
    For those backend devices which need IO boundary, the bdev layer
    should split the large IO request first before getting the
    bounce buffer, because existing code can only support up to
    64KiB data length for bounce buffer.
    
    There is one scenario that for iSCSI READ request, the iSCSI
    layer will not allocate data buffer for the request, so for
    this case if the IO boundary is required, we must split the
    parent bdev_io based on IO boundary first, and then get the
    data buffer from bdev layer, the parent bdev_io need to
    record each child's offset position, and copy children's
    data buffer when all the children IOs are finished.
    
    Since existing iSCSI layer can't support multiple vectors
    yet, so for now we need the data copy, but once the iSCSI
    layer can support multiple vectors, we can remove the data
    copy.
    
    Change-Id: I67661f5fa4c3c7c561b45c86146759aa3477adbf
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/bdev/raid/bdev_raid.c
+++ lib/bdev/raid/bdev_raid.c
@@ -831,7 +831,7 @@ raid_bdev_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_i
 {
 	switch (bdev_io->type) {
 	case SPDK_BDEV_IO_TYPE_READ:
-		if (bdev_io->u.bdev.iovs[0].iov_base == NULL) {
+		if (bdev_io->u.bdev.iovs == NULL || bdev_io->u.bdev.iovs[0].iov_base == NULL) {
 			spdk_bdev_io_get_buf(bdev_io, raid_bdev_get_buf_cb,
 					     bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
 		} else {
