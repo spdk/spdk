a3a98ad184c8bbe92d91cd878b01f489dbc0e1e0
Jim Harris <james.r.harris@intel.com>
2019-05-06 13:18:07 -0700

    test: don't call python explicitly
    We use python3 now, not python.  This is important
    because in next patch we will use a new 'aliases'
    keyword argument in the argparse API that's only
    available in Python 3.
    
    While here, clean up some documentation that was
    instructing users to call python explicitly instead
    of just invoking scripts/rpc.py directly.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I0c11e131cd56f7c994aa8fe21b4b00ae33489fdd

--- test/ocf/integrity/bdevperf-iotypes.sh
+++ test/ocf/integrity/bdevperf-iotypes.sh
@@ -5,7 +5,7 @@ rootdir=$(readlink -f $curdir/../../..)
 source $rootdir/test/common/autotest_common.sh
 
 bdevperf=$rootdir/test/bdev/bdevperf/bdevperf
-rpc_py="python $rootdir/scripts/rpc.py"
+rpc_py="$rootdir/scripts/rpc.py"
 
 $bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -t 4 -w flush
 $bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -t 4 -w unmap
