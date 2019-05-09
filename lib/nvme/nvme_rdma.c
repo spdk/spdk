6629841dc28f656cbea2c6f574d5cd56ce3bef04
Jim Harris <james.r.harris@intel.com>
2019-05-09 10:40:12 -0700

    nvme: check is_enabled flag at common layer
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I85e8289d10b481d3ca1cd125f73bd5abc4d1bf16

--- lib/nvme/nvme_rdma.c
+++ lib/nvme/nvme_rdma.c
@@ -1558,11 +1558,10 @@ nvme_rdma_qpair_submit_request(struct spdk_nvme_qpair *qpair,
 	assert(req != NULL);
 
 	rdma_req = nvme_rdma_req_get(rqpair);
-	if (!rdma_req || !qpair->is_enabled) {
+	if (!rdma_req) {
 		/*
-		 * No rdma_req is available, or the qpair is disabled due to
-		 *  an in-progress reset.  Queue the request to be processed
-		 *  later.
+		 * No rdma_req is available, so queue the request to be
+		 *  processed later.
 		 */
 		STAILQ_INSERT_TAIL(&qpair->queued_req, req, stailq);
 		return 0;
