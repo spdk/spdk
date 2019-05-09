3bf4c6060856e5fa8ea3b8a0c39c1262a0d20626
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-05-09 16:31:16 +0200

    autorun.sh: print test configuration at the beginning
    We're about to disable bash xtraces in autotest_common.sh
    and currently that's the only place where the test
    configuration is printed.
    
    It might be still useful to print the entire configuration,
    so now we'll be doing that from autorun.sh - just once!
    
    Change-Id: I1371caacfc95c19b24a429b81baffeeef7dd1142
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Acked-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/453874
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,15 @@ rootdir=$(readlink -f $(dirname $0))
 
 conf=~/autorun-spdk.conf
 
+# If the configuration of tests is not provided, no tests will be carried out.
+if [[ ! -f $conf ]]; then
+	echo "ERROR: $conf doesn't exist"
+	exit 1
+fi
+
+echo "Test configuration:"
+cat "$conf"
+
 # Runs agent scripts
 $rootdir/autobuild.sh "$conf"
 sudo WITH_DPDK_DIR="$WITH_DPDK_DIR" $rootdir/autotest.sh "$conf"
