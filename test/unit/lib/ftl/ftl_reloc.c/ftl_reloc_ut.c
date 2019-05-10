0c187997575120f1158a5b971c26c886bd9edc63
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-05-10 11:06:04 -0400

    lib/ftl: Stop using ftl_io for internal relocation logic
    Added ftl_reloc_move structure to internally track
    relocation requests. This patch remove unnecessary
    dependency between relocation module and ftl_io.
    
    Change-Id: I9dd1bca6039b7c8961cb22a9f85ea1c5737a0a6f
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- test/unit/lib/ftl/ftl_reloc.c/ftl_reloc_ut.c
+++ test/unit/lib/ftl/ftl_reloc.c/ftl_reloc_ut.c
@@ -128,6 +128,7 @@ int
 ftl_io_read(struct ftl_io *io)
 {
 	io->cb.fn(io->cb.ctx, 0);
+	free(io);
 	return 0;
 }
 
@@ -135,6 +136,8 @@ int
 ftl_io_write(struct ftl_io *io)
 {
 	io->cb.fn(io->cb.ctx, 0);
+	free(io->lba.vector);
+	free(io);
 	return 0;
 }
 
@@ -213,7 +216,12 @@ cleanup_reloc(struct spdk_ftl_dev *dev, struct ftl_reloc *reloc)
 {
 	size_t i;
 
+	for (i = 0; i < ftl_dev_num_bands(reloc->dev); ++i) {
+		SPDK_CU_ASSERT_FATAL(reloc->brelocs[i].active == false);
+	}
+
 	ftl_reloc_free(reloc);
+
 	for (i = 0; i < ftl_dev_num_bands(dev); ++i) {
 		test_free_ftl_band(&dev->bands[i]);
 	}
@@ -459,6 +467,7 @@ test_reloc_empty_band(void)
 	ftl_reloc(reloc);
 
 	CU_ASSERT_EQUAL(breloc->num_lbks, 0);
+	SPDK_CU_ASSERT_FATAL(breloc->moves != NULL);
 
 	cleanup_reloc(dev, reloc);
 }
