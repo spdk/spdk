f686bddce77be91ce727bb5efd6eee9dcf3de800
Jim Harris <james.r.harris@intel.com>
2019-05-13 06:48:19 -0700

    nvme: add nvme process_completions variants with tsc
    This will allow the driver to make decisions such as
    cq doorbell coalescing based on the current tsc.
    
    Note that we have the user pass in the tsc, rather
    than having the driver explicitly get it, since getting
    the tsc can be expensive, and the calling application
    may already have a fairly recent tsc collected for other
    reasons.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I063b38011a487df41ca6cb38c06d16b025032803

--- test/unit/lib/nvme/nvme_qpair.c/nvme_qpair_ut.c
+++ test/unit/lib/nvme/nvme_qpair.c/nvme_qpair_ut.c
@@ -69,7 +69,8 @@ nvme_transport_qpair_submit_request(struct spdk_nvme_qpair *qpair, struct nvme_r
 }
 
 int32_t
-nvme_transport_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_completions)
+nvme_transport_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_completions,
+					 uint64_t current_tsc)
 {
 	/* TODO */
 	return 0;
