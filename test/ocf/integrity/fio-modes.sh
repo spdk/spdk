2aa74314582362e94355012eb944df2f3e396476
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-05-15 23:53:16 +0000

    [TEST] ocf: check tests on lower memory usage
    Change-Id: Ia6a252bbe6894194a52e51a28d28376159602e40
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- test/ocf/integrity/fio-modes.sh
+++ test/ocf/integrity/fio-modes.sh
@@ -15,40 +15,29 @@ function cleanup(){
 	rm -f $curdir/modes.conf
 }
 
-truncate -s 101M $curdir/aio_core_m0
-truncate -s 101M $curdir/aio_core_m1
-
-truncate -s 101M $curdir/aio_cache_pt
-truncate -s 101M $curdir/aio_core_pt
 truncate -s 101M $curdir/aio_cache_wb
 truncate -s 101M $curdir/aio_core_wb
 
 trap "cleanup; exit 1" SIGINT SIGTERM EXIT
 
-echo "
-[Malloc]
-  NumberOfLuns 1
-  LunSizeInMB 300
-  BlockSize 512
+nvme_cfg=$($rootdir/scripts/gen_nvme.sh)
+
+config="
+$nvme_cfg
 
 [AIO]
-  AIO $curdir/aio_core_m0  aio_core_m0  512
-  AIO $curdir/aio_core_m1  aio_core_m1  512
-
-  AIO $curdir/aio_cache_pt aio_cache_pt 512
-  AIO $curdir/aio_core_pt  aio_core_pt  512
   AIO $curdir/aio_cache_wb aio_cache_wb 512
   AIO $curdir/aio_core_wb  aio_core_wb  512
 
+[Split]
+  Split Nvme0n1 2 101
+
 [OCF]
-  OCF WT_MCache-0 wt Malloc0      aio_core_m0
-  OCF WT_MCache-1 wt Malloc0      aio_core_m1
+  OCF WB_AIOCache wb Nvme0n1p0 Nvme0n1p1
+"
+echo "$config" > $curdir/modes.conf
 
-  OCF PT_AIOCache pt aio_cache_pt aio_core_pt
-  OCF WB_AIOCache wb aio_cache_wb aio_core_wb
-" > $curdir/modes.conf
-
-fio_verify --filename=WT_MCache-0:WT_MCache-1:PT_AIOCache:WB_AIOCache --spdk_conf=$curdir/modes.conf
+fio_verify --filename=WB_AIOCache --spdk_conf=$curdir/modes.conf
 
 trap - SIGINT SIGTERM EXIT
 cleanup
