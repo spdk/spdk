1c9895a3c116ca42786c52118d5fdd52785493cc
Jim Harris <james.r.harris@intel.com>
2019-05-16 14:39:52 -0700

    test/nvmf: remove -p num_queues restrictions
    There's a bug in Linux 5.1 (possibly 5.0 too) where
    the kernel initiator driver crashes if it can't get
    one queue per CPU.  This will get fixed eventually,
    but for now we need to remove the cases where we
    restrict the number of queues per controller so that
    we can test on newer kernels.
    
    Even on cases where we're testing the SPDK initiator,
    there's no real need to restrict the number of queue
    pairs.
    
    The kernel will eventually get fixed, but we should
    be testing with default behavior anyways (the kernel
    wants lots of queues).  We'll also want to add some
    regression tests to make sure the kernel doesn't
    break again.  But that will all come later.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I9979e6d94456e075688b822b042936b63e518a4a

--- test/nvmf/target/filesystem.sh
+++ test/nvmf/target/filesystem.sh
@@ -21,7 +21,7 @@ nvmftestinit
 for incapsule in 0 4096; do
 	nvmfappstart "-m 0xF"
 
-	$rpc_py nvmf_create_transport -t rdma -u 8192 -p 4 -c $incapsule
+	$rpc_py nvmf_create_transport -t rdma -u 8192 -c $incapsule
 
 	$rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc1
 	$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
--- test/nvmf/target/nvme_cli.sh
+++ test/nvmf/target/nvme_cli.sh
@@ -25,7 +25,7 @@ timing_enter nvme_cli
 nvmftestinit
 nvmfappstart "-m 0xF"
 
-$rpc_py nvmf_create_transport -t rdma -u 8192 -p 4
+$rpc_py nvmf_create_transport -t rdma -u 8192
 
 $rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc0
 $rpc_py construct_malloc_bdev $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc1
