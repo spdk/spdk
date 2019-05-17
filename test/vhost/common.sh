d7553f7f81764eee37e517d945ee9360a51f0f82
Ben Walker <benjamin.walker@intel.com>
2019-05-17 11:20:35 -0700

    test/vhost: Put temporary data inside repostiroy
    The vhost tests create some temporary files as they run. Previously,
    these were placed outside of the SPDK repository. Instead, put them
    into a directory within the repository so that the final
    git porcelain check catches when we fail to clean up properly.
    
    The git porcelain check does immediately catch several places where
    the tests fail to clean up, so also fix those.
    
    Change-Id: Ic964fc6672b8a4ea088ace1b11c712eb70f7be08
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/vhost/common.sh
+++ test/vhost/common.sh
@@ -4,12 +4,10 @@ set -e
 : ${QEMU_PREFIX="/usr/local/qemu/spdk-3.0.0"}
 
 BASE_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
-
-# Default running dir -> spdk/..
-[[ -z "$TEST_DIR" ]] && TEST_DIR=$BASE_DIR/../../../
-
-TEST_DIR="$(mkdir -p $TEST_DIR && cd $TEST_DIR && echo $PWD)"
 SPDK_BUILD_DIR=$BASE_DIR/../../
+
+TEST_DIR=$SPDK_BUILD_DIR/test/vhost/test_run
+mkdir -p $TEST_DIR
 
 SPDK_VHOST_SCSI_TEST_DIR=$TEST_DIR/vhost
 
@@ -24,7 +22,6 @@ echo "Using SSH key file $SPDK_VHOST_SSH_KEY_FILE"
 VM_BASE_DIR="$TEST_DIR/vms"
 
 
-mkdir -p $TEST_DIR
 
 #
 # Source config describing QEMU and VHOST cores and NUMA
@@ -454,6 +451,8 @@ function vm_kill_all()
 	for vm in $(vm_list_all); do
 		vm_kill $vm
 	done
+
+	rm -rf $VM_BASE_DIR
 }
 
 # Shutdown all VM in $VM_BASE_DIR
@@ -491,6 +490,8 @@ function vm_shutdown_all()
 		((timeo-=1))
 		sleep 1
 	done
+
+	rm -rf $VM_BASE_DIR
 
 	$shell_restore_x
 	error "Timeout waiting for some VMs to shutdown"
