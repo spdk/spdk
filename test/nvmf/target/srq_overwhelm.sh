cb624969f58598506dfd24affb4f70fdc55b8adb
Seth Howell <seth.howell@intel.com>
2019-05-24 15:58:19 -0700

    scripts/fio.py: add argparse for command line parameters
    This script was getting a bit unwieldy. adding argparse will make it
    easier to add more options in the future or to set default values and
    validate arguments going forward.
    
    Change-Id: I1ffdbdf2082287ceb8a88cd3eb6ecf9bbd6c9e11
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/nvmf/target/srq_overwhelm.sh
+++ test/nvmf/target/srq_overwhelm.sh
@@ -39,7 +39,7 @@ done
 # working even at very high queue depths because the rdma qpair doesn't fail.
 # It is normal to see the initiator timeout and reconnect waiting for completions from an overwhelmmed target,
 # but the connection should come up and FIO should complete without errors.
-$rootdir/scripts/fio.py nvmf 1048576 128 read 10 13
+$rootdir/scripts/fio.py -p nvmf -i 1048576 -d 128 -t read -r 10 -n 13
 
 sync
 
