d28391230dfdaa5790598711b3077ad605572c35
Mateusz Kozlowski <mateusz.kozlowski@intel.com>
2019-05-28 09:55:31 +0200

    lib/ftl: Add public get chunk info admin command
    The patch also changes, so the command calculates the needed
    chunk offset based on PPA, instead of relying on the user to
    provide one.
    
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>
    Change-Id: Ic4eec1b86ded4eb71de860015403294ed0c8c266

--- lib/ftl/ftl_core.h
+++ lib/ftl/ftl_core.h
@@ -249,6 +249,9 @@ size_t	ftl_head_md_num_lbks(const struct spdk_ftl_dev *dev);
 int	ftl_restore_md(struct spdk_ftl_dev *dev, ftl_restore_fn cb);
 int	ftl_restore_device(struct ftl_restore *restore, ftl_restore_fn cb);
 int	ftl_band_set_direct_access(struct ftl_band *band, bool access);
+int	ftl_retrieve_chunk_info(struct spdk_ftl_dev *dev, struct ftl_ppa ppa,
+				struct spdk_ocssd_chunk_information_entry *info,
+				unsigned int num_entries);
 
 #define ftl_to_ppa(addr) \
 	(struct ftl_ppa) { .ppa = (uint64_t)(addr) }
