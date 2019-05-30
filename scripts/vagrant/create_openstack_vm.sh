fa1aed36111960a53f65fd7f74b25d22aa4f521a
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-05-30 12:23:19 +0200

    scripts/vagrant: Add possibility to create vm image with devstack
    Change-Id: I880dbe9d29ec4822193e6f570107f7fc12e56e3d
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ scripts/vagrant/create_openstack_vm.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+# create_vhost_vm.sh
+#
+# Creates a virtual machine image used as a dependency for running vhost tests
+
+set -e
+
+testdir=$(readlink -f $(dirname $0))
+SPDK_DIR=$(readlink -f $testdir/../..)
+
+# The command line help
+display_help() {
+	echo
+	echo " Usage:"
+	echo
+	echo "  --http-proxy"
+	echo "  -h help"
+	echo
+}
+
+while getopts ":h-:" opt; do
+	case "${opt}" in
+		-)
+		case "${OPTARG}" in
+			http-proxy=*)
+				http_proxy=$OPTARG
+				https_proxy=$http_proxy
+			;;
+			*)
+				echo "  Invalid argument -$OPTARG" >&2
+				echo "  Try \"$0 -h\"" >&2
+				exit 1
+				;;
+		esac
+		;;
+		h)
+			display_help >&2
+			exit 0
+		;;
+		*)
+			echo "  Invalid argument: -$OPTARG" >&2
+			echo "  Try: \"$0 -h\"" >&2
+			exit 1
+		;;
+	esac
+done
+export SPDK_DIR
+export SPDK_VAGRANT_VMRAM=8192
+export SPDK_VAGRANT_VMCPU=10
+SPDK_VAGRANT_DISTRO="ubuntu18"
+
+mkdir -vp "${testdir}/${SPDK_VAGRANT_DISTRO}"
+cp ${testdir}/Vagrantfile_openstack_vm ${testdir}/${SPDK_VAGRANT_DISTRO}/Vagrantfile
+
+pushd "${testdir}/${SPDK_VAGRANT_DISTRO}"
+if [ ! -z "${http_proxy}" ]; then
+	export http_proxy
+	export https_proxy
+	if vagrant plugin list | grep -q vagrant-proxyconf; then
+		echo "vagrant-proxyconf already installed... skipping"
+	else
+		vagrant plugin install vagrant-proxyconf
+	fi
+fi
+VBoxManage setproperty machinefolder "${testdir}/${SPDK_VAGRANT_DISTRO}"
+vagrant up
+vagrant halt
+VBoxManage setproperty machinefolder default
+
+# Convert Vbox .vmkd image to qcow2
+vmdk_img=$(find ${testdir}/${SPDK_VAGRANT_DISTRO} -name "*.vmdk")
+qemu-img convert -f vmdk -O qcow2 ${vmdk_img} ${testdir}/${SPDK_VAGRANT_DISTRO}/openstack_vm_image.qcow2
+
+echo ""
+echo "  SUCCESS!"
+echo ""
