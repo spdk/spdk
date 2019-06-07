dfb9aaffc215db0cf2b77ca28841c2fe5b5a3cb4
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2019-06-07 03:23:21 -0400

    lib/ftl: fix PAGE_SIZE for AArch64
    Not only 4KB page size is supported on AArch64. The most common is 4KB
    and 64KB. 16KB is supported too. We can get it by "getconf PAGESIZE".
    
    Change-Id: I6f09741372d35e868228528d976fe3b5a65ff96c
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/454447 (master)
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- mk/spdk.common.mk
+++ mk/spdk.common.mk
@@ -84,7 +84,7 @@ COMMON_CFLAGS += -march=native
 endif
 ifeq ($(TARGET_MACHINE),aarch64)
 COMMON_CFLAGS += -march=armv8-a+crc
-COMMON_CFLAGS += -DPAGE_SIZE=4096
+COMMON_CFLAGS += -DPAGE_SIZE=$(shell getconf PAGESIZE)
 endif
 
 ifeq ($(CONFIG_WERROR), y)
