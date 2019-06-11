619def8b1a5874116210f1c7c5829e4f3fe9b223
Changpeng Liu <changpeng.liu@intel.com>
2019-06-11 02:48:21 -0400

    nvmf: update the reservation information for ACQUIRE/RLEASE commands
    Change-Id: Ibfebffa4d683da08ae8f9350cce144fafe6a5538
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- lib/nvmf/subsystem.c
+++ lib/nvmf/subsystem.c
@@ -1959,6 +1959,11 @@ exit:
 
 		}
 	}
+	if (update_sgroup && ns->ptpl_activated) {
+		if (nvmf_ns_update_reservation_info(ns)) {
+			status = SPDK_NVME_SC_INTERNAL_DEVICE_ERROR;
+		}
+	}
 	req->rsp->nvme_cpl.status.sct = SPDK_NVME_SCT_GENERIC;
 	req->rsp->nvme_cpl.status.sc = status;
 	return update_sgroup;
@@ -2059,6 +2064,11 @@ nvmf_ns_reservation_release(struct spdk_nvmf_ns *ns,
 	}
 
 exit:
+	if (update_sgroup && ns->ptpl_activated) {
+		if (nvmf_ns_update_reservation_info(ns)) {
+			status = SPDK_NVME_SC_INTERNAL_DEVICE_ERROR;
+		}
+	}
 	req->rsp->nvme_cpl.status.sct = SPDK_NVME_SCT_GENERIC;
 	req->rsp->nvme_cpl.status.sc = status;
 	return update_sgroup;
