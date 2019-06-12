a0854b163559ae36fb3d4c1bf593a5b1794707ef
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-06-12 16:37:24 +0900

    dif: Change data_len of spdk_dif_set_md_interleave_iovs to be remaining length
    This patch changes the meaning of the data_len parameter of
    spdk_dif_set_md_interleave_iovs from `Expected data length of the payload`
    to `Expected length of the newly read data in the extended LBA payload`.
    
    This change will align the parameters of spdk_dif_set_md_intereleave_iovs
    to of spdk_dif_generate_stream.
    
    Change-Id: I7f9c45e78be977625713acb79d2ae82d4375f419
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/util/dif.c
+++ lib/util/dif.c
@@ -1402,16 +1402,11 @@ spdk_dif_set_md_interleave_iovs(struct iovec *iovs, int iovcnt,
 	}
 
 	data_block_size = ctx->block_size - ctx->md_size;
-	num_blocks = data_len / data_block_size;
+	num_blocks = (data_offset + data_len) / data_block_size;
 
-	if ((data_len % data_block_size) != 0) {
-		SPDK_ERRLOG("Data length must be a multiple of data block size\n");
+	if (((data_offset + data_len) % data_block_size) != 0) {
+		SPDK_ERRLOG("Data offset + length must be a multiple of data block size\n");
 		return -EINVAL;
-	}
-
-	if (data_offset >= data_len) {
-		SPDK_ERRLOG("Data offset must be smaller than data length\n");
-		return -ERANGE;
 	}
 
 	_dif_sgl_init(&dif_sgl, iovs, iovcnt);
