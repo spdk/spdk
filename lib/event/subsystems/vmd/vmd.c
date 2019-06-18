71ab3b4975655ce2fa456c3070563bf9dbbc6175
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-06-18 07:28:04 -0400

    event/subsystems/vmd: Added VMD subsystem
    Added new VMD subsystem to enumerate
    devices behind VMD when event framewrok
    is used.
    
    To enable VMD, user need to provide Enable
    flag via config file.
    
    Change-Id: I89bfe22b127c00d358dac7336ffb44b0c0f426ea
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

---
+++ lib/event/subsystems/vmd/vmd.c
@@ -0,0 +1,71 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+#include "spdk/conf.h"
+
+#include "spdk/vmd.h"
+
+#include "spdk_internal/event.h"
+
+static void
+spdk_vmd_subsystem_init(void)
+{
+	struct spdk_conf_section *sp;
+	int rc = 0;
+
+	sp = spdk_conf_find_section(NULL, "Vmd");
+	if (sp != NULL) {
+		if (spdk_conf_section_get_boolval(sp, "Enable", false)) {
+			rc = spdk_vmd_init();
+		}
+	}
+
+	spdk_subsystem_init_next(rc);
+}
+
+static void
+spdk_vmd_subsystem_fini(void)
+{
+	spdk_subsystem_fini_next();
+}
+
+static struct spdk_subsystem g_spdk_subsystem_vmd = {
+	.name = "vmd",
+	.init = spdk_vmd_subsystem_init,
+	.fini = spdk_vmd_subsystem_fini,
+	.config = NULL,
+	.write_config_json = NULL,
+};
+
+SPDK_SUBSYSTEM_REGISTER(g_spdk_subsystem_vmd);
