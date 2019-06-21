576f0cc51b6f460cb62b927e11dfcbe461d45d08
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-06-21 14:20:22 +0900

    nvmf: Hide DIF setting of the backend bdev if DIF insert/strip is enabled
    When the NVMf controller's flag dif_insert_or_strip is enabled, DIF is
    inserted for write I/O and stripped for read I/O, and the corresponding
    NVMe-oF initiator should not be aware of the DIF setting of the
    backend bdev.
    
    Hence this patch hides the DIF setting of the backend bdev
    when the flag dif_insert_or_strip is enabled.
    
    Change-Id: I3c14880c2e94cba7f76b1bca78afb36bfe884e26
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -1549,7 +1549,7 @@ spdk_nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
 		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
 	}
 
-	spdk_nvmf_bdev_ctrlr_identify_ns(ns, nsdata);
+	spdk_nvmf_bdev_ctrlr_identify_ns(ns, nsdata, ctrlr->dif_insert_or_strip);
 
 	/* Due to bug in the Linux kernel NVMe driver we have to set noiob no larger than mdts */
 	max_num_blocks = ctrlr->admin_qpair->transport->opts.max_io_size /
