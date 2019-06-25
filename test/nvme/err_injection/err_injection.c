f522610c3c71f421f6fe14c4ec10fb62b96414d1
Maciej Szwed <maciej.szwed@intel.com>
2019-06-25 10:58:03 +0200

    test/nvme: Allow NVMe with 0 namespaces in tests
    Some tests return error if no namespace is found,
    but such scenario should be accepted.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Icc799eac1ebc2d4f16017722da3c20d58788fb33

--- test/nvme/err_injection/err_injection.c
+++ test/nvme/err_injection/err_injection.c
@@ -82,10 +82,7 @@ attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 	dev->ctrlr = ctrlr;
 	nsid = spdk_nvme_ctrlr_get_first_active_ns(ctrlr);
 	dev->ns = spdk_nvme_ctrlr_get_ns(ctrlr, nsid);
-	if (dev->ns == NULL) {
-		failed = 1;
-		return;
-	}
+
 	dev->qpair = spdk_nvme_ctrlr_alloc_io_qpair(ctrlr, NULL, 0);
 	if (dev->qpair == NULL) {
 		failed = 1;
