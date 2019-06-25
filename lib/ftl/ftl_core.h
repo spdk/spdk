59911b06bb116f9688e94d9a997d3a15e9a52e27
Konrad Sztyber <konrad.sztyber@intel.com>
2019-06-25 10:43:16 +0200

    lib/ftl: separate non-volatile scrub function
    The cache needs to be scrubbed during the initial device creation as
    well as after power loss recovery. This patch extracts the scrubbing
    code into a separate function.
    
    Change-Id: I2cb32e6993a3531470f29f466d990f0d96e45def
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- lib/ftl/ftl_core.h
+++ lib/ftl/ftl_core.h
@@ -294,6 +294,8 @@ int	ftl_retrieve_chunk_info(struct spdk_ftl_dev *dev, struct ftl_ppa ppa,
 				unsigned int num_entries);
 int	ftl_nv_cache_write_header(struct ftl_nv_cache *nv_cache, spdk_bdev_io_completion_cb cb_fn,
 				  void *cb_arg);
+int	ftl_nv_cache_scrub(struct ftl_nv_cache *nv_cache, spdk_bdev_io_completion_cb cb_fn,
+			   void *cb_arg);
 int	ftl_flush_active_bands(struct spdk_ftl_dev *dev, spdk_ftl_fn cb_fn, void *cb_arg);
 
 #define ftl_to_ppa(addr) \
