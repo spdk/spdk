705348dd0ac773860795bf89e26121dd7c47fd0d
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-06-26 16:45:25 +0900

    nvmf: Add dif_mode to transport options
    This is a place holder and subsequent patches will use the option
    dif_mode and provide JSON RPCs to configure it.
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: I7e3fbb1d49c47647a9a0a1a2149152801591b283

--- include/spdk/nvmf.h
+++ include/spdk/nvmf.h
@@ -63,6 +63,16 @@ struct spdk_nvmf_poll_group;
 struct spdk_json_write_ctx;
 struct spdk_nvmf_transport;
 
+enum spdk_nvmf_dif_mode {
+	/* End-to-end mode (DIF pass-through) */
+	SPDK_NVMF_DIF_MODE_E2E = 0,
+
+	/* Local mode (DIF insert and strip) */
+	SPDK_NVMF_DIF_MODE_LOCAL,
+
+	NUM_SPDK_NVMF_DIF_MODE,
+};
+
 struct spdk_nvmf_transport_opts {
 	uint16_t	max_queue_depth;
 	uint16_t	max_qpairs_per_ctrlr;
@@ -75,6 +85,7 @@ struct spdk_nvmf_transport_opts {
 	uint32_t	max_srq_depth;
 	bool		no_srq;
 	bool		c2h_success;
+	enum spdk_nvmf_dif_mode	dif_mode;
 };
 
 /**
