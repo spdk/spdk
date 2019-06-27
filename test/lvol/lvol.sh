1c858038d6c917be191418fd2ce5635797446f2e
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-06-27 19:29:23 +0200

    test/lvol: rewrite construct_logical_volume_nonexistent_lvs_uuid to bash
    Change-Id: I1223de42f328d16864e49e795efb4b81f0c5ba17
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- test/lvol/lvol.sh
+++ test/lvol/lvol.sh
@@ -21,7 +21,6 @@ function usage() {
     echo "    --block-size          Block size for this bdev"
     echo "-x                        set -x for script debug"
     echo "    --test-cases=         List test cases which will be run:
-                                    100: 'construct_logical_volume_nonexistent_lvs_uuid',
                                     101: 'construct_lvol_bdev_on_full_lvol_store',
                                     102: 'construct_lvol_bdev_name_twice',
                                     150: 'resize_lvol_bdev_positive',
