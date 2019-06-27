6bd4027205f0e63f95b46adc78a3e26beee55df7
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-06-27 19:29:23 +0200

    test/lvol: rewrite construct_logical_volume_nonexistent_lvs_uuid to bash
    Change-Id: I1223de42f328d16864e49e795efb4b81f0c5ba17
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/lvol/test_cases.py
+++ test/lvol/test_cases.py
@@ -111,7 +111,6 @@ def case_message(func):
     def inner(*args, **kwargs):
         test_name = {
             # bdev_lvol_create - negative tests
-            100: 'construct_logical_volume_nonexistent_lvs_uuid',
             101: 'bdev_lvol_create_on_full_lvol_store',
             102: 'bdev_lvol_create_name_twice',
             # resize_lvol_store - positive tests
@@ -306,27 +305,6 @@ class TestCases(object):
     def get_lvs_cluster_size(self, lvs_name="lvs_test"):
         lvs = self.c.bdev_lvol_get_lvstores(lvs_name)[0]
         return int(int(lvs['cluster_size']) / MEGABYTE)
-
-    @case_message
-    def test_case100(self):
-        """
-        construct_logical_volume_nonexistent_lvs_uuid
-
-        Negative test for constructing a new logical_volume.
-        Call bdev_lvol_create with lvs_uuid which does not
-        exist in configuration.
-        """
-        fail_count = 0
-        # Try to call bdev_lvol_create with lvs_uuid which does not exist
-        if self.c.bdev_lvol_create(self._gen_lvs_uuid(),
-                                   self.lbd_name,
-                                   32) == 0:
-            fail_count += 1
-
-        # Expected result:
-        # - return code != 0
-        # - ENODEV response printed to stdout
-        return fail_count
 
     @case_message
     def test_case101(self):
