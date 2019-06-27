1d717c7cd95ddd8fed83bfe59e86ef77e0ef1c24
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-06-27 10:49:26 -0400

    test/vmd: Basic VMD tests
    Added basic tests for NVMe disks behind VMD.
    
    Change-Id: I9613ca4aa813bab16f4085ac809dbdc8028f1cfc
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/459639
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -252,6 +252,10 @@ if [ $SPDK_RUN_FUNCTIONAL_TEST -eq 1 ]; then
 	if [ $SPDK_TEST_BDEV_FTL -eq 1 ]; then
 		run_test suite ./test/ftl/ftl.sh
 	fi
+
+	if [ $SPDK_TEST_VMD -eq 1 ]; then
+		run_test suite ./test/vmd/vmd.sh
+	fi
 fi
 
 timing_enter cleanup
