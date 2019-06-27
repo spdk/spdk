52b912ca49d151d325bcfaad2be00c914e086ae7
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-06-27 20:10:34 +0200

    test/lvol: rewrite construct_lvs_name_twice to bash
    Change-Id: Ia14fc807c7812e3de4035f3753dc021bdc8df463
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- test/lvol/basic.sh
+++ test/lvol/basic.sh
@@ -37,6 +37,23 @@ function test_construct_lvs() {
 	rpc_cmd delete_malloc_bdev "$malloc_name"
 }
 
+# try to create two lvs with conflicting aliases
+function test_construct_lvs_conflict_alias() {
+	# create the first one
+	malloc1_name=$(rpc_cmd construct_malloc_bdev $MALLOC_SIZE_MB $MALLOC_BS)
+	lvs1_uuid=$(rpc_cmd construct_lvol_store "$malloc1_name" lvs_test)
+
+	# second one
+	malloc2_name=$(rpc_cmd construct_malloc_bdev $MALLOC_SIZE_MB $MALLOC_BS)
+	! rpc_cmd construct_lvol_store "$malloc2_name" lvs_test
+
+	# clean up
+	rpc_cmd destroy_lvol_store -u "$lvs1_uuid"
+	! rpc_cmd get_lvol_stores -u "$lvs1_uuid"
+	rpc_cmd delete_malloc_bdev "$malloc1_name"
+	rpc_cmd delete_malloc_bdev "$malloc2_name"
+}
+
 # create lvs + lvol on top, verify lvol's parameters
 function test_construct_lvol() {
 	# create an lvol store
@@ -252,6 +269,7 @@ trap "killprocess $spdk_pid; exit 1" SIGINT SIGTERM EXIT
 waitforlisten $spdk_pid
 
 run_test test_construct_lvs
+run_test test_construct_lvs_conflict_alias
 run_test test_construct_lvol
 run_test test_construct_multi_lvols
 run_test test_construct_lvols_conflict_alias
