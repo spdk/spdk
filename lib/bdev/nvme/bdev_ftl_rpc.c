60c8845fd07e65b4b129389e7aef2ed221481bd5
Mateusz Kozlowski <mateusz.kozlowski@intel.com>
2019-06-28 10:16:46 +0200

    bdev/ftl: construct_ftl_bdev respects default ftl config
    Changed initialization of the ftl lib when using an rpc call to
    allow for usage of any default configuration parameters (currently
    only allow_open_bands is exposed).
    
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>
    Change-Id: I73457dfcacc6b1adeffd13ecc6e98001749e00cf
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/459741
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- lib/bdev/nvme/bdev_ftl_rpc.c
+++ lib/bdev/nvme/bdev_ftl_rpc.c
@@ -46,7 +46,7 @@ struct rpc_construct_ftl {
 	char *punits;
 	char *uuid;
 	char *cache_bdev;
-	bool allow_open_bands;
+	struct spdk_ftl_conf ftl_conf;
 };
 
 static void
@@ -68,8 +68,8 @@ static const struct spdk_json_object_decoder rpc_construct_ftl_decoders[] = {
 	{"uuid", offsetof(struct rpc_construct_ftl, uuid), spdk_json_decode_string, true},
 	{"cache", offsetof(struct rpc_construct_ftl, cache_bdev), spdk_json_decode_string, true},
 	{
-		"allow_open_bands", offsetof(struct rpc_construct_ftl, allow_open_bands),
-		spdk_json_decode_bool, true
+		"allow_open_bands", offsetof(struct rpc_construct_ftl, ftl_conf) +
+		offsetof(struct spdk_ftl_conf, allow_open_bands), spdk_json_decode_bool, true
 	},
 };
 
@@ -111,6 +111,8 @@ spdk_rpc_construct_ftl_bdev(struct spdk_jsonrpc_request *request,
 	char range[FTL_RANGE_MAX_LENGTH];
 	int rc;
 
+	spdk_ftl_conf_init_defaults(&req.ftl_conf);
+
 	if (spdk_json_decode_object(params, rpc_construct_ftl_decoders,
 				    SPDK_COUNTOF(rpc_construct_ftl_decoders),
 				    &req)) {
@@ -128,7 +130,7 @@ spdk_rpc_construct_ftl_bdev(struct spdk_jsonrpc_request *request,
 	opts.name = req.name;
 	opts.mode = SPDK_FTL_MODE_CREATE;
 	opts.cache_bdev = req.cache_bdev;
-	opts.allow_open_bands = req.allow_open_bands;
+	opts.ftl_conf = req.ftl_conf;
 
 	/* Parse trtype */
 	rc = spdk_nvme_transport_id_parse_trtype(&opts.trid.trtype, req.trtype);
