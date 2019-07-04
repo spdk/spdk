6c40bbc95ef70bff32bece048c4fc576cf11c627
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-07-04 09:28:13 -0400

    lib/zdev: Added spdk_bdev_get_zone_info() command
    spdk_bdev_get_zone_info() is used for retriving
    information about zones inside zoned namespace.
    
    Change-Id: I8f931505245e984c0b1ee35ed6592c978ee47544
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- lib/bdev/bdev_internal.h
+++ lib/bdev/bdev_internal.h
@@ -31,37 +31,19 @@
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "spdk/stdinc.h"
+#ifndef SPDK_BDEV_INTERNAL_H
+#define SPDK_BDEV_INTERNAL_H
 
-#include "spdk/zdev.h"
+#include "spdk/bdev.h"
 
-#include "bdev_internal.h"
+struct spdk_bdev;
+struct spdk_bdev_io;
+struct spdk_bdev_channel;
 
-const struct spdk_zdev_info *
-spdk_zdev_get_info(const struct spdk_zdev *zdev)
-{
-	return &zdev->info;
-}
+struct spdk_bdev_io *spdk_bdev_get_io(struct spdk_bdev_channel *channel);
+void		     spdk_bdev_io_init(struct spdk_bdev_io *bdev_io,
+				       struct spdk_bdev *bdev, void *cb_arg,
+				       spdk_bdev_io_completion_cb cb);
+void		     spdk_bdev_io_submit(struct spdk_bdev_io *bdev_io);
 
-int
-spdk_zdev_get_zone_info(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
-			uint64_t start_lba, size_t num_zones, struct spdk_zdev_zone_info *info,
-			spdk_bdev_io_completion_cb cb, void *cb_arg)
-{
-	struct spdk_bdev *bdev = spdk_bdev_desc_get_bdev(desc);
-	struct spdk_bdev_io *bdev_io;
-	struct spdk_bdev_channel *channel = spdk_io_channel_get_ctx(ch);
-
-	bdev_io = spdk_bdev_get_io(channel);
-	if (!bdev_io) {
-		return -ENOMEM;
-	}
-
-	bdev_io->internal.ch = channel;
-	bdev_io->internal.desc = desc;
-	bdev_io->type = SPDK_BDEV_IO_TYPE_ZONE_INFO;
-	spdk_bdev_io_init(bdev_io, bdev, cb_arg, cb);
-
-	spdk_bdev_io_submit(bdev_io);
-	return 0;
-}
+#endif /* SPDK_BDEV_INTERNAL_H */
