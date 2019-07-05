aa35048d1550778396536956c0998bde635d946e
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-07-05 06:17:55 -0400

    lib/bdev: Zoned bdev public header
    Added new public header for zoned bdev. Zoned bdev is an
    extension of the bdev interface. Generic concept comes
    from ATA/SCSI and is also being worked as an NVMe TP.
    
    Zoned device logical blocks space is divided into fixed-sized
    zones. Each zone is described by its start logical block address
    and capacity. Writes to a single zone need to be sequential.
    After zone is fully written it need to be reset to write to it
    again. Such writing schema could be very beneficial in terms of
    write amplification factor for NAND based devices.
    SPDK Flash Translation Layer library will be consuming this
    interface in the future.
    
    Extending SPDK bdev interface will allow to use existing bdev
    infrastructure for this new type of devices.
    
    Zoned device have several properties defined in spdk_bdev
    structure:
     - zone_size: default size of each zone
     - max_open_zone: maximum number of open zones
     - optimal_open_zones: optimal number of open zones to get
    		       best performance on writes
    
    Single zone properties are defined in spdk_bdev_zone_info
    structure:
     - start_lba: first logical block of z zone
     - write_pointer: logical block address in the zone at
    		  which next write shall occur.
     - capacity: maximum number of logical blocks that may
    	     be written in the zone when zone is empty.
     - state: zone state
    
    Several zone states are defined: Empty, Open, Full, Closed,
    Read Only and Offline.
    
    To change zone state zone actions are defined: Close, Finish,
    Open and Reset.
    
    Change-Id: I5fcc22d548c15743329344cae96f5ff73e268504
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/460642
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Maciej Szwed <maciej.szwed@intel.com>

--- include/spdk/bdev_module.h
+++ include/spdk/bdev_module.h
@@ -340,6 +340,21 @@ struct spdk_bdev {
 	bool zoned;
 
 	/**
+	 * Default size of each zone (in blocks).
+	 */
+	uint64_t zone_size;
+
+	/**
+	 * Maximum number of open zones.
+	 */
+	uint32_t max_open_zones;
+
+	/**
+	 * Optimal number of open zones.
+	 */
+	uint32_t optimal_open_zones;
+
+	/**
 	 * Pointer to the bdev module that registered this bdev.
 	 */
 	struct spdk_bdev_module *module;
