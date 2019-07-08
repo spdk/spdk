cf0c4829de186aa06cce3b516165a9e78ea1156b
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-07-08 20:39:55 +0000

    spdkcli: test pmem_pool_info comand
    Add missing test for `pmem_pool_info` command.
    
    Test steps were rearranged slightly because
      `pmem_pool_info` command needs to be invoked
      prior to `bdevs/pmemblk create`.
    
    Change-Id: I4f4d76791e3151fec0337db95b1a2848dc8face1
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/461225
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- test/spdkcli/pmem.sh
+++ test/spdkcli/pmem.sh
@@ -9,7 +9,7 @@ MATCH_FILE="spdkcli_pmem.test"
 SPDKCLI_BRANCH="/bdevs/pmemblk"
 
 timing_enter spdkcli_pmem
-trap 'on_error_exit;' ERR
+trap 'rm -f $testdir/match_files/spdkcli_pmem_info.test; on_error_exit;' ERR
 
 timing_enter run_spdk_tgt
 run_spdk_tgt
@@ -17,14 +17,22 @@ timing_exit run_spdk_tgt
 
 timing_enter spdkcli_create_pmem_config
 $spdkcli_job "'/bdevs/pmemblk create_pmem_pool /tmp/sample_pmem0 32 512' '' True
-'/bdevs/pmemblk create /tmp/sample_pmem0 pmem_bdev0' 'pmem_bdev0' True
 '/bdevs/pmemblk create_pmem_pool /tmp/sample_pmem1 32 512' '' True
+"
+
+# Saving pmem pool info before they get claimed by /bdevs/pmemblk create
+$rootdir/scripts/spdkcli.py /bdevs/pmemblk pmem_pool_info /tmp/sample_pmem0 >> $testdir/match_files/spdkcli_pmem_info.test
+$rootdir/scripts/spdkcli.py /bdevs/pmemblk pmem_pool_info /tmp/sample_pmem1 >> $testdir/match_files/spdkcli_pmem_info.test
+
+$spdkcli_job "'/bdevs/pmemblk create /tmp/sample_pmem0 pmem_bdev0' 'pmem_bdev0' True
 '/bdevs/pmemblk create /tmp/sample_pmem1 pmem_bdev1' 'pmem_bdev1' True
 "
+
 timing_exit spdkcli_create_pmem_config
 
 timing_enter spdkcli_check_match
 check_match
+$rootdir/test/app/match/match -v $testdir/match_files/spdkcli_pmem_info.test.match
 timing_exit spdkcli_check_match
 
 timing_enter spdkcli_clear_pmem_config
@@ -34,6 +42,7 @@ $spdkcli_job "'/bdevs/pmemblk delete pmem_bdev0' 'pmem_bdev0'
 '/bdevs/pmemblk delete_pmem_pool /tmp/sample_pmem1' ''
 "
 rm -f /tmp/sample_pmem
+rm -f $testdir/match_files/spdkcli_pmem_info.test
 timing_exit spdkcli_clear_pmem_config
 
 killprocess $spdk_tgt_pid
