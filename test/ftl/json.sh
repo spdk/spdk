12e612501edfb887282a1e75046632b18bb38b5d
Konrad Sztyber <konrad.sztyber@intel.com>
2019-07-15 12:58:56 +0200

    test/ftl: reduce the number of required parallel units
    One of the tests required the OC device to have at least 8 parallel
    units.  It's not really necessary as we can test the same thing on
    fewer (4) units.
    
    Change-Id: Ib3ccd7fee0b8ecd2663b12e4aa9aa802ebe238a8
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- test/ftl/json.sh
+++ test/ftl/json.sh
@@ -20,18 +20,18 @@ $rootdir/test/app/bdev_svc/bdev_svc & svcpid=$!
 waitforlisten $svcpid
 
 # Create new bdev from json configuration
-$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme0 -l 0-3 | $rpc_py load_subsystem_config
+$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme0 -l 0-1 | $rpc_py load_subsystem_config
 
 uuid=$($rpc_py get_bdevs | jq -r '.[0].uuid')
 
 $rpc_py delete_ftl_bdev -b nvme0
 
 # Restore bdev from json configuration
-$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme0 -l 0-3 -u $uuid | $rpc_py load_subsystem_config
+$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme0 -l 0-1 -u $uuid | $rpc_py load_subsystem_config
 # Create new bdev from json configuration
-$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme1 -l 4-5 | $rpc_py load_subsystem_config
+$rootdir/scripts/gen_ftl.sh -j -a $device -n nvme1 -l 2-2 | $rpc_py load_subsystem_config
 # Create new bdev from RPC
-$rpc_py construct_ftl_bdev -b nvme2 -a $device -l 7-7
+$rpc_py construct_ftl_bdev -b nvme2 -a $device -l 3-3
 
 $rpc_py delete_ftl_bdev -b nvme2
 $rpc_py delete_ftl_bdev -b nvme0
