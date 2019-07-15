f613c9c0a5244e66bc37f19e64ccbfe7eaae03c5
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-07-15 12:47:39 +0200

    test/lvol: rewrite nested_construct_lvol_bdev_on_full_lvol_store to bash
    Change-Id: I6a33d54260f2181995b962db21efc9556a5a4c0c
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- test/lvol/lvol.sh
+++ test/lvol/lvol.sh
@@ -34,7 +34,6 @@ function usage() {
                                     301: 'delete_lvol_store_underlying_bdev',
                                     350: 'nested_destroy_logical_volume_negative',
                                     400: 'nested_construct_logical_volume_positive',
-                                    500: 'nested_bdev_lvol_create_on_full_lvol_store',
                                     550: 'delete_bdev_positive',
                                     551: 'delete_lvol_bdev',
                                     552: 'bdev_lvol_delete_lvstore_with_clones',
