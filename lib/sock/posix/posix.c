80dd8d0b232525e67fcbc40eea9841c5854921d6
Ziye Yang <ziye.yang@intel.com>
2019-07-15 20:04:45 +0800

    sock: Add the socket priority setting function.
    Purpose: This API can be used to set the socket
    with different priority.
    
    Signed-off-by: Ziye Yang <ziye.yang@intel.com>
    Change-Id: I9df1122bf6ae640eba731e635a1784f4e9da4104

--- lib/sock/posix/posix.c
+++ lib/sock/posix/posix.c
@@ -427,6 +427,22 @@ spdk_posix_sock_set_sendbuf(struct spdk_sock *_sock, int sz)
 			  &sz, sizeof(sz));
 }
 
+static int
+spdk_posix_sock_set_priority(struct spdk_sock *_sock, int priority)
+{
+	int rc = -1;
+
+#if defined(SO_PRIORITY)
+	struct spdk_posix_sock *sock = __posix_sock(_sock);
+
+	assert(sock != NULL);
+
+	rc = setsockopt(sock->fd, SOL_SOCKET, SO_PRIORITY,
+			&priority, sizeof(priority));
+#endif
+	return rc;
+}
+
 static bool
 spdk_posix_sock_is_ipv6(struct spdk_sock *_sock)
 {
@@ -620,6 +636,7 @@ static struct spdk_net_impl g_posix_net_impl = {
 	.set_recvlowat	= spdk_posix_sock_set_recvlowat,
 	.set_recvbuf	= spdk_posix_sock_set_recvbuf,
 	.set_sendbuf	= spdk_posix_sock_set_sendbuf,
+	.set_priority	= spdk_posix_sock_set_priority,
 	.is_ipv6	= spdk_posix_sock_is_ipv6,
 	.is_ipv4	= spdk_posix_sock_is_ipv4,
 	.get_placement_id	= spdk_posix_sock_get_placement_id,
