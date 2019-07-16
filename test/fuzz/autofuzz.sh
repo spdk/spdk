f52dcc0099c3afcb7eab3ba1375785e36413e765
Seth Howell <seth.howell@intel.com>
2019-07-16 00:34:35 -0700

    test: add autofuzz.sh script family
    These scripts are intended to be run as part of a standalone automated
    jenkins fuzz testing job. hey are intentionally set to run for
    approximately 20 minutes at a time.
    
    Change-Id: I861e01abf8b86a7531111119655f487f8d982200
    Signed-off-by: Seth Howell <seth.howell@intel.com>

---
+++ test/fuzz/autofuzz.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+
+source "$rootdir/test/common/autotest_common.sh"
+
+# The term transport is used a bit loosely for vhost tests.
+allowed_nvme_transports=("rdma" "tcp")
+allowed_vhost_transports=("scsi" "blk" "all")
+bad_transport=true
+config_params="--enable-asan --enable-ubsan --enable-debug --without-isal"
+
+# This argument is used in addition to the test arguments in autotest_common.sh
+for i in "$@"; do
+	case "$i" in
+		--module=*)
+			TEST_MODULE="${i#*=}"
+			;;
+	esac
+done
+
+timing_enter autofuzz
+if [ $TEST_MODULE == "nvmf" ]; then
+	allowed_transports=( "${allowed_nvme_transports[@]}" )
+	if [ $TEST_TRANSPORT == "rdma" ]; then
+		config_params="$config_params --with-rdma"
+	fi
+elif [ $TEST_MODULE == "vhost" ]; then
+	allowed_transports=( "${allowed_vhost_transports[@]}" )
+	config_params="$config_params --with-virtio"
+else
+	echo "Invalid module specified. Please specify either nvmf or vhost."
+	exit 1
+fi
+
+for transport in ${allowed_transports[@]}; do
+	if [ $transport == $TEST_TRANSPORT ]; then
+		bad_transport=false
+	fi
+done
+
+if $bad_transport; then
+	echo "invalid transport. Please supply one of the following for module: $TEST_MODULE."
+	echo "${allowed_transports[@]}"
+	exit 1
+fi
+
+timing_enter make
+cd $rootdir
+./configure $config_params
+$MAKE $MAKEFLAGS
+timing_exit make
+
+# supply --iso to each test module so that it can run setup.sh.
+timing_enter fuzz_module
+if [ $TEST_MODULE == "nvmf" ]; then
+	sudo $testdir/autofuzz_nvmf.sh --iso --transport=$TEST_TRANSPORT
+fi
+
+if [ $TEST_MODULE == "vhost" ]; then
+	sudo $testdir/autofuzz_vhost.sh --iso --transport=$TEST_TRANSPORT
+fi
+timing_exit fuzz_module
+timing_exit autofuzz
---
+++ test/fuzz/autofuzz_vhost.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -e
+
+rootdir=$(readlink -f $(dirname $0))/../..
+source $rootdir/test/common/autotest_common.sh
+source "$rootdir/scripts/common.sh"
+
+VHOST_APP="$rootdir/app/vhost/vhost -p 0"
+FUZZ_RPC_SOCK="/var/tmp/spdk_fuzz.sock"
+FUZZ_APP="$rootdir/test/app/fuzz/vhost_fuzz/vhost_fuzz -r $FUZZ_RPC_SOCK --wait-for-rpc"
+
+vhost_rpc_py="$rootdir/scripts/rpc.py"
+fuzz_generic_rpc_py="$rootdir/scripts/rpc.py -s $FUZZ_RPC_SOCK"
+fuzz_specific_rpc_py="$rootdir/test/app/fuzz/common/fuzz_rpc.py -s $FUZZ_RPC_SOCK"
+
+timing_enter vhost_fuzz_test
+
+#todo refactor this to use the vhosttestinit function when it becomes available.
+timing_enter setup
+$rootdir/scripts/setup.sh
+timing_exit setup
+
+$VHOST_APP >$output_dir/vhost_fuzz_tgt_output.txt 2>&1 &
+vhostpid=$!
+waitforlisten $vhostpid
+
+trap "killprocess $vhostpid; exit 1" SIGINT SIGTERM exit
+
+$FUZZ_APP -t 1200 2>$output_dir/vhost_autofuzz_output1.txt &
+fuzzpid=$!
+waitforlisten $fuzzpid $FUZZ_RPC_SOCK
+
+trap "killprocess $vhostpid; killprocess $fuzzpid; exit 1" SIGINT SIGTERM exit
+
+if [ $TEST_TRANSPORT == "bdev" ] || [ $TEST_TRANSPORT == "all" ]; then
+    $vhost_rpc_py construct_malloc_bdev -b Malloc0 64 512
+    $vhost_rpc_py construct_vhost_blk_controller Vhost.1 Malloc0
+    sleep 1
+
+    # test the vhost blk controller with valid data buffers.
+    $fuzz_specific_rpc_py fuzz_vhost_create_dev -s `pwd`/Vhost.1 -b -v
+fi
+
+if [ $TEST_TRANSPORT == "scsi" ] || [ $TEST_TRANSPORT == "all" ]; then
+    $vhost_rpc_py construct_malloc_bdev -b Malloc1 64 512
+    $vhost_rpc_py construct_vhost_scsi_controller naa.VhostScsi0.1
+    $vhost_rpc_py add_vhost_scsi_lun naa.VhostScsi0.1 0 Malloc1
+
+    $vhost_rpc_py construct_malloc_bdev -b Malloc2 64 512
+    $vhost_rpc_py construct_vhost_scsi_controller naa.VhostScsi0.2
+    $vhost_rpc_py add_vhost_scsi_lun naa.VhostScsi0.2 0 Malloc2
+    sleep 1
+
+    # test the vhost scsi I/O queue with valid data buffers on a valid lun.
+    $fuzz_specific_rpc_py fuzz_vhost_create_dev -s `pwd`/naa.VhostScsi0.1 -l -v
+    # test the vhost scsi management queue with valid data buffers.
+    $fuzz_specific_rpc_py fuzz_vhost_create_dev -s `pwd`/naa.VhostScsi0.2 -v -m
+fi
+
+# The test won't actually begin until this option is passed in.
+$fuzz_generic_rpc_py start_subsystem_init
+
+wait $fuzzpid
+
+trap - SIGINT SIGTERM exit
+
+killprocess $vhostpid
+killprocess $fuzzpid
+timing_exit vhost_fuzz_test
