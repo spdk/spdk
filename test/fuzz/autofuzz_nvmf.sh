80e546ccaf94ef186e60739023f2ed4a162ac48e
Seth Howell <seth.howell@intel.com>
2019-07-16 00:34:35 -0700

    test: add autofuzz.sh script family
    These scripts are intended to be run as part of a standalone automated
    jenkins fuzz testing job. hey are intentionally set to run for
    approximately 20 minutes at a time.
    
    Change-Id: I861e01abf8b86a7531111119655f487f8d982200
    Signed-off-by: Seth Howell <seth.howell@intel.com>

---
+++ test/fuzz/autofuzz_nvmf.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+source $rootdir/test/common/autotest_common.sh
+source $rootdir/test/nvmf/common.sh
+
+rpc_py="$rootdir/scripts/rpc.py"
+
+nvmftestinit
+
+timing_enter nvmf_fuzz_test
+
+echo "[Nvme]" > $testdir/nvmf_fuzz.conf
+echo "  TransportID \"trtype:$TEST_TRANSPORT adrfam:IPv4 subnqn:nqn.2016-06.io.spdk:cnode1 traddr:$NVMF_FIRST_TARGET_IP trsvcid:$NVMF_PORT\" Nvme0" >> $testdir/nvmf_fuzz.conf
+
+$NVMF_APP -m 0xF &>$output_dir/nvmf_autofuzz_tgt_output.txt &
+nvmfpid=$!
+
+trap "process_shm --id $NVMF_APP_SHM_ID; rm -f $testdir/nvmf_fuzz.conf; killprocess $nvmfpid; nvmftestfini $1; exit 1" SIGINT SIGTERM EXIT
+
+waitforlisten $nvmfpid
+$rpc_py nvmf_create_transport -t $TEST_TRANSPORT -u 8192
+
+$rpc_py construct_malloc_bdev -b Malloc0 64 512
+
+$rpc_py nvmf_subsystem_create nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
+$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Malloc0
+$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t $TEST_TRANSPORT -a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT
+
+# Note that we chose a consistent seed to ensure that this test is consistent in nightly builds.
+$rootdir/test/app/fuzz/nvme_fuzz/nvme_fuzz -m 0xF0 -r "/var/tmp/nvme_fuzz" -t 12 -C $testdir/nvmf_fuzz.conf -N -a 2>$output_dir/nvmf_autofuzz_logs.txt
+
+rm -f $testdir/nvmf_fuzz.conf
+$rpc_py delete_nvmf_subsystem nqn.2016-06.io.spdk:cnode1
+
+trap - SIGINT SIGTERM EXIT
+
+nvmfcleanup
+killprocess $nvmfpid
+
+nvmftestfini
+timing_exit nvmf_fuzz_test
