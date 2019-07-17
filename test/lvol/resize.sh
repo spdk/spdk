2c12b1b6a23df29a38508de30917924003468f4e
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-07-17 07:51:59 +0200

    test/lvol: rewrite negative resize tests to bash
    Change-Id: I8f8daa5e1afd747712f8d562966486b7618ba047
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/lvol/resize.sh
+++ test/lvol/resize.sh
@@ -53,12 +53,45 @@ function test_resize_lvol() {
 	rpc_cmd bdev_malloc_delete "$malloc_name"
 }
 
+# negative test for resizing a logical volume
+# call bdev_lvol_resize with logical volume which does not exist in configuration
+# call bdev_lvol_resize with size argument bigger than size of base bdev
+function test_resize_lvol_negative() {
+	# create an lvol store
+	malloc_name=$(rpc_cmd bdev_malloc_create $MALLOC_SIZE_MB $MALLOC_BS)
+	lvs_uuid=$(rpc_cmd bdev_lvol_create_lvstore "$malloc_name" lvs_test)
+
+	# create an lvol on top
+	lvol_uuid=$(rpc_cmd bdev_lvol_create -u "$lvs_uuid" lvol_test "$LVS_DEFAULT_CAPACITY_MB")
+
+	# try to resize another, inexistent lvol
+	dummy_uuid="00000000-0000-0000-0000-000000000000"
+	rpc_cmd bdev_lvol_resize "$dummy_uuid" 0 && false
+	# just make sure the size of the real lvol did not change
+	lvol=$(rpc_cmd bdev_get_bdevs -b "$lvol_uuid")
+	[ "$(jq -r '.[0].num_blocks' <<< "$lvol")" = "$(( LVS_DEFAULT_CAPACITY / MALLOC_BS ))" ]
+
+	# try to resize an lvol to a size bigger than lvs
+	rpc_cmd bdev_lvol_resize "$lvol_uuid" "$MALLOC_SIZE_MB" && false
+	# just make sure the size of the real lvol did not change
+	lvol=$(rpc_cmd bdev_get_bdevs -b "$lvol_uuid")
+	[ "$(jq -r '.[0].num_blocks' <<< "$lvol")" = "$(( LVS_DEFAULT_CAPACITY / MALLOC_BS ))" ]
+
+	# clean up
+	rpc_cmd bdev_lvol_delete "$lvol_uuid"
+	rpc_cmd bdev_get_bdevs -b "$lvol_uuid" && false
+	rpc_cmd bdev_lvol_delete_lvstore -u "$lvs_uuid"
+	rpc_cmd bdev_lvol_get_lvstores -u "$lvs_uuid" && false
+	rpc_cmd bdev_malloc_delete "$malloc_name"
+}
+
 $rootdir/app/spdk_tgt/spdk_tgt &
 spdk_pid=$!
 trap 'killprocess "$spdk_pid"; exit 1' SIGINT SIGTERM EXIT
 waitforlisten $spdk_pid
 
 run_test "test_resize_lvol" test_resize_lvol
+run_test "test_resize_lvol_negative" test_resize_lvol_negative
 
 trap - SIGINT SIGTERM EXIT
 killprocess $spdk_pid
