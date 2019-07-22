dd5097e7c53cb92623ebca40e42eb2ee99134319
Seth Howell <seth.howell@intel.com>
2019-07-21 21:17:03 -0700

    test/fuzz: Make test timeout configurable.
    We should be able to change the runtime of the application based
    on command line arguments without having to update the bash scripts.
    
    Change-Id: I9fbd8e0386d7e3c39da853a89bf4aff08cb916f0
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/fuzz/autofuzz.sh
+++ test/fuzz/autofuzz.sh
@@ -5,17 +5,22 @@ rootdir=$(readlink -f $testdir/../..)
 
 source "$rootdir/test/common/autotest_common.sh"
 
+TEST_TIMEOUT=1200
+
 # The term transport is used a bit loosely for vhost tests.
 allowed_nvme_transports=("rdma" "tcp")
 allowed_vhost_transports=("scsi" "blk" "all")
 bad_transport=true
 config_params="--enable-asan --enable-ubsan --enable-debug --without-isal"
 
-# This argument is used in addition to the test arguments in autotest_common.sh
+# These arguments are used in addition to the test arguments in autotest_common.sh
 for i in "$@"; do
 	case "$i" in
 		--module=*)
 			TEST_MODULE="${i#*=}"
+			;;
+		--timeout=*)
+			TEST_TIMEOUT="${i#*=}"
 			;;
 	esac
 done
@@ -55,11 +60,11 @@ timing_exit make
 # supply --iso to each test module so that it can run setup.sh.
 timing_enter fuzz_module
 if [ "$TEST_MODULE" == "nvmf" ]; then
-	sudo $testdir/autofuzz_nvmf.sh --iso --transport=$TEST_TRANSPORT
+	sudo $testdir/autofuzz_nvmf.sh --iso --transport=$TEST_TRANSPORT --timeout=$TEST_TIMEOUT
 fi
 
 if [ "$TEST_MODULE" == "vhost" ]; then
-	sudo $testdir/autofuzz_vhost.sh --iso --transport=$TEST_TRANSPORT
+	sudo $testdir/autofuzz_vhost.sh --iso --transport=$TEST_TRANSPORT --timeout=$TEST_TIMEOUT
 fi
 timing_exit fuzz_module
 timing_exit autofuzz
