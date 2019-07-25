8c451e65999d647d700ae207990fffe5ac6b3aa2
yidong0635 <dongx.yi@intel.com>
2019-07-25 14:42:48 -0400

    test/iscsi_tgt: Add missing transport type assignment for nvmf startup.
    This iscsi target testing combines with nvmf target. Here fio_remote_nvme.sh starts
    with nvmf startup. "nvmftestinit" need to assign transport type, otherwise code will
    exit with error reporting. Meanwhile change "-t rdma" to $NVMF_TRANSPORT_OPTS which
    is the same as testing in NVMF.
    
    Meanwhile add tests with iscsi types in autotest.
    ./iscsi_tgt.sh posix --transport=$SPDK_TEST_NVMF_TRANSPORT
    And
    ./iscsi_tgt.sh vpp --transport=$SPDK_TEST_NVMF_TRANSPORT
    
    Change-Id: Ia36b6c6014588ec921a75ba4ba8d252706b0228f
    Signed-off-by: yidong0635 <dongx.yi@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -188,7 +188,10 @@ if [ $SPDK_RUN_FUNCTIONAL_TEST -eq 1 ]; then
 	timing_exit lib
 
 	if [ $SPDK_TEST_ISCSI -eq 1 ]; then
-		run_test suite ./test/iscsi_tgt/iscsi_tgt.sh posix
+		run_test suite ./test/iscsi_tgt/iscsi_tgt.sh posix --transport=$SPDK_TEST_NVMF_TRANSPORT
+		if [ $SPDK_TEST_VPP -eq 1 ]; then
+			run_test suite ./test/iscsi_tgt/iscsi_tgt.sh vpp --transport=$SPDK_TEST_NVMF_TRANSPORT
+		fi
 		run_test suite ./test/spdkcli/iscsi.sh
 
 		# Run raid spdkcli test under iSCSI since blockdev tests run on systems that can't run spdkcli yet
