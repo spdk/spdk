3a82f30c5cd3ff3a9692d2bcb12e3b5ca53a59fc
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-07-31 09:04:58 +0900

    iscsi: Set CID to connection at start and avoid passing it by local variable
    CID is only used for logging. Hence we can set it to the connection
    up front.
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: I55d50b4d225b0a597e92901970db43c917d63b32

--- test/unit/lib/iscsi/iscsi.c/iscsi_ut.c
+++ test/unit/lib/iscsi/iscsi.c/iscsi_ut.c
@@ -178,15 +178,14 @@ op_login_session_normal_test(void)
 
 	/* expect failure: NULL params for target name */
 	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   NULL, 0);
+					   NULL);
 	CU_ASSERT(rc != 0);
 	CU_ASSERT(rsph->status_class == ISCSI_CLASS_INITIATOR_ERROR);
 	CU_ASSERT(rsph->status_detail == ISCSI_LOGIN_MISSING_PARMS);
 
 	/* expect failure: incorrect key for target name */
 	param.next = NULL;
-	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   &param, 0);
+	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1, &param);
 	CU_ASSERT(rc != 0);
 	CU_ASSERT(rsph->status_class == ISCSI_CLASS_INITIATOR_ERROR);
 	CU_ASSERT(rsph->status_detail == ISCSI_LOGIN_MISSING_PARMS);
@@ -194,8 +193,7 @@ op_login_session_normal_test(void)
 	/* expect failure: NULL target name */
 	param.key = "TargetName";
 	param.val = NULL;
-	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   &param, 0);
+	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1, &param);
 	CU_ASSERT(rc != 0);
 	CU_ASSERT(rsph->status_class == ISCSI_CLASS_INITIATOR_ERROR);
 	CU_ASSERT(rsph->status_detail == ISCSI_LOGIN_MISSING_PARMS);
@@ -206,8 +204,7 @@ op_login_session_normal_test(void)
 	snprintf(conn.initiator_name, sizeof(conn.initiator_name),
 		 "%s", UT_INITIATOR_NAME1);
 	rsph->tsih = 1; /* to append the session */
-	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   &param, 0);
+	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1, &param);
 	CU_ASSERT(conn.target_port == NULL);
 	CU_ASSERT(rc != 0);
 	CU_ASSERT(rsph->status_class == ISCSI_CLASS_INITIATOR_ERROR);
@@ -220,8 +217,7 @@ op_login_session_normal_test(void)
 	sess.tsih = UT_ISCSI_TSIH;
 	rsph->tsih = UT_ISCSI_TSIH >> 8; /* to append the session */
 	sess.tag = 1;
-	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   &param, 0);
+	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1, &param);
 	CU_ASSERT(conn.target_port == NULL);
 	CU_ASSERT(rc != 0);
 	CU_ASSERT(rsph->status_class == ISCSI_CLASS_INITIATOR_ERROR);
@@ -230,15 +226,13 @@ op_login_session_normal_test(void)
 	/* expect suceess: drop the session */
 	rsph->tsih = 0; /* to create the session */
 	g_spdk_iscsi.AllowDuplicateIsid = false;
-	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   &param, 0);
+	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1, &param);
 	CU_ASSERT(rc == 0);
 
 	/* expect suceess: create the session */
 	rsph->tsih = 0; /* to create the session */
 	g_spdk_iscsi.AllowDuplicateIsid = true;
-	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1,
-					   &param, 0);
+	rc = iscsi_op_login_session_normal(&conn, &rsp_pdu, UT_INITIATOR_NAME1, &param);
 	CU_ASSERT(rc == 0);
 
 	free(g_spdk_iscsi.session);
