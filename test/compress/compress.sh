e5f99279aa3ad7a9d8b08f435724ac010240af54
paul luse <paul.e.luse@intel.com>
2019-07-31 18:12:30 -0400

    test/compress: enable QAT testing in CI
    Bah, don't look at this  Thought of a much faster simpler
    way to do it... tomorrow
    
    Change-Id: If4bde8ed4b25363da668ab6084c67af4e95ec66d
    Signed-off-by: paul luse <paul.e.luse@intel.com>

--- test/compress/compress.sh
+++ test/compress/compress.sh
@@ -15,6 +15,37 @@ function compress_err_cleanup() {
 	$rootdir/examples/nvme/perf/perf -q 1 -o 131072 -w write -t 2
 }
 
+function run_bdevio() {
+	$rootdir/test/bdev/bdevio/bdevio -w &
+	bdevio_pid=$!
+	trap "killprocess $bdevio_pid; compress_err_cleanup; exit 1" SIGINT SIGTERM EXIT
+	waitforlisten $bdevio_pid
+	$rpc_py set_compress_pmd -p $1
+	$rpc_py construct_nvme_bdev -b "Nvme0" -t "pcie" -a $bdf
+	waitforbdev $compress_bdev
+	$rootdir/test/bdev/bdevio/tests.py perform_tests
+	trap - SIGINT SIGTERM EXIT
+	killprocess $bdevio_pid
+}
+
+function run_bdevperf() {
+	$rootdir/test/bdev/bdevperf/bdevperf -z -q $qd  -o $iosize -w verify -t $runtime &
+	bdevperf_pid=$!
+	trap "killprocess $bdevperf_pid; compress_err_cleanup; exit 1" SIGINT SIGTERM EXIT
+	waitforlisten $bdevperf_pid
+	$rpc_py set_compress_pmd -p $1
+	$rpc_py construct_nvme_bdev -b "Nvme0" -t "pcie" -a $bdf
+	waitforbdev $compress_bdev
+	$rootdir/test/bdev/bdevperf/bdevperf.py perform_tests
+	if [ $RUN_NIGHTLY -eq 0 ]; then
+		# now cleanup the vols, deleting the compression vol also deletes the pmem file
+		$rpc_py delete_compress_bdev COMP_lvs0/lv0
+		$rpc_py destroy_lvol_store -l lvs0
+	fi
+	trap - SIGINT SIGTERM EXIT
+	killprocess $bdevperf_pid
+}
+
 # use the bdev svc to create a compress bdev, this assumes
 # there is no other metadata on the nvme device, we will put a
 # compress vol on a thin provisioned lvol on nvme
@@ -27,51 +58,33 @@ bdf=$(iter_pci_class_code 01 08 02 | head -1)
 $rpc_py construct_nvme_bdev -b "Nvme0" -t "pcie" -a $bdf
 lvs_u=$($rpc_py construct_lvol_store Nvme0n1 lvs0)
 $rpc_py construct_lvol_bdev -t -u $lvs_u lv0 100
-# this will force isal_pmd as some of the CI systems need a qat driver update
-$rpc_py set_compress_pmd -p 2
 compress_bdev=$($rpc_py construct_compress_bdev -b lvs0/lv0 -p /tmp)
 trap - SIGINT SIGTERM EXIT
 killprocess $bdev_svc_pid
 
-# run bdevio test
 timing_enter compress_test
-$rootdir/test/bdev/bdevio/bdevio -w &
-bdevio_pid=$!
-trap "killprocess $bdevio_pid; compress_err_cleanup; exit 1" SIGINT SIGTERM EXIT
-waitforlisten $bdevio_pid
-$rpc_py set_compress_pmd -p 2
-$rpc_py construct_nvme_bdev -b "Nvme0" -t "pcie" -a $bdf
-waitforbdev $compress_bdev
-$rootdir/test/bdev/bdevio/tests.py perform_tests
-trap - SIGINT SIGTERM EXIT
-killprocess $bdevio_pid
 
-#run bdevperf with slightly different params for nightly
+# run bdevio test, ISAL for per patch and both for nightly.
+run_bdevio(2)
+if [ $RUN_NIGHTLY -eq 1 ]; then
+	run_bdevio(1)
+fi
+
+# run bdevperf with slightly different params for nightly
 qd=32
 runtime=3
 iosize=4096
+run_bdevperf(2)
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	qd=64
 	runtime=30
 	iosize=16384
+	run_bdevperf(1)
 fi
-$rootdir/test/bdev/bdevperf/bdevperf -z -q $qd  -o $iosize -w verify -t $runtime &
-bdevperf_pid=$!
-trap "killprocess $bdevperf_pid; compress_err_cleanup; exit 1" SIGINT SIGTERM EXIT
-waitforlisten $bdevperf_pid
-$rpc_py set_compress_pmd -p 2
-$rpc_py construct_nvme_bdev -b "Nvme0" -t "pcie" -a $bdf
-waitforbdev $compress_bdev
-$rootdir/test/bdev/bdevperf/bdevperf.py perform_tests
-if [ $RUN_NIGHTLY -eq 0 ]; then
-	# now cleanup the vols, deleting the compression vol also deletes the pmem file
-	$rpc_py delete_compress_bdev COMP_lvs0/lv0
-	$rpc_py destroy_lvol_store -l lvs0
-fi
-trap - SIGINT SIGTERM EXIT
-killprocess $bdevperf_pid
 
-# run perf with nvmf using compress bdev for nightly test only
+# run perf with nvmf using compress bdev for nightly test only. To save time,
+# this test will alwways run on QAT if available and ISAL if not but we will
+# not run it twice.
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	export TEST_TRANSPORT=tcp && nvmftestinit
 	nvmfappstart "-m 0x7"
