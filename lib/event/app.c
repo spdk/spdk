94710ed13d3009d15eb0545ce4e38338af97e5b2
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2019-08-02 07:29:09 -0400

    [WIP] app: remove dependency on app.c for subsystem start
    TBD
    
    Change-Id: I535e079339cedaf0950767a8204002ab5885d8a5
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- lib/event/app.c
+++ lib/event/app.c
@@ -1061,14 +1061,7 @@ spdk_rpc_start_subsystem_init_cpl(void *arg1)
 	assert(spdk_get_thread() == g_app_thread);
 
 	spdk_rpc_set_state(SPDK_RPC_RUNTIME);
-	/* If we're loading JSON config file, we're still operating on a fake,
-	 * temporary RPC server. We'll have to defer calling the app start callback
-	 * until this temporary server is shut down and a real one - listening on
-	 * the proper socket - is started.
-	 */
-	if (g_spdk_app.json_config_file == NULL) {
-		spdk_app_start_application();
-	}
+	spdk_app_start_application();
 
 	w = spdk_jsonrpc_begin_result(request);
 	spdk_json_write_bool(w, true);
