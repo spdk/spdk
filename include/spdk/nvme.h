014d37ef1faa5f8f582b727e65c56ccdd4e4518b
Changpeng Liu <changpeng.liu@intel.com>
2019-08-01 21:42:54 -0400

    nvme: add an option 'no_shn_notification' to driver
    We will do a normal shutdown notification for most cases, and
    it will take some time e.g. 2 seconds to take the action.  If
    user environment has several drives, because the process is
    synchronous, so the time maybe longer that user can accept.
    
    Since user can know exactly what they would like to do next,
    so here we provide an option to user, user can set it to true
    to skip the shutdown notification process so that they can
    have very quick startup.
    
    Change-Id: Ie7f87115d57776729fab4cdac489cae6dc13511b
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- include/spdk/nvme.h
+++ include/spdk/nvme.h
@@ -77,6 +77,11 @@ struct spdk_nvme_ctrlr_opts {
 	bool use_cmb_sqs;
 
 	/**
+	 * Don't initiate shutdown processing
+	 */
+	bool no_shn_notification;
+
+	/**
 	 * Type of arbitration mechanism
 	 */
 	enum spdk_nvme_cc_ams arb_mechanism;
