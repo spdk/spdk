0517a9fb713f3a257d9b872f644f4f2c2f59ffcb
Ziye Yang <ziye.yang@intel.com>
2019-08-03 17:50:28 +0800

    sock: Chagne spdk_sock_recv/readv/writev to async function.
    This patch is used to add two parameters, e.g.,
    cb_fn, cb_arg in the 3 functions, and prepare to make it
    async functions later.
    
    Signed-off-by: Ziye Yang <ziye.yang@intel.com>
    Change-Id: I69021b0dffd6a7592ca9faedcc6770fd00d7ad35

--- lib/sock/vpp/vpp.c
+++ lib/sock/vpp/vpp.c
@@ -848,7 +848,8 @@ spdk_vpp_sock_close(struct spdk_sock *_sock)
 }
 
 static ssize_t
-spdk_vpp_sock_recv(struct spdk_sock *_sock, void *buf, size_t len)
+spdk_vpp_sock_recv(struct spdk_sock *_sock, void *buf, size_t len, spdk_sock_op_cb cb_fn,
+		   void *cb_arg)
 {
 	struct spdk_vpp_session *session = __vpp_session(_sock);
 	int rc;
@@ -887,7 +888,8 @@ spdk_vpp_sock_recv(struct spdk_sock *_sock, void *buf, size_t len)
 }
 
 static ssize_t
-spdk_vpp_sock_readv(struct spdk_sock *_sock, struct iovec *iov, int iovcnt)
+spdk_vpp_sock_readv(struct spdk_sock *_sock, struct iovec *iov, int iovcnt, spdk_sock_op_cb cb_fn,
+		    void *cb_arg)
 {
 	ssize_t total = 0;
 	int i, rc;
@@ -896,7 +898,7 @@ spdk_vpp_sock_readv(struct spdk_sock *_sock, struct iovec *iov, int iovcnt)
 	assert(g_svm.vpp_initialized);
 
 	for (i = 0; i < iovcnt; ++i) {
-		rc = spdk_vpp_sock_recv(_sock, iov[i].iov_base, iov[i].iov_len);
+		rc = spdk_vpp_sock_recv(_sock, iov[i].iov_base, iov[i].iov_len, NULL, NULL);
 		if (rc < 0) {
 			if (total > 0) {
 				break;
@@ -916,7 +918,8 @@ spdk_vpp_sock_readv(struct spdk_sock *_sock, struct iovec *iov, int iovcnt)
 }
 
 static ssize_t
-spdk_vpp_sock_writev(struct spdk_sock *_sock, struct iovec *iov, int iovcnt)
+spdk_vpp_sock_writev(struct spdk_sock *_sock, struct iovec *iov, int iovcnt, spdk_sock_op_cb cb_fn,
+		     void *cb_arg)
 {
 	struct spdk_vpp_session *session = __vpp_session(_sock);
 	ssize_t total = 0;
