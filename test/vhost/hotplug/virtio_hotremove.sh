4070c6496d0852b68604e04ab186e1676c058935
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-08-07 16:23:24 -0400

    test/hotplug: Add hotremove test using virtio initiator
    This is basic test to check if hotremove is working properly.
    It uses bdevperf in order to quickly prepare env for hotremove test.
    
    Steps for this test:
    1. Run vhost
    2. Add nvme to vhost and split it
    3. Create blk and scsi controllers and assign two nvme splits to them.
    4. Run bdevperf for both disks.
    5. Delete nvme to simulate hotremove.
    6. Check if bdevperf ended with success.
    
    Change-Id: I6ba084041af1442fd254141803774758b840147c
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

---
+++ test/vhost/hotplug/virtio_hotremove.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -xe
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../../..)
+source $rootdir/test/common/autotest_common.sh
+source $rootdir/test/vhost/common.sh
+source $rootdir/test/vhost/hotplug/common.sh
+
+function run_bdevperf_test {
+	trap - ERR
+	set +xe
+	$rootdir/test/bdev/bdevperf/bdevperf.py perform_tests
+	ret_value=$?
+	if [ $ret_value -ne 0 ]; then
+		echo "Perform test ended with error"
+		return 1
+	fi
+}
+
+vhost_run 0
+$rpc_py bdev_nvme_set_hotplug -e
+$rpc_py bdev_split_create Nvme0n1 2
+$rpc_py vhost_create_scsi_controller scsi_ctrl
+$rpc_py vhost_scsi_controller_add_target scsi_ctrl 0 Nvme0n1p0
+$rpc_py vhost_create_blk_controller blk_ctrl Nvme0n1p1
+
+$rootdir/test/bdev/bdevperf/bdevperf -c $testdir/bdev.conf -g -q 128 -o 4096 -w verify -z -t 15 &
+bdevperf_pid=$!
+waitforlisten $bdevperf_pid
+trap 'killprocess $bdevperf_pid; vhost_kill 0; exit 1' ERR
+run_bdevperf_test &
+perftest_pid=$!
+sleep 5
+# Do hotremove
+$rpc_py bdev_nvme_detach_controller "Nvme0"
+sleep 5
+if ! kill -0 $bdevperf_pid 2>/dev/null; then
+	error "Bdevperf exited but shouldn't"
+fi
+sleep 5
+if wait $perftest_pid; then
+	error "Hot-remove exited with success when error was expected"
+fi
+if killprocess $bdevperf_pid; then
+	error "Bdevperf ended with success"
+fi
+
+# Do hotattach
+get_traddr "Nvme0"
+$rpc_py vhost_delete_controller blk_ctrl
+$rpc_py bdev_nvme_attach_controller -b "HotInNvme1" -t PCIe -a "$traddr"
+$rpc_py bdev_split_create HotInNvme1n1 2
+$rpc_py vhost_scsi_controller_add_target scsi_ctrl 0 HotInNvme1n1p0
+$rpc_py vhost_create_blk_controller blk_ctrl HotInNvme1n1p1
+$rootdir/test/bdev/bdevperf/bdevperf -c $testdir/bdev.conf -g -q 128 -o 4096 -w verify -t 5
+
+$rpc_py vhost_scsi_controller_remove_target scsi_ctrl 0
+$rpc_py vhost_delete_controller scsi_ctrl
+$rpc_py vhost_delete_controller blk_ctrl
+vhost_kill 0
