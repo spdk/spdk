d35cf1f65589684fffcc6d15dfad4c063de6dc50
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-08-07 16:23:24 -0400

    test/hotplug: Add hotremove test using virtio initiator
    This is basic test to check if hotremove is working properly.
    It uses virtio initiator in order to quickly prepare env for hotremove test.
    
    Steps for this test:
    1. Run vhost
    2. Add nvme to vhost and split it
    3. Create blk and scsi controllers and assign two nvme splits to them.
    4. Run fio for both disks.
    5. Delete nvme to simulate hotremove.
    6. Check if fio ended with error.
    
    Change-Id: I6ba084041af1442fd254141803774758b840147c
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/vhost/vhost.sh
+++ test/vhost/vhost.sh
@@ -40,6 +40,11 @@ run_test suite $WORKDIR/vhost_boot/vhost_boot.sh --vm_image=$VM_IMAGE
 report_test_completion "vhost_boot"
 timing_exit vhost_boot
 
+timing_enter vhost_hotplug
+run_test suite $WORKDIR/vhost/manual.sh -vhr
+report_test_completion "vhost_hotplug"
+timing_exit vhost_hotplug
+
 if [ $RUN_NIGHTLY -eq 1 ]; then
 	timing_enter integrity_blk
 	echo 'Running blk integrity suite...'
