d9ef586f4b21ca09efdb5d0b15accf8fd0517638
Seth Howell <seth.howell@intel.com>
2019-08-08 18:09:58 -0700

    autobuild.sh: run hello_bdev with shared objects
    It is important to make sure that linking of shared libraries is
    configured properly and symbols will be properly stored. This is an
    early step in that effort.
    
    Change-Id: I94e6b6e441ca536b270c3f41754bb7bd15fc03ae
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -79,6 +79,12 @@ $MAKE $MAKEFLAGS clean
 if [ $SPDK_BUILD_SHARED_OBJECT -eq 1 ]; then
 	./configure $config_params --with-shared
 	$MAKE $MAKEFLAGS
+
+	# run a quick example application to make sure that the linking worked.
+	sudo ./scripts/setup.sh
+	sudo LD_LIBRARY_PATH=./build/lib/:./dpdk/build/lib/ ./examples/bdev/hello_world/hello_bdev -c ./examples/bdev/hello_world/bdev.conf
+	sudo ./scripts/setup.sh reset
+
 	$MAKE $MAKEFLAGS clean
 	report_test_completion "shared_object_build"
 fi
