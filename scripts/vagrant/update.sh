25a850e5b5017922f015d4935bc0760769ef700a
yidong0635 <dongx.yi@intel.com>
2019-08-12 12:10:07 -0400

    vagrant: Remove fedora26 and duplicated behaviors when setup.
    README.md: Remove key words about fedora26/fedora27 from examples,
    	fedora26/fedora27 base boxes have been removed.
    update.sh: Remove assigned setup for centos /fedora /fedora26 .
    	Remove duplicate behaviors in scripts. And solve some kind of syncronized
    	error on fedora30.
    
    Change-Id: Ib0341636c783146be165c1a6aec41fc1fe361951
    Signed-off-by: yidong0635 <dongx.yi@intel.com>

--- scripts/vagrant/update.sh
+++ scripts/vagrant/update.sh
@@ -62,9 +62,13 @@ else
 	if [ -f /etc/lsb-release ];then
 		. /etc/lsb-release
 	elif [ -f /etc/redhat-release ];then
-		yum update -y
 		yum install -y redhat-lsb
 		DISTRIB_ID=$(lsb_release -si)
+		if [ "$DISTRIB_ID" == "fedora" ];then
+			dnf clean all
+			dnf update -y
+		fi
+		yum update -y
 		DISTRIB_RELEASE=$(lsb_release -sr)
 		DISTRIB_CODENAME=$(lsb_release -sc)
 		DISTRIB_DESCRIPTION=$(lsb_release -sd)
@@ -79,21 +83,6 @@ else
 		# Standard update + upgrade dance
 		apt-get update --assume-yes --no-install-suggests --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 		apt-get upgrade --assume-yes --no-install-suggests --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-		${SPDK_DIR}/scripts/pkgdep.sh
-	elif [ "$DISTRIB_ID" == "CentOS" ]; then
-		# Standard update + upgrade dance
-		yum check-update
-		yum update -y
-		${SPDK_DIR}/scripts/pkgdep.sh
-	elif [ "$DISTRIB_ID" == "Fedora" ]; then
-		if [ "$DISTRIB_RELEASE" = "26" ]; then
-			echo
-			echo "  Run \"${SPDK_DIR}/test/common/config/vm_setup.sh\" to complete setup of Fedora 26"
-			echo
-		else
-			yum check-update
-			yum update -y
-			${SPDK_DIR}/scripts/pkgdep.sh
-		fi
 	fi
+	${SPDK_DIR}/scripts/pkgdep.sh
 fi
