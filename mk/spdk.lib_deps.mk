89a11ba63aa88495352b399c173c4f2eb9270760
Seth Howell <seth.howell@intel.com>
2019-08-12 15:46:38 -0700

    make: start adding inter-lib dependencies.
    This will make it much easier to link against some subset of spdk
    libraries when building SPDK applications.
    
    Change-Id: I8ad95a001965288a8b5e38eb252391fef68d7138
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- mk/spdk.lib_deps.mk
+++ mk/spdk.lib_deps.mk
@@ -31,5 +31,49 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-DEPDIRS-iscsi := scsi
-DEPDIRS-bdev := notify
+# A quick note on organization:
+#
+# Each grouping is independent from itself. it depends only on libraries
+# in the grouping above it. All dependencies are listed alphabetically within
+# groups. The only exception to this is the JSON_LIBS grouping which is a special
+# case since they almost always occur together.
+
+JSON_LIBS := json jsonrpc rpc
+
+DEPDIRS-log :=
+
+DEPDIRS-ioat := log
+DEPDIRS-sock := log
+DEPDIRS-util := log
+DEPDIRS-vmd := log
+
+DEPDIRS-conf := log util
+DEPDIRS-json := log util
+DEPDIRS-nvme := log sock util
+DEPDIRS-reduce := log util
+DEPDIRS-thread := log util
+
+DEPDIRS-blob := log util thread
+DEPDIRS-copy := thread
+DEPDIRS-jsonrpc := log util json
+DEPDIRS-virtio := log util json thread
+
+DEPDIRS-lvol := log util blob
+DEPDIRS-rpc := log util json jsonrpc
+
+DEPDIRS-log_rpc := log $(JSON_LIBS)
+DEPDIRS-net := log util $(JSON_LIBS)
+DEPDIRS-notify := log util $(JSON_LIBS)
+DEPDIRS-trace := log util $(JSON_LIBS)
+
+DEPDIRS-bdev := log util conf thread $(JSON_LIBS) notify trace
+DEPDIRS-blobfs := log conf thread blob trace
+DEPDIRS-event := log util conf thread $(JSON_LIBS) trace
+
+DEPDIRS-ftl := log util nvme thread trace bdev
+DEPDIRS-nbd := log util thread $(JSON_LIBS) bdev
+DEPDIRS-nvmf := log sock util nvme thread $(JSON_LIBS) trace bdev
+DEPDIRS-scsi := log util thread $(JSON_LIBS) trace bdev
+
+DEPDIRS-iscsi := log sock util conf thread $(JSON_LIBS) trace event scsi
+DEPDIRS-vhost := log util conf thread $(JSON_LIBS) bdev event scsi
