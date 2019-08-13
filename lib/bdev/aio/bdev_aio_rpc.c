9b030426a73e837ea656b0e9fb5e927dfa4092b4
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-08-13 09:57:00 +0200

    RPC: rename RPC construct_aio_bdev to bdev_aio_create
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: Ie1b5ffaf5e4322d37e23fe4386cf433d1ea3bfc6

--- lib/bdev/aio/bdev_aio_rpc.c
+++ lib/bdev/aio/bdev_aio_rpc.c
@@ -57,8 +57,8 @@ static const struct spdk_json_object_decoder rpc_construct_aio_decoders[] = {
 };
 
 static void
-spdk_rpc_construct_aio_bdev(struct spdk_jsonrpc_request *request,
-			    const struct spdk_json_val *params)
+spdk_rpc_bdev_aio_create(struct spdk_jsonrpc_request *request,
+			 const struct spdk_json_val *params)
 {
 	struct rpc_construct_aio req = {};
 	struct spdk_json_write_ctx *w;
@@ -87,7 +87,8 @@ spdk_rpc_construct_aio_bdev(struct spdk_jsonrpc_request *request,
 cleanup:
 	free_rpc_construct_aio(&req);
 }
-SPDK_RPC_REGISTER("construct_aio_bdev", spdk_rpc_construct_aio_bdev, SPDK_RPC_RUNTIME)
+SPDK_RPC_REGISTER("bdev_aio_create", spdk_rpc_bdev_aio_create, SPDK_RPC_RUNTIME)
+SPDK_RPC_REGISTER_ALIAS_DEPRECATED(bdev_aio_create, construct_aio_bdev)
 
 struct rpc_delete_aio {
 	char *name;
