e5f0e91dd3d343cf7217e81409b71e841c96d3fb
Mateusz Kozlowski <mateusz.kozlowski@intel.com>
2019-08-21 10:38:53 +0200

    bdev/zone: Register/unregister zoned bdev
    Add registration and unregistration of block zoned bdev. Attach it to the
    underlying bdev during creation and unattach at deletion.
    
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>
    Change-Id: I773aff6c7609952f28c02dd1794f0529a781b2e1

--- module/bdev/zone/vbdev_zone_block.c
+++ module/bdev/zone/vbdev_zone_block.c
@@ -40,6 +40,360 @@
 
 #include "spdk_internal/log.h"
 
+static int vbdev_block_init(void);
+static int vbdev_block_get_ctx_size(void);
+static void vbdev_block_finish(void);
+static int vbdev_block_config_json(struct spdk_json_write_ctx *w);
+static void vbdev_block_examine(struct spdk_bdev *bdev);
+
+static struct spdk_bdev_module bdev_zoned_if = {
+	.name = "bdev_zoned_block",
+	.module_init = vbdev_block_init,
+	.module_fini = vbdev_block_finish,
+	.config_text = NULL,
+	.config_json = vbdev_block_config_json,
+	.examine_config = vbdev_block_examine,
+	.get_ctx_size = vbdev_block_get_ctx_size,
+};
+
+SPDK_BDEV_MODULE_REGISTER(bdev_zoned_block, &bdev_zoned_if)
+
+/* List of block_vbdev names and their base bdevs via configuration file.
+ * Used so we can parse the conf once at init and use this list in examine().
+ */
+struct bdev_names {
+	char			*vbdev_name;
+	char			*bdev_name;
+	TAILQ_ENTRY(bdev_names)	link;
+};
+static TAILQ_HEAD(, bdev_names) g_bdev_names = TAILQ_HEAD_INITIALIZER(g_bdev_names);
+
+/* List of block vbdevs and associated info for each. */
+struct vbdev_block {
+	struct spdk_bdev		*base_bdev; /* the block device we're attaching to */
+	struct spdk_bdev_desc		*base_desc; /* its descriptor we get from open */
+	struct spdk_bdev		bdev;    /* the block zoned bdev */
+	TAILQ_ENTRY(vbdev_block)	link;
+};
+static TAILQ_HEAD(, vbdev_block) g_bdev_nodes = TAILQ_HEAD_INITIALIZER(g_bdev_nodes);
+
+struct vbdev_io_channel {
+	struct spdk_io_channel	*base_ch; /* IO channel of base device */
+};
+
+struct block_vbdev_io {
+	/* bdev related */
+	struct spdk_io_channel *ch;
+
+	/* for bdev_io_wait */
+	struct spdk_bdev_io_wait_entry bdev_io_wait;
+};
+
+static int
+vbdev_block_init(void)
+{
+	return 0;
+}
+
+static void
+vbdev_remove_config(struct bdev_names *name)
+{
+	TAILQ_REMOVE(&g_bdev_names, name, link);
+	free(name->bdev_name);
+	free(name->vbdev_name);
+	free(name);
+}
+
+static void
+vbdev_block_finish(void)
+{
+	struct bdev_names *name;
+
+	while ((name = TAILQ_FIRST(&g_bdev_names))) {
+		vbdev_remove_config(name);
+	}
+
+	return;
+}
+
+static int
+vbdev_block_get_ctx_size(void)
+{
+	return sizeof(struct block_vbdev_io);
+}
+
+static int
+vbdev_block_config_json(struct spdk_json_write_ctx *w)
+{
+	struct vbdev_block *bdev_node;
+
+	TAILQ_FOREACH(bdev_node, &g_bdev_nodes, link) {
+		spdk_json_write_object_begin(w);
+		spdk_json_write_named_string(w, "method", "bdev_zone_block_create");
+		spdk_json_write_named_object_begin(w, "params");
+		spdk_json_write_named_string(w, "bdev_name", spdk_bdev_get_name(bdev_node->base_bdev));
+		spdk_json_write_named_string(w, "name", spdk_bdev_get_name(&bdev_node->bdev));
+		spdk_json_write_object_end(w);
+		spdk_json_write_object_end(w);
+	}
+
+	return 0;
+}
+
+static void
+vbdev_block_write_config_json(struct spdk_bdev *bdev, struct spdk_json_write_ctx *w)
+{
+	return;
+}
+
+/* Callback for unregistering the IO device. */
+static void
+_device_unregister_cb(void *io_device)
+{
+	struct vbdev_block *bdev_node  = io_device;
+
+	free(bdev_node->bdev.name);
+	free(bdev_node);
+}
+
+static int
+vbdev_block_destruct(void *ctx)
+{
+	struct vbdev_block *bdev_node = (struct vbdev_block *)ctx;
+
+	TAILQ_REMOVE(&g_bdev_nodes, bdev_node, link);
+
+	/* Unclaim the underlying bdev. */
+	spdk_bdev_module_release_bdev(bdev_node->base_bdev);
+
+	/* Close the underlying bdev. */
+	spdk_bdev_close(bdev_node->base_desc);
+
+	/* Unregister the io_device. */
+	spdk_io_device_unregister(bdev_node, _device_unregister_cb);
+
+	return 0;
+}
+
+static void
+vbdev_block_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io)
+{
+	spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_FAILED);
+}
+
+static bool
+vbdev_block_io_type_supported(void *ctx, enum spdk_bdev_io_type io_type)
+{
+	struct vbdev_block *bdev_node = (struct vbdev_block *)ctx;
+
+	return spdk_bdev_io_type_supported(bdev_node->base_bdev, io_type);
+}
+
+static struct spdk_io_channel *
+vbdev_block_get_io_channel(void *ctx)
+{
+	struct vbdev_block *bdev_node = (struct vbdev_block *)ctx;
+
+	return spdk_get_io_channel(bdev_node);
+}
+
+static int
+vbdev_block_dump_info_json(void *ctx, struct spdk_json_write_ctx *w)
+{
+	struct vbdev_block *bdev_node = (struct vbdev_block *)ctx;
+
+	spdk_json_write_name(w, "vbdev");
+	spdk_json_write_object_begin(w);
+	spdk_json_write_named_string(w, "name", spdk_bdev_get_name(&bdev_node->bdev));
+	spdk_json_write_named_string(w, "bdev_name", spdk_bdev_get_name(bdev_node->base_bdev));
+	spdk_json_write_object_end(w);
+
+	return 0;
+}
+
+/* When we register our vbdev this is how we specify our entry points. */
+static const struct spdk_bdev_fn_table vbdev_block_fn_table = {
+	.destruct		= vbdev_block_destruct,
+	.submit_request		= vbdev_block_submit_request,
+	.io_type_supported	= vbdev_block_io_type_supported,
+	.get_io_channel		= vbdev_block_get_io_channel,
+	.dump_info_json		= vbdev_block_dump_info_json,
+	.write_config_json	= vbdev_block_write_config_json,
+};
+
+static void
+vbdev_block_base_bdev_hotremove_cb(void *ctx)
+{
+	struct vbdev_block *bdev_node, *tmp;
+	struct spdk_bdev *bdev_find = ctx;
+
+	TAILQ_FOREACH_SAFE(bdev_node, &g_bdev_nodes, link, tmp) {
+		if (bdev_find == bdev_node->base_bdev) {
+			spdk_bdev_unregister(&bdev_node->bdev, NULL, NULL);
+		}
+	}
+}
+
+static int
+block_vbdev_ch_create_cb(void *io_device, void *ctx_buf)
+{
+	struct vbdev_io_channel *bdev_ch = ctx_buf;
+	struct vbdev_block *bdev_node = io_device;
+
+	bdev_ch->base_ch = spdk_bdev_get_io_channel(bdev_node->base_desc);
+
+	return 0;
+}
+
+static void
+block_vbdev_ch_destroy_cb(void *io_device, void *ctx_buf)
+{
+	struct vbdev_io_channel *bdev_ch = ctx_buf;
+
+	spdk_put_io_channel(bdev_ch->base_ch);
+}
+
+static int
+block_vbdev_insert_name(const char *bdev_name, const char *vbdev_name)
+{
+	struct bdev_names *name;
+
+	TAILQ_FOREACH(name, &g_bdev_names, link) {
+		if (strcmp(vbdev_name, name->vbdev_name) == 0) {
+			SPDK_ERRLOG("block zoned bdev %s already exists\n", vbdev_name);
+			return -EEXIST;
+		}
+		if (strcmp(bdev_name, name->bdev_name) == 0) {
+			SPDK_ERRLOG("base bdev %s already claimed\n", bdev_name);
+			return -EEXIST;
+		}
+	}
+
+	name = calloc(1, sizeof(struct bdev_names));
+	if (!name) {
+		SPDK_ERRLOG("could not allocate bdev_names\n");
+		return -ENOMEM;
+	}
+
+	name->bdev_name = strdup(bdev_name);
+	if (!name->bdev_name) {
+		SPDK_ERRLOG("could not allocate name->bdev_name\n");
+		free(name);
+		return -ENOMEM;
+	}
+
+	name->vbdev_name = strdup(vbdev_name);
+	if (!name->vbdev_name) {
+		SPDK_ERRLOG("could not allocate name->vbdev_name\n");
+		free(name->bdev_name);
+		free(name);
+		return -ENOMEM;
+	}
+
+	TAILQ_INSERT_TAIL(&g_bdev_names, name, link);
+
+	return 0;
+}
+
+static int
+vbdev_block_register(struct spdk_bdev *bdev)
+{
+	struct bdev_names *name, *tmp;
+	struct vbdev_block *bdev_node;
+	int rc = 0;
+
+	/* Check our list of names from config versus this bdev and if
+	 * there's a match, create the pt_node & bdev accordingly.
+	 */
+	TAILQ_FOREACH_SAFE(name, &g_bdev_names, link, tmp) {
+		if (strcmp(name->bdev_name, bdev->name) != 0) {
+			continue;
+		}
+
+		if (spdk_bdev_is_zoned(bdev)) {
+			SPDK_ERRLOG("Base bdev (%s) is a zoned bdev\n", bdev->name);
+			rc = -ENODEV;
+			goto free_config;
+		}
+
+		bdev_node = calloc(1, sizeof(struct vbdev_block));
+		if (!bdev_node) {
+			rc = -ENOMEM;
+			SPDK_ERRLOG("could not allocate bdev_node\n");
+			goto free_config;
+		}
+
+		/* The base bdev that we're attaching to. */
+		bdev_node->base_bdev = bdev;
+		bdev_node->bdev.name = strdup(name->vbdev_name);
+		if (!bdev_node->bdev.name) {
+			rc = -ENOMEM;
+			SPDK_ERRLOG("could not allocate bdev_node name\n");
+			goto strdup_failed;
+		}
+		bdev_node->bdev.product_name = "vbdev_block";
+
+		/* Copy some properties from the underlying base bdev. */
+		bdev_node->bdev.write_cache = bdev->write_cache;
+		bdev_node->bdev.required_alignment = bdev->required_alignment;
+		bdev_node->bdev.optimal_io_boundary = bdev->optimal_io_boundary;
+		bdev_node->bdev.blocklen = bdev->blocklen;
+		bdev_node->bdev.blockcnt = bdev->blockcnt;
+		bdev_node->bdev.write_unit_size = bdev->write_unit_size;
+
+		bdev_node->bdev.md_interleave = bdev->md_interleave;
+		bdev_node->bdev.md_len = bdev->md_len;
+		bdev_node->bdev.dif_type = bdev->dif_type;
+		bdev_node->bdev.dif_is_head_of_md = bdev->dif_is_head_of_md;
+		bdev_node->bdev.dif_check_flags = bdev->dif_check_flags;
+
+		bdev_node->bdev.zoned = true;
+		bdev_node->bdev.ctxt = bdev_node;
+		bdev_node->bdev.fn_table = &vbdev_block_fn_table;
+		bdev_node->bdev.module = &bdev_zoned_if;
+		TAILQ_INSERT_TAIL(&g_bdev_nodes, bdev_node, link);
+
+		spdk_io_device_register(bdev_node, block_vbdev_ch_create_cb, block_vbdev_ch_destroy_cb,
+					sizeof(struct vbdev_io_channel),
+					name->vbdev_name);
+
+		rc = spdk_bdev_open(bdev, true, vbdev_block_base_bdev_hotremove_cb,
+				    bdev, &bdev_node->base_desc);
+		if (rc) {
+			SPDK_ERRLOG("could not open bdev %s\n", spdk_bdev_get_name(bdev));
+			goto open_failed;
+		}
+
+		rc = spdk_bdev_module_claim_bdev(bdev, bdev_node->base_desc, bdev_node->bdev.module);
+		if (rc) {
+			SPDK_ERRLOG("could not claim bdev %s\n", spdk_bdev_get_name(bdev));
+			goto claim_failed;
+		}
+
+		rc = spdk_bdev_register(&bdev_node->bdev);
+		if (rc) {
+			SPDK_ERRLOG("could not register zoned bdev\n");
+			goto register_failed;
+		}
+	}
+
+	return rc;
+
+register_failed:
+	spdk_bdev_module_release_bdev(&bdev_node->bdev);
+claim_failed:
+	spdk_bdev_close(bdev_node->base_desc);
+open_failed:
+	TAILQ_REMOVE(&g_bdev_nodes, bdev_node, link);
+	spdk_io_device_unregister(bdev_node, NULL);
+	free(bdev_node->bdev.name);
+strdup_failed:
+	free(bdev_node);
+free_config:
+	vbdev_remove_config(name);
+	return rc;
+}
+
 int
 spdk_vbdev_zone_block_create(const char *bdev_name, const char *vbdev_name, size_t num_zones,
 			     size_t max_open_zones)
@@ -47,16 +401,9 @@ spdk_vbdev_zone_block_create(const char *bdev_name, const char *vbdev_name, size
 	struct spdk_bdev *bdev = NULL;
 	int rc = 0;
 
-	bdev = spdk_bdev_get_by_name(bdev_name);
-
-	if (!bdev) {
-		SPDK_ERRLOG("Base bdev (%s) doesn't exist\n", bdev_name);
-		return -ENODEV;
-	}
-
-	if (spdk_bdev_is_zoned(bdev)) {
-		SPDK_ERRLOG("Base bdev (%s) is a zone dev\n", bdev_name);
-		return -ENODEV;
+	if (num_zones == 0) {
+		SPDK_ERRLOG("Can't create vbdev with 0 zones\n");
+		return -EINVAL;
 	}
 
 	if (num_zones < max_open_zones) {
@@ -65,13 +412,55 @@ spdk_vbdev_zone_block_create(const char *bdev_name, const char *vbdev_name, size
 		return -EINVAL;
 	}
 
+	/* Insert the bdev into our global name list even if it doesn't exist yet,
+	 * it may show up soon...
+	 */
+	rc = block_vbdev_insert_name(bdev_name, vbdev_name);
+	if (rc) {
+		return rc;
+	}
+
+	bdev = spdk_bdev_get_by_name(bdev_name);
+	if (!bdev) {
+		/* This is not an error, we tracked the name above and it still
+		 * may show up later.
+		 */
+		return 0;
+	}
+
+	rc = vbdev_block_register(bdev);
+
 	return rc;
 }
 
 void
 spdk_vbdev_zone_block_delete(const char *name, spdk_bdev_unregister_cb cb_fn, void *cb_arg)
 {
-	cb_fn(cb_arg, 0);
+	struct bdev_names *name_node;
+	struct spdk_bdev *bdev = NULL;
+
+	bdev = spdk_bdev_get_by_name(name);
+	if (!bdev || bdev->module != &bdev_zoned_if) {
+		cb_fn(cb_arg, -ENODEV);
+		return;
+	}
+
+	TAILQ_FOREACH(name_node, &g_bdev_names, link) {
+		if (strcmp(name_node->vbdev_name, bdev->name) == 0) {
+			vbdev_remove_config(name_node);
+			break;
+		}
+	}
+
+	spdk_bdev_unregister(bdev, cb_fn, cb_arg);
+}
+
+static void
+vbdev_block_examine(struct spdk_bdev *bdev)
+{
+	vbdev_block_register(bdev);
+
+	spdk_bdev_module_examine_done(&bdev_zoned_if);
 }
 
 SPDK_LOG_REGISTER_COMPONENT("vbdev_zone_block", SPDK_LOG_VBDEV_ZONE_BLOCK)
