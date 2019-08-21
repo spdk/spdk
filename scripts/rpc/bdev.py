3ac49758e3fac9dab03be24b5dec559c04d8112a
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-08-21 05:03:04 -0400

    scripts/rpc: Rename rpcs related to ftl bdev
    Rename construct_ftl_bdev to bdev_ftl_create
    Rename delete_ftl_bdev to bdev_ftl_delete
    
    Change-Id: I661ecea2ed5f53880c9a77819fcca064f0410535
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- scripts/rpc/bdev.py
+++ scripts/rpc/bdev.py
@@ -646,7 +646,8 @@ def destruct_split_vbdev(client, base_bdev):
     return client.call('destruct_split_vbdev', params)
 
 
-def construct_ftl_bdev(client, name, trtype, traddr, punits, **kwargs):
+@deprecated_alias('construct_ftl_bdev')
+def bdev_ftl_create(client, name, trtype, traddr, punits, **kwargs):
     """Construct FTL bdev
 
     Args:
@@ -664,10 +665,11 @@ def construct_ftl_bdev(client, name, trtype, traddr, punits, **kwargs):
         if value is not None:
             params[key] = value
 
-    return client.call('construct_ftl_bdev', params)
+    return client.call('bdev_ftl_create', params)
 
 
-def delete_ftl_bdev(client, name):
+@deprecated_alias('delete_ftl_bdev')
+def bdev_ftl_delete(client, name):
     """Delete FTL bdev
 
     Args:
@@ -675,7 +677,7 @@ def delete_ftl_bdev(client, name):
     """
     params = {'name': name}
 
-    return client.call('delete_ftl_bdev', params)
+    return client.call('bdev_ftl_delete', params)
 
 
 def get_bdevs(client, name=None):
