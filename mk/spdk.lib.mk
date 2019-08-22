05d7a65f0ee2f92900c1a78c0a47e16594d724c2
Seth Howell <seth.howell@intel.com>
2019-08-22 10:26:08 -0700

    mk: add a lib deps file.
    By adding this file and modifying the way we define a couple of
    variables in the makefiles, we can actually avoid having to redefine
    the library dependencies on a per file basis.
    
    Change-Id: Ieab4aa1021b0341fc21e3b65677a9ad7f70559c2
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- mk/spdk.lib.mk
+++ mk/spdk.lib.mk
@@ -32,6 +32,7 @@
 #
 
 include $(SPDK_ROOT_DIR)/mk/spdk.common.mk
+include $(SPDK_ROOT_DIR)/mk/spdk.lib_deps.mk
 
 SPDK_MAP_FILE = $(SPDK_ROOT_DIR)/shared_lib/spdk.map
 LIB := $(call spdk_lib_list_to_static_libs,$(LIBNAME))
@@ -63,7 +64,7 @@ else
 BUILD_DEP := $(DEP)
 endif
 
-SPDK_DEP_LIBS = $(call spdk_lib_list_to_shared_libs,$(SPDK_DEP_LIBNAMES))
+SPDK_DEP_LIBS = $(call spdk_lib_list_to_shared_libs,$(DEPDIRS-$(LIBNAME)))
 
 .PHONY: all clean $(DIRS-y)
 
