spdk/spdk

commit 8cf1945432f383ce8906f000d3296d1584573234

iscsi: Hot remove callback closes LUN directly without using event call
This patch is in the patch series to migrate iSCSI connection management
from core based to SPDK thread based.

The callback to hot removal of LUN, iscsi_conn_remove_lun, is called
on the same core when the corresponding LUN is opened. Additionally,
all operations in iscsi_conn_remove_lun are completed synchronously.

Hence inline _iscsi_conn_remove_lun into iscsi_conn_remove_lun.
Add assert to check the function is called on the specified core.

This change is helpful to achieve the goal of the patch series
because spdk_event can have two parameters but spdk_msg can have
only a single variable, and hence we cannot convert simply and have
to introduce a context allocated dynamically otherwise.

Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
Change-Id: Iaebf18265dfe839f7361b09539527a1806aed1c4
Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/463551
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Paul Luse <paul.e.luse@intel.com>
Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>
Reviewed-by: Broadcom SPDK FC-NVMe CI <spdk-ci.pdl@broadcom.com>

lib/iscsi/conn.c
@@ -603,13 +603,14 @@ iscsi_conn_close_luns(struct spdk_iscsi_conn *conn)
 }
 
 static void
-_iscsi_conn_remove_lun(void *arg1, void *arg2)
+iscsi_conn_remove_lun(struct spdk_scsi_lun *lun, void *remove_ctx)
 {
-	struct spdk_iscsi_conn *conn = arg1;
-	struct spdk_scsi_lun *lun = arg2;
+	struct spdk_iscsi_conn *conn = remove_ctx;
 	int lun_id = spdk_scsi_lun_get_id(lun);
 	struct spdk_iscsi_pdu *pdu, *tmp_pdu;
 	struct spdk_iscsi_task *iscsi_task, *tmp_iscsi_task;
+
+	assert(conn->lcore == spdk_env_get_current_core());
 
 	/* If a connection is already in stating status, just return */
 	if (conn->state >= ISCSI_CONN_STATE_EXITING) {
@@ -646,17 +647,6 @@ _iscsi_conn_remove_lun(void *arg1, void *arg2)
 	}
 
 	iscsi_conn_close_lun(conn, lun_id);
-}
-
-static void
-iscsi_conn_remove_lun(struct spdk_scsi_lun *lun, void *remove_ctx)
-{
-	struct spdk_iscsi_conn *conn = remove_ctx;
-	struct spdk_event *event;
-
-	event = spdk_event_allocate(conn->lcore, _iscsi_conn_remove_lun,
-				    conn, lun);
-	spdk_event_call(event);
 }
 
 static void

commit 0bea50013be5b8cb6d106719a1b3e3e8e6d77df9

iscsi: Restore the previous bug fix for LUN hot plug
One recent commit destroyed the critical bug fix for LUN hot plug.
Hence this patch restores the critical bug fix. Simple revert is
not possible because connections are assigned to poll groups
instead of cores now. But we can revert easily because earlier
version of the recent patch did that.

Fixes #925

The github issue was caused by commit 8cf1945432f383ce8906f000d3296d1584573234.
The bug fix restored by this patch is commit 1f6a78620d1f4b7470ccf5ff378fd2eb6cf49c5f.
The reference we can follow to create this patch is earlier version
of commit fb641c4b542ec7616eeb6b88cce1e14069e66d62.

Reported-by: yidong0635 <dongx.yi@intel.com>

Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
Signed-off-by: Ziye Yang <ziye.yang@intel.com>
Signed-off-by: Ben Walker <benjamin.walker@intel.com>
Change-Id: Ie1ba14a59ce48149a8474cbffc56aa08adc1fc4d

lib/iscsi/conn.c
@@ -601,13 +601,22 @@ iscsi_conn_close_luns(struct spdk_iscsi_conn *conn)
 	}
 }
 
+struct _iscsi_conn_remove_ctx {
+	struct spdk_iscsi_conn *conn;
+	struct spdk_scsi_lun *lun;
+};
+
 static void
-iscsi_conn_remove_lun(struct spdk_scsi_lun *lun, void *remove_ctx)
+_iscsi_conn_remove_lun(void *_ctx)
 {
-	struct spdk_iscsi_conn *conn = remove_ctx;
+	struct _iscsi_conn_remove_ctx *ctx = _ctx;
+	struct spdk_iscsi_conn *conn = ctx->conn;
+	struct spdk_scsi_lun *lun = ctx->lun;
 	int lun_id = spdk_scsi_lun_get_id(lun);
 	struct spdk_iscsi_pdu *pdu, *tmp_pdu;
 	struct spdk_iscsi_task *iscsi_task, *tmp_iscsi_task;
+
+	free(ctx);
 
 	assert(spdk_io_channel_get_thread(spdk_io_channel_from_ctx(conn->pg)) ==
 	       spdk_get_thread());
@@ -647,6 +656,25 @@ iscsi_conn_remove_lun(struct spdk_scsi_lun *lun, void *remove_ctx)
 	}
 
 	iscsi_conn_close_lun(conn, lun_id);
+}
+
+static void
+iscsi_conn_remove_lun(struct spdk_scsi_lun *lun, void *remove_ctx)
+{
+	struct spdk_iscsi_conn *conn = remove_ctx;
+	struct _iscsi_conn_remove_ctx *ctx;
+
+	ctx = calloc(1, sizeof(*ctx));
+	if (!ctx) {
+		SPDK_ERRLOG("Unable to remove lun from connection\n");
+		return;
+	}
+
+	ctx->conn = conn;
+	ctx->lun = lun;
+
+	spdk_thread_send_msg(spdk_io_channel_get_thread(spdk_io_channel_from_ctx(conn->pg)),
+			     _iscsi_conn_remove_lun, ctx);
 }
 
 static void