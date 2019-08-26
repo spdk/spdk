e875d8f2c057b052833c9eecfd392a51396e596c
JinYu <jin.yu@intel.com>
2019-08-26 17:21:54 +0800

    nvmf_example: add an nvmf example
    use the nvmf lib to add an nvmf example which
    doesn't use the spdk app framework. It can show
    how to use the nvmf lib to build your own nvmf target.
    
    We encouage you to use the RPC cmds so this example will
    not supply the configuration file. You can read the
    README.md to get the steps how to build your nvmf target
    and test the IO status.
    
    Change-Id: I8d175934f7364d40fb44d36e85c31d8ea5063654
    Signed-off-by: JinYu <jin.yu@intel.com>

---
+++ test/nvmf/target/nvmf_example.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../../..)
+source $rootdir/test/common/autotest_common.sh
+source $rootdir/test/nvmf/common.sh
+
+rpc_py="$rootdir/scripts/rpc.py"
+
+function build_nvmf_example_args()
+{
+        if [ $SPDK_RUN_NON_ROOT -eq 1 ]; then
+                echo "sudo -u $(logname) ./examples/nvmf/nvmf/nvmf -i $NVMF_APP_SHM_ID"
+        else
+                echo "./examples/nvmf/nvmf/nvmf -i $NVMF_APP_SHM_ID"
+        fi
+}
+
+NVMF_EXAMPLE="$(build_nvmf_example_args)"
+
+function nvmfexamplestart()
+{
+        timing_enter start_nvmf_example
+        $NVMF_EXAMPLE $1
+        nvmfpid=$!
+        #trap 'process_shm --id $NVMF_APP_SHM_ID; nvmftestfini; exit 1' SIGINT SIGTERM EXIT
+        #waitforlisten $nvmfpid
+        timing_exit start_nvmf_example
+}
+
+timing_enter nvmf_example_test
+nvmftestinit
+nvmfexamplestart "-m 0xF"
+
+#sleep 5
+###other test items
+
+#trap - SIGINT SIGTERM EXIT
+nvmftestfini
+timing_exit nvmf_example_test
