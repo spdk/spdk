0137f3cb9ec3c5e8ef9b6c6ec49c35a4a545294d
JinYu <jin.yu@intel.com>
2019-08-26 17:21:54 +0800

    nvmf_example: construct the nvmf target
    Construct nvmf target.
    
    Change-Id: I126d2f6b9f96840975fbc8fb833d0d7bc03b35fd
    Signed-off-by: JinYu <jin.yu@intel.com>

--- examples/nvmf/nvmf/nvmf.c
+++ examples/nvmf/nvmf/nvmf.c
@@ -37,6 +37,19 @@
 #include "spdk/string.h"
 #include "spdk/thread.h"
 #include "spdk/bdev.h"
+#include "spdk/nvmf.h"
+
+#include "spdk_internal/event.h"
+
+#define SPDK_NVMF_DEFAULT_NAMESPACES	32
+#define ACCEPT_TIMEOUT_US		10000 /* 10ms */
+#define DEFAULT_CONN_SCHED		CONNECT_SCHED_ROUND_ROBIN
+
+enum spdk_nvmf_connect_sched {
+	CONNECT_SCHED_ROUND_ROBIN = 0,
+	CONNECT_SCHED_HOST_IP,
+	CONNECT_SCHED_TRANSPORT_OPTIMAL_GROUP,
+};
 
 #include "spdk_internal/event.h"
 
@@ -53,8 +66,20 @@ struct nvmf_thread {
 	TAILQ_HEAD(, spdk_lw_thread)	threads;
 	TAILQ_ENTRY(nvmf_thread)	link;
 };
+
+struct nvmf_target {
+	struct spdk_nvmf_tgt	*tgt;
+
+	struct target_opts {
+		int max_subsystems;
+		int acceptor_poll_rate;
+		int conn_sched;
+	} tgt_params;
+};
+
 TAILQ_HEAD(, nvmf_thread) g_threads = TAILQ_HEAD_INITIALIZER(g_threads);
 
+static struct nvmf_target *g_nvmf_tgt = NULL;
 static struct nvmf_thread *g_master_thread = NULL;
 static struct nvmf_thread *g_next_thread = NULL;
 static struct spdk_thread *init_thread = NULL;
@@ -301,6 +326,100 @@ nvmf_bdev_fini_start(void)
 	}
 }
 
+static void
+nvmf_tgt_destroy_done(void *ctx, int status)
+{
+	*(bool *)ctx = true;
+}
+
+static void
+nvmf_destroy_nvmf_tgt(struct nvmf_target *nvmf_target)
+{
+	bool done = false;
+
+	if (!nvmf_target) {
+		return;
+	}
+
+	if (nvmf_target->tgt) {
+		spdk_nvmf_tgt_destroy(nvmf_target->tgt, nvmf_tgt_destroy_done, &done);
+
+		do {
+			spdk_thread_poll(init_thread, 0, 0);
+		} while (!done);
+	}
+
+	free(nvmf_target);
+
+	fprintf(stdout, "destroyed the nvmf target service\n");
+}
+
+static int
+nvmf_tgt_init(void)
+{
+	g_nvmf_tgt = calloc(1, sizeof(struct nvmf_target));
+	if (g_nvmf_tgt == NULL) {
+		fprintf(stderr, "fail to allocate g_nvmf_tgt\n");
+		return -ENOMEM;
+	}
+
+	/* set the default value */
+	g_nvmf_tgt->tgt_params.max_subsystems = SPDK_NVMF_DEFAULT_NAMESPACES;
+	g_nvmf_tgt->tgt_params.acceptor_poll_rate = ACCEPT_TIMEOUT_US;
+	g_nvmf_tgt->tgt_params.conn_sched = DEFAULT_CONN_SCHED;
+
+	return 0;
+}
+
+static int
+nvmf_tgt_add_discovery_subsystem(struct nvmf_target *nvmf_tgt)
+{
+	struct spdk_nvmf_subsystem *subsystem;
+
+	subsystem = spdk_nvmf_subsystem_create(nvmf_tgt->tgt, SPDK_NVMF_DISCOVERY_NQN,
+					       SPDK_NVMF_SUBTYPE_DISCOVERY, 0);
+	if (subsystem == NULL) {
+		fprintf(stderr, "failed to create discovery nvmf library subsystem\n");
+		return -EINVAL;
+	}
+
+	spdk_nvmf_subsystem_set_allow_any_host(subsystem, true);
+	return 0;
+}
+
+static int
+nvmf_create_nvmf_tgt(void)
+{
+	int rc;
+	struct spdk_nvmf_target_opts tgt_opts;
+
+	rc = nvmf_tgt_init();
+	if (rc) {
+		return rc;
+	}
+
+	tgt_opts.max_subsystems = g_nvmf_tgt->tgt_params.max_subsystems;
+	snprintf(tgt_opts.name, sizeof(tgt_opts.name), "%s", "nvmf_example");
+	g_nvmf_tgt->tgt = spdk_nvmf_tgt_create(&tgt_opts);
+	if (g_nvmf_tgt->tgt == NULL) {
+		fprintf(stderr, "spdk_nvmf_tgt_create() failed\n");
+		free(g_nvmf_tgt);
+		return -EINVAL;
+	}
+
+	/* create and add discovery subsystem */
+	rc = nvmf_tgt_add_discovery_subsystem(g_nvmf_tgt);
+	if (rc != 0) {
+		fprintf(stderr, "spdk_add_nvmf_discovery_subsystem() failed\n");
+		nvmf_destroy_nvmf_tgt(g_nvmf_tgt);
+		return rc;
+	}
+
+	fprintf(stdout, "created a nvmf target service\n");
+
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	int rc;
@@ -324,14 +443,27 @@ int main(int argc, char **argv)
 	if (rc != 0) {
 		fprintf(stderr, "failed to create initialization thread\n");
 		g_threads_exit = true;
+		nvmf_work_fn(g_master_thread);
+		goto exit;
 	}
 
 	/* Initialize the bdev layer */
 	nvmf_bdev_init_start();
 
-	nvmf_work_fn(g_master_thread);
-	nvmf_bdev_fini_start();
+	/* Initialize the nvmf tgt */
+	rc = nvmf_create_nvmf_tgt();
+	if (rc != 0) {
+		fprintf(stderr, "failed to create nvmf target\n");
+		nvmf_bdev_fini_start();
+		g_threads_exit = true;
+		nvmf_work_fn(g_master_thread);
+		goto exit;
+	}
 
+	nvmf_work_fn(g_master_thread);
+	nvmf_destroy_nvmf_tgt(g_nvmf_tgt);
+	nvmf_bdev_fini_start();
+exit:
 	spdk_env_thread_wait_all();
 	nvmf_destroy_threads();
 	return rc;
