33c5003b2c70fbe39c06792ca090aee3d2c91891
Konrad Sztyber <konrad.sztyber@intel.com>
2019-08-27 13:57:20 +0200

    zdev/ocssd: attach / detach call
    Added attach / detach RPC calls allowing for constructing / deleting
    zoned bdevs on top of Open Channel SSDs.  The calls are similar to their
    counterparts found in NVMe bdev - the user passes the address of the
    controller and devices are created for each available namespace.
    
    Change-Id: I31d271126dba4369ac2eaebd4cc7bdd460e5f808
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- scripts/rpc/bdev.py
+++ scripts/rpc/bdev.py
@@ -651,6 +651,32 @@ def delete_ftl_bdev(client, name):
     return client.call('delete_ftl_bdev', params)
 
 
+def zdev_ocssd_attach_controller(client, name, trtype, traddr):
+    """Creates Open Channel zdevs on specified Open Channel controller
+
+    Args:
+        name: name of the zdev
+        trtype: transport type
+        traddr: transport address
+    """
+    params = {'name': name,
+              'trtype': trtype,
+              'traddr': traddr}
+
+    return client.call('zdev_ocssd_attach_controller', params)
+
+
+def zdev_ocssd_detach_controller(client, name):
+    """Detaches Open Channel controller and deletes all bdevs created on top of it.
+
+    Args:
+        name: name of the controller
+    """
+    params = {'name': name}
+
+    return client.call('zdev_ocssd_detach_controller', params)
+
+
 def get_bdevs(client, name=None):
     """Get information about block devices.
 
