a4c44d52f3439756581e8ee1809cf4bfa0422811
Mateusz Kozlowski <mateusz.kozlowski@intel.com>
2019-08-27 12:10:52 +0200

    bdev/zone: Zone append handling added to vbdev
    Changed write function to be able to work in zone append mode.
    
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>
    Change-Id: I3af98f1d8e69eac6922e19addebabbc6ce847c63

--- module/bdev/zone_block/vbdev_zone_block.c
+++ module/bdev/zone_block/vbdev_zone_block.c
@@ -100,6 +100,8 @@ struct zone_block_io {
 	struct bdev_zone_block *bdev_zone_block;
 	/* zone IO was issued to */
 	struct block_zone *zone;
+	/* Offset within the zone the IO was sent to */
+	uint64_t offset;
 };
 
 static int
@@ -439,6 +441,9 @@ _zone_block_complete_write(struct spdk_bdev_io *bdev_io, bool success, void *cb_
 		if (zone->zone_info.write_pointer == zone->zone_info.zone_id + zone->zone_info.capacity) {
 			zone->zone_info.state = SPDK_BDEV_ZONE_STATE_FULL;
 		}
+		if (orig_io->type == SPDK_BDEV_IO_TYPE_ZONE_APPEND) {
+			orig_io->u.bdev.offset_blocks = zone->zone_info.zone_id + io_ctx->offset;
+		}
 	}
 
 	__atomic_store_n(&zone->busy, false, __ATOMIC_SEQ_CST);
@@ -463,8 +468,13 @@ zone_block_write(struct bdev_zone_block *bdev_node, struct zone_block_io_channel
 	uint64_t offset = 0;
 	int rc = 0;
 	bool busy;
+	bool is_append = bdev_io->type == SPDK_BDEV_IO_TYPE_ZONE_APPEND;
 
-	zone = zone_block_get_zone_containing_lba(bdev_node, lba, &offset);
+	if (is_append) {
+		zone = zone_block_get_zone_by_slba(bdev_node, lba);
+	} else {
+		zone = zone_block_get_zone_containing_lba(bdev_node, lba, &offset);
+	}
 	if (!zone) {
 		SPDK_ERRLOG("Trying to write to invalid zone (lba 0x%lx)\n", lba);
 		return -EINVAL;
@@ -490,6 +500,10 @@ zone_block_write(struct bdev_zone_block *bdev_node, struct zone_block_io_channel
 	}
 
 	wp = zone->zone_info.write_pointer;
+	if (is_append) {
+		offset = wp - lba;
+		lba = wp;
+	}
 
 	if (lba != wp) {
 		SPDK_ERRLOG("Trying to write to zone with invalid address (lba 0x%lx, wp 0x%lx)\n", lba, wp);
@@ -497,6 +511,11 @@ zone_block_write(struct bdev_zone_block *bdev_node, struct zone_block_io_channel
 		goto write_fail;
 	}
 
+	if (!is_append) {
+		lba = zone->physical_slba + offset;
+	}
+
+	io_ctx->offset = offset;
 	num_blocks_left = zone->zone_info.zone_id + zone->zone_info.capacity - wp;
 	if (len > num_blocks_left) {
 		SPDK_ERRLOG("Write exceeds zone capacity (lba 0x%lx, len 0x%lx, wp 0x%lx)\n", lba, len, wp);
@@ -513,14 +532,14 @@ zone_block_write(struct bdev_zone_block *bdev_node, struct zone_block_io_channel
 
 	if (bdev_io->u.bdev.md_buf == NULL) {
 		rc = spdk_bdev_writev_blocks(bdev_node->base_desc, ch->base_ch, bdev_io->u.bdev.iovs,
-					     bdev_io->u.bdev.iovcnt, zone->physical_slba + offset,
+					     bdev_io->u.bdev.iovcnt, lba,
 					     bdev_io->u.bdev.num_blocks, _zone_block_complete_write,
 					     bdev_io);
 	} else {
 		rc = spdk_bdev_writev_blocks_with_md(bdev_node->base_desc, ch->base_ch,
 						     bdev_io->u.bdev.iovs, bdev_io->u.bdev.iovcnt,
 						     bdev_io->u.bdev.md_buf,
-						     zone->physical_slba + offset,
+						     lba,
 						     bdev_io->u.bdev.num_blocks,
 						     _zone_block_complete_write, bdev_io);
 	}
@@ -609,11 +628,12 @@ zone_block_submit_request(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_
 	case SPDK_BDEV_IO_TYPE_UNMAP:
 		rc = zone_block_unmap(bdev_node, dev_ch, bdev_io);
 		break;
-	case SPDK_BDEV_IO_TYPE_WRITE:
-		rc = zone_block_write(bdev_node, dev_ch, bdev_io);
-		break;
 	case SPDK_BDEV_IO_TYPE_READ:
 		rc = zone_block_read(bdev_node, dev_ch, bdev_io);
+		break;
+	case SPDK_BDEV_IO_TYPE_WRITE:
+	case SPDK_BDEV_IO_TYPE_ZONE_APPEND:
+		rc = zone_block_write(bdev_node, dev_ch, bdev_io);
 		break;
 	default:
 		SPDK_ERRLOG("vbdev_block: unknown I/O type %d\n", bdev_io->type);
@@ -640,6 +660,7 @@ zone_block_io_type_supported(void *ctx, enum spdk_bdev_io_type io_type)
 	case SPDK_BDEV_IO_TYPE_UNMAP:
 	case SPDK_BDEV_IO_TYPE_WRITE:
 	case SPDK_BDEV_IO_TYPE_READ:
+	case SPDK_BDEV_IO_TYPE_ZONE_APPEND:
 		return true;
 	default:
 		return false;
