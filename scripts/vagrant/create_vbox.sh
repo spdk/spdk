7b7e942917d9c35bc488aaf945ca8704251e0f7a
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-08-28 16:08:21 +0200

    scripts/vagrant: Add more NVMe emulation options
    Current scripts/vagrant allow us to create only a single file for
    emulating NVMe with single namespace.
    This patch allow to create multiple emulated drives with multiple namespaces.
    This patch also allows to emulate OCSSD disks which are used in OCF tests.
    
    Change-Id: Ia7a4667bd405ba18235e72bcdf608270002b5242
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>

--- scripts/vagrant/create_vbox.sh
+++ scripts/vagrant/create_vbox.sh
@@ -19,7 +19,11 @@ display_help() {
 	echo
 	echo "  distro = <centos7 | ubuntu16 | ubuntu18 | fedora28 | fedora29 | fedora 30 | freebsd11> "
 	echo
-	echo "  -b <nvme-backing-file>          default: ${NVME_FILE}"
+	echo "  -b <nvme-backing-file>          nvme file path with name"
+	echo "                                  type of emulated nvme disk"
+	echo "                                  usage: type <number_of_namespaces> types available: ocssd, nvme"
+	echo "                                  If no -b option is specified then this option defaults to emulating single"
+	echo "                                  NVMe with 1 namespace and assumes path: /var/lib/libvirt/images/nvme_disk.img"
 	echo "  -s <ram-size> in kb             default: ${SPDK_VAGRANT_VMRAM}"
 	echo "  -n <num-cpus> 1 to 4            default: ${SPDK_VAGRANT_VMCPU}"
 	echo "  -x <http-proxy>                 default: \"${SPDK_VAGRANT_HTTP_PROXY}\""
@@ -39,7 +43,10 @@ display_help() {
 	echo "  $0 -x http://user:password@host:port fedora28"
 	echo "  $0 -s 2048 -n 2 ubuntu16"
 	echo "  $0 -rv freebsd"
-	echo "  $0 fedora28 "
+	echo "  $0 fedora28"
+	echo "  $0 -b /var/lib/libvirt/images/nvme1.img,nvme,1 fedora30"
+	echo "  $0 -b /var/lib/libvirt/images/ocssd.img,ocssd fedora30"
+	echo "  $0 -b /var/lib/libvirt/images/nvme5.img,nvme,5 -b /var/lib/libvirt/images/ocssd.img,ocssd fedora30"
 	echo
 }
 
@@ -58,7 +65,9 @@ SPDK_VAGRANT_VMRAM=4096
 SPDK_VAGRANT_PROVIDER="virtualbox"
 SPDK_QEMU_EMULATOR=""
 OPTIND=1
-NVME_FILE="nvme_disk.img"
+NVME_DISKS_TYPE=""
+NVME_DISKS_NAMESPACES=""
+NVME_FILE=""
 
 while getopts ":b:n:s:x:p:vrldh-:" opt; do
 	case "${opt}" in
@@ -101,7 +110,7 @@ while getopts ":b:n:s:x:p:vrldh-:" opt; do
 			DEPLOY_TEST_VM=1
 		;;
 		b)
-			NVME_FILE=$OPTARG
+			NVME_FILE+="${OPTARG#*=} "
 		;;
 		*)
 			echo "  Invalid argument: -$OPTARG" >&2
@@ -151,6 +160,27 @@ if ! echo "$SPDK_VAGRANT_DISTRO" | grep -q fedora && [ $DEPLOY_TEST_VM -eq 1 ]; 
 	echo "Warning: Test machine deployment is only available on fedora distros. Disabling it for this build"
 	DEPLOY_TEST_VM=0
 fi
+if [ -z "$NVME_FILE" ]; then
+	TMP="/var/lib/libvirt/images/nvme_disk.img"
+	NVME_DISKS_TYPE="nvme"
+	NVME_DISKS_NAMESPACES="1"
+else
+	TMP=""
+	for args in $NVME_FILE; do
+		while IFS=, read -r path type namespace; do
+			TMP+="$path,";
+			if [ -z "$type" ]; then
+				type="nvme"
+			fi
+			NVME_DISKS_TYPE+="$type,";
+			if [ -z "$namespace" ]; then
+				namespace="1"
+			fi
+			NVME_DISKS_NAMESPACES+="$namespace,";
+		done <<< $args
+	done
+fi
+NVME_FILE=$TMP
 
 if [ ${VERBOSE} = 1 ]; then
 	echo
@@ -160,6 +190,8 @@ if [ ${VERBOSE} = 1 ]; then
 	echo HELP=$HELP
 	echo DRY_RUN=$DRY_RUN
 	echo NVME_FILE=$NVME_FILE
+	echo NVME_DISKS_TYPE=$NVME_DISKS_TYPE
+	echo NVME_DISKS_NAMESPACES=$NVME_DISKS_NAMESPACES
 	echo SPDK_VAGRANT_DISTRO=$SPDK_VAGRANT_DISTRO
 	echo SPDK_VAGRANT_VMCPU=$SPDK_VAGRANT_VMCPU
 	echo SPDK_VAGRANT_VMRAM=$SPDK_VAGRANT_VMRAM
@@ -177,6 +209,8 @@ export SPDK_VAGRANT_VMRAM
 export SPDK_DIR
 export COPY_SPDK_DIR
 export DEPLOY_TEST_VM
+export NVME_DISKS_TYPE
+export NVME_DISKS_NAMESPACES
 export NVME_FILE
 
 if [ -n "$SPDK_VAGRANT_PROVIDER" ]; then
@@ -207,6 +241,9 @@ if [ ${DRY_RUN} = 1 ]; then
 	printenv SPDK_VAGRANT_PROVIDER
 	printenv SPDK_VAGRANT_HTTP_PROXY
 	printenv SPDK_QEMU_EMULATOR
+	printenv NVME_DISKS_TYPE
+	printenv NVME_DISKS_NAMESPACES
+	printenv NVME_FILE
 	printenv SPDK_DIR
 fi
 
