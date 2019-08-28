5d6cc9b72fea1ef234cac3fa03cb16ee81184b6f
Xiaodong Liu <xiaodong.liu@intel.com>
2019-08-28 16:06:50 +0800

    blobfs: add make_bdev_blobfs RPC as mkfs
    Following test/blobfs/mkfs case, add it as one RPC
    method to let build a new blobfs on given block
    device.
    
    Change-Id: I0ffbb1add95dfbc8655e0238ed6f3cd519dd945b
    Signed-off-by: Xiaodong Liu <xiaodong.liu@intel.com>

--- lib/blobfs/blobfs_rpc.c
+++ lib/blobfs/blobfs_rpc.c
@@ -169,3 +169,135 @@ invalid:
 }
 
 SPDK_RPC_REGISTER("check_bdev_blobfs", spdk_rpc_check_bdev_blobfs, SPDK_RPC_RUNTIME)
+
+struct rpc_make_bdev_blobfs {
+	char *bdev_name;
+	uint32_t	cluster_sz;
+	bool	force;
+
+	/* Used to search one available nbd device */
+	struct spdk_filesystem *fs;
+	struct spdk_jsonrpc_request *request;
+};
+
+static void
+free_rpc_make_bdev_blobfs(struct rpc_make_bdev_blobfs *req)
+{
+	free(req->bdev_name);
+}
+
+static const struct spdk_json_object_decoder rpc_make_bdev_blobfs_decoders[] = {
+	{"bdev_name", offsetof(struct rpc_make_bdev_blobfs, bdev_name), spdk_json_decode_string},
+	{"cluster_sz", offsetof(struct rpc_make_bdev_blobfs, cluster_sz), spdk_json_decode_uint32, true},
+	{"force", offsetof(struct rpc_make_bdev_blobfs, force), spdk_json_decode_bool, true},
+};
+
+static void
+_make_bdev_blobfs_unload_cb(void *ctx, int fserrno)
+{
+	struct rpc_make_bdev_blobfs *req = ctx;
+	struct spdk_jsonrpc_request *request = req->request;
+	struct spdk_json_write_ctx *w;
+
+	if (fserrno) {
+		SPDK_ERRLOG("Failed to unload blobfs on bdev %s: errno %d\n", req->bdev_name, fserrno);
+		spdk_jsonrpc_send_error_response(req->request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						 "blobfs unload failed");
+
+		free_rpc_make_bdev_blobfs(req);
+		return;
+	}
+
+	w = spdk_jsonrpc_begin_result(request);
+	spdk_json_write_bool(w, true);
+	spdk_jsonrpc_end_result(request, w);
+
+	free_rpc_make_bdev_blobfs(req);
+}
+
+static void
+_make_bdev_blobfs_unload(void *arg1, void *arg2)
+{
+	struct rpc_make_bdev_blobfs *req = arg1;
+
+	spdk_fs_unload(req->fs, _make_bdev_blobfs_unload_cb, req);
+}
+
+static void
+_make_bdev_blobfs_init_cb(void *ctx, struct spdk_filesystem *fs, int fserrno)
+{
+	struct spdk_event *event;
+	struct rpc_make_bdev_blobfs *req = ctx;
+
+	if (fserrno) {
+		SPDK_ERRLOG("Failed to init blobfs on bdev %s: errno %d", req->bdev_name, fserrno);
+		spdk_jsonrpc_send_error_response(req->request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						 "blobfs init failed");
+
+		free_rpc_make_bdev_blobfs(req);
+		return;
+	}
+
+	req->fs = fs;
+
+	event = spdk_event_allocate(0, _make_bdev_blobfs_unload, req, NULL);
+	spdk_event_call(event);
+}
+
+static void
+spdk_rpc_make_bdev_blobfs(struct spdk_jsonrpc_request *request,
+			  const struct spdk_json_val *params)
+{
+	struct rpc_make_bdev_blobfs *req;
+	struct spdk_bdev *bdev;
+	struct spdk_blobfs_opts blobfs_opt;
+	struct spdk_bs_dev *bs_dev;
+
+	req = calloc(1, sizeof(*req));
+	if (req == NULL) {
+		SPDK_ERRLOG("could not allocate rpc_make_bdev_blobfs request.\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR, "Out of memory");
+		return;
+	}
+
+	if (spdk_json_decode_object(params, rpc_make_bdev_blobfs_decoders,
+				    SPDK_COUNTOF(rpc_make_bdev_blobfs_decoders),
+				    req)) {
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						 "spdk_json_decode_object failed");
+		goto invalid;
+	}
+
+	if (req->bdev_name == NULL) {
+		goto invalid;
+	}
+
+	bdev = spdk_bdev_get_by_name(req->bdev_name);
+	if (bdev == NULL) {
+		SPDK_INFOLOG(SPDK_LOG_BLOBFS, "bdev %s not found\n", req->bdev_name);
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "Specified bdev doesn't exist");
+		goto invalid;
+	}
+
+	spdk_fs_opts_init(&blobfs_opt);
+	if (req->cluster_sz) {
+		blobfs_opt.cluster_sz = req->cluster_sz;
+	}
+
+	req->request = request;
+	bs_dev = spdk_bdev_create_bs_dev(bdev, NULL, NULL);
+	if (blobfs_opt.cluster_sz) {
+		spdk_fs_init(bs_dev, &blobfs_opt, NULL, _make_bdev_blobfs_init_cb, req);
+	} else {
+		spdk_fs_init(bs_dev, NULL, NULL, _make_bdev_blobfs_init_cb, req);
+	}
+
+	return;
+
+invalid:
+	free_rpc_make_bdev_blobfs(req);
+}
+
+SPDK_RPC_REGISTER("make_bdev_blobfs", spdk_rpc_make_bdev_blobfs, SPDK_RPC_RUNTIME)
--- scripts/rpc/blobfs.py
+++ scripts/rpc/blobfs.py
@@ -7,3 +7,20 @@ def check_bdev_blobfs(client, bdev_name):
         'bdev_name': bdev_name
     }
     return client.call('check_bdev_blobfs', params)
+
+
+def make_bdev_blobfs(client, bdev_name, cluster_sz=None, force=None):
+    """Build blobfs on bdev.
+    Args:
+    bdev_name: block device name to build blobfs
+    cluster_sz: Size of cluster in bytes. Must be multiple of 4KiB page size
+    force: Build new blobfs even one blobfs is already existed
+    """
+    params = {
+        'bdev_name': bdev_name
+    }
+    if cluster_sz:
+        params['cluster_sz'] = cluster_sz
+    if force:
+        params['force'] = force
+    return client.call('make_bdev_blobfs', params)
