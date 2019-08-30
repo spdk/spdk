09a9238c1de1cfc99e9d3142deacdbc39ef82111
Karol Latecki <karol.latecki@intel.com>
2019-08-30 16:12:54 +0200

    [RFC][WiP] scripts/vm_setup: add Arch Linux test dependencies install
    Change-Id: Ifde26c1f1d35c8fe06e18ddba33fe2d054b6d2cf
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

---
+++ test/common/config/vm_setup_arch.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+        # librbd-devel \ ---------
+        # pmempool \ -----------------
+        # perl-open \ -------------
+        # targetcli \ ----------------
+        # librdmacm-utils \ ---------------
+        # libibverbs-utils \ -------------
+        # rpm-build \ ----------------
+        # iptables \
+        # clang-analyzer \
+        # bc
+sudo pacman -Sy --noconfirm --needed valgrind \
+    jq \
+    nvme-cli \
+    ceph \
+    gdb \
+    fio \
+    linux-headers \
+    gflags \
+    autoconf \
+    automake \
+    lib-tool \
+    libutil-linux \
+    libiscsi \
+    open-isns \
+    glib2 \
+    pixman \
+    flex \
+    bison \
+    elfutils \
+    libelf \
+    astyle \
+    gptfdisk \
+    socat \
+    sshfs \
+    sshpass \
+    python-pandas \
+    btrfs-progs \
+    iptables \
+    clang \
+    bc \
+    perl-switch
+
+
+
+
