15914a760555c0d3fc0894ff1c821c75c0b91798
Changpeng Liu <changpeng.liu@intel.com>
2019-09-02 23:48:49 -0400

    bdev/nvme: add arbitration configuration parameters to NVMe controller
    Change-Id: I9c69797670dbe652ee3f9dbe21125e9b46a96dda
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>

--- module/bdev/nvme/bdev_nvme.h
+++ module/bdev/nvme/bdev_nvme.h
@@ -50,6 +50,10 @@ struct spdk_bdev_nvme_opts {
 	enum spdk_bdev_timeout_action action_on_timeout;
 	uint64_t timeout_us;
 	uint32_t retry_count;
+	uint32_t arbitration_burst;
+	uint32_t low_priority_weight;
+	uint32_t medium_priority_weight;
+	uint32_t high_priority_weight;
 	uint64_t nvme_adminq_poll_period_us;
 	uint64_t nvme_ioq_poll_period_us;
 	uint32_t io_queue_requests;
