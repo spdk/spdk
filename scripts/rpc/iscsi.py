8fa589aa953eaf50074dbd9947549bfb8b060376
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-09-03 05:59:23 -0400

    rpc: Rename add_initiators_to_initiator_group to iscsi_initiator_group_add_initiators
    Change-Id: I776f52a5e15c1cb7949fffc76196634b37b5274b
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- scripts/rpc/iscsi.py
+++ scripts/rpc/iscsi.py
@@ -1,3 +1,4 @@
+from .helpers import deprecated_alias
 
 
 def set_iscsi_options(
@@ -387,7 +388,8 @@ def add_initiator_group(client, tag, initiators, netmasks):
     return client.call('add_initiator_group', params)
 
 
-def add_initiators_to_initiator_group(
+@deprecated_alias('add_initiators_to_initiator_group')
+def iscsi_initiator_group_add_initiators(
         client,
         tag,
         initiators=None,
@@ -408,7 +410,7 @@ def add_initiators_to_initiator_group(
         params['initiators'] = initiators
     if netmasks:
         params['netmasks'] = netmasks
-    return client.call('add_initiators_to_initiator_group', params)
+    return client.call('iscsi_initiator_group_add_initiators', params)
 
 
 def delete_initiators_from_initiator_group(
