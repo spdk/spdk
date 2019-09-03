1da669b81d3fdf1452de51aba46f73d5c7f17dff
jiaqizho <jiaqi.zhou@intel.com>
2019-09-03 14:31:29 +0800

    [bdev] new merge bdev
    - add merge bdev in module/bdev/merge
    - add some bdevperf file for o+t demo test
    - add fix test mode in bdev_merge.c
    - add a function to show I/O statistic of merge bdev bdefore destructing
    - add some conf files for merge bdev test
    - waiting for test
    
    Change-Id: I822723c772aa067651b0d9fb312e3846ccf73b43
    Signed-off-by: jiaqizho <jiaqi.zhou@intel.com>
    Signed-off-by: Liu.Lanfeng <lanfeng.liu@intel.com>

--- mk/spdk.modules.mk
+++ mk/spdk.modules.mk
@@ -32,7 +32,7 @@
 #
 
 BLOCKDEV_MODULES_LIST = bdev_malloc bdev_null bdev_nvme bdev_passthru bdev_lvol
-BLOCKDEV_MODULES_LIST += bdev_raid bdev_error bdev_gpt bdev_split bdev_delay
+BLOCKDEV_MODULES_LIST += bdev_raid bdev_error bdev_gpt bdev_split bdev_delay 
 BLOCKDEV_MODULES_LIST += blobfs blob_bdev blob lvol vmd nvme
 
 ifeq ($(CONFIG_CRYPTO),y)
