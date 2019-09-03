6b2c9e6985eeef964b6c1ebb40e19568a77db03e
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-09-03 04:53:37 -0400

    rpc: Rename add_initiator_group to iscsi_create_initiator_group
    Change-Id: I5ea7daff795340818251aeac2cc21b4b8a7457c6
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/iscsi_tgt/initiator/initiator.sh
+++ test/iscsi_tgt/initiator/initiator.sh
@@ -30,7 +30,7 @@ echo "iscsi_tgt is listening. Running tests..."
 timing_exit start_iscsi_tgt
 
 $rpc_py iscsi_create_portal_group $PORTAL_TAG $TARGET_IP:$ISCSI_PORT
-$rpc_py add_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
+$rpc_py iscsi_create_initiator_group $INITIATOR_TAG $INITIATOR_NAME $NETMASK
 $rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE
 # "Malloc0:0" ==> use Malloc0 blockdev for LUN0
 # "1:2" ==> map PortalGroup1 to InitiatorGroup2
