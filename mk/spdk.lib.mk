8868aaf76e7a5417481f77b7c7372ba3cd65ad8c
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-09-09 10:09:00 -0400

    lib/ftl: Moving FTL to work on top of zoned bdev
    This patch replaces OC SSD as underlaying device
    for FTL library with abstracted zoned device.
    
    Change-Id: I0b8ff08c61793d7f1685942031aecf9cc88d1f24
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- mk/spdk.lib.mk
+++ mk/spdk.lib.mk
@@ -32,7 +32,7 @@
 #
 
 include $(SPDK_ROOT_DIR)/mk/spdk.common.mk
-include $(SPDK_ROOT_DIR)/mk/spdk.lib_deps.mk
+
 
 SPDK_MAP_FILE = $(SPDK_ROOT_DIR)/shared_lib/spdk.map
 LIB := $(call spdk_lib_list_to_static_libs,$(LIBNAME))
