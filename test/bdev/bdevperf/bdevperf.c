35a830f4a716264d2ebf2e193f14ddda98c588de
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-09-09 14:40:21 +0900

    bdevperf: Factor out write task preparation into a function
    This is a preparation to use zero copy bdev I/O APIs for write I/O.
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: Id475bf6305ff6d6f3eb5658f3ab0fe11f588f72e

--- test/bdev/bdevperf/bdevperf.c
+++ test/bdev/bdevperf/bdevperf.c
@@ -551,6 +551,22 @@ bdevperf_zcopy_complete(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg
 static __thread unsigned int seed = 0;
 
 static void
+bdevperf_prep_write_task(struct bdevperf_task *task)
+{
+	struct io_target *target = task->target;
+
+	if (g_verify || g_reset) {
+		generate_data(task->buf, g_buf_size,
+			      spdk_bdev_get_block_size(target->bdev),
+			      task->md_buf, spdk_bdev_get_md_size(target->bdev),
+			      target->io_size_blocks, rand_r(&seed) % 256);
+	}
+	task->iov.iov_base = task->buf;
+	task->iov.iov_len = g_buf_size;
+	task->io_type = SPDK_BDEV_IO_TYPE_WRITE;
+}
+
+static void
 bdevperf_prep_task(struct bdevperf_task *task)
 {
 	struct io_target *target = task->target;
@@ -576,15 +592,7 @@ bdevperf_prep_task(struct bdevperf_task *task)
 		   (g_rw_percentage != 0 && ((rand_r(&seed) % 100) < g_rw_percentage))) {
 		task->io_type = SPDK_BDEV_IO_TYPE_READ;
 	} else {
-		if (g_verify || g_reset) {
-			generate_data(task->buf, g_buf_size,
-				      spdk_bdev_get_block_size(target->bdev),
-				      task->md_buf, spdk_bdev_get_md_size(target->bdev),
-				      target->io_size_blocks, rand_r(&seed) % 256);
-		}
-		task->iov.iov_base = task->buf;
-		task->iov.iov_len = g_buf_size;
-		task->io_type = SPDK_BDEV_IO_TYPE_WRITE;
+		bdevperf_prep_write_task(task);
 	}
 }
 
