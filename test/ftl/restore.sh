efc2b92df6b8a3b012bbfda5435d511c6c2e1318
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-09-09 10:09:00 -0400

    lib/ftl: Moving FTL to work on top of zoned bdev
    This patch replaces OC SSD as underlaying device
    for FTL library with abstracted zoned device.
    
    Change-Id: I0b8ff08c61793d7f1685942031aecf9cc88d1f24
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- test/ftl/restore.sh
+++ test/ftl/restore.sh
@@ -31,7 +31,10 @@ restore_kill() {
 	rm -f $testdir/testfile2.md5
 	rm -f $testdir/config/ftl.json
 
-	$rpc_py delete_ftl_bdev -b nvme0
+	$rpc_py delete_ftl_bdev -b ftl0
+	$rpc_py bdev_ocssd_delete nvme0n1
+	$rpc_py delete_nvme_controller nvme0
+
 	killprocess $svcpid
 	rmmod nbd || true
 }
@@ -46,7 +49,9 @@ if [ -n "$nv_cache" ]; then
 	nvc_bdev=$(create_nv_cache_bdev nvc0 $device $nv_cache $pu_count)
 fi
 
-ftl_construct_args="construct_ftl_bdev -b nvme0 -a $device"
+$rpc_py construct_nvme_bdev -b nvme0 -a $device -m ocssd -t pcie
+$rpc_py bdev_ocssd_create -c nvme0 -b nvme0n1 -n 1
+ftl_construct_args="construct_ftl_bdev -b ftl0 -d nvme0n1"
 
 [ -n "$uuid" ]     && ftl_construct_args+=" -u $uuid"
 [ -n "$nv_cache" ] && ftl_construct_args+=" -c $nvc_bdev"
@@ -55,7 +60,7 @@ $rpc_py $ftl_construct_args
 
 # Load the nbd driver
 modprobe nbd
-$rpc_py start_nbd_disk nvme0 /dev/nbd0
+$rpc_py start_nbd_disk ftl0 /dev/nbd0
 waitfornbd nbd0
 
 $rpc_py save_config > $testdir/config/ftl.json
