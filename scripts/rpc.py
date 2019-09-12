b0d8ff51509914fe8dca2488afd3fd592b9a484a
Chunyang Hui <chunyang.hui@intel.com>
2019-09-12 17:40:38 +0800

    opal: Add rpc for create and delete opal bdev
    Change-Id: Ic2cbe079aa16d839b92fc4d0b3ee64c061fa2d3e
    Signed-off-by: Chunyang Hui <chunyang.hui@intel.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -2013,6 +2013,35 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
     domain:bus:device.function format or domain.bus.device.function format""")
     p.set_defaults(func=ioat_scan_copy_engine)
 
+    # opal
+    def bdev_opal_create(args):
+        print_json(rpc.bdev.bdev_opal_create(args.client,
+                                             nvme_ctrlr_name=args.nvme_ctrlr_name,
+                                             nsid=args.nsid,
+                                             locking_range_id=args.locking_range_id,
+                                             range_start=args.range_start,
+                                             range_length=args.range_length,
+                                             password=args.password))
+
+    p = subparsers.add_parser('bdev_opal_create', help="""Create opal bdev on specified NVMe controller""")
+    p.add_argument('-b', '--nvme_ctrlr_name', help='nvme ctrlr name')
+    p.add_argument('-n', '--nsid', help='namespace ID (only support nsid=1 for now)', type=int)
+    p.add_argument('-i', '--locking_range_id', help='locking range id', type=int)
+    p.add_argument('-s', '--range_start', help='locking range start LBA', type=int)
+    p.add_argument('-l', '--range_length', help='locking range length (in blocks)', type=int)
+    p.add_argument('-p', '--password', help='admin password')
+    p.set_defaults(func=bdev_opal_create)
+
+    def bdev_opal_delete(args):
+        rpc.bdev.bdev_opal_delete(args.client,
+                                  bdev_name=args.bdev_name,
+                                  password=args.password)
+
+    p = subparsers.add_parser('bdev_opal_delete', help="""delete a virtual opal bdev""")
+    p.add_argument('-b', '--bdev_name', help='opal virtual bdev')
+    p.add_argument('-p', '--password', help='admin password')
+    p.set_defaults(func=bdev_opal_delete)
+
     # bdev_nvme_send_cmd
     def bdev_nvme_send_cmd(args):
         print_dict(rpc.nvme.bdev_nvme_send_cmd(args.client,
