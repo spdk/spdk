de659c66bc648b6ca3b0fc3fa1637eeeaa9d38ca
Chunyang Hui <chunyang.hui@intel.com>
2019-09-12 17:41:44 +0800

    opal: Add test scripts for opal vbdev
    Change-Id: I9b2a2d00efc0a6a2d96b7b2240884f7afd770dbe
    Signed-off-by: Chunyang Hui <chunyang.hui@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -258,6 +258,10 @@ if [ $SPDK_RUN_FUNCTIONAL_TEST -eq 1 ]; then
         if [ $SPDK_TEST_REDUCE -eq 1 ]; then
                 run_test suite ./test/compress/compress.sh
         fi
+
+	if [ $SPDK_TEST_OPAL -eq 1 ]; then
+		run_test suite ./test/nvme/nvme_opal.sh
+	fi
 fi
 
 timing_enter cleanup
