6956b0ec1e6b1ba6c93f6d22fc0d5a6216ab79d5
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-09-16 11:23:41 +0200

    test: Shellcheck - correct rule "backslash+linefeed is literal"
    Correct shellcheck rule SC1004: This backslash+linefeed is literal.
    Break outside single quotes if you just want to break the line.
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: I7b872fa3c66685c1e8e7989f7b3b74a9e4f35325
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/468467
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

--- test/iscsi_tgt/ext4test/ext4test.sh
+++ test/iscsi_tgt/ext4test/ext4test.sh
@@ -44,7 +44,7 @@ iscsiadm -m discovery -t sendtargets -p $TARGET_IP:$ISCSI_PORT
 iscsiadm -m node --login -p $TARGET_IP:$ISCSI_PORT
 waitforiscsidevices 1
 
-trap 'for new_dir in $(dir -d /mnt/*dir); do umount $new_dir; rm -rf $new_dir; done; \
+trap 'for new_dir in $(dir -d /mnt/*dir); do umount $new_dir; rm -rf $new_dir; done;
 	iscsicleanup; killprocess $pid; iscsitestfini $1 $2; exit 1' SIGINT SIGTERM EXIT
 
 echo "Test error injection"
