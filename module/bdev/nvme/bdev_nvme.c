b70679275478d962eb132c0ae60acb56e25b7f1f
Maciej Szwed <maciej.szwed@intel.com>
2019-09-16 10:34:07 +0200

    bdev/nvme: Do not use the same pointer in rpc and bdev code
    Due to upcoming change we cannot use the same count
    pointer in rpc call and bdev creation function.
    With async bdev creation there will be a problem
    when freeing context.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I98da89481d7f506161d8adf5a1b2365907385a13

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -1217,17 +1217,17 @@ bdev_nvme_create_bdevs(struct nvme_async_probe_ctx *ctx)
 			continue;
 		}
 		assert(nvme_bdev->id == nsid);
-		if (j < *ctx->count) {
+		if (j < ctx->count) {
 			ctx->names[j] = nvme_bdev->disk.name;
 			j++;
 		} else {
 			SPDK_ERRLOG("Maximum number of namespaces supported per NVMe controller is %zu. Unable to return all names of created bdevs\n",
-				    *ctx->count);
+				    ctx->count);
 			return -1;
 		}
 	}
 
-	*ctx->count = j;
+	ctx->count = j;
 
 	return 0;
 }
@@ -1269,7 +1269,7 @@ connect_attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 
 end:
 	if (ctx->cb_fn) {
-		ctx->cb_fn(ctx->cb_ctx, rc);
+		ctx->cb_fn(ctx->cb_ctx, ctx->count, rc);
 	}
 }
 
@@ -1293,7 +1293,7 @@ int
 spdk_bdev_nvme_create(struct spdk_nvme_transport_id *trid,
 		      struct spdk_nvme_host_id *hostid,
 		      const char *base_name,
-		      const char **names, size_t *count,
+		      const char **names, size_t count,
 		      const char *hostnqn,
 		      uint32_t prchk_flags,
 		      spdk_bdev_create_nvme_fn cb_fn,
