9dc3531e4060f4e2ce55b818121064f956d8f33e
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-09-16 17:04:39 +0200

    [WIP] lib/nvme: add nvme character device
    This implementation is strongly based on Jim Harris RFC cuse example
    available here: https://review.gerrithub.io/c/spdk/spdk/+/463558
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>
    Change-Id: I0fc9a9a1ef3c9c2b3112d07c2b4b1f8d49665ee1

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -34,6 +34,11 @@
 #include "nvme_internal.h"
 #include "spdk/nvme_ocssd.h"
 
+#ifdef SPDK_CONFIG_CUSE
+/* FIXIT: This include is for testing purposes */
+#include "nvme_cuse.h"
+#endif
+
 static void nvme_qpair_abort_reqs(struct spdk_nvme_qpair *qpair, uint32_t dnr);
 
 struct nvme_string {
@@ -453,7 +458,14 @@ spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_
 		SPDK_ERRLOG("CQ error, abort requests after transport retry counter exceeded\n");
 		qpair->ctrlr->is_failed = true;
 	}
+
 	qpair->in_completion_context = 0;
+
+#ifdef SPDK_CONFIG_CUSE
+	/* FIXIT: this is for testing purposes */
+	spdk_nvme_io_msg_process();
+#endif
+
 	if (qpair->delete_after_completion_context) {
 		/*
 		 * A request to delete this qpair was made in the context of this completion
