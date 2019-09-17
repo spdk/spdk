27edf150ff792df6745a9cf5aa3de82a162f695a
JinYu <jin.yu@intel.com>
2019-09-18 02:42:52 +0800

    nvmf_example: add the acceptor poller
    Add acceptor poller to handle all the connections.
    
    Change-Id: I3228547d63a5812b90fe4394641cf124d9e64d12
    Signed-off-by: JinYu <jin.yu@intel.com>

--- test/nvmf/target/nvmf_example.sh
+++ test/nvmf/target/nvmf_example.sh
@@ -7,6 +7,9 @@ source $rootdir/test/nvmf/common.sh
 
 rpc_py="$rootdir/scripts/rpc.py"
 
+MALLOC_BDEV_SIZE=64
+MALLOC_BLOCK_SIZE=512
+
 function build_nvmf_example_args()
 {
         if [ $SPDK_RUN_NON_ROOT -eq 1 ]; then
@@ -21,8 +24,10 @@ NVMF_EXAMPLE="$(build_nvmf_example_args)"
 function nvmfexamplestart()
 {
         timing_enter start_nvmf_example
-        $NVMF_EXAMPLE $1
+        $NVMF_EXAMPLE $1 &
         nvmfpid=$!
+        trap 'process_shm --id $NVMF_APP_SHM_ID; nvmftestfini; exit 1' SIGINT SIGTERM EXIT
+        waitforlisten $nvmfpid
         timing_exit start_nvmf_example
 }
 
@@ -30,5 +35,27 @@ timing_enter nvmf_example_test
 nvmftestinit
 nvmfexamplestart "-m 0xF"
 
+#create transport
+$rpc_py nvmf_create_transport $NVMF_TRANSPORT_OPTS -u 8192
+#create malloc bdev
+malloc_bdevs="$($rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE) "
+#create subsystem
+$rpc_py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
+
+#add ns to subsystem
+for malloc_bdev in $malloc_bdevs; do
+        $rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 "$malloc_bdev"
+done
+
+#add listener to subsystem
+$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t $TEST_TRANSPORT -a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT
+
+perf="$rootdir/examples/nvme/perf/perf"
+
+$perf -q 64 -o 4096 -w randrw -M 30 -t 10 \
+-r "trtype:${TEST_TRANSPORT} adrfam:IPv4 traddr:${NVMF_FIRST_TARGET_IP} trsvcid:${NVMF_PORT} \
+subnqn:nqn.2016-06.io.spdk:cnode1"
+
+trap - SIGINT SIGTERM EXIT
 nvmftestfini
 timing_exit nvmf_example_test
