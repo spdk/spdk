a818660d9ce5c476ba6730f7df9a40caebc95291
Xiaodong Liu <xiaodong.liu@intel.com>
2019-09-17 15:31:59 +0800

    blobfs: add bdev_blobfs_mount RPC as FUSE
    This RPC method will mount blobfs on bdev to one host
    path through FUSE. Then on the host path, user can
    directly do some file operations which will be mapped
    to blobfs.
    The FUSE mount of blobfs can be umounted directly by
    SHELL umount or fusermount command.
    
    Change-Id: I7c322d978b39bbc7255fced345a749ad5bfa7077
    Signed-off-by: Xiaodong Liu <xiaodong.liu@intel.com>

--- module/blobfs/bdev/blobfs_bdev_rpc.c
+++ module/blobfs/bdev/blobfs_bdev_rpc.c
@@ -42,7 +42,6 @@
 #include "spdk/util.h"
 
 #include "spdk_internal/log.h"
-
 #include "blobfs_bdev.h"
 
 struct rpc_bdev_blobfs_detect {
@@ -212,3 +211,89 @@ invalid:
 }
 
 SPDK_RPC_REGISTER("bdev_blobfs_create", spdk_rpc_bdev_blobfs_create, SPDK_RPC_RUNTIME)
+
+struct rpc_bdev_blobfs_mount {
+	char *bdev_name;
+	char *mountpoint;
+
+	struct spdk_jsonrpc_request *request;
+};
+
+static void
+free_rpc_bdev_blobfs_mount(struct rpc_bdev_blobfs_mount *req)
+{
+	free(req->bdev_name);
+	free(req->mountpoint);
+	free(req);
+}
+
+static const struct spdk_json_object_decoder rpc_bdev_blobfs_mount_decoders[] = {
+	{"bdev_name", offsetof(struct rpc_bdev_blobfs_mount, bdev_name), spdk_json_decode_string},
+	{"mountpoint", offsetof(struct rpc_bdev_blobfs_mount, mountpoint), spdk_json_decode_string},
+};
+
+static void
+_rpc_bdev_blobfs_mount_done(void *cb_arg, int fserrno)
+{
+	struct rpc_bdev_blobfs_mount *req = cb_arg;
+	struct spdk_json_write_ctx *w;
+
+	if (fserrno == -EINVAL) {
+		spdk_jsonrpc_send_error_response(req->request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "Invalid Parameters");
+
+		return;
+	} else if (fserrno != 0) {
+		spdk_jsonrpc_send_error_response(req->request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+						 spdk_strerror(-fserrno));
+
+		return;
+	}
+
+	w = spdk_jsonrpc_begin_result(req->request);
+	spdk_json_write_bool(w, true);
+	spdk_jsonrpc_end_result(req->request, w);
+
+	free_rpc_bdev_blobfs_mount(req);
+}
+
+static void
+spdk_rpc_bdev_blobfs_mount(struct spdk_jsonrpc_request *request,
+			   const struct spdk_json_val *params)
+{
+	struct rpc_bdev_blobfs_mount *req;
+	int rc;
+
+	req = calloc(1, sizeof(*req));
+	if (req == NULL) {
+		SPDK_ERRLOG("could not allocate rpc_bdev_blobfs_mount request.\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR, "Out of memory");
+		return;
+	}
+
+	if (spdk_json_decode_object(params, rpc_bdev_blobfs_mount_decoders,
+				    SPDK_COUNTOF(rpc_bdev_blobfs_mount_decoders),
+				    req)) {
+		SPDK_ERRLOG("spdk_json_decode_object failed\n");
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
+						 "spdk_json_decode_object failed");
+
+		rc = -EINVAL;
+		goto invalid;
+	}
+
+	req->request = request;
+	rc = spdk_blobfs_bdev_mount(req->bdev_name, req->mountpoint, _rpc_bdev_blobfs_mount_done, req);
+	if (rc != 0) {
+		goto invalid;
+	}
+
+	return;
+
+invalid:
+	_rpc_bdev_blobfs_mount_done(req, rc);
+
+	return;
+}
+
+SPDK_RPC_REGISTER("bdev_blobfs_mount", spdk_rpc_bdev_blobfs_mount, SPDK_RPC_RUNTIME)
