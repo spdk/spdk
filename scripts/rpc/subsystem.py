b18413dbcaf3e4bab6ea9600c9d88a4ddf441a98
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-09-18 10:24:58 +0200

    RPC: rename get_subsystems to framework_get_subsystems
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: Iaeaf20e2a16b41796cb89a92fadd99cb1d341720

--- scripts/rpc/subsystem.py
+++ scripts/rpc/subsystem.py
@@ -1,5 +1,9 @@
-def get_subsystems(client):
-    return client.call('get_subsystems')
+from .helpers import deprecated_alias
+
+
+@deprecated_alias('get_subsystems')
+def framework_get_subsystems(client):
+    return client.call('framework_get_subsystems')
 
 
 def get_subsystem_config(client, name):
