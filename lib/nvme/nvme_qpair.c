cc827e84a0bdf1363f67fe0c0406bad326502b93
Seth Howell <seth.howell@intel.com>
2019-09-20 10:27:32 -0700

    nvme_rdma: add simple disconnect handling.
    This is step one in adding transport agnostic disconnect/reconnect
    handling on the NVMe-oF initiator.
    
    Add a flag to the nvme qpair to indicate when the transport qpair fails
    or becomes invalid. This will allow the generic layer to respond to this
    information as it sees fit.
    
    For now, just add a quick check in the admin completion queue. If that
    qpair is failed, try to do a reset and follow the normal path.
    
    This flag is currently only set in response to cm_events in the rdma
    transport. If the rdma admin qpair receives a disconnect request, it
    will alert the upper layer and the reset will take care of failing up
    the stack.
    
    Also add a test to confirm the target disconnect behavior. If run on
    master the target_disconnet.sh script will take about 10 minutes because
    FIO never gets informed of the controller failure, but as of this patch,
    fio properly picks up the disconnect and exits right after the target.
    
    Change-Id: I5bd2c33c04c55c0a6367877ec4d5c27bb234ebd0
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -420,7 +420,7 @@ nvme_qpair_check_enabled(struct spdk_nvme_qpair *qpair)
 int32_t
 spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_completions)
 {
-	int32_t ret;
+	int32_t ret = 0;
 	int32_t resubmit_rc;
 	int32_t i;
 	struct nvme_request *req, *tmp;
@@ -436,6 +436,20 @@ spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_
 		 *  in progress.
 		 */
 		return 0;
+	}
+
+	if (spdk_unlikely(qpair->transport_qp_is_failed)) {
+		if (nvme_qpair_is_admin_queue(qpair)) {
+			nvme_robust_mutex_unlock(&qpair->ctrlr->ctrlr_lock);
+			ret = spdk_nvme_ctrlr_reset(qpair->ctrlr);
+			nvme_robust_mutex_lock(&qpair->ctrlr->ctrlr_lock);
+		} else if (!qpair->ctrlr->adminq->transport_qp_is_failed) {
+			/* TODO: add the individual qpair recovery code here */
+			ret = -ENXIO;
+		}
+
+		/* If the admin qpair is failed, defer to it to reset the ctrlr. */
+		return ret;
 	}
 
 	/* error injection for those queued error requests */
@@ -635,7 +649,14 @@ _nvme_qpair_submit_request(struct spdk_nvme_qpair *qpair, struct nvme_request *r
 		req->submit_tick = 0;
 	}
 
-	if (spdk_likely(qpair->is_enabled)) {
+	if (spdk_unlikely(qpair->transport_qp_is_failed)) {
+		/*
+		 * If the transport qpair  is failed, we cannot submit any requests, even admin.
+		 * It is the transports responsibility during a reconnect to take its qpair state
+		 * out of failed.
+		 */
+		STAILQ_INSERT_TAIL(&qpair->queued_req, req, stailq);
+	} else if (spdk_likely(qpair->is_enabled)) {
 		rc = nvme_transport_qpair_submit_request(qpair, req);
 	} else if (req->cmd.opc == SPDK_NVME_OPC_FABRIC && qpair->is_connecting) {
 		rc = nvme_transport_qpair_submit_request(qpair, req);
