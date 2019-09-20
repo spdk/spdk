94a0403fb110d76cb5a60244f92ab2aeb3068c60
Konrad Sztyber <konrad.sztyber@intel.com>
2019-09-20 14:26:24 +0200

    bdev/ocssd: create / delete calls
    Added a way to create and delete OCSSD bdevs on top of OC NVMe
    controller.  The controller needs to be created using the regular NVMe
    rpc by specifying 'ocssd' mode. For instance, the following:
    
     rpc.py construct_nvme_bdev -b nvme0 -a 0000:00:04.0 -m ocssd -t pcie
     rpc.py bdev_ocssd_create -c nvme0 -b nvme0n1
    
    creates Open Channel controller nvme0 and OCSSD bdev nvme0n1 on top of
    it.  The bdevs can be deleted either by the bdev_ocssd_delete call or by
    deleting the NVMe controller, in which case all bdevs are destroyed.
    
    Change-Id: I9f2f02103fc5570a53bd26479c8690be206829c3
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -1893,6 +1893,29 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
     p.add_argument('name', help='Virtio device name. E.g. VirtioUser0')
     p.set_defaults(func=remove_virtio_bdev)
 
+    # OCSSD
+    def bdev_ocssd_create(args):
+        print_json(rpc.bdev.bdev_ocssd_create(args.client,
+                                              ctrlr_name=args.ctrlr_name,
+                                              bdev_name=args.name,
+                                              nsid=int(args.nsid)))
+
+    p = subparsers.add_parser('bdev_ocssd_create',
+                              help='Creates zoned bdev on specified Open Channel controller')
+    p.add_argument('-c', '--ctrlr_name', help='Name of the OC NVMe controller', required=True)
+    p.add_argument('-b', '--name', help='Name of the bdev to create', required=True)
+    p.add_argument('-n', '--nsid', help='Namespace ID', required=False)
+    p.set_defaults(func=bdev_ocssd_create)
+
+    def bdev_ocssd_delete(args):
+        print_json(rpc.bdev.bdev_ocssd_delete(args.client,
+                                              name=args.name))
+
+    p = subparsers.add_parser('bdev_ocssd_delete',
+                              help='Deletes Open Channel bdev')
+    p.add_argument('name', help='Name of the Open Channel bdev')
+    p.set_defaults(func=bdev_ocssd_delete)
+
     # ioat
     def scan_ioat_copy_engine(args):
         pci_whitelist = []
