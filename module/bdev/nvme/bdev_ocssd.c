e91dfe995e44716449753e892aedae0293da5710
Konrad Sztyber <konrad.sztyber@intel.com>
2019-09-20 14:16:08 +0200

    bdev/ocssd: initial Open Channel bdev implementation
    This patch adds initial Open Channel zoned bdev implementation.  The
    bdev will allow to use the zoned API on top of OCSSD devices.
    
    Added the ability to create the OCSSD NVMe controller.  The controller can
    be created using the regular NVMe's rpc by specifing the 'ocssd' mode.
    
    Change-Id: I31d271126dba4369ac2eaebd4cc7bdd460e5f808
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

---
+++ module/bdev/nvme/bdev_ocssd.c
@@ -0,0 +1,131 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+#include "spdk/bdev_module.h"
+#include "spdk/likely.h"
+#include "spdk/log.h"
+#include "spdk/string.h"
+#include "spdk/nvme_ocssd.h"
+#include "spdk/nvme_ocssd_spec.h"
+#include "spdk_internal/log.h"
+#include "common.h"
+#include "bdev_ocssd.h"
+
+static int
+bdev_ocssd_library_init(void)
+{
+	return 0;
+}
+
+static void
+bdev_ocssd_library_fini(void)
+{
+}
+
+static int
+bdev_ocssd_config_json(struct spdk_json_write_ctx *w)
+{
+	return 0;
+}
+
+static int
+bdev_ocssd_get_ctx_size(void)
+{
+	return 0;
+}
+
+static struct spdk_bdev_module ocssd_if = {
+	.name = "ocssd",
+	.module_init = bdev_ocssd_library_init,
+	.module_fini = bdev_ocssd_library_fini,
+	.config_json = bdev_ocssd_config_json,
+	.get_ctx_size = bdev_ocssd_get_ctx_size,
+};
+
+SPDK_BDEV_MODULE_REGISTER(ocssd, &ocssd_if);
+
+static int
+bdev_ocssd_poll_adminq(void *ctx)
+{
+	struct nvme_bdev_ctrlr *ctrlr = ctx;
+
+	return spdk_nvme_ctrlr_process_admin_completions(ctrlr->ctrlr);
+}
+
+static void
+bdev_ocssd_destruct_ctrlr_cb(struct nvme_bdev_ctrlr *nvme_bdev_ctrlr)
+{
+	assert(nvme_bdev_ctrlr->ref == 0);
+
+	spdk_poller_unregister(&nvme_bdev_ctrlr->adminq_timer_poller);
+}
+
+int
+spdk_bdev_ocssd_create_ctrlr(const struct spdk_nvme_transport_id *trid)
+{
+	struct nvme_bdev_ctrlr *nvme_bdev_ctrlr;
+
+	nvme_bdev_ctrlr = nvme_bdev_ctrlr_get(trid);
+	if (!nvme_bdev_ctrlr) {
+		SPDK_ERRLOG("Failed to find NVMe controller: %s\n", trid->traddr);
+		return -ENODEV;
+	}
+
+	if (!spdk_nvme_ctrlr_is_ocssd_supported(nvme_bdev_ctrlr->ctrlr)) {
+		SPDK_ERRLOG("Specified controller doesn't support Open Channel\n");
+		return -EINVAL;
+	}
+
+	nvme_bdev_ctrlr->adminq_timer_poller = spdk_poller_register(bdev_ocssd_poll_adminq,
+					       nvme_bdev_ctrlr, 100000ULL);
+	if (!nvme_bdev_ctrlr->adminq_timer_poller) {
+		SPDK_ERRLOG("Failed to register admin queue poller\n");
+		return -ENOMEM;
+	}
+
+	nvme_bdev_ctrlr->destruct_ctrlr_fn = bdev_ocssd_destruct_ctrlr_cb;
+	nvme_bdev_ctrlr->mode = SPDK_NVME_OCSSD_CTRLR;
+
+	return 0;
+}
+
+int
+spdk_bdev_ocssd_create_bdevs(struct nvme_async_probe_ctx *ctx)
+{
+	ctx->count = 0;
+	ctx->bdevs_done = true;
+	return 0;
+}
+
+SPDK_LOG_REGISTER_COMPONENT("bdev_ocssd", SPDK_LOG_BDEV_OCSSD)
