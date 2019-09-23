a12af705619599ded49c9e9ac0b8658ce1075f3f
Maciej Szwed <maciej.szwed@intel.com>
2019-09-23 10:52:41 +0200

    bdev/nvme: [workaround] Add 'mode' to nvme_bdev struct
    This is temporary workaround that is required to
    distinguish FTL controller from OCSSD controller.
    It will be removed when FTL is moved out of NVMe
    bdev module.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I1f05cdebd2e8ce4ae081eafeef3e0770c394402c

--- module/bdev/nvme/common.h
+++ module/bdev/nvme/common.h
@@ -58,6 +58,12 @@ struct spdk_bdev_nvme_opts {
 	uint32_t io_queue_requests;
 };
 
+enum nvme_ctrlr_mode {
+	SPDK_NVME_STANDARD_CTRLR,
+	SPDK_NVME_OCSSD_CTRLR,
+	SPDK_NVME_FTL_CTRLR
+};
+
 struct nvme_bdev {
 	struct spdk_bdev	disk;
 	struct nvme_bdev_ctrlr	*nvme_bdev_ctrlr;
@@ -104,6 +110,7 @@ struct nvme_bdev_ctrlr {
 	int				ref;
 	bool				destruct;
 	bool				detaching;
+	enum nvme_ctrlr_mode		mode;
 	/**
 	 * PI check flags. This flags is set to NVMe controllers created only
 	 * through construct_nvme_bdev RPC or .INI config file. Hot added
