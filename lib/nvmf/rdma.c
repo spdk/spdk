56d389be12403daac1b3ae2c206955b9a4e5422e
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-09-24 10:01:53 +0900

    nvmf: Hold number of allocated buffers in struct spdk_nvmf_request
    This patch makes multi SGL case possible to call spdk_nvmf_request_get_buffers()
    per WR.
    
    This patch has an unrelated fix to clear req->iovcnt in
    reset_nvmf_rdma_request() in UT. We can do the fix in a separate patch
    but include it in this patch because it is very small.
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: If6e5af0505fb199c95ef5d0522b579242a7cef29

--- lib/nvmf/rdma.c
+++ lib/nvmf/rdma.c
@@ -1705,7 +1705,7 @@ spdk_nvmf_rdma_request_fill_iovs(struct spdk_nvmf_rdma_transport *rtransport,
 	return rc;
 
 err_exit:
-	spdk_nvmf_request_free_buffers(req, &rgroup->group, &rtransport->transport, num_buffers);
+	spdk_nvmf_request_free_buffers(req, &rgroup->group, &rtransport->transport);
 	memset(wr->sg_list, 0, sizeof(wr->sg_list[0]) * wr->num_sge);
 	wr->num_sge = 0;
 	req->iovcnt = 0;
@@ -1806,7 +1806,7 @@ nvmf_rdma_request_fill_iovs_multi_sgl(struct spdk_nvmf_rdma_transport *rtranspor
 	return 0;
 
 err_exit:
-	spdk_nvmf_request_free_buffers(req, &rgroup->group, &rtransport->transport, num_buffers);
+	spdk_nvmf_request_free_buffers(req, &rgroup->group, &rtransport->transport);
 	nvmf_rdma_request_free_data(rdma_req, rtransport);
 	return rc;
 }
@@ -1955,8 +1955,7 @@ nvmf_rdma_request_free(struct spdk_nvmf_rdma_request *rdma_req,
 	if (rdma_req->req.data_from_pool) {
 		rgroup = rqpair->poller->group;
 
-		spdk_nvmf_request_free_buffers(&rdma_req->req, &rgroup->group, &rtransport->transport,
-					       rdma_req->req.iovcnt);
+		spdk_nvmf_request_free_buffers(&rdma_req->req, &rgroup->group, &rtransport->transport);
 	}
 	nvmf_rdma_request_free_data(rdma_req, rtransport);
 	rdma_req->req.length = 0;
