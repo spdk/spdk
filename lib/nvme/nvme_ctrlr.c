41f1e225a953e9f6a1ab5c0e09ec95f9535f032c
Seth Howell <seth.howell@intel.com>
2019-09-25 15:32:40 -0700

    nvme: fix confusion around  nvme_ctrlr_set_state
    In most places, we are passing NVME_TIMEOUT_INFINITE as the
    timeout_in_ms argument to nvme_ctrlr_set_state, presumably in an attempt
    to specify an infinite timeout. However, nvme_ctrlr_set_state only
    checked against 0 when setting the actual timeout, and we didn't have
    any logic to check for overflow so we just ended up setting random
    timeout_tsc values which changes the behavior of the
    nvme_ctrlr_process_init function in several places.
    
    So, change NVME_TIMEOUT_INFINITE to 0, and add some integer overflow
    checking to nvme_ctrlr_set_state.
    
    Change-Id: Ic9d0cc57ed153df30c3b20313c3742072a5f992d
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_ctrlr.c
+++ lib/nvme/nvme_ctrlr.c
@@ -828,16 +828,32 @@ static void
 nvme_ctrlr_set_state(struct spdk_nvme_ctrlr *ctrlr, enum nvme_ctrlr_state state,
 		     uint64_t timeout_in_ms)
 {
+	uint64_t timeout_in_tsc;
+
 	ctrlr->state = state;
-	if (timeout_in_ms == 0) {
+	if (timeout_in_ms == NVME_TIMEOUT_INFINITE) {
 		SPDK_DEBUGLOG(SPDK_LOG_NVME, "setting state to %s (no timeout)\n",
 			      nvme_ctrlr_state_string(ctrlr->state));
 		ctrlr->state_timeout_tsc = NVME_TIMEOUT_INFINITE;
-	} else {
-		SPDK_DEBUGLOG(SPDK_LOG_NVME, "setting state to %s (timeout %" PRIu64 " ms)\n",
-			      nvme_ctrlr_state_string(ctrlr->state), timeout_in_ms);
-		ctrlr->state_timeout_tsc = spdk_get_ticks() + (timeout_in_ms * spdk_get_ticks_hz()) / 1000;
+		return;
 	}
+
+	if (timeout_in_ms > UINT64_MAX / (spdk_get_ticks() / 1000)) {
+		SPDK_NOTICELOG("Specified timeout would cause integer overflow. Defaulting to no timeout\n");
+		ctrlr->state_timeout_tsc = NVME_TIMEOUT_INFINITE;
+		return;
+	}
+
+	timeout_in_tsc = (timeout_in_ms * spdk_get_ticks_hz()) / 1000;
+	if (timeout_in_tsc > spdk_get_ticks() + timeout_in_tsc) {
+		SPDK_NOTICELOG("Specified timeout would cause integer overflow. Defaulting to no timeout.\n");
+		ctrlr->state_timeout_tsc = NVME_TIMEOUT_INFINITE;
+		return;
+	}
+
+	SPDK_DEBUGLOG(SPDK_LOG_NVME, "setting state to %s (timeout %" PRIu64 " ms)\n",
+				nvme_ctrlr_state_string(ctrlr->state), timeout_in_ms);
+	ctrlr->state_timeout_tsc = spdk_get_ticks() + timeout_in_tsc;
 }
 
 static void
