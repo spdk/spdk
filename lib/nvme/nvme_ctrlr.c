1cf1ed7ec92a1d6f12b587372f991bd8875413c7
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-09-26 13:39:52 +0200

    [WIP] lib/nvme: nvme character device
    This implementation is strongly based on Jim Harris RFC cuse example
    available here: https://review.gerrithub.io/c/spdk/spdk/+/463558
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>
    Change-Id: I0fc9a9a1ef3c9c2b3112d07c2b4b1f8d49665ee1

--- lib/nvme/nvme_ctrlr.c
+++ lib/nvme/nvme_ctrlr.c
@@ -34,6 +34,7 @@
 #include "spdk/stdinc.h"
 
 #include "nvme_internal.h"
+#include "nvme_cuse.h"
 
 #include "spdk/env.h"
 #include "spdk/string.h"
@@ -179,6 +180,12 @@ spdk_nvme_ctrlr_get_default_ctrlr_opts(struct spdk_nvme_ctrlr_opts *opts, size_t
 	if (FIELD_OK(disable_error_logging)) {
 		opts->disable_error_logging = false;
 	}
+
+	/* FIXIT! Cuse - turned on for testing. It should be false by default. */
+	if (FIELD_OK(enable_cuse_devices)) {
+		opts->enable_cuse_devices = true;
+	}
+
 #undef FIELD_OK
 }
 
@@ -2427,6 +2434,12 @@ nvme_ctrlr_destruct(struct spdk_nvme_ctrlr *ctrlr)
 	struct spdk_nvme_qpair *qpair, *tmp;
 
 	SPDK_DEBUGLOG(SPDK_LOG_NVME, "Prepare to destruct SSD: %s\n", ctrlr->trid.traddr);
+
+#ifdef SPDK_CONFIG_CUSE
+	if (ctrlr->opts.enable_cuse_devices) {
+		spdk_nvme_cuse_stop(ctrlr);
+	}
+#endif
 
 	nvme_transport_admin_qpair_abort_aers(ctrlr->adminq);
 
