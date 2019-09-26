8bf7f45cf01ff129892ea18f47d3cd5a7660421b
Seth Howell <seth.howell@intel.com>
2019-09-26 12:55:30 -0700

    nvme: always set ctrlr->is_failed through API
    Use the standard API function to fail the controller in all cases.
    
    Change-Id: I06dfce08326c23472a1caa8f6efbac2fd1a720f2
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -455,7 +455,7 @@ spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_
 	ret = nvme_transport_qpair_process_completions(qpair, max_completions);
 	if (ret < 0) {
 		SPDK_ERRLOG("CQ error, abort requests after transport retry counter exceeded\n");
-		qpair->ctrlr->is_failed = true;
+		nvme_ctrlr_fail(qpair->ctrlr, false);
 	}
 	qpair->in_completion_context = 0;
 	if (qpair->delete_after_completion_context) {
