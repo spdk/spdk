40a79ce3647d90f2f1ad5ac4aa8f25c72d01f10b
Ben Walker <benjamin.walker@intel.com>
2019-09-27 13:30:09 -0700

    util: Add spdk_iovcpy, an iovec to iovec copy function.
    Like memcpy, but works on two iovecs.
    
    Change-Id: Ia1cf462a95690286f0c19325fc10937b9ba6baf3
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/unit/unittest.sh
+++ test/unit/unittest.sh
@@ -155,6 +155,7 @@ $valgrind $testdir/lib/util/crc32_ieee.c/crc32_ieee_ut
 $valgrind $testdir/lib/util/crc32c.c/crc32c_ut
 $valgrind $testdir/lib/util/string.c/string_ut
 $valgrind $testdir/lib/util/dif.c/dif_ut
+$valgrind $testdir/lib/util/iov.c/iov_ut
 $valgrind $testdir/lib/util/pipe.c/pipe_ut
 
 if [ $(uname -s) = Linux ]; then
