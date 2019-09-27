e1b704f7f657972dbc725a29ba29775e27ed73c7
Seth Howell <seth.howell@intel.com>
2019-09-27 13:42:00 -0700

    nvme: move queued request resubmit to generic layer
    We were already passing up from each transport the number of completions
    done during the transport specific call. So just use that return code
    and batch all of the submissions together at one time in the generic
    code.
    
    This change and subsequent moves of code from the transport layer to the
    genric layers is aimed at making reset handling at the generic NVMe
    layer simpler.
    
    Change-Id: I028aea86d76352363ffffe661deec2215bc9c450
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -420,6 +420,7 @@ int32_t
 spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_completions)
 {
 	int32_t ret;
+	int32_t resubmit_rc = 0;
 	struct nvme_request *req, *tmp;
 
 	if (qpair->ctrlr->is_failed) {
@@ -460,7 +461,19 @@ spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_
 		 *  routine - so it is safe to delete it now.
 		 */
 		spdk_nvme_ctrlr_free_io_qpair(qpair);
+		return ret;
 	}
+
+	/*
+	 * At this point, ret must represent the number of completions we reaped.
+	 * submit as many queued requests as we completed.
+	 */
+	while (resubmit_rc == 0 && !STAILQ_EMPTY(&qpair->queued_req) && !qpair->ctrlr->is_resetting) {
+		req = STAILQ_FIRST(&qpair->queued_req);
+		STAILQ_REMOVE_HEAD(&qpair->queued_req, stailq);
+		resubmit_rc = nvme_qpair_resubmit_request(qpair, req);
+	}
+
 	return ret;
 }
 
