ff365475d8b237635dde407e39d9f6f6bf96944a
Chunyang Hui <chunyang.hui@intel.com>
2019-09-29 23:56:12 +0800

    bdev/opal: Add rpc commands for opal vbdev
    Add nvme_opal_init, opal_revert_tper, opal_get_info
    rpc commands.
    
    Change-Id: Ib53492c02a1c18603834640d23f9fb2e7eb08657
    Signed-off-by: Chunyang Hui <chunyang.hui@intel.com>

--- module/bdev/nvme/vbdev_opal.h
+++ module/bdev/nvme/vbdev_opal.h
@@ -40,6 +40,11 @@
 int spdk_vbdev_opal_create(const char *nvme_ctrlr_name, uint32_t nsid, uint8_t locking_range_id,
 			   uint64_t range_start, uint64_t range_length, const char *password);
 
+struct spdk_opal_locking_range_info *spdk_vbdev_opal_get_info_from_bdev(struct spdk_bdev *opal_bdev,
+		const char *password);
+
 int spdk_vbdev_opal_destruct(const char *bdev_name, const char *password);
 
+int spdk_vbdev_opal_revert_tper(struct nvme_bdev_ctrlr *nvme_ctrlr, const char *password,
+				spdk_opal_revert_cb cb_fn, void *cb_ctx);
 #endif
