17a99d427b0525aa674b845cf574ca3d95dff7eb
Konrad Sztyber <konrad.sztyber@intel.com>
2019-10-02 08:20:07 +0200

    bdev/nvme: separate variable for probe name array size
    Use a separate variable to hold the size of the names array in
    nvme_async_probe_ctx to allow using the counter to keep track of bdevs
    created asynchronously.
    
    Change-Id: I615617c6c2e253b804879e3016a729b35f119db3
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -1176,7 +1176,6 @@ bdev_nvme_create_bdevs(struct nvme_async_probe_ctx *ctx, spdk_bdev_create_nvme_f
 {
 	struct nvme_bdev_ctrlr	*nvme_bdev_ctrlr;
 	struct nvme_bdev	*nvme_bdev, *tmp;
-	size_t			j;
 	int			rc;
 
 	nvme_bdev_ctrlr = nvme_bdev_ctrlr_get(&ctx->trid);
@@ -1196,15 +1195,15 @@ bdev_nvme_create_bdevs(struct nvme_async_probe_ctx *ctx, spdk_bdev_create_nvme_f
 	 * Report the new bdevs that were created in this call.
 	 * There can be more than one bdev per NVMe controller since one bdev is created per namespace.
 	 */
-	j = 0;
+	ctx->count = 0;
 
 	TAILQ_FOREACH_SAFE(nvme_bdev, &nvme_bdev_ctrlr->bdevs, tailq, tmp) {
 		if (!nvme_bdev->active) {
 			continue;
 		}
-		if (j < ctx->count) {
-			ctx->names[j] = nvme_bdev->disk.name;
-			j++;
+		if (ctx->count < ctx->max_names) {
+			ctx->names[ctx->count] = nvme_bdev->disk.name;
+			ctx->count++;
 		} else {
 			SPDK_ERRLOG("Maximum number of namespaces supported per NVMe controller is %zu. Unable to return all names of created bdevs\n",
 				    ctx->count);
@@ -1213,7 +1212,7 @@ bdev_nvme_create_bdevs(struct nvme_async_probe_ctx *ctx, spdk_bdev_create_nvme_f
 		}
 	}
 
-	cb_fn(cb_arg, j, 0);
+	cb_fn(cb_arg, ctx->count, 0);
 }
 
 static void
@@ -1300,7 +1299,7 @@ spdk_bdev_nvme_create(struct spdk_nvme_transport_id *trid,
 	}
 	ctx->base_name = base_name;
 	ctx->names = names;
-	ctx->count = count;
+	ctx->max_names = count;
 	ctx->cb_fn = cb_fn;
 	ctx->cb_ctx = cb_ctx;
 	ctx->prchk_flags = prchk_flags;
--- module/bdev/nvme/common.h
+++ module/bdev/nvme/common.h
@@ -109,6 +109,7 @@ struct nvme_async_probe_ctx {
 	struct spdk_nvme_probe_ctx *probe_ctx;
 	const char *base_name;
 	const char **names;
+	size_t max_names;
 	size_t count;
 	uint32_t prchk_flags;
 	struct spdk_poller *poller;
