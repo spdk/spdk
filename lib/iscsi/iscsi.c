2d83468334f2722e8de8596d944bf53d591ca7f4
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-10-04 13:53:45 +0900

    lib/iscsi: Merge receiving incoming PDU and its repetition in a single state machine
    Merge spdk_iscsi_read_pdu() and iscsi_conn_handle_incoming_pdus()
    into spdk_iscsi_handle_incoming_pdus() by moving loop count and
    loop break check.
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: I967681b8e9b86681a906b18719e91e1d387450d7

--- lib/iscsi/iscsi.c
+++ lib/iscsi/iscsi.c
@@ -4722,9 +4722,12 @@ iscsi_pdu_payload_handle(struct spdk_iscsi_conn *conn, struct spdk_iscsi_pdu *pd
 	return rc;
 }
 
+#define PDU_RECV_LOOP_COUNT	16
+
 int
-spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
+spdk_iscsi_handle_incoming_pdus(struct spdk_iscsi_conn *conn)
 {
+	int pdu_recv_loop_cnt = 0;
 	enum iscsi_pdu_recv_state prev_state;
 	struct spdk_iscsi_pdu *pdu;
 	struct spdk_mempool *pool;
@@ -4757,7 +4760,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 				}
 				pdu->bhs_valid_bytes += rc;
 				if (pdu->bhs_valid_bytes < ISCSI_BHS_LEN) {
-					return 0;
+					return pdu_recv_loop_cnt;
 				}
 			}
 
@@ -4775,7 +4778,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 
 				pdu->ahs_valid_bytes += rc;
 				if (pdu->ahs_valid_bytes < ahs_len) {
-					return 0;
+					return pdu_recv_loop_cnt;
 				}
 			}
 
@@ -4792,7 +4795,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 
 				pdu->hdigest_valid_bytes += rc;
 				if (pdu->hdigest_valid_bytes < ISCSI_DIGEST_LEN) {
-					return 0;
+					return pdu_recv_loop_cnt;
 				}
 			}
 			conn->pdu_recv_state = ISCSI_PDU_RECV_STATE_AWAIT_PDU_PAYLOAD;
@@ -4815,7 +4818,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 				}
 				pdu->mobj = spdk_mempool_get(pool);
 				if (pdu->mobj == NULL) {
-					return 0;
+					return pdu_recv_loop_cnt;
 				}
 				pdu->data_buf = pdu->mobj->buf;
 
@@ -4834,7 +4837,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 
 				pdu->data_valid_bytes += rc;
 				if (pdu->data_valid_bytes < data_len) {
-					return 0;
+					return pdu_recv_loop_cnt;
 				}
 			}
 
@@ -4851,7 +4854,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 
 				pdu->ddigest_valid_bytes += rc;
 				if (pdu->ddigest_valid_bytes < ISCSI_DIGEST_LEN) {
-					return 0;
+					return pdu_recv_loop_cnt;
 				}
 			}
 
@@ -4873,7 +4876,7 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 						spdk_put_pdu(pdu);
 						conn->pdu_in_progress = NULL;
 						conn->pdu_recv_state = ISCSI_PDU_RECV_STATE_AWAIT_PDU_PAYLOAD;
-						return 0;
+						return pdu_recv_loop_cnt;
 					} else {
 						conn->pdu_recv_state = ISCSI_PDU_RECV_STATE_ERROR;
 						break;
@@ -4896,9 +4899,10 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 			spdk_put_pdu(pdu);
 			conn->pdu_in_progress = NULL;
 			conn->pdu_recv_state = ISCSI_PDU_RECV_STATE_AWAIT_PDU_READY;
+			pdu_recv_loop_cnt++;
 
 			spdk_trace_record(TRACE_ISCSI_TASK_EXECUTED, 0, 0, (uintptr_t)pdu, 0);
-			return 1;
+			break;
 		case ISCSI_PDU_RECV_STATE_ERROR:
 			spdk_put_pdu(pdu);
 			conn->pdu_in_progress = NULL;
@@ -4908,9 +4912,10 @@ spdk_iscsi_read_pdu(struct spdk_iscsi_conn *conn)
 			SPDK_ERRLOG("connection should not come here\n");
 			break;
 		}
-	} while (prev_state != conn->pdu_recv_state);
+	} while (prev_state != conn->pdu_recv_state &&
+		 pdu_recv_loop_cnt < PDU_RECV_LOOP_COUNT && !conn->is_stopped);
 
-	return 0;
+	return pdu_recv_loop_cnt;
 }
 
 bool
