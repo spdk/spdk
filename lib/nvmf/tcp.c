ecb28faec78277446e5df53dee0ac591032e181b
Ben Walker <benjamin.walker@intel.com>
2019-10-04 10:52:09 -0700

    nvmf/tcp: Use the new sock_is_connected function during shutdown
    Change-Id: I3cf8765bbbcddaeda731188c7911b1966b953bc4
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/nvmf/tcp.c
+++ lib/nvmf/tcp.c
@@ -2083,9 +2083,7 @@ spdk_nvmf_tcp_sock_process(struct spdk_nvmf_tcp_qpair *tqpair)
 			spdk_nvmf_tcp_pdu_payload_handle(tqpair);
 			break;
 		case NVME_TCP_PDU_RECV_STATE_ERROR:
-			/* Check whether the connection is closed. Each time, we only read 1 byte every time */
-			rc = nvme_tcp_read_data(tqpair->sock, 1, (void *)&pdu->hdr->common);
-			if (rc < 0) {
+			if (!spdk_sock_is_connected(tqpair->sock)) {
 				return NVME_TCP_PDU_FATAL;
 			}
 			break;
