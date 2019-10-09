7dd8ee08b50b630ef09ad0948a7a0febaf0e94dc
Ben Walker <benjamin.walker@intel.com>
2019-10-09 14:06:15 -0700

    sock/posix: Zero copy send
    If available, automatically use MSG_ZEROCOPY when sending on sockets.
    Storage workloads contain sufficient data transfer sizes that this is
    always a performance improvement, regardless of workload.
    
    Change-Id: I14429d78c22ad3bc036aec13c9fce6453e899c92
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- test/nvmf/target/filesystem.sh
+++ test/nvmf/target/filesystem.sh
@@ -21,7 +21,12 @@ function nvmf_filesystem_create {
 		force=-f
 	fi
 
-	mkfs.${fstype} $force /dev/nvme0n1p1
+	local i=0
+	while ! mkfs.${fstype} $force /dev/nvme0n1p1; do
+		[ $i -lt 15 ] || break
+		i=$((i+1))
+		sleep 1
+	done
 
 	mount /dev/nvme0n1p1 /mnt/device
 	touch /mnt/device/aaa
@@ -29,6 +34,15 @@ function nvmf_filesystem_create {
 	rm /mnt/device/aaa
 	sync
 	umount /mnt/device
+
+	# Make sure the target did not crash
+	kill -0 $nvmfpid;
+
+	# Make sure the device is still present
+	lsblk -l -o NAME | grep -q -w "nvme0n1";
+
+	# Make sure the partition is still present
+	lsblk -l -o NAME | grep -q -w "nvme0n1p1";
 }
 
 function nvmf_filesystem_part {
