024cb07190384850d3a7786eaa7a29c957762ea9
Maciej Szwed <maciej.szwed@intel.com>
2019-10-14 14:35:11 +0200

    bdev/nvme: Use Namespace object for tracking bdevs
    In future implementations it will be possible to create
    different types of namespaces (standard, OCSSD, ...).
    This patch introduces new nvme_namespace type which
    will make possible to implement such cases.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: I27747d3985915f45c0e0a28dd5f391cca06b13cc

--- module/bdev/nvme/common.h
+++ module/bdev/nvme/common.h
@@ -44,6 +44,12 @@ extern pthread_mutex_t g_bdev_nvme_mutex;
 
 #define NVME_MAX_CONTROLLERS 1024
 
+struct nvme_namespace {
+	uint32_t		id;
+	bool			active;
+	struct nvme_bdev	*bdev;
+};
+
 struct nvme_bdev_ctrlr {
 	/**
 	 * points to pinned, physically contiguous memory region;
@@ -62,8 +68,8 @@ struct nvme_bdev_ctrlr {
 	 */
 	uint32_t			prchk_flags;
 	uint32_t			num_ns;
-	/** Array of bdevs indexed by nsid - 1 */
-	struct nvme_bdev		*bdevs;
+	/** Array of pointers to namespaces indexed by nsid - 1 */
+	struct nvme_namespace		**namespaces;
 
 	struct spdk_opal_dev		*opal_dev;
 
@@ -76,9 +82,8 @@ struct nvme_bdev_ctrlr {
 struct nvme_bdev {
 	struct spdk_bdev	disk;
 	struct nvme_bdev_ctrlr	*nvme_bdev_ctrlr;
-	uint32_t		id;
-	bool			active;
 	struct spdk_nvme_ns	*ns;
+	struct nvme_namespace	*nvme_ns;
 };
 
 typedef void (*spdk_bdev_create_nvme_fn)(void *ctx, size_t bdev_count, int rc);
