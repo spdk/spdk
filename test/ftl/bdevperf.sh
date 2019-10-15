4d33f669c29ea4eee0f42d403b5be3245f840a0d
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-10-15 08:14:49 -0400

    bdev/ftl: Examine config support
    Bdev FTL is dependent on base bdev and cache
    bdev. Allow for examine config if dependent
    bdevs are not ready during bdev FTL creation.
    
    Change-Id: I917994d7015f3b74a29ccd066f0c6989ad3c1c4e
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- test/ftl/bdevperf.sh
+++ test/ftl/bdevperf.sh
@@ -19,9 +19,9 @@ for (( i=0; i<${#tests[@]}; i++ )) do
 
 	trap 'killprocess $bdevperf_pid; exit 1' SIGINT SIGTERM EXIT
 	waitforlisten $bdevperf_pid
+	$rpc_py construct_ftl_bdev -b ftl0 -d nvme0n1
 	$rpc_py bdev_nvme_attach_controller -b nvme0 -a $device -t pcie
 	$rpc_py bdev_ocssd_create -c nvme0 -b nvme0n1 -n 1
-	$rpc_py construct_ftl_bdev -b ftl0 -d nvme0n1
 	$rootdir/test/bdev/bdevperf/bdevperf.py perform_tests
 	$rpc_py delete_ftl_bdev -b ftl0
 	$rpc_py bdev_ocssd_delete nvme0n1
