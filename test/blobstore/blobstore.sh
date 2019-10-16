d318373329a765ab260c39cb290cb401af37c53a
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-10-16 07:55:45 +0200

    test: Shellcheck - correct rule: Use a ( subshell ) to avoid having to cd back
    Correct shellcheck rule SC2103: Use a ( subshell ) to avoid having to cd back.
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: I474e11f5753c3482c7924548ab7c0183d4ac419d

--- test/blobstore/blobstore.sh
+++ test/blobstore/blobstore.sh
@@ -18,9 +18,8 @@ $rootdir/scripts/gen_nvme.sh > $testdir/blobcli.conf
 # generate random data file for import/export diff
 dd if=/dev/urandom of=$testdir/test.pattern bs=1M count=1
 
-cd $testdir
-$rootdir/examples/blob/cli/blobcli -c $testdir/blobcli.conf -b Nvme0n1 -T $testdir/test.bs > $testdir/btest.out
-cd -
+(cd $testdir &&
+$rootdir/examples/blob/cli/blobcli -c $testdir/blobcli.conf -b Nvme0n1 -T $testdir/test.bs > $testdir/btest.out)
 
 # the tool leaves some trailing whitespaces that we need to strip out
 sed -i 's/[[:space:]]*$//' $testdir/btest.out
