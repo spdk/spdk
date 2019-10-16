fdc28f79ad7842a7f96b30b590ee56e8d9393f5f
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-10-16 09:13:42 +0200

    test: Shellcheck - correct rule: Useless echo?
    Correct shellcheck rule SC2116: Useless echo?
    Instead of 'cmd $(echo foo)', just use 'cmd foo'.
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: I940eb1ec72ba00ecb89ca770300f5d37e90e8a86

--- test/json_config/json_config.sh
+++ test/json_config/json_config.sh
@@ -45,13 +45,11 @@ function tgt_check_notification_types() {
 	timing_enter $FUNCNAME
 
 	local ret=0
-	local enabled_types="
-		bdev_register
-		bdev_unregister
-	"
+	local enabled_types="bdev_register
+		bdev_unregister	"
 
 	get_types=$(tgt_rpc notify_get_types | jq -r '.[]')
-	if [ "$(echo $enabled_types)" != "$(echo $get_types)" ]; then
+	if [ "$enabled_types" != "$get_types" ]; then
 		echo "ERROR: expected types:" $enabled_types ", but got:" $get_types
 		ret=1
 	fi
