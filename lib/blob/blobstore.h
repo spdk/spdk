e6c9fa78b70badead1da2e214758e167d2e2c9d5
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2019-10-17 06:36:33 -0400

    lib/blob: require SPDK_EXTENTS_PER_ET to be power of 2
    Force number of Extents to fit into Extent Table to
    be power of 2, in order to simplify calculations
    on cluster allocations.
    
    At this time SPDK_BS_PAGE_SIZE is 4k, which would
    results in SPDK_EXTENTS_PER_ET to be 512.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I7e09d92b00dfe5c12d7dd10ac0fc5a9a10d526ac

--- lib/blob/blobstore.h
+++ lib/blob/blobstore.h
@@ -361,9 +361,9 @@ SPDK_STATIC_ASSERT(SPDK_BS_PAGE_SIZE == sizeof(struct spdk_blob_md_page), "Inval
 
 #define SPDK_BS_MAX_DESC_SIZE sizeof(((struct spdk_blob_md_page*)0)->descriptors)
 
-/* TODO: Set to low amount for purpose of testing */
 /* Maximum number of LBA's a single Extent Page can fit */
-#define SPDK_EXTENTS_PER_EP 100
+#define SPDK_EXTENTS_PER_EP_MAX ((SPDK_BS_MAX_DESC_SIZE - sizeof(struct spdk_blob_md_descriptor_extent)) / sizeof(uint32_t))
+#define SPDK_EXTENTS_PER_EP (spdk_align64pow2(SPDK_EXTENTS_PER_EP_MAX + 1) >> 1u)
 
 #define SPDK_BS_SUPER_BLOCK_SIG "SPDKBLOB"
 
