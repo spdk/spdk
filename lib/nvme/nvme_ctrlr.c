6ecf423dd9374e4919ed06672af8f21ce1e26eb2
Seth Howell <seth.howell@intel.com>
2019-10-17 16:23:43 -0700

    nvme_ctrlr: add get/set for ctrlr->is_failed.
    These will be useful helper functions for the trid modification code
    that gets introduced later.
    
    Change-Id: Ief73e3045710bf35c511794c19b4dfefb93018f1
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_ctrlr.c
+++ lib/nvme/nvme_ctrlr.c
@@ -613,6 +613,12 @@ nvme_ctrlr_set_supported_features(struct spdk_nvme_ctrlr *ctrlr)
 	nvme_ctrlr_set_arbitration_feature(ctrlr);
 }
 
+bool
+spdk_nvme_ctrlr_is_failed(struct spdk_nvme_ctrlr *ctrlr)
+{
+	return ctrlr->is_failed;
+}
+
 void
 nvme_ctrlr_fail(struct spdk_nvme_ctrlr *ctrlr, bool hot_remove)
 {
@@ -625,6 +631,12 @@ nvme_ctrlr_fail(struct spdk_nvme_ctrlr *ctrlr, bool hot_remove)
 	}
 	ctrlr->is_failed = true;
 	SPDK_ERRLOG("ctrlr %s in failed state.\n", ctrlr->trid.traddr);
+}
+
+void
+spdk_nvme_ctrlr_fail(struct spdk_nvme_ctrlr *ctrlr)
+{
+	nvme_ctrlr_fail(ctrlr, false);
 }
 
 static void
