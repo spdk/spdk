4fb5278adbc94e70055f6918fea95376428c0bc0
Ben Walker <benjamin.walker@intel.com>
2019-10-18 11:15:01 -0700

    sock/posix: Use sendmsg instead of writev when flushing
    This is preparation to add flags to the call.
    
    Change-Id: I36109c069b42bd3ec22e023079c62d41a435f44c
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- module/sock/posix/posix.c
+++ module/sock/posix/posix.c
@@ -600,6 +600,8 @@ spdk_posix_sock_close(struct spdk_sock *_sock)
 static int
 spdk_posix_sock_flush(struct spdk_posix_sock *sock)
 {
+	struct msghdr msg = {};
+	int flags;
 	struct iovec iovs[DEFAULT_MAX_IOV];
 	int iovcnt;
 	struct spdk_posix_sock_request *req;
@@ -646,7 +648,10 @@ spdk_posix_sock_flush(struct spdk_posix_sock *sock)
 	}
 
 	/* Perform the vectored write */
-	rc = writev(sock->fd, iovs, iovcnt);
+	msg.msg_iov = iovs;
+	msg.msg_iovlen = iovcnt;
+	flags = 0;
+	rc = sendmsg(sock->fd, &msg, flags);
 	if (rc <= 0) {
 		if (errno == EAGAIN || errno == EWOULDBLOCK) {
 			return 0;
