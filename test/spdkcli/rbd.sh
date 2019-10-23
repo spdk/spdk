9ef30613e5d6a19cfe0ae1a73aba65c07bf2cbbc
dongx.yi <dongx.yi@intel.com>
2019-10-23 11:15:46 -0400

    test/rbd: Fix some issues in rbd.sh.
    This patch follows with
    https://review.gerrithub.io/c/spdk/spdk/+/471947.
    Some commands report errors about transferred meaning.
    
    This is relatedi to issue #986
    
    Signed-off-by: dongx.yi <dongx.yi@intel.com>
    Change-Id: I396a47853823bb66554fbae7e1f96e4236826850

--- test/spdkcli/rbd.sh
+++ test/spdkcli/rbd.sh
@@ -18,9 +18,7 @@ timing_enter spdkcli_create_rbd_config
 trap 'rbd_cleanup; on_error_exit' ERR
 rbd_cleanup
 rbd_setup 127.0.0.1
-$spdkcli_job "'/bdevs/rbd create rbd foo 512' 'Ceph0' True
-'/bdevs/rbd create rbd foo 512' 'Ceph1' True
-"
+$spdkcli_job "\"/bdevs/rbd create rbd foo 512' 'Ceph0' True "\"/bdevs/rbd create rbd foo 512 'Ceph1' True"
 timing_exit spdkcli_create_rbd_config
 
 timing_enter spdkcli_check_match
@@ -28,9 +26,7 @@ check_match
 timing_exit spdkcli_check_match
 
 timing_enter spdkcli_clear_rbd_config
-$spdkcli_job "\"/bdevs/rbd delete Ceph0' 'Ceph0'
-'/bdevs/rbd delete_all' 'Ceph1'
-"
+$spdkcli_job "\"/bdevs/rbd delete Ceph0 'Ceph0' "\"/bdevs/rbd delete_all' 'Ceph1' "
 rbd_cleanup
 timing_exit spdkcli_clear_rbd_config
 
