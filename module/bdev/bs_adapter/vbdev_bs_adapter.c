a58271a02fa0b9f75c840e1d79d9badfa2449178
Kozlowski Mateusz <mateusz.kozlowski@intel.com>
2019-10-24 09:18:33 -0400

    bdev/bs_adapter: Create and delete legacy mode vbdev
    Added create and delete functions for legacy sector size bdev.
    The bdev will be responsible for converting 4KiB sector size bdevs
    to 512B.
    
    Signed-off-by: Kozlowski Mateusz <mateusz.kozlowski@intel.com>
    Change-Id: I0c024efa1a5eb68bd2d3289f3e8943b77b12fd8a

---
+++ module/bdev/bs_adapter/vbdev_bs_adapter.c
@@ -0,0 +1,64 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "vbdev_bs_adapter.h"
+
+#include "spdk/config.h"
+#include "spdk/nvme.h"
+
+#include "spdk_internal/log.h"
+
+int
+spdk_vbdev_bs_adapter_create(const char *bdev_name, const char *vbdev_name)
+{
+	struct spdk_bdev *bdev = NULL;
+	int rc = 0;
+
+	bdev = spdk_bdev_get_by_name(bdev_name);
+
+	if (!bdev) {
+		SPDK_ERRLOG("Base bdev (%s) doesn't exist\n", bdev_name);
+		return -ENODEV;
+	}
+
+	return rc;
+}
+
+void
+spdk_vbdev_bs_adapter_delete(const char *name, spdk_bdev_unregister_cb cb_fn, void *cb_arg)
+{
+}
+
+SPDK_LOG_REGISTER_COMPONENT("vbdev_bs_adapter", SPDK_LOG_VBDEV_BS_ADAPTER)
