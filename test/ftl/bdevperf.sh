f2c8083ef37ff10d227a6a3538bd983570be20ad
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-10-25 03:31:14 -0400

    lib/ftl: Functional tests
    This patch adjust functional tests to
    new FTL stack.
    
    Change-Id: I0b8ff08c61793d7f1685942031aecf9cc88d1f24
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/468738
    Community-CI: Broadcom SPDK FC-NVMe CI <spdk-ci.pdl@broadcom.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- test/ftl/bdevperf.sh
+++ test/ftl/bdevperf.sh
@@ -3,15 +3,32 @@
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
+source $testdir/common.sh
 
 tests=('-q 1 -w randwrite -t 4 -o 69632' '-q 128 -w randwrite -t 4 -o 4096' '-q 128 -w verify -t 4 -o 4096')
 device=$1
-ftl_bdev_conf=$testdir/config/ftl.conf
+rpc_py=$rootdir/scripts/rpc.py
 
-$rootdir/scripts/gen_ftl.sh -a $device -n nvme0 > $ftl_bdev_conf
+ftl_bdev_conf=$testdir/config/ftl.conf
+gen_ftl_nvme_conf > $ftl_bdev_conf
 
 for (( i=0; i<${#tests[@]}; i++ )) do
 	timing_enter "${tests[$i]}"
-	$rootdir/test/bdev/bdevperf/bdevperf -c $ftl_bdev_conf ${tests[$i]}
+	$rootdir/test/bdev/bdevperf/bdevperf -z -T ftl0 ${tests[$i]} -c $ftl_bdev_conf &
+	bdevperf_pid=$!
+
+	trap 'killprocess $bdevperf_pid; exit 1' SIGINT SIGTERM EXIT
+	waitforlisten $bdevperf_pid
+	$rpc_py bdev_nvme_attach_controller -b nvme0 -a $device -t pcie
+	$rpc_py bdev_ocssd_create -c nvme0 -b nvme0n1
+	$rpc_py bdev_ftl_create -b ftl0 -d nvme0n1
+	$rootdir/test/bdev/bdevperf/bdevperf.py perform_tests
+	$rpc_py delete_ftl_bdev -b ftl0
+	$rpc_py bdev_ocssd_delete nvme0n1
+	$rpc_py delete_nvme_controller nvme0
+	killprocess $bdevperf_pid
+	trap - SIGINT SIGTERM EXIT
 	timing_exit "${tests[$i]}"
 done
+
+rm -f $ftl_bdev_conf
