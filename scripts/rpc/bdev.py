0af5a778f05844459d9f95c7da2ea4dd4a1d0d4e
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-10-25 03:31:14 -0400

    lib/ftl: Functional tests
    This patch adjust functional tests to
    new FTL stack.
    
    Change-Id: I0b8ff08c61793d7f1685942031aecf9cc88d1f24
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- scripts/rpc/bdev.py
+++ scripts/rpc/bdev.py
@@ -884,18 +884,16 @@ def bdev_split_delete(client, base_bdev):
 
 
 @deprecated_alias('construct_ftl_bdev')
-def bdev_ftl_create(client, name, trtype, traddr, **kwargs):
+def bdev_ftl_create(client, name, base_bdev, **kwargs):
     """Construct FTL bdev
 
     Args:
         name: name of the bdev
-        trtype: transport type
-        traddr: transport address
+        base_bdev: name of the base bdev
         kwargs: optional parameters
     """
     params = {'name': name,
-              'trtype': trtype,
-              'traddr': traddr}
+              'base_bdev': base_bdev}
     for key, value in kwargs.items():
         if value is not None:
             params[key] = value
