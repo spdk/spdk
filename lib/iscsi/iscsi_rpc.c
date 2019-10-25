99e41d67b2dcfa696124bc1888357dddfe68cf27
Jim Harris <james.r.harris@intel.com>
2019-10-25 16:04:51 -0700

    rpc: add spdk_rpc_verify_methods()
    This returns true if all registered methods and aliases
    are correct.  False means that an error like one of the
    following occurred:
    
    - duplicate method with same name
    - alias specified for non-existant method
    - alias specified for another alias
    
    Also plumb this so that incorrect RPCs cause an SPDK
    application to exit.
    
    Note: there are cases where this would have been helpful
    during the recent RPC renaming.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I235a433c9b8c01e82f16288a8d295e96c54e4eb1

--- lib/iscsi/iscsi_rpc.c
+++ lib/iscsi/iscsi_rpc.c
@@ -1644,5 +1644,6 @@ spdk_rpc_iscsi_set_options(struct spdk_jsonrpc_request *request,
 	spdk_json_write_bool(w, true);
 	spdk_jsonrpc_end_result(request, w);
 }
+
 SPDK_RPC_REGISTER("iscsi_set_options", spdk_rpc_iscsi_set_options, SPDK_RPC_STARTUP)
 SPDK_RPC_REGISTER_ALIAS_DEPRECATED(iscsi_set_options, set_iscsi_options)
