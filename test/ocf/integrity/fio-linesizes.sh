51122c046cbc2c2e0c8188ff15b3c9a6cc9d720c
Robert Baldyga <robert.baldyga@intel.com>
2019-10-28 13:06:22 +0100

    test/ocf: add integrity tests with various cache line sizes
    Add basic integrity tests with running OCF in WT mode with
    all possible cache line size settings.
    
    Change-Id: Ic1a42c8509d19274392edfad7f3ea483bf3fd694
    Signed-off-by: Robert Baldyga <robert.baldyga@intel.com>

---
+++ test/ocf/integrity/fio-linesizes.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+curdir=$(dirname $(readlink -f "$BASH_SOURCE"))
+rootdir=$(readlink -f $curdir/../../..)
+plugindir=$rootdir/examples/bdev/fio_plugin
+
+source $rootdir/test/common/autotest_common.sh
+
+function fio_verify(){
+	fio_bdev $curdir/test.fio --aux-path=/tmp/ --ioengine=spdk_bdev $@
+}
+
+function cleanup(){
+	rm -f $curdir/linesizes.conf
+}
+
+trap "cleanup; exit 1" SIGINT SIGTERM EXIT
+
+nvme_cfg=$($rootdir/scripts/gen_nvme.sh)
+
+config="
+$nvme_cfg
+
+[Split]
+  Split Nvme0n1 8 101
+
+[OCF]
+  OCF WT_Nvme8k  pt  8192 Nvme0n1p0 Nvme0n1p1
+  OCF WT_Nvme16k wt 16384 Nvme0n1p2 Nvme0n1p3
+  OCF WT_Nvme32k wb 32768 Nvme0n1p4 Nvme0n1p5
+  OCF WT_Nvme64k wb 65536 Nvme0n1p6 Nvme0n1p7
+"
+echo "$config" > $curdir/linesizes.conf
+
+fio_verify --filename=WT_Nvme8k:WT_Nvme16k:WT_Nvme32k:WT_Nvme64k --spdk_conf=$curdir/linesizes.conf
+
+trap - SIGINT SIGTERM EXIT
+cleanup
