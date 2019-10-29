spdk/spdk

commit bb01a08915983a701faa5d961dfe3c3c5722c824

nvme: plumb disconnect/connect in reset path
This will (finally) enable resets for fabrics
controllers.

Move some of the work previously done in enable_admin_queue
up to this new disconnect/connect logic.

Signed-off-by: Jim Harris <james.r.harris@intel.com>
Change-Id: I6239f0c0f36192db921d33f2322b1874b9382a01

Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/453939
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>
Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

lib/nvme/nvme_ctrlr.c
@@ -845,10 +845,15 @@ spdk_nvme_ctrlr_reset(struct spdk_nvme_ctrlr *ctrlr)
 	nvme_transport_admin_qpair_abort_aers(ctrlr->adminq);
 
 	/* Disable all queues before disabling the controller hardware. */
-	nvme_qpair_disable(ctrlr->adminq);
 	TAILQ_FOREACH(qpair, &ctrlr->active_io_qpairs, tailq) {
 		nvme_qpair_disable(qpair);
+		nvme_transport_ctrlr_disconnect_qpair(ctrlr, qpair);
 	}
+	nvme_qpair_disable(ctrlr->adminq);
+	nvme_qpair_complete_error_reqs(ctrlr->adminq);
+	nvme_transport_qpair_abort_reqs(ctrlr->adminq, 0 /* retry */);
+	nvme_transport_ctrlr_disconnect_qpair(ctrlr, ctrlr->adminq);
+	nvme_transport_ctrlr_connect_qpair(ctrlr, ctrlr->adminq);
 
 	/* Doorbell buffer config is invalid during reset */
 	nvme_ctrlr_free_doorbell_buffer(ctrlr);
@@ -1917,9 +1922,7 @@ static void
 nvme_ctrlr_enable_admin_queue(struct spdk_nvme_ctrlr *ctrlr)
 {
 	nvme_transport_qpair_reset(ctrlr->adminq);
-	nvme_qpair_complete_error_reqs(ctrlr->adminq);
 	nvme_qpair_enable(ctrlr->adminq);
-	nvme_transport_qpair_abort_reqs(ctrlr->adminq, 0 /* retry */);
 }
 
 /**

test/unit/lib/nvme/nvme_ctrlr.c/nvme_ctrlr_ut.c
@@ -174,6 +174,11 @@ nvme_transport_ctrlr_connect_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nv
 	return 0;
 }
 
+void
+nvme_transport_ctrlr_disconnect_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_qpair *qpair)
+{
+}
+
 int
 nvme_transport_qpair_reset(struct spdk_nvme_qpair *qpair)
 {

commit 101fce06fc9b54378c1bc130de90cb22848dc14b

nvme: don't disconnect qpairs from admin thread.
Disconnecting qpairs from the admin thread during a reset led to an
inevitable race with the data thread. QP related memory is freed during
the disconnect and cannot be touched from the other threads.

The only way to fix this is to force the qpair disconnect onto the
data thread.

fixes: bb01a089

Change-Id: I8a39e444c7cbbe85fafca42ffd040e929721ce95
Signed-off-by: Seth Howell <seth.howell@intel.com>

include/spdk/nvme.h
@@ -1091,6 +1091,8 @@ struct spdk_nvme_qpair *spdk_nvme_ctrlr_alloc_io_qpair(struct spdk_nvme_ctrlr *c
  * This function is intended to be called on qpairs that have already been connected,
  * but have since entered a failed state as indicated by a return value of -ENXIO from
  * either spdk_nvme_qpair_process_completions or one of the spdk_nvme_ns_cmd_* functions.
+ * This function touches qpair sensitive state so it must only be called from the same
+ * thread as spdk_nvme_qpair_process_completions and the spdk_nvme_ns_cmd_* functions.
  *
  * \param qpair The qpair to reconnect.
  *

lib/nvme/nvme_ctrlr.c
@@ -412,9 +412,13 @@ spdk_nvme_ctrlr_reconnect_io_qpair(struct spdk_nvme_qpair *qpair)
 		goto out;
 	}
 
+	/* We have to confirm that any old memory is cleaned up. */
+	nvme_transport_ctrlr_disconnect_qpair(ctrlr, qpair);
+
 	rc = nvme_transport_ctrlr_connect_qpair(ctrlr, qpair);
 	if (rc) {
 		nvme_qpair_set_state(qpair, NVME_QPAIR_DISABLED);
+		qpair->transport_qp_is_failed = true;
 		rc = -EAGAIN;
 		goto out;
 	}
@@ -1073,7 +1077,7 @@ spdk_nvme_ctrlr_reset(struct spdk_nvme_ctrlr *ctrlr)
 	/* Disable all queues before disabling the controller hardware. */
 	TAILQ_FOREACH(qpair, &ctrlr->active_io_qpairs, tailq) {
 		nvme_qpair_set_state(qpair, NVME_QPAIR_DISABLED);
-		nvme_transport_ctrlr_disconnect_qpair(ctrlr, qpair);
+		qpair->transport_qp_is_failed = true;
 	}
 	nvme_qpair_set_state(ctrlr->adminq, NVME_QPAIR_DISABLED);
 	nvme_qpair_complete_error_reqs(ctrlr->adminq);
@@ -1099,18 +1103,6 @@ spdk_nvme_ctrlr_reset(struct spdk_nvme_ctrlr *ctrlr)
 			SPDK_ERRLOG("controller reinitialization failed\n");
 			rc = -1;
 			break;
-		}
-	}
-
-	if (rc == 0) {
-		/* Reinitialize qpairs */
-		TAILQ_FOREACH(qpair, &ctrlr->active_io_qpairs, tailq) {
-			if (nvme_transport_ctrlr_connect_qpair(ctrlr, qpair) != 0) {
-				nvme_qpair_set_state(qpair, NVME_QPAIR_DISABLED);
-				rc = -1;
-				continue;
-			}
-			nvme_qpair_set_state(qpair, NVME_QPAIR_CONNECTED);
 		}
 	}
 

lib/nvme/nvme_qpair.c
@@ -458,7 +458,7 @@ spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_
 		 * qpair is not enabled, likely because a controller reset is
 		 *  in progress.
 		 */
-		return 0;
+		return -ENXIO;
 	}
 
 	/* error injection for those queued error requests */

test/unit/lib/nvme/nvme_qpair.c/nvme_qpair_ut.c
@@ -243,18 +243,18 @@ static void test_nvme_qpair_process_completions(void)
 	ctrlr.is_removed = false;
 	ctrlr.is_resetting = true;
 	rc = spdk_nvme_qpair_process_completions(&qpair, 0);
-	CU_ASSERT(rc == 0);
+	CU_ASSERT(rc == -ENXIO);
 	CU_ASSERT(g_called_transport_process_completions == false);
 	/* We also need to make sure we didn't abort the requests. */
 	CU_ASSERT(!STAILQ_EMPTY(&qpair.queued_req));
 	CU_ASSERT(g_num_cb_passed == 0);
 	CU_ASSERT(g_num_cb_failed == 0);
 
-	/* The case where we aren't resetting, but are enablign the qpair is the same as above. */
+	/* The case where we aren't resetting, but are enabling the qpair is the same as above. */
 	ctrlr.is_resetting = false;
 	qpair.state = NVME_QPAIR_ENABLING;
 	rc = spdk_nvme_qpair_process_completions(&qpair, 0);
-	CU_ASSERT(rc == 0);
+	CU_ASSERT(rc == -ENXIO);
 	CU_ASSERT(g_called_transport_process_completions == false);
 	CU_ASSERT(!STAILQ_EMPTY(&qpair.queued_req));
 	CU_ASSERT(g_num_cb_passed == 0);