db6cf19efc8509b697b0a84e2ff279a648afced3
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-11-08 07:25:48 +0900

    lib/iscsi: Ensure the last subtask frees the primary task for LUN hotplug
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: Ib160e9331c7b3ebd6222b445f1661bc24b963e59

--- lib/iscsi/conn.c
+++ lib/iscsi/conn.c
@@ -313,21 +313,26 @@ error_return:
 void
 spdk_iscsi_conn_free_pdu(struct spdk_iscsi_conn *conn, struct spdk_iscsi_pdu *pdu)
 {
+	struct spdk_iscsi_task *primary;
+
 	if (pdu->task) {
+		primary = spdk_iscsi_task_get_primary(pdu->task);
 		if (pdu->bhs.opcode == ISCSI_OP_SCSI_DATAIN) {
 			if (pdu->task->scsi.offset > 0) {
 				conn->data_in_cnt--;
 				if (pdu->bhs.flags & ISCSI_DATAIN_STATUS) {
 					/* Free the primary task after the last subtask done */
 					conn->data_in_cnt--;
-					spdk_iscsi_task_put(spdk_iscsi_task_get_primary(pdu->task));
+					spdk_iscsi_task_put(primary);
 				}
 				spdk_iscsi_conn_handle_queued_datain_tasks(conn);
 			}
 		} else if (pdu->bhs.opcode == ISCSI_OP_SCSI_RSP &&
 			   pdu->task->scsi.status != SPDK_SCSI_STATUS_GOOD) {
 			if (pdu->task->scsi.offset > 0) {
-				spdk_iscsi_task_put(spdk_iscsi_task_get_primary(pdu->task));
+				if (primary->bytes_completed == primary->scsi.transfer_len) {
+					spdk_iscsi_task_put(primary);
+				}
 			}
 		}
 		spdk_iscsi_task_put(pdu->task);
@@ -357,6 +362,7 @@ iscsi_conn_free_tasks(struct spdk_iscsi_conn *conn)
 	TAILQ_FOREACH_SAFE(iscsi_task, &conn->queued_datain_tasks, link, tmp_iscsi_task) {
 		if (!iscsi_task->is_queued) {
 			TAILQ_REMOVE(&conn->queued_datain_tasks, iscsi_task, link);
+			iscsi_task->scsi.transfer_len = iscsi_task->current_datain_offset;
 			spdk_iscsi_task_put(iscsi_task);
 		}
 	}
@@ -524,6 +530,7 @@ _iscsi_conn_remove_lun(void *_ctx)
 	TAILQ_FOREACH_SAFE(iscsi_task, &conn->queued_datain_tasks, link, tmp_iscsi_task) {
 		if ((!iscsi_task->is_queued) && (lun == iscsi_task->scsi.lun)) {
 			TAILQ_REMOVE(&conn->queued_datain_tasks, iscsi_task, link);
+			iscsi_task->scsi.transfer_len = iscsi_task->current_datain_offset;
 			spdk_iscsi_task_put(iscsi_task);
 		}
 	}
