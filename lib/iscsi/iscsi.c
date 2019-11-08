8bb29f36863f9be4c3c8d4115d61b71ee55fe403
Ziye Yang <ziye.yang@intel.com>
2019-11-08 23:16:14 +0800

    lib/iscsi: Make the data_in_cnt only used for subread tasks.
    Purpose: Do not let the primary task do the I/O if it is
    splitted into subtasks. This will make the code simplier,
    when all the sub I/Os are finished, we can free the
    primary task.
    
    Signed-off-by: Ziye Yang <ziye.yang@intel.com>
    Change-Id: I9bbe4b8dd92a2996f35ad810b33676e34670c77e

--- lib/iscsi/iscsi.c
+++ lib/iscsi/iscsi.c
@@ -3023,18 +3023,7 @@ iscsi_transfer_in(struct spdk_iscsi_conn *conn, struct spdk_iscsi_task *task)
 	if (task->scsi.status != SPDK_SCSI_STATUS_GOOD) {
 		if (task != primary) {
 			conn->data_in_cnt--;
-			/* Handle the case when primary task return success but the subtask failed */
-			if (primary->bytes_completed == primary->scsi.transfer_len &&
-			    primary->scsi.status == SPDK_SCSI_STATUS_GOOD) {
-				conn->data_in_cnt--;
-			}
-		} else {
-			/* handle the case that it is a primary task which has subtasks */
-			if (primary->scsi.transfer_len != primary->scsi.length) {
-				conn->data_in_cnt--;
-			}
 		}
-
 		return 0;
 	}
 
@@ -3248,20 +3237,6 @@ int spdk_iscsi_conn_handle_queued_datain_tasks(struct spdk_iscsi_conn *conn)
 	       conn->data_in_cnt < MAX_LARGE_DATAIN_PER_CONNECTION) {
 		task = TAILQ_FIRST(&conn->queued_datain_tasks);
 		assert(task->current_datain_offset <= task->scsi.transfer_len);
-
-		if (task->current_datain_offset == 0) {
-			task->scsi.lun = spdk_scsi_dev_get_lun(conn->dev, task->lun_id);
-			if (task->scsi.lun == NULL) {
-				TAILQ_REMOVE(&conn->queued_datain_tasks, task, link);
-				spdk_scsi_task_process_null_lun(&task->scsi);
-				spdk_iscsi_task_cpl(&task->scsi);
-				return 0;
-			}
-			task->current_datain_offset = task->scsi.length;
-			conn->data_in_cnt++;
-			iscsi_queue_task(conn, task);
-			continue;
-		}
 		if (task->current_datain_offset < task->scsi.transfer_len) {
 			struct spdk_iscsi_task *subtask;
 			uint32_t remaining_size = 0;
@@ -3270,22 +3245,22 @@ int spdk_iscsi_conn_handle_queued_datain_tasks(struct spdk_iscsi_conn *conn)
 			subtask = spdk_iscsi_task_get(conn, task, spdk_iscsi_task_cpl);
 			assert(subtask != NULL);
 			subtask->scsi.offset = task->current_datain_offset;
-			subtask->scsi.length = DMIN32(SPDK_BDEV_LARGE_BUF_MAX_SIZE, remaining_size);
 			spdk_scsi_task_set_data(&subtask->scsi, NULL, 0);
-			task->current_datain_offset += subtask->scsi.length;
 			conn->data_in_cnt++;
 
 			if (spdk_scsi_dev_get_lun(conn->dev, task->lun_id) == NULL) {
-				/* Remove the primary task from the list if this is the last subtask */
-				if (task->current_datain_offset == task->scsi.transfer_len) {
-					TAILQ_REMOVE(&conn->queued_datain_tasks, task, link);
-				}
-				subtask->scsi.transfer_len = subtask->scsi.length;
+				/* Stop splitting I/Os for remaining data when we detect LUN is removed */
+				TAILQ_REMOVE(&conn->queued_datain_tasks, task, link);
+				task->current_datain_offset += remaining_size;
+				assert(task->current_datain_offset == task->scsi.transfer_len);
+				subtask->scsi.transfer_len = remaining_size;
 				spdk_scsi_task_process_null_lun(&subtask->scsi);
 				spdk_iscsi_task_cpl(&subtask->scsi);
 				return 0;
 			}
 
+			subtask->scsi.length = DMIN32(SPDK_BDEV_LARGE_BUF_MAX_SIZE, remaining_size);
+			task->current_datain_offset += subtask->scsi.length;
 			iscsi_queue_task(conn, subtask);
 		}
 		if (task->current_datain_offset == task->scsi.transfer_len) {
