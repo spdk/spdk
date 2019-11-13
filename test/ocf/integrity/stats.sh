71396a9fa93b37b9777d9a63b657ba7c8945e971
Vitaliy Mysak <vitaliy.mysak@intel.com>
2019-11-13 09:20:03 +0100

    test/ocf: split tests for io types and get_stats
    In bdevperf-iotypes we used to call "bdev_ocf_get_stats" RPC
    which is not a part of IO types testing.
    As a result, we started to get failures caused by that rpc call which
    were not related to ocf bdev IO handling (issue #1025).
    
    This patch isolates the RPC call to separate test.
    
    Change-Id: I3a72ef52452868d49a6ec1ae4d3f9643496c2a28
    Signed-off-by: Vitaliy Mysak <vitaliy.mysak@intel.com>

--- test/ocf/integrity/stats.sh
+++ test/ocf/integrity/stats.sh
@@ -7,6 +7,9 @@ source $rootdir/test/common/autotest_common.sh
 bdevperf=$rootdir/test/bdev/bdevperf/bdevperf
 rpc_py="$rootdir/scripts/rpc.py"
 
-$bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -t 4 -w flush
-$bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -t 4 -w unmap
-$bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -t 4 -w write
+$bdevperf -c $curdir/mallocs.conf -q 128 -o 4096 -w write -t 120 -r /var/tmp/spdk.sock &
+bdev_perf_pid=$!
+waitforlisten $bdev_perf_pid
+sleep 1
+$rpc_py bdev_ocf_get_stats MalCache1
+killprocess $bdev_perf_pid
