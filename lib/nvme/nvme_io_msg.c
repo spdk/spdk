28a3abb8b2297c3bee40545a1e64a0d46cabf9fe
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-11-13 06:38:38 -0500

    lib/nvme: change api for io message
    API changes in this patch:
    
     1) nvme_io_msg_ctrlr_start                         => nvme_io_msg_ctrlr_register
     2) nvme_io_msg_ctrlr_stop with (shutdown == false) => nvme_io_msg_ctrlr_unregister
     3) nvme_io_msg_ctrlr_stop with (shutdown == true)  => nvme_io_msg_ctrlr_detach
    
    
    Change-Id: I60153ebbfb0d0b22575128d106f9333c3887213d
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- lib/nvme/nvme_io_msg.c
+++ lib/nvme/nvme_io_msg.c
@@ -107,7 +107,8 @@ spdk_nvme_io_msg_process(struct spdk_nvme_ctrlr *ctrlr)
 }
 
 int
-nvme_io_msg_ctrlr_start(struct spdk_nvme_ctrlr *ctrlr, struct nvme_io_msg_producer *io_msg_producer)
+nvme_io_msg_ctrlr_register(struct spdk_nvme_ctrlr *ctrlr,
+			   struct nvme_io_msg_producer *io_msg_producer)
 {
 	if (io_msg_producer == NULL) {
 		SPDK_ERRLOG("io_msg_producer cannot be NULL\n");
@@ -144,20 +145,27 @@ nvme_io_msg_ctrlr_start(struct spdk_nvme_ctrlr *ctrlr, struct nvme_io_msg_produc
 }
 
 void
-nvme_io_msg_ctrlr_stop(struct spdk_nvme_ctrlr *ctrlr, struct nvme_io_msg_producer *io_msg_producer,
-		       bool shutdown)
+nvme_io_msg_ctrlr_detach(struct spdk_nvme_ctrlr *ctrlr)
 {
-	if (STAILQ_EMPTY(&ctrlr->io_producers) && shutdown) {
-		/* Shutdown path with no producers registered = io msg ctrlr not started */
+	if (STAILQ_EMPTY(&ctrlr->io_producers)) {
 		return;
 	}
 
+	/* FIXIT! There are still registered producers, so we cannot
+	 *        safely detach/shutdown controller
+	 */
+	assert(false);
+}
+
+void
+nvme_io_msg_ctrlr_unregister(struct spdk_nvme_ctrlr *ctrlr,
+			     struct nvme_io_msg_producer *io_msg_producer)
+{
 	if (io_msg_producer != NULL) {
 		STAILQ_REMOVE(&ctrlr->io_producers, io_msg_producer, nvme_io_msg_producer, link);
 	}
 
-	if (!STAILQ_EMPTY(&ctrlr->io_producers) && !shutdown) {
-		/* There are still some registered producers */
+	if (!STAILQ_EMPTY(&ctrlr->io_producers)) {
 		return;
 	}
 
@@ -170,4 +178,6 @@ nvme_io_msg_ctrlr_stop(struct spdk_nvme_ctrlr *ctrlr, struct nvme_io_msg_produce
 	}
 
 	pthread_mutex_destroy(&ctrlr->external_io_msgs_lock);
+
+	nvme_io_msg_ctrlr_detach(ctrlr);
 }
