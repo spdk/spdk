1d8e212db443b6a12ddbe1f041d490eb8fcc7f7a
Seth Howell <seth.howell@intel.com>
2019-11-13 16:20:09 -0700

    test/nvmf: add a test for bad discovery ctrlr.
    Make sure we don't crash or do any other bad memory accesses when we
    fail to connect to a discovery controller.
    
    Change-Id: I7c3e25899bc78f4beff9ad017612bacf286d52c7
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/nvmf/host/target_disconnect.sh
+++ test/nvmf/host/target_disconnect.sh
@@ -29,6 +29,19 @@ timing_enter target_disconnect
 
 nvmftestinit
 
+# Test to make sure we don't segfault or access null pointers when we try to connect to
+# a discovery controller that doesn't exist yet.
+set +e
+$rootdir/examples/nvme/reconnect/reconnect -q 32 -o 4096 -w randrw -M 50 -t 10 -c 0xF \
+-r "trtype:$TEST_TRANSPORT adrfam:IPv4 traddr:$NVMF_FIRST_TARGET_IP trsvcid:$NVMF_PORT"
+# If the program crashes, the high bit of $? will be set so we will get a value in the hundreds.
+# But if the reconnect code detects errors and exits normally it will return 1.
+if [ $? != 1 ]; then
+	set -e
+	exit 1
+fi
+set -e
+
 disconnect_init $NVMF_FIRST_TARGET_IP
 
 # If perf doesn't shut down, this test will time out.
