c8718708b7c6ac7b908129bbeccb1326c19deca5
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-11-15 06:14:19 -0500

    [WIP] lib/nvme: remove device name parameter from nvme cuse
    This patch removes posibility to set cuse device path. Instead
    "/dev/spdk/nvme*" path is used.
    
    TODO: Detect device path conflict when multiple instances of
          SPDK are used.
    
    
    Change-Id: I7c3087772a3661eebe03fce21356c35cc8204b49
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- test/nvme/spdk_nvme_cli_cuse.sh
+++ test/nvme/spdk_nvme_cli_cuse.sh
@@ -19,7 +19,7 @@ waitforlisten $spdk_tgt_pid
 bdf=$(iter_pci_class_code 01 08 02 | head -1)
 
 $rpc_py bdev_nvme_attach_controller -b Nvme0 -t PCIe -a ${bdf}
-$rpc_py bdev_nvme_cuse_register -n Nvme0 -p spdk/nvme0
+$rpc_py bdev_nvme_cuse_register -n Nvme0
 
 sleep 5
 
