bfa31640e96d2f93c3b967049e89808fa9201573
Seth Howell <seth.howell@intel.com>
2019-11-15 11:33:32 -0700

    nvme/transport: don't return from NVME_TRANSPORT_CALL
    Returning directly from the NVME_TRANSPORT_CALL macro is really slick
    and allows us to avoid a lot of lines of code, but it doesn't allow us
    to do anything within the transport functions after calling the macro.
    
    There are several places where we want to be able to modify state within
    these functions before returning.
    
    Change-Id: Id4183ade3259e6c259218bef637ab9b94084cce5
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_transport.c
+++ lib/nvme/nvme_transport.c
@@ -44,34 +44,57 @@ nvme_transport_unknown(enum spdk_nvme_transport_type trtype)
 	SPDK_ERRLOG("Unknown transport %d\n", (int)trtype);
 	abort();
 }
-#define TRANSPORT_DEFAULT(trtype)	default: nvme_transport_unknown(trtype);
+#define TRANSPORT_DEFAULT(trtype)				default: nvme_transport_unknown(trtype);
 #else
 #define TRANSPORT_DEFAULT(trtype)
 #endif
 
-#define TRANSPORT_PCIE(func_name, args)	case SPDK_NVME_TRANSPORT_PCIE: return nvme_pcie_ ## func_name args;
+#define TRANSPORT_PCIE(return_var, func_name, args)		case SPDK_NVME_TRANSPORT_PCIE: return_var = nvme_pcie_ ## func_name args; break;
 
-#define TRANSPORT_FABRICS_TCP(func_name, args)	case SPDK_NVME_TRANSPORT_TCP: return nvme_tcp_ ## func_name args;
+#define TRANSPORT_FABRICS_TCP(return_var, func_name, args)	case SPDK_NVME_TRANSPORT_TCP: return_var = nvme_tcp_ ## func_name args; break;
 
 #ifdef SPDK_CONFIG_RDMA
-#define TRANSPORT_FABRICS_RDMA(func_name, args)	case SPDK_NVME_TRANSPORT_RDMA: return nvme_rdma_ ## func_name args;
-#define TRANSPORT_RDMA_AVAILABLE		true
+#define TRANSPORT_FABRICS_RDMA(return_var, func_name, args)	case SPDK_NVME_TRANSPORT_RDMA: return_var = nvme_rdma_ ## func_name args; break;
+#define TRANSPORT_RDMA_AVAILABLE				true
 #else
-#define TRANSPORT_FABRICS_RDMA(func_name, args)	case SPDK_NVME_TRANSPORT_RDMA: SPDK_UNREACHABLE();
-#define TRANSPORT_RDMA_AVAILABLE		false
+#define TRANSPORT_FABRICS_RDMA(return_var, func_name, args)	case SPDK_NVME_TRANSPORT_RDMA: SPDK_UNREACHABLE();
+#define TRANSPORT_RDMA_AVAILABLE				false
 #endif
-#define TRANSPORT_FABRICS_FC(func_name, args)	case SPDK_NVME_TRANSPORT_FC: SPDK_UNREACHABLE();
+#define TRANSPORT_FABRICS_FC(return_var, func_name, args)	case SPDK_NVME_TRANSPORT_FC: SPDK_UNREACHABLE();
 
-#define NVME_TRANSPORT_CALL(trtype, func_name, args)		\
-	do {							\
-		switch (trtype) {				\
-		TRANSPORT_PCIE(func_name, args)			\
-		TRANSPORT_FABRICS_RDMA(func_name, args)		\
-		TRANSPORT_FABRICS_FC(func_name, args)		\
-		TRANSPORT_FABRICS_TCP(func_name, args)		\
-		TRANSPORT_DEFAULT(trtype)			\
-		}						\
-		SPDK_UNREACHABLE();				\
+#define TRANSPORT_PCIE_V(func_name, args)			case SPDK_NVME_TRANSPORT_PCIE: nvme_pcie_ ## func_name args; break;
+
+#define TRANSPORT_FABRICS_TCP_V(func_name, args)		case SPDK_NVME_TRANSPORT_TCP: nvme_tcp_ ## func_name args; break;
+
+#ifdef SPDK_CONFIG_RDMA
+#define TRANSPORT_FABRICS_RDMA_V(func_name, args)		case SPDK_NVME_TRANSPORT_RDMA: nvme_rdma_ ## func_name args; break;
+#define TRANSPORT_RDMA_AVAILABLE				true
+#else
+#define TRANSPORT_FABRICS_RDMA_V(func_name, args)		case SPDK_NVME_TRANSPORT_RDMA: SPDK_UNREACHABLE();
+#define TRANSPORT_RDMA_AVAILABLE				false
+#endif
+#define TRANSPORT_FABRICS_FC_V(func_name, args)			case SPDK_NVME_TRANSPORT_FC: SPDK_UNREACHABLE();
+
+#define NVME_TRANSPORT_CALL(trtype, return_var, func_name, args)	\
+	do {								\
+		switch (trtype) {					\
+		TRANSPORT_PCIE(return_var, func_name, args)		\
+		TRANSPORT_FABRICS_RDMA(return_var, func_name, args)	\
+		TRANSPORT_FABRICS_FC(return_var, func_name, args)	\
+		TRANSPORT_FABRICS_TCP(return_var, func_name, args)	\
+		TRANSPORT_DEFAULT(trtype)				\
+		}							\
+	} while (0)
+
+#define NVME_TRANSPORT_CALL_V(trtype, func_name, args)			\
+	do {								\
+		switch (trtype) {					\
+		TRANSPORT_PCIE_V(func_name, args)			\
+		TRANSPORT_FABRICS_RDMA_V(func_name, args)		\
+		TRANSPORT_FABRICS_FC_V(func_name, args)			\
+		TRANSPORT_FABRICS_TCP_V(func_name, args)		\
+		TRANSPORT_DEFAULT(trtype)				\
+		}							\
 	} while (0)
 
 bool
@@ -96,135 +119,192 @@ struct spdk_nvme_ctrlr *nvme_transport_ctrlr_construct(const struct spdk_nvme_tr
 		const struct spdk_nvme_ctrlr_opts *opts,
 		void *devhandle)
 {
-	NVME_TRANSPORT_CALL(trid->trtype, ctrlr_construct, (trid, opts, devhandle));
+	struct spdk_nvme_ctrlr *ctrlr = NULL;
+
+	NVME_TRANSPORT_CALL(trid->trtype, ctrlr, ctrlr_construct, (trid, opts, devhandle));
+	return ctrlr;
 }
 
 int
 nvme_transport_ctrlr_scan(struct spdk_nvme_probe_ctx *probe_ctx,
 			  bool direct_connect)
 {
-	NVME_TRANSPORT_CALL(probe_ctx->trid.trtype, ctrlr_scan, (probe_ctx, direct_connect));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(probe_ctx->trid.trtype, rc, ctrlr_scan, (probe_ctx, direct_connect));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_destruct(struct spdk_nvme_ctrlr *ctrlr)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_destruct, (ctrlr));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_destruct, (ctrlr));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_enable(struct spdk_nvme_ctrlr *ctrlr)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_enable, (ctrlr));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_enable, (ctrlr));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_set_reg_4(struct spdk_nvme_ctrlr *ctrlr, uint32_t offset, uint32_t value)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_set_reg_4, (ctrlr, offset, value));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_set_reg_4, (ctrlr, offset, value));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_set_reg_8(struct spdk_nvme_ctrlr *ctrlr, uint32_t offset, uint64_t value)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_set_reg_8, (ctrlr, offset, value));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_set_reg_8, (ctrlr, offset, value));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_get_reg_4(struct spdk_nvme_ctrlr *ctrlr, uint32_t offset, uint32_t *value)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_get_reg_4, (ctrlr, offset, value));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_get_reg_4, (ctrlr, offset, value));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_get_reg_8(struct spdk_nvme_ctrlr *ctrlr, uint32_t offset, uint64_t *value)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_get_reg_8, (ctrlr, offset, value));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_get_reg_8, (ctrlr, offset, value));
+	return rc;
 }
 
 uint32_t
 nvme_transport_ctrlr_get_max_xfer_size(struct spdk_nvme_ctrlr *ctrlr)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_get_max_xfer_size, (ctrlr));
+	uint32_t rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_get_max_xfer_size, (ctrlr));
+	return rc;
 }
 
 uint16_t
 nvme_transport_ctrlr_get_max_sges(struct spdk_nvme_ctrlr *ctrlr)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_get_max_sges, (ctrlr));
+	uint16_t rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_get_max_sges, (ctrlr));
+	return rc;
 }
 
 void *
 nvme_transport_ctrlr_alloc_cmb_io_buffer(struct spdk_nvme_ctrlr *ctrlr, size_t size)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_alloc_cmb_io_buffer, (ctrlr, size));
+	void *buffer = NULL;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, buffer, ctrlr_alloc_cmb_io_buffer, (ctrlr, size));
+	return buffer;
 }
 
 int
 nvme_transport_ctrlr_free_cmb_io_buffer(struct spdk_nvme_ctrlr *ctrlr, void *buf, size_t size)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_free_cmb_io_buffer, (ctrlr, buf, size));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_free_cmb_io_buffer, (ctrlr, buf, size));
+	return rc;
 }
 
 struct spdk_nvme_qpair *
 nvme_transport_ctrlr_create_io_qpair(struct spdk_nvme_ctrlr *ctrlr, uint16_t qid,
 				     const struct spdk_nvme_io_qpair_opts *opts)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_create_io_qpair, (ctrlr, qid, opts));
+	struct spdk_nvme_qpair *qpair = NULL;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, qpair, ctrlr_create_io_qpair, (ctrlr, qid, opts));
+	return qpair;
 }
 
 int
 nvme_transport_ctrlr_delete_io_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_qpair *qpair)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_delete_io_qpair, (ctrlr, qpair));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_delete_io_qpair, (ctrlr, qpair));
+	return rc;
 }
 
 int
 nvme_transport_ctrlr_connect_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_qpair *qpair)
 {
+	int rc = 0;
+
 	nvme_qpair_set_state(qpair, NVME_QPAIR_CONNECTING);
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_connect_qpair, (ctrlr, qpair));
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, rc, ctrlr_connect_qpair, (ctrlr, qpair));
+	return rc;
 }
 
 volatile struct spdk_nvme_registers *
 nvme_transport_ctrlr_get_registers(struct spdk_nvme_ctrlr *ctrlr)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_get_registers, (ctrlr));
+	volatile struct spdk_nvme_registers *regs = NULL;
+	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, regs, ctrlr_get_registers, (ctrlr));
+
+	return regs;
 }
 
 void
 nvme_transport_ctrlr_disconnect_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_qpair *qpair)
 {
-	NVME_TRANSPORT_CALL(ctrlr->trid.trtype, ctrlr_disconnect_qpair, (ctrlr, qpair));
+	NVME_TRANSPORT_CALL_V(ctrlr->trid.trtype, ctrlr_disconnect_qpair, (ctrlr, qpair));
 }
 
 void
 nvme_transport_qpair_abort_reqs(struct spdk_nvme_qpair *qpair, uint32_t dnr)
 {
 	assert(dnr <= 1);
-	NVME_TRANSPORT_CALL(qpair->trtype, qpair_abort_reqs, (qpair, dnr));
+	NVME_TRANSPORT_CALL_V(qpair->trtype, qpair_abort_reqs, (qpair, dnr));
 }
 
 int
 nvme_transport_qpair_reset(struct spdk_nvme_qpair *qpair)
 {
-	NVME_TRANSPORT_CALL(qpair->trtype, qpair_reset, (qpair));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(qpair->trtype, rc, qpair_reset, (qpair));
+	return rc;
 }
 
 int
 nvme_transport_qpair_submit_request(struct spdk_nvme_qpair *qpair, struct nvme_request *req)
 {
-	NVME_TRANSPORT_CALL(qpair->trtype, qpair_submit_request, (qpair, req));
+	int rc = 0;
+
+	NVME_TRANSPORT_CALL(qpair->trtype, rc, qpair_submit_request, (qpair, req));
+	return rc;
 }
 
 int32_t
 nvme_transport_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_completions)
 {
-	NVME_TRANSPORT_CALL(qpair->trtype, qpair_process_completions, (qpair, max_completions));
+	int32_t rc = 0;
+
+	NVME_TRANSPORT_CALL(qpair->trtype, rc, qpair_process_completions, (qpair, max_completions));
+	return rc;
 }
 
 void
 nvme_transport_admin_qpair_abort_aers(struct spdk_nvme_qpair *qpair)
 {
-	NVME_TRANSPORT_CALL(qpair->trtype, admin_qpair_abort_aers, (qpair));
+	NVME_TRANSPORT_CALL_V(qpair->trtype, admin_qpair_abort_aers, (qpair));
 }
