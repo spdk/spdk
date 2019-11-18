a80913d85d96b4b0f8887724a9bdffe6c8b8956d
Jim Harris <james.r.harris@intel.com>
2019-11-18 15:43:33 -0700

    bdev: rename spdk_bdev_get_io to bdev_channel_get_io
    This function is not part of the SPDK public API, so
    remove the spdk_ prefix from it to make it more clear
    this is internal to the SPDK bdev library.
    
    Also add "channel" to the name since this function is
    operating on the channel parameter.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I57897ab4bf601b90551259b7cf6efa63152ed02f

--- lib/bdev/bdev_internal.h
+++ lib/bdev/bdev_internal.h
@@ -40,7 +40,7 @@ struct spdk_bdev;
 struct spdk_bdev_io;
 struct spdk_bdev_channel;
 
-struct spdk_bdev_io *spdk_bdev_get_io(struct spdk_bdev_channel *channel);
+struct spdk_bdev_io *bdev_channel_get_io(struct spdk_bdev_channel *channel);
 
 void bdev_io_init(struct spdk_bdev_io *bdev_io, struct spdk_bdev *bdev, void *cb_arg,
 		       spdk_bdev_io_completion_cb cb);
