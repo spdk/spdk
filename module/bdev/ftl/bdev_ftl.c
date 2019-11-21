9b847830ee3ea1c0cdb0b514ed869dccc05f26e7
Wojciech Malikowski <wojciech.malikowski@intel.com>
2019-11-21 13:31:56 +0100

    bdev/ftl: Complete io immediately
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Change-Id: I19d42f3dc1faad6bb0094681bfe90ad29b88aae4

--- module/bdev/ftl/bdev_ftl.c
+++ module/bdev/ftl/bdev_ftl.c
@@ -172,8 +172,7 @@ bdev_ftl_cb(void *arg, int status)
 
 	io->status = status;
 
-	cnt = spdk_ring_enqueue(io->ring, (void **)&io, 1, NULL);
-	assert(cnt == 1);
+	bdev_ftl_complete_io(io, 0);
 }
 
 static int
