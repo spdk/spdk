b3978fc8da12d0e50c0beb0ab095c2474c236248
Seth Howell <seth.howell@intel.com>
2019-11-25 16:17:50 -0700

    [RFC] nvme: add an enum for why a qpair disconnected
    Change-Id: I1a9517d9673051615942c873416505704740691a
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- lib/nvme/nvme_internal.h
+++ lib/nvme/nvme_internal.h
@@ -362,6 +362,8 @@ struct spdk_nvme_qpair {
 	 */
 	uint8_t				no_deletion_notification_needed: 1;
 
+	uint8_t				transport_failure_reason: 2;
+
 	enum spdk_nvme_transport_type	trtype;
 
 	STAILQ_HEAD(, nvme_request)	free_req;
