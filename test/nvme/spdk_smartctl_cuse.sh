eea2f98a96c7f98f4a07bb935d026c70dc9c9bd3
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-11-25 09:29:32 -0500

    test/nvme: add cuse smartctl tests
    TODO: Verify data provided by smartctl. For now output is just
          dumped as json logs.
    
    Change-Id: Id6ed39a4e6c4d360111aea96f630bb678f5b306e
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

---
+++ test/nvme/spdk_smartctl_cuse.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+source $rootdir/scripts/common.sh
+source $rootdir/test/common/autotest_common.sh
+
+timing_enter nvme_cli_cuse
+
+SMARTCTL_CMD='smartctl -d nvme,0xffffffff'
+rpc_py=$rootdir/scripts/rpc.py
+
+$rootdir/app/spdk_tgt/spdk_tgt -m 0x3 &
+spdk_tgt_pid=$!
+trap 'kill -9 ${spdk_tgt_pid}; exit 1' SIGINT SIGTERM EXIT
+
+waitforlisten $spdk_tgt_pid
+
+bdf=$(iter_pci_class_code 01 08 02 | head -1)
+
+$rpc_py bdev_nvme_attach_controller -b Nvme0 -t PCIe -a ${bdf}
+$rpc_py bdev_nvme_cuse_register -n Nvme0
+
+sleep 5
+
+if [ ! -c /dev/spdk/nvme0 ]; then
+	return 1
+fi
+
+${SMARTCTL_CMD} -j -a /dev/spdk/nvme0 || true
+
+${SMARTCTL_CMD} -j -i /dev/spdk/nvme0n1 || true
+
+${SMARTCTL_CMD} -j -c /dev/spdk/nvme0 || true
+
+${SMARTCTL_CMD} -j -H /dev/spdk/nvme0 || true
+
+${SMARTCTL_CMD} -j -A /dev/spdk/nvme0 || true
+
+${SMARTCTL_CMD} -j -l error /dev/spdk/nvme0 || true
+
+${SMARTCTL_CMD} -j -t select,10-20 /dev/spdk/nvme0n1 || true
+
+${SMARTCTL_CMD} -j -t select,10+11 /dev/spdk/nvme0n1 || true
+
+${SMARTCTL_CMD} -j -x /dev/spdk/nvme0 || true
+
+$rpc_py bdev_nvme_detach_controller Nvme0
+sleep 1
+if [ -c /dev/spdk/nvme1 ]; then
+	return 1
+fi
+
+trap - SIGINT SIGTERM EXIT
+killprocess $spdk_tgt_pid
+
+report_test_completion spdk_nvme_cli_cuse
+timing_exit nvme_cli_cuse
