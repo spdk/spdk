3488a6a0cb996167c3739d9ba4f15d26e25f6803
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2019-11-25 09:29:32 -0500

    test/nvme: add cuse smartctl tests
    Change-Id: Id6ed39a4e6c4d360111aea96f630bb678f5b306e
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -180,6 +180,7 @@ if [ $SPDK_RUN_FUNCTIONAL_TEST -eq 1 ]; then
 		fi
 		if [[ $SPDK_TEST_NVME_CUSE -eq 1 ]]; then
 			run_test suite test/nvme/spdk_nvme_cli_cuse.sh
+			run_test suite test/nvme/spdk_smartctl_cuse.sh
 		fi
 		# Only test hotplug without ASAN enabled. Since if it is
 		# enabled, it catches SEGV earlier than our handler which
