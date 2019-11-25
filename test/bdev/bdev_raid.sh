40e4e420b838114c87a971d8abdb321780bd605e
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-11-25 10:57:47 +0100

    Fixed nightly error in bdev_raid.sh
    In nightly tests there was error:
        rpc.py: error: unrecognized arguments: Base_2"
    caused by patch: 029251878d59870eb1e2cc17fc36de68f1fa2df8
    The problem was caused by chars escapes.
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: If6ebce1de5845bc14a957f4b68250da163309a05
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/475676
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- test/bdev/bdev_raid.sh
+++ test/bdev/bdev_raid.sh
@@ -65,7 +65,7 @@ function configure_raid_bdev() {
 	cat <<- EOL >> $testdir/rpcs.txt
 	bdev_malloc_create 32 512 -b Base_1
 	bdev_malloc_create 32 512 -b Base_2
-	bdev_raid_create -z 64 -r 0 -b \"Base_1 Base_2\" -n raid0
+	bdev_raid_create -z 64 -r 0 -b "Base_1 Base_2" -n raid0
 	EOL
 	$rpc_py < $testdir/rpcs.txt
 
