78f045fb1908d9c1496e3a88e6f4f9f2ab2f377e
GangCao <gang.cao@intel.com>
2019-11-27 14:15:22 -0500

    RPC: release the resource in error case
    Change-Id: Ie1757ef7b3e382492448f2fca0f5a165cdfac08c
    Signed-off-by: GangCao <gang.cao@intel.com>

--- lib/rpc/rpc.c
+++ lib/rpc/rpc.c
@@ -157,13 +157,24 @@ spdk_rpc_listen(const char *listen_addr)
 			return -1;
 		}
 
-		snprintf(g_rpc_lock_path, sizeof(g_rpc_lock_path), "%s.lock",
-			 g_rpc_listen_addr_unix.sun_path);
+		rc = snprintf(g_rpc_lock_path, sizeof(g_rpc_lock_path), "%s.lock",
+			      g_rpc_listen_addr_unix.sun_path);
+		if (rc < 0 || (size_t)rc >= sizeof(g_rpc_lock_path)) {
+			SPDK_ERRLOG("RPC lock path too long\n");
+			unlink(g_rpc_listen_addr_unix.sun_path);
+			g_rpc_listen_addr_unix.sun_path[0] = '\0';
+			g_rpc_lock_path[0] = '\0';
+			return -1;
+		}
 
 		g_rpc_lock_fd = open(g_rpc_lock_path, O_RDONLY | O_CREAT, 0600);
 		if (g_rpc_lock_fd == -1) {
 			SPDK_ERRLOG("Cannot open lock file %s: %s\n",
 				    g_rpc_lock_path, spdk_strerror(errno));
+			unlink(g_rpc_lock_path);
+			g_rpc_lock_path[0] = '\0';
+			unlink(g_rpc_listen_addr_unix.sun_path);
+			g_rpc_listen_addr_unix.sun_path[0] = '\0';
 			return -1;
 		}
 
@@ -171,6 +182,12 @@ spdk_rpc_listen(const char *listen_addr)
 		if (rc != 0) {
 			SPDK_ERRLOG("RPC Unix domain socket path %s in use. Specify another.\n",
 				    g_rpc_listen_addr_unix.sun_path);
+			close(g_rpc_lock_fd);
+			g_rpc_lock_fd = -1;
+			unlink(g_rpc_lock_path);
+			g_rpc_lock_path[0] = '\0';
+			unlink(g_rpc_listen_addr_unix.sun_path);
+			g_rpc_listen_addr_unix.sun_path[0] = '\0';
 			return -1;
 		}
 
@@ -179,6 +196,7 @@ spdk_rpc_listen(const char *listen_addr)
 		 * if it still exists from a previous process.
 		 */
 		unlink(g_rpc_listen_addr_unix.sun_path);
+		g_rpc_listen_addr_unix.sun_path[0] = '\0';
 
 		g_jsonrpc_server = spdk_jsonrpc_server_listen(AF_UNIX, 0,
 				   (struct sockaddr *)&g_rpc_listen_addr_unix,
