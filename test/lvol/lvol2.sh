b633d27e0c04e7cf7eb65afa0cf0203c4b320528
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-12-01 16:43:50 -0500

    test/lvol: rewrite snapshot_readonly and snapshot_compare_with_lvol_bdev to bash
    Change-Id: I27e58b05c3c8e55b06ea64f1640c2a5702449b26
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/lvol/lvol2.sh
+++ test/lvol/lvol2.sh
@@ -11,6 +11,7 @@ run_test suite test/lvol/basic.sh
 run_test suite test/lvol/resize.sh
 run_test suite test/lvol/hotremove.sh
 run_test suite test/lvol/tasting.sh
+run_test suite test/lvol/snapshot_clone.sh
 timing_exit basic
 
 timing_exit lvol
