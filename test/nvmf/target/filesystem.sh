2e2c64669bd104eb3acf0a0c0dc793eff1a62b68
Seth Howell <seth.howell@intel.com>
2019-12-05 14:05:18 -0700

    test/nvmf/filesystem: explicitly use nvme0n1
    This test has always relied on /dev/nvme0n1 being the location of our
    test drive which means that it is the only one in the system at the time
    the test is run.
    
    So instead of trying to loop multiple non-existant nvme drives, just
    call it out directly.
    
    Also, add a todo to remove the dependence on NVMe0n1 since we could run
    this test on a machine where the main filesystem is on an NVMe drive
    which would break this test.
    
    Change-Id: Ibb2448d9367a5c80d85a1c91b0c6e44e58237751
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/nvmf/target/filesystem.sh
+++ test/nvmf/target/filesystem.sh
@@ -12,11 +12,31 @@ rpc_py="$rootdir/scripts/rpc.py"
 
 nvmftestinit
 
-for incapsule in 0 4096; do
+function nvmf_filesystem_create {
+	fstype=$1
+
+	if [ $fstype = ext4 ]; then
+		force=-F
+	else
+		force=-f
+	fi
+
+	mkfs.${fstype} $force /dev/nvme0n1p1
+
+	mount /dev/nvme0n1p1 /mnt/device
+	touch /mnt/device/aaa
+	sync
+	rm /mnt/device/aaa
+	sync
+	umount /mnt/device
+}
+
+function nvmf_filesystem_part {
+	incapsule=$1
+
 	nvmfappstart "-m 0xF"
 
 	$rpc_py nvmf_create_transport $NVMF_TRANSPORT_OPTS -u 8192 -c $incapsule
-
 	$rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc1
 	$rpc_py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
 	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Malloc1
@@ -24,40 +44,28 @@ for incapsule in 0 4096; do
 
 	nvme connect -t $TEST_TRANSPORT -n "nqn.2016-06.io.spdk:cnode1" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
 
+	# TODO: fix this to wait for the proper NVMe device.
+	# if we are hosting the local filesystem on an NVMe drive, this test will fail
+	# because it relies on the no other NVMe drives being present in the system.
 	waitforblk "nvme0n1"
 
 	mkdir -p /mnt/device
 
-	devs=$(lsblk -l -o NAME | grep nvme)
+	parted -s /dev/nvme0n1 mklabel msdos  mkpart primary '0%' '100%'
+	partprobe
+	sleep 1
 
-	for dev in $devs; do
-		timing_enter parted
-		parted -s /dev/$dev mklabel msdos  mkpart primary '0%' '100%'
-		partprobe
-		timing_exit parted
-		sleep 1
+	if [ $incapsule -eq 0 ]; then
+		run_test "case" "filesystem_ext4" nvmf_filesystem_create "ext4"
+		run_test "case" "filesystem_btrfs" nvmf_filesystem_create "btrfs"
+		run_test "case" "filesystem_xfs" nvmf_filesystem_create "xfs"
+	else
+		run_test "case" "filesystem_incapsule_ext4" nvmf_filesystem_create "ext4"
+		run_test "case" "filesystem_incapsule_btrfs" nvmf_filesystem_create "btrfs"
+		run_test "case" "filesystem_incapsule_xfs" nvmf_filesystem_create "xfs"
+	fi
 
-		for fstype in "ext4" "btrfs" "xfs"; do
-			timing_enter $fstype
-			if [ $fstype = ext4 ]; then
-				force=-F
-			else
-				force=-f
-			fi
-
-			mkfs.${fstype} $force /dev/${dev}p1
-
-			mount /dev/${dev}p1 /mnt/device
-			touch /mnt/device/aaa
-			sync
-			rm /mnt/device/aaa
-			sync
-			umount /mnt/device
-			timing_exit $fstype
-		done
-
-		parted -s /dev/$dev rm 1
-	done
+	parted -s /dev/nvme0n1 rm 1
 
 	sync
 	nvme disconnect -n "nqn.2016-06.io.spdk:cnode1" || true
@@ -67,6 +75,9 @@ for incapsule in 0 4096; do
 	trap - SIGINT SIGTERM EXIT
 
 	killprocess $nvmfpid
-done
+}
+
+run_test "suite" "nvmf_filesystem_no_incapsule" nvmf_filesystem_part 0
+run_test "suite" "nvmf_filesystem_incapsule" nvmf_filesystem_part 4096
 
 nvmftestfini
