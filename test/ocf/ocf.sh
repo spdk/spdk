906f7089d207cf5a1a658f076ab7a0b6acf31d2d
Seth Howell <seth.howell@intel.com>
2019-12-05 13:07:44 -0700

    test/ocf: use run_test framework directly.
    Change-Id: I7363102aac4363e1d8cf8e68281ad869afb4269b
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/ocf/ocf.sh
+++ test/ocf/ocf.sh
@@ -5,17 +5,12 @@ rootdir=$(readlink -f $testdir/../..)
 
 source $rootdir/test/common/autotest_common.sh
 
-function suite()
-{
-	run_test suite "ocf_$(basename "$@")" "$@"
-}
-
-suite "$testdir/integrity/fio-modes.sh"
-suite "$testdir/integrity/bdevperf-iotypes.sh"
-suite "$testdir/integrity/stats.sh"
-suite "$testdir/management/create-destruct.sh"
-suite "$testdir/management/multicore.sh"
-suite "$testdir/management/persistent-metadata.sh"
-suite "$testdir/management/remove.sh"
+run_test "case" "ocf_fio_modes" "$testdir/integrity/fio-modes.sh"
+run_test "case" "ocf_bdevperf_iotypes" "$testdir/integrity/bdevperf-iotypes.sh"
+run_test "case" "ocf_stats" "$testdir/integrity/stats.sh"
+run_test "case" "ocf_create_destruct" "$testdir/management/create-destruct.sh"
+run_test "case" "ocf_multicore" "$testdir/management/multicore.sh"
+run_test "case" "ocf_persistent_metadata" "$testdir/management/persistent-metadata.sh"
+run_test "case" "ocf_remove" "$testdir/management/remove.sh"
 
 report_test_completion "ocf"
