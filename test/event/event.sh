671ced3b6da129f2df6cb636526dbd427eb5b50f
Seth Howell <seth.howell@intel.com>
2019-12-05 12:29:32 -0700

    test/event: follow test_run framework
    Change-Id: I6159292441269819eebcf858a6b7447a95cbbc7c
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/event/event.sh
+++ test/event/event.sh
@@ -4,7 +4,7 @@ testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
 
-$testdir/event_perf/event_perf -m 0xF -t 1
-$testdir/reactor/reactor -t 1
-$testdir/reactor_perf/reactor_perf -t 1
+run_test "case" "event_perf" $testdir/event_perf/event_perf -m 0xF -t 1
+run_test "case" "event_reactor" $testdir/reactor/reactor -t 1
+run_test "case" "event_reactor_perf" $testdir/reactor_perf/reactor_perf -t 1
 report_test_completion "event"
