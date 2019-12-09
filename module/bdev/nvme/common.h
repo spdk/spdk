0f54adb290520f4c4ff4349e83f7a8753868a858
Konrad Sztyber <konrad.sztyber@intel.com>
2019-12-09 11:18:35 +0100

    bdev/nvme: mark controllers used by bdev_ftl
    Currently, the controllers managed by bdev_ftl are skipped in
    bdev_nvme's config_json output by verifying if they support Open
    Channel.  Since new bdev_ocssd also uses Open Channel controllers and
    it relies on bdev_nvme's config_json, additional flag was added to mark
    that a controller is used by bdev_ftl.
    
    This is a temporary solution that should be removed once bdev_ftl
    becomes a virtual bdev and starts using bdevs instead of NVMe
    controllers.
    
    Change-Id: Ib25b61a72f0912d7a51119357f5c221941af50ad
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- module/bdev/nvme/common.h
+++ module/bdev/nvme/common.h
@@ -93,6 +93,14 @@ struct nvme_bdev_ctrlr {
 
 	struct spdk_poller		*adminq_timer_poller;
 
+	/**
+	 * Temporary workaround to distinguish between controllers managed by
+	 * bdev_ocssd and those used by bdev_ftl.  Once bdev_ftl becomes a
+	 * virtual bdev and starts using bdevs instead of controllers, this flag
+	 * can be removed.
+	 */
+	bool				ftl_managed;
+
 	/** linked list pointer for device list */
 	TAILQ_ENTRY(nvme_bdev_ctrlr)	tailq;
 };
