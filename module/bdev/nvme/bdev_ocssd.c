db785a64bb7da4e9fcb3ad55df7c1b42998c5504
Konrad Sztyber <konrad.sztyber@intel.com>
2019-12-09 13:40:23 +0100

    bdev/ocssd: unmap command
    Added support for unmap command.  It works very similarly to a zone
    reset, but requires the user to pass the offset and length in blocks
    instead of zones.
    
    Change-Id: I3921f38b8b67ea59de92d78a19303b1da4f1011a
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- module/bdev/nvme/bdev_ocssd.c
+++ module/bdev/nvme/bdev_ocssd.c
@@ -574,6 +574,28 @@ bdev_ocssd_reset_zone(struct spdk_io_channel *ioch, struct spdk_bdev_io *bdev_io
 	return rc;
 }
 
+static int
+bdev_ocssd_unmap(struct spdk_io_channel *ioch, struct spdk_bdev_io *bdev_io)
+{
+	struct nvme_bdev *bdev = bdev_io->bdev->ctxt;
+	uint64_t zone_size = bdev->disk.zone_size;
+	uint64_t num_zones = bdev_io->u.bdev.num_blocks / zone_size;
+
+	if (bdev_io->u.bdev.offset_blocks % zone_size != 0) {
+		SPDK_ERRLOG("Unaligned zone address for unmap request: %"PRIu64"\n",
+			    bdev_io->u.bdev.offset_blocks);
+		return -EINVAL;
+	}
+
+	if (bdev_io->u.bdev.num_blocks % zone_size != 0) {
+		SPDK_ERRLOG("Unaligned length for zone unmap request: %"PRIu64"\n",
+			    bdev_io->u.bdev.num_blocks);
+		return -EINVAL;
+	}
+
+	return bdev_ocssd_reset_zone(ioch, bdev_io, bdev_io->u.bdev.offset_blocks, num_zones);
+}
+
 static int _bdev_ocssd_get_zone_info(struct spdk_bdev_io *bdev_io);
 
 static void
@@ -710,6 +732,10 @@ _bdev_ocssd_submit_request(struct spdk_io_channel *ioch, struct spdk_bdev_io *bd
 		rc = bdev_ocssd_write(ioch, bdev_io);
 		break;
 
+	case SPDK_BDEV_IO_TYPE_UNMAP:
+		rc = bdev_ocssd_unmap(ioch, bdev_io);
+		break;
+
 	case SPDK_BDEV_IO_TYPE_ZONE_MANAGEMENT:
 		rc = bdev_ocssd_zone_management(ioch, bdev_io);
 		break;
@@ -796,6 +822,7 @@ bdev_ocssd_io_type_supported(void *ctx, enum spdk_bdev_io_type type)
 	case SPDK_BDEV_IO_TYPE_WRITE:
 	case SPDK_BDEV_IO_TYPE_ZONE_MANAGEMENT:
 	case SPDK_BDEV_IO_TYPE_ZONE_APPEND:
+	case SPDK_BDEV_IO_TYPE_UNMAP:
 		return true;
 
 	default:
