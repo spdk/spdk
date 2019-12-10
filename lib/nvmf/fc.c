f038354efa4889f5cd96f8a1ca2d06b30d604b6b
Seth Howell <seth.howell@intel.com>
2019-12-10 13:12:55 -0700

    lib/nvmf: enable pluggable NVMe-oF transports.
    Change-Id: If1fd7d6c2385f42ca32dea0f8ecb528a60778d40
    Signed-off-by: Seth Howell <seth.howell@intel.com>
    Reviewed-on: https://review.gerrithub.io/c/spdk/spdk/+/477504
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- lib/nvmf/fc.c
+++ lib/nvmf/fc.c
@@ -3950,5 +3950,6 @@ done:
 	return err;
 }
 
+SPDK_NVMF_TRANSPORT_REGISTER(fc, &spdk_nvmf_transport_fc);
 SPDK_LOG_REGISTER_COMPONENT("nvmf_fc_adm_api", SPDK_LOG_NVMF_FC_ADM_API);
 SPDK_LOG_REGISTER_COMPONENT("nvmf_fc", SPDK_LOG_NVMF_FC)
