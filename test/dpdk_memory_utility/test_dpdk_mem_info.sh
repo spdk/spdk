8aef7c7b8308b32d8423534f8c1c13a77fc4466a
Seth Howell <seth.howell@intel.com>
2019-12-12 14:41:38 -0700

    test: add test to validate dpdk mem utility
    This test essentially confirms that the new RPC properly creates a test
    file and that the script can parse that file and run through a basic set
    of operations and exit without an error.
    
    Change-Id: Idf0c831020696a3a62fcef13171eedf3fcf63f5b
    Signed-off-by: Seth Howell <seth.howell@intel.com>

---
+++ test/dpdk_memory_utility/test_dpdk_mem_info.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+source $rootdir/test/common/autotest_common.sh
+
+rpc_py="$rootdir/scripts/rpc.py"
+SPDK_APP="$rootdir/app/spdk_tgt/spdk_tgt"
+MEM_SCRIPT="$rootdir/scripts/dpdk_mem_info.py"
+
+$SPDK_APP &
+spdkpid=$!
+
+waitforlisten $spdkpid
+
+trap 'killprocess $spdkpid' SIGINT SIGTERM EXIT
+
+$rpc_py env_dpdk_get_mem_stats
+
+$MEM_SCRIPT
+
+$MEM_SCRIPT -m 0
+
+trap - SIGINT SIGTERM EXIT
+killprocess $spdkpid
