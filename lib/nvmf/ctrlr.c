dd0f0f760aeab6ac2ad49423eb058be3f7f68432
Maciej Szwed <maciej.szwed@intel.com>
2019-12-13 11:17:45 +0100

    nvmf: Return ACWU and NACWU values in indentify structures
    For ACWU we always set value 1 because bdev holds
    information specific for namespace only. This value
    actually does not matter because we also set NACWU
    which makes ACWU irrelevant. We set ACWU because
    NVMe specs requires ACWU != 0 if fused commands
    are supported.
    
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Change-Id: Ida4357026d3b32677fc824b3cd878e7ad8ef2680

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -1626,6 +1626,7 @@ spdk_nvmf_ctrlr_identify_ctrlr(struct spdk_nvmf_ctrlr *ctrlr, struct spdk_nvme_c
 	cdata->sgls.keyed_sgl = 1;
 	cdata->sgls.sgl_offset = 1;
 	cdata->fuses.compare_and_write = 0;
+	cdata->acwu = 1;
 	spdk_strcpy_pad(cdata->subnqn, subsystem->subnqn, sizeof(cdata->subnqn), '\0');
 
 	SPDK_DEBUGLOG(SPDK_LOG_NVMF, "ctrlr data: maxcmd 0x%x\n", cdata->maxcmd);
