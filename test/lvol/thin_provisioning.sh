6c84d86f74c11fe0b118dc48dae62c9d6ec8207f
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-12-17 05:42:59 -0500

    test/lvol: rewrite thin_provisioning_read_empty_bdev to bash
    Change-Id: Id4609dbd3ac196dca13395007831b11e01af70b9
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/lvol/thin_provisioning.sh
+++ test/lvol/thin_provisioning.sh
@@ -60,12 +60,50 @@ function test_thin_lvol_check_space() {
 	rpc_cmd bdev_malloc_delete "$malloc_name"
 }
 
+# Check if we can create thin provisioned bdev on empty lvol store
+# and check if we can read from this device and it returns zeroes.
+function test_thin_lvol_check_zeroes() {
+	malloc_name=$(rpc_cmd bdev_malloc_create $MALLOC_SIZE_MB $MALLOC_BS)
+	lvs_uuid=$(rpc_cmd bdev_lvol_create_lvstore "$malloc_name" lvs_test)
+	lvs=$(rpc_cmd bdev_lvol_get_lvstores -u "$lvs_uuid")
+	free_clusters_start="$(jq -r '.[0].free_clusters' <<< "$lvs")"
+
+	# Create thick and thin provisioned lvol bdevs with size equals to lvol store space
+	lbd_name0=lvol_test0
+	lbd_name1=lvol_test1
+	lvol_size_mb=$(( LVS_DEFAULT_CAPACITY_MB ))
+	# Round down lvol size to the nearest cluster size boundary
+	lvol_size_mb=$(( lvol_size_mb / LVS_DEFAULT_CLUSTER_SIZE_MB * LVS_DEFAULT_CLUSTER_SIZE_MB ))
+	lvol_size=$(( lvol_size_mb * 1024 * 1024 ))
+	lvol_uuid0=$(rpc_cmd bdev_lvol_create -u "$lvs_uuid" $lbd_name0 "$lvol_size_mb")
+	lvol_uuid1=$(rpc_cmd bdev_lvol_create -u "$lvs_uuid" $lbd_name1 "$lvol_size_mb" -t)
+
+	nbd_start_disks "$DEFAULT_RPC_ADDR" "$lvol_uuid0" /dev/nbd0
+	nbd_start_disks "$DEFAULT_RPC_ADDR" "$lvol_uuid1" /dev/nbd1
+
+	# Fill the whole thick provisioned lvol bdev
+	run_fio_test /dev/nbd0 0 $lvol_size "write" "0xcc"
+
+	# Perform read operations on thin provisioned lvol bdev
+	# and check if they return zeroes
+	run_fio_test /dev/nbd1 0 $lvol_size "read" "0x00"
+
+	# Clean up
+	nbd_stop_disks "$DEFAULT_RPC_ADDR" /dev/nbd1
+	nbd_stop_disks "$DEFAULT_RPC_ADDR" /dev/nbd0
+	rpc_cmd bdev_lvol_delete "$lvol_uuid1"
+	rpc_cmd bdev_lvol_delete "$lvol_uuid0"
+	rpc_cmd bdev_lvol_delete_lvstore -u "$lvs_uuid"
+	rpc_cmd bdev_malloc_delete "$malloc_name"
+}
+
 $rootdir/app/spdk_tgt/spdk_tgt &
 spdk_pid=$!
 trap 'killprocess "$spdk_pid"; exit 1' SIGINT SIGTERM EXIT
 waitforlisten $spdk_pid
 
 run_test "test_thin_lvol_check_space" test_thin_lvol_check_space
+run_test "test_thin_lvol_check_zeroes" test_thin_lvol_check_zeroes
 
 trap - SIGINT SIGTERM EXIT
 killprocess $spdk_pid
