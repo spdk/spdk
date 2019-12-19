baef595d5c4095a9ea63127be363a072eb837543
Maciej Wawryk <maciejx.wawryk@intel.com>
2019-12-19 15:17:49 +0100

    test: fix issue with changing device names in waitforblk function
    During test before upgrading our vm's to Fedora31 there were issue with failing
    nvmf test on function waitforblk. In some cases hardcoded nvme0n1 name was not
    found in system. Here is fix that change searching by name to searching by nvme
    serial.
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: Ic5ee70804652a057fa26b8cc004b5227262d2122

--- test/nvmf/target/filesystem.sh
+++ test/nvmf/target/filesystem.sh
@@ -14,6 +14,7 @@ nvmftestinit
 
 function nvmf_filesystem_create {
 	fstype=$1
+	nvme_name=$2
 
 	if [ $fstype = ext4 ]; then
 		force=-F
@@ -21,9 +22,9 @@ function nvmf_filesystem_create {
 		force=-f
 	fi
 
-	mkfs.${fstype} $force /dev/nvme0n1p1
+	mkfs.${fstype} $force /dev/${nvme_name}p1
 
-	mount /dev/nvme0n1p1 /mnt/device
+	mount /dev/${nvme_name}p1 /mnt/device
 	touch /mnt/device/aaa
 	sync
 	rm /mnt/device/aaa
@@ -38,34 +39,32 @@ function nvmf_filesystem_part {
 
 	$rpc_py nvmf_create_transport $NVMF_TRANSPORT_OPTS -u 8192 -c $incapsule
 	$rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc1
-	$rpc_py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001
+	$rpc_py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode1 -a -s $NVMF_SERIAL
 	$rpc_py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Malloc1
 	$rpc_py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t $TEST_TRANSPORT -a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT
 
 	nvme connect -t $TEST_TRANSPORT -n "nqn.2016-06.io.spdk:cnode1" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
 
-	# TODO: fix this to wait for the proper NVMe device.
-	# if we are hosting the local filesystem on an NVMe drive, this test will fail
-	# because it relies on the no other NVMe drives being present in the system.
-	waitforblk "nvme0n1"
+	waitforserial "$NVMF_SERIAL"
+	nvme_name=$(lsblk -l -o NAME,SERIAL | awk {'if ($2=="${NVMF_SERIAL}") print $1'})
 
 	mkdir -p /mnt/device
 
-	parted -s /dev/nvme0n1 mklabel msdos  mkpart primary '0%' '100%'
+	parted -s /dev/${nvme_name} mklabel msdos  mkpart primary '0%' '100%'
 	partprobe
 	sleep 1
 
 	if [ $incapsule -eq 0 ]; then
-		run_test "filesystem_ext4" nvmf_filesystem_create "ext4"
-		run_test "filesystem_btrfs" nvmf_filesystem_create "btrfs"
-		run_test "filesystem_xfs" nvmf_filesystem_create "xfs"
+		run_test "filesystem_ext4" nvmf_filesystem_create "ext4" ${nvme_name}
+		run_test "filesystem_btrfs" nvmf_filesystem_create "btrfs" ${nvme_name}
+		run_test "filesystem_xfs" nvmf_filesystem_create "xfs" ${nvme_name}
 	else
-		run_test "filesystem_incapsule_ext4" nvmf_filesystem_create "ext4"
-		run_test "filesystem_incapsule_btrfs" nvmf_filesystem_create "btrfs"
-		run_test "filesystem_incapsule_xfs" nvmf_filesystem_create "xfs"
+		run_test "filesystem_incapsule_ext4" nvmf_filesystem_create "ext4" ${nvme_name}
+		run_test "filesystem_incapsule_btrfs" nvmf_filesystem_create "btrfs" ${nvme_name}
+		run_test "filesystem_incapsule_xfs" nvmf_filesystem_create "xfs" ${nvme_name}
 	fi
 
-	parted -s /dev/nvme0n1 rm 1
+	parted -s /dev/${nvme_name} rm 1
 
 	sync
 	nvme disconnect -n "nqn.2016-06.io.spdk:cnode1" || true
