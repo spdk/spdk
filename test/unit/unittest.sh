1a60d233555b190d9bb1ba7d64f5acfcd2b857cf
Seth Howell <seth.howell@intel.com>
2019-12-19 15:46:55 -0700

    test/unit: add run_test to all other tests.
    Change-Id: I69dc9721cb7ccb103f404eccc680f46d1adc8d13
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- test/unit/unittest.sh
+++ test/unit/unittest.sh
@@ -149,7 +149,7 @@ if [ $(uname -m) = "aarch64" ]; then
 	export LD_HWCAP_MASK=1
 fi
 
-$valgrind $testdir/include/spdk/histogram_data.h/histogram_ut
+run_test "unittest_include" $valgrind $testdir/include/spdk/histogram_data.h/histogram_ut
 
 run_test "unittest_bdev" unittest_bdev
 if grep -q '#define SPDK_CONFIG_CRYPTO 1' $rootdir/include/spdk/config.h; then
@@ -173,16 +173,16 @@ fi
 run_test "unittest_iscsi" unittest_iscsi
 run_test "unittest_json" unittest_json
 
-$valgrind $testdir/lib/sock/sock.c/sock_ut
+run_test "unittest_sock" $valgrind $testdir/lib/sock/sock.c/sock_ut
 
 run_test "unittest_nvme" unittest_nvme
 if grep -q '#define SPDK_CONFIG_RDMA 1' $rootdir/include/spdk/config.h; then
 	run_test "unittest_nvme_rdma" $valgrind $testdir/lib/nvme/nvme_rdma.c/nvme_rdma_ut
 fi
 
-$valgrind $testdir/lib/ioat/ioat.c/ioat_ut
+run_test "unittest_ioat" $valgrind $testdir/lib/ioat/ioat.c/ioat_ut
 
-$valgrind $testdir/lib/log/log.c/log_ut
+run_test "unittest_log" $valgrind $testdir/lib/log/log.c/log_ut
 
 run_test "unittest_nvmf" unittest_nvmf
 if [ -e $testdir/lib/nvmf/fc.c/fc_ut ]; then
@@ -199,20 +199,20 @@ fi
 
 run_test "unittest_scsi" unittest_scsi
 
-$valgrind $testdir/lib/lvol/lvol.c/lvol_ut
+run_test "unittest_lvol" $valgrind $testdir/lib/lvol/lvol.c/lvol_ut
 
-$valgrind $testdir/lib/notify/notify.c/notify_ut
+run_test "unittest_notify" $valgrind $testdir/lib/notify/notify.c/notify_ut
 
 if grep -q '#define SPDK_CONFIG_REDUCE 1' $rootdir/config.h; then
-	$valgrind $testdir/lib/reduce/reduce.c/reduce_ut
+	run_test "unittest_bdev_reduce" $valgrind $testdir/lib/reduce/reduce.c/reduce_ut
 fi
 
-$valgrind $testdir/lib/thread/thread.c/thread_ut
+run_test "unittest_thread" $valgrind $testdir/lib/thread/thread.c/thread_ut
 
 run_test "unittest_util" unittest_util
 
 if [ $(uname -s) = Linux ]; then
-$valgrind $testdir/lib/vhost/vhost.c/vhost_ut
+	run_test "unittest_vhost" $valgrind $testdir/lib/vhost/vhost.c/vhost_ut
 fi
 
 # local unit test coverage
