16176fd940df25d1d4002d0cf0e174362fe5e6cc
Pawel Kaminski <pawelx.kaminski@intel.com>
2019-12-20 18:36:33 -0500

    test/lvol: rewrite thin_provisioning_filling_disks_less_than_lvs_size to bash
    Change-Id: Iec5277f86e2f663eafd2c4c32515c2bf30a1e068
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/lvol/common.sh
+++ test/lvol/common.sh
@@ -29,21 +29,21 @@ function round_down() {
 }
 
 function run_fio_test() {
-	file=$1
-	offset=$2
-	size=$3
-	rw=$4
-	pattern=$5
-	extra_params=""
+	local file=$1
+	local offset=$2
+	local size=$3
+	local rw=$4
+	local pattern=$5
+	local extra_params=""
 	if [ -n "$6" ]; then
 		extra_params=$6
 	fi
 
-	pattern_template=""
+	local pattern_template=""
 	if [ $pattern ]; then
 		pattern_template="--do_verify=1 --verify=pattern --verify_pattern=$pattern --verify_state_save=0"
 	fi
 
-	fio_template="fio --name=fio_test --filename=$file --offset=$offset --size=$size --rw=$rw --direct=1 $extra_params $pattern_template"
+	local fio_template="fio --name=fio_test --filename=$file --offset=$offset --size=$size --rw=$rw --direct=1 $extra_params $pattern_template"
 	$fio_template
 }
