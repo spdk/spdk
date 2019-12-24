7f4e55df0dbbe54632f4e4c565436e524a919a79
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2019-12-24 01:38:41 -0500

    bdevperf: Free all targets before exiting bdevperf application
    We will use SPDK application framework (SPDK thread, IO channel, and
    etc) to manage IO target groups, and will need to free IO target
    groups before exiting bdevperf application.  As a preparation,
    move bdevperf_free_targets() before returning from spdk_app_start().
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: Id695a88107f87a9573c281f7f7d26d91d3a307b5

--- test/bdev/bdevperf/bdevperf.c
+++ test/bdev/bdevperf/bdevperf.c
@@ -468,6 +468,7 @@ end_run(void *arg1, void *arg2)
 		if (g_request && !g_shutdown) {
 			rpc_perform_tests_cb(rc);
 		} else {
+			bdevperf_free_targets();
 			spdk_app_stop(rc);
 		}
 	}
@@ -1314,6 +1315,7 @@ bdevperf_run(void *arg1)
 
 	rc = bdevperf_test();
 	if (rc) {
+		bdevperf_free_targets();
 		spdk_app_stop(1);
 		return;
 	}
@@ -1340,6 +1342,7 @@ spdk_bdevperf_shutdown_cb(void)
 	g_shutdown = true;
 
 	if (g_target_count == 0) {
+		bdevperf_free_targets();
 		spdk_app_stop(0);
 		return;
 	}
@@ -1490,7 +1493,6 @@ main(int argc, char **argv)
 		g_run_failed = true;
 	}
 
-	bdevperf_free_targets();
 	spdk_app_fini();
 	return g_run_failed;
 }
