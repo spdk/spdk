134280e12d159ed3913fa004382dc0ace465a143
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2020-01-03 05:44:53 -0500

    lib/blob: pass Extent Page offset on cluster allocation
    Extent Pages claim and insertion can be asynchronous
    in case of cluster allocation by writing to new cluster.
    
    In that case a cluster and extent page is first claimed,
    then message is passed to md_thread to actually insert
    them into the md and sync.
    
    This patch adds parameters to pass the Extent Page offset
    in such case.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I46d8ace9cd5abc0bfe48174c2f2ec218145b9c75

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -54,7 +54,7 @@ static int spdk_bs_register_md_thread(struct spdk_blob_store *bs);
 static int spdk_bs_unregister_md_thread(struct spdk_blob_store *bs);
 static void _spdk_blob_close_cpl(spdk_bs_sequence_t *seq, void *cb_arg, int bserrno);
 static void _spdk_blob_insert_cluster_on_md_thread(struct spdk_blob *blob, uint32_t cluster_num,
-		uint64_t cluster, spdk_blob_op_complete cb_fn, void *cb_arg);
+		uint64_t cluster, uint32_t extent, spdk_blob_op_complete cb_fn, void *cb_arg);
 
 static int _spdk_blob_set_xattr(struct spdk_blob *blob, const char *name, const void *value,
 				uint16_t value_len, bool internal);
@@ -114,7 +114,7 @@ _spdk_blob_insert_cluster(struct spdk_blob *blob, uint32_t cluster_num, uint64_t
 
 static int
 _spdk_bs_allocate_cluster(struct spdk_blob *blob, uint32_t cluster_num,
-			  uint64_t *lowest_free_cluster, bool update_map)
+			  uint64_t *lowest_free_cluster, uint32_t *lowest_free_extent, bool update_map)
 {
 	pthread_mutex_lock(&blob->bs->used_clusters_mutex);
 	*lowest_free_cluster = spdk_bit_array_find_first_clear(blob->bs->used_clusters,
@@ -1535,6 +1535,7 @@ _spdk_blob_resize(struct spdk_blob *blob, uint64_t sz)
 	uint64_t	i;
 	uint64_t	*tmp;
 	uint64_t	lfc; /* lowest free cluster */
+	uint32_t	lfe; /* lowest free extent */
 	uint64_t	num_clusters;
 	struct spdk_blob_store *bs;
 
@@ -1594,9 +1595,11 @@ _spdk_blob_resize(struct spdk_blob *blob, uint64_t sz)
 
 	if (spdk_blob_is_thin_provisioned(blob) == false) {
 		lfc = 0;
+		lfe = 0;
 		for (i = num_clusters; i < sz; i++) {
-			_spdk_bs_allocate_cluster(blob, i, &lfc, true);
+			_spdk_bs_allocate_cluster(blob, i, &lfc, &lfe, true);
 			lfc++;
+			lfe++;
 		}
 	}
 
@@ -1761,6 +1764,7 @@ struct spdk_blob_copy_cluster_ctx {
 	uint8_t *buf;
 	uint64_t page;
 	uint64_t new_cluster;
+	uint32_t new_extent;
 	spdk_bs_sequence_t *seq;
 };
 
@@ -1821,7 +1825,7 @@ _spdk_blob_write_copy_cpl(spdk_bs_sequence_t *seq, void *cb_arg, int bserrno)
 
 	cluster_number = _spdk_bs_page_to_cluster(ctx->blob->bs, ctx->page);
 
-	_spdk_blob_insert_cluster_on_md_thread(ctx->blob, cluster_number, ctx->new_cluster,
+	_spdk_blob_insert_cluster_on_md_thread(ctx->blob, cluster_number, ctx->new_cluster, ctx->new_extent,
 					       _spdk_blob_insert_cluster_cpl, ctx);
 }
 
@@ -1895,7 +1899,7 @@ _spdk_bs_allocate_and_copy_cluster(struct spdk_blob *blob,
 		}
 	}
 
-	rc = _spdk_bs_allocate_cluster(blob, cluster_number, &ctx->new_cluster, false);
+	rc = _spdk_bs_allocate_cluster(blob, cluster_number, &ctx->new_cluster, &ctx->new_extent, false);
 	if (rc != 0) {
 		spdk_free(ctx->buf);
 		free(ctx);
@@ -1926,7 +1930,7 @@ _spdk_bs_allocate_and_copy_cluster(struct spdk_blob *blob,
 					     _spdk_bs_dev_byte_to_lba(blob->back_bs_dev, blob->bs->cluster_sz),
 					     _spdk_blob_write_copy, ctx);
 	} else {
-		_spdk_blob_insert_cluster_on_md_thread(ctx->blob, cluster_number, ctx->new_cluster,
+		_spdk_blob_insert_cluster_on_md_thread(ctx->blob, cluster_number, ctx->new_cluster, ctx->new_extent,
 						       _spdk_blob_insert_cluster_cpl, ctx);
 	}
 }
@@ -6071,7 +6075,7 @@ _spdk_blob_insert_cluster_msg(void *arg)
 
 static void
 _spdk_blob_insert_cluster_on_md_thread(struct spdk_blob *blob, uint32_t cluster_num,
-				       uint64_t cluster, spdk_blob_op_complete cb_fn, void *cb_arg)
+				       uint64_t cluster, uint32_t extent, spdk_blob_op_complete cb_fn, void *cb_arg)
 {
 	struct spdk_blob_insert_cluster_ctx *ctx;
 
