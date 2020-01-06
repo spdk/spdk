25014cb2b47c721b4e20ea2d2108ffd5385bc1bf
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2020-01-06 01:06:32 -0500

    bdevperf: Use calloc to remove unnecesasry zeroings in construct_target()
    Use calloc to simplify bdevperf_construct_target().
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: Ice8626c318e686518adb7f8c4d77d46476228790

--- test/bdev/bdevperf/bdevperf.c
+++ test/bdev/bdevperf/bdevperf.c
@@ -283,7 +283,7 @@ bdevperf_construct_target(struct spdk_bdev *bdev)
 	int block_size, data_block_size;
 	int rc;
 
-	target = malloc(sizeof(struct io_target));
+	target = calloc(1, sizeof(struct io_target));
 	if (!target) {
 		fprintf(stderr, "Unable to allocate memory for new target.\n");
 		/* Return immediately because all mallocs will presumably fail after this */
@@ -307,16 +307,12 @@ bdevperf_construct_target(struct spdk_bdev *bdev)
 	}
 
 	target->bdev = bdev;
-	target->io_completed = 0;
-	target->current_queue_depth = 0;
-	target->offset_in_ios = 0;
 
 	block_size = spdk_bdev_get_block_size(bdev);
 	data_block_size = spdk_bdev_get_data_block_size(bdev);
 	target->io_size_blocks = g_io_size / data_block_size;
 	target->buf_size = target->io_size_blocks * block_size;
 
-	target->dif_check_flags = 0;
 	if (spdk_bdev_is_dif_check_enabled(bdev, SPDK_DIF_CHECK_TYPE_REFTAG)) {
 		target->dif_check_flags |= SPDK_DIF_FLAGS_REFTAG_CHECK;
 	}
@@ -326,9 +322,6 @@ bdevperf_construct_target(struct spdk_bdev *bdev)
 
 	target->size_in_ios = spdk_bdev_get_num_blocks(bdev) / target->io_size_blocks;
 
-	target->is_draining = false;
-	target->run_timer = NULL;
-	target->reset_timer = NULL;
 	TAILQ_INIT(&target->task_list);
 
 	/* Mapping each created target to target group */
