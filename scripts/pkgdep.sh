523a9dbe68df369653485674e6258a45f6721282
Maciej Wawryk <maciejx.wawryk@intel.com>
2020-01-15 09:57:19 +0100

    scripts/pkgdep.sh: Add missing libpmem for reduce
    Add missing libpmem for reduce in Ubuntu and move libpmem
    to minimal required dependencies
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: I596a1e6660f8b6f92bdab81046fa3533ae78fca3

--- scripts/pkgdep.sh
+++ scripts/pkgdep.sh
@@ -52,6 +52,8 @@ if [ -s /etc/redhat-release ]; then
 	fi
 	# Additional dependencies for ISA-L used in compression
 	yum install -y autoconf automake libtool help2man
+	# Additional dependencies for building pmem based backends
+	yum install -y libpmemblk-devel || true
 	# Additional dependencies for DPDK
 	yum install -y numactl-devel nasm
 	if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
@@ -78,8 +80,6 @@ if [ -s /etc/redhat-release ]; then
 	yum install -y libibverbs-devel librdmacm-devel
 	# Additional dependencies for building docs
 	yum install -y doxygen mscgen graphviz
-	# Additional dependencies for building pmem based backends
-	yum install -y libpmemblk-devel || true
 	# Additional dependencies for FUSE and CUSE
 	yum install -y fuse3-devel
 elif [ -f /etc/debian_version ]; then
@@ -103,6 +103,9 @@ elif [ -f /etc/debian_version ]; then
 	apt-get install -y libnuma-dev nasm
 	# Additional dependencies for ISA-L used in compression
 	apt-get install -y autoconf automake libtool help2man
+	if [[ $NAME == "Ubuntu" ]] && [[ $VERSION_ID -gt 1800 ]]; then
+		apt-get install -y libpmem-dev
+	fi
 	if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
 		# Tools for developers
 		apt-get install -y git astyle pep8 lcov clang sg3-utils pciutils shellcheck
@@ -133,6 +136,8 @@ elif [ -f /etc/SuSE-release ] || [ -f /etc/SUSE-brand ]; then
 	zypper install -y libnuma-devel nasm
 	# Additional dependencies for ISA-L used in compression
 	zypper install -y autoconf automake libtool help2man
+	# Additional dependencies for building pmem based backends
+	zypper install -y libpmemblk-devel
 	if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
 		# Tools for developers
 		zypper install -y git-core lcov python-pycodestyle sg3_utils \
@@ -142,8 +147,6 @@ elif [ -f /etc/SuSE-release ] || [ -f /etc/SUSE-brand ]; then
 	fi
 	# Additional dependencies for NVMe over Fabrics
 	zypper install -y rdma-core-devel
-	# Additional dependencies for building pmem based backends
-	zypper install -y libpmemblk-devel
 	# Additional dependencies for building docs
 	zypper install -y doxygen mscgen graphviz
 	# Additional dependencies for FUSE and CUSE
@@ -171,6 +174,14 @@ elif [ -f /etc/arch-release ]; then
 	pacman -Sy --needed --noconfirm numactl nasm
 	# Additional dependencies for ISA-L used in compression
 	pacman -Sy --needed --noconfirm autoconf automake libtool help2man
+	# Additional dependencies for building pmem based backends
+	pacman -Sy --needed --noconfirm ndctl
+	git clone https://github.com/pmem/pmdk.git /tmp/pmdk -b 1.6.1
+	make -C /tmp/pmdk -j$(nproc)
+	make install -C /tmp/pmdk
+	echo "/usr/local/lib" > /etc/ld.so.conf.d/pmdk.conf
+	ldconfig
+	rm -rf /tmp/pmdk
 	if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
 		# Tools for developers
 		pacman -Sy --needed --noconfirm git astyle autopep8 \
@@ -218,14 +229,6 @@ elif [ -f /etc/arch-release ]; then
 		makepkg -si --needed --noconfirm;
 		cd .. && rm -rf rdma-core;
 		popd"
-	# Additional dependencies for building pmem based backends
-	pacman -Sy --needed --noconfirm ndctl
-	git clone https://github.com/pmem/pmdk.git /tmp/pmdk -b 1.6.1
-	make -C /tmp/pmdk -j$(nproc)
-	make install -C /tmp/pmdk
-	echo "/usr/local/lib" > /etc/ld.so.conf.d/pmdk.conf
-	ldconfig
-	rm -rf /tmp/pmdk
 else
 	echo "pkgdep: unknown system type."
 	exit 1
