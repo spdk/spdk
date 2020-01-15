b2acfd81abcea588d95f95453fe868cf341349fb
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2020-01-15 04:32:34 -0500

    lib/blob: add EXTENT_RLE descriptor description
    Since further patches will be adding new descriptors
    that are related to cluster layout throughout the blobstore,
    add description for existing descriptor too.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I722eb633445685789d5185ed59dfc910f76b109f

--- lib/blob/blobstore.h
+++ lib/blob/blobstore.h
@@ -243,10 +243,17 @@ struct spdk_bs_md_mask {
 };
 
 #define SPDK_MD_DESCRIPTOR_TYPE_PADDING 0
-#define SPDK_MD_DESCRIPTOR_TYPE_EXTENT_RLE 1
 #define SPDK_MD_DESCRIPTOR_TYPE_XATTR 2
 #define SPDK_MD_DESCRIPTOR_TYPE_FLAGS 3
 #define SPDK_MD_DESCRIPTOR_TYPE_XATTR_INTERNAL 4
+
+/* Following descriptors define cluster layout in a blob. */
+
+/* EXTENT_RLE descriptor holds array of LBA that point to
+ * begining of allocated clusters. The array is run-lenght encoded,
+ * with 0's being unallocated clusters. It is part of serialized
+ * metadata chain for a blob. */
+#define SPDK_MD_DESCRIPTOR_TYPE_EXTENT_RLE 1
 
 struct spdk_blob_md_descriptor_xattr {
 	uint8_t		type;
