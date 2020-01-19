68ecf86dc2c3b42092fb0ff8f026e1aae773ed45
Ziye Yang <ziye.yang@intel.com>
2020-01-19 21:22:35 +0800

    lib/iscsi: Add real callback for DATAIN PDU complete
    Since only after DATAIN pdu sending out, we can have
    free slot to handle queued data in tasks.
    
    Signed-off-by: Ziye Yang <ziye.yang@intel.com>
    Change-Id: I49a52597e8660453ea90c5960d020eb53f81265d

--- lib/iscsi/iscsi.c
+++ lib/iscsi/iscsi.c
@@ -2910,6 +2910,14 @@ get_transfer_task(struct spdk_iscsi_conn *conn, uint32_t transfer_tag)
 	return NULL;
 }
 
+static void
+spdk_iscsi_conn_datain_pdu_complete(void *arg)
+{
+	struct spdk_iscsi_conn *conn = arg;
+
+	spdk_iscsi_conn_handle_queued_datain_tasks(conn);
+}
+
 static int
 iscsi_send_datain(struct spdk_iscsi_conn *conn,
 		  struct spdk_iscsi_task *task, int datain_flag,
@@ -3012,7 +3020,7 @@ iscsi_send_datain(struct spdk_iscsi_conn *conn,
 		}
 	}
 
-	spdk_iscsi_conn_write_pdu(conn, rsp_pdu, spdk_iscsi_conn_pdu_generic_complete, NULL);
+	spdk_iscsi_conn_write_pdu(conn, rsp_pdu, spdk_iscsi_conn_datain_pdu_complete, conn);
 
 	return DataSN;
 }
