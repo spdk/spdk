035c6b9455df1681d43a199594150569ad3f68c3
Karol Latecki <karol.latecki@intel.com>
2020-01-21 11:34:43 +0100

    test/nvme-perf: reorganize fio config generation
    Previous changes moved some of the fio CLI parameters into
    a bash heredoc to save into a text file, but it was put in
    wrong place in the script.
    
    Move global fio parameters a couple lines up so that the
    resulting fio config file has the correct form of:
    [global]
      global_params
    [testjob]
      testjob_params
    
    Change-Id: I6271cdde9f9bb7a1976a82f372c21a0e0d8675b4
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>

--- test/nvme/perf/run_perf.sh
+++ test/nvme/perf/run_perf.sh
@@ -128,9 +128,7 @@ do
 
 			cp $NVME_FIO_RESULTS $result_dir/perf_results_${MIX}_${PLUGIN}_${NO_CORES}cpus_${DATE}_${k}_disks_${j}.output
 		else
-			create_fio_config $k $PLUGIN "$DISK_NAMES" "$DISKS_NUMA" "$CORES"
 			desc="Running Test: Blocksize=${BLK_SIZE} Workload=$RW MIX=${MIX} qd=${IODEPTH} io_plugin/driver=$PLUGIN"
-
 			cat <<- EOF >> $BASE_DIR/config.fio
 				rw=$RW
 				rwmixread=$MIX
@@ -143,6 +141,8 @@ do
 				description=$desc
 				log_avg_msec=250
 				EOF
+
+			create_fio_config $k $PLUGIN "$DISK_NAMES" "$DISKS_NUMA" "$CORES"
 
 			echo "USING CONFIG:"
 			cat $BASE_DIR/config.fio
