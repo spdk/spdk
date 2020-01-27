9cca2da119ba97e8c1f0e7570d82089a2236d25c
Pawel Kaminski <pawelx.kaminski@intel.com>
2020-01-27 11:36:50 -0500

    test/vhost: Replace ini config with json in shared test
    Change-Id: Iad98a16f3a01c68c34e23bcd880035c8c6550da4
    Signed-off-by: Pawel Kaminski <pawelx.kaminski@intel.com>

--- test/vhost/shared/shared.sh
+++ test/vhost/shared/shared.sh
@@ -10,7 +10,7 @@ rpc_py="$rootdir/scripts/rpc.py -s $(get_vhost_dir 0)/rpc.sock"
 function run_spdk_fio() {
 	fio_bdev --ioengine=spdk_bdev \
 	"$rootdir/test/vhost/common/fio_jobs/default_initiator.job" --runtime=10 --rw=randrw \
-	--spdk_mem=1024 --spdk_single_seg=1 --spdk_conf=$testdir/bdev.conf "$@"
+	--spdk_mem=1024 --spdk_single_seg=1 --spdk_json_conf=$testdir/bdev.json "$@"
 }
 
 vhosttestinit
