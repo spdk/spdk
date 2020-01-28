e06dfb63b26f49778d455dadfbdee2e4173f2592
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2020-01-28 07:11:58 -0500

    [WIP]test/blob: add test to verify backwards compatibility
    This patch verfies blobstore against SPDK 19.10.1 release.
    
    Test steps:
    1) Uses SPDK 19.10.1 to create lvolstore and lvols
    2) Load them into SPDK 20.01 and verify integrity
    3) Re-load lvols using SPDK 19.10.1 again to verify no
    changes were made with new SPDK
    
    This test script will require changes to how jobs
    execute this test. For now it is not part of
    per-patch or nightly, but will be executed
    as separate job.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: Ie40a2accf40ad882a62cb5cce3f8492ddfe977ef

---
+++ test/lvol/backward.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../..)
+source $rootdir/test/common/autotest_common.sh
+source $rootdir/test/lvol/common.sh
+source $rootdir/test/bdev/nbd_common.sh
+
+new_spdk=$rootdir/app/spdk_tgt/spdk_tgt
+old_spdk=$1
+
+function check_fields() {
+	lvols=$(rpc_cmd bdev_get_bdevs | jq -r '[ .[] | select(.product_name == "Logical Volume") ]')
+	[ "$(jq length <<< "$lvols")" == "2" ]
+	[ "$lvols" = "$old_lvols" ]
+
+	lvs=$(rpc_cmd bdev_lvol_get_lvstores)
+	[ "$(jq length <<< "$lvs")" == "1" ]
+	[ "$lvs" = "$old_lvs" ]
+}
+
+# create lvs + multiple lvols, verify their params
+function lvol_backwards_compatibility() {
+	$old_spdk &
+	spdk_pid=$!
+	trap 'killprocess "$spdk_pid"; rm -f $testdir/aio_bdev; exit 1' SIGINT SIGTERM EXIT
+	waitforlisten $spdk_pid
+	truncate -s 400M $testdir/aio_bdev
+
+	# create an lvol store
+	aio_bs=4096
+	aio_bdev=$(rpc_cmd bdev_aio_create $testdir/aio_bdev aio_bdev $aio_bs)
+
+	# round down lvol size to the nearest cluster size boundary
+	# thick provision size is 1/2 aio bdev size, and thin provision size is times 1000 over aio size
+	lvol_size_mb=$(( LVS_DEFAULT_CAPACITY_MB / 2 ))
+	lvol_size_mb=$(( lvol_size_mb / LVS_DEFAULT_CLUSTER_SIZE_MB * LVS_DEFAULT_CLUSTER_SIZE_MB ))
+	lvol_size=$(( lvol_size_mb * 1024 * 1024 ))
+	lvol_thin_size_mb=$(( lvol_size_mb * 2000 ))
+
+	# create lvol store, thick and thin provisioned lvols
+	lvs_uuid=$(rpc_cmd bdev_lvol_create_lvstore "$aio_bdev" lvs_test)
+	lvol_uuid1=$(rpc_cmd bdev_lvol_create -u "$lvs_uuid" "lvol_test_thick" "$lvol_size_mb")
+	lvol_uuid2=$(rpc_cmd bdev_lvol_create -t -u "$lvs_uuid" "lvol_test_thin" "$lvol_thin_size_mb")
+
+	# make sure test is ran against SPDK v19.10.1
+	version=$(rpc_cmd spdk_get_version)
+	[ "$(jq -r '.version' <<< "$version")" = "SPDK v19.10.1" ]
+
+	nbd_start_disks "$DEFAULT_RPC_ADDR" "$lvol_uuid2" /dev/nbd0
+	run_fio_test /dev/nbd0 $(( lvol_size / 2 )) $(( lvol_size / 2 )) "write" "0xcc"
+	nbd_stop_disks "$DEFAULT_RPC_ADDR" /dev/nbd0
+
+	# save original lvs and lvol fields
+	old_lvs=$(rpc_cmd bdev_lvol_get_lvstores)
+	[ "$(jq length <<< "$old_lvs")" == "1" ]
+	old_lvols=$(rpc_cmd bdev_get_bdevs | jq -r '[ .[] | select(.product_name == "Logical Volume") ]')
+	[ "$(jq length <<< "$old_lvols")" == "2" ]
+
+	# kill SPDK v19.10.1 and start current commit
+	killprocess $spdk_pid
+	$new_spdk &
+	spdk_pid=$!
+	waitforlisten $spdk_pid
+
+	# make sure test is ran against pre-release SPDK 20.01
+	version=$(rpc_cmd spdk_get_version)
+	[ "$(jq -r '.fields.major' <<< "$version")" = "20" ]
+	[ "$(jq -r '.fields.minor' <<< "$version")" = "1" ]
+	[ "$(jq -r '.fields.patch' <<< "$version")" = "0" ]
+	[ "$(jq -r '.fields.suffix' <<< "$version")" = "-pre" ]
+
+	# reload lvol store
+	aio_bdev=$(rpc_cmd bdev_aio_create $testdir/aio_bdev aio_bdev $aio_bs)
+
+	# compare lvs and lvol fields after reloading in new SPDK version
+	check_fields
+
+	nbd_start_disks "$DEFAULT_RPC_ADDR" "$lvol_uuid2" /dev/nbd0
+	run_fio_test /dev/nbd0 $(( lvol_size / 2 )) $(( lvol_size / 2 )) "read" "0xcc"
+	run_fio_test /dev/nbd0 0 $(( lvol_size / 2 )) "read" "0x00"
+	nbd_stop_disks "$DEFAULT_RPC_ADDR" /dev/nbd0
+
+	# kill pre-release SPDK 20.01 and start SPDK v19.10.1
+	killprocess $spdk_pid
+	$old_spdk &
+	spdk_pid=$!
+	waitforlisten $spdk_pid
+
+	# make sure test is ran against SPDK v19.10.1
+	version=$(rpc_cmd spdk_get_version)
+	[ "$(jq -r '.version' <<< "$version")" = "SPDK v19.10.1" ]
+
+	# reload lvol store
+	aio_bdev=$(rpc_cmd bdev_aio_create $testdir/aio_bdev aio_bdev $aio_bs)
+
+	# compare lvs and lvol fields after reloading in old SPDK version
+	check_fields
+
+	nbd_start_disks "$DEFAULT_RPC_ADDR" "$lvol_uuid2" /dev/nbd0
+	run_fio_test /dev/nbd0 $(( lvol_size / 2 )) $(( lvol_size / 2 )) "read" "0xcc"
+	run_fio_test /dev/nbd0 0 $(( lvol_size / 2 )) "read" "0x00"
+	nbd_stop_disks "$DEFAULT_RPC_ADDR" /dev/nbd0
+
+	# clean up
+	rpc_cmd bdev_lvol_delete_lvstore -u "$lvs_uuid"
+	rpc_cmd bdev_lvol_get_lvstores -u "$lvs_uuid" && false
+	rpc_cmd bdev_aio_delete "$aio_bdev"
+	check_leftover_devices
+
+	trap - SIGINT SIGTERM EXIT
+	killprocess $spdk_pid
+	rm -f $testdir/aio_bdev
+}
+
+run_test "lvol_backwards_compatibility" lvol_backwards_compatibility
