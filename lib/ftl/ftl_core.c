496580eef62688b414d8261f6f77c1ee4670deea
Wojciech Malikowski <wojciech.malikowski@intel.com>
2020-01-30 05:53:21 -0500

    lib/ftl: Set zone state to "full" when all writes to it are completed
    Zone state should be changed to "full" when zone is
    fully written.
    
    Change-Id: Ib9ad29d1a0e788fba8a7eae881e07d384fa90548
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>

--- lib/ftl/ftl_core.c
+++ lib/ftl/ftl_core.c
@@ -1572,6 +1572,10 @@ ftl_io_child_write_cb(struct ftl_io *io, void *ctx, int status)
 	zone->busy = false;
 	zone->info.write_pointer += io->num_blocks;
 
+	if (zone->info.write_pointer == ftl_get_num_blocks_in_band(io->dev)) {
+		zone->info.state = SPDK_BDEV_ZONE_STATE_FULL;
+	}
+
 	/* If some other write on the same band failed the write pointer would already be freed */
 	if (spdk_likely(wptr)) {
 		wptr->num_outstanding--;
