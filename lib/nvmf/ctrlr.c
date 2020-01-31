90c3555d98055562cefcf8e256aaf6357248bbb5
Ben Walker <benjamin.walker@intel.com>
2020-01-31 15:44:36 -0700

    nvmf: Allow fabrics get/set on ASQ and ACQ
    This is meaningless for network devices, but will be useful
    when emulating the more complete register state of local devices.
    
    Change-Id: I37052e514101c298a1f66cc72135a8c3dd669003
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -746,6 +746,34 @@ nvmf_prop_set_aqa(struct spdk_nvmf_ctrlr *ctrlr, uint64_t value)
 	return true;
 }
 
+static uint64_t
+nvmf_prop_get_asq(struct spdk_nvmf_ctrlr *ctrlr)
+{
+	return ctrlr->vcprop.asq;
+}
+
+static bool
+nvmf_prop_set_asq(struct spdk_nvmf_ctrlr *ctrlr, uint64_t value)
+{
+	ctrlr->vcprop.asq = (uint32_t)value;
+
+	return true;
+}
+
+static uint64_t
+nvmf_prop_get_acq(struct spdk_nvmf_ctrlr *ctrlr)
+{
+	return ctrlr->vcprop.acq;
+}
+
+static bool
+nvmf_prop_set_acq(struct spdk_nvmf_ctrlr *ctrlr, uint64_t value)
+{
+	ctrlr->vcprop.acq = (uint32_t)value;
+
+	return true;
+}
+
 struct nvmf_prop {
 	uint32_t ofst;
 	uint8_t size;
@@ -768,6 +796,8 @@ static const struct nvmf_prop nvmf_props[] = {
 	PROP(cc,   4, nvmf_prop_get_cc,   nvmf_prop_set_cc),
 	PROP(csts, 4, nvmf_prop_get_csts, NULL),
 	PROP(aqa, 4, nvmf_prop_get_aqa, nvmf_prop_set_aqa),
+	PROP(asq, 4, nvmf_prop_get_asq, nvmf_prop_set_asq),
+	PROP(acq, 4, nvmf_prop_get_acq, nvmf_prop_set_acq),
 };
 
 static const struct nvmf_prop *
