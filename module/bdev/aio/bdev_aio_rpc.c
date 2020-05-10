0668d53b3b1058fc714407fefd656de2b57d55d9
Seth Howell <seth.howell@intel.com>
2020-05-10 00:04:07 -0700

    module/bdev_aio: remove spdk prefix from static functions
    Signed-off-by: Seth Howell <seth.howell@intel.com>
    Change-Id: I68a454a3473bc30b20b796d570267d0d84d06b06
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2318
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- module/bdev/aio/bdev_aio_rpc.c
+++ module/bdev/aio/bdev_aio_rpc.c
@@ -57,8 +57,8 @@ static const struct spdk_json_object_decoder rpc_construct_aio_decoders[] = {
 };
 
 static void
-spdk_rpc_bdev_aio_create(struct spdk_jsonrpc_request *request,
-			 const struct spdk_json_val *params)
+rpc_bdev_aio_create(struct spdk_jsonrpc_request *request,
+		    const struct spdk_json_val *params)
 {
 	struct rpc_construct_aio req = {};
 	struct spdk_json_write_ctx *w;
@@ -87,7 +87,7 @@ spdk_rpc_bdev_aio_create(struct spdk_jsonrpc_request *request,
 cleanup:
 	free_rpc_construct_aio(&req);
 }
-SPDK_RPC_REGISTER("bdev_aio_create", spdk_rpc_bdev_aio_create, SPDK_RPC_RUNTIME)
+SPDK_RPC_REGISTER("bdev_aio_create", rpc_bdev_aio_create, SPDK_RPC_RUNTIME)
 SPDK_RPC_REGISTER_ALIAS_DEPRECATED(bdev_aio_create, construct_aio_bdev)
 
 struct rpc_delete_aio {
@@ -105,7 +105,7 @@ static const struct spdk_json_object_decoder rpc_delete_aio_decoders[] = {
 };
 
 static void
-_spdk_rpc_bdev_aio_delete_cb(void *cb_arg, int bdeverrno)
+_rpc_bdev_aio_delete_cb(void *cb_arg, int bdeverrno)
 {
 	struct spdk_jsonrpc_request *request = cb_arg;
 	struct spdk_json_write_ctx *w = spdk_jsonrpc_begin_result(request);
@@ -115,8 +115,8 @@ _spdk_rpc_bdev_aio_delete_cb(void *cb_arg, int bdeverrno)
 }
 
 static void
-spdk_rpc_bdev_aio_delete(struct spdk_jsonrpc_request *request,
-			 const struct spdk_json_val *params)
+rpc_bdev_aio_delete(struct spdk_jsonrpc_request *request,
+		    const struct spdk_json_val *params)
 {
 	struct rpc_delete_aio req = {NULL};
 	struct spdk_bdev *bdev;
@@ -135,7 +135,7 @@ spdk_rpc_bdev_aio_delete(struct spdk_jsonrpc_request *request,
 		goto cleanup;
 	}
 
-	bdev_aio_delete(bdev, _spdk_rpc_bdev_aio_delete_cb, request);
+	bdev_aio_delete(bdev, _rpc_bdev_aio_delete_cb, request);
 
 	free_rpc_delete_aio(&req);
 
@@ -144,5 +144,5 @@ spdk_rpc_bdev_aio_delete(struct spdk_jsonrpc_request *request,
 cleanup:
 	free_rpc_delete_aio(&req);
 }
-SPDK_RPC_REGISTER("bdev_aio_delete", spdk_rpc_bdev_aio_delete, SPDK_RPC_RUNTIME)
+SPDK_RPC_REGISTER("bdev_aio_delete", rpc_bdev_aio_delete, SPDK_RPC_RUNTIME)
 SPDK_RPC_REGISTER_ALIAS_DEPRECATED(bdev_aio_delete, delete_aio_bdev)
