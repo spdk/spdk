798551bf2a231ee2499deb863049e00491ce981c
Seth Howell <seth.howell@intel.com>
2020-05-09 22:35:36 -0700

    module/accel: remove spdk prefix from static functions in IOAT module.
    Signed-off-by: Seth Howell <seth.howell@intel.com>
    Change-Id: I83026fbe8ae2dbdb159616cf98af4be1556a20ea
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2315
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

--- module/accel/ioat/accel_engine_ioat_rpc.c
+++ module/accel/ioat/accel_engine_ioat_rpc.c
@@ -76,8 +76,8 @@ static const struct spdk_json_object_decoder rpc_ioat_scan_accel_engine_decoder[
 };
 
 static void
-spdk_rpc_ioat_scan_accel_engine(struct spdk_jsonrpc_request *request,
-				const struct spdk_json_val *params)
+rpc_ioat_scan_accel_engine(struct spdk_jsonrpc_request *request,
+			   const struct spdk_json_val *params)
 {
 	struct rpc_ioat_scan_accel_engine req = {};
 	struct spdk_json_write_ctx *w;
@@ -111,6 +111,6 @@ spdk_rpc_ioat_scan_accel_engine(struct spdk_jsonrpc_request *request,
 	spdk_json_write_bool(w, true);
 	spdk_jsonrpc_end_result(request, w);
 }
-SPDK_RPC_REGISTER("ioat_scan_accel_engine", spdk_rpc_ioat_scan_accel_engine, SPDK_RPC_STARTUP)
+SPDK_RPC_REGISTER("ioat_scan_accel_engine", rpc_ioat_scan_accel_engine, SPDK_RPC_STARTUP)
 SPDK_RPC_REGISTER_ALIAS_DEPRECATED(ioat_scan_accel_engine, ioat_scan_copy_engine)
 SPDK_RPC_REGISTER_ALIAS_DEPRECATED(ioat_scan_accel_engine, scan_ioat_copy_engine)
