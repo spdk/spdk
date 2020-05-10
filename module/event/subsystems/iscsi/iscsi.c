e503deb1d256983dc7168a3177d6d5708ac9ed35
Seth Howell <seth.howell@intel.com>
2020-05-10 12:18:56 -0700

    module/event_iscsi: add a map file and remove spdk prefix.
    Signed-off-by: Seth Howell <seth.howell@intel.com>
    Change-Id: I8253838975a44ba8dcccb55ca1b41c27d5e3d231
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2349
    Community-CI: Mellanox Build Bot
    Community-CI: Broadcom CI
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- module/event/subsystems/iscsi/iscsi.c
+++ module/event/subsystems/iscsi/iscsi.c
@@ -38,41 +38,41 @@
 #include "spdk_internal/event.h"
 
 static void
-spdk_iscsi_subsystem_init_complete(void *cb_arg, int rc)
+iscsi_subsystem_init_complete(void *cb_arg, int rc)
 {
 	spdk_subsystem_init_next(rc);
 }
 
 static void
-spdk_iscsi_subsystem_init(void)
+iscsi_subsystem_init(void)
 {
-	spdk_iscsi_init(spdk_iscsi_subsystem_init_complete, NULL);
+	spdk_iscsi_init(iscsi_subsystem_init_complete, NULL);
 }
 
 static void
-spdk_iscsi_subsystem_fini_done(void *arg)
+iscsi_subsystem_fini_done(void *arg)
 {
 	spdk_subsystem_fini_next();
 }
 
 static void
-spdk_iscsi_subsystem_fini(void)
+iscsi_subsystem_fini(void)
 {
-	spdk_iscsi_fini(spdk_iscsi_subsystem_fini_done, NULL);
+	spdk_iscsi_fini(iscsi_subsystem_fini_done, NULL);
 }
 
 static void
-spdk_iscsi_subsystem_config_json(struct spdk_json_write_ctx *w)
+iscsi_subsystem_config_json(struct spdk_json_write_ctx *w)
 {
 	spdk_iscsi_config_json(w);
 }
 
 static struct spdk_subsystem g_spdk_subsystem_iscsi = {
 	.name = "iscsi",
-	.init = spdk_iscsi_subsystem_init,
-	.fini = spdk_iscsi_subsystem_fini,
+	.init = iscsi_subsystem_init,
+	.fini = iscsi_subsystem_fini,
 	.config = spdk_iscsi_config_text,
-	.write_config_json = spdk_iscsi_subsystem_config_json,
+	.write_config_json = iscsi_subsystem_config_json,
 };
 
 SPDK_SUBSYSTEM_REGISTER(g_spdk_subsystem_iscsi);
