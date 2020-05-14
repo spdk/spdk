34ea308e1204c6447baf64d6d3bdb988874382f4
Xiaodong Liu <xiaodong.liu@intel.com>
2020-05-14 02:40:21 -0400

    test/event: add app_repeat test case
    app_repeat is used to test calling spdk_app_start/stop
    repeatedly.
    * "-t <num>" is used to specify the maximum number of
    repeated rounds.
    *SIGUSR1 can be used to trigger a new round of
    spdk_app_stop/start.
    *Shutdown signals like SIGTERM can be used to shutdown
    the application.
    
    Change-Id: I10a205ea2e9da3b4ecd6381bf25b457ce909a621
    Signed-off-by: Xiaodong Liu <xiaodong.liu@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2447
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-by: Vitaliy Mysak <vitaliy.mysak@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

---
+++ test/event/app_repeat/app_repeat.c
@@ -0,0 +1,115 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "spdk/stdinc.h"
+
+#include "spdk/event.h"
+#include "spdk/string.h"
+#include "spdk/thread.h"
+
+struct spdk_app_opts g_opts = {};
+static const char g_app_repeat_get_opts_string[] = "t:";
+static int g_repeat_times = 2;
+static bool g_exit;
+
+static void
+app_repeat_usage(void)
+{
+	printf(" -t <num>                  number of times to repeat calling spdk_app_start/stop\n");
+}
+
+static int
+app_repeat_parse_arg(int ch, char *arg)
+{
+	switch (ch) {
+	case 't':
+		g_repeat_times = spdk_strtol(arg, 0);
+		if (g_repeat_times < 2) {
+			return -EINVAL;
+		}
+		break;
+	default:
+		return -EINVAL;
+	}
+	return 0;
+}
+
+static void
+app_repeat_started(void *arg1)
+{
+	int index = *(int *)arg1;
+
+	printf("spdk_app_start is called in Round %d.\n", index);
+}
+
+static void _app_repeat_shutdown_cb(void)
+{
+	printf("Shutdown signal received, exit.\n");
+	g_exit = true;
+	spdk_app_stop(0);
+}
+
+static void _app_repeat_usr1_handler(int signal)
+{
+	printf("USR1 signal received, restart spdk application framework.\n");
+	spdk_app_stop(0);
+}
+
+int
+main(int argc, char **argv)
+{
+	int rc;
+	int i;
+
+	spdk_app_opts_init(&g_opts);
+	g_opts.name = "app_repeat";
+	g_opts.shutdown_cb = _app_repeat_shutdown_cb;
+	g_opts.usr1_handler = _app_repeat_usr1_handler;
+	if ((rc = spdk_app_parse_args(argc, argv, &g_opts, g_app_repeat_get_opts_string,
+				      NULL, app_repeat_parse_arg, app_repeat_usage)) !=
+	    SPDK_APP_PARSE_ARGS_SUCCESS) {
+		return rc;
+	}
+
+	for (i = 0; i < g_repeat_times; i++) {
+		rc = spdk_app_start(&g_opts, app_repeat_started, &i);
+		spdk_app_fini();
+
+		if (rc) {
+			fprintf(stderr, "Failed to call spdk_app_start in Round %d.\n", i);
+			break;
+		}
+	}
+
+	return rc;
+}
--- test/event/event.sh
+++ test/event/event.sh
@@ -3,7 +3,42 @@
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
+source $rootdir/test/bdev/nbd_common.sh
+
+function app_repeat_test() {
+	local rpc_server=/var/tmp/spdk-nbd.sock
+	local nbd_list=("/dev/nbd0" "/dev/nbd1")
+	local bdev_list=("Malloc0" "Malloc1")
+	local repeat_times=4
+
+	modprobe nbd
+	$rootdir/test/event/app_repeat/app_repeat -r $rpc_server -m 0x3 -t $repeat_times &
+	repeat_pid=$!
+	trap 'killprocess $repeat_pid; exit 1' SIGINT SIGTERM EXIT
+	echo "Process app_repeat pid: $repeat_pid"
+
+	for i in {0..2}; do
+		echo "spdk_app_start Round $i"
+		waitforlisten $repeat_pid $rpc_server
+
+		$rootdir/scripts/rpc.py -s $rpc_server bdev_malloc_create 64 4096
+		$rootdir/scripts/rpc.py -s $rpc_server bdev_malloc_create 64 4096
+
+		nbd_rpc_data_verify $rpc_server "${bdev_list[*]}" "${nbd_list[*]}"
+		./scripts/rpc.py -s $rpc_server spdk_kill_instance SIGUSR1
+	done
+
+	waitforlisten $repeat_pid $rpc_server
+	killprocess $repeat_pid
+	trap - SIGINT SIGTERM EXIT
+
+	return 0
+}
 
 run_test "event_perf" $testdir/event_perf/event_perf -m 0xF -t 1
 run_test "event_reactor" $testdir/reactor/reactor -t 1
 run_test "event_reactor_perf" $testdir/reactor_perf/reactor_perf -t 1
+
+if [ $(uname -s) = Linux ] && modprobe -n nbd; then
+	run_test "app_repeat" app_repeat_test
+fi
