511de06bb50a1d7f72a4de5361c29c4a35c9b8f2
Michal Berger <michalx.berger@intel.com>
2020-05-18 15:16:16 +0200

    pkgdep: Introduce SUSE-specific source
    Change-Id: Ie106459d7dbe92e32933dad569b79c348a8f0052
    Signed-off-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2506
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

---
+++ scripts/pkgdep/sles.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Minimal install
+zypper install -y gcc gcc-c++ make cunit-devel libaio-devel libopenssl-devel \
+	libuuid-devel python-base ncurses-devel ninja meson
+# Additional dependencies for DPDK
+zypper install -y libnuma-devel nasm
+# Additional dependencies for ISA-L used in compression
+zypper install -y autoconf automake libtool help2man
+if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
+	# Tools for developers
+	zypper install -y git-core lcov python-pycodestyle sg3_utils \
+		pciutils ShellCheck
+	# Additional (optional) dependencies for showing backtrace in logs
+	zypper install libunwind-devel || true
+	install_shfmt
+fi
+if [[ $INSTALL_PMEM == "true" ]]; then
+	# Additional dependencies for building pmem based backends
+	zypper install -y libpmemblk-devel
+fi
+if [[ $INSTALL_FUSE == "true" ]]; then
+	# Additional dependencies for FUSE and NVMe-CUSE
+	zypper install -y fuse3-devel
+fi
+if [[ $INSTALL_RDMA == "true" ]]; then
+	# Additional dependencies for RDMA transport in NVMe over Fabrics
+	zypper install -y rdma-core-devel
+fi
+if [[ $INSTALL_DOCS == "true" ]]; then
+	# Additional dependencies for building docs
+	zypper install -y doxygen mscgen graphviz
+fi
