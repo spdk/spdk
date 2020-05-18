af8f939e2f74153f7592127ee2747990264822c5
Michal Berger <michalx.berger@intel.com>
2020-05-18 15:16:50 +0200

    pkgdep: Intrdouce FreeBSD-specific source
    Change-Id: I3617a08089b11ace54f34ce2f21bd120a9aa05a3
    Signed-off-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2507
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

---
+++ scripts/pkgdep/freebsd.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Minimal install
+pkg install -y gmake cunit openssl git bash misc/e2fsprogs-libuuid python \
+	ncurses ninja meson
+# Additional dependencies for ISA-L used in compression
+pkg install -y autoconf automake libtool help2man
+if [[ $INSTALL_DEV_TOOLS == "true" ]]; then
+	# Tools for developers
+	pkg install -y devel/astyle bash py27-pycodestyle \
+		misc/e2fsprogs-libuuid sysutils/sg3_utils nasm
+	install_shfmt
+fi
+if [[ $INSTALL_DOCS == "true" ]]; then
+	# Additional dependencies for building docs
+	pkg install -y doxygen mscgen graphviz
+fi
