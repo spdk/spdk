e8d8cef0fd304de02785a235b87a4a46de292435
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2020-05-25 05:44:36 -0400

    make: allow individual SO version for each library
    Based on patch:
    (19392783)make: rev SO versions individually for libraries.
    
    It allows each library to update its own version separate
    from the SO_SUFFIX_ALL==2.0.
    
    This will allow increasing SO_MINOR version when needed.
    
    Change-Id: Ic381a848e5f0e5af4b7f68725eb45138e00ca65b
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2593
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

--- mk/spdk.lib.mk
+++ mk/spdk.lib.mk
@@ -37,7 +37,11 @@ include $(SPDK_ROOT_DIR)/mk/spdk.lib_deps.mk
 SPDK_MAP_FILE = $(SPDK_ROOT_DIR)/shared_lib/spdk.map
 LIB := $(call spdk_lib_list_to_static_libs,$(LIBNAME))
 SHARED_LINKED_LIB := $(subst .a,.so,$(LIB))
+ifdef SO_SUFFIX
+SHARED_REALNAME_LIB := $(subst .so,.so.$(SO_SUFFIX),$(SHARED_LINKED_LIB))
+else
 SHARED_REALNAME_LIB := $(subst .so,.so.$(SO_SUFFIX_ALL),$(SHARED_LINKED_LIB))
+endif
 
 ifeq ($(CONFIG_SHARED),y)
 DEP := $(SHARED_LINKED_LIB)
