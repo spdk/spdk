cc163dd47ec7962aa15d641d74feb1d4c2aba881
Michal Berger <michalx.berger@intel.com>
2020-06-16 10:51:57 +0200

    test/common: Add spdk_dd to applications.sh
    Change-Id: I08fa2b00ff06384c08b9d5a81ae038aee729209f
    Signed-off-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2897
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/common/applications.sh
+++ test/common/applications.sh
@@ -9,6 +9,7 @@ VHOST_FUZZ_APP=("$_test_app_dir/fuzz/vhost_fuzz/vhost_fuzz")
 ISCSI_APP=("$_app_dir/iscsi_tgt")
 NVMF_APP=("$_app_dir/nvmf_tgt")
 VHOST_APP=("$_app_dir/vhost")
+DD_APP=("$_app_dir/spdk_dd")
 
 # Check if apps should execute under debug flags
 if [[ -e $_root/include/spdk/config.h ]]; then
@@ -18,5 +19,6 @@ if [[ -e $_root/include/spdk/config.h ]]; then
 		ISCSI_APP+=("--logflag=all")
 		NVMF_APP+=("--logflag=all")
 		VHOST_APP+=("--logflag=all")
+		DD_APP+=("--logflag=all")
 	fi
 fi
