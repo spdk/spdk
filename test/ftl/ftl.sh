058be487c3978f59f17bc41e0adbd0955814df00
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2020-06-18 05:45:19 -0400

    test/ftl: point to absolute path to setup.sh
    $rootdir is used already in this script, but couple instances
    were left out.
    We should always point to $rootdir or $testdir when refering to
    scripts and test files.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: Ifeb4527c6fd42881a6e04c1406a765f57498ae7d
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/2937
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>

--- test/ftl/ftl.sh
+++ test/ftl/ftl.sh
@@ -9,7 +9,7 @@ rpc_py=$rootdir/scripts/rpc.py
 
 function at_ftl_exit() {
 	# restore original driver
-	PCI_WHITELIST="$device" PCI_BLACKLIST="" DRIVER_OVERRIDE="$ocssd_original_dirver" ./scripts/setup.sh
+	PCI_WHITELIST="$device" PCI_BLACKLIST="" DRIVER_OVERRIDE="$ocssd_original_dirver" $rootdir/scripts/setup.sh
 }
 
 read -r device _ <<< "$OCSSD_PCI_DEVICES"
@@ -27,7 +27,7 @@ ocssd_original_dirver="$(basename $(readlink /sys/bus/pci/devices/$device/driver
 trap 'at_ftl_exit' SIGINT SIGTERM EXIT
 
 # OCSSD is blacklisted so bind it to vfio/uio driver before testing
-PCI_WHITELIST="$device" PCI_BLACKLIST="" DRIVER_OVERRIDE="" ./scripts/setup.sh
+PCI_WHITELIST="$device" PCI_BLACKLIST="" DRIVER_OVERRIDE="" $rootdir/scripts/setup.sh
 
 # Use first regular NVMe disk (non-OC) as non-volatile cache
 nvme_disks=$($rootdir/scripts/gen_nvme.sh --json | jq -r \
