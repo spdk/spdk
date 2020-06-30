f6a0f36c2e6e74e32a26dce503fb24af81ddc066
Michal Berger <michalx.berger@intel.com>
2020-06-30 14:49:52 +0200

    test/dd: Add simple test for copying data between two bdevs
    Change-Id: I43cb41a033aa5be669f73f7bcd8d8b645e078abc
    Signed-off-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/3125
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/dd/dd.sh
+++ test/dd/dd.sh
@@ -8,3 +8,4 @@ nvmes=($(nvme_in_userspace))
 
 run_test "spdk_dd_basic_rw" "$testdir/basic_rw.sh" "${nvmes[@]}"
 run_test "spdk_dd_posix" "$testdir/posix.sh"
+run_test "spdk_dd_bdev_to_bdev" "$testdir/bdev_to_bdev.sh" "${nvmes[@]}"
