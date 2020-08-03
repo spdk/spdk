8f7cb5e6dde083518cb953b266dc68ce3b259a42
WANGHAILIANG <hailiangx.e.wang@intel.com>
2020-08-03 16:45:06 +0800

    script/pkgdep: fix pip-3 to pip3
    Change-Id: Ibda02ab0efcd333d0623a6efe9c540ddd65b0af2
    Signed-off-by: WANGHAILIANG <hailiangx.e.wang@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/3627
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot

--- scripts/pkgdep/rhel.sh
+++ scripts/pkgdep/rhel.sh
@@ -16,8 +16,8 @@ else
 	yum install -y python
 fi
 yum install -y python3-pip
-pip-3 install ninja
-pip-3 install meson
+pip3 install ninja
+pip3 install meson
 
 # Additional dependencies for SPDK CLI - not available in rhel and centos
 if ! echo "$ID $VERSION_ID" | grep -E -q 'rhel 7|centos 7'; then
