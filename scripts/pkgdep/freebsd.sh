684b104389d9e9e29883e2efed0cf565e61b9c27
Michal Berger <michalx.berger@intel.com>
2020-09-11 13:26:27 +0200

    scripts/pkgdep: Disable liburing installation on FreeBSD
    Change-Id: Ifc5e28feacc27f1607a2aa19f3147c6cc9cad106
    Signed-off-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/4161
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Ziye Yang <ziye.yang@intel.com>
    Reviewed-by: John Kariuki <John.K.Kariuki@intel.com>

--- scripts/pkgdep/freebsd.sh
+++ scripts/pkgdep/freebsd.sh
@@ -14,3 +14,9 @@ if [[ $INSTALL_DOCS == "true" ]]; then
 	# Additional dependencies for building docs
 	pkg install -y doxygen mscgen graphviz
 fi
+
+if [[ $INSTALL_LIBURING == "true" ]]; then
+	printf 'liburing is not supported on %s, disabling\n' \
+		"$(freebsd-version)"
+	INSTALL_LIBURING=false
+fi
