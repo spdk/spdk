26811624b80eade1edaf5cb79d9e3c8d2d7e523b
Karol Latecki <karol.latecki@intel.com>
2020-10-07 14:53:13 +0200

    autobuild/external: preserve PATH env
    sudo -E is not enough to preserve PATH, which after
    sudo does not contain /usr/local/bin where "meson"
    bin needed for DPDK is located.
    
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>
    Change-Id: Ied3126dac66fc667a278aa3a973565eab02c1f6e
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/4549
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- autobuild.sh
+++ autobuild.sh
@@ -215,7 +215,7 @@ function build_doc() {
 
 function autobuild_test_suite() {
 	run_test "autobuild_check_format" ./scripts/check_format.sh
-	run_test "autobuild_external_code" sudo -E $rootdir/test/external_code/test_make.sh $rootdir
+	run_test "autobuild_external_code" sudo -E --preserve-env=PATH $rootdir/test/external_code/test_make.sh $rootdir
 	if [ "$SPDK_TEST_OCF" -eq 1 ]; then
 		run_test "autobuild_ocf_precompile" ocf_precompile
 	fi
