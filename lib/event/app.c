spdk/spdk

commit 8a76c2484a2eae4014a1c22e985b20b2cef801df

event/app: allow to enable log flag for release builds
Log flags could also report on SPDK_LOG_INFO level by using
SPDK_INFOLOG() macro. Yet this did not result in additional
log due to check for debug build.

This patch allows release builds to use that flag.
Meanwhile the -L option besides turning on particular log flag
changes print log level to SPDK_LOG_DEBUG.

Applied changes to serveral applications to follow the
behaviour of event framework.

Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
Change-Id: Ib0d6cce33263c350d6d83300d60e7d15bdfe4b64
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/4557
Reviewed-by: Jacek Kalwas <jacek.kalwas@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>
Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

lib/event/app.c
@@ -964,18 +964,13 @@ spdk_app_parse_args(int argc, char **argv, struct spdk_app_opts *opts,
 			}
 			break;
 		case LOGFLAG_OPT_IDX:
-#ifndef DEBUG
-			SPDK_ERRLOG("%s must be configured with --enable-debug for -L flag\n",
-				    argv[0]);
-			usage(app_usage);
-			goto out;
-#else
 			rc = spdk_log_set_flag(optarg);
 			if (rc < 0) {
 				SPDK_ERRLOG("unknown flag\n");
 				usage(app_usage);
 				goto out;
 			}
+#ifdef DEBUG
 			opts->print_level = SPDK_LOG_DEBUG;
 			break;
 #endif

examples/nvme/abort/abort.c
@@ -711,12 +711,8 @@ parse_args(int argc, char **argv)
 				usage(argv[0]);
 				exit(EXIT_FAILURE);
 			}
+#ifdef DEBUG
 			spdk_log_set_print_level(SPDK_LOG_DEBUG);
-#ifndef DEBUG
-			fprintf(stderr, "%s must be rebuilt with CONFIG_DEBUG=y for -T flag.\n",
-				argv[0]);
-			usage(argv[0]);
-			return 0;
 #endif
 			break;
 		default:

examples/nvme/identify/identify.c
@@ -1830,12 +1830,8 @@ parse_args(int argc, char **argv)
 				usage(argv[0]);
 				exit(EXIT_FAILURE);
 			}
+#ifdef DEBUG
 			spdk_log_set_print_level(SPDK_LOG_DEBUG);
-#ifndef DEBUG
-			fprintf(stderr, "%s must be rebuilt with CONFIG_DEBUG=y for -L flag.\n",
-				argv[0]);
-			usage(argv[0]);
-			return 0;
 #endif
 			break;
 		case 'H':

examples/nvme/perf/perf.c
@@ -1936,12 +1936,8 @@ parse_args(int argc, char **argv)
 				usage(argv[0]);
 				exit(EXIT_FAILURE);
 			}
+#ifdef DEBUG
 			spdk_log_set_print_level(SPDK_LOG_DEBUG);
-#ifndef DEBUG
-			fprintf(stderr, "%s must be rebuilt with CONFIG_DEBUG=y for -T flag.\n",
-				argv[0]);
-			usage(argv[0]);
-			return 0;
 #endif
 			break;
 		case 'V':

examples/nvme/reconnect/reconnect.c
@@ -761,12 +761,8 @@ parse_args(int argc, char **argv)
 				usage(argv[0]);
 				exit(EXIT_FAILURE);
 			}
+#ifdef DEBUG
 			spdk_log_set_print_level(SPDK_LOG_DEBUG);
-#ifndef DEBUG
-			fprintf(stderr, "%s must be rebuilt with CONFIG_DEBUG=y for -T flag.\n",
-				argv[0]);
-			usage(argv[0]);
-			return 0;
 #endif
 			break;
 		default:

lib/log/log_flags.c
@@ -172,17 +172,12 @@ spdk_log_get_next_flag(struct spdk_log_flag *flag)
 void
 spdk_log_usage(FILE *f, const char *log_arg)
 {
-#ifdef DEBUG
 	struct spdk_log_flag *flag;
-	fprintf(f, " %s, --logflag <flag>    enable debug log flag (all", log_arg);
+	fprintf(f, " %s, --logflag <flag>    enable log flag (all", log_arg);
 
 	TAILQ_FOREACH(flag, &g_log_flags, tailq) {
 		fprintf(f, ", %s", flag->name);
 	}
 
 	fprintf(f, ")\n");
-#else
-	fprintf(f, " %s, --logflag <flag>    enable debug log flag (not supported"
-		" - must reconfigure with --enable-debug)\n", log_arg);
-#endif
 }

test/nvme/aer/aer.c
@@ -337,12 +337,8 @@ parse_args(int argc, char **argv)
 				usage(argv[0]);
 				exit(EXIT_FAILURE);
 			}
+#ifdef DEBUG
 			spdk_log_set_print_level(SPDK_LOG_DEBUG);
-#ifndef DEBUG
-			fprintf(stderr, "%s must be rebuilt with CONFIG_DEBUG=y for -L flag.\n",
-				argv[0]);
-			usage(argv[0]);
-			return 0;
 #endif
 			break;
 		case 'T':

commit eecc14df1958049e4134ab0a01daa610a598a789

event: fix fallthrough in spdk_app_parse_args
This issue was only seen in release builds since the
break was hidden under DEBUG.  This seems to indicate
we aren't compiling release builds with -werror in
CI.

Fixes: 8a76c2484 (event/app: allow to enable log flag...)

Signed-off-by: Jim Harris <james.r.harris@intel.com>
Change-Id: Ia5766af1d4a22f9269dd18c47d408a9d05b3ceb1
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/4714
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

lib/event/app.c
@@ -972,8 +972,8 @@ spdk_app_parse_args(int argc, char **argv, struct spdk_app_opts *opts,
 			}
 #ifdef DEBUG
 			opts->print_level = SPDK_LOG_DEBUG;
-			break;
 #endif
+			break;
 		case HUGE_UNLINK_OPT_IDX:
 			opts->unlink_hugepage = true;
 			break;