b3a8025d1e22f4b9c686b87c42c07100f5eea822
Liu Xiaodong <xiaodong.liu@intel.com>
2020-10-27 05:10:00 -0400

    hello_sock: print new line after print usage
    Change-Id: I25b82d80b5f622c346df0577eac274b4b50a30a1
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/4883
    Community-CI: Mellanox Build Bot
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- examples/sock/hello_world/hello_sock.c
+++ examples/sock/hello_world/hello_sock.c
@@ -88,7 +88,7 @@ hello_sock_usage(void)
 	printf(" -P port       port number\n");
 	printf(" -N sock_impl  socket implementation, e.g., -N posix or -N uring\n");
 	printf(" -S            start in server mode\n");
-	printf(" -V            print out additional informations");
+	printf(" -V            print out additional informations\n");
 }
 
 /*
