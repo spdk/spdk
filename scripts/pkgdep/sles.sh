0e99c68d61a6950eb1d6e5e552a4a835999c9fca
Changpeng Liu <changpeng.liu@intel.com>
2020-11-23 23:01:51 -0500

    pkgdep: install cmake
    cmake is required by the coming libmuser, it's already installed
    in some environments, here install it with more environments.
    
    Change-Id: Ia7ce5ddaad95342e88ddcec327ac9c0eee51297d
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/5207
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- scripts/pkgdep/sles.sh
+++ scripts/pkgdep/sles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Minimal install
-zypper install -y gcc gcc-c++ make cunit-devel libaio-devel libopenssl-devel \
+zypper install -y gcc gcc-c++ make cmake cunit-devel libaio-devel libopenssl-devel \
 	libuuid-devel python-base ncurses-devel ninja meson
 # Additional dependencies for DPDK
 zypper install -y libnuma-devel nasm
