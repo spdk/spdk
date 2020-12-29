6ea36f46c4566148c398613514103f013cab0ea3
Alexey Marchuk <alexeymar@mellanox.com>
2020-12-29 18:36:22 +0300

    nvme: Update CQ error logging
    Current version provides unclear output
    
    Change-Id: Ib044b00b5f91b1e363911f1b79c51c73c8a6920c
    Signed-off-by: Alexey Marchuk <alexeymar@mellanox.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/5743
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Ziye Yang <ziye.yang@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- lib/nvme/nvme_qpair.c
+++ lib/nvme/nvme_qpair.c
@@ -713,7 +713,7 @@ spdk_nvme_qpair_process_completions(struct spdk_nvme_qpair *qpair, uint32_t max_
 	qpair->in_completion_context = 1;
 	ret = nvme_transport_qpair_process_completions(qpair, max_completions);
 	if (ret < 0) {
-		SPDK_ERRLOG("CQ error, abort requests after transport retry counter exceeded\n");
+		SPDK_ERRLOG("CQ transport error %d on qpair id %hu\n", ret, qpair->id);
 		if (nvme_qpair_is_admin_queue(qpair)) {
 			nvme_ctrlr_fail(qpair->ctrlr, false);
 		}
