0b6f2e236d0966f3f862d6c5327ebb893eaf6061
wanghailiangx <hailiangx.e.wang@intel.com>
2021-01-21 10:54:04 -0500

    example/nvme/plugin: set bs 4160 for Extended Data LBA
    The original value of bs is 4096, we can rewrite it in another script.
    When spdk_nvme_ns_supports_extended_lba(ns) is 1, we need to
    test with an integer multiple value of extended_sector_size, such as 4160.
    
    Change-Id: I8257bee4f741f62597bb2bb645ee9c59cf57bf33
    Signed-off-by: wanghailiangx <hailiangx.e.wang@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6018
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

--- test/nvme/nvme.sh
+++ test/nvme/nvme.sh
@@ -30,7 +30,12 @@ function nvme_fio_test() {
 		if $SPDK_EXAMPLE_DIR/identify -r "trtype:PCIe traddr:${bdf}" | grep -E "^Number of Namespaces" - | grep -q "0" -; then
 			continue
 		fi
-		fio_nvme $PLUGIN_DIR/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.}"
+		if $SPDK_EXAMPLE_DIR/identify -r "trtype:PCIe traddr:${bdf}" | grep -q "Extended Data LBA"; then
+			bs=4160
+		else
+			bs=4096
+		fi
+		fio_nvme $PLUGIN_DIR/example_config.fio --filename="trtype=PCIe traddr=${bdf//:/.}" --bs="$bs"
 		ran_fio=true
 	done
 	$ran_fio || (echo "No valid NVMe drive found. Failing test." && false)
