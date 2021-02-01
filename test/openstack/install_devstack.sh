02da8ebd2ef51128d2fd822f73e739de14f8c228
Maciej Wawryk <maciejx.wawryk@intel.com>
2021-02-01 11:27:10 +0100

    test/openstack: retry git clone when failed
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: If0c814b36c29aa301a6b77a3b5e74465cccfc0bf
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6195
    Reviewed-by: Maciej Szwed <maciej.szwed@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-by: Michal Berger <michalx.berger@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot

--- test/openstack/install_devstack.sh
+++ test/openstack/install_devstack.sh
@@ -37,11 +37,15 @@ su -c "./unstack.sh" -s /bin/bash stack
 cd /opt/stack
 rm -rf cinder devstack glance keystone heat horizon neutron nova placement requirements tacker tacker-horizon tempest
 
-if [[ $branch == "master" ]]; then
-	su -c "git clone --depth 1 https://opendev.org/openstack-dev/devstack" -s /bin/bash stack
-else
-	su -c "git clone --depth 1 https://opendev.org/openstack-dev/devstack -b stable/$branch" -s /bin/bash stack
-fi
+r=0
+until [[ $r -ge 20 ]]; do
+	if [[ $branch == "master" ]]; then
+		su -c "git clone --depth 1 https://opendev.org/openstack-dev/devstack" -s /bin/bash stack && break
+	else
+		su -c "git clone --depth 1 https://opendev.org/openstack-dev/devstack -b stable/$branch" -s /bin/bash stack && break
+	fi
+	r=$((r + 1))
+done
 cp $rootdir/scripts/vagrant/local.conf /opt/stack/devstack/local.conf
 
 cd /opt/stack/devstack
