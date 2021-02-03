a3b56e7f4db9bd172ad2d3d0b6b62265a433b9b0
Changpeng Liu <changpeng.liu@intel.com>
2021-02-03 17:24:06 +0800

    test/nvmf_vfio_user: add one more device to the target
    Previously we only tested one device with vfio-user target,
    now add one more device to the test script.
    
    Change-Id: I6923a313b23f93e6fdc3f25baa060b9af43ba7f7
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6228
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: <dongx.yi@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/nvmf/target/nvmf_vfio_user.sh
+++ test/nvmf/target/nvmf_vfio_user.sh
@@ -5,8 +5,9 @@ rootdir=$(readlink -f $testdir/../../..)
 source $rootdir/test/common/autotest_common.sh
 source $rootdir/test/nvmf/common.sh
 
-MALLOC_BDEV_SIZE=512
+MALLOC_BDEV_SIZE=64
 MALLOC_BLOCK_SIZE=512
+NUM_DEVICES=2
 
 rpc_py="$rootdir/scripts/rpc.py"
 
@@ -14,11 +15,6 @@ export TEST_TRANSPORT=VFIOUSER
 
 rm -rf /var/run/muser
 rm -rf /dev/shm/muser
-
-mkdir -p /var/run/muser
-mkdir -p /var/run/muser/iommu_group
-mkdir -p /var/run/muser/domain/muser0/8
-mkdir -p /dev/shm/muser/muser0
 
 # Start the target
 "${NVMF_APP[@]}" -m 0x1 &
@@ -32,23 +28,33 @@ sleep 1
 
 $rpc_py nvmf_create_transport -t VFIOUSER
 
-$rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc0
-$rpc_py nvmf_create_subsystem nqn.2019-07.io.spdk:cnode0 -a -s SPDK0
-$rpc_py nvmf_subsystem_add_ns nqn.2019-07.io.spdk:cnode0 Malloc0
-$rpc_py nvmf_subsystem_add_listener nqn.2019-07.io.spdk:cnode0 -t VFIOUSER -a "/var/run/muser/domain/muser0/8" -s 0
+mkdir -p /var/run/muser
+mkdir -p /var/run/muser/iommu_group
 
-ln -s /var/run/muser/domain/muser0/8 /var/run/muser/domain/muser0/8/iommu_group
-ln -s /var/run/muser/domain/muser0/8 /var/run/muser/iommu_group/8
-ln -s /var/run/muser/domain/muser0/8/bar0 /dev/shm/muser/muser0/bar0
+for i in $(seq 1 $NUM_DEVICES); do
+	mkdir -p /var/run/muser/domain/muser$i/$i
+	mkdir -p /dev/shm/muser/muser$i
 
-$SPDK_EXAMPLE_DIR/identify -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -g -L nvme -L nvme_vfio -L vfio_pci
-sleep 1
-$SPDK_EXAMPLE_DIR/perf -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -s 256 -g -q 128 -o 4096 -w read -t 10 -c 0x2
-sleep 1
-$SPDK_EXAMPLE_DIR/perf -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -s 256 -g -q 128 -o 4096 -w write -t 10 -c 0x2
-sleep 1
-$SPDK_EXAMPLE_DIR/reconnect -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -g -q 32 -o 4096 -w randrw -M 50 -t 10 -c 0xE
-sleep 1
+	$rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc$i
+	$rpc_py nvmf_create_subsystem nqn.2019-07.io.spdk:cnode$i -a -s SPDK$i
+	$rpc_py nvmf_subsystem_add_ns nqn.2019-07.io.spdk:cnode$i Malloc$i
+	$rpc_py nvmf_subsystem_add_listener nqn.2019-07.io.spdk:cnode$i -t VFIOUSER -a "/var/run/muser/domain/muser$i/$i" -s 0
+
+	ln -s /var/run/muser/domain/muser$i/$i /var/run/muser/domain/muser$i/$i/iommu_group
+	ln -s /var/run/muser/domain/muser$i/$i /var/run/muser/iommu_group/$i
+	ln -s /var/run/muser/domain/muser$i/$i/bar0 /dev/shm/muser/muser$i/bar0
+done
+
+for i in $(seq 1 $NUM_DEVICES); do
+	$SPDK_EXAMPLE_DIR/identify -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -g -L nvme -L nvme_vfio -L vfio_pci
+	sleep 1
+	$SPDK_EXAMPLE_DIR/perf -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -s 256 -g -q 128 -o 4096 -w read -t 5 -c 0x2
+	sleep 1
+	$SPDK_EXAMPLE_DIR/perf -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -s 256 -g -q 128 -o 4096 -w write -t 5 -c 0x2
+	sleep 1
+	$SPDK_EXAMPLE_DIR/reconnect -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -g -q 32 -o 4096 -w randrw -M 50 -t 5 -c 0xE
+	sleep 1
+done
 
 killprocess $nvmfpid
 
