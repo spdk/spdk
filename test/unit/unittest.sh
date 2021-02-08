82d7226c8ec0d71b1a13dd8ede4baa91c0647b88
paul luse <paul.e.luse@intel.com>
2021-02-08 17:56:19 -0500

    test/accel: add first UT for accel framework
    Implementation is pretty solid now, add first test with many
    more to follow.
    
    Signed-off-by: paul luse <paul.e.luse@intel.com>
    Change-Id: I4b80f3108fcd07919949bcd14dadfdfeb10c45fd
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6332
    Community-CI: Broadcom CI
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/unit/unittest.sh
+++ test/unit/unittest.sh
@@ -193,6 +193,7 @@ if [ $(uname -s) = Linux ]; then
 	run_test "unittest_ftl" unittest_ftl
 fi
 
+run_test "unittest_accel" $valgrind $testdir/lib/accel/accel.c/accel_engine_ut
 run_test "unittest_ioat" $valgrind $testdir/lib/ioat/ioat.c/ioat_ut
 if grep -q '#define SPDK_CONFIG_IDXD 1' $rootdir/include/spdk/config.h; then
 	run_test "unittest_idxd" $valgrind $testdir/lib/idxd/idxd.c/idxd_ut
