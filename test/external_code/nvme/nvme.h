91b30e7aec98bb7ededfef609924aa3f5ac71dd6
Konrad Sztyber <konrad.sztyber@intel.com>
2021-02-11 15:49:35 +0100

    test/external_code: initial definitions for basic NVMe driver
    This patch introduces initial definitions for a stripped down version of
    an out-of-tree NVMe driver.  It's purpose is to showcase SPDK's
    interfaces for writing user-space drivers for various types of devices.
    The choice of NVMe as the example is based on the fact that it can be
    emulated by QEMU and the code can be borrowed from SPDK's regular NVMe
    driver.
    
    This driver will only provide the most basic functionalities (e.g.
    device probing, controller initialization, only admin queue support,
    etc.) and won't have support for any device quirks.  Therefore, it's
    only intented to be used with emulated devices.
    
    In addition, an application utilizing the driver to list all available
    NVMe controllers and print their identify data will be added.  It'll
    be a very basic, stripped down version of `examples/nvme/identify`.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I67c748aabf75a37ca72dfb74301a610f7c4ae2bc
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6663
    Community-CI: Broadcom CI
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

---
+++ test/external_code/nvme/nvme.h
@@ -0,0 +1,71 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef EXTERNAL_NVME_H
+#define EXTERNAL_NVME_H
+
+#include "spdk/env.h"
+
+struct nvme_ctrlr;
+
+/**
+ * Callback for nvme_probe() to report a device that has been attached to
+ * the userspace NVMe driver.
+ *
+ * \param cb_ctx Opaque value passed to spdk_nvme_attach_cb().
+ * \param addr NVMe PCI address.
+ * \param ctrlr Opaque handle to NVMe controller.
+ */
+typedef void (*nvme_attach_cb)(void *cb_ctx, const struct spdk_pci_addr *addr,
+			       struct nvme_ctrlr *ctrlr);
+
+/**
+ * Enumerate PCIe bus and attach all NVMe devices found to the driver.
+ *
+ * \param cb_ctx Opaque value which will be passed back in cb_ctx parameter of
+ * the callbacks.
+ * \param attach_cb will be called for each NVMe device found
+ *
+ * \return 0 on success, negative errno on failure.
+ */
+int nvme_probe(nvme_attach_cb attach_cb, void *ctx);
+
+/**
+ * Detach specified device returned by nvme_probe()'s attach_cb. After returning
+ * the nvme_ctrlr handle is no longer valid.
+ *
+ * \param ctrlr Opaque handle to NVMe controller.
+ */
+void nvme_detach(struct nvme_ctrlr *ctrlr);
+
+#endif /* EXTERNAL_NVME_H */
