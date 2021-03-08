ae53ab2dddad4ce7bf158f40b56a6bb234430f81
Konrad Sztyber <konrad.sztyber@intel.com>
2021-03-08 11:20:23 +0100

    test/external_code: renamed test case titles
    Replaced the "_tc[0-9]+" suffix with a more descriptive title describing
    the test case being executed.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: Id1c2f6dcf155d8ae99dc490983ab4bb1451bfefd
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6764
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot

--- test/external_code/test_make.sh
+++ test/external_code/test_make.sh
@@ -25,26 +25,30 @@ export VFIO_LIB_DIR="$SPDK_DIR/libvfio-user/build/release/lib"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SPDK_LIB_DIR:$DPDK_LIB_DIR:$VFIO_LIB_DIR:"$test_root/passthru"
 
 # The default target is to make both the app and bdev and link them against the combined SPDK shared library libspdk.so.
-run_test "external_make_tc1" make -C $test_root hello_world_bdev_shared_combo
-run_test "external_run_tc1" $test_root/hello_world/hello_bdev --json $test_root/hello_world/bdev_external.json -b TestPT
+run_test "external_make_hello_bdev_shared_combo" make -C $test_root hello_world_bdev_shared_combo
+run_test "external_run_hello_bdev_shared_combo" $test_root/hello_world/hello_bdev \
+	--json $test_root/hello_world/bdev_external.json -b TestPT
 
 make -C $test_root clean
 
 # Make just the application linked against the combined SPDK shared library libspdk.so.
-run_test "external_make_tc2" make -C $test_root hello_world_no_bdev_shared_combo
-run_test "external_run_tc2" $test_root/hello_world/hello_bdev --json $test_root/hello_world/bdev.json -b Malloc0
+run_test "external_make_hello_no_bdev_shared_combo" make -C $test_root hello_world_no_bdev_shared_combo
+run_test "external_run_hello_no_bdev_shared_combo" $test_root/hello_world/hello_bdev \
+	--json $test_root/hello_world/bdev.json -b Malloc0
 
 make -C $test_root clean
 
 # Make both the application and bdev against individual SPDK shared libraries.
-run_test "external_make_tc3" make -C $test_root hello_world_bdev_shared_iso
-run_test "external_run_tc3" $test_root/hello_world/hello_bdev --json $test_root/hello_world/bdev_external.json -b TestPT
+run_test "external_make_hello_bdev_shared_iso" make -C $test_root hello_world_bdev_shared_iso
+run_test "external_run_hello_bdev_shared_iso" $test_root/hello_world/hello_bdev \
+	--json $test_root/hello_world/bdev_external.json -b TestPT
 
 make -C $test_root clean
 
 # Make just the application linked against individual SPDK shared libraries.
-run_test "external_make_tc4" make -C $test_root hello_world_no_bdev_shared_iso
-run_test "external_run_tc4" $test_root/hello_world/hello_bdev --json $test_root/hello_world/bdev.json -b Malloc0
+run_test "external_make_hello_no_bdev_shared_iso" make -C $test_root hello_world_no_bdev_shared_iso
+run_test "external_run_hello_no_bdev_shared_iso" $test_root/hello_world/hello_bdev \
+	--json $test_root/hello_world/bdev.json -b Malloc0
 
 make -C $test_root clean
 
@@ -53,14 +57,16 @@ $SPDK_DIR/configure --without-shared --without-isal --without-ocf --disable-asan
 make -C $SPDK_DIR -j$(nproc)
 
 # Make both the application and bdev against individual SPDK archives.
-run_test "external_make_tc5" make -C $test_root hello_world_bdev_static
-run_test "external_run_tc5" $test_root/hello_world/hello_bdev --json $test_root/hello_world/bdev_external.json -b TestPT
+run_test "external_make_hello_bdev_static" make -C $test_root hello_world_bdev_static
+run_test "external_run_hello_bdev_static" $test_root/hello_world/hello_bdev \
+	--json $test_root/hello_world/bdev_external.json -b TestPT
 
 make -C $test_root clean
 
 # Make just the application linked against individual SPDK archives.
-run_test "external_make_tc6" make -C $test_root hello_world_no_bdev_static
-run_test "external_run_tc6" $test_root/hello_world/hello_bdev --json $test_root/hello_world/bdev.json -b Malloc0
+run_test "external_make_hello_no_bdev_static" make -C $test_root hello_world_no_bdev_static
+run_test "external_run_hello_no_bdev_static" $test_root/hello_world/hello_bdev \
+	--json $test_root/hello_world/bdev.json -b Malloc0
 
 make -C $test_root clean
 make -C $SPDK_DIR -j$(nproc) clean
