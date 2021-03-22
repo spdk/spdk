53603323e5f12071d97d21cbd1f36badac097da6
Karol Latecki <karol.latecki@intel.com>
2021-03-22 17:51:26 +0100

    autotest.sh: call rdma_device_init only for RDMA tests
    Check test flags before calling rdma_device_init if
    it's really needed, so that we don't call it when doing
    TCP tests.
    
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/6998 (master)
    
    (cherry picked from commit b7423187a2516d77689a4ab94e1249c1475baba5)
    Change-Id: I2675a008cf84c7c4084cb83b44c279d2a497c3cb
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/7805
    Tested-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/spdkcli/nvmf.sh
+++ test/spdkcli/nvmf.sh
@@ -10,14 +10,16 @@ MATCH_FILE="spdkcli_nvmf.test"
 SPDKCLI_BRANCH="/nvmf"
 
 trap 'on_error_exit; revert_soft_roce' ERR
-rdma_device_init
 
 timing_enter run_nvmf_tgt
 run_nvmf_tgt
 timing_exit run_nvmf_tgt
 
-RDMA_IP_LIST=$(get_available_rdma_ips)
-NVMF_TARGET_IP=$(echo "$RDMA_IP_LIST" | head -n 1)
+NVMF_TARGET_IP="127.0.0.1"
+if [[ $TEST_TRANSPORT == "rdma" ]]; then
+	rdma_device_init
+	NVMF_TARGET_IP=$(get_available_rdma_ips | head -n 1)
+fi
 
 timing_enter spdkcli_create_nvmf_config
 $spdkcli_job "'/bdevs/malloc create 32 512 Malloc1' 'Malloc1' True
@@ -26,33 +28,33 @@ $spdkcli_job "'/bdevs/malloc create 32 512 Malloc1' 'Malloc1' True
 '/bdevs/malloc create 32 512 Malloc4' 'Malloc4' True
 '/bdevs/malloc create 32 512 Malloc5' 'Malloc5' True
 '/bdevs/malloc create 32 512 Malloc6' 'Malloc6' True
-'nvmf/transport create RDMA max_io_qpairs_per_ctrlr=4 io_unit_size=8192' '' True
+'nvmf/transport create $TEST_TRANSPORT max_io_qpairs_per_ctrlr=4 io_unit_size=8192' '' True
 '/nvmf/subsystem create nqn.2014-08.org.spdk:cnode1 N37SXV509SRW\
   max_namespaces=4 allow_any_host=True' 'nqn.2014-08.org.spdk:cnode1' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/namespaces create Malloc3 1' 'Malloc3' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/namespaces create Malloc4 2' 'Malloc4' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses create \
- RDMA $NVMF_TARGET_IP 4260 IPv4' '$NVMF_TARGET_IP:4260' True
+ $TEST_TRANSPORT $NVMF_TARGET_IP 4260 IPv4' '$NVMF_TARGET_IP:4260' True
 '/nvmf/subsystem create nqn.2014-08.org.spdk:cnode2 N37SXV509SRD\
  max_namespaces=2 allow_any_host=True' 'nqn.2014-08.org.spdk:cnode2' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode2/namespaces create Malloc2' 'Malloc2' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode2/listen_addresses create \
- RDMA $NVMF_TARGET_IP 4260 IPv4' '$NVMF_TARGET_IP:4260' True
+ $TEST_TRANSPORT $NVMF_TARGET_IP 4260 IPv4' '$NVMF_TARGET_IP:4260' True
 '/nvmf/subsystem create nqn.2014-08.org.spdk:cnode3 N37SXV509SRR\
  max_namespaces=2 allow_any_host=True' 'nqn.2014-08.org.spdk:cnode2' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode3/namespaces create Malloc1' 'Malloc1' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode3/listen_addresses create \
- RDMA $NVMF_TARGET_IP 4260 IPv4' '$NVMF_TARGET_IP:4260' True
+ $TEST_TRANSPORT $NVMF_TARGET_IP 4260 IPv4' '$NVMF_TARGET_IP:4260' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode3/listen_addresses create \
- RDMA $NVMF_TARGET_IP 4261 IPv4' '$NVMF_TARGET_IP:4261' True
+ $TEST_TRANSPORT $NVMF_TARGET_IP 4261 IPv4' '$NVMF_TARGET_IP:4261' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode3/hosts create \
  nqn.2014-08.org.spdk:cnode1' 'nqn.2014-08.org.spdk:cnode1' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode3/hosts create \
  nqn.2014-08.org.spdk:cnode2' 'nqn.2014-08.org.spdk:cnode2' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1 allow_any_host True' 'Allow any host'
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1 allow_any_host False' 'Allow any host' True
-'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses create RDMA $NVMF_TARGET_IP 4261 IPv4' '$NVMF_TARGET_IP:4261' True
-'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses create RDMA $NVMF_TARGET_IP 4262 IPv4' '$NVMF_TARGET_IP:4262' True
+'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses create $TEST_TRANSPORT $NVMF_TARGET_IP 4261 IPv4' '$NVMF_TARGET_IP:4261' True
+'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses create $TEST_TRANSPORT $NVMF_TARGET_IP 4262 IPv4' '$NVMF_TARGET_IP:4262' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/hosts create nqn.2014-08.org.spdk:cnode2' 'nqn.2014-08.org.spdk:cnode2' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/namespaces create Malloc5' 'Malloc5' True
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/namespaces create Malloc6' 'Malloc6' True
@@ -68,7 +70,7 @@ $spdkcli_job "'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/namespaces delete nsi
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/namespaces delete_all' 'Malloc4'
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/hosts delete nqn.2014-08.org.spdk:cnode2' 'nqn.2014-08.org.spdk:cnode2'
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode3/hosts delete_all' 'nqn.2014-08.org.spdk:cnode1'
-'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses delete RDMA $NVMF_TARGET_IP 4262' '$NVMF_TARGET_IP:4262'
+'/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses delete $TEST_TRANSPORT $NVMF_TARGET_IP 4262' '$NVMF_TARGET_IP:4262'
 '/nvmf/subsystem/nqn.2014-08.org.spdk:cnode1/listen_addresses delete_all' '$NVMF_TARGET_IP:4261'
 '/nvmf/subsystem delete nqn.2014-08.org.spdk:cnode3' 'nqn.2014-08.org.spdk:cnode3'
 '/nvmf/subsystem delete_all' 'nqn.2014-08.org.spdk:cnode2'
