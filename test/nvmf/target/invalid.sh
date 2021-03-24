33227bbcc04a34470c5ffd64917a30038c691bdb
Maciej Wawryk <maciejx.wawryk@intel.com>
2021-03-24 13:27:58 +0100

    test/nvmf: add remove non-existent subsystem listener test
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: I3504e874d8ec1c8bacbd394e3d4d208dc155f45c
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/7038
    Community-CI: Broadcom CI
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>

--- test/nvmf/target/invalid.sh
+++ test/nvmf/target/invalid.sh
@@ -55,6 +55,17 @@ out=$("$rpc" nvmf_create_subsystem -s "$(gen_random_s 21)" "$nqn$RANDOM" 2>&1) &
 out=$("$rpc" nvmf_create_subsystem -d "$(gen_random_s 41)" "$nqn$RANDOM" 2>&1) && false
 [[ $out == *"Invalid MN"* ]]
 
+# Attempt to delete non-existing subsytem listener
+$rpc nvmf_create_transport --trtype "$TEST_TRANSPORT"
+$rpc nvmf_create_subsystem $nqn -s SPDK001 -a
+if [[ $TEST_TRANSPORT == "TCP" ]]; then
+	IP="127.0.0.1"
+else
+	IP=$(echo "$RDMA_IP_LIST" | head -n 1)
+fi
+out=$("$rpc" nvmf_subsystem_remove_listener "$nqn" -t "$TEST_TRANSPORT" -a "$IP" -s 4421 2>&1) && false
+[[ $out != *"Unable to stop listener."* ]]
+
 # Attempt to delete non-existing target
 out=$("$multi_target_rpc" nvmf_delete_target --name "$target" 2>&1) && false
 [[ $out == *"The specified target doesn't exist, cannot delete it."* ]]
