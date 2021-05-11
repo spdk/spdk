5fdb2b761b88ea7672f7f80bfa7aaebb4255334d
Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
2021-05-12 06:49:20 +0900

    thread: Make struct spdk_poller and enum spdk_poller_state private in thread.c
    Move the definition of struct spdk_poller and enum spdk_poller_state
    from include/spdk_internal/thread.h to lib/thread/thread.c.
    
    Signed-off-by: Shuhei Matsumoto <shuhei.matsumoto.xt@hitachi.com>
    Change-Id: I4480a0b7a2a94ef97ff3185e458221e8c473665d
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/7799
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

--- include/spdk_internal/thread.h
+++ include/spdk_internal/thread.h
@@ -37,49 +37,9 @@
 #include "spdk/stdinc.h"
 #include "spdk/thread.h"
 
-#define SPDK_MAX_POLLER_NAME_LEN	256
 #define SPDK_MAX_THREAD_NAME_LEN	256
 
-enum spdk_poller_state {
-	/* The poller is registered with a thread but not currently executing its fn. */
-	SPDK_POLLER_STATE_WAITING,
-
-	/* The poller is currently running its fn. */
-	SPDK_POLLER_STATE_RUNNING,
-
-	/* The poller was unregistered during the execution of its fn. */
-	SPDK_POLLER_STATE_UNREGISTERED,
-
-	/* The poller is in the process of being paused.  It will be paused
-	 * during the next time it's supposed to be executed.
-	 */
-	SPDK_POLLER_STATE_PAUSING,
-
-	/* The poller is registered but currently paused.  It's on the
-	 * paused_pollers list.
-	 */
-	SPDK_POLLER_STATE_PAUSED,
-};
-
-struct spdk_poller {
-	TAILQ_ENTRY(spdk_poller)	tailq;
-
-	/* Current state of the poller; should only be accessed from the poller's thread. */
-	enum spdk_poller_state		state;
-
-	uint64_t			period_ticks;
-	uint64_t			next_run_tick;
-	uint64_t			run_count;
-	uint64_t			busy_count;
-	spdk_poller_fn			fn;
-	void				*arg;
-	struct spdk_thread		*thread;
-	int				interruptfd;
-	spdk_poller_set_interrupt_mode_cb set_intr_cb_fn;
-	void				*set_intr_cb_arg;
-
-	char				name[SPDK_MAX_POLLER_NAME_LEN + 1];
-};
+struct spdk_poller;
 
 struct spdk_poller_stats {
 	uint64_t	run_count;
