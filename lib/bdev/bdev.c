c514ba50537efe523e80b038a2b1d53ef08cde7a
Konrad Sztyber <konrad.sztyber@intel.com>
2021-06-21 08:49:59 +0200

    lib/bdev: replace spdk_trace_record_tsc() with spdk_trace_record()
    Replaced calls to `spdk_trace_record_tsc(spdk_get_ticks(), ...)` with
    `spdk_trace_record(...)`, which does the same thing but is more consise.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: Ib96e0bc0225490dadf857e1ddd2a3ecbf71e98c8
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/8444
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Ziye Yang <ziye.yang@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

--- lib/bdev/bdev.c
+++ lib/bdev/bdev.c
@@ -2152,8 +2152,7 @@ bdev_io_split_submit(struct spdk_bdev_io *bdev_io, struct iovec *iov, int iovcnt
 		} else {
 			bdev_io->internal.status = SPDK_BDEV_IO_STATUS_FAILED;
 			if (bdev_io->u.bdev.split_outstanding == 0) {
-				spdk_trace_record_tsc(spdk_get_ticks(), TRACE_BDEV_IO_DONE, 0, 0,
-						      (uintptr_t)bdev_io, 0);
+				spdk_trace_record(TRACE_BDEV_IO_DONE, 0, 0, (uintptr_t)bdev_io, 0);
 				TAILQ_REMOVE(&bdev_io->internal.ch->io_submitted, bdev_io, internal.ch_link);
 				bdev_io->internal.cb(bdev_io, false, bdev_io->internal.caller_ctx);
 			}
@@ -2268,8 +2267,7 @@ _bdev_rw_split(void *_bdev_io)
 							if (bdev_io->u.bdev.split_outstanding == 0) {
 								SPDK_ERRLOG("The first child io was less than a block size\n");
 								bdev_io->internal.status = SPDK_BDEV_IO_STATUS_FAILED;
-								spdk_trace_record_tsc(spdk_get_ticks(), TRACE_BDEV_IO_DONE, 0, 0,
-										      (uintptr_t)bdev_io, 0);
+								spdk_trace_record(TRACE_BDEV_IO_DONE, 0, 0, (uintptr_t)bdev_io, 0);
 								TAILQ_REMOVE(&bdev_io->internal.ch->io_submitted, bdev_io, internal.ch_link);
 								bdev_io->internal.cb(bdev_io, false, bdev_io->internal.caller_ctx);
 							}
@@ -2370,8 +2368,7 @@ bdev_io_split_done(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
 	 */
 	if (parent_io->u.bdev.split_remaining_num_blocks == 0) {
 		assert(parent_io->internal.cb != bdev_io_split_done);
-		spdk_trace_record_tsc(spdk_get_ticks(), TRACE_BDEV_IO_DONE, 0, 0,
-				      (uintptr_t)parent_io, 0);
+		spdk_trace_record(TRACE_BDEV_IO_DONE, 0, 0, (uintptr_t)parent_io, 0);
 		TAILQ_REMOVE(&parent_io->internal.ch->io_submitted, parent_io, internal.ch_link);
 		parent_io->internal.cb(parent_io, parent_io->internal.status == SPDK_BDEV_IO_STATUS_SUCCESS,
 				       parent_io->internal.caller_ctx);
