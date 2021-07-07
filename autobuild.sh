a56704a0e18e3ee76d9e8e8f7ba3b9e1348254aa
Pawel Piatek <pawelx.piatek@intel.com>
2021-07-07 10:48:08 +0200

    autobuild: Update patches for mainline DPDK
    Patch f95e331be3a1f856b816948990dd2afc67ea4020 is no longer needed,
    since it was solved at upstream level.
    
    Signed-off-by: Pawel Piatek <pawelx.piatek@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/8698 (master)
    
    (cherry picked from 93c4adda4d097821087ba18ee22160dbc6c558f7)
    Change-Id: Icf064b121351566988c26390f45286e7a456ca39
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/8731
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -133,12 +133,13 @@ function build_native_dpdk() {
 			wget https://github.com/karlatec/dpdk/commit/3219c0cfc38803aec10c809dde16e013b370bda9.patch -O dpdk-pci.patch
 			wget https://github.com/karlatec/dpdk/commit/adf8f7638de29bc4bf9ba3faf12bbdae73acda0c.patch -O dpdk-qat.patch
 		else
-			wget https://github.com/karlatec/dpdk/commit/f95e331be3a1f856b816948990dd2afc67ea4020.patch -O dpdk-pci.patch
 			wget https://github.com/karlatec/dpdk/commit/6fd2fa906ffdcee04e6ce5da40e61cb841be9827.patch -O dpdk-qat.patch
 		fi
 		git config --local user.name "spdk"
 		git config --local user.email "nomail@all.com"
-		git am dpdk-pci.patch
+		if [[ -f dpdk-pci.patch ]]; then
+			git am dpdk-pci.patch
+		fi
 		git am dpdk-qat.patch
 	fi
 
