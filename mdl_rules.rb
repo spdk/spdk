de3599f079d75a5e0a24bf1c1bf66557bb29cd30
wawryk <maciejx.wawryk@intel.com>
2021-08-02 12:46:13 +0200

    markdownlint: enable rule MD019
    MD019 - Multiple spaces after hash on atx style header
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: I3007f757997a3e9c5e22a926cffad2c68d2ca5ce
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/9037
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>

--- mdl_rules.rb
+++ mdl_rules.rb
@@ -3,7 +3,6 @@ exclude_rule 'MD003'
 exclude_rule 'MD004'
 exclude_rule 'MD010'
 rule 'MD013', :line_length => 170
-exclude_rule 'MD019'
 exclude_rule 'MD020'
 exclude_rule 'MD021'
 exclude_rule 'MD022'
