7be3e185a9c127b6aef29d2182f91b9583e5f037
Maciej Wawryk <maciejx.wawryk@intel.com>
2021-08-10 09:26:40 +0200

    markdownlint: enable rule MD029
    MD029 - Ordered list item prefix
    Set style to 'ordered'
    
    Signed-off-by: Maciej Wawryk <maciejx.wawryk@intel.com>
    Change-Id: If2068d88f90fb8324714ea3a2bd8bfa447d9df58
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/9140
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- mdl_rules.rb
+++ mdl_rules.rb
@@ -4,7 +4,7 @@ exclude_rule 'MD004'
 exclude_rule 'MD010'
 rule 'MD013', :line_length => 170
 exclude_rule 'MD024'
-exclude_rule 'MD029'
+rule 'MD029', :style => "ordered"
 exclude_rule 'MD030'
 exclude_rule 'MD031'
 exclude_rule 'MD032'
