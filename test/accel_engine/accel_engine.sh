ab48766fd13d73916a29a8c8720ad33ee556560a
paul luse <paul.e.luse@intel.com>
2021-08-16 15:50:24 -0400

    test/accel_engine: add more test cases
    There's been a few silly escapes lately that we can catch with
    some quick tests.
    
    Signed-off-by: paul luse <paul.e.luse@intel.com>
    Change-Id: I6f6e9591573b2ab54da239981423341aeb0bdbb9
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/9190
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Monica Kenguva <monica.kenguva@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>

--- test/accel_engine/accel_engine.sh
+++ test/accel_engine/accel_engine.sh
@@ -4,6 +4,13 @@ testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
 
-#Test case 1: crc32c test
+#Run through all SW ops with defaults for a quick sanity check
 #To save time, only use verification case
-run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -q 128 -o 4096 -t 2 -w crc32c -y
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w crc32c -y
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w crc32c -y -C 2
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy -y
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w fill -y
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy_crc32c -y
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy_crc32c -y -C 2
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w dualcast -y
+run_test "accel_engine" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w compare -y
