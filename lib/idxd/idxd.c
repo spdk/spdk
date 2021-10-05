e1c20e37558a32b4351c27041bc9e45c71ed8a0f
Monica Kenguva <monica.kenguva@intel.com>
2021-10-05 20:57:56 +0000

    examples/idxd: Add batch functionality to idxd perf tool
    Signed-off-by: Monica Kenguva <monica.kenguva@intel.com>
    Change-Id: Ib4e1c4d68092316b261cb84f0a1918a7e733dddf
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/9735
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- lib/idxd/idxd.c
+++ lib/idxd/idxd.c
@@ -1149,6 +1149,7 @@ spdk_idxd_process_events(struct spdk_idxd_io_channel *chan)
 			op->hw.status = 0;
 			if (op->desc->opcode == IDXD_OPCODE_BATCH) {
 				_free_batch(op->batch, chan);
+				TAILQ_INSERT_TAIL(&chan->ops_pool, op, link);
 			} else if (op->batch == NULL) {
 				TAILQ_INSERT_HEAD(&chan->ops_pool, op, link);
 			}
