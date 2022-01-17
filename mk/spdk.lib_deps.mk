81dca2888446f0f832a60f67c4706c8bf429d994
Wojciech Malikowski <wojciech.malikowski@intel.com>
2022-01-17 11:41:16 +0100

    ftl: remove deprecated ftl library
    Signed-off-by: Wojciech Malikowski <wojciech.malikowski@intel.com>
    Change-Id: I3ebb05be3f1b9864b238cb74f469b4fdf573cd0d
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/11120
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>

--- mk/spdk.lib_deps.mk
+++ mk/spdk.lib_deps.mk
@@ -132,9 +132,6 @@ DEPDIRS-bdev_rbd := $(BDEV_DEPS_THREAD)
 DEPDIRS-bdev_uring := $(BDEV_DEPS_THREAD)
 DEPDIRS-bdev_virtio := $(BDEV_DEPS_THREAD) virtio
 DEPDIRS-bdev_zone_block := $(BDEV_DEPS_THREAD)
-ifeq ($(OS),Linux)
-DEPDIRS-bdev_ftl := $(BDEV_DEPS_THREAD) ftl
-endif
 
 # module/event
 
--- mk/spdk.modules.mk
+++ mk/spdk.modules.mk
@@ -47,7 +47,6 @@ endif
 endif
 
 ifeq ($(OS),Linux)
-BLOCKDEV_MODULES_LIST += bdev_ftl ftl
 BLOCKDEV_MODULES_LIST += bdev_aio
 BLOCKDEV_MODULES_PRIVATE_LIBS += -laio
 INTR_BLOCKDEV_MODULES_LIST += bdev_aio
