a6eafd95d4bc845785b52c789f03414b537559d9
Karol Latecki <karol.latecki@intel.com>
2022-04-05 17:16:03 +0200

    autobuild: group crypto patch apply for DPDK
    Crypto patch for increasing maximum number of crypto
    devs applies to all DPDK releases since 21.11.
    Change the conditionals so that the patch is applied
    to any new DPDK version.
    Also change the patch location to "21.11+" directory
    to indicate that patch is not exclusive for 21.11
    version.
    
    Change-Id: I341f88958c6bf6c4d77909a06bf7ce91200cb70e
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/12159
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Reviewed-by: Michal Berger <michallinuxstuff@gmail.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@mellanox.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- autobuild.sh
+++ autobuild.sh
@@ -187,13 +187,11 @@ function build_native_dpdk() {
 
 	cd $external_dpdk_base_dir
 	if [ "$(uname -s)" = "Linux" ]; then
-		if [[ $dpdk_ver == 20.11* ]]; then
+		if lt $dpdk_ver 21.11.0; then
 			patch -p1 < "$rootdir/test/common/config/pkgdep/patches/dpdk/20.11/dpdk_pci.patch"
 			patch -p1 < "$rootdir/test/common/config/pkgdep/patches/dpdk/20.11/dpdk_qat.patch"
-		elif [[ $dpdk_ver == 21.11* ]]; then
-			patch -p1 < "$rootdir/test/common/config/pkgdep/patches/dpdk/21.11/dpdk_qat.patch"
-		elif [[ $dpdk_ver == 22.03* ]]; then
-			patch -p1 < "$rootdir/test/common/config/pkgdep/patches/dpdk/22.03/dpdk_qat.patch"
+		else
+			patch -p1 < "$rootdir/test/common/config/pkgdep/patches/dpdk/21.11+/dpdk_qat.patch"
 		fi
 	fi
 
