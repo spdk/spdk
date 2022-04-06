84863d99a8354f300b7f36f89ea41497e859413a
Gal Hammer <gal.hammer@huawei.com>
2022-04-06 12:28:36 +0300

    bdev/ocf: Fix no memory error value check
    On I/O request completion, OCF uses OCF_ERR_NO_MEM (and not ENOMEM) to
    report a memory allocation failure.
    
    Signed-off-by: Gal Hammer <gal.hammer@huawei.com>
    Signed-off-by: Shai Fultheim <shai.fultheim@huawei.com>
    Change-Id: If06608d7aa0fc747a79564b7372c915d99307235
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/12352
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Rafal Stefanowski <rafal.stefanowski@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>

--- module/bdev/ocf/vbdev_ocf.c
+++ module/bdev/ocf/vbdev_ocf.c
@@ -565,7 +565,7 @@ vbdev_ocf_io_submit_cb(struct ocf_io *io, int error)
 
 	if (error == 0) {
 		spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_SUCCESS);
-	} else if (error == -ENOMEM) {
+	} else if (error == -OCF_ERR_NO_MEM) {
 		spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_NOMEM);
 	} else {
 		spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_FAILED);
