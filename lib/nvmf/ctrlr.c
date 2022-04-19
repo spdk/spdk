48408177b5a47c20fb3796fc40cb1fca74aba30d
John Levon <john.levon@nutanix.com>
2022-04-19 11:00:02 +0100

    lib/nvmf: add a comment on max admin queue size
    Signed-off-by: John Levon <john.levon@nutanix.com>
    Change-Id: I247e95843bd15a341a66f7ab07d9639bea403bd4
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/12301
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Paul Luse <paul.e.luse@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- lib/nvmf/ctrlr.c
+++ lib/nvmf/ctrlr.c
@@ -1223,6 +1223,10 @@ nvmf_prop_set_aqa(struct spdk_nvmf_ctrlr *ctrlr, uint32_t value)
 
 	aqa.raw = value;
 
+	/*
+	 * We don't need to explicitly check for maximum size, as the fields are
+	 * limited to 12 bits (4096).
+	 */
 	if (aqa.bits.asqs < SPDK_NVME_ADMIN_QUEUE_MIN_ENTRIES - 1 ||
 	    aqa.bits.acqs < SPDK_NVME_ADMIN_QUEUE_MIN_ENTRIES - 1 ||
 	    aqa.bits.reserved1 != 0 || aqa.bits.reserved2 != 0) {
