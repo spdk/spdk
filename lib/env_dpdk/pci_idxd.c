87060965b3dcbfff3f18ebacceba3374634cbd24
paul luse <paul.e.luse@intel.com>
2022-05-20 08:59:23 -0700

    include/env: update PCI ID names from IDXD->DSA
    In prep for adding IAA support
    
    Signed-off-by: paul luse <paul.e.luse@intel.com>
    Change-Id: I7eed173f9f907aa1c010d12db87b8dc27cd7495b
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/12760
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- lib/env_dpdk/pci_idxd.c
+++ lib/env_dpdk/pci_idxd.c
@@ -37,7 +37,7 @@
 
 #define SPDK_IDXD_PCI_DEVICE(DEVICE_ID) SPDK_PCI_DEVICE(SPDK_PCI_VID_INTEL, DEVICE_ID)
 static struct spdk_pci_id idxd_driver_id[] = {
-	{SPDK_IDXD_PCI_DEVICE(PCI_DEVICE_ID_INTEL_IDXD)},
+	{SPDK_IDXD_PCI_DEVICE(PCI_DEVICE_ID_INTEL_DSA)},
 	{ .vendor_id = 0, /* sentinel */ },
 };
 
