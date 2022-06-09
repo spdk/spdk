400aadf83f642a88ccee358411e52f6642bce89a
Jaroslaw Chachulski <jaroslawx.chachulski@intel.com>
2022-06-09 05:03:05 -0400

    scripts/vagrant: Add ubuntu 22.04 lts support.
    Add support for ubuntu 22.04 lts.
    
    Signed-off-by: Jaroslaw Chachulski <jaroslawx.chachulski@intel.com>
    Change-Id: I6f598bbf1961c8266620acb69a388e94ed794d8f
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/12988
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- scripts/vagrant/create_vbox.sh
+++ scripts/vagrant/create_vbox.sh
@@ -19,7 +19,7 @@ display_help() {
 	echo
 	echo " Usage: ${0##*/} [-b nvme-backing-file] [-n <num-cpus>] [-s <ram-size>] [-x <http-proxy>] [-hvrldcufaoH] <distro>"
 	echo
-	echo "  distro = <centos7 | centos8 | ubuntu1604 | ubuntu1804 | ubuntu2004 | fedora33 |"
+	echo "  distro = <centos7 | centos8 | ubuntu1604 | ubuntu1804 | ubuntu2004 | ubuntu2204 | fedora33 |"
 	echo "            fedora34 | fedora35 | freebsd11 | freebsd12 | arch | clearlinux | rocky8>"
 	echo
 	echo "  -s <ram-size> in MB             Default: ${SPDK_VAGRANT_VMRAM}"
@@ -169,7 +169,7 @@ SPDK_VAGRANT_DISTRO="$*"
 
 case "${SPDK_VAGRANT_DISTRO}" in
 	centos[78]) ;&
-	ubuntu1[68]04 | ubuntu2004) ;&
+	ubuntu1[68]04 | ubuntu2[02]04) ;&
 	fedora3[3-6]) ;&
 	freebsd1[12]) ;&
 	rocky8) ;&
