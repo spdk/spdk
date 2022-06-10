978a8363bd24bef879ca8b9c05f0a81c7e891c65
Artur Paszkiewicz <artur.paszkiewicz@intel.com>
2022-06-10 10:28:16 +0200

    ftl_bitmap
    Signed-off-by: Artur Paszkiewicz <artur.paszkiewicz@intel.com>
    Signed-off-by: Kozlowski Mateusz <mateusz.kozlowski@intel.com>
    Change-Id: I36b6b8331cd6483c5bd202e5f9103c351d705da8

---
+++ lib/ftl/utils/ftl_bitmap.h
@@ -0,0 +1,139 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef FTL_BITMAP_H_
+#define FTL_BITMAP_H_
+
+#include "spdk/stdinc.h"
+
+struct ftl_bitmap;
+
+/**
+ * @brief The required alignment for buffer used for bitmap
+ */
+extern const size_t ftl_bitmap_buffer_alignment;
+
+/**
+ * @brief Converts number of bits to bitmap size need to create it
+ *
+ * @param bits Number of bits
+ *
+ * @return Size needed to create bitmap which will hold space for specified number of bits
+ */
+uint64_t ftl_bitmap_bits_to_size(uint64_t bits);
+
+/**
+ * @brief Converts number of bits to blocks
+ *
+ * @param bits Number of bits
+ *
+ * @return Number of blocks needed to create bitmap which will hold space for specified number of bits
+ */
+uint64_t ftl_bitmap_bits_to_blocks(uint64_t bits);
+
+/**
+ * @brief Creates a bitmap object using a preallocated buffer
+ *
+ * @param buf The buffer
+ * @param size Size of the buffer
+ *
+ * @return On success - pointer to the allocated bitmap object, otherwise NULL
+ */
+struct ftl_bitmap *ftl_bitmap_create(void *buf, size_t size);
+
+/**
+ * @brief Destroys the bitmap object
+ *
+ * @param bitmap The bitmap
+ */
+void ftl_bitmap_destroy(struct ftl_bitmap *bitmap);
+
+/**
+ * @brief Gets the value of the specified bit
+ *
+ * @param bitmap The bitmap
+ * @param bit Index of the bit
+ *
+ * @return True if bit is set, otherwise false
+ */
+bool ftl_bitmap_get(const struct ftl_bitmap *bitmap, uint64_t bit);
+
+/**
+ * @brief Sets the specified bit
+ *
+ * @param bitmap The bitmap
+ * @param bit Index of the bit
+ */
+void ftl_bitmap_set(struct ftl_bitmap *bitmap, uint64_t bit);
+
+/**
+ * @brief Clears the specified bit
+ *
+ * @param bitmap The bitmap
+ * @param bit Index of the bit
+ */
+void ftl_bitmap_clear(struct ftl_bitmap *bitmap, uint64_t bit);
+
+/**
+ * @brief Finds the first set bit
+ *
+ * @param bitmap The bitmap
+ * @param start_bit Index of the bit from which to begin searching
+ * @param end_bit Index of the bit up to which to search
+ *
+ * @return Index of the first set bit or UINT64_MAX if none found
+ */
+uint64_t ftl_bitmap_find_first_set(struct ftl_bitmap *bitmap, uint64_t start_bit, uint64_t end_bit);
+
+/**
+ * @brief Finds the first clear bit
+ *
+ * @param bitmap The bitmap
+ * @param start_bit Index of the bit from which to begin searching
+ * @param end_bit Index of the bit up to which to search
+ *
+ * @return Index of the first clear bit or UINT64_MAX if none found
+ */
+uint64_t ftl_bitmap_find_first_clear(struct ftl_bitmap *bitmap, uint64_t start_bit,
+				     uint64_t end_bit);
+
+/**
+ * @brief Iterates over and counts set bits
+ *
+ * @param bitmap The bitmap
+ *
+ * @return Count of sets bits
+ */
+uint64_t ftl_bitmap_count_set(struct ftl_bitmap *bitmap);
+
+#endif /* FTL_BITMAP_H_ */
