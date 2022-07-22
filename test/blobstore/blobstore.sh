a70a338f5ad035db9d3504040dee90407797be65
Michal Berger <michal.berger@intel.com>
2022-07-23 00:40:46 +0200

    test/blobstore: Generate json config with single nvme bdev
    The match run against btest.out.match expects to find only single
    nvme in the configuration, however, some CI nodes may come with
    multiple nvme drives causing this particular test to fail. To
    mitigate this, make sure to create config with only a single nvme
    bdev.
    
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Change-Id: Ifa390269b0e0a040b1aea752735c2692cbabac6b
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/13762
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Dong Yi <dongx.yi@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/blobstore/blobstore.sh
+++ test/blobstore/blobstore.sh
@@ -11,7 +11,7 @@ rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
 
 # Nvme0 target configuration
-$rootdir/scripts/gen_nvme.sh --json-with-subsystems > $testdir/blobcli.json
+"$rootdir/scripts/gen_nvme.sh" --json-with-subsystems -n 1 > "$testdir/blobcli.json"
 
 # generate random data file for import/export diff
 dd if=/dev/urandom of=$testdir/test.pattern bs=1M count=1
