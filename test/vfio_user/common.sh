66f47a03fb0e8d1af231e4185466f23bbfa13dea
Kamil Godzwon <kamilx.godzwon@intel.com>
2022-08-04 05:35:41 -0400

    pkgdep/vfio-user: change vfio-user source branch
    Changed vfio-user source branch for installing and testing.
    
    Replaced vfio-user-dbfix branch with vfio-user-irqmask2 due
    to the recent issue #2600.
    
    Signed-off-by: Kamil Godzwon <kamilx.godzwon@intel.com>
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Change-Id: Ide525cdd3496c2a3dbecea4e44e6edff63438859
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/13867
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/vfio_user/common.sh
+++ test/vfio_user/common.sh
@@ -4,7 +4,7 @@
 source "$rootdir/test/vhost/common.sh"
 
 # Verify vfio-user support of qemu.
-VFIO_QEMU_BIN=${VFIO_QEMU_BIN:-/usr/local/qemu/vfio-user-dbfix/bin/qemu-system-x86_64}
+VFIO_QEMU_BIN=${VFIO_QEMU_BIN:-/usr/local/qemu/vfio-user-irqmask2/bin/qemu-system-x86_64}
 
 if [[ ! -e $VFIO_QEMU_BIN ]]; then
 	error "$VFIO_QEMU_BIN QEMU not found, cannot run the vfio-user tests"
