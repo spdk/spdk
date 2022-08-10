97661e86b7172b5dd71750f819200da69c8d5eb3
Jim Harris <james.r.harris@intel.com>
2022-08-10 20:38:16 +0000

    nvme/pcie: add cpl status to PCIE_COMPLETE trace event
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I51e87f0f23b84956f96ab2efc62ad99a8d74cd4e
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/13989
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Reviewed-by: Dong Yi <dongx.yi@intel.com>
    Reviewed-by: Jacek Kalwas <jacek.kalwas@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- lib/nvme/nvme_pcie_common.c
+++ lib/nvme/nvme_pcie_common.c
@@ -677,7 +677,7 @@ nvme_pcie_qpair_complete_tracker(struct spdk_nvme_qpair *qpair, struct nvme_trac
 	req = tr->req;
 
 	spdk_trace_record(TRACE_NVME_PCIE_COMPLETE, qpair->id, 0, (uintptr_t)req,
-			  (uint32_t)req->cmd.cid);
+			  (uint32_t)req->cmd.cid, (uint32_t)cpl->status_raw);
 
 	assert(req != NULL);
 
@@ -1828,7 +1828,9 @@ SPDK_TRACE_REGISTER_FN(nvme_pcie, "nvme_pcie", TRACE_GROUP_NVME_PCIE)
 		{
 			"NVME_PCIE_COMPLETE", TRACE_NVME_PCIE_COMPLETE,
 			OWNER_NVME_PCIE_QP, OBJECT_NVME_PCIE_TR, 0,
-			{{ "cid", SPDK_TRACE_ARG_TYPE_INT, 4 }}
+			{	{ "cid", SPDK_TRACE_ARG_TYPE_INT, 4 },
+				{ "cpl", SPDK_TRACE_ARG_TYPE_PTR, 4 }
+			}
 		},
 	};
 
