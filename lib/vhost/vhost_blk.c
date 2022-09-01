75cc6fd62ff9f859a1b2964ee4db1840b133219e
Jim Harris <james.r.harris@intel.com>
2022-09-01 14:09:52 +0000

    vhost: move the session_start_done calls to common layer
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I355790f87ef148af85d5c13002260f1120749ae5
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/14340
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-by: Dong Yi <yidong0635@126.com>
    Reviewed-by: GangCao <gang.cao@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>

--- lib/vhost/vhost_blk.c
+++ lib/vhost/vhost_blk.c
@@ -1309,15 +1309,14 @@ vhost_blk_start(struct spdk_vhost_dev *vdev,
 		 */
 		if (vsession->virtqueue[i].vring.desc == NULL) {
 			SPDK_ERRLOG("%s: queue %"PRIu32" is empty\n", vsession->name, i);
-			rc = -1;
-			goto out;
+			return -1;
 		}
 	}
 
 	rc = alloc_task_pool(bvsession);
 	if (rc != 0) {
 		SPDK_ERRLOG("%s: failed to alloc task pool.\n", vsession->name);
-		goto out;
+		return rc;
 	}
 
 	if (bvdev->bdev) {
@@ -1325,8 +1324,7 @@ vhost_blk_start(struct spdk_vhost_dev *vdev,
 		if (!bvsession->io_channel) {
 			free_task_pool(bvsession);
 			SPDK_ERRLOG("%s: I/O channel allocation failed\n", vsession->name);
-			rc = -1;
-			goto out;
+			return -1;
 		}
 	}
 
@@ -1343,7 +1341,7 @@ vhost_blk_start(struct spdk_vhost_dev *vdev,
 
 		if (rc) {
 			SPDK_ERRLOG("%s: Interrupt register failed\n", vsession->name);
-			goto out;
+			return rc;
 		}
 	}
 
@@ -1358,8 +1356,6 @@ vhost_blk_start(struct spdk_vhost_dev *vdev,
 	spdk_poller_register_interrupt(bvsession->requestq_poller, vhost_blk_poller_set_interrupt_mode,
 				       bvsession);
 
-out:
-	vhost_user_session_start_done(vsession, rc);
 	return rc;
 }
 
