spdk/spdk

commit fb87f80c20c061f0db5f65bfca5abe2bbdd02831

scripts/check_format.sh: only check tracked files
Change-Id: Ie6370bbcbcf08cb365ac54f53e8794ecdcbdedd9
Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

scripts/check_format.sh
@@ -16,9 +16,8 @@ if hash astyle; then
 	#  as-is to enable ongoing work to synch with a generic upstream DPDK vhost library,
 	#  rather than making diffs more complicated by a lot of changes to follow SPDK
 	#  coding standards.
-	astyle --options=.astylerc "*.c" --exclude="rte_vhost" >> astyle.log
-	astyle --options=.astylerc --exclude=test/cpp_headers "*.cpp" >> astyle.log
-	astyle --options=.astylerc "*.h" --exclude="rte_vhost" >> astyle.log
+	git ls-files '*.[ch]' '*.cpp' | grep -v rte_vhost | grep -v cpp_headers | \
+		xargs astyle --options=.astylerc >> astyle.log
 	if grep -q "^Formatted" astyle.log; then
 		echo " errors detected"
 		git diff

.astylerc
@@ -22,5 +22,4 @@ lineend=linux # LF line endings
 
 # General options
 suffix=none
-recursive
 formatted

commit 2f1cf63cfbbee843bcc13078bd86f749018a7332

check_format.sh: remove cpp_headers exclusion
We don't track any files in test/cpp_headers, so there
is no need to do a grep -v to exclude files that
contain "cpp_headers".  This was a remnant from before
we started using git ls-files to determine which files
to run through astyle.

Fixes: fb87f80c ("scripts/check_format.sh: only check tracked files")

Signed-off-by: Jim Harris <james.r.harris@intel.com>
Change-Id: If08f0bd5c96990dc5e4ca4641c53f70a82590470
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/14689
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Krzysztof Karas <krzysztof.karas@intel.com>
Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>

scripts/check_format.sh
@@ -127,11 +127,9 @@ function check_c_style() {
 			rm -f astyle.log
 			touch astyle.log
 			git ls-files '*.[ch]' \
-				| grep -v cpp_headers \
 				| xargs -P$(nproc) -n10 astyle --break-return-type --attach-return-type-decl \
 					--options=.astylerc >> astyle.log
 			git ls-files '*.cpp' '*.cc' '*.cxx' '*.hh' '*.hpp' \
-				| grep -v cpp_headers \
 				| xargs -P$(nproc) -n10 astyle --options=.astylerc >> astyle.log
 			if grep -q "^Formatted" astyle.log; then
 				echo " errors detected"