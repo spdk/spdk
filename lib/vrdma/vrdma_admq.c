d0faee42a249963b471777847fcb6be8c358fb18
Lei Wang <leiwan@nvidia.com>
2022-10-10 08:21:25 +0800

    Add admin queue handling
    1, poll PI and cmd entry
    2, handle cmd and write back
    3, update CI
    
    Signed-off-by: Lei Wang <leiwan@nvidia.com>

--- lib/vrdma/vrdma_admq.c
+++ lib/vrdma/vrdma_admq.c
@@ -30,10 +30,15 @@
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "spdk/stdinc.h"
+#include "snap_vrdma_ctrl.h"
 
+#include "spdk/stdinc.h"
 #include "spdk/env.h"
 #include "spdk/barrier.h"
+#include "spdk/conf.h"
+#include "spdk/log.h"
+#include "spdk/likely.h"
+
 #include "spdk/vrdma_admq.h"
 #include "spdk/vrdma_controller.h"
 #include "snap_vrdma_ctrl.h"
@@ -54,16 +59,15 @@ static inline int aqe_sanity_check(struct vrdma_admin_cmd_entry *aqe)
 }
 
 static int vrdma_aq_open_dev(struct vrdma_ctrl *ctrl,
-								struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	/* No action and just return OK*/
 	aqe->resp.open_device_resp.err_code = aq_msg_err_code_success;
 	return 0;
 }
 
-
 static int vrdma_aq_query_dev(struct vrdma_ctrl *ctrl,
-								struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 
 	struct snap_device *sdev = ctrl->sctrl->sdev;
@@ -86,126 +90,126 @@ static int vrdma_aq_query_dev(struct vrdma_ctrl *ctrl,
 }
 
 static int vrdma_aq_query_port(struct vrdma_ctrl *ctrl,
-								struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_query_gid(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_modify_gid(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_create_pd(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_destroy_pd(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_reg_mr(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+	        	struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_dereg_mr(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+			struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_create_cq(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_destroy_cq(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_create_qp(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_destroy_qp(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_query_qp(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_modify_qp(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_create_ceq(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_modify_ceq(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_destroy_ceq(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_create_ah(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
 }
 
 static int vrdma_aq_destroy_ah(struct vrdma_ctrl *ctrl,
-							struct vrdma_admin_cmd_entry *aqe)
+				struct vrdma_admin_cmd_entry *aqe)
 {
 	//TODO
 	return 0;
@@ -286,4 +290,345 @@ int vrdma_parse_admq_entry(struct vrdma_ctrl *ctrl,
 	}
 
 	return ret;
+}
+
+//need invoker to guarantee pi is bigger than pre_pi
+static inline int vrdma_aq_rollback(struct vrdma_admin_sw_qp *aq, uint16_t pi,
+                                           uint16_t q_size)
+{
+	return (pi % q_size < aq->admq->ci % q_size);
+}
+
+static bool vrdma_aq_sm_idle(struct vrdma_admin_sw_qp *aq,
+                                    enum vrdma_aq_cmd_sm_op_status status)
+{
+	SPDK_ERRLOG("vrdma admq in invalid state %d\n",
+					   VRDMA_CMD_STATE_IDLE);
+	return false;
+}
+
+static bool vrdma_aq_sm_read_pi(struct vrdma_admin_sw_qp *aq,
+                                   enum vrdma_aq_cmd_sm_op_status status)
+{
+	int ret;
+	struct vrdma_ctrl *ctrl = container_of(aq, struct vrdma_ctrl, sw_qp);
+	uint64_t pi_addr = ctrl->sctrl->adminq_driver_addr + offsetof(struct vrdma_admin_queue, pi);
+
+	if (status != VRDMA_CMD_SM_OP_OK) {
+		SPDK_ERRLOG("failed to update admq CI, status %d\n", status);
+		aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+		return true;
+	}
+
+	SPDK_NOTICELOG("vrdam poll admin pi: admq pa 0x%llx\n", ctrl->sctrl->adminq_driver_addr);
+
+	aq->state = VRDMA_CMD_STATE_HANDLE_PI;
+	aq->poll_comp.count = 1;
+
+	ret = snap_dma_q_read(ctrl->sctrl->adminq_dma_q, &aq->admq->pi, sizeof(uint16_t),
+				          ctrl->sctrl->adminq_mr->lkey, pi_addr,
+				          ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+	if (spdk_unlikely(ret)) {
+		SPDK_ERRLOG("failed to read admin PI, ret %d\n", ret);
+		aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+	}
+
+	return true;
+}
+
+static bool vrdma_aq_sm_handle_pi(struct vrdma_admin_sw_qp *aq,
+                                    enum vrdma_aq_cmd_sm_op_status status)
+{
+
+	if (status != VRDMA_CMD_SM_OP_OK) {
+		SPDK_ERRLOG("failed to get admq PI, status %d\n", status);
+		aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+		return true;
+	}
+
+	if (aq->admq->pi > aq->admq->ci) {
+		aq->state = VRDMA_CMD_STATE_READ_CMD_ENTRY;
+	} else {
+		aq->state = VRDMA_CMD_STATE_POLL_PI;
+	}
+
+	return false;
+}
+
+static bool vrdma_aq_sm_read_cmd(struct vrdma_admin_sw_qp *aq,
+                                    enum vrdma_aq_cmd_sm_op_status status)
+{
+	struct vrdma_ctrl *ctrl = container_of(aq, struct vrdma_ctrl, sw_qp);
+	uint16_t pi = aq->admq->pi;
+	uint32_t aq_poll_size = 0;
+	uint64_t host_ring_addr;
+	uint8_t *local_ring_addr;
+	uint32_t offset = 0;
+	uint16_t num = 0;
+	uint16_t q_size = ctrl->sctrl->adminq_q_size;
+	int ret;
+
+	host_ring_addr = ctrl->sctrl->adminq_driver_addr +
+		             offsetof(struct vrdma_admin_queue, ring);
+	SPDK_NOTICELOG("vrdam poll admin cmd: admq pa 0x%llx\n", ctrl->sctrl->adminq_driver_addr);
+
+	aq->state = VRDMA_CMD_STATE_PARSE_CMD_ENTRY;
+	aq->num_to_parse = pi - aq->pre_ci;
+
+	//fetch the delta PI number entry in one time
+	if (!vrdma_aq_rollback(aq, pi, q_size)) {
+		aq->poll_comp.count = 1;
+		num = pi - aq->pre_ci;
+		aq_poll_size = num * sizeof(struct vrdma_admin_cmd_entry);
+		offset = (aq->pre_pi % q_size) * sizeof(struct vrdma_admin_cmd_entry);
+	    	host_ring_addr = (uint8_t *)host_ring_addr + offset;
+		ret = snap_dma_q_read(ctrl->sctrl->adminq_dma_q, aq->admq->ring, aq_poll_size,
+				              ctrl->sctrl->adminq_mr->lkey, host_ring_addr,
+				              ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+		if (spdk_unlikely(ret)) {
+			SPDK_ERRLOG("no roll back failed to read admin CMD entry, ret %d\n", ret);
+		    aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+		    return true;
+		}
+	} else {
+		/* aq roll back case, first part */
+		aq->poll_comp.count = 1;
+		num = q_size - (aq->pre_ci % q_size);
+		aq_poll_size = num * sizeof(struct vrdma_admin_cmd_entry);
+		offset = (aq->pre_ci % q_size) * sizeof(struct vrdma_admin_cmd_entry);
+		host_ring_addr = (uint8_t *)host_ring_addr + offset;
+		ret = snap_dma_q_read(ctrl->sctrl->adminq_dma_q, aq->admq->ring, aq_poll_size,
+				              ctrl->sctrl->adminq_mr->lkey, host_ring_addr,
+				              ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+		if (spdk_unlikely(ret)) {
+			SPDK_ERRLOG("roll back failed to first read admin CMD entry, ret %d\n", ret);
+		    aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+			return true;
+		}
+
+		/* calculate second poll size */
+		local_ring_addr = (uint8_t *)aq->admq->ring + num * sizeof(struct vrdma_admin_cmd_entry);
+		aq->poll_comp.count++;
+		num = pi % q_size;
+		aq_poll_size = num * sizeof(struct vrdma_admin_cmd_entry);
+		host_ring_addr = ctrl->sctrl->adminq_driver_addr + offsetof(struct vrdma_admin_queue, ring);
+		ret = snap_dma_q_read(ctrl->sctrl->adminq_dma_q, local_ring_addr, aq_poll_size,
+				              ctrl->sctrl->adminq_mr->lkey, host_ring_addr,
+				              ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+		if (spdk_unlikely(ret)) {
+			SPDK_ERRLOG("roll back failed to second read admin CMD entry, ret %d\n", ret);
+		    aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+static bool vrdma_aq_sm_parse_cmd(struct vrdma_admin_sw_qp *aq,
+                                           enum vrdma_aq_cmd_sm_op_status status)
+{
+	uint16_t i;
+	int ret = 0;
+	struct vrdma_ctrl *ctrl = container_of(aq, struct vrdma_ctrl, sw_qp);
+
+	if (status != VRDMA_CMD_SM_OP_OK) {
+		SPDK_ERRLOG("failed to get admq cmd entry, status %d\n", status);
+		aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+		return true;
+	}
+
+    	aq->state = VRDMA_CMD_STATE_WRITE_CMD_BACK;
+	for (i = 0; i < aq->num_to_parse; i++) {
+		ret = vrdma_parse_admq_entry(ctrl, &(aq->admq->ring[i]));
+		if (ret) {
+			aq->num_to_parse = i;
+			break;
+		}
+	}
+	return true;
+}
+
+static bool vrdma_aq_sm_write_cmd(struct vrdma_admin_sw_qp *aq,
+                                           enum vrdma_aq_cmd_sm_op_status status)
+{
+	struct vrdma_ctrl *ctrl = container_of(aq, struct vrdma_ctrl, sw_qp);
+	uint16_t num_to_write = aq->num_to_parse;
+	uint16_t pi = aq->admq->pi;
+	uint16_t ci = aq->admq->ci;
+	uint32_t aq_poll_size = 0;
+	uint64_t host_ring_addr;
+	uint8_t *local_ring_addr;
+	uint32_t offset = 0;
+	uint16_t num = 0;
+	uint16_t q_size = ctrl->sctrl->adminq_q_size;
+	int ret;
+
+	host_ring_addr = ctrl->sctrl->adminq_driver_addr +
+		             offsetof(struct vrdma_admin_queue, ring);
+	SPDK_NOTICELOG("vrdam write admin cmd: admq pa 0x%llx\n", ctrl->sctrl->adminq_driver_addr);
+
+	aq->state = VRDMA_CMD_STATE_UPDATE_CI;
+
+	//write back entries in one time
+	if ((num_to_write + ci % q_size) < q_size ) {
+		aq->poll_comp.count = 1;
+		aq_poll_size = num_to_write * sizeof(struct vrdma_admin_cmd_entry);
+		offset = (ci % q_size) * sizeof(struct vrdma_admin_cmd_entry);
+	    host_ring_addr = (uint8_t *)host_ring_addr + offset;
+		ret = snap_dma_q_write(ctrl->sctrl->adminq_dma_q, aq->admq->ring, aq_poll_size,
+				              ctrl->sctrl->adminq_mr->lkey, host_ring_addr,
+				              ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+		if (spdk_unlikely(ret)) {
+			SPDK_ERRLOG("no roll back failed to write back admin CMD entry, ret %d\n", ret);
+		    aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+		    return true;
+		}
+	} else {
+		/* aq roll back case, first part */
+		aq->poll_comp.count = 1;
+		num = q_size - (ci % q_size);
+		aq_poll_size = num * sizeof(struct vrdma_admin_cmd_entry);
+		offset = (ci % q_size) * sizeof(struct vrdma_admin_cmd_entry);
+		host_ring_addr = (uint8_t *)host_ring_addr + offset;
+		ret = snap_dma_q_write(ctrl->sctrl->adminq_dma_q, aq->admq->ring, aq_poll_size,
+				              ctrl->sctrl->adminq_mr->lkey, host_ring_addr,
+				              ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+		if (spdk_unlikely(ret)) {
+			SPDK_ERRLOG("roll back failed to first write admin CMD entry, ret %d\n", ret);
+		    aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+			return true;
+		}
+
+		/* calculate second poll size */
+		local_ring_addr = (uint8_t *)aq->admq->ring + num * sizeof(struct vrdma_admin_cmd_entry);
+		aq->poll_comp.count++;
+		num = num_to_write - num;
+		aq_poll_size = num * sizeof(struct vrdma_admin_cmd_entry);
+		host_ring_addr = ctrl->sctrl->adminq_driver_addr + offsetof(struct vrdma_admin_queue, ring);
+		ret = snap_dma_q_write(ctrl->sctrl->adminq_dma_q, local_ring_addr, aq_poll_size,
+				              ctrl->sctrl->adminq_mr->lkey, host_ring_addr,
+				              ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+		if (spdk_unlikely(ret)) {
+			SPDK_ERRLOG("roll back failed to second write admin CMD entry, ret %d\n", ret);
+		    aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+			return true;
+		}
+	}
+
+	aq->admq->ci += num_to_write;
+	return false;
+}
+
+static bool vrdma_aq_sm_update_ci(struct vrdma_admin_sw_qp *aq,
+                                           enum vrdma_aq_cmd_sm_op_status status)
+{
+	int ret;
+	struct vrdma_ctrl *ctrl = container_of(aq, struct vrdma_ctrl, sw_qp);
+	uint64_t ci_addr  = ctrl->sctrl->adminq_driver_addr + offsetof(struct vrdma_admin_queue, ci);
+
+	if (status != VRDMA_CMD_SM_OP_OK) {
+		SPDK_ERRLOG("failed to write back admq, status %d\n", status);
+		aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+		return true;
+	}
+
+	SPDK_NOTICELOG("vrdam update admq CI: admq pa 0x%llx\n", ctrl->sctrl->adminq_driver_addr);
+
+	aq->state = VRDMA_CMD_STATE_POLL_PI;
+	aq->poll_comp.count = 1;
+	ret = snap_dma_q_write(ctrl->sctrl->adminq_dma_q, &aq->admq->ci, sizeof(uint16_t),
+				          ctrl->sctrl->adminq_mr->lkey, ci_addr,
+				          ctrl->sctrl->xmkey->mkey, &aq->poll_comp);
+	if (spdk_unlikely(ret)) {
+		SPDK_ERRLOG("failed to update admq CI, ret %d\n", ret);
+		aq->state = VRDMA_CMD_STATE_FATAL_ERR;
+	}
+
+	return false;
+}
+
+static bool vrdma_aq_sm_fatal_error(struct vrdma_admin_sw_qp *aq,
+                                       enum vrdma_aq_cmd_sm_op_status status)
+{
+	/*
+	 * TODO: maybe need to add more handling
+	 */
+
+	return false;
+}
+
+//sm array states must be according to the order of vrdma_aq_cmd_sm_state
+static struct vrdma_aq_sm_state vrdma_aq_sm_arr[] = {
+/*VRDMA_CMD_STATE_IDLE					*/	{vrdma_aq_sm_idle},
+/*VRDMA_CMD_STATE_INIT_CI		        */	{vrdma_aq_sm_idle},
+/*VRDMA_CMD_STATE_POLL_PI			    */	{vrdma_aq_sm_read_pi},
+/*VRDMA_CMD_STATE_HANDLE_PI			    */	{vrdma_aq_sm_handle_pi},
+/*VRDMA_CMD_STATE_READ_CMD_ENTRY	    */	{vrdma_aq_sm_read_cmd},
+/*VRDMA_CMD_STATE_PARSE_CMD_ENTRY	    */	{vrdma_aq_sm_parse_cmd},
+/*VRDMA_CMD_STATE_WRITE_CMD_BACK	    */	{vrdma_aq_sm_write_cmd},
+/*VRDMA_CMD_STATE_UPDATE_CI	            */	{vrdma_aq_sm_update_ci},
+/*VIRTQ_CMD_STATE_FATAL_ERR				*/	{vrdma_aq_sm_fatal_error},
+											};
+
+struct vrdma_state_machine vrdma_sm  = { vrdma_aq_sm_arr, sizeof(vrdma_aq_sm_arr) / sizeof(struct vrdma_aq_sm_state) };
+
+/**
+ * vrdma_aq_cmd_progress() - admq command state machine progress handle
+ * @aq:	admq to be processed
+ * @status:	status of calling function (can be a callback)
+ *
+ * Return: 0 (Currently no option to fail)
+ */
+int vrdma_aq_cmd_progress(struct vrdma_admin_sw_qp *aq,
+		          enum vrdma_aq_cmd_sm_op_status status)
+{
+	struct vrdma_state_machine *sm;
+	bool repeat = true;
+
+	while (repeat) {
+		repeat = false;
+		SPDK_NOTICELOG("vrdma admq cmd sm state: %d\n", aq->state);
+		sm = aq->custom_sm;
+		if (spdk_likely(aq->state < VRDMA_CMD_NUM_OF_STATES))
+			repeat = sm->sm_array[aq->state].sm_handler(aq, status);
+		else
+			SPDK_ERRLOG("reached invalid state %d\n", aq->state);
+	}
+
+	return 0;
+}
+
+static void vrdma_aq_sm_dma_cb(struct snap_dma_completion *self, int status)
+{
+	enum vrdma_aq_cmd_sm_op_status op_status = VRDMA_CMD_SM_OP_OK;
+	struct vrdma_admin_sw_qp *aq = container_of(self, struct vrdma_admin_sw_qp,
+						                        poll_comp);
+
+	if (status != IBV_WC_SUCCESS) {
+		SPDK_ERRLOG("error in dma for vrdma admq state %d\n", aq->state);
+		op_status = VRDMA_CMD_SM_OP_ERR;
+	}
+	vrdma_aq_cmd_progress(aq, op_status);
+}
+
+int vrdma_ctrl_adminq_progress(void *ctrl)
+{
+	struct vrdma_ctrl *vdev_ctrl = ctrl;
+	struct vrdma_admin_sw_qp *aq = &vdev_ctrl->sw_qp;
+	enum vrdma_aq_cmd_sm_op_status status = VRDMA_CMD_SM_OP_OK;
+	int n = 0;
+
+	if (aq->pre_ci == VRDMA_INVALID_CI_PI ||
+		aq->state < VRDMA_CMD_STATE_INIT_CI) {
+		return 0;
+	}
+
+	if (aq->state == VRDMA_CMD_STATE_INIT_CI) {
+		vrdma_aq_sm_read_pi(aq, status);
+	}
+
+	n += snap_dma_q_progress(vdev_ctrl->sctrl->adminq_dma_q);
+
+	return n;
 }
