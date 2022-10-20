4a78a37fd731f05a17aa34765bd79beae0cfc8c3
Changpeng Liu <changpeng.liu@intel.com>
2022-10-20 14:25:12 +0800

    test/vfu_virtio: add SPDK virtio initiator bdevperf tests
    Change-Id: Ifc86834c1fae7087b4e25e00d9576decffa7248a
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/15075
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/vfio_user/vfio_user.sh
+++ test/vfio_user/vfio_user.sh
@@ -16,5 +16,6 @@ run_test "vfio_user_nvme_fio" $WORKDIR/nvme/vfio_user_fio.sh
 run_test "vfio_user_nvme_restart_vm" $WORKDIR/nvme/vfio_user_restart_vm.sh
 run_test "vfio_user_virtio_blk_restart_vm" $WORKDIR/virtio/fio_restart_vm.sh virtio_blk
 run_test "vfio_user_virtio_scsi_restart_vm" $WORKDIR/virtio/fio_restart_vm.sh virtio_scsi
+run_test "vfio_user_virtio_bdevperf" $WORKDIR/virtio/initiator_bdevperf.sh
 
 vhosttestfini
