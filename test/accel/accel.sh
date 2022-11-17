75b8a8e0fd7b67846077b445a64a90242b76bc79
paul luse <paul.e.luse@intel.com>
2022-11-17 14:55:33 -0700

    test/accel:  fix run_test naming convention
    Test names were not unique per test
    
    Signed-off-by: paul luse <paul.e.luse@intel.com>
    Change-Id: I638f2e90f6bdcd092fe9f0a37241014771550554
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/15514
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/accel/accel.sh
+++ test/accel/accel.sh
@@ -6,11 +6,11 @@ source $rootdir/test/common/autotest_common.sh
 
 #Run through all SW ops with defaults for a quick sanity check
 #To save time, only use verification case
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w crc32c -y
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w crc32c -y -C 2
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy -y
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w fill -y
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy_crc32c -y
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy_crc32c -y -C 2
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w dualcast -y
-run_test "accel" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w compare -y
+run_test "accel_crc32c" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w crc32c -y
+run_test "accel_crc32c_C2" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w crc32c -y -C 2
+run_test "accel_copy" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy -y
+run_test "accel_fill" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w fill -y
+run_test "accel_copy_crc32c" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy_crc32c -y
+run_test "accel_copy_crc32c_C2" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w copy_crc32c -y -C 2
+run_test "accel_dualcast" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w dualcast -y
+run_test "accel_compare" $SPDK_EXAMPLE_DIR/accel_perf -t 1 -w compare -y
