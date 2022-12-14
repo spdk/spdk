48fb1771efbc0b19ffee08703c56788aed763a7e
Changpeng Liu <changpeng.liu@intel.com>
2022-12-14 10:09:11 +0800

    test/vfio_user: remove unnecessary VFIO_QEMU_BIN default value
    The QEMU binary is configured in the CI configuration file, so
    we don't need to set it in the script now.
    
    Change-Id: I6d0f4092f7a15bda5718f60c9a3b42f7127a9807
    Signed-off-by: Changpeng Liu <changpeng.liu@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/15913
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/vfio_user/common.sh
+++ test/vfio_user/common.sh
@@ -9,8 +9,6 @@
 source "$rootdir/test/vhost/common.sh"
 
 # Verify vfio-user support of qemu.
-VFIO_QEMU_BIN=${VFIO_QEMU_BIN:-/usr/local/qemu/vfio-user-irqmask2/bin/qemu-system-x86_64}
-
 if [[ ! -e $VFIO_QEMU_BIN ]]; then
 	error "$VFIO_QEMU_BIN QEMU not found, cannot run the vfio-user tests"
 	return 1
