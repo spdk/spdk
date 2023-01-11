bf1deaffd5485f16a2e11d540bd20d0e51075a15
Karol Latecki <karol.latecki@intel.com>
2023-01-11 07:47:13 +0100

    autotest: remove app/spdk_lspci from coverage
    Remove app/spdk_lspci as it is used only
    for enumerating devices behind VMD endpoints.
    VMD is not run in usual set of CI tests due
    to lack of adequate hardware, thus spdk_lspci
    is never used.
    
    Signed-off-by: Karol Latecki <karol.latecki@intel.com>
    Change-Id: I08c94ff5d17faf6aebf18caed1f985e5304ba9a5
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/16236
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- autotest.sh
+++ autotest.sh
@@ -376,6 +376,7 @@ if hash lcov && ! [[ "$CC_TYPE" == *"clang"* ]]; then
 	$LCOV -q -r $out/cov_total.info '*/dpdk/*' -o $out/cov_total.info
 	$LCOV -q -r $out/cov_total.info '/usr/*' -o $out/cov_total.info
 	$LCOV -q -r $out/cov_total.info '*/examples/vmd/*' -o $out/cov_total.info
+	$LCOV -q -r $out/cov_total.info '*/app/spdk_lspci/*' -o $out/cov_total.info
 	owner=$(stat -c "%U" .)
 	sudo -u $owner git clean -f "*.gcda"
 	rm -f cov_base.info cov_test.info OLD_STDOUT OLD_STDERR
