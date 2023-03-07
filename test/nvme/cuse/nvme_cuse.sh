a75bdf361c44bfc8a3c5e22e42fd16a8f33bca10
Michal Berger <michal.berger@intel.com>
2023-03-07 11:23:38 +0100

    test/nvme/cuse: Don't check OACS in the parent test
    This job is already done by the target test. Also, this makes sure
    that nvme_ns_manage_cuse is always executed (note that we don't
    consider it as a test to be skipped).
    
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Change-Id: I6d964230e4d961152912b1f8a0636712039eeae6
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/17080
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>

--- test/nvme/cuse/nvme_cuse.sh
+++ test/nvme/cuse/nvme_cuse.sh
@@ -18,32 +18,7 @@ run_test "nvme_cuse_app" $testdir/cuse
 run_test "nvme_cuse_rpc" $testdir/nvme_cuse_rpc.sh
 run_test "nvme_cli_cuse" $testdir/spdk_nvme_cli_cuse.sh
 run_test "nvme_smartctl_cuse" $testdir/spdk_smartctl_cuse.sh
-
-# Only run Namespace management test case when such device is present
-bdfs=$(get_nvme_bdfs)
-
-$rootdir/scripts/setup.sh reset
-sleep 1
-
-# Find bdf that supports Namespace management
-for bdf in $bdfs; do
-	nvme_name=$(get_nvme_ctrlr_from_bdf ${bdf})
-	if [[ -z "$nvme_name" ]]; then
-		continue
-	fi
-
-	# Check Optional Admin Command Support for Namespace Management
-	oacs=$(nvme id-ctrl /dev/${nvme_name} | grep oacs | cut -d: -f2)
-	oacs_ns_manage=$((oacs & 0x8))
-
-	if [[ "$oacs_ns_manage" -ne 0 ]]; then
-		break
-	fi
-done
-
-if [[ "$oacs_ns_manage" -ne 0 ]]; then
-	run_test "nvme_ns_manage_cuse" $testdir/nvme_ns_manage_cuse.sh
-fi
-$rootdir/scripts/setup.sh
-
+run_test "nvme_ns_manage_cuse" $testdir/nvme_ns_manage_cuse.sh
 rmmod cuse
+
+"$rootdir/scripts/setup.sh"
