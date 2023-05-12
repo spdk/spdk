f35e7f2457d146a82405a461bb36fe036c0c6b07
Konrad Sztyber <konrad.sztyber@intel.com>
2023-05-12 16:10:02 +0200

    test/external_code: check csts.cfs during initialization
    The regular NVMe driver also checks this bit, so we should also do that
    in the external_code tests.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I74443d743a9ac0fa5b38cf7e6ee83ecd223584d6
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/18086
    Reviewed-by: Michal Berger <michal.berger@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- test/external_code/nvme/nvme.c
+++ test/external_code/nvme/nvme.c
@@ -484,7 +484,7 @@ process_ctrlr_init(struct nvme_ctrlr *ctrlr)
 		ctrlr->state = NVME_CTRLR_STATE_DISABLE_WAIT_FOR_READY_0;
 		break;
 	case NVME_CTRLR_STATE_DISABLE_WAIT_FOR_READY_1:
-		if (csts.bits.rdy) {
+		if (csts.bits.rdy || csts.bits.cfs) {
 			cc.bits.en = 0;
 			nvme_ctrlr_set_cc(ctrlr, &cc);
 			ctrlr->state = NVME_CTRLR_STATE_DISABLE_WAIT_FOR_READY_0;
