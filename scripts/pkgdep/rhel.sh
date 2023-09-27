4506cc6003b878faf9c5314b1e26ac8f8d13f71e
Michal Berger <michal.berger@intel.com>
2023-09-27 10:39:35 +0200

    scripts/pkgdep: Add unzip to dependencies
    It's required by some of the common installers (e.g. go-related) and
    some distros, like centos7, rocky8, don't provide it by default. Add
    to all distros we actively support under CI.
    
    Change-Id: Icbc384decbcdd31a562235d60c9628641d9877af
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/20088
    Reviewed-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Pawel Piatek <pawelx.piatek@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- scripts/pkgdep/rhel.sh
+++ scripts/pkgdep/rhel.sh
@@ -100,7 +100,7 @@ fi
 
 yum install -y gcc gcc-c++ make CUnit-devel libaio-devel openssl-devel \
 	libuuid-devel libiscsi-devel ncurses-devel json-c-devel libcmocka-devel \
-	clang clang-devel python3-pip
+	clang clang-devel python3-pip unzip
 
 # Minimal install
 # workaround for arm: ninja fails with dep on skbuild python module
