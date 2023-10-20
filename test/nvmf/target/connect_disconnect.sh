5c6d6aa7f463bd5cd0547247cc40acd1cdbc231f
Michal Berger <michal.berger@intel.com>
2023-10-20 13:53:33 +0200

    test/nvmf: Always define host{id,nqn} while connecting over nvme-fabrics
    This relates to recent changes in nvme-fabrics:
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ae8bd606e0
    
    (cherry-picked from 75bf61ffb7500316913a9c367726c636d31f4e31)
    Change-Id: Ib9ae3c1c6d88f1765cd3ce0d8ef1d99d049d7cd9
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/20382 (master)
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/20953
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/nvmf/target/connect_disconnect.sh
+++ test/nvmf/target/connect_disconnect.sh
@@ -33,7 +33,7 @@ fi
 
 set +x
 for i in $(seq 1 $num_iterations); do
-	$NVME_CONNECT -t $TEST_TRANSPORT -n "nqn.2016-06.io.spdk:cnode1" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
+	$NVME_CONNECT "${NVME_HOST[@]}" -t $TEST_TRANSPORT -n "nqn.2016-06.io.spdk:cnode1" -a "$NVMF_FIRST_TARGET_IP" -s "$NVMF_PORT"
 	waitforserial "$NVMF_SERIAL"
 	nvme disconnect -n "nqn.2016-06.io.spdk:cnode1"
 	waitforserial_disconnect "$NVMF_SERIAL"
