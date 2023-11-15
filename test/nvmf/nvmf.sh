c2ab5b0821e2b44c567bc829890d47e5243e8dca
Michal Berger <michal.berger@intel.com>
2023-11-15 12:07:49 +0100

    test/nvmf: Move the nvmf_device_removal test to nightly
    This is temporary mitigation targeting the following issue:
    https://github.com/spdk/spdk/issues/3116
    
    Change-Id: I0c5634c4aa1c28774cdb5ef200f6a7eeebf9293a
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/20583
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <jim.harris@samsung.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- test/nvmf/nvmf.sh
+++ test/nvmf/nvmf.sh
@@ -67,7 +67,8 @@ if [[ $NET_TYPE == phy ]]; then
 		if ((${#TCP_INTERFACE_LIST[@]} > 0)); then
 			run_test "nvmf_perf_adq" $rootdir/test/nvmf/target/perf_adq.sh "${TEST_ARGS[@]}"
 		fi
-	else
+	elif [[ $RUN_NIGTLY -eq 1 ]]; then
+		# FIXME: For now, lock this test suite under nightly due to https://github.com/spdk/spdk/issues/3116
 		run_test "nvmf_device_removal" test/nvmf/target/device_removal.sh "${TEST_ARGS[@]}"
 	fi
 	run_test "nvmf_shutdown" $rootdir/test/nvmf/target/shutdown.sh "${TEST_ARGS[@]}"
