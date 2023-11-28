c47b9b42589d27dd5fb2598d89fe1bf2521b0f07
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2023-11-28 16:46:06 +0100

    lib/env_dpdk: updated version for DPDK 24.03 main tests
    DPDK main development now targets DPDK 24.03 release.
    Updated to that version for validation purposes.
    
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Change-Id: I6f8f49306eb68b37eaaa84b3ce942aee992230a9
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/20755
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Ben Walker <ben@nvidia.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Michal Berger <michal.berger@intel.com>
    Reviewed-by: Kamil Godzwon <kamilx.godzwon@intel.com>
    Reviewed-by: Pawel Piatek <pawelx.piatek@intel.com>
    Community-CI: Mellanox Build Bot

--- lib/env_dpdk/pci_dpdk.c
+++ lib/env_dpdk/pci_dpdk.c
@@ -32,7 +32,7 @@ dpdk_pci_init(void)
 	 * Only DPDK in development has additional suffix past minor version.
 	 */
 	if (strlen(release) != 0) {
-		if (year == 23 && month == 11 && minor == 0) {
+		if (year == 24 && month == 3 && minor == 0) {
 			g_dpdk_fn_table = &fn_table_2211;
 			SPDK_NOTICELOG("In-development %s is used. There is no support for it in SPDK. "
 				       "Enabled only for validation.\n", rte_version());
@@ -59,13 +59,14 @@ dpdk_pci_init(void)
 		}
 		g_dpdk_fn_table = &fn_table_2211;
 	} else if (year == 23) {
-		/* Only 23.07.0 and 23.03.0 is supported */
-		if ((month != 7 || minor != 0) &&
+		/* Only 23.11.0, 23.07.0 and 23.03.0 are supported. */
+		if ((month != 11 || minor != 0) &&
+		    (month != 7 || minor != 0) &&
 		    (month != 3 || minor != 0)) {
 			SPDK_ERRLOG("DPDK version 23.%02d.%d is not supported.\n", month, minor);
 			return -EINVAL;
 		}
-		/* There were no changes between 22.11 and 23.07, so use the 22.11 implementation */
+		/* There were no changes between 22.11 and 23.11, so use the 22.11 implementation. */
 		g_dpdk_fn_table = &fn_table_2211;
 	} else if (year < 21 || (year == 21 && month < 11)) {
 		SPDK_ERRLOG("DPDK version %02d.%02d.%d is not supported.\n", year, month, minor);
