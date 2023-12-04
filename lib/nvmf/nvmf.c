796e672e63d4642c4df98d69f6dccbba607b568a
Konrad Sztyber <konrad.sztyber@intel.com>
2023-12-04 15:28:42 +0100

    nvmf: save referral configuration
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I8dbf7a929a875f6ed0bca6d3e181928e7a41c29b
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/20903
    Community-CI: Mellanox Build Bot
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <jim.harris@samsung.com>

--- lib/nvmf/nvmf.c
+++ lib/nvmf/nvmf.c
@@ -706,6 +706,7 @@ spdk_nvmf_tgt_write_config_json(struct spdk_json_write_ctx *w, struct spdk_nvmf_
 {
 	struct spdk_nvmf_subsystem *subsystem;
 	struct spdk_nvmf_transport *transport;
+	struct spdk_nvmf_referral *referral;
 
 	spdk_json_write_object_begin(w);
 	spdk_json_write_named_string(w, "method", "nvmf_set_max_subsystems");
@@ -730,6 +731,23 @@ spdk_nvmf_tgt_write_config_json(struct spdk_json_write_ctx *w, struct spdk_nvmf_
 		spdk_json_write_object_begin(w);
 		spdk_json_write_named_string(w, "method", "nvmf_create_transport");
 		nvmf_transport_dump_opts(transport, w, true);
+		spdk_json_write_object_end(w);
+	}
+
+	TAILQ_FOREACH(referral, &tgt->referrals, link) {
+		spdk_json_write_object_begin(w);
+		spdk_json_write_named_string(w, "method", "nvmf_discovery_add_referral");
+
+		spdk_json_write_named_object_begin(w, "params");
+		spdk_json_write_named_object_begin(w, "address");
+		nvmf_transport_listen_dump_trid(&referral->trid, w);
+		spdk_json_write_object_end(w);
+		spdk_json_write_named_bool(w, "secure_channel",
+					   referral->entry.treq.secure_channel ==
+					   SPDK_NVMF_TREQ_SECURE_CHANNEL_REQUIRED);
+		spdk_json_write_named_string(w, "subnqn", referral->trid.subnqn);
+		spdk_json_write_object_end(w);
+
 		spdk_json_write_object_end(w);
 	}
 
