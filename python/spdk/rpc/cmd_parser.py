d4d9a181db314d25e048ca14d48f0a7019ae5fa3
Konrad Sztyber <konrad.sztyber@intel.com>
2023-12-15 11:17:13 +0100

    rpc: strip go_client from global vars
    Commit a5ea50e718 ("go/rpc: Integration of Go client as shared library
    with rpc.py") added a --go-client option, but didn't add it to the
    stripped globals array, which means that it'd always be part of
    parameters for RPCs that use **kwargs, e.g.:
    
    $ scripts/rpc.py --dry-run nvmf_create_transport -t tcp
    Request:
    {
      "method": "nvmf_create_transport",
      "params": {
        "go_client": false,
        "trtype": "tcp",
        "zcopy": false,
        "no_srq": false,
        "c2h_success": true,
        "dif_insert_or_strip": false,
        "no_wr_batching": false,
        "disable_mappable_bar0": false,
        "disable_adaptive_irq": false,
        "disable_shadow_doorbells": false
      }
    }
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I7d1cb5d4509d8360f155df27c01d862591ab248e
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/21062
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <jim.harris@samsung.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- python/spdk/rpc/cmd_parser.py
+++ python/spdk/rpc/cmd_parser.py
@@ -3,7 +3,7 @@
 #  All rights reserved.
 
 args_global = ['server_addr', 'port', 'timeout', 'verbose', 'dry_run', 'conn_retries',
-               'is_server', 'rpc_plugin', 'called_rpc_name', 'func', 'client']
+               'is_server', 'rpc_plugin', 'called_rpc_name', 'func', 'client', 'go_client']
 
 
 def strip_globals(kwargs):
