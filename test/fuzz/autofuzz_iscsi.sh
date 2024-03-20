35212373c12f6e2cb5a4d15bf37cbf9e937c33f2
Kamil Godzwon <kamilx.godzwon@intel.com>
2024-03-20 07:26:28 -0400

    test/fuzz/iscsi: add --wait-for-rpc for ISCSI_APP parameters
    After running fuzzer it turned out that we have to add this
    parameter as it was done for test/iscsi_tgt/fuzz/fuzz.sh
    
    Signed-off-by: Kamil Godzwon <kamilx.godzwon@intel.com>
    Change-Id: I382a3ddfcf3718d23f89868fc35a014db2b41dfd
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/22412
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Michal Berger <michal.berger@intel.com>

--- test/fuzz/autofuzz_iscsi.sh
+++ test/fuzz/autofuzz_iscsi.sh
@@ -38,7 +38,7 @@ done
 
 timing_enter start_iscsi_tgt
 
-"${ISCSI_APP[@]}" -m $ISCSI_TEST_CORE_MASK &> $output_dir/iscsi_autofuzz_tgt_output.txt &
+"${ISCSI_APP[@]}" -m $ISCSI_TEST_CORE_MASK --wait-for-rpc &> $output_dir/iscsi_autofuzz_tgt_output.txt &
 iscsipid=$!
 
 trap 'killprocess $iscsipid; exit 1' SIGINT SIGTERM EXIT
