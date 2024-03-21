7755333c42e895a8df46ee3e8591fd29789b25ab
Michal Berger <michal.berger@intel.com>
2024-03-21 03:52:15 -0400

    test/common: Allow to override rpc_cmd()'s timeout
    This time tells read how long to wait for data in a pipe before
    aborting. For some RPCs, which for instance try to attach to multiple
    big nvme ctrls, the hard-coded timeout of 15s may be simply not
    enough.
    
    Change-Id: I3620477aa2f2b96a5822c4fbab4ff2da4c5e3f84
    Signed-off-by: Michal Berger <michal.berger@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/22452
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <jim.harris@samsung.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -561,7 +561,7 @@ function rpc_cmd() {
 		return 0
 	fi
 
-	while read -t 15 -ru $RPC_PIPE_OUTPUT rsp; do
+	while read -t "${RPC_PIPE_TIMEOUT:-15}" -ru $RPC_PIPE_OUTPUT rsp; do
 		if [[ $rsp == "**STATUS="* ]]; then
 			status[${rsp#*=}]=$rsp
 			if ((++status_number == cmds_number)); then
