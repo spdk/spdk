ed25c7b29bf65445ac658294e8c7cf4405623b9e
Marcin Spiewak <marcin.spiewak@intel.com>
2024-05-10 15:06:21 +0200

    bdev/nvme: wrong key in ERRLOG message
    Fixed issue found by Coverity.
    
    In nvme_ctrlr_create() function, wrong key was
    passed to SPDK_ERRLOG(). It was ctx->drv_opts.tls_psk,
    while it should be ctx->drv_opts.dhchap_ctrlr_key.
    Now it is fixed.
    
    Change-Id: I3e696452bb167a90668c66ca3bac4987fabbd3e6
    Signed-off-by: Marcin Spiewak <marcin.spiewak@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/23070
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Jim Harris <jim.harris@samsung.com>

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -5463,7 +5463,7 @@ nvme_ctrlr_create(struct spdk_nvme_ctrlr *ctrlr,
 					spdk_key_get_name(ctx->drv_opts.dhchap_ctrlr_key));
 			if (nvme_ctrlr->dhchap_ctrlr_key == NULL) {
 				SPDK_ERRLOG("Couldn't get a reference to the key '%s'\n",
-					    spdk_key_get_name(ctx->drv_opts.tls_psk));
+					    spdk_key_get_name(ctx->drv_opts.dhchap_ctrlr_key));
 				rc = -ENOKEY;
 				goto err;
 			}
