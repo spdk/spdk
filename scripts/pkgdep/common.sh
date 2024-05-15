spdk/spdk

commit fe09e7d494fee0be71428a047d03b02b5be7dd21

scripts/pkgdep: support shfmt for arm64
Get shfmt package according to arch.

Change-Id: Ib51feb64368e1ef559c886d9899dd7a1f28cff95
Signed-off-by: Richael Zhuang <richael.zhuang@arm.com>
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/9996
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Michal Berger <michalx.berger@intel.com>
Reviewed-by: Ben Walker <benjamin.walker@intel.com>
Reviewed-by: Jim Harris <james.r.harris@intel.com>

scripts/pkgdep/common.sh
@@ -25,17 +25,28 @@ install_shfmt() {
 	local shfmt_dir_out=${SHFMT_DIR_OUT:-/usr/bin}
 	local shfmt_url
 	local os
+	local arch
 
 	if hash "$shfmt" && [[ $("$shfmt" --version) == "v$shfmt_version" ]]; then
 		echo "$shfmt already installed"
 		return 0
 	fi 2> /dev/null
 
+	arch=$(uname -m)
 	os=$(uname -s)
 
+	case "$arch" in
+		x86_64) arch="amd64" ;;
+		aarch64) arch="arm" ;;
+		*)
+			echo "Not supported arch (${arch:-Unknown}), skipping"
+			return 0
+			;;
+	esac
+
 	case "$os" in
-		Linux) shfmt_url=https://github.com/mvdan/sh/releases/download/v$shfmt_version/shfmt_v${shfmt_version}_linux_amd64 ;;
-		FreeBSD) shfmt_url=https://github.com/mvdan/sh/releases/download/v$shfmt_version/shfmt_v${shfmt_version}_freebsd_amd64 ;;
+		Linux) shfmt_url=https://github.com/mvdan/sh/releases/download/v$shfmt_version/shfmt_v${shfmt_version}_linux_${arch} ;;
+		FreeBSD) shfmt_url=https://github.com/mvdan/sh/releases/download/v$shfmt_version/shfmt_v${shfmt_version}_freebsd_${arch} ;;
 		*)
 			echo "Not supported OS (${os:-Unknown}), skipping"
 			return 0

commit c3870302ff258b8c5f594a7c860b8d3e6c2d503d

scripts/pkgdep: Fix install_shfmt() under FreeBSD
The arch check under FreeBSD was unfortunately broken since
fe09e7d494. Fix it.

Change-Id: Ie43f0614c4380669c8709dadff80bd5dd82ff9ad
Signed-off-by: Michal Berger <michal.berger@intel.com>
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/23085
Reviewed-by: Jim Harris <jim.harris@samsung.com>
Community-CI: Mellanox Build Bot
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>

scripts/pkgdep/common.sh
@@ -56,6 +56,7 @@ install_shfmt() {
 	case "$arch" in
 		x86_64) arch="amd64" ;;
 		aarch64) arch="arm" ;;
+		amd64) ;; # FreeBSD
 		*)
 			echo "Not supported arch (${arch:-Unknown}), skipping"
 			return 0