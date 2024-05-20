91fcde065a5883d85ca1034a9a1b254e1eadbcad
Jim Harris <jim.harris@samsung.com>
2024-05-20 11:45:28 -0700

    rocksdb: use spdk_thread_send_msg() instead of spdk_event_call()
    All bdev calls must be made from an spdk_thread context. Use this instead of
    relying on hack in reactor code that sets the spdk_thread context before
    calling an event. This new approach is much cleaner and lets us remove the
    hack in an upcoming patch.
    
    Signed-off-by: Jim Harris <jim.harris@samsung.com>
    Change-Id: I66f5148fff4d74d5770bb6ecd4f89ba05201b78a
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/23263
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
    Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- lib/rocksdb/env_spdk.cc
+++ lib/rocksdb/env_spdk.cc
@@ -24,7 +24,7 @@ namespace rocksdb
 
 struct spdk_filesystem *g_fs = NULL;
 struct spdk_bs_dev *g_bs_dev;
-uint32_t g_lcore = 0;
+struct spdk_thread *g_app_thread;
 std::string g_bdev_name;
 volatile bool g_spdk_ready = false;
 volatile bool g_spdk_start_failure = false;
@@ -65,21 +65,9 @@ set_channel()
 }
 
 static void
-__call_fn(void *arg1, void *arg2)
-{
-	fs_request_fn fn;
-
-	fn = (fs_request_fn)arg1;
-	fn(arg2);
-}
-
-static void
 __send_request(fs_request_fn fn, void *arg)
 {
-	struct spdk_event *event;
-
-	event = spdk_event_allocate(g_lcore, __call_fn, (void *)fn, arg);
-	spdk_event_call(event);
+	spdk_thread_send_msg(g_app_thread, fn, arg);
 }
 
 static std::string
@@ -645,7 +633,7 @@ rocksdb_run(__attribute__((unused)) void *arg1)
 		exit(1);
 	}
 
-	g_lcore = spdk_env_get_first_core();
+	g_app_thread = spdk_get_thread();
 
 	printf("using bdev %s\n", g_bdev_name.c_str());
 	spdk_fs_load(g_bs_dev, __send_request, fs_load_cb, NULL);
