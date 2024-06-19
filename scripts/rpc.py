spdk/spdk

commit 679c3183e633c7fdf45b0d34d496b090a6d87853

lvol: set default timeout to 90.0 in bdev_lvol_create_lvstore
The function bdev_lvol_create with the default unmap clear method may
take more than the default 60.0 sec. Increase the timeout to 90.0 for
the function.

Fixes #3050

Change-Id: I6b8ac214b20601e7247ba789b485f53d3545955c
Signed-off-by: Marek Chomnicki <marek.chomnicki@intel.com>
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/22753
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
Reviewed-by: Michal Berger <michal.berger@intel.com>
Community-CI: Mellanox Build Bot

scripts/rpc.py
@@ -1969,6 +1969,9 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
 
     # lvol
     def bdev_lvol_create_lvstore(args):
+        # The default unmap clear method may take over 60.0 sec.
+        if args.timeout is None:
+            args.client.timeout = 90.0
         print_json(rpc.lvol.bdev_lvol_create_lvstore(args.client,
                                                      bdev_name=args.bdev_name,
                                                      lvs_name=args.lvs_name,

commit 6121d38ef43bbfdeb40b2d2961ba0dccd3360d5e

rpc.py: use user-specified timeout when executing script
Generally this was working, but bdev_lvol_create_lvstore recently
changed to set its own default (90 seconds) if the user had not
passed one.

The original script execution code didn't account for individual
RPCs doing this, which would result in this RPC and those following
in a script to take on the 90 second default.

Fixes: 679c3183 ("lvol: set default timeout to 90.0 in bdev_lvol_create_lvstore")

Signed-off-by: Jim Harris <jim.harris@samsung.com>
Change-Id: I2d5b88d74ff621392e06040609a305a8aaebcb6c
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/23766
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
Reviewed-by: Changpeng Liu <changpeng.liu@intel.com>
Community-CI: Mellanox Build Bot

scripts/rpc.py
@@ -3781,7 +3781,7 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
         args.func(args)
         check_called_name(args.called_rpc_name)
 
-    def execute_script(parser, client, fd):
+    def execute_script(parser, client, timeout, fd):
         executed_rpc = ""
         for rpc_call in map(str.rstrip, fd):
             if not rpc_call.strip():
@@ -3793,6 +3793,7 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
                 continue
             args = parser.parse_args(rpc_args)
             args.client = client
+            args.timeout = timeout
             try:
                 call_rpc_func(args)
             except JSONRPCException as ex:
@@ -3904,4 +3905,4 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
             print(ex.message)
             exit(1)
     else:
-        execute_script(parser, args.client, sys.stdin)
+        execute_script(parser, args.client, args.timeout, sys.stdin)