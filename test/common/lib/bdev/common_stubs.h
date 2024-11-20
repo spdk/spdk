ff173863b114ffb0d2b86e2825badcc504fc5fa1
Shuhei Matsumoto <smatsumoto@nvidia.com>
2024-11-20 13:49:57 +0900

    ut/bdev: Remove duplication with many stups among unit test files
    Move all duplicated stubs in bdev.c, mt/bdev.c, and part.c unit test
    files into the new file common_stubs.h in test/common/lib/bdev.
    
    Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
    Change-Id: Ic3d75821bf828e196fa576a18feae90d8bd2ffeb
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/25455
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Community-CI: Community CI Samsung <spdk.community.ci.samsung@gmail.com>
    Community-CI: Mellanox Build Bot

---
+++ test/common/lib/bdev/common_stubs.h
@@ -0,0 +1,22 @@
+/*   SPDX-License-Identifier: BSD-3-Clause
+ *   Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ */
+
+#include "spdk_internal/mock.h"
+
+DEFINE_STUB(spdk_notify_send, uint64_t, (const char *type, const char *ctx), 0);
+DEFINE_STUB(spdk_notify_type_register, struct spdk_notify_type *, (const char *type), NULL);
+DEFINE_STUB(spdk_memory_domain_get_dma_device_id, const char *, (struct spdk_memory_domain *domain),
+	    "test_domain");
+DEFINE_STUB(spdk_memory_domain_get_dma_device_type, enum spdk_dma_device_type,
+	    (struct spdk_memory_domain *domain), 0);
+DEFINE_STUB_V(spdk_accel_sequence_finish,
+	      (struct spdk_accel_sequence *seq, spdk_accel_completion_cb cb_fn, void *cb_arg));
+DEFINE_STUB_V(spdk_accel_sequence_abort, (struct spdk_accel_sequence *seq));
+DEFINE_STUB_V(spdk_accel_sequence_reverse, (struct spdk_accel_sequence *seq));
+DEFINE_STUB(spdk_accel_append_copy, int,
+	    (struct spdk_accel_sequence **seq, struct spdk_io_channel *ch, struct iovec *dst_iovs,
+	     uint32_t dst_iovcnt, struct spdk_memory_domain *dst_domain, void *dst_domain_ctx,
+	     struct iovec *src_iovs, uint32_t src_iovcnt, struct spdk_memory_domain *src_domain,
+	     void *src_domain_ctx, spdk_accel_step_cb cb_fn, void *cb_arg), 0);
+DEFINE_STUB(spdk_accel_get_memory_domain, struct spdk_memory_domain *, (void), NULL);
