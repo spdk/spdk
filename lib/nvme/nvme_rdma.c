4702174462af7f7a15319e6ea2b9fe4d6d8bc88c
Jim Harris <jim.harris@nvidia.com>
2025-04-18 04:24:40 -0700

    Revert "nvme/rdma: Add a field to mark a nvme_rdma_qpair used by an established connection"
    This reverts commit ea1b90747c5ffcb71d2a7f7c99425e3362b7fe5d.
    
    Fixes issue #3642.
    
    Change-Id: I809598fdda538fb54702d86c5b4a15c5bc621ba2
    Signed-off-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/25799
    Reviewed-by: Krzysztof Goreczny <krzysztof.goreczny@dell.com>
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- lib/nvme/nvme_rdma.c
+++ lib/nvme/nvme_rdma.c
@@ -240,7 +240,6 @@ struct nvme_rdma_qpair {
 
 	uint8_t					stale_conn_retry_count;
 	bool					need_destroy;
-	bool					connected;
 	TAILQ_ENTRY(nvme_rdma_qpair)		link_connecting;
 };
 
@@ -453,7 +452,6 @@ nvme_rdma_qpair_process_cm_event(struct nvme_rdma_qpair *rqpair)
 			rc = spdk_rdma_provider_qp_complete_connect(rqpair->rdma_qp);
 		/* fall through */
 		case RDMA_CM_EVENT_ESTABLISHED:
-			rqpair->connected = true;
 			accept_data = (struct spdk_nvmf_rdma_accept_private_data *)event->param.conn.private_data;
 			if (accept_data == NULL) {
 				rc = -1;
@@ -463,7 +461,6 @@ nvme_rdma_qpair_process_cm_event(struct nvme_rdma_qpair *rqpair)
 			}
 			break;
 		case RDMA_CM_EVENT_DISCONNECTED:
-			rqpair->connected = false;
 			rqpair->qpair.transport_failure_reason = SPDK_NVME_QPAIR_FAILURE_REMOTE;
 			break;
 		case RDMA_CM_EVENT_DEVICE_REMOVAL:
@@ -2191,7 +2188,7 @@ _nvme_rdma_ctrlr_disconnect_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvm
 	rqpair->state = NVME_RDMA_QPAIR_STATE_EXITING;
 
 	if (rqpair->cm_id) {
-		if (rqpair->rdma_qp && rqpair->connected) {
+		if (rqpair->rdma_qp) {
 			rc = spdk_rdma_provider_qp_disconnect(rqpair->rdma_qp);
 			if ((qpair->ctrlr != NULL) && (rc == 0)) {
 				rc = nvme_rdma_process_event_start(rqpair, RDMA_CM_EVENT_DISCONNECTED,
