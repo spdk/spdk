6adcfcf0a64044272ecabb0477669c44c0a51cd2
Jacek Kalwas <jacek.kalwas@nutanix.com>
2025-04-26 04:55:03 +0200

    bdev/nvme: adjust elapsed var type
    Elapsed var always has positive value so having int type is misleading.
    Likely it was defined as int type due to sign vs unsigned comparison
    compilation warning however it is done after loss timeout check for
    negative value so we can safely cast to suppress it.
    
    Change-Id: Icb73091f5dedfd14e8f0d64833b878b6b74916ea
    Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/25820
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -2098,15 +2098,16 @@ bdev_nvme_failover_trid(struct nvme_ctrlr *nvme_ctrlr, bool remove, bool start)
 static bool
 bdev_nvme_check_ctrlr_loss_timeout(struct nvme_ctrlr *nvme_ctrlr)
 {
-	int32_t elapsed;
+	uint32_t elapsed;
 
 	if (nvme_ctrlr->opts.ctrlr_loss_timeout_sec == 0 ||
 	    nvme_ctrlr->opts.ctrlr_loss_timeout_sec == -1) {
 		return false;
 	}
 
+	assert(nvme_ctrlr->opts.ctrlr_loss_timeout_sec >= 0);
 	elapsed = (spdk_get_ticks() - nvme_ctrlr->reset_start_tsc) / spdk_get_ticks_hz();
-	if (elapsed >= nvme_ctrlr->opts.ctrlr_loss_timeout_sec) {
+	if (elapsed >= (uint32_t)nvme_ctrlr->opts.ctrlr_loss_timeout_sec) {
 		return true;
 	} else {
 		return false;
