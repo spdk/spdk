87ffce61b5a245ff8c0d015cc33497675206eea1
Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
2025-09-05 16:44:22 +0200

    test/blobstore: modernize the three blobstore scripts
    Only intended as general cleanup, as those scripts were bit outdated compared
    to current test scripts in SPDK. The tests should run exactly the same after
    this change.
    
    One highlight is removal of exception for FreeBSD, lvol tests use aio/nbd and
    other similar tools so the SPDK_TEST_LVOL flag at least for now will cannot be
    enabled there anyway. No need to make an exception for blobstore tests here.
    
    Change-Id: Ic92c02f7504b4a1815c6d8c487577794b9da5f43
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26550
    Reviewed-by: Ben Walker <ben@nvidia.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- test/blobstore/blob_io_wait/blob_io_wait.sh
+++ test/blobstore/blob_io_wait/blob_io_wait.sh
@@ -3,51 +3,57 @@
 #  Copyright (C) 2018 Intel Corporation
 #  All rights reserved.
 #
+testdir=$(readlink -f "$(dirname "$0")")
+rootdir=$(readlink -f "$testdir/../../..")
 
-testdir=$(readlink -f $(dirname $0))
-rootdir=$(readlink -f $testdir/../../..)
-source $rootdir/test/common/autotest_common.sh
+source "$rootdir/test/common/autotest_common.sh"
 
-truncate -s 64M $testdir/aio.bdev
+function cleanup() {
+	rm -f "$testdir/bdevperf.json"
+	rm -f "$testdir/aio.bdev"
+}
 
-$rootdir/test/app/bdev_svc/bdev_svc --wait-for-rpc &
+bdevperf="$_examples_dir/bdevperf"
+
+truncate -s 64M "$testdir/aio.bdev"
+
+"$rootdir/test/app/bdev_svc/bdev_svc" --wait-for-rpc &
 bdev_svc_pid=$!
 
-trap 'rm -f $testdir/bdevperf.json; rm -f $testdir/aio.bdev; killprocess $bdev_svc_pid; exit 1' SIGINT SIGTERM EXIT
+trap 'cleanup; killprocess "$bdev_svc_pid"; exit 1' SIGINT SIGTERM EXIT
 
-waitforlisten $bdev_svc_pid
+waitforlisten "$bdev_svc_pid"
 # Minimal number of bdev io pool (128) and cache (1)
 $rpc_py iobuf_set_options --small-pool-count 8192 --large-pool-count 1024
 $rpc_py bdev_set_options --bdev-io-pool-size 128 --bdev-io-cache-size 1
 $rpc_py framework_start_init
-$rpc_py bdev_aio_create $testdir/aio.bdev aio0 4096
+$rpc_py bdev_aio_create "$testdir/aio.bdev" aio0 4096
 $rpc_py bdev_lvol_create_lvstore aio0 lvs0
 $rpc_py bdev_lvol_create -l lvs0 lvol0 32
-$rpc_py save_config > $testdir/bdevperf.json
+$rpc_py save_config > "$testdir/bdevperf.json"
 
-killprocess $bdev_svc_pid
+killprocess "$bdev_svc_pid"
 
-$rootdir/build/examples/bdevperf --json $testdir/bdevperf.json -q 128 -o 4096 -w write -t 5 -r /var/tmp/spdk.sock &
+$bdevperf --json "$testdir/bdevperf.json" -q 128 -o 4096 -w write -t 5 -r /var/tmp/spdk.sock &
 bdev_perf_pid=$!
-waitforlisten $bdev_perf_pid
+waitforlisten "$bdev_perf_pid"
 $rpc_py bdev_enable_histogram aio0 -e
 sleep 2
-$rpc_py bdev_get_histogram aio0 | $rootdir/scripts/histogram.py
+$rpc_py bdev_get_histogram aio0 | "$rootdir/scripts/histogram.py"
 $rpc_py bdev_enable_histogram aio0 -d
-wait $bdev_perf_pid
+wait "$bdev_perf_pid"
 
-$rootdir/build/examples/bdevperf --json $testdir/bdevperf.json -q 128 -o 4096 -w read -t 5 -r /var/tmp/spdk.sock &
+$bdevperf --json "$testdir/bdevperf.json" -q 128 -o 4096 -w read -t 5 -r /var/tmp/spdk.sock &
 bdev_perf_pid=$!
-waitforlisten $bdev_perf_pid
+waitforlisten "$bdev_perf_pid"
 $rpc_py bdev_enable_histogram aio0 -e
 sleep 2
-$rpc_py bdev_get_histogram aio0 | $rootdir/scripts/histogram.py
+$rpc_py bdev_get_histogram aio0 | "$rootdir/scripts/histogram.py"
 $rpc_py bdev_enable_histogram aio0 -d
-wait $bdev_perf_pid
+wait "$bdev_perf_pid"
 
-$rootdir/build/examples/bdevperf --json $testdir/bdevperf.json -q 128 -o 4096 -w unmap -t 1
-
+$bdevperf --json "$testdir/bdevperf.json" -q 128 -o 4096 -w unmap -t 1
 sync
-rm -f $testdir/bdevperf.json
-rm -f $testdir/aio.bdev
+
 trap - SIGINT SIGTERM EXIT
+cleanup
