c7d9a60dae14cf5595abc7251982a1f15da4f461
Boris Glimcher <Boris.Glimcher@emc.com>
2025-09-11 15:02:35 -0400

    scripts/pkgdep: add curl package to ubuntu
    curl is not shipping by default with ubuntu, so install it
    
    tested using:
    ```
        ./scripts/pkgdep.sh --all
        ./configure --enable-coverage
        source ./scripts/common.sh
        make -j
    ```
    
    and those images:
      - docker.io/library/ubuntu:20.04
      - docker.io/library/ubuntu:22.04
      - docker.io/library/ubuntu:24.04
      - docker.io/library/debian:12.12
      - docker.io/library/debian:11.11
    
    Change-Id: Ia789db9f3d8dee289339601cc5db151692c1dc11
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26604
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Karol Latecki <karol.latecki@nutanix.com>
    Community-CI: Mellanox Build Bot
    Reviewed-by: Michal Berger <michal.berger@nutanix.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>

--- scripts/pkgdep/debian.sh
+++ scripts/pkgdep/debian.sh
@@ -31,7 +31,8 @@ case "$VERSION_CODENAME" in
 esac
 
 apt-get install -y gcc g++ make libcunit1-dev libaio-dev libssl-dev libjson-c-dev libcmocka-dev uuid-dev libiscsi-dev \
-	libkeyutils-dev libncurses5-dev libncursesw5-dev python3 python3-pip python3-dev unzip libfuse3-dev patchelf
+	libkeyutils-dev libncurses5-dev libncursesw5-dev python3 python3-pip python3-dev unzip libfuse3-dev patchelf \
+	curl
 
 pip3 install ninja
 pip3 install meson
