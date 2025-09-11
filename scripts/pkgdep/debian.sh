9f1376321cbe0da095327fab9f4905ccec45d30e
Boris Glimcher <Boris.Glimcher@emc.com>
2025-09-11 15:41:45 -0400

    scripts/pkgdep: simplify ubuntu pkgdep
    pkgconf is newer supported version and present in
    all versions we officially support, see list below
    
    venv should alwasy be used to simplify installation
    and avoid conflicts and dependencies with system packages
    
    tested using:
    ```
        ./scripts/pkgdep.sh --all
        ./configure --enable-coverage
        source ./scripts/common.sh
        make -j
    ```
    
    and those images:
      - docker.io/library/ubuntu:20.04
      - docker.io/library/ubuntu:22.04
      - docker.io/library/ubuntu:24.04
      - docker.io/library/debian:12.12
      - docker.io/library/debian:11.11
    
    Change-Id: I6c654c35604d5723f5baf7e66c6b738d93a1264b
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26606
    Community-CI: Mellanox Build Bot
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Michal Berger <michal.berger@nutanix.com>

--- scripts/pkgdep/debian.sh
+++ scripts/pkgdep/debian.sh
@@ -6,34 +6,23 @@
 #  Copyright (c) 2022 Dell Inc, or its subsidiaries.
 #
 
-case "$VERSION_CODENAME" in
-	bookworm | noble)
-		# These ship with pip which enforces PEP668
-		apt-get install -y python3-venv
-		virtdir=${PIP_VIRTDIR:-/var/spdk/dependencies/pip}
-
-		mkdir --p "$virtdir"
-		pkgdep_toolpath pip "$virtdir/bin"
-
-		pip3() (
-			if [[ ! -e $virtdir/bin/activate ]]; then
-				python3 -m venv --upgrade-deps --system-site-packages "$virtdir"
-			fi
-			source "$virtdir/bin/activate"
-			"$virtdir/bin/pip3" "$@"
-		)
-
-		apt-get install -y pkgconf
-		;;
-	*)
-		apt-get install -y pkg-config
-		;;
-esac
-
 apt-get install -y gcc g++ make libcunit1-dev libaio-dev libssl-dev libjson-c-dev libcmocka-dev uuid-dev libiscsi-dev \
 	libkeyutils-dev libncurses5-dev libncursesw5-dev python3 python3-pip python3-dev unzip libfuse3-dev patchelf \
-	curl procps
+	curl procps pkgconf python3-venv
 
+# per PEP668 work inside virtual env
+virtdir=${PIP_VIRTDIR:-/var/spdk/dependencies/pip}
+if python3 -c 'import sys; exit(0 if sys.version_info >= (3,9) else 1)'; then
+	python3 -m venv --upgrade-deps --system-site-packages "$virtdir"
+else
+	# --upgrade-deps was introduced only in Python 3.9.0 (October 5, 2020).
+	python3 -m venv --system-site-packages "$virtdir"
+	"$virtdir"/bin/pip install --upgrade pip setuptools
+fi
+pkgdep_toolpath pip "$virtdir/bin"
+source "$virtdir/bin/activate"
+
+# install python packages
 pip3 install ninja
 pip3 install meson
 pip3 install pyelftools
