46e75c1d70d812e3d0e08a9a05610b2540a4def4
Valerii Hlushkov <valerii.hlushkov@datacore.com>
2025-09-13 12:19:45 +0000

    accel/cuda: add test scripts for xor/copy/fill accelerators
    The scripts allows testing and comparing of xor, copy and fill
    operations with accel_cuda and software accelerator modules
    using accel_perf utility.
    
    Change-Id: I6fe5a62c5132ec697de6218d690de99077af4cce
    Signed-off-by: Valerii Hlushkov <valerii.hlushkov@datacore.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26623
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Ben Walker <ben@nvidia.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>

---
+++ test/accel/scripts/accel_fill.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+#  SPDX-License-Identifier: BSD-3-Clause
+#  Copyright (c) 2025 StarWind Software, Inc.
+#  All rights reserved.
+#
+
+testdir=$(readlink -f $(dirname $0))
+rootdir=$(readlink -f $testdir/../../..)
+
+# default test parameters
+module=software
+size=65536
+mask=0x1
+nbufs=1
+queue=64
+
+[[ $1 != "" ]] && module=$1
+[[ $2 != "" ]] && size=$((1024 * $2))
+[[ $3 != "" ]] && mask=$3
+[[ $4 != "" ]] && [[ $4 -gt 0 && $4 -le 16 ]] && nbufs=$4
+[[ $5 != "" ]] && [[ $5 -gt 0 && $5 -lt 4096 ]] && queue=$5
+[[ $6 != "" ]] && option=$6
+
+echo "FILL with $module (block $size, core_mask $mask, nbufs $nbufs, queue $queue $option)..."
+
+if [ $module = "cuda" ]; then
+	$rootdir/build/examples/accel_perf -m $mask -q $queue -w fill -o $size -C $nbufs $option -r /var/tmp/spdk.sock --wait-for-rpc &
+	spdk_pid=$!
+	waitforlisten $spdk_pid
+	sleep 1
+
+	$rootdir/scripts/rpc.py cuda_scan_accel_module
+	$rootdir/scripts/rpc.py accel_assign_opc -o fill -m accel_cuda
+	$rootdir/scripts/rpc.py framework_start_init
+
+	echo "Waiting for accel_perf PID" $spdk_pid
+	wait $spdk_pid
+else
+	$rootdir/build/examples/accel_perf -m $mask -q $queue -w fill -o $size -C $nbufs $option
+fi
