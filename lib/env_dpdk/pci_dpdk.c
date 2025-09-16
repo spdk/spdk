90997994dae67a65361b51b5a67bf51097670ddd
Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
2025-09-16 20:06:44 +0200

    env: add support for DPDK 25.07
    While here, moved 'development' version of DPDK to 25.11.
    
    Change-Id: Ie59f95534f731e451afd957e1a4d290d0fcfa186
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26639
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>

--- lib/env_dpdk/pci_dpdk.c
+++ lib/env_dpdk/pci_dpdk.c
@@ -32,7 +32,7 @@ dpdk_pci_init(void)
 	 * Only DPDK in development has additional suffix past minor version.
 	 */
 	if (strlen(release) != 0) {
-		if (year == 25 && month == 7 && minor == 0) {
+		if (year == 25 && month == 11 && minor == 0) {
 			g_dpdk_fn_table = &fn_table_2211;
 			SPDK_NOTICELOG("In-development %s is used. There is no support for it in SPDK. "
 				       "Enabled only for validation.\n", rte_version());
@@ -75,8 +75,9 @@ dpdk_pci_init(void)
 		/* There were no changes between 22.11 and 24.*, so use the 22.11 implementation. */
 		g_dpdk_fn_table = &fn_table_2211;
 	} else if (year == 25) {
-		/* Only 25.03.0 is supported. */
-		if (month != 3 || minor != 0) {
+		/* Only 25.03.0 and 25.07.0 are supported. */
+		if ((month != 7 || minor != 0) &&
+		    (month != 3 || minor != 0)) {
 			goto not_supported;
 		}
 		/* There were no changes between 22.11 and 25.*, so use the 22.11 implementation. */
