a4752b45869a8fefb5d28180362678ed9c5359e9
Boris Glimcher <Boris.Glimcher@emc.com>
2025-11-07 18:39:28 -0500

    scripts/check_format: Remove pep8, pycodestyle
    Rely solely on ruff and mypy while checking the python code.
    
    Change-Id: I80a5bb899ec5231d416ec5601ef76870d7ccb4f8
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27059
    Reviewed-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Michal Berger <michal.berger@nutanix.com>
    Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -471,33 +471,8 @@ function check_attr_packed() {
 function check_python_style() {
 	local rc=0
 
-	if hash pycodestyle 2> /dev/null; then
-		PEP8=pycodestyle
-	elif hash pep8 2> /dev/null; then
-		PEP8=pep8
-	fi
-
-	if [ -n "${PEP8}" ]; then
-		echo -n "Checking Python ${PEP8} style..."
-
-		PEP8_ARGS=" --max-line-length=140"
-
-		error=0
-		git ls-files '*.py' | xargs -P$(nproc) -n1 $PEP8 $PEP8_ARGS > pep8.log || error=1
-		if [ $error -ne 0 ]; then
-			echo " Python formatting errors detected"
-			cat pep8.log
-			rc=1
-		else
-			echo " OK"
-		fi
-		rm -f pep8.log
-	else
-		echo "You do not have pycodestyle or pep8 installed so your Python style is not being checked!"
-	fi
-
 	if hash ruff 2> /dev/null; then
-		echo -n "Checking Python ruff style..."
+		echo -n "Linting Python with ruff..."
 		if out=$(ruff --config "$rootdir/python/pyproject.toml" check 2>&1); then
 			echo " OK"
 		else
@@ -513,7 +488,7 @@ function check_python_style() {
 	fi
 
 	if hash mypy 2> /dev/null; then
-		echo -n "Checking Python mypy style..."
+		echo -n "Performing static type checking with mypy..."
 		if out=$(mypy --config-file "$rootdir/python/pyproject.toml" python 2>&1); then
 			echo " OK"
 		else
