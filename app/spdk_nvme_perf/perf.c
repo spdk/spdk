spdk/spdk

commit 86cd9f5c2fef69b964250d88684c1bd1af89c03c

nvme/perf: make seed for PRNG thread local again
It may become a significant bottleneck for multi core case
due to very frequent cache invalidation (it is in/out parameter).

Even with current lightweight PRNG spdk_rand_xorshift64()
the gain could be 2-digit % in some environments.

For older rand_r it was even higher.

This effectively reverts change introduced by 6ee7dd5
https://review.spdk.io/c/spdk/spdk/+/7791.

Keeping zipf in ns_entry still makes sense, so no change there.

No change for bdevperf needed as job struct containing seed
is associated with one thread.

Signed-off-by: Maciej Szulik <maciej.szulik@intel.com>
Change-Id: I9de2460b2c6a264dc5d2c212162ff9ea98547ace
Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26373
Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
Community-CI: Mellanox Build Bot
Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
Reviewed-by: Jim Harris <jim.harris@nvidia.com>

app/spdk_nvme_perf/perf.c
@@ -88,7 +88,6 @@ struct ns_entry {
 	uint32_t		block_size;
 	uint32_t		md_size;
 	bool			md_interleave;
-	uint64_t		seed;
 	struct spdk_zipf	*zipf;
 	bool			pi_loc;
 	enum spdk_nvme_pi_type	pi_type;
@@ -775,7 +774,6 @@ register_file(const char *path)
 	entry->io_size_blocks = g_io_size_bytes / blklen;
 
 	if (g_is_random) {
-		entry->seed = rand();
 		if (g_zipf_theta > 0) {
 			entry->zipf = spdk_zipf_create(entry->size_in_ios, g_zipf_theta, 0);
 		}
@@ -1282,7 +1280,6 @@ register_ns(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_ns *ns)
 	entry->io_size_blocks = g_io_size_bytes / sector_size;
 
 	if (g_is_random) {
-		entry->seed = spdk_rand_xorshift64_seed();
 		if (g_zipf_theta > 0) {
 			entry->zipf = spdk_zipf_create(entry->size_in_ios, g_zipf_theta, 0);
 		}
@@ -1437,6 +1434,8 @@ register_ctrlr(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_trid_entry *trid_
 	}
 }
 
+static __thread uint64_t seed = 0;
+
 static inline void
 submit_single_io(struct perf_task *task)
 {
@@ -1450,7 +1449,7 @@ submit_single_io(struct perf_task *task)
 	if (entry->zipf) {
 		offset_in_ios = spdk_zipf_generate(entry->zipf);
 	} else if (g_is_random) {
-		rand_value = spdk_rand_xorshift64(&entry->seed);
+		rand_value = spdk_rand_xorshift64(&seed);
 		offset_in_ios = rand_value % entry->size_in_ios;
 	} else {
 		offset_in_ios = ns_ctx->offset_in_ios++;
@@ -1463,7 +1462,7 @@ submit_single_io(struct perf_task *task)
 
 	if ((g_rw_percentage == 100) ||
 	    (g_rw_percentage != 0 &&
-	     ((spdk_rand_xorshift64(&entry->seed) % 100) < (uint64_t)g_rw_percentage))) {
+	     ((spdk_rand_xorshift64(&seed) % 100) < (uint64_t)g_rw_percentage))) {
 		task->is_read = true;
 	} else {
 		task->is_read = false;
@@ -3117,6 +3116,7 @@ nvme_poll_ctrlrs(void *arg)
 	int rc;
 
 	spdk_unaffinitize_thread();
+	seed = spdk_rand_xorshift64_seed();
 
 	if (g_tpoint_group_mask) {
 		spdk_trace_register_user_thread();

commit 6d2a028bebdb5d5bf68a8f585cfbbc5443a217ba

nvme/perf: fix seed init on worker threads
Seed is used on worker threads and should be initialized there,
otherwise there is no randomness.

No need for admin thread (nvme_poll_ctrlrs) to initialize it
as seed is not used there.

It still seems beneficial in certain cases over ns local thread-shared
seed for randrw pattern.

The patch fixes "nvme/perf: make seed for PRNG thread local again"
https://review.spdk.io/c/spdk/spdk/+/26373

Fixes: 86cd9f5c2fef69b964250d88684c1bd1af89c03c
Signed-off-by: Maciej Szulik <maciej.szulik@intel.com>
Change-Id: I81a73ede980fe114dde02a172b048e37600b6bb5
Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27120
Community-CI: Mellanox Build Bot
Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
Reviewed-by: Jim Harris <jim.harris@nvidia.com>

app/spdk_nvme_perf/perf.c
@@ -1687,6 +1687,9 @@ work_fn(void *arg)
 	TAILQ_HEAD(, perf_task)	swap;
 	struct perf_task *task;
 
+	/* Initialize thread local seed. */
+	seed = spdk_rand_xorshift64_seed();
+
 	/* Allocate queue pairs for each namespace. */
 	TAILQ_FOREACH(ns_ctx, &worker->ns_ctx, link) {
 		if (init_ns_worker_ctx(ns_ctx) != 0) {
@@ -3116,7 +3119,6 @@ nvme_poll_ctrlrs(void *arg)
 	int rc;
 
 	spdk_unaffinitize_thread();
-	seed = spdk_rand_xorshift64_seed();
 
 	if (g_tpoint_group_mask) {
 		spdk_trace_register_user_thread();