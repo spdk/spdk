e7e2d543c90d2c7541b0966d3bcd2ae740b58367
Karol Latecki <karol.latecki@nutanix.com>
2025-12-12 10:38:10 +0100

    python/spdk: fix mypy module import error
    Mypy 1.19.0 complains about module not possible
    to import. Expand existing "ignore" section with
    appropriate ignore type.
    
    Change-Id: Ib1637fab2de8ccf2f5224cf6a1db407ab35b0da9
    Signed-off-by: Karol Latecki <karol.latecki@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27318
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Reviewed-by: Jacek Kalwas <jacek.kalwas@nutanix.com>

--- python/spdk/__init__.py
+++ python/spdk/__init__.py
@@ -4,6 +4,6 @@
 
 try:
     # version.py is generated during the build, so ignore it if it doesn't exist
-    from .version import __version__  # type: ignore[import-untyped]
+    from .version import __version__  # type: ignore[import-untyped,import-not-found]
 except ModuleNotFoundError:
     __version__ = '0.0'
