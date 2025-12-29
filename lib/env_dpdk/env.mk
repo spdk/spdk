6c1799bd0870d094e82886c488e33d7fbde72d97
Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
2025-12-29 10:54:41 +0100

    dpdkbuild: explicitly link UADK when requested
    Since UADK was explicitly requested via CONFIG_DPDK_UADK, it makes sense
    to add it to DPDK lib list to be linked. Rather than skip if the DPDK version
    does not have this library.
    
    Change-Id: Ief11b1f4c7b0a9bcdc4de3a11d63c699ad549c1f
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27384
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Community-CI: Mellanox Build Bot

--- lib/env_dpdk/env.mk
+++ lib/env_dpdk/env.mk
@@ -72,9 +72,7 @@ DPDK_LIB_LIST += rte_common_mlx5 rte_bus_auxiliary rte_crypto_mlx5
 endif
 
 ifeq ($(CONFIG_DPDK_UADK),y)
-ifneq (, $(wildcard $(DPDK_LIB_DIR)/librte_crypto_uadk.*))
 DPDK_LIB_LIST += rte_crypto_uadk
-endif
 endif
 endif
 
@@ -84,9 +82,7 @@ ifneq (, $(wildcard $(DPDK_LIB_DIR)/librte_compress_isal.*))
 DPDK_LIB_LIST += rte_compress_isal
 endif
 ifeq ($(CONFIG_DPDK_UADK),y)
-ifneq (, $(wildcard $(DPDK_LIB_DIR)/librte_compress_uadk.*))
 DPDK_LIB_LIST += rte_compress_uadk
-endif
 endif
 endif
 
