f0e09f700d42e09624c13a45035cdb5c65fe742e
Boris Glimcher <Boris.Glimcher@emc.com>
2026-01-03 18:42:34 -0500

    lib/ublk: combine RPC params to the same struct
    commit 9b8579e4af16f0826736c09db858e0737f7556c5
    introduced optional user copy control flag to RPC
    but for some reason in a separate struct...
    
    this patch combines the 2 structs for simplicity
    and easy parsing/linting and schema compliance
    
    Change-Id: Id24d0a5af911ba646d9851cb2180e0dbb4b169bf
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27404
    Reviewed-by: Changpeng Liu <changpeliu@tencent.com>
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>

--- lib/ublk/ublk_rpc.c
+++ lib/ublk/ublk_rpc.c
@@ -13,10 +13,12 @@
 
 struct rpc_ublk_create_target {
 	char		*cpumask;
+	bool		disable_user_copy;
 };
 
 static const struct spdk_json_object_decoder rpc_ublk_create_target_decoders[] = {
 	{"cpumask", offsetof(struct rpc_ublk_create_target, cpumask), spdk_json_decode_string, true},
+	{"disable_user_copy", offsetof(struct rpc_ublk_create_target, disable_user_copy), spdk_json_decode_bool, true},
 };
 
 static void
@@ -32,15 +34,15 @@ rpc_ublk_create_target(struct spdk_jsonrpc_request *request, const struct spdk_j
 	struct rpc_ublk_create_target req = {};
 
 	if (params != NULL) {
-		if (spdk_json_decode_object_relaxed(params, rpc_ublk_create_target_decoders,
-						    SPDK_COUNTOF(rpc_ublk_create_target_decoders),
-						    &req)) {
+		if (spdk_json_decode_object(params, rpc_ublk_create_target_decoders,
+					    SPDK_COUNTOF(rpc_ublk_create_target_decoders),
+					    &req)) {
 			SPDK_ERRLOG("spdk_json_decode_object failed\n");
 			rc = -EINVAL;
 			goto invalid;
 		}
 	}
-	rc = ublk_create_target(req.cpumask, params);
+	rc = ublk_create_target(req.cpumask, req.disable_user_copy);
 	if (rc != 0) {
 		goto invalid;
 	}
