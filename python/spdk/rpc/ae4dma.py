2d2ac103b673d5333af3adfcde485735d67bfbcc
Boris Glimcher <Boris.Glimcher@emc.com>
2026-01-17 20:14:08 -0500

    python/rpc: remove ae4dma module
    the modules in this folder were deprecated a long time ago
    no need for a simple python function to just invoke call method
    can call it directly.
    
    so removing this code, probably due to timing of this patch
    and removal of other modules was off, so this is the only leftover.
    
    Change-Id: I9118f25f61808e2aaca229954bd3fd99f03dc73e
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27515
    Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Reviewed-by: Mateusz Kozlowski <mateusz.kozlowski@solidigm.com>

--- python/spdk/rpc/ae4dma.py
+++
@@ -1,8 +0,0 @@
-#  SPDX-License-Identifier: BSD-3-Clause
-#  Copyright (C) 2024 Advanced Micro Devices, Inc
-#  All rights reserved.
-
-def ae4dma_scan_accel_module(client):
-    """Enable AE4DMA accel module.
-    """
-    return client.call('ae4dma_scan_accel_module')
