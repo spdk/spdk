spdk/spdk

commit 57fa5cff58538476b0eb5d63d9ba62f79d9ee7d2

nvmf: add utils to copy sn, mn
It is a prework to use it in incoming spdk_nvmf_subsystem_create_ext
function.

While here add missing rc check of snprintf.

Change-Id: I6b9ff64d795989a848df36150432862acd51536e
Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27789
Reviewed-by: Jim Harris <jim.harris@nvidia.com>
Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
Community-CI: Mellanox Build Bot

lib/nvmf/nvmf_rpc.c
@@ -412,7 +412,8 @@ rpc_nvmf_create_subsystem(struct spdk_jsonrpc_request *request,
 	}
 
 	if (req->serial_number) {
-		if (spdk_nvmf_subsystem_set_sn(subsystem, req->serial_number)) {
+		rc = nvmf_subsystem_copy_sn(subsystem->sn, req->serial_number, sizeof(subsystem->sn));
+		if (rc < 0) {
 			SPDK_ERRLOG("Subsystem %s: invalid serial number '%s'\n", req->nqn, req->serial_number);
 			spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
 							     "Invalid SN %s", req->serial_number);
@@ -421,7 +422,8 @@ rpc_nvmf_create_subsystem(struct spdk_jsonrpc_request *request,
 	}
 
 	if (req->model_number) {
-		if (spdk_nvmf_subsystem_set_mn(subsystem, req->model_number)) {
+		rc = nvmf_subsystem_copy_mn(subsystem->mn, req->model_number, sizeof(subsystem->mn));
+		if (rc < 0) {
 			SPDK_ERRLOG("Subsystem %s: invalid model number '%s'\n", req->nqn, req->model_number);
 			spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
 							     "Invalid MN %s", req->model_number);

lib/nvmf/nvmf_internal.h
@@ -709,4 +709,7 @@ nvmf_get_transport_poll_group(struct spdk_nvmf_poll_group *group,
  */
 uint16_t nvmf_subsystem_gen_cntlid(struct spdk_nvmf_subsystem *subsystem);
 
+int nvmf_subsystem_copy_sn(char *dst, const char *sn, size_t size);
+int nvmf_subsystem_copy_mn(char *dst, const char *mn, size_t size);
+
 #endif /* __NVMF_INTERNAL_H__ */

lib/nvmf/subsystem.c
@@ -2654,11 +2654,11 @@ spdk_nvmf_subsystem_get_sn(const struct spdk_nvmf_subsystem *subsystem)
 }
 
 int
-spdk_nvmf_subsystem_set_sn(struct spdk_nvmf_subsystem *subsystem, const char *sn)
+nvmf_subsystem_copy_sn(char *dst, const char *sn, size_t size)
 {
 	size_t len, max_len;
 
-	max_len = sizeof(subsystem->sn) - 1;
+	max_len = size - 1;
 	len = strlen(sn);
 	if (len > max_len) {
 		SPDK_DEBUGLOG(nvmf, "Invalid sn \"%s\": length %zu > max %zu\n",
@@ -2672,9 +2672,13 @@ spdk_nvmf_subsystem_set_sn(struct spdk_nvmf_subsystem *subsystem, const char *sn
 		return -1;
 	}
 
-	snprintf(subsystem->sn, sizeof(subsystem->sn), "%s", sn);
+	return snprintf(dst, size, "%s", sn) < 0 ? -1 : 0;
+}
 
-	return 0;
+int
+spdk_nvmf_subsystem_set_sn(struct spdk_nvmf_subsystem *subsystem, const char *sn)
+{
+	return nvmf_subsystem_copy_sn(subsystem->sn, sn, sizeof(subsystem->sn));
 }
 
 const char *
@@ -2684,14 +2688,14 @@ spdk_nvmf_subsystem_get_mn(const struct spdk_nvmf_subsystem *subsystem)
 }
 
 int
-spdk_nvmf_subsystem_set_mn(struct spdk_nvmf_subsystem *subsystem, const char *mn)
+nvmf_subsystem_copy_mn(char *dst, const char *mn, size_t size)
 {
 	size_t len, max_len;
 
 	if (mn == NULL) {
 		mn = MODEL_NUMBER_DEFAULT;
 	}
-	max_len = sizeof(subsystem->mn) - 1;
+	max_len = size - 1;
 	len = strlen(mn);
 	if (len > max_len) {
 		SPDK_DEBUGLOG(nvmf, "Invalid mn \"%s\": length %zu > max %zu\n",
@@ -2705,9 +2709,13 @@ spdk_nvmf_subsystem_set_mn(struct spdk_nvmf_subsystem *subsystem, const char *mn
 		return -1;
 	}
 
-	snprintf(subsystem->mn, sizeof(subsystem->mn), "%s", mn);
+	return snprintf(dst, size, "%s", mn) < 0 ? -1 : 0;
+}
 
-	return 0;
+int
+spdk_nvmf_subsystem_set_mn(struct spdk_nvmf_subsystem *subsystem, const char *mn)
+{
+	return nvmf_subsystem_copy_mn(subsystem->mn, mn, sizeof(subsystem->mn));
 }
 
 const char *

commit 98a4788e6f2724f464696da4acead76de4c9c789

nvmf/rpc: fix subsystem leak after invalid cntlid
A successful serial or model number copy leaves rc set to zero. If
controller ID range validation then fails, the cleanup condition does
not destroy the newly registered subsystem.

Destroy the newly created subsystem directly when controller ID
validation fails or the start request cannot be submitted. If an
accepted start request later fails, clean it up in the completion
callback.

Add a regression test that retries the same NQN after the failed RPC.

Fixes: 57fa5cff5853 ("nvmf: add utils to copy sn, mn")
Change-Id: I21a62977487f707f0aeee17c25ad8502e5f8fc3e
Signed-off-by: Amit Engel <amit.engel@dell.com>
Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/29121
Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
Reviewed-by: Jim Harris <jim.harris@nvidia.com>
Reviewed-by: Ben Walker <ben@nvidia.com>

lib/nvmf/nvmf_rpc.c
@@ -298,7 +298,7 @@ rpc_nvmf_create_subsystem(struct spdk_jsonrpc_request *request,
 	struct spdk_nvmf_subsystem_opts opts;
 	struct spdk_nvmf_subsystem *subsystem = NULL;
 	struct spdk_nvmf_tgt *tgt;
-	int rc = -1;
+	int rc;
 
 	req.min_cntlid = NVMF_MIN_CNTLID;
 	req.max_cntlid = NVMF_MAX_CNTLID;
@@ -362,6 +362,7 @@ rpc_nvmf_create_subsystem(struct spdk_jsonrpc_request *request,
 			    req.max_cntlid);
 		spdk_jsonrpc_send_error_response_fmt(request, SPDK_JSONRPC_ERROR_INVALID_PARAMS,
 						     "Invalid cntlid range [%u-%u]", req.min_cntlid, req.max_cntlid);
+		spdk_nvmf_subsystem_destroy(subsystem, NULL, NULL);
 		goto cleanup;
 	}
 
@@ -371,14 +372,11 @@ rpc_nvmf_create_subsystem(struct spdk_jsonrpc_request *request,
 	if (rc) {
 		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
 						 "Failed to start subsystem");
+		spdk_nvmf_subsystem_destroy(subsystem, NULL, NULL);
 	}
 
 cleanup:
 	free_rpc_nvmf_create_subsystem(&req);
-
-	if (rc && subsystem) {
-		spdk_nvmf_subsystem_destroy(subsystem, NULL, NULL);
-	}
 }
 SPDK_RPC_REGISTER("nvmf_create_subsystem", rpc_nvmf_create_subsystem, SPDK_RPC_RUNTIME)
 

test/nvmf/target/invalid.sh
@@ -70,8 +70,12 @@ out=$("$rpc" nvmf_subsystem_remove_listener "$nqn" -t "$TEST_TRANSPORT" -a "$IP"
 [[ $out != *"Unable to stop listener."* ]]
 
 # Attempt to create subsystem with invalid controller ID range - outside [1, 0xffef]
-out=$("$rpc" nvmf_create_subsystem "$nqn$RANDOM" -i 0 2>&1) && false
+# A valid serial used to leave rc set to 0. Reusing the NQN verifies failure cleanup.
+invalid_nqn=$nqn$RANDOM
+out=$("$rpc" nvmf_create_subsystem "$invalid_nqn" -s SPDK001 -i 0 2>&1) && false
 [[ $out == *"Invalid cntlid range"* ]]
+"$rpc" nvmf_create_subsystem "$invalid_nqn"
+"$rpc" nvmf_delete_subsystem "$invalid_nqn"
 out=$("$rpc" nvmf_create_subsystem "$nqn$RANDOM" -i 65520 2>&1) && false
 [[ $out == *"Invalid cntlid range"* ]]
 out=$("$rpc" nvmf_create_subsystem "$nqn$RANDOM" -I 0 2>&1) && false